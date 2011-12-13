b59555476ccb4b5476fa46d645d2e3cde10f7754
ChooJoy <choojoy.work@gmail.com>
2011-12-14 00:46:57 +0400

    filters

---
+++ site/frontend/helpers/Filters.php
@@ -0,0 +1,28 @@
+<?php
+
+class Filters
+{
+	public static function add_nofollow($value)
+	{
+		$dom = new DOMDocument;
+		$dom->loadHTML($value);
+		$dom->strictErrorChecking = false;
+
+		$links = $dom->getElementsByTagName('a');
+		foreach ($links as $link)
+		{
+			$href = $link->getAttribute('href');
+			
+			if (! $link->hasAttribute('rel') AND strpos($href, 'happy-giraffe.ru') === false)
+			{
+				$link->setAttribute('rel', 'nofollow');
+			}
+			elseif ($link->getAttribute('rel') == 'nofollow')
+			{
+				$link->removeAttribute('rel');
+			}
+		}
+
+		return $dom->saveHTML();
+	}
+}

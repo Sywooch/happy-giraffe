926f5a1d4b8b50fa62739423f52d03deda300818
Alex Kireev <alexk984@gmail.com>
2011-12-14 14:14:45 +0400

    china method design #7

--- site/frontend/modules/babySex/views/default/china.php
+++ site/frontend/modules/babySex/views/default/china.php
@@ -5,71 +5,6 @@
 $year = date('Y');
 $model = new ChinaCalendarForm();
 ?>
-<style type="text/css">
-    input {
-        border: 1px solid #000;
-    }
-
-    table td.boy {
-        background: #0babd9;
-    }
-
-    table td.girl {
-        background: #d921cc;
-    }
-
-    table.calendar-table {
-        border-collapse: separate;
-        border-spacing: 5px;
-    }
-
-    .sex-test-table-boy div {
-        height: 20px;
-        width: 30px;
-        background: #0babd9;
-        margin-right: 10px;
-    }
-
-    .sex-test-table-cur-boy {
-        border: 3px solid #000;
-    }
-
-    .sex-test-table-girl div {
-        height: 20px;
-        width: 30px;
-        margin-right: 10px;
-        background: #d921cc;
-    }
-
-    .sex-test-table-cur-girl {
-        border: 3px solid #000;
-    }
-
-    #china-calendar-result {
-        margin: 30px 0;
-    }
-
-    #china-calendar-result .boy {
-        width: 30px;
-        height: 30px;
-        float: left;
-        background: #0babd9;
-        margin: 5px;
-    }
-
-    #china-calendar-result .girl {
-        width: 30px;
-        height: 30px;
-        float: left;
-        margin: 5px;
-        background: #d921cc;
-    }
-
-    #japan-result table td {
-        width: 30px;
-        height: 30px;
-    }
-</style>
 <script type="text/javascript">
     var arr = new Array(
         new Array(2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1),
@@ -107,52 +42,76 @@ $model = new ChinaCalendarForm();
         var d = new Date();
         year = d.getFullYear();
 
-        $('#china-submit').click(function () {
+        $('.calc_bt').click(function () {
             var d = new Date();
-            var baby_m = d.getMonth() + 1;
-            var baby_y = $('#china-conception-y').val();
-            var mother_m = parseInt($('#china-mother-m').val());
-            var baby_m = $('#china-conception-m').val();
-            var mother_y = $('#china-mother-y').val();
+            var baby_y = parseInt($('#child_yr_cal').val());
+            var mother_m = parseInt($('#mam_mn_cal').val());
+            var baby_m = parseInt($('#ch_mn_cal').val());
+            var mother_y = parseInt($('#mam_yr_cal').val());
             year = baby_y;
 
             var age = baby_y - mother_y;
             if (baby_m <= mother_m)
                 var age = baby_y - mother_y - 1;
+            if (age < 18 || age > 45){
+                $('#china-calendar-result').html('');
+                $('.wh_wait').hide();
+                return false;
+            }
 
             ShowCalendar();
+
+            var gender = GetGenderFromAge(age, baby_m);
+            $('.wh_wait').hide();
+            if (gender == 1) {
+                $('.wh_son').show();
+            }
+            if (gender == 2) {
+                $('.wh_daughter').show();
+            }
+
             return false;
         });
 
         $('body').delegate('#prev', 'click', function () {
-            var mother_y = $('#china-mother-y').val();
+            var mother_y = $('#mam_yr_cal').val();
             var age = year - mother_y - 1;
-            if (age < 18)
+            if (age < 18  || (age==18 && $('#mam_mn_cal').val() == 12))
                 return false;
-
             year--;
             ShowCalendar();
+            if (age == 18 || (age==19 && $('#mam_mn_cal').val() == 12))
+                $('#prev').removeClass('l_arr_mth_active').addClass('l_arr_mth');
+
             return false;
         });
 
         $('body').delegate('#next', 'click', function () {
-            var mother_y = $('#china-mother-y').val();
+            var mother_y = $('#mam_yr_cal').val();
             var age = year - mother_y;
             if (age > 45)
                 return false;
+
             year++;
             ShowCalendar();
+            if (age == 45)
+                $('#next').removeClass('r_arr_mth_active').addClass('r_arr_mth');
+
             return false;
         });
     });
 
     function ShowCalendar() {
-        var calendar2_html = '<div class="years">';
-        calendar2_html += '<a id="prev" href="#">prev</a><br>';
+        var calendar2_html = '<div class="month_calendar"><div class="choice_month">' +
+            '<a href="#" id="prev" class="l_arr_mth_active">&larr;</a>' +
+            '<a href="#" id="next"  class="r_arr_mth_active">&rarr;</a>' +
+            '<span>' + year + '</span>' +
+            '</div><table class="calendar_body"><tr><th>Янв</th><th>Фев</th><th>Мар</th><th>Апр</th><th>Май</th>' +
+            '<th>Июн</th><th>Июл</th><th>Авг</th><th>Сен</th><th>Окт</th><th>Ноя</th><th>Дек</th></tr><tr>';
 
         //calc mother age
-        var mother_m = parseInt($('#china-mother-m').val());
-        var mother_y = parseInt($('#china-mother-y').val());
+        var mother_m = parseInt($('#mam_mn_cal').val());
+        var mother_y = parseInt($('#mam_yr_cal').val());
 
         for (var i = 1; i <= 12; i++) {
             var age = year - mother_y;
@@ -160,16 +119,18 @@ $model = new ChinaCalendarForm();
                 var age = year - mother_y - 1;
 
             var gender = GetGenderFromAge(age, i);
+            var cell_class = '';
             if (gender == 1)
-                calendar2_html += "<div class='boy'></div>";
+                cell_class = "cal_item boy_day";
             if (gender == 2)
-                calendar2_html += "<div class='girl'></div>";
-            if (gender == 0)
-                calendar2_html += "<div></div>";
+                cell_class = "cal_item girl_day";
+            if (year == $('#child_yr_cal').val() && i == $('#ch_mn_cal').val())
+                cell_class += ' active_item';
+
+            calendar2_html += "<td><div class='" + cell_class + "'></div></td>";
         }
-        calendar2_html += '</div>';
-        calendar2_html += '<br><a id="next" href="#">next</a>';
-        $('#china-calendar-result').html(year + '<br>' + calendar2_html);
+        calendar2_html += '</tr></table></div>';
+        $('#china-calendar-result').html(calendar2_html);
     }
 
     function GetGenderFromAge(age, month) {
@@ -178,34 +139,66 @@ $model = new ChinaCalendarForm();
         if (age - 45 > 0)
             return 0;
         var result = arr[age - 18][month - 1];
-//        console.log(result);
         return result;
     }
 </script>
-<div id="china-calendar">
+
+<div class="child_sex_china_banner">
     <?php $form = $this->beginWidget('CActiveForm', array(
     'id' => 'china-calendar-form',
     'enableAjaxValidation' => false,
 ));?>
-
-    <big>Год и месяц рождения матери:</big>
-    <?php echo $form->dropDownList($model, 'mother_m', HDate::ruMonths(), array('id' => 'china-mother-m', 'class' => 'wid100')); ?>
-    <?php echo $form->dropDownList($model, 'mother_y', HDate::Range($year - 18, $year - 46), array('id' => 'china-mother-y', 'class' => 'wid100')); ?>
-    <br>
-    <big>Месяц зачатия</big>
-    <?php echo $form->dropDownList($model, 'baby_m', HDate::ruMonths(), array('id' => 'china-conception-m', 'class' => 'wid100')); ?>
-    <?php echo $form->dropDownList($model, 'baby_y', HDate::Range($year + 2, 2000), array('id' => 'china-conception-y', 'class' => 'wid100')); ?>
-    <br>
+    <div class="mam_bd">
+        <span class="title_pt_bn">Месяц и год рождения матери:</span>
+        <ul class="lists_td">
+            <li>
+                <?php echo $form->dropDownList($model, 'mother_m', HDate::ruMonths(), array('id' => 'mam_mn_cal', 'class' => 'mn_cal')); ?>
+            </li>
+            <li>
+                <?php echo $form->dropDownList($model, 'mother_y', HDate::Range($year - 46, $year - 18), array('id' => 'mam_yr_cal', 'class' => 'yr_cal')); ?>
+            </li>
+        </ul>
+    </div>
+    <!-- .mam_bd -->
+    <div class="child_bd">
+        <span class="title_pt_bn"><ins>Месяц и год зачатия ребенка:</ins></span>
+        <ul class="lists_td">
+            <li>
+                <?php echo $form->dropDownList($model, 'baby_m', HDate::ruMonths(), array('id' => 'ch_mn_cal', 'class' => 'mn_cal')); ?>
+            </li>
+            <li>
+                <?php echo $form->dropDownList($model, 'baby_y', HDate::Range($year - 10, $year + 2), array('id' => 'child_yr_cal', 'class' => 'yr_cal')); ?>
+            </li>
+        </ul>
+    </div>
+    <!-- .child_bd -->
     <?php echo $form->hiddenField($model, 'review_year', array('id' => 'china_review_year')) ?>
-
-    <?php echo CHtml::link('<span><span>Рассчитать</span></span>', '', array(
-    'class' => 'btn btn-yellow-medium',
-    'id' => 'china-submit'
-)); ?>
-
+    <input type="button" class="calc_bt" value="Рассчитать"/>
     <?php $this->endWidget(); ?>
-</div>
+</div><!-- .child_sex_china_banner -->
+
 <div id="china-calendar-result">
 
 </div>
 <div class="clear"></div>
+
+<div class="wh_wait wh_daughter" style="display: none;">
+    <span class="title_wh_wait">У Вас будет дочь</span>
+
+    <p>Одним из основных свидетельств правильного течения беременности является набор веса согласно принятым нормам.
+        Оптимальный набор веса при беременности — это 10–14 кг. Набираемый вес при беременности складывается из
+        нескольких показателей: вес ребенка, матки, околоплодных вод, плаценты, а также увеличиваются молочные железы,
+        объем циркулирующей крови, ну и, конечно, появляется запас жировой ткани. Желательно, чтобы набор веса при
+        беременности происходил постепенно, без рывков.ткани. Желательно, чтобы набор веса при беременности происходил
+        постепенно, без рывков.ткани. Желательно, чтобы набор веса при беременности происходил постепенно.</p>
+</div><!-- .wh_wait -->
+<div class="wh_wait wh_son" style="display: none;">
+    <span class="title_wh_wait">У Вас будет сын</span>
+
+    <p>Одним из основных свидетельств правильного течения беременности является набор веса согласно принятым нормам.
+        Оптимальный набор веса при беременности — это 10–14 кг. Набираемый вес при беременности складывается из
+        нескольких показателей: вес ребенка, матки, околоплодных вод, плаценты, а также увеличиваются молочные железы,
+        объем циркулирующей крови, ну и, конечно, появляется запас жировой ткани. Желательно, чтобы набор веса при
+        беременности происходил постепенно, без рывков.ткани. Желательно, чтобы набор веса при беременности происходил
+        постепенно, без рывков.ткани. Желательно, чтобы набор веса при беременности происходил постепенно.</p>
+</div><!-- .wh_wait -->

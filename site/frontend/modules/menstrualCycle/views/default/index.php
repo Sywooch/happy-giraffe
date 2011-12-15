a74de0ec8170438af390babc5859504cabc3af4b
Alex Kireev <alexk984@gmail.com>
2011-12-15 16:14:31 +0400

    blood group anhance #7

--- site/frontend/modules/menstrualCycle/views/default/index.php
+++ site/frontend/modules/menstrualCycle/views/default/index.php
@@ -38,10 +38,6 @@ $model = new MenstrualCycleForm();?>
                 return false;
             var year = $('#review_year').val();
 
-//            var d = new Date();
-//            if (month == d.getMonth() + 1 && year == d.getFullYear())
-//                return false;
-
             month--;
             if (month == 0) {
                 month = 12;
@@ -81,30 +77,7 @@ $model = new MenstrualCycleForm();?>
 //                        $('#result').animate({opacity:1}, 'fast');
 //                    });
 
-//                    $('#result2').html(data);
-//                    $('#result > .mother_calendar .calendar_body').animate({opacity:0}, 'fast', 'swing', function () {
-//                        $('#result > .mother_calendar .calendar_body').html($('#result2 > .mother_calendar .calendar_body').html());
-//                        $('#result > .mother_calendar .calendar_body').animate({opacity:1}, 'fast', 'swing');
-//                    });
-//                    if (started) {
-//                        $('#result > .mother_calendar .choice_month span').animate({opacity:0}, 'fast', 'swing', function () {
-//                            $('#result > .mother_calendar .choice_month span').html($('#result2 > .mother_calendar .choice_month span').html());
-//                            $('#result > .mother_calendar .choice_month span').animate({opacity:1}, 'fast');
-//                        });
-//                    }
-//                    if (started) {
-//                        $('#next-m .next_month_cal .mother_calendar').animate({opacity:0}, 300, 'swing', function () {
-//                            $('#next-m .next_month_cal .mother_calendar').html($('#result2 .next_month_cal .mother_calendar').html());
-//                            $('#next-m .next_month_cal .mother_calendar').animate({opacity:1}, 300);
-//                        });
-//                    }
-//                    else {
-//                        $('#next-m').animate({opacity:0}, 1, 'swing', function () {
-//                            $('#next-m').html($('#result2 .bottom-wrap').html());
-//                            $('#next-m').animate({opacity:1}, 300);
-//                        });
-//                        started = true;
-//                    }
+                    started = true;
                 }
             });
         }

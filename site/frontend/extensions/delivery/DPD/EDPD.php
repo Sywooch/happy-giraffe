2811004559cd0370a15ed799403de0d4267c42b9
Alex Dulub <dulubao@gmail.com>
2011-12-19 14:27:23 +0200

    dpd

--- site/frontend/extensions/delivery/DPD/EDPD.php
+++ site/frontend/extensions/delivery/DPD/EDPD.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This class represents table 'shop_delivery_edpd';
+ * @property $id
+ * @property $weight
+ * @property $city
+ * @property $zone
+ * @property $price
+ */
 class EDPD extends CActiveRecord {
 
 	public $additionPropretys = true;
@@ -10,17 +17,11 @@ class EDPD extends CActiveRecord {
 	}
 
 	public function rules() {
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
 			array('id', 'numerical', 'integerOnly' => true),
-			array('DPD_city, DPD_weight', 'required'),
-			array('id, DPD_price, DPD_city, DPD_zone, DPD_weight', 'safe'),
+			array('city, weight', 'required'),
+			array('id, price, city, zone, weight', 'safe'),
 		);
-	}
-
-	public function init() {
-		
 	}
 
 	/**
@@ -35,17 +36,11 @@ class EDPD extends CActiveRecord {
 	 * @return string the associated database table name
 	 */
 	public function tableName() {
-		return '{{shop__delivery_' . __CLASS__ . '}}';
+		return '{{shop_delivery_edpd}}';
 	}
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations() {
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
+	
+	public function primaryKey() {
+		return 'id';
 	}
 
 	/**
@@ -54,7 +49,10 @@ class EDPD extends CActiveRecord {
 	public function attributeLabels() {
 		return array(
 			'id' => 'ID',
-			'DPD_price' => 'Price',
+			'price' => 'Цена',
+			'zone' => 'Зона',
+			'city' => 'Город',
+			'weght' => 'Вес'
 		);
 	}
 
@@ -63,20 +61,16 @@ class EDPD extends CActiveRecord {
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
 	public function search() {
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
-
 		$criteria = new CDbCriteria;
-
 		$criteria->compare('id', $this->id);
-		$criteria->compare('DPD_price', $this->DPD_price, true);
-		$criteria->compare('DPD_zone', $this->DPD_zone, true);
-		$criteria->compare('DPD_city', $this->DPD_city, true);
-		$criteria->compare('DPD_weight', $this->DPD_weight, true);
+		$criteria->compare('price', $this->price, true);
+		$criteria->compare('zone', $this->zone, true);
+		$criteria->compare('city', $this->city, true);
+		$criteria->compare('weight', $this->weight, true);
 
 		return new CActiveDataProvider(get_class($this), array(
-					'criteria' => $criteria,
-				));
+			'criteria' => $criteria,
+		));
 	}
 
 	/*
@@ -84,86 +78,56 @@ class EDPD extends CActiveRecord {
 	 */
 
 	public function getDeliveryCost($param) {
-		$searchCities = array();
-		if (is_array($param['orderCity'])) {
-			if ($k = array_search("Москва", $param['orderCity']))
-				unset($param['orderCity'][$k]);
-			$searchCities = $param['orderCity'];
-		} else {
-			if (isset($param['orderCity'])) {
-				$searchCities = array($param['orderCity']);
-			} else {
-				return false;
-			}
-		}
-
-		if (isset($searchCities[0]) && $searchCities[0] == "Москва")
+		if (!isset($param['orderCity'])) {
 			return false;
-
-		Yii::import('ext.delivery.DPD.ETarif');
-		Yii::import('ext.delivery.DPD.ETarifZones');
-		$tarifs = Yii::app()->db->createCommand()
-				->leftjoin('{{shop__delivery_ETarifZones}}', '`{{shop__delivery_ETarifZones}}`.`tarifzones_zone`=`tarif_zone`')
-				->where(array('and', 'tarif_weight1 <=' . $param['orderWeight'],
-					'tarif_weight2 >=' . min(array(30, $param['orderWeight'])),
-					array('in', '{{shop__delivery_ETarifZones}}.tarifzones_city', $searchCities)
-						)
-				)
-				->from(ETarif::model()->tableName())
-				->queryAll();
-
+		}
+		$orderCities = array();
+		if (!is_array($param['orderCity'])) {
+			$orderCities = array($param['orderCity']);
+		}
+		else {
+			$orderCities = $param['orderCity'];
+		}
+		$badKey = array_search('Москва', $orderCities);
+		if ($badKey !== false) {
+			unset($orderCities[$badKey]);
+		}
+		$orderCities = array_unique($orderCities);
+		Yii::import('ext.delivery.DPD.EDPDTarif');
+		$tarifs = EDPDTarif::model()->getTarifs($orderCities, $param);
 		$prices = array();
 		foreach ($tarifs as $k => $tarif) {
-			$prices[$k]['price'] = $tarif['tarif_price'];
+			$prices[$k]['price'] = $tarif['price'];
 			if ($param['orderWeight'] > 30) {
-				$prices[$k]['price'] += ($param['orderWeight'] - 30) * $tarif['tarif_overload'];
+				$prices[$k]['price'] += ($param['orderWeight'] - 30) * $tarif['overload'];
 			}
-			$prices[$k]['destination'] = $tarif['tarifzones_city'];
-			$this->DPD_price = $prices[$k]['price'];
-			$this->DPD_zone = $tarif['tarif_zone'];
-			$this->DPD_city = $prices[$k]['destination'];
-			$this->DPD_weight = $param['orderWeight'];
+			$prices[$k]['destination'] = $tarif['city'];
+			$this->price = $prices[$k]['price'];
+			$this->zone = $tarif['zone'];
+			$this->city = $prices[$k]['destination'];
+			$this->weight = $param['orderWeight'];
 		}
 
 		return $prices;
 	}
 
 	public function getForm($param) {
-		Yii::import('ext.delivery.DPD.ETarifZones');
-		$criteria = new CDbCriteria;
-		$criteria->compare('tarifzones_city', $param['orderCity'], true);
-		$cityValues = ETarifZones::model()->findAll($criteria);
-		$cities = array();
-		if ($cityValues) {
-			//Если нашли город из заказа в списке городов то ставим его первым в списке
-			foreach ($cityValues as $city) {
-				$cities[$city->tarifzones_city] = $city->tarifzones_city;
-			}
-		}
-
-		//Добавляем полный список городов для доставки    
-		$cityValues = ETarifZones::model()->findAll();
-		foreach ($cityValues as $city) {
-			$cities[$city->tarifzones_city] = $city->tarifzones_city;
-		}
-
-		$this->DPD_weight = $param['orderWeight'];
-
+		$this->weight = $param['orderWeight'];
 		$params = array(
 			'elements' => array(
 				__CLASS__ => array(
 					'type' => 'form',
 					'elements' => array(
-						'DPD_city' => array(
+						'city' => array(
 							'type' => 'text',
 						),
-						'DPD_zone' => array(
+						'zone' => array(
 							'type' => 'hidden',
 						),
-						'DPD_price' => array(
+						'price' => array(
 							'type' => 'hidden',
 						),
-						'DPD_weight' => array(
+						'weight' => array(
 							'type' => 'hidden',
 						)
 					),
@@ -179,16 +143,16 @@ class EDPD extends CActiveRecord {
 				__CLASS__ => array(
 					'type' => 'form',
 					'elements' => array(
-						'DPD_city' => array(
+						'city' => array(
 							'type' => 'text',
 						),
-						'DPD_zone' => array(
+						'zone' => array(
 							'type' => 'hidden',
 						),
-						'DPD_price' => array(
+						'price' => array(
 							'type' => 'hidden',
 						),
-						'DPD_weight' => array(
+						'weight' => array(
 							'type' => 'hidden',
 						)
 					),
@@ -204,16 +168,16 @@ class EDPD extends CActiveRecord {
 				__CLASS__ => array(
 					'type' => 'form',
 					'elements' => array(
-						'DPD_city' => array(
+						'city' => array(
 							'type' => 'hidden',
 						),
-						'DPD_zone' => array(
+						'zone' => array(
 							'type' => 'hidden',
 						),
-						'DPD_price' => array(
+						'price' => array(
 							'type' => 'hidden',
 						),
-						'DPD_weight' => array(
+						'weight' => array(
 							'type' => 'hidden',
 						)
 					),
@@ -224,9 +188,8 @@ class EDPD extends CActiveRecord {
 	}
 
 	public function getDestination() {
-		return $this->DPD_city;
+		return $this->city;
 	}
-
 }
 
 ?>

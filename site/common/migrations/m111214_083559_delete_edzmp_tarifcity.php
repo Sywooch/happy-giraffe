fef72cb4923ce98ab7be149312a649dd130c5323
Alex Dulub <dulubao@gmail.com>
2011-12-21 16:07:40 +0200

    fix migrations

--- site/common/migrations/m111214_083559_delete_edzmp_tarifcity.php
+++ site/common/migrations/m111214_083559_delete_edzmp_tarifcity.php
@@ -4,11 +4,12 @@ class m111214_083559_delete_edzmp_tarifcity extends CDbMigration
 {
 	public function up()
 	{
-		$this->dropTable('shop__delivery_epricecity');
+		$this->execute("DROP TABLE IF EXISTS `shop__delivery_epricecity`");
 	}
 
 	public function down()
 	{
+		
 		echo "m111214_083559_delete_edzmp_tarifcity does not support migration down.\n";
 		return false;
 	}

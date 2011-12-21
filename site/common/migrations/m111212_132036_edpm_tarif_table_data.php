c08aaa124c5b73b57fcf0751b1463b380c507a82
Alex Dulub <dulubao@gmail.com>
2011-12-21 15:57:36 +0200

    fix migrations

--- site/common/migrations/m111212_132036_edpm_tarif_table_data.php
+++ site/common/migrations/m111212_132036_edpm_tarif_table_data.php
@@ -6,10 +6,10 @@ class m111212_132036_edpm_tarif_table_data extends CDbMigration
 	
 	public function up()
 	{
-		$sql = "INSERT INTO `:table` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 0, 500, 350);";
-		$sql .= "INSERT INTO `:table` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 500, 1000, 250);";
-		$sql .= "INSERT INTO `:table` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 1000, 1500, 100);";
-		$sql .= "INSERT INTO `:table` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 1000, NULL, 0);";
+		$sql = "INSERT INTO `:shop__delivery_edpm_tarif` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 0, 500, 350);";
+		$sql .= "INSERT INTO `:shop__delivery_edpm_tarif` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 500, 1000, 250);";
+		$sql .= "INSERT INTO `:shop__delivery_edpm_tarif` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 1000, 1500, 100);";
+		$sql .= "INSERT INTO `:shop__delivery_edpm_tarif` (`id`, `order_price_from`, `order_price_to`, `price`) VALUES (NULL, 1000, NULL, 0);";
 		$this->execute($sql, array(':table' => $this->_table));
 	}
 

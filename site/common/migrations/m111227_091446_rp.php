1285cf6433f0fd69fbf0ce8b58ef28a1ba073a70
ChooJoy <choojoy.work@gmail.com>
2011-12-27 13:15:24 +0400

    travel

---
+++ site/common/migrations/m111227_091446_rp.php
@@ -0,0 +1,25 @@
+<?php
+
+class m111227_091446_rp extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m111227_091446_rp does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}

ba3ae8fad9372bce001fee4898560d9ebe233849
ChooJoy <choojoy.work@gmail.com>
2011-12-28 22:20:48 +0400

    travel

--- site/frontend/modules/recipeBook/views/default/_view.php
+++ site/frontend/modules/recipeBook/views/default/_view.php
@@ -9,9 +9,7 @@
         <a href="<?php echo $this->createUrl('/recipeBook/default/view', array('id'=>$data->id))
             ?>"><h1><?php echo $data->name ?></h1></a>
         <div class="user">
-            <div class="ava <?php echo ($data->user->gender == 1)?'male':'female' ?> avatar">
-
-            </div>
+            <?php $this->widget('AvatarWidget', array('user' => $data->user)); ?>
             <a class="username"><?php echo $data->user->first_name.' '.$data->user->last_name ?></a>
         </div>
 

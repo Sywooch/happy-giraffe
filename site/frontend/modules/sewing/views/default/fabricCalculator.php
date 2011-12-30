95a136b8eac7ba61dafb50b7ebb9f439fe74aeb8
unknown <alexk984@gmail.com>
2011-12-30 14:47:26 +0300

    fabric calc design #10

--- site/frontend/modules/sewing/views/default/fabricCalculator.php
+++ site/frontend/modules/sewing/views/default/fabricCalculator.php
@@ -11,7 +11,7 @@
         var W = document.getElementById("w").value;
         var H = document.getElementById("h").value;
         if (!parseInt(PLUS) && parseInt(PLUS) != 0 || !parseInt(W) || !parseInt(H) || !parseInt(NIT)) {
-            alert('Введите целое число.');
+
             return;
         }
         LEN = parseInt(LEN);
@@ -23,7 +23,8 @@
         var RES_H = H * NIT / LEN * 2.54 + PLUS * 2;
         RES_W = Math.round(RES_W);
         RES_H = Math.round(RES_H);
-        document.getElementById("res").innerHTML = 'ширина = ' + RES_W + ' см, высота = ' + RES_H + ' см';
+        document.getElementById("res").innerHTML = RES_W + ' x ' + RES_H + ' см';
+        $('#res').show();
     }
 
     function clean() {
@@ -39,7 +40,7 @@
         var W1 = document.getElementById("w1").value;
         var H1 = document.getElementById("h1").value;
         if (!parseInt(PLUS1) && parseInt(PLUS1) != 0 || !parseInt(W1) || !parseInt(H1)) {
-            alert('Введите целое число.');
+            
             return;
         }
         AIDA = parseInt(AIDA);
@@ -50,7 +51,8 @@
         var RES_H = H1 / AIDA * 2.54 + PLUS1 * 2;
         RES_W = Math.round(RES_W);
         RES_H = Math.round(RES_H);
-        document.getElementById("res1").innerHTML = 'ширина = ' + RES_W + ' см, высота = ' + RES_H + ' см';
+        document.getElementById("res1").innerHTML = RES_W + ' x ' + RES_H + ' см';
+        $('#res1').show();
 
     }
     function clean1() {
@@ -61,102 +63,68 @@
 
     }
 </script>
-
-<h3 align="center"><font face="Verdana" size="2">КАЛЬКУЛЯТОР ТКАНИ</font></h3>
-
-<div style="padding-left: 7px; padding-right: 7px" class="text"><font face="Verdana" size="2">Если вы хотите узнать
-    размер готовой вышивки, то в графе для
-    припусков поставьте "0". Результат выводится с округлением до целого числа в
-    сантиметрах.</font></div>
-<hr size="1" color="#84DAFF">
-
-<div class="text">
-    <h4 align="center"><font face="Verdana" size="2">Для льна или другой ткани равномерного переплетения</font></h4>
-    <font face="Verdana"><font size="2">Введите размер схемы в стежках:<br>
-        по ширине </font> <input type="text" size="5" id="w"><font size="2">
-        по высоте </font> <input type="text" size="5" id="h"><font size="2"><br>
-
-        Введите количество нитей для одного "креста":
-    </font>
-        <input type="text" value="2" size="3" id="nit"><font size="2"><br>
-            (обычно это 2х2 нити)<br>
-            Прибавьте на припуски с каждой стороны:
-        </font>
-        <input type="text" value="5" size="3" id="plus"><font size="2"> см<br>
-
-            Выберите номер ткани:
-        </font>
-        <select id="len">
-
-            <option value="22">25</option>
-            <option value="27">27</option>
-            <option value="28" selected="">28</option>
-            <option value="32">32</option>
-            <option value="36">36</option>
-
-        </select><font size="2"><br>
-
-            (количество нитей в 1 дюйме)
-        </font></font>
-
-    <p>
-
-
-        <strong><font size="4" color="red" id="res"></font></strong>
-
-    </p>
-    <hr size="1" color="#84DAFF">
-
-    <center>
-        <font face="Verdana">
-            <input type="button" value="Очистить" onclick="clean()"><font size="2"> </font>
-
-            <input type="button" value="Рассчитать" onclick="pushed()"><font size="2">
-        </font></font>
-    </center>
-</div>
-
-
-<div class="text">
-    <h4 align="center"><font face="Verdana" size="2">Для канвы</font></h4>
-
-    <font face="Verdana"><font size="2">Введите размер схемы в крестиках:<br>
-        по ширине </font> <input type="text" size="5" id="w1"><font size="2">
-
-        по высоте </font> <input type="text" size="5" id="h1"><font size="2"><br>
-
-        Прибавьте на припуски (будет прибавлено с каждой стороны):
-    </font>
-        <input type="text" value="5" size="3" id="plus1"><font size="2"> см<br>
-
-            Выберите номер канвы:
-        </font>
-        <select id="aida">
-            <option value="11">11</option>
-            <option value="14" selected="">14</option>
-
-            <option value="16">16</option>
-            <option value="18">18</option>
-            <option value="22">22</option>
-        </select><font size="2"><br>
-            (количество клеток в 1 дюйме)
-        </font></font>
-
-    <p>
-
-
-        <strong><font size="4" color="red" id="res1"></font></strong>
-
-    </p>
-    <hr size="1" color="#84DAFF">
-
-    <center>
-        <font face="Verdana">
-            <input type="button" value="Очистить" onclick="clean1()"><font size="2"> </font>
-            <input type="button" value="Рассчитать" onclick="pushed1()"><font size="2">
-        </font>
-
-            <p style="margin-top: 0; margin-bottom: 0">&nbsp;</p>
-        </font></center>
-    <font face="Verdana">
-    </font></div>
+<div class="embroidery_service">
+    <img src="/images/service_much_tissue.jpg" alt="" title="" />
+    <div class="tissue_left">
+        <div class="tissue_type">
+            <span><ins>Для льна</ins>или другой ткани<br />равномерного переплетения</span>
+            <div class="style_rarr"></div><!-- .style_rarr -->
+        </div><!-- .tissue_type -->
+        <div class="tissue_calc">
+            <ul>
+                <li>
+                    <ins>Введите размер схемы<br /> в стежках:</ins>
+                    Ширина <input type="text" class="wh_t" id="w"/>
+                    Высота <input type="text" class="wh_t" id="h"/>
+                </li>
+                <li>
+                    <ins>Введите количество нитей  для <br />одного “креста:<input type="text" class="much_t" id="nit"/></ins>
+                    (обычно это 2х2 нити)
+                </li>
+                <li>
+                    <ins>Прибавьте на припуски: <input type="text" class="much_t" id="plus"/> см</ins>
+                    (будет прибавлено с каждой стороны)
+                </li>
+                <li>
+                    <ins>Выберите номер ткани:
+                        <?php echo CHtml::dropDownList('size', 28, array(25=>25,27=>27,28=>28,32=>32,36=>36), array('id' => 'len', 'class' => "num_cal")) ?>
+                    </ins>
+                    (количество нитей в одном дюйме)
+                </li>
+            </ul>
+            <input type="button" class="calc_bt" value="Рассчитать"  onclick="pushed()"/>
+        </div><!-- .tissue_calc -->
+        <div class="tissue_result" id="res" style="display: none;">
+            64 x 64 <span>см</span>
+        </div><!-- .tissue_result -->
+    </div><!-- .tissue_left -->
+    <div class="tissue_right">
+        <div class="tissue_type">
+            <span><ins>Для канвы</ins></span>
+            <div class="style_rarr"></div><!-- .style_rarr -->
+        </div><!-- .tissue_type -->
+        <div class="tissue_calc">
+            <ul>
+                <li>
+                    <ins>Введите размер схемы<br /> в стежках:</ins>
+                    Ширина <input type="text" class="wh_t" id="w1"/>
+                    Высота <input type="text" class="wh_t" id="h1"/>
+                </li>
+                <li>
+                    <ins>Введите количество нитей  для <br />одного “креста:<input type="text" class="much_t" id="plus1"/></ins>
+                    (обычно это 2х2 нити)
+                </li>
+                <li>
+                    <ins>Выберите номер ткани:
+                        <?php echo CHtml::dropDownList('size', 14, array(11=>11,14=>14,16=>16,18=>18,22=>22), array('id' => 'aida', 'class' => "num_cal")) ?>
+                    </ins>
+                    (количество нитей в одном дюйме)
+                </li>
+            </ul>
+            <input type="button" class="calc_bt" value="Рассчитать"  onclick="pushed1()"/>
+        </div><!-- .tissue_calc -->
+        <div class="tissue_result" id="res1" style="display: none;">
+            64 x 64 <span>см</span>
+        </div><!-- .tissue_result -->
+    </div><!-- .tissue_right -->
+</div><!-- .embroidery_service -->	

a73ec22c64388787500b77334df115c115ab8b51
unknown <alexk984@gmail.com>
2012-01-11 14:29:15 +0300

    migration fixes

--- site/common/migrations/m120105_141517_drop_offer_id.php
+++ site/common/migrations/m120105_141517_drop_offer_id.php
@@ -7,7 +7,8 @@ class m120105_141517_drop_offer_id extends CDbMigration
 	public function up()
 	{
         $this->truncateTable($this->_table);
-        $this->dropIndex('offer_id',$this->_table);
+//        $this->dropIndex('offer_id',$this->_table);
+        $this->dropForeignKey('offer_id',$this->_table);
 	}
 
 	public function down()

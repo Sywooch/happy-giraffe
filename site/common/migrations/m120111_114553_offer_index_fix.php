441a79397d426dc4b6aeb2763cb76a52f87587ca
unknown <alexk984@gmail.com>
2012-01-11 14:54:27 +0300

    migration fixes

--- site/common/migrations/m120111_114553_offer_index_fix.php
+++ site/common/migrations/m120111_114553_offer_index_fix.php
@@ -3,11 +3,11 @@
 class m120111_114553_offer_index_fix extends CDbMigration
 {
     private $_table = 'bag_offer_vote';
-    
+
 	public function up()
 	{
         $this->execute('SET foreign_key_checks = 0;
-        DROP INDEX offer_id ON '.$this->_table.';
+        ALTER TABLE '.$this->_table.' DROP INDEX offer_id;
         SET foreign_key_checks = 1;');
 	}
 

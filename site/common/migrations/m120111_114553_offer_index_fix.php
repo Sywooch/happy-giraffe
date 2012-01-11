1a7251734697e3a247516e0a1678629a26de9609
unknown <alexk984@gmail.com>
2012-01-11 14:58:18 +0300

    migration fixes

--- site/common/migrations/m120111_114553_offer_index_fix.php
+++ site/common/migrations/m120111_114553_offer_index_fix.php
@@ -7,13 +7,26 @@ class m120111_114553_offer_index_fix extends CDbMigration
 	public function up()
 	{
         $this->execute('SET foreign_key_checks = 0;
-        ALTER TABLE '.$this->_table.' DROP INDEX offer_id;
+        DROP TABLE '.$this->_table.'
         SET foreign_key_checks = 1;');
+
+        $this->execute('CREATE TABLE IF NOT EXISTS `bag_offer_vote` (
+  `user_id` int(11) unsigned NOT NULL,
+  `vote` tinyint(1) NOT NULL,
+  `object_id` int(10) unsigned NOT NULL,
+  KEY `bag_user_vote_user_fk` (`user_id`),
+  KEY `bag_user_vote_object_fk` (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `bag_offer_vote`
+  ADD CONSTRAINT `bag_user_vote_object_fk` FOREIGN KEY (`object_id`) REFERENCES `bag_offer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bag_user_vote_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+');
 	}
 
 	public function down()
 	{
-
+        
 	}
 
 	/*

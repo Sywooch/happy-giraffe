9876d44e56d77f1c9fc9c628f20ea36ff7c03784
unknown <alexk984@gmail.com>
2012-01-11 14:38:21 +0300

    migration fixes

--- site/common/migrations/m120105_141517_drop_offer_id.php
+++ site/common/migrations/m120105_141517_drop_offer_id.php
@@ -7,10 +7,15 @@ class m120105_141517_drop_offer_id extends CDbMigration
 	public function up()
 	{
         $this->truncateTable($this->_table);
-        //$this->dropIndex('offer_id',$this->_table);
+
+        $this->execute('SET foreign_key_checks = 0;');
+
+        $this->dropIndex('offer_id',$this->_table);
         $this->dropColumn($this->_table,'user_id');
         $this->addColumn($this->_table,'user_id', 'int(11) UNSIGNED not null');
         $this->addForeignKey($this->_table.'_user_fk', $this->_table, 'user_id', 'user', 'id','CASCADE',"CASCADE");
+
+        $this->execute('SET foreign_key_checks = 1;');
 	}
 
 	public function down()

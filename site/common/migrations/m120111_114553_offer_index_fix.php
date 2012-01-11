f9d45de54404703a14a87d4a2b079895d9623c07
unknown <alexk984@gmail.com>
2012-01-11 14:48:42 +0300

    migration fixes

--- site/common/migrations/m120111_114553_offer_index_fix.php
+++ site/common/migrations/m120111_114553_offer_index_fix.php
@@ -2,6 +2,8 @@
 
 class m120111_114553_offer_index_fix extends CDbMigration
 {
+    private $_table = 'bag_offer_vote';
+    
 	public function up()
 	{
         $this->execute('SET foreign_key_checks = 0;

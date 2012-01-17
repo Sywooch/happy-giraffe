09ac46f5c09942b0a47dbf29956b2c24d6aac5f7
ChooJoy <choojoy.work@gmail.com>
2012-01-17 18:39:24 +0400

    brands

---
+++ site/backend/views/brand/_tr.php
@@ -0,0 +1,44 @@
+<tr>
+    <td class="name_ct">
+        <a href="#" class="no_child edit">Jetem</a>
+    </td>
+    <td class="logo_ct">
+        <img src="images/content/logo1.jpg" alt=""/>
+    </td>
+    <td class="active_ct">
+        <ul>
+            <li><a href="#" title="Подробно о бренде">
+                <img src="images/icons/info_catg_icon.png" alt="Подробно о бренде"/></a></li>
+            <li><a href="#" title="Посмотреть в магазине">
+                <img src="images/icons/view_shop_icon.png" alt="Посмотреть в магазине"/></a></li>
+        </ul>
+    </td>
+    <td class="goods_ct">
+        <ul>
+            <li>Товаров - <a href="#">25673</a></li>
+            <li>Категорий - <a href="#">256</a></li>
+        </ul>
+    </td>
+    <td class="sell_ct">
+        <ul>
+            <li>
+                <ins>Оборот</ins>
+                - 23 256 789
+            </li>
+            <li>
+                <ins>Прибыль</ins>
+                - 8 363 123
+            </li>
+        </ul>
+    </td>
+    <td class="ad_ct">
+        <ul>
+            <li><a href="#deactivate" class="activate fancy" title="Деактивировать">Состояние
+                элемента</a></li>
+            <li><a href="#delete_category" class="fancy" title="Удалить категорию"><img
+                src="images/icons/delete_sm_icon.png"
+                alt="Удалить категорию"/></a>
+            </li>
+        </ul>
+    </td>
+</tr>

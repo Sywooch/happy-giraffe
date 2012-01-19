e86435b506fbe330baa2eabc2077dbe5e3b54742
unknown <alexk984@gmail.com>
2012-01-19 17:53:35 +0300

    product feature in admin panel

--- site/backend/views/Product/_attributes.php
+++ site/backend/views/Product/_attributes.php
@@ -4,53 +4,134 @@
  * @var AttributeSetMap[] $attributeMap
  */
 ?>
+<script type="text/javascript">
+    $(function () {
+        $('.characteristic a.edit').click(function () {
+            var block = $(this).parent();
+            block.find('a').hide();
+            block.find('p').show();
+
+            return false;
+        });
+
+        $('.characteristic input.set-value').click(function () {
+            var block = $(this).parent().parent();
+            var value = block.find('input[type=text]').val();
+            var id = block.find('input[type=hidden]').val();
+            SetAttributeValue(id, value, value, block);
+        });
+
+        $('.characteristic input.set-text').click(function () {
+            var block = $(this).parent().parent();
+            var value = block.find('input[type=text]').val();
+            var id = block.find('input[type=hidden]').val();
+
+            $.ajax({
+                url:'<?php echo Yii::app()->createUrl("product/SetAttributeTextValue") ?>',
+                data:{
+                    value:value,
+                    attr_id:id,
+                    product_id:model_id
+                },
+                type:'POST',
+                success:function (data) {
+                    block.find('a').text(value).show();
+                    block.find('p').hide();
+                },
+                context:block
+            });
+        });
+
+        $('.characteristic input.set-yes').click(function () {
+            var block = $(this).parent().parent();
+            var id = block.find('input[type=hidden]').val();
+            SetAttributeValue(id, 1, 'Да', block);
+        });
+
+        $('.characteristic input.set-no').click(function () {
+            var block = $(this).parent().parent();
+            var id = block.find('input[type=hidden]').val();
+            SetAttributeValue(id, 0, 'Нет', block);
+        });
+
+        $('.characteristic input.set-enum').click(function () {
+            var block = $(this).parent().parent();
+            var id = block.find('input[type=hidden]').val();
+            var value = block.find('select').val();
+            var text = block.find("select option[value='" + value + "']").text()
+            SetAttributeValue(id, value, text, block);
+        });
+
+    });
+
+    function SetAttributeValue(attr_id, value, set_test, block) {
+        $.ajax({
+            url:'<?php echo Yii::app()->createUrl("product/SetAttributeValue") ?>',
+            data:{
+                value:value,
+                attr_id:attr_id,
+                product_id:model_id
+            },
+            type:'POST',
+            success:function (data) {
+                block.find('a').text(set_test).show();
+                block.find('p').hide();
+            },
+            context:block
+        });
+    }
+</script>
 <?php foreach ($attributeMap as $attribute): ?>
 <?php $attr = $attribute->map_attribute;
-$value = $model->GetAttributeValue($attr) ?>
-    <tr>
-        <td class="name"><?php echo $attr->attribute_title ?></td>
-        <td>
-            <input type="hidden" value="<?php echo $attr->attribute_id ?>">
+    if ($model->isNewRecord)
+        $value = false;
+    else
+        $value = $model->GetAttributeValue($attr)
+    ?>
+<tr>
+    <td class="name"><?php echo $attr->attribute_title ?></td>
+    <td>
+        <input type="hidden" value="<?php echo $attr->attribute_id ?>">
 
-<?php if ($attr->attribute_type == Attribute::TYPE_BOOL): ?>
-            <p<?php if ($value !== null) echo ' style="display: none;"' ?>>
-                <input type="button" class="smallGrey set-yes" value="Да"/>
-                <input type="button" class="smallGrey set-no" value="Нет"/>
-            </p>
-            <a<?php if (empty($value)) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
-    <?php endif ?>
+        <?php if ($attr->attribute_type == Attribute::TYPE_BOOL): ?>
+        <p<?php if ($value !== false) echo ' style="display: none;"' ?>>
+            <input type="button" class="smallGrey set-yes" value="Да"/>
+            <input type="button" class="smallGrey set-no" value="Нет"/>
+        </p>
+        <a<?php if ($value === false) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
+        <?php endif ?>
 
-<?php if ($attr->attribute_type == Attribute::TYPE_ENUM): ?>
-            <p<?php if ($value !== null) echo ' style="display: none;"' ?>>
-        <?php echo CHtml::dropDownList('attr_value', ' ', $attr->GetEnumList(), array('empty' => ' ')); ?>
-        <input type="button" class="smallGreen set-enum" value="Ok"/>
-    </p>
-            <a<?php if (empty($value)) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
-    <?php endif ?>
+        <?php if ($attr->attribute_type == Attribute::TYPE_ENUM): ?>
+        <p<?php if ($value !== false) echo ' style="display: none;"' ?>>
+            <?php echo CHtml::dropDownList('attr_value', ' ', $attr->GetEnumList(), array('empty' => ' ')); ?>
+            <input type="button" class="smallGreen set-enum" value="Ok"/>
+        </p>
+        <a<?php if ($value === false) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
+        <?php endif ?>
 
-<?php if ($attr->attribute_type == Attribute::TYPE_INTG): ?>
-            <p<?php if ($value !== null) echo ' style="display: none;"' ?>>
-        <input type="text" value="<?php echo $value ?>"/>
-        <input type="button" class="smallGreen set-value" value="Ok"/>
-    </p>
-            <a<?php if (empty($value)) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
-    <?php endif ?>
+        <?php if ($attr->attribute_type == Attribute::TYPE_INTG): ?>
+        <p<?php if ($value !== false) echo ' style="display: none;"' ?>>
+            <input type="text" value="<?php echo $value ?>"/>
+            <input type="button" class="smallGreen set-value" value="Ok"/>
+        </p>
+        <a<?php if ($value === false) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
+        <?php endif ?>
 
-<?php if ($attr->attribute_type == Attribute::TYPE_TEXT): ?>
-            <p<?php if ($value !== null) echo ' style="display: none;"' ?>>
-        <input type="text" value="<?php echo $value ?>"/>
-        <input type="button" class="smallGreen set-text" value="Ok"/>
-    </p>
-            <a<?php if (empty($value)) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
-    <?php endif ?>
+        <?php if ($attr->attribute_type == Attribute::TYPE_TEXT): ?>
+        <p<?php if ($value !== false) echo ' style="display: none;"' ?>>
+            <input type="text" value="<?php echo $value ?>"/>
+            <input type="button" class="smallGreen set-text" value="Ok"/>
+        </p>
+        <a<?php if ($value === false) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
+        <?php endif ?>
 
-<?php if ($attr->attribute_type == Attribute::TYPE_MEASURE): ?>
-            <p<?php if ($value !== null) echo ' style="display: none;"' ?>>
-        <input type="text" value=""/>
-        <input type="button" class="smallGreen" value="Ok"/>
-    </p>
-            <a<?php if (empty($value)) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
-    <?php endif ?>
+        <?php if ($attr->attribute_type == Attribute::TYPE_MEASURE): ?>
+        <p<?php if ($value !== false) echo ' style="display: none;"' ?>>
+            <input type="text" value=""/>
+            <input type="button" class="smallGreen" value="Ok"/>
+        </p>
+        <a<?php if ($value === false) echo ' style="display: none;"' ?> href="#" class="edit"><?php echo $value ?></a>
+        <?php endif ?>
 
-</td>
+    </td>
 <?php endforeach; ?>

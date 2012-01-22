e200c77e287a5236ce770b97fc7b7cdda8523f44
unknown <alexk984@gmail.com>
2012-01-22 14:16:49 +0300

    admin

--- site/backend/components/views/OnOffWidget.php
+++ site/backend/components/views/OnOffWidget.php
@@ -4,38 +4,42 @@
     $js = "
         $('body').delegate('a.activate, a.deactivate', 'click', function(e) {
             e.preventDefault();
-            $.ajax({
-                type: 'POST',
-                url: '" . Yii::app()->createUrl('ajax/onOff') . "',
-                data: {
-                    modelName: $(this).find('input[name=modelName]').val(),
-                    modelPk: $(this).find('input[name=modelPk]').val()
-                },
-                success: function(response) {
-                    if (response == '1')
-                    {
-                        $(this).toggleClass('activate deactivate');
-                        if ($(this).hasClass('activate'))
+            ConfirmPopup('Вы точно хотите ' + $(this).find('input[name=showQuestion]').val() + '  эелемент?', $(this), function (owner) {
+                $.ajax({
+                    type: 'POST',
+                    url: '" . Yii::app()->createUrl('ajax/onOff') . "',
+                    data: {
+                        modelName: owner.find('input[name=modelName]').val(),
+                        modelPk: owner.find('input[name=modelPk]').val()
+                    },
+                    success: function(response) {
+                        if (response == '1')
                         {
-                            $(this).attr('title', 'Деактивировать');
-                            $('.active_items ins').text(parseInt($('.active_items ins').text()) + 1);
-                            $('.deactive_items ins').text(parseInt($('.deactive_items ins').text()) - 1);
+                            $(this).toggleClass('activate deactivate');
+                            if ($(this).hasClass('activate'))
+                            {
+                                $(this).attr('title', 'Деактивировать');
+                                $('.active_items ins').text(parseInt($('.active_items ins').text()) + 1);
+                                $('.deactive_items ins').text(parseInt($('.deactive_items ins').text()) - 1);
+                                $(this).find('input[name=showQuestion]').val('Деактивировать');
+                            }
+                            else
+                            {
+                                $(this).attr('title', 'Активировать');
+                                $('.active_items ins').text(parseInt($('.active_items ins').text()) - 1);
+                                $('.deactive_items ins').text(parseInt($('.deactive_items ins').text()) + 1);
+                                $(this).find('input[name=showQuestion]').val('Активировать');
+                            }
+                            $(this).tooltip({
+                                track: true,
+                                delay: 0,
+                                showURL: false,
+                                fade: 200
+                            });
                         }
-                        else
-                        {
-                            $(this).attr('title', 'Активировать');
-                            $('.active_items ins').text(parseInt($('.active_items ins').text()) - 1);
-                            $('.deactive_items ins').text(parseInt($('.deactive_items ins').text()) + 1);
-                        }
-                        $(this).tooltip({
-                            track: true,
-                            delay: 0,
-                            showURL: false,
-                            fade: 200
-                        });
-                    }
-                },
-                context: $(this)
+                    },
+                    context: owner
+                });
             });
         });
     ";
@@ -45,6 +49,7 @@
 
 <a href="#" class="<?php echo ($modelActive ? 'activate' : 'deactivate'); ?>" title="<?php echo ($modelActive ? 'Деактивировать' : 'Активировать'); ?>">
     Состояние элемента
+    <?php echo CHtml::hiddenField('showQuestion', 'Деактивировать'); ?>
     <?php echo CHtml::hiddenField('modelName', $modelName); ?>
     <?php echo CHtml::hiddenField('modelPk', $modelPk); ?>
 </a>

1f5bc00a7b328b9c7f8ca7da38407d4221d218e4
unknown <alexk984@gmail.com>
2012-01-31 10:44:39 +0300

    liveinternat stats

--- site/frontend/helpers/HDate.php
+++ site/frontend/helpers/HDate.php
@@ -25,6 +25,23 @@ class HDate
             12 => "Декабрь");
     }
 
+    public static function ruShortMonths()
+    {
+        return array(
+            1 => "Янв",
+            2 => "Фев",
+            3 => "Мар",
+            4 => "Апр",
+            5 => "Май",
+            6 => "Июн",
+            7 => "Июл",
+            8 => "Авг",
+            9 => "Сен",
+            10 => "Окт",
+            11 => "Ноя",
+            12 => "Дек");
+    }
+
     public static function ruMonth($num)
     {
         switch ($num) {

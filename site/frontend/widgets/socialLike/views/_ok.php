c7208feaa534e01caa9cdbba542e9e32cadfec92
lnghost <lnghost@hotmail.com>
2012-02-07 16:52:51 +0400

    Like fix

--- site/frontend/widgets/socialLike/views/_ok.php
+++ site/frontend/widgets/socialLike/views/_ok.php
@@ -1,2 +1,17 @@
-<a target="_blank" class="mrc__plugin_uber_like_button" href="http://connect.mail.ru/share?share_url=<?php echo urlencode(Yii::app()->createAbsoluteUrl(Yii::app()->request->pathInfo)); ?>" data-mrc-config="{'type' : 'button', 'caption-mm' : '1', 'caption-ok' : '3', 'counter' : 'true', 'text' : 'true', 'width' : '100%'}">Нравится</a>
+<?php
+$params = array();
+foreach($this->options as $key => $value)
+{
+    if($key == 'image')
+        $key = 'imageurl';
+    if($key == 'description')
+        $value = substr($value, 0, 10);
+    $params[$key] = $value;
+}
+
+if(!isset($params['url']))
+    $params['url'] = urlencode(Yii::app()->createAbsoluteUrl(Yii::app()->request->pathInfo));
+$href = 'http://connect.mail.ru/share?' . Yii::app()->urlManager->createPathInfo($params, '=', '&amp;');
+?>
+<a target="_blank" class="mrc__plugin_uber_like_button" href="<?php echo $href; ?>" data-mrc-config="{'type' : 'button', 'caption-mm' : '1', 'caption-ok' : '3', 'counter' : 'true', 'text' : 'true', 'width' : '100%'}">Нравится</a>
 <script src="http://cdn.connect.mail.ru/js/loader.js" type="text/javascript" charset="UTF-8"></script>
--- site/frontend/widgets/socialLike/views/_tw.php
+++ site/frontend/widgets/socialLike/views/_tw.php
@@ -1,6 +1,8 @@
-<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo Yii::app()->createAbsoluteUrl(Yii::app()->request->pathInfo); ?>">Tweet</a>
 <?php
 $js = '!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");';
 Yii::app()->clientScript
     ->registerScript('fb_init', $js, CClientScript::POS_END);
+$url = isset($this->options['url']) ? $this->options['url'] : Yii::app()->createAbsoluteUrl(Yii::app()->request->pathInfo);
+$text = isset($this->options['title']) ? ' data-text="' . $this->options['title'] . '"' : '';
 ?>
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo $url; ?>"<?php echo $text ?>>Tweet</a>

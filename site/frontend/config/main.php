9b0e090e90a956ba79474c6bb8286a301b609a8a
lnghost <lnghost@hotmail.com>
2012-02-08 15:28:17 +0400

    Like fixes

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -6,6 +6,7 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
+    'id' => 'happy-giraffe',
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Клуб',
 	'theme'=>'happy_giraffe',

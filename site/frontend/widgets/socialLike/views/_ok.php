0820df2effb7a08c64d10ee81cde6be19cd04f86
lnghost <lnghost@hotmail.com>
2012-02-09 16:27:20 +0400

    Like fixes
    Signed-off-by: lnghost <lnghost@hotmail.com>

---
+++ site/frontend/widgets/socialLike/views/_ok.php
@@ -0,0 +1,9 @@
+<?php
+Yii::app()->clientScript
+    ->registerScriptFile('http://stg.odnoklassniki.ru/share/odkl_share.js')
+    ->registerCssFile('http://stg.odnoklassniki.ru/share/odkl_share.css')
+    ->registerScript('ok_init', 'ODKL.init();', CClientScript::POS_READY);
+?>
+<div style="float: left;">
+    <a class="odkl-klass-oc" href="<?php echo $this->options['url']; ?>" onclick="ODKL.Share(this);return false;" ><span>0</span></a>
+</div>

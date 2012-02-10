1442bcf8953ff1aa3dc1d15b022220589d8d7f7c
unknown <alexk984@gmail.com>
2012-02-10 16:42:37 +0300

    im module fixes

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -214,7 +214,7 @@ return array(
         ),
         'comet'=>array(
             'class' => 'ext.Dklab_Realplexor',
-            'host' => 'test.dev.happy-giraffe.ru',
+            'host' => 'plexor.dev.happy-giraffe.ru',
             'port' => 10010,
             'namespace' => 'crm_',
         )

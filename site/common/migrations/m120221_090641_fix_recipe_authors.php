88af250ab3c00c0fbbf13e06ea82ef3f0eafec17
Kireev Alex <alexk984@gmail.com>
2012-02-21 14:21:20 +0400

    migration fix

--- site/common/migrations/m120221_090641_fix_recipe_authors.php
+++ site/common/migrations/m120221_090641_fix_recipe_authors.php
@@ -2,20 +2,22 @@
 
 class m120221_090641_fix_recipe_authors extends CDbMigration
 {
-    private $_table = 'recipeBook_recipe';
-
     public function up()
     {
-        Yii::import('site.frontend.modules.recipeBook.models.*');
-        Yii::import('site.frontend.components.*');
         $user_ids = Yii::app()->db->createCommand('select DISTINCT user_id from recipeBook_recipe WHERE user_id IS NOT NULL')->queryColumn();
-        print_r($user_ids);
+        $exist_user_ids = array();
+        foreach($user_ids as $user_id){
+            $exist = Yii::app()->db->createCommand('select id from user WHERE id ='.$user_id)->queryScalar();
+            if (!empty($exist))
+                $exist_user_ids[] = $user_id;
+        }
+        print_r($exist_user_ids);
 
         $user_not_set = Yii::app()->db->createCommand('select * from recipeBook_recipe WHERE user_id IS NULL')->queryAll();
 
-        if (!empty($user_ids))
+        if (!empty($exist_user_ids))
             foreach ($user_not_set as $row) {
-                $user_id = $user_ids[rand(0, count($user_ids) - 1)];
+                $user_id = $exist_user_ids[rand(0, count($exist_user_ids) - 1)];
                 Yii::app()->db->createCommand()->update('recipeBook_recipe',
                     array('user_id' => $user_id), 'id=' . $row['id']);
             }

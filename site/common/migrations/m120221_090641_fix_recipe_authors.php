dde7afad958daa5a9dd89d9a8d2d5ed8aca00686
Kireev Alex <alexk984@gmail.com>
2012-02-21 13:54:10 +0400

    services fixes

--- site/common/migrations/m120221_090641_fix_recipe_authors.php
+++ site/common/migrations/m120221_090641_fix_recipe_authors.php
@@ -10,28 +10,19 @@ class m120221_090641_fix_recipe_authors extends CDbMigration
         Yii::import('site.frontend.components.*');
         $user_ids = Yii::app()->db->createCommand('select DISTINCT user_id from recipeBook_recipe WHERE user_id IS NOT NULL')->queryColumn();
         print_r($user_ids);
-        $user_not_set = RecipeBookRecipe::model()->findAll('user_id IS NULL');
+
+        $user_not_set = Yii::app()->db->createCommand('select * from recipeBook_recipe WHERE user_id IS NULL')->queryAll();
 
         if (!empty($user_ids))
-        foreach ($user_not_set as $model) {
-            $model->user_id = $user_ids[rand(0, count($user_ids)-1)];
-            $model->update('user_id');
-        }
+            foreach ($user_not_set as $row) {
+                $user_id = $user_ids[rand(0, count($user_ids) - 1)];
+                Yii::app()->db->createCommand()->update('recipeBook_recipe',
+                    array('user_id' => $user_id), 'id=' . $row['id']);
+            }
     }
 
     public function down()
     {
 
     }
-
-    /*
-     // Use safeUp/safeDown to do migration with transaction
-     public function safeUp()
-     {
-     }
-
-     public function safeDown()
-     {
-     }
-     */
 }

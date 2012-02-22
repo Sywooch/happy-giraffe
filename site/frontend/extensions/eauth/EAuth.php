a064ac67fa83d9151e88b6c69842b22bb837f305
lnghost <lnghost@hotmail.com>
2012-02-22 11:51:41 +0400

    SA test

--- site/frontend/extensions/eauth/EAuth.php
+++ site/frontend/extensions/eauth/EAuth.php
@@ -102,7 +102,8 @@ class EAuth extends CApplicationComponent {
 	 * @param mixed $url url to redirect. Can be route or normal url. See {@link CHtml::normalizeUrl}.
 	 * @param boolean $jsRedirect whether to use redirect while popup window is used. Defaults to true.
 	 */
-	public function redirect($url, $jsRedirect = true) {		
+	public function redirect($url, $jsRedirect = true) {
+        echo $url;exit;
 		require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'EAuthRedirectWidget.php';				
 		$widget = Yii::app()->getWidgetFactory()->createWidget($this, 'EAuthRedirectWidget', array(
 			'url' => CHtml::normalizeUrl($url),

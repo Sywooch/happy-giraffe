f07a7fba305cb1a027874a475a12b3add84639ac
Kireev Alex <alexk984@gmail.com>
2012-02-22 17:55:31 +0400

    right fixes

--- site/frontend/modules/im/components/Im.php
+++ site/frontend/modules/im/components/Im.php
@@ -64,10 +64,9 @@ class Im
             foreach ($dialog->messageUsers as $user) {
                 if ($user->user_id !== $this->_user_id && !in_array($user->user_id, $this->_dialogs)) {
                     $um = $this->getUser($user->user_id);
-                    if ($um === null){
-
+                    if ($um === null)
                         continue;
-                    }
+
                     $users [] = $user->user_id;
                     $new_dialog['name'] = $this->getUser($user->user_id)->getFullName();
                     $new_dialog['users'][] = $user->user_id;

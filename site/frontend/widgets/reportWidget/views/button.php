16edb3238a012095b47141880975ee14b397207a
Kireev Alex <alexk984@gmail.com>
2012-02-23 11:19:51 +0400

    merge fixes

---
+++ site/frontend/widgets/reportWidget/views/button.php
@@ -0,0 +1,3 @@
+<a href="javascript:void(0);" onclick="Report.getForm('<?php echo $this->entity_name ?>', <?php echo $this->entity_id; ?>, <?php echo $selector ?>);">
+    <span>Нарушение!</span>
+</a>

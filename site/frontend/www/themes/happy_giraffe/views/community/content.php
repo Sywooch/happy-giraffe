b24c6a83457d4946b737efeffb4528d9f8ecfdff
Kireev Alex <alexk984@gmail.com>
2012-02-23 10:57:33 +0400

    user permissions, relate to community

--- site/frontend/www/themes/happy_giraffe/views/community/content.php
+++ site/frontend/www/themes/happy_giraffe/views/community/content.php
@@ -156,16 +156,16 @@ $cs
         }
         ?>
         <?php if ($c->contentAuthor->id == Yii::app()->user->id ||
-        Yii::app()->authManager->checkAccess('редактирование тем в сообществах',
-            Yii::app()->user->getId(), array(
-                'community_id'=>null,
-            ))): ?>
+        Yii::app()->authManager->checkAccess('редактирование тем в сообществах',Yii::app()->user->getId(), array(
+                'community_id'=>$c->rubric->community->id,
+            )) ||
+        Yii::app()->authManager->checkAccess('перенос темы из сообщества в сообщество',Yii::app()->user->getId())): ?>
         <?php echo CHtml::link('редактировать', ($c->type->slug == 'travel') ? $this->createUrl('community/editTravel', array('id' => $c->id)) : $this->createUrl('community/edit', array('content_id' => $c->id))); ?>
         <?php endif; ?>
         <?php if ($c->contentAuthor->id == Yii::app()->user->id ||
             Yii::app()->authManager->checkAccess('удаление тем в сообществах',
                 Yii::app()->user->getId(), array(
-                    'community_id'=>null,
+                    'community_id'=>$c->rubric->community->id,
                 ))): ?>
         <?php echo CHtml::link('удалить', $this->createUrl('#', array('id' => $c->id)), array('id' => 'CommunityContent_delete_' . $c->id, 'submit' => array('community/delete', 'id' => $c->id), 'confirm' => 'Вы точно хотите удалить тему?')); ?>
         <?php endif; ?>

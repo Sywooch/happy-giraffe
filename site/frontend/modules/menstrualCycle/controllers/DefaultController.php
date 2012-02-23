9bebfef428be7caea91e95f4c6ed45dc7b5406c9
ChooJoy <choojoy.work@gmail.com>
2012-02-23 13:25:53 +0400

    Services titles

--- site/frontend/modules/menstrualCycle/controllers/DefaultController.php
+++ site/frontend/modules/menstrualCycle/controllers/DefaultController.php
@@ -16,6 +16,8 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
+        $this->pageTitle = 'Менструальный цикл - составь свой женский календарь';
+
         $this->render('index');
     }
 

6d793d818dfe8c26dbbecda972452abf7ad15277
Kireev Alex <alexk984@gmail.com>
2012-02-24 14:31:59 +0400

    fix

--- site/common/migrations/m120224_082443_col_names_fixes.php
+++ site/common/migrations/m120224_082443_col_names_fixes.php
@@ -17,13 +17,17 @@ class m120224_082443_col_names_fixes extends CDbMigration
 
         $this->_table = 'club_community_content';
         $this->alterColumn($this->_table, 'created', 'TIMESTAMP NULL');
-        $this->addColumn($this->_table, 'updated', 'TIMESTAMP NULL on update CURRENT_TIMESTAMP DEFAULT null AFTER `text`');
+        $this->addColumn($this->_table, 'updated', 'TIMESTAMP NULL on update CURRENT_TIMESTAMP DEFAULT null AFTER `name`');
         $this->alterColumn($this->_table, 'created', 'TIMESTAMP NOT NULL');
 	}
 
 	public function down()
 	{
         $this->_table = 'comment';
+        $this->dropColumn($this->_table,'updated');
+        $this->_table = 'message_log';
+        $this->dropColumn($this->_table,'updated');
+        $this->_table = 'club_community_content';
         $this->dropColumn($this->_table,'updated');
 	}
 }

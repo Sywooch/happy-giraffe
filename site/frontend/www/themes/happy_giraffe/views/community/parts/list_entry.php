3d6471fc8d30d6e893e030e3011fabc5a63ccc83
Kireev Alex <alexk984@gmail.com>
2012-02-25 12:23:35 +0400

    user rights on english

--- site/frontend/www/themes/happy_giraffe/views/community/parts/list_entry.php
+++ site/frontend/www/themes/happy_giraffe/views/community/parts/list_entry.php
@@ -42,15 +42,15 @@
 					break;
 			}
 		?>
-        <?php if ($c->contentAuthor->id == Yii::app()->user->id ||
-        Yii::app()->authManager->checkAccess('edit post',Yii::app()->user->id, array(
+        <?php if ($c->contentAuthor->id == Yii::app()->user->getId() ||
+        Yii::app()->authManager->checkAccess('edit post',Yii::app()->user->getId(), array(
             'community_id'=>$c->rubric->community->id,
         )) ||
-        Yii::app()->authManager->checkAccess('transfer post',Yii::app()->user->id)): ?>
+        Yii::app()->authManager->checkAccess('transfer post',Yii::app()->user->getId())): ?>
 			<?php echo CHtml::link('редактировать', ($c->type->slug == 'travel') ? $this->createUrl('community/editTravel', array('id' => $c->id)) : $this->createUrl('community/edit', array('content_id' => $c->id))); ?>
         <?php endif; ?>
-        <?php if ($c->contentAuthor->id == Yii::app()->user->id || Yii::app()->authManager->checkAccess('delete post',
-            Yii::app()->user->id, array(
+        <?php if ($c->contentAuthor->id == Yii::app()->user->getId() || Yii::app()->authManager->checkAccess('delete post',
+            Yii::app()->user->getId(), array(
                 'community_id'=>$c->rubric->community->id,
             ))): ?>
 			<?php echo CHtml::link('удалить', $this->createUrl('#', array('id' => $c->id)), array('submit'=>array('admin/communityContent/delete','id'=>$c->id),'confirm'=>'Вы уверены?')); ?>

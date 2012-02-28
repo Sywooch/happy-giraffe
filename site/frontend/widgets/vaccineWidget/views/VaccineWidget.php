5b079deceab71ad7b051a276bdcff74b2857c1f2
Kireev Alex <alexk984@gmail.com>
2012-02-28 15:20:33 +0400

    fixes

--- site/frontend/widgets/vaccineWidget/views/VaccineWidget.php
+++ site/frontend/widgets/vaccineWidget/views/VaccineWidget.php
@@ -49,7 +49,7 @@ else
 				</td>
                 <td><?php echo $day->age ?></td>
                 <td><?php echo $day->GetText() ?></td>
-                <td><?php echo CHtml::link($day->vaccine->name, '#') ?></td>
+                <td><?php echo $day->vaccine->name ?></td>
 			</tr>
         <?php if (!empty($baby_id)):?>
 			<tr class="bottom">

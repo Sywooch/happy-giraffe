e6aa82f3594cba51f3d6dcae855a5f08bda7833f
lnghost <lnghost@hotmail.com>
2012-02-29 17:38:21 +0400

    Hotfix

--- site/frontend/widgets/user/UserAlbumWidget.php
+++ site/frontend/widgets/user/UserAlbumWidget.php
@@ -3,7 +3,7 @@
  * User: Eugene
  * Date: 29.02.12
  */
-class UserAlbumWidget extends CWidget
+class UserAlbumWidget extends UserCoreWidget
 {
     /**
      * @var User
@@ -12,6 +12,7 @@ class UserAlbumWidget extends CWidget
 
     public function run()
     {
+        parent::init();
         $albums = $this->model->getRelated('albums', true, array('limit' => 2));
         Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl . '/stylesheets/user.css');
         $this->render('UserAlbumWidget', array(

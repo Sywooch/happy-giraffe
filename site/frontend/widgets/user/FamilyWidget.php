9ebe024672b23925410ab14840a54e499c103657
Kireev Alex <alexk984@gmail.com>
2012-03-01 19:06:06 +0400

    profile fixes

--- site/frontend/widgets/user/FamilyWidget.php
+++ site/frontend/widgets/user/FamilyWidget.php
@@ -8,7 +8,7 @@ class FamilyWidget extends UserCoreWidget
     public function init()
     {
         parent::init();
-        $this->visible = $this->isMyProfile || (count($this->user->babies) > 0) || (
+        $this->visible = (count($this->user->babies) > 0) || (
             User::relationshipStatusHasPartner($this->user->relationship_status) && isset($this->user->partner));
     }
 }

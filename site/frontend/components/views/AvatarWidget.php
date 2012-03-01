6f396a8daa778fe624b7e48f278c815d95cbddb7
Kireev Alex <alexk984@gmail.com>
2012-03-01 19:17:22 +0400

    ava fix

--- site/frontend/components/views/AvatarWidget.php
+++ site/frontend/components/views/AvatarWidget.php
@@ -1,6 +1,6 @@
-<a class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>">
+<div class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>">
     <a href="<?php echo Yii::app()->createUrl('user/profile', array('user_id'=>$user->id)) ?>">
         <?php echo CHtml::image($user->getAva()); ?>
     </a>
-</a>
+</div>
 <?php if($this->withMail) echo $user->getDialogLink(); ?>

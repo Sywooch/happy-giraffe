f240e0427ad5818b204d19dfda7f9f47cd1cb991
Kireev Alex <alexk984@gmail.com>
2012-03-01 19:16:50 +0400

    profile fixes

--- site/frontend/components/views/AvatarWidget.php
+++ site/frontend/components/views/AvatarWidget.php
@@ -1,5 +1,6 @@
-<a class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>"
-   href="<?php echo Yii::app()->createUrl('user/profile', array('user_id'=>$user->id)) ?>">
+<a class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>">
+    <a href="<?php echo Yii::app()->createUrl('user/profile', array('user_id'=>$user->id)) ?>">
         <?php echo CHtml::image($user->getAva()); ?>
+    </a>
 </a>
 <?php if($this->withMail) echo $user->getDialogLink(); ?>

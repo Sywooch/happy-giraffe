ea6fa64f783562cb30bbec3bf4f2300a9d9d0eb8
Kireev Alex <alexk984@gmail.com>
2012-03-01 19:08:51 +0400

    profile fixes

--- site/frontend/components/views/AvatarWidget.php
+++ site/frontend/components/views/AvatarWidget.php
@@ -1,4 +1,6 @@
 <div class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>">
+    <a href="<?php echo $this->createUrl('user/profile', array('user_id'=>$user->id)) ?>">
 	<?php if ($ava = $user->pic_small->getUrl('ava')) echo CHtml::image($ava); ?>
+    </a>
 </div>
 <?php if($this->withMail) echo $user->getDialogLink(); ?>

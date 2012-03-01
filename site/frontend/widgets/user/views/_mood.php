386b8cd25d06813f6a7586424eaa27fcd68848ce
ChooJoy <choojoy.work@gmail.com>
2012-03-01 20:27:27 +0400

    fixes

--- site/frontend/widgets/user/views/_mood.php
+++ site/frontend/widgets/user/views/_mood.php
@@ -1,1 +1,5 @@
-Мое настроение &ndash; <img src="/images/mood_smiles/mood_smile_<?php echo $mood->id; ?>.png" /><?php if ($canUpdate): ?> <a href="" class="pseudo small">Изменить</a><?php endif; ?>
+<div id="userMoodTooltip">
+    <span><?php echo $mood->name; ?></span>
+</div>
+
+Мое настроение &ndash; <img id="userMood" src="/images/mood_smiles/mood_smile_<?php echo $mood->id; ?>.png" /><?php if ($canUpdate): ?> <a href="" class="pseudo small">Изменить</a><?php endif; ?>

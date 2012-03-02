039427a76d74a225b090f52f963e89a60e9a26a8
Eugene <lnghost@hotmail.com>
2012-03-02 16:03:52 +0400

    sf

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -250,7 +250,7 @@ class SiteController extends Controller
 	 */
 	public function actionLogout()
 	{
-		Yii::app()->user->logout();
+		Yii::app()->user->logout(false);
 		$this->redirect(Yii::app()->request->urlReferrer);
 	}
 

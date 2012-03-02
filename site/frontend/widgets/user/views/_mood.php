9400a42611c34b1ae5027e5044187cf7f7a4e67b
ChooJoy <choojoy.work@gmail.com>
2012-03-02 13:11:24 +0400

    fix

--- site/frontend/widgets/user/views/_mood.php
+++ site/frontend/widgets/user/views/_mood.php
@@ -2,4 +2,4 @@
     <span><?php echo $mood->name; ?></span>
 </div>
 
-Мое настроение &ndash; <img id="userMood" src="/images/mood_smiles/mood_smile_<?php echo $mood->id; ?>.png" /><?php if ($canUpdate): ?> <a href="" class="pseudo small">Изменить</a><?php endif; ?>
+Мое настроение &ndash; <img id="userMood" src="/images/widget/mood/<?php echo $mood->id; ?>.png" /><?php if ($canUpdate): ?> <a href="" class="pseudo small">Изменить</a><?php endif; ?>

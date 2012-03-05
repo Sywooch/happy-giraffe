2247fbf339e1496d204602ed734f63c6db3acd0e
ChooJoy <choojoy.work@gmail.com>
2012-03-05 15:50:38 +0400

    communities

--- site/frontend/components/views/AvatarWidget.php
+++ site/frontend/components/views/AvatarWidget.php
@@ -1,6 +1,5 @@
-<div class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?>">
-    <a href="<?php echo Yii::app()->createUrl('user/profile', array('user_id'=>$user->id)) ?>">
+<div class="ava<?php if ($user->gender !== null) echo ($user->gender) ? ' male' : ' female'; ?> avatar">
+    <a href="<?php echo Yii::app()->createUrl('user/profile', array('user_id' => $user->id)) ?>">
         <?php echo CHtml::image($user->getAva()); ?>
     </a>
 </div>
-<?php if($this->withMail) echo $user->getDialogLink(); ?>

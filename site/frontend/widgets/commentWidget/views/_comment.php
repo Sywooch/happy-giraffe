966c524b02e1ea4941f05c1bfa48442def7ea9bf
Eugene <lnghost@hotmail.com>
2012-03-05 16:46:22 +0400

    Comment hotfix
    Signed-off-by: Eugene <lnghost@hotmail.com>

--- site/frontend/widgets/commentWidget/views/_comment.php
+++ site/frontend/widgets/commentWidget/views/_comment.php
@@ -1,40 +1,43 @@
 <li class="clearfix item" id="comment_<?php echo $data->id; ?>">
-    <div class="user">
-        <?php $this->widget('AvatarWidget', array('user' => $data->author, 'withMail' => false)); ?>
-        <div class="details">
-            <span class="icon-status status-<?php echo $data->author->online == 1 ? 'online' : 'offline'; ?>"></span>
-            <a href="javascript:void(0);" class="username"><?php echo $data->author->fullName; ?></a>
-            <?php if($data->author->country !== null): ?>
+    <div class="clearfix">
+        <div class="user">
+            <?php $this->widget('AvatarWidget', array('user' => $data->author, 'withMail' => false)); ?>
+            <div class="details">
+                <span
+                    class="icon-status status-<?php echo $data->author->online == 1 ? 'online' : 'offline'; ?>"></span>
+                <a href="javascript:void(0);" class="username"><?php echo $data->author->fullName; ?></a>
+                <?php if ($data->author->country !== null): ?>
                 <div class="location">
                     <div class="flag flag-<?php echo $data->author->country->iso_code; ?>"></div>
                     <?php echo $data->author->country->name; ?>
                 </div>
-            <?php endif; ?>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
-    <div class="content">
-        <div class="meta">
-            <span class="num" id="cp_<?php echo $data->position; ?>"><?php echo $data->position; ?></span>
-            <span class="date"><?php echo HDate::GetFormattedTime($data->created, ', '); ?></span>
-            <?php if(($data->response_id !== 0 && $response = $data->response) || ($data->quote_id !== 0 && $response = $data->quote)): ?>
+        <div class="content">
+            <div class="meta">
+                <span class="num" id="cp_<?php echo $data->position; ?>"><?php echo $data->position; ?></span>
+                <span class="date"><?php echo HDate::GetFormattedTime($data->created, ', '); ?></span>
+                <?php if (($data->response_id !== 0 && $response = $data->response) || ($data->quote_id !== 0 && $response = $data->quote)): ?>
                 <div class="answer">
                     Ответ для
                     <?php $this->widget('AvatarWidget', array('user' => $response->author, 'withMail' => false)); ?>
-                    на <span class="num"><a href="#" onclick="return Comment.goTo(<?php echo $response->position; ?>, <?php echo $currentPage + 1; ?>);"><?php echo $response->position; ?></a></span>
+                    на <span class="num"><a href="#"
+                                            onclick="return Comment.goTo(<?php echo $response->position; ?>, <?php echo $currentPage + 1; ?>);"><?php echo $response->position; ?></a></span>
                 </div>
-            <?php endif; ?>
-        </div>
-        <?php if(($data->quote_id !== 0 && $data->quote)): ?>
-            <input type="hidden" name="selectable_quote" value="<?php echo $data->quote_text != '' ? 1 : 0; ?>" />
+                <?php endif; ?>
+            </div>
+            <?php if (($data->quote_id !== 0 && $data->quote)): ?>
+            <input type="hidden" name="selectable_quote" value="<?php echo $data->quote_text != '' ? 1 : 0; ?>"/>
             <div class="quote" id="commentQuote_<?php echo $data->quote->id; ?>">
                 <?php echo $data->quote_text != '' ? $data->quote_text : $data->quote->text; ?>
             </div>
-        <?php endif; ?>
-        <?php if($data->removed == 0): ?>
+            <?php endif; ?>
+            <?php if ($data->removed == 0): ?>
             <div class="content-in">
                 <?php echo $data->text; ?>
             </div>
-            <?php if(!Yii::app()->user->isGuest): ?>
+            <?php if (!Yii::app()->user->isGuest): ?>
                 <div class="actions">
                     <?php
                     if ($data->author->id != Yii::app()->user->id) {
@@ -43,38 +46,39 @@
                         $this->endWidget();
                     }
                     ?>
-                    <?php if (Yii::app()->user->checkAccess('editComment',array('user_id'=>$data->author->id)) || Yii::app()->user->checkAccess('removeComment',array('user_id'=>$data->author->id))): ?>
-                        <div class="admin-actions">
-                            <?php if (Yii::app()->user->checkAccess('editComment',array('user_id'=>$data->author->id))): ?>
-                                <?php echo CHtml::link('<i class="icon"></i>', '', array('class' => 'edit', 'onclick' => 'return Comment.edit(this);')); ?>
-                            <?php endif; ?>
-                            <?php if (Yii::app()->user->checkAccess('removeComment',array('user_id'=>$data->author->id))): ?>
-                                <?php $this->widget('site.frontend.widgets.removeWidget.RemoveWidget', array(
-                                    'model' => $data,
-                                    'callback' => 'Comment.remove',
-                                    'author' => !Yii::app()->user->isGuest && Yii::app()->user->id == $data->author->id
-                                )); ?>
-                            <?php endif; ?>
-                        </div>
+                    <?php if (Yii::app()->user->checkAccess('editComment', array('user_id' => $data->author->id)) || Yii::app()->user->checkAccess('removeComment', array('user_id' => $data->author->id))): ?>
+                    <div class="admin-actions">
+                        <?php if (Yii::app()->user->checkAccess('editComment', array('user_id' => $data->author->id))): ?>
+                        <?php echo CHtml::link('<i class="icon"></i>', '', array('class' => 'edit', 'onclick' => 'return Comment.edit(this);')); ?>
+                        <?php endif; ?>
+                        <?php if (Yii::app()->user->checkAccess('removeComment', array('user_id' => $data->author->id))): ?>
+                        <?php $this->widget('site.frontend.widgets.removeWidget.RemoveWidget', array(
+                            'model' => $data,
+                            'callback' => 'Comment.remove',
+                            'author' => !Yii::app()->user->isGuest && Yii::app()->user->id == $data->author->id
+                        )); ?>
+                        <?php endif; ?>
+                    </div>
                     <?php endif; ?>
                     <a href="javascript:void(0);" onclick="return Comment.response(this);">Ответить</a>
                     &nbsp;
                     <a href="javascript:void(0);" onclick="return Comment.quote(this);">С цитатой</a>
                 </div>
+                <?php endif; ?>
             <?php endif; ?>
-        <?php endif; ?>
-        <?php if($data->removed == 1): ?>
+            <?php if ($data->removed == 1): ?>
             <div class="content-in">
-                <?php if($data->remove->type == 0): ?>
-                    Комментарий удален автором.
+                <?php if ($data->remove->type == 0): ?>
+                Комментарий удален автором.
                 <?php else: ?>
-                    <?php if($data->remove->type != 4): ?>
-                        Комментарий удален. Причина: <?php echo Removed::$types[$data->remove->type]; ?>
+                <?php if ($data->remove->type != 4): ?>
+                    Комментарий удален. Причина: <?php echo Removed::$types[$data->remove->type]; ?>
                     <?php else: ?>
-                        Комментарий удален модератором.
+                    Комментарий удален модератором.
                     <?php endif; ?>
                 <?php endif; ?>
             </div>
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </li>

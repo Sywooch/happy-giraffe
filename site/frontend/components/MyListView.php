86550f3c5f1e849b0e4c6b45f6fa889b8ccea735
Eugene <lnghost@hotmail.com>
2012-03-06 11:47:02 +0400

    Ôèêñû êîììåíòàðèåâ

--- site/frontend/components/MyListView.php
+++ site/frontend/components/MyListView.php
@@ -16,7 +16,11 @@ class MyListView extends CListView
         $js = 'var history_' . $this->id . ' = new AjaxHistory("' .$this->id  . '");';
         Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/javascripts/history.js')
             ->registerScript('history_' . $this->id, $js, CClientScript::POS_END);
+        $ajaxUpdate = null;
+        if($this->afterAjaxUpdate)
+            $ajaxUpdate = $this->afterAjaxUpdate;
         $this->afterAjaxUpdate = 'function(id, data) {
+            '.($ajaxUpdate ? $ajaxUpdate : '').'
             history_' . $this->id . '.changeBrowserUrl($.fn.yiiListView.getUrl(id));
         }';
     }

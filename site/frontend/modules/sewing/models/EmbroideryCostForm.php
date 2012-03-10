2a35246d6a20e6c3841013d634703e167ab5025e
Kireev Alex <alexk984@gmail.com>
2012-03-10 09:33:17 +0400

    sewing fix

--- site/frontend/modules/sewing/models/EmbroideryCostForm.php
+++ site/frontend/modules/sewing/models/EmbroideryCostForm.php
@@ -13,6 +13,10 @@ class EmbroideryCostForm extends CFormModel
     public $colors_count;
     public $canva;
 
+    public $more_colors;
+    public $small_canva;
+    public $user_design;
+
     /**
      * @var YarnProjects
      */
@@ -24,6 +28,7 @@ class EmbroideryCostForm extends CFormModel
             array('width, height, cross_price, material_price', 'required', 'message'=>'Введите {attribute}'),
             array('width, height, canva, colors_count', 'numerical', 'integerOnly' => true, 'message'=>'Вводите только цифры'),
             array('cross_price, material_price, design_price', 'numerical', 'message'=>'Вводите только цифры'),
+            array('small_canva, more_colors, user_design', 'safe')
         );
     }
 
@@ -38,5 +43,19 @@ class EmbroideryCostForm extends CFormModel
             'colors_count'=>'Количество цветов',
             'canva'=>'Размер канвы',
         );
+    }
+
+    public function afterValidate()
+    {
+        if ($this->small_canva && empty($this->canva))
+            $this->addError('canva', 'Введите размер канвы');
+
+        if ($this->more_colors && empty($this->colors_count))
+            $this->addError('colors_count', 'Введите количество цветов');
+
+        if ($this->user_design && empty($this->design_price))
+            $this->addError('design_price', 'Введите стоимость вашего дизайна');
+
+        parent::afterValidate();
     }
 }

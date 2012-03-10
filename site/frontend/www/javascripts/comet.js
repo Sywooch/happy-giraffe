7490693b77cafcb6d12e2bdac7dde52665d516ef
Eugene <lnghost@hotmail.com>
2012-03-10 18:02:22 +0400

    Hotfixes

--- site/frontend/www/javascripts/comet.js
+++ site/frontend/www/javascripts/comet.js
@@ -1,20 +1,10 @@
 function Comet() {
     this.events = new Array();
-    this.events[0] = [];
-    this.events[1] = [];
-    this.events[2] = [];
-    this.events[3] = [];
-    this.events[4] = [];
-    this.events[5] = [];
-    this.events[6] = [];
-    this.events[7] = [];
-    this.events[8] = [];
-    this.events[9] = [];
-    this.events[10] = [];
-
 }
 
 Comet.prototype.call = function(type, result, id) {
+    if(this.events[type] == undefined)
+        this.events[type] = new Array();
     for(var i in this.events[type]) {
         var func = this.events[type][i];
         if(this[func] != 'undefined')
@@ -36,17 +26,3 @@ Comet.prototype.addEvent = function(type, event) {
 }
 
 var comet = new Comet;
-
-/*Comet.prototype.testfunc = function(result, id) {
-    cl(123);
-}
-
-// Custom function, ex.
-Comet.prototype.testfunc2 = function(result, id) {
-    cl(456);
-}
-comet.addEvent(0, 'testfunc2');*/
-
-
-
-

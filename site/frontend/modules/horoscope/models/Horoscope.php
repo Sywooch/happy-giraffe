034b5105f8091931e317816754b85ed28d62bd6c
Kireev Alex <alexk984@gmail.com>
2012-03-11 22:17:39 +0400

    horoscope fixes

--- site/frontend/modules/horoscope/models/Horoscope.php
+++ site/frontend/modules/horoscope/models/Horoscope.php
@@ -99,6 +99,13 @@ class Horoscope extends CActiveRecord
         );
     }
 
+    public function defaultScope()
+    {
+        return array(
+            'order' => 'date desc',
+        );
+    }
+
     /**
      * Retrieves a list of models based on the current search/filter conditions.
      * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.

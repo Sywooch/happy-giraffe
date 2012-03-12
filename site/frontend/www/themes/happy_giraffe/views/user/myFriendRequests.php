91e2ef88e965e49b974d7f2bf0ca596be7a35f3a
ChooJoy <choojoy.work@gmail.com>
2012-03-12 15:30:56 +0400

    profile

---
+++ site/frontend/www/themes/happy_giraffe/views/user/myFriendRequests.php
@@ -0,0 +1,19 @@
+<div class="user-cols clearfix">
+
+    <div class="col-1">
+
+        <div class="side-filter">
+            <?php $this->renderPartial('_friends_sidebar'); ?>
+        </div>
+
+    </div>
+
+    <div class="col-23 clearfix">
+
+        <div class="content-title">Друзья</div>
+
+
+
+    </div>
+
+</div>

02ef54786be13ba659eda0c05f4944bc3b0a2c21
Eugene <lnghost@hotmail.com>
2012-03-13 20:20:32 +0400

    Albums
    Signed-off-by: Eugene <lnghost@hotmail.com>

--- site/frontend/www/themes/happy_giraffe/views/albums/attach_index.php
+++ site/frontend/www/themes/happy_giraffe/views/albums/attach_index.php
@@ -1,6 +1,29 @@
-<p><?php echo CHtml::link('Добавить фото', array('photos/add'), array('class' => 'fancy')); ?></p>
-<ul>
-    <?php foreach ($dataProvider->getData() as $model): ?>
-    <li><?php echo CHtml::link($model->title, array('album/attachView', 'id' => $model->id)); ?></li>
-    <?php endforeach; ?>
-</ul>
+<div class="popup" id="contestPhotoPick">
+    <a onclick="$.fancybox.close();" class="popup-close" href="javascript:void(0);">закрыть</a>
+    <div class="title">Фотография для конкурса «Осенние прогулки»</div>
+    <div class="nav">
+        <ul>
+            <li class="active"><a href="">С компьютера</a></li>
+            <li><a href="">Из моих альбомов</a></li>
+        </ul>
+    </div>
+    <form>
+        <div class="form">
+            <div class="photo-upload clearfix">
+                <div class="text">Фото</div>
+                <div class="photo">
+                    <div class="in">
+                        <div class="file-fake">
+                            <button class="btn btn-orange"><span><span>Обзор...</span></span></button>
+                            <input type="file">
+                        </div>
+                    </div>
+                </div>
+                <div class="note">Загрузите файл<br>(jpg, gif, png не более 4 МБ)</div>
+            </div>
+            <div class="form-bottom">
+                <button class="btn btn-green-medium"><span><span>Добавить на конкурс</span></span></button>
+            </div>
+        </div>
+    </form>
+</div>

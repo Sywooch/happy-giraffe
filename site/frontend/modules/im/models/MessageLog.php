510cd2ef4f924f17b6f1df9dc7b1c7039520fc5a
Kireev Alex <alexk984@gmail.com>
2012-03-14 23:25:35 +0400

    im test

--- site/frontend/modules/im/models/MessageLog.php
+++ site/frontend/modules/im/models/MessageLog.php
@@ -293,6 +293,7 @@ class MessageLog extends CActiveRecord
             ->limit(3)
             ->queryAll();
 
+        echo implode(',', Im::model($user_id)->getDialogIds());
         $data = array();
         foreach ($models as $m) {
             $data[] = array(

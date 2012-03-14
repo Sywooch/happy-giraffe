6a9cda6f41a6ba31ec330484b80513a18f9c393a
Eugene <lnghost@hotmail.com>
2012-03-14 15:11:19 +0400

    Interests

--- site/frontend/widgets/user/assets/interest.js
+++ site/frontend/widgets/user/assets/interest.js
@@ -3,7 +3,9 @@ var Interest = {
 };
 
 Interest.changeCategory = function(link) {
+    $(link).parent().addClass('active').siblings().removeClass('active');
     var index = $(link).parent().index();
-    $('#interestsEdit').find('.interests-list ul.active').hide();
+    $('#interestsEdit').find('.interests-list ul.active').removeClass('active');
+    $('#interestsEdit').find('.interests-list ul:eq('+index+')').addClass('active');
     return false;
 }

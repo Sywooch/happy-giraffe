3ee060580f6ee50d60b49a7594591f17d11851ca
Kireev Alex <alexk984@gmail.com>
2012-03-14 23:21:40 +0400

    im test

--- site/frontend/modules/im/models/MessageLog.php
+++ site/frontend/modules/im/models/MessageLog.php
@@ -285,7 +285,7 @@ class MessageLog extends CActiveRecord
         $models = Yii::app()->db->createCommand()
             ->select(array('id', 'user_id', 'text', 'created', 'read_status', 'dialog_id'))
             ->from('message_log as t')
-            ->where('t.dialog_id IN (:dialogs) AND t.user_id <> :user_id AND t.id not in (SELECT message_id FROM message_deleted WHERE user_id = :user_id)', array(
+            ->where(' t.dialog_id IN (:dialogs) AND t.user_id != :user_id AND t.id not in (SELECT message_id FROM message_deleted WHERE user_id = :user_id) ', array(
             ':user_id' => Yii::app()->user->id,
             ':dialogs' => implode(',', Im::model($user_id)->getDialogIds())
         ))

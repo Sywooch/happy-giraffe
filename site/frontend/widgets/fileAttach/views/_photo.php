73212ba1d28efe2f05b835f0206731aa313d8055
Eugene <lnghost@hotmail.com>
2012-03-14 10:21:01 +0400

    Album Attaches

---
+++ site/frontend/widgets/fileAttach/views/_photo.php
@@ -0,0 +1,22 @@
+<li>
+    <table>
+        <tbody>
+            <tr>
+                <td class="img">
+                    <div>
+                        <?php echo CHtml::link(CHtml::image($data->getPreviewUrl(150, 150)), array('/albums/photo', 'id' => $data->id)); ?>
+                        <div class="contest-send">
+                            <a class="btn btn-green-medium" href=""><span><span>Добавить<br>на конкурс</span></span></a>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr class="rank"><td><span><?php echo Rating::model()->countByEntity($data); ?></span> баллов</td></tr>
+            <?php if($data->title != ''): ?>
+                <tr class="title">
+                    <td align="center"><div><?php echo $data->title; ?></div></td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</li>

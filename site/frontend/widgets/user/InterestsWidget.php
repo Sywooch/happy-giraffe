3568233827ec7bb142bc166d93e06473e32f216e
Eugene <lnghost@hotmail.com>
2012-03-16 09:02:06 +0400

    Fixes

--- site/frontend/widgets/user/InterestsWidget.php
+++ site/frontend/widgets/user/InterestsWidget.php
@@ -8,6 +8,13 @@ class InterestsWidget extends UserCoreWidget
     public function init()
     {
         Yii::import('site.common.models.interest.*');
+
+        if((Yii::app()->user->isGuest || Yii::app()->user->id != $this->user->id) && count($this->user->interests) == 0)
+        {
+            $this->visible = false;
+            return;
+        }
+
         $basePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
         $baseUrl = Yii::app()->getAssetManager()->publish($basePath, false, 1, YII_DEBUG);
         Yii::app()->clientScript->registerScriptFile($baseUrl . '/interest.js');

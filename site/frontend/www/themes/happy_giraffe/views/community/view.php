4f933911411548048d4f3386757e635607b03314
ChooJoy <choojoy.work@gmail.com>
2012-03-16 19:11:15 +0400

    fix

--- site/frontend/www/themes/happy_giraffe/views/community/view.php
+++ site/frontend/www/themes/happy_giraffe/views/community/view.php
@@ -45,7 +45,7 @@
             }
         ?>
             <div class="block">
-                <b><?php echo CHtml::link($rc->name, $this->createUrl('community/view', array('community_id' => $c->rubric->community->id, 'content_type_slug' => $rc->type->slug, 'content_id' => $rc->id))); ?></b>
+                <b><?php echo CHtml::link($rc->name, $this->createUrl('community/view', array('community_id' => $data->rubric->community->id, 'content_type_slug' => $rc->type->slug, 'content_id' => $rc->id))); ?></b>
                 <p><?php echo $content; ?></p>
             </div>
         <?php

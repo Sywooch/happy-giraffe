b58f4491e813cc620fa7ab2247e21e1be13f1ca7
Kireev Alex <alexk984@gmail.com>
2012-03-16 18:53:28 +0400

    fixes

--- site/common/migrations/m120316_145013_user_relations.php
+++ site/common/migrations/m120316_145013_user_relations.php
@@ -8,25 +8,25 @@ class m120316_145013_user_relations extends CDbMigration
         SET foreign_key_checks = 0;
 LTER TABLE  `user` DROP FOREIGN KEY  `user_country_fk` ;
 
-ALTER TABLE  `user` ADD FOREIGN KEY (  `country_id` ) REFERENCES  `hg`.`geo_country` (
+ALTER TABLE  `user` ADD FOREIGN KEY (  `country_id` ) REFERENCES  `happy_giraffe`.`geo_country` (
 `id`
 ) ON DELETE SET NULL ON UPDATE SET NULL ;
 
 ALTER TABLE  `user` DROP FOREIGN KEY  `user_settlement_fk` ;
 
-ALTER TABLE  `user` ADD FOREIGN KEY (  `settlement_id` ) REFERENCES  `hg`.`geo_rus_settlement` (
+ALTER TABLE  `user` ADD FOREIGN KEY (  `settlement_id` ) REFERENCES  `happy_giraffe`.`geo_rus_settlement` (
 `id`
 ) ON DELETE SET NULL ON UPDATE SET NULL ;
 
 ALTER TABLE  `user` DROP FOREIGN KEY  `user_street_fk` ;
 
-ALTER TABLE  `user` ADD FOREIGN KEY (  `street_id` ) REFERENCES  `hg`.`geo_rus_street` (
+ALTER TABLE  `user` ADD FOREIGN KEY (  `street_id` ) REFERENCES  `happy_giraffe`.`geo_rus_street` (
 `id`
 ) ON DELETE SET NULL ON UPDATE SET NULL ;
 
 ALTER TABLE  `user` DROP FOREIGN KEY  `user_ibfk_1` ;
 
-ALTER TABLE  `user` ADD FOREIGN KEY (  `mood_id` ) REFERENCES  `hg`.`user_moods` (
+ALTER TABLE  `user` ADD FOREIGN KEY (  `mood_id` ) REFERENCES  `happy_giraffe`.`user_moods` (
 `id`
 ) ON DELETE SET NULL ON UPDATE SET NULL ;
 SET foreign_key_checks = 1;

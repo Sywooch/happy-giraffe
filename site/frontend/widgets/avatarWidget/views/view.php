8f93cefaf13e7c85888e74faad948c19c6d0a691
Kireev Alex <alexk984@gmail.com>
2012-03-16 16:33:03 +0400

    set virtual and moderators

--- site/frontend/widgets/avatarWidget/views/view.php
+++ site/frontend/widgets/avatarWidget/views/view.php
@@ -2,26 +2,31 @@
     $class = 'ava';
     if ($this->user->gender !== null) $class .= ' ' . (($this->user->gender) ? 'male' : 'female');
     if ($this->size !== 'ava') $class .= ' ' . $this->size;
-?>
 
+$show_link_to_profile = true;
+if ($this->user->role == 'virtual user' || $this->user->role == 'moderator')
+    $show_link_to_profile = false;
+
+$link_to_profile = $this->user->url;
+if ($this->size == 'big' && $this->user->id == Yii::app()->user->getId())
+    $link_to_profile = Yii::app()->createUrl('profile/photo', array('returnUrl'=>urlencode(Yii::app()->createUrl('user/profile', array('user_id'=>$this->user->id)))));
+
+?>
 <div class="user-info clearfix">
-    <?php if (Yii::app()->user->getId() > 10371):?>
-    <a class="<?=$class?>" href="<?=($this->user->id == Yii::app()->user->id && $this->size == 'big')? Yii::app()->createUrl('profile/photo', array('returnUrl'=>urlencode(Yii::app()->createUrl('user/profile', array('user_id'=>Yii::app()->user->getId()))))):$this->user->url?>">
+    <a <?php if (!$show_link_to_profile) echo 'onclick="return false;"';?> class="<?=$class?>"
+        href="<?=($show_link_to_profile)?$link_to_profile:'#'?>">
         <?php echo CHtml::image($this->user->getAva($this->size)); ?>
     </a>
-    <?php else: ?>
-    <a <?php if ($this->size != 'big') echo 'onclick="return false;"';?> class="<?=$class?>" href="<?=($this->user->id == Yii::app()->user->id && $this->size == 'big')? Yii::app()->createUrl('profile/photo', array('returnUrl'=>urlencode(Yii::app()->createUrl('user/profile', array('user_id'=>Yii::app()->user->getId()))))):'#'//$this->user->url?>">
-        <?php echo CHtml::image($this->user->getAva($this->size)); ?>
-    </a>
-    <?php endif ?>
     <?php if(!$this->small): ?>
         <div class="details">
             <span class="icon-status status-<?php echo $this->user->online == 1 ? 'online' : 'offline'; ?>"></span>
-            <?php if (Yii::app()->user->getId() > 10371):?>
+
+            <?php if ($show_link_to_profile):?>
                 <a href="<?=$this->user->url ?>"><?php echo $this->user->fullName ?></a>
             <?php else: ?>
                 <?php echo $this->user->fullName ?>
             <?php endif ?>
+
             <?php if ($this->user->country !== null): ?>
                 <div class="location">
                     <div class="flag flag-<?php echo $this->user->country->iso_code; ?>"></div>

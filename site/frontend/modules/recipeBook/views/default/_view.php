c6752f9c3f3bb858f2bd2d5ff9cdf84f854dd365
ChooJoy <choojoy.work@gmail.com>
2012-03-20 20:27:06 +0400

    fix

--- site/frontend/modules/recipeBook/views/default/_view.php
+++ site/frontend/modules/recipeBook/views/default/_view.php
@@ -13,7 +13,7 @@
         </div>
 
         <div class="meta">
-            <div class="time"><?php echo Yii::app()->dateFormatter->format('d MMMM yyyy, HH:mm', strtotime($data->create_time)) ?></div>
+            <div class="time"><?php echo Yii::app()->dateFormatter->format('d MMMM yyyy, HH:mm', $data->created) ?></div>
             <div class="seen">Просмотров:&nbsp;<span><?php echo $data->views_amount ?></span></div>
 
         </div>

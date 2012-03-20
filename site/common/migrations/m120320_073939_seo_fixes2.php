a7ef1b20484d4be9793eb5a92fb08b5b4b7e5978
Kireev Alex <alexk984@gmail.com>
2012-03-20 19:47:25 +0400

    name fix

--- site/common/migrations/m120320_073939_seo_fixes2.php
+++ site/common/migrations/m120320_073939_seo_fixes2.php
@@ -5,7 +5,6 @@ class m120320_073939_seo_fixes2 extends CDbMigration
 	public function up()
 	{
         $this->delete('seo__keywords');
-        $this->truncateTable('seo__keywords');
         $this->executeFile(Yii::getPathOfAlias('site'). '/common/migrations/seo__keywords.txt');
         $this->executeFile(Yii::getPathOfAlias('site'). '/common/migrations/seo__key_stats.txt');
 	}

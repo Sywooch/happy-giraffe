ef61a713c81d6971fcc5d8215ff1f9e2a123e13f
Kireev Alex <alexk984@gmail.com>
2012-03-22 17:12:43 +0400

    geo

---
+++ site/common/migrations/m120322_124124_drop_geo_fk.php
@@ -0,0 +1,26 @@
+<?php
+
+class m120322_124124_drop_geo_fk extends CDbMigration
+{
+	public function up()
+	{
+        $this->execute('SET foreign_key_checks = 0; alter table user DROP FOREIGN KEY user_settlement_fk;');
+	}
+
+	public function down()
+	{
+//		echo "m120322_124124_drop_geo_fk does not support migration down.\n";
+//		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}

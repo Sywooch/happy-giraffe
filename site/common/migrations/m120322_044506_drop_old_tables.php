5a7a35cc90216144aa1d7dc46777e7cfbc708ef2
Kireev Alex <alexk984@gmail.com>
2012-03-22 13:09:10 +0400

    fix

--- site/common/migrations/m120322_044506_drop_old_tables.php
+++ site/common/migrations/m120322_044506_drop_old_tables.php
@@ -4,10 +4,10 @@ class m120322_044506_drop_old_tables extends CDbMigration
 {
 	public function up()
 	{
-        $this->delete('seo_keywords');
-        $this->delete('seo_stats');
-        $this->execute('drop table IF EXISTS seo_keywords');
-        $this->execute('drop table IF EXISTS seo_stats');
+//        $this->delete('seo_keywords');
+//        $this->delete('seo_stats');
+//        $this->execute('drop table IF EXISTS seo_keywords');
+//        $this->execute('drop table IF EXISTS seo_stats');
         $this->execute('drop table IF EXISTS club_photo_comment');
         $this->execute('drop table IF EXISTS shop_product_comment');
 	}

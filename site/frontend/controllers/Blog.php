ed0a524859bdae816b4599cbe50b5c9b511a6d88
ChooJoy <choojoy.work@gmail.com>
2012-03-26 19:28:02 +0400

    start

---
+++ site/frontend/controllers/Blog.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: choo
+ * Date: 26.03.2012
+ */
+class Blog extends Controller
+{
+    public function actionAdd($content_type_slug = 'post')
+    {
+        $content_type = CommunityContentType::model()->findByAttributes(array('slug' => $content_type_slug));
+        $model = new CommunityContent;
+        $model->type_id = $content_type->id;
+        $slave_model_name = 'Community' . ucfirst($content_type->slug);
+        $slave_model = new $slave_model_name;
+        $this->render('form', array(
+            'model' => $model,
+            'slave_model' => $slave_model,
+        ));
+    }
+}
---
+++ site/frontend/www/themes/happy_giraffe/views/blog/form.php
@@ -0,0 +1,99 @@
+<div class="add-nav default-nav clearfix">
+    <div class="content-title">Добавить в блог:</div>
+    <ul>
+        <li class="active"><a href="">Запись</a></li>
+        <li><a href="">Видео</a></li>
+    </ul>
+</div>
+
+<div class="main">
+    <div class="main-in">
+
+        <?php $form = $this->widget('CActiveForm'); ?>
+            <div class="form row-form clearfix">
+
+                <?php if ($model->type_id == 1): ?>
+                    <div class="row clearfix">
+                        <div class="row-title">Заголовок:</div>
+                        <div class="row-elements">
+                            <?php echo $form->textField($model, 'name', array('class' => 'w-400')); ?>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="row-title">Текст:</div>
+                        <div class="row-elements">
+                            <img src="/images/editor.gif" />
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($model->type_id == 2): ?>
+                    <div class="row clearfix">
+                        <div class="row-title">Заголовок:</div>
+                        <div class="row-elements">
+                            <input type="text" class="w-400"/>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="row-title">Ссылка на видео:</div>
+                        <div class="row-elements">
+                            <input type="text" class="w-400"/>
+                            <div class="row-note">
+                                <div class="tale"></div>
+                                <p><span class="mdash">&mdash;</span>Вы можете разместить видео с YouTube и RuTube</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="row clearfix test-video w-400">
+                        <div class="row-title">
+                            <div class="img">
+                                <img src="http://i2.ytimg.com/vi/1H8s0Xthr-8/default.jpg" />
+                                <a href="" class="remove"></a>
+                            </div>
+                            Проверить ролик? <a href="" class="btn btn-green-small"><span><span>ДА</span></span></a>
+                            <p class="small w-200">В случае, если ссылка добавилась некорректно, вы сможете изменить ее до добавления</p>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="row-title">Комментарий к видео:</div>
+                        <div class="row-elements">
+                            <img src="/images/editor.gif" />
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <div class="row clearfix">
+                    <div class="row-title">Рубрика:</div>
+                    <div class="row-elements">
+                        <div class="select-box">
+                            <select class="chzn w-200">
+                                <option>Рубрика 1</option>
+                                <option>Рубрика 2</option>
+                                <option>Рубрика 3</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row row-buttons">
+                    <button class="btn btn-gray-medium"><span><span>Отменить</span></span></button>
+                    <button class="btn btn-yellow-medium"><span><span>Предпросмотр</span></span></button>
+                    <button class="btn btn-green-medium"><span><span>Добавить</span></span></button>
+                </div>
+
+            </div>
+        <?php $this->endWidget(); ?>
+
+    </div>
+</div>
+
+<div class="side-left">
+
+    <center><img src="/images/img_blog_add.png" /></center>
+
+</div>

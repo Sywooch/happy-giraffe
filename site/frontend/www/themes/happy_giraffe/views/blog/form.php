88ba16e041f03b1da00b91e437b82fcfbbc832ee
ChooJoy <choojoy.work@gmail.com>
2012-03-26 19:44:08 +0400

    start

--- site/frontend/www/themes/happy_giraffe/views/blog/form.php
+++ site/frontend/www/themes/happy_giraffe/views/blog/form.php
@@ -1,9 +1,19 @@
 <div class="add-nav default-nav clearfix">
     <div class="content-title">Добавить в блог:</div>
-    <ul>
-        <li class="active"><a href="">Запись</a></li>
-        <li><a href="">Видео</a></li>
-    </ul>
+    <?php
+        $this->widget('zii.widgets.CMenu', array(
+            'items'=>array(
+                array(
+                    'label' => 'Запись',
+                    'url' => array('/blog/add', array('content_type_slug' => 'post')),
+                ),
+                array(
+                    'label' => 'Видео',
+                    'url' => array('/blog/add', array('content_type_slug' => 'video')),
+                ),
+            ),
+        ));
+    ?>
 </div>
 
 <div class="main">
@@ -16,14 +26,24 @@
                     <div class="row clearfix">
                         <div class="row-title">Заголовок:</div>
                         <div class="row-elements">
-                            <?php echo $form->textField($model, 'name', array('class' => 'w-400')); ?>
+                            <?php echo $form->textField($model, 'name', array('class' => 'w-500')); ?>
                         </div>
                     </div>
 
                     <div class="row clearfix">
                         <div class="row-title">Текст:</div>
                         <div class="row-elements">
-                            <img src="/images/editor.gif" />
+                            <?php
+                                $this->widget('ext.ckeditor.CKEditorWidget', array(
+                                    'model' => $slave_model,
+                                    'attribute' => 'text',
+                                    'config' => array(
+                                        'toolbar' => 'Chat',
+                                        'width' => 410,
+                                        'height' => 100,
+                                    ),
+                                ));
+                            ?>
                         </div>
                     </div>
                 <?php endif; ?>
@@ -32,14 +52,14 @@
                     <div class="row clearfix">
                         <div class="row-title">Заголовок:</div>
                         <div class="row-elements">
-                            <input type="text" class="w-400"/>
+                            <?php echo $form->textField($model, 'name', array('class' => 'w-400')); ?>
                         </div>
                     </div>
 
                     <div class="row clearfix">
                         <div class="row-title">Ссылка на видео:</div>
                         <div class="row-elements">
-                            <input type="text" class="w-400"/>
+                            <?php echo $form->textField($model, 'link', array('class' => 'w-400')); ?>
                             <div class="row-note">
                                 <div class="tale"></div>
                                 <p><span class="mdash">&mdash;</span>Вы можете разместить видео с YouTube и RuTube</p>
@@ -62,7 +82,17 @@
                     <div class="row clearfix">
                         <div class="row-title">Комментарий к видео:</div>
                         <div class="row-elements">
-                            <img src="/images/editor.gif" />
+                            <?php
+                                $this->widget('ext.ckeditor.CKEditorWidget', array(
+                                    'model' => $slave_model,
+                                    'attribute' => 'text',
+                                    'config' => array(
+                                        'toolbar' => 'Chat',
+                                        'width' => 410,
+                                        'height' => 100,
+                                    ),
+                                ));
+                            ?>
                         </div>
                     </div>
                 <?php endif; ?>
@@ -71,18 +101,14 @@
                     <div class="row-title">Рубрика:</div>
                     <div class="row-elements">
                         <div class="select-box">
-                            <select class="chzn w-200">
-                                <option>Рубрика 1</option>
-                                <option>Рубрика 2</option>
-                                <option>Рубрика 3</option>
-                            </select>
+                            <?php echo $form->dropDownList($model, 'rubric_id', CHtml::listData($rubrics, 'id', 'name'), array('class' => 'chzn w-200')); ?>
                         </div>
                     </div>
                 </div>
 
                 <div class="row row-buttons">
                     <button class="btn btn-gray-medium"><span><span>Отменить</span></span></button>
-                    <button class="btn btn-yellow-medium"><span><span>Предпросмотр</span></span></button>
+                    <!--<button class="btn btn-yellow-medium"><span><span>Предпросмотр</span></span></button>-->
                     <button class="btn btn-green-medium"><span><span>Добавить</span></span></button>
                 </div>
 

bc67a34ba7cc61f2d80916cfc6155d29ae7bf001
ChooJoy <choojoy.work@gmail.com>
2012-03-26 18:47:45 +0400

    start

--- site/common/models/CommunityRubric.php
+++ site/common/models/CommunityRubric.php
@@ -102,24 +102,4 @@ class CommunityRubric extends CActiveRecord
 			'community_id' => 'Community',
 		);
 	}
-
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('name',$this->name,true);
-		$criteria->compare('community_id',$this->community_id,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
 }

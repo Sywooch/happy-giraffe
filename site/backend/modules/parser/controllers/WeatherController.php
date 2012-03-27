7efedeadca5e15b473301d17d5c04aa8823ed0ea
Kireev Alex <alexk984@gmail.com>
2012-03-27 12:31:16 +0400

    weather + geo fixes

---
+++ site/backend/modules/parser/controllers/WeatherController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: alexk984
+ * Date: 27.03.12
+ */
+
+class WeatherController extends BController
+{
+    private $country_id = 109;
+
+    public function actionIndex()
+    {
+
+    }
+}

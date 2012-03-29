9899de769c054114f41a3fef9b46fcb01c6647f3
Kireev Alex <alexk984@gmail.com>
2012-03-29 13:06:03 +0400

    fixes

--- site/frontend/www/themes/happy_giraffe/views/profile/data.php
+++ site/frontend/www/themes/happy_giraffe/views/profile/data.php
@@ -1,19 +1,3 @@
-<?php Yii::app()->clientScript
-    ->registerScriptFile('/javascripts/location.js')
-    ->registerScript('profile-location', '
-        UserLocation.regionUrl = "' . Yii::app()->createUrl('geo/geo/regions') . '";
-        UserLocation.cityUrl = "' . Yii::app()->createUrl('geo/geo/cities') . '";
-        UserLocation.saveUrl = "' . Yii::app()->createUrl('geo/geo/saveLocation') . '";
-        UserLocation.regionIsCityUrl = "' . Yii::app()->createUrl('geo/geo/regionIsCity') . '";
-    ')
-    ->registerScriptFile('http://api-maps.yandex.ru/1.1/index.xml?key=' . Yii::app()->params['yandex_map_key']);
-$regions = array('' => '');
-
-if ($this->user->getUserAddress()->country_id !== null) {
-    $regions = array('' => '') + CHtml::listData(GeoRegion::model()->findAll(array(
-        'order' => 'id', 'select' => 'id,name', 'condition' => 'country_id = ' . $this->user->userAddress->country_id)), 'id', 'name');
-}
-?>
 <?php
 $form = $this->beginWidget('CActiveForm', array(
     'id' => 'profile-form'
@@ -82,81 +66,6 @@ $form = $this->beginWidget('CActiveForm', array(
 
     </div>
 
-    <div class="row row-inline clearfix">
-
-        <div class="row-title">Место жительства:</div>
-        <div class="row-elements">
-            <div class="col">
-                <div class="select-box with-search">
-                    <?php echo CHtml::dropDownList('country_id', $this->user->getUserAddress()->country_id,
-                    array('' => '') + CHtml::listData(GeoCountry::model()->findAll(array('order' => 'pos')), 'id', 'name'),
-                    array(
-                        'class' => 'chzn w-200',
-                        'data-placeholder' => 'Выберите страну',
-                        'onchange' => 'UserLocation.SelectCounty($(this));'
-                    )) ?>
-                    <div class="col">
-                        <?php echo CHtml::dropDownList('region_id', $this->user->getUserAddress()->region_id, $regions,
-                        array(
-                            'class' => 'chzn w-200',
-                            'data-placeholder' => 'Выберите регион',
-                            'onchange' => 'UserLocation.RegionChanged($(this));'
-                        )); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row row-inline clearfix settlement" <?php
-        if ($this->user->getUserAddress()->region !== null
-            && $this->user->getUserAddress()->region->isCity()
-        ) echo ' style="display:none;"' ?>>
-
-        <div class="row-title">Населенный пункт:</div>
-        <div class="row-elements">
-            <?php
-            $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
-                'id' => 'city_name',
-                'name' => 'city_name',
-                'value' => ($this->user->getUserAddress()->city === null) ? '' : $this->user->getUserAddress()->city->name,
-                'source' => "js: function(request, response){
-                            $.ajax({
-                                url: '" . $this->createUrl('/geo/geo/cities') . "',
-                                dataType: 'json',
-                                data: {
-                                    term: request.term,
-                                    country_id: $('#country_id').val(),
-                                    region_id: $('#region_id').val()
-                                },
-                                success: function (data)
-                                {
-                                    response(data);
-                                }
-                            })
-                        }",
-                'options' => array(
-                    'select' => "js:function (event, ui)
-                                {
-                                    $('#city_id').val(ui.item.id);
-                                    geocoder = new YMaps.Geocoder($('#country_id option:selected').text()
-                                        + ', ' + $('#region_id option:selected').text()
-                                        + ', ' + ui.item.label);
-                                    ShowNewLoc();
-                                }
-                            ",
-                    'htmlOptions' => array(
-                        'placeholder' => 'Выберите город'
-                    )
-                ),
-            ));
-            ?>
-            <?php echo CHtml::hiddenField('city_id', $this->user->userAddress->city_id); ?>&nbsp;&nbsp;
-            <div class="text-inline"> Введите свой город, поселок, село<br>или деревню</div>
-        </div>
-    </div>
-</div>
-
-<div id="YMapsID" style="width:600px;height:400px;margin-bottom: 20px;"></div>
 
 <div class="row row-inline clearfix">
 
@@ -196,71 +105,6 @@ $form = $this->beginWidget('CActiveForm', array(
         <span><span>Сохранить<img src="/images/arrow_r.png"/></span></span>
     </button>
 </div>
-<script type="text/javascript">
-    var directionsDisplay;
-    var geocoder;
-    var map;
-    var placemark;
-    var s;
 
-    $(function () {
-        BalloonStyle();
-
-        // Создание экземпляра карты и его привязка к созданному контейнеру
-        map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
-        // Добавление элементов управления
-        map.addControl(new YMaps.TypeControl());
-        map.addControl(new YMaps.ToolBar());
-        map.addControl(new YMaps.Zoom());
-        map.addControl(new YMaps.MiniMap());
-        map.addControl(new YMaps.ScaleLine());
-        //map.addControl(new YMaps.SearchControl());
-
-        // Создание объекта геокодера
-        var user_loc = "<?php echo $this->user->userAddress->getLocationString() ?>";
-        geocoder = new YMaps.Geocoder(user_loc);
-        ShowNewLoc();
-
-        YMaps.Events.observe(geocoder, geocoder.Events.Fault, function (geocoder, errorMessage) {
-            console.log("Произошла ошибка: " + errorMessage)
-        });
-
-        $("#country_id").chosen({allow_single_deselect:true}).change(function () {
-            geocoder = new YMaps.Geocoder($("#country_id option:selected").text());
-            ShowNewLoc();
-        });
-
-        $('#region_id').chosen({allow_single_deselect:true}).change(function () {
-            //console.log($("#region_id option:selected").text());
-            geocoder = new YMaps.Geocoder($("#country_id option:selected").text() + ", " + $("#region_id option:selected").text());
-            ShowNewLoc();
-        });
-    });
-
-    function ShowNewLoc() {
-        console.log('1');
-        YMaps.Events.observe(geocoder, geocoder.Events.Load, function (geocoder) {
-            if (geocoder.length()) {
-                map.setBounds(geocoder.get(0).getBounds());
-                //console.log(geocoder.get(0));
-                map.removeOverlay(placemark);
-                placemark = new YMaps.Placemark(geocoder.get(0).getCoordPoint(), {style:s});
-                placemark.name = geocoder.request;
-                map.addOverlay(placemark);
-            }
-        });
-    }
-
-    function BalloonStyle() {
-        // Создает стиль
-        s = new YMaps.Style();
-        // Создает стиль значка метки
-        s.iconStyle = new YMaps.IconStyle();
-
-        //стиль метки
-        s.iconStyle.href = "/images/map_marker.png";
-        s.iconStyle.size = new YMaps.Point(31, 35);
-        s.iconStyle.offset = new YMaps.Point(-17, -40);
-    }
-</script>
 <?php $this->endWidget(); ?>
+</div>

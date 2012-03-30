a27fe8d0f1b8d3c60932d6bfba82e58b564cf31c
Kireev Alex <alexk984@gmail.com>
2012-03-30 17:41:01 +0400

    happy giraffe comments

--- site/frontend/widgets/commentWidget/views/_comment.php
+++ site/frontend/widgets/commentWidget/views/_comment.php
@@ -1,17 +1,21 @@
-<?php if ($data->removed == 0): ?>
-<li class="clearfix item" id="comment_<?php echo $data->id; ?>">
-    <div class="clearfix">
-        <div class="user">
-            <?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array('user' => $data->author, 'friendButton' => true)); ?>
-        </div>
-        <?php $this->render($this->type . '_comment', array('type' => $this->type, 'data' => $data, 'currentPage'=>$currentPage)); ?>
-    </div>
-    <div class="comment-action">
-
-    </div>
-</li>
+<?php if ($data->author_id == 1): ?>
+    <img src="/images/happy_giraffe_comment.jpg" alt="Веселый Жираф приветствует вас!">
 <?php else: ?>
-<div class="removed-comment content-in">
-    <?= $data->getRemoveDescription() ?>
-</div>
+<?php if ($data->removed == 0): ?>
+    <li class="clearfix item" id="comment_<?php echo $data->id; ?>">
+        <div class="clearfix">
+            <div class="user">
+                <?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array('user' => $data->author, 'friendButton' => true)); ?>
+            </div>
+            <?php $this->render($this->type . '_comment', array('type' => $this->type, 'data' => $data, 'currentPage' => $currentPage)); ?>
+        </div>
+        <div class="comment-action">
+
+        </div>
+    </li>
+    <?php else: ?>
+    <div class="removed-comment content-in">
+        <?= $data->getRemoveDescription() ?>
+    </div>
+    <?php endif; ?>
 <?php endif; ?>

339aac2d0737f21a595a9ea6599f34377b40d3d4
Kireev Alex <alexk984@gmail.com>
2012-03-31 15:11:14 +0400

    bugfixes

--- site/frontend/www/themes/happy_giraffe/views/layouts/profile.php
+++ site/frontend/www/themes/happy_giraffe/views/layouts/profile.php
@@ -10,8 +10,8 @@
                 $this->widget('zii.widgets.CMenu', array(
                 'items' => array(
                     array('label' => 'Личная информация', 'url' => array('/profile/index'), 'active' => $this->action->id == 'index'),
-                    array('label' => 'Ваша фотография', 'url' => array('/profile/photo'), 'active' => $this->action->id == 'photo'),
-                    array('label' => 'Моя семья', 'url' => array('/profile/family'), 'active' => $this->action->id == 'family'),
+//                    array('label' => 'Ваша фотография', 'url' => array('/profile/photo'), 'active' => $this->action->id == 'photo'),
+//                    array('label' => 'Моя семья', 'url' => array('/profile/family'), 'active' => $this->action->id == 'family'),
                     array('label' => 'Доступ', 'url' => array('/profile/access'), 'active' => $this->action->id == 'access'),
                     //array('label' => 'Черный список', 'url' => array('/profile/blacklist'), 'active' => $this->action->id == 'blacklist'),
                     //array('label' => 'Подписка', 'url' => array('/profile/subscription'), 'active' => $this->action->id == 'subscription'),

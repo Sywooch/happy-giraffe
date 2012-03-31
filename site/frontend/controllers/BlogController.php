9f1f28e7d89f2c0279f6fb27cfb1755f79ded857
ChooJoy <choojoy.work@gmail.com>
2012-03-31 12:58:23 +0400

    fix

--- site/frontend/controllers/BlogController.php
+++ site/frontend/controllers/BlogController.php
@@ -35,6 +35,7 @@ class BlogController extends Controller
     {
         $params = array_filter(CMap::mergeArray(
             array(
+                'user_id' => isset($this->actionParams['user_id']) ? $this->actionParams['user_id'] : null,
                 'rubric_id' => isset($this->actionParams['rubric_id']) ? $this->actionParams['rubric_id'] : null,
                 'content_type_slug' => isset($this->actionParams['content_type_slug']) ? $this->actionParams['content_type_slug'] : null,
             ),

2ecbe9a94740c030a143f459b8854e64ee9ee2d6
ChooJoy <choojoy.work@gmail.com>
2012-03-31 11:15:36 +0400

    ava

--- site/frontend/www/javascripts/user_common.js
+++ site/frontend/www/javascripts/user_common.js
@@ -43,7 +43,7 @@ $(function() {
     });
 
     $('#change_ava').delegate('a.renew', 'click', function() {
-        $('#change_ava > div.photo').trigger('click');
+        alert('123');
     });
 });
 

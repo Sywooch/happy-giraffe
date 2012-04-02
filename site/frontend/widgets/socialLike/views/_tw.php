cef7d4e76b23e3ff76c4df49fb14779c7af3b565
Eugene <lnghost@hotmail.com>
2012-04-02 15:50:41 +0400

    Fixes

--- site/frontend/widgets/socialLike/views/_tw.php
+++ site/frontend/widgets/socialLike/views/_tw.php
@@ -1,4 +1,4 @@
-<a onclick="return Social.open('tw', this.href, 'Опубликовать ссылку в Twitter', 800, 300, this);"
+<a onclick="return Social.open('tw', this.href, 'twitter', 800, 300, this);"
    rel="nofollow"
    href="http://twitter.com/intent/tweet?text=<?php echo urlencode($this->options['title'] . ' ' . $this->options['url']); ?>"
    title="Поделиться с друзьями в Twitter"

8ec8085a10eaca990a9446fe356328b8c88b23d7
ChooJoy <choojoy.work@gmail.com>
2012-04-03 20:30:57 +0400

    last

--- site/frontend/www/themes/happy_giraffe/views/layouts/community.php
+++ site/frontend/www/themes/happy_giraffe/views/layouts/community.php
@@ -139,6 +139,37 @@
             </ul>
 
         </div>
+
+        <?php if ($this->action->id == 'view'): ?>
+            <div id="yandex_ad"></div>
+            <script type="text/javascript">
+                (function(w, d, n, s, t) {
+                    w[n] = w[n] || [];
+                    w[n].push(function() {
+                        Ya.Direct.insertInto(87026, "yandex_ad", {
+                            site_charset: "utf-8",
+                            ad_format: "direct",
+                            font_size: 1,
+                            type: "vertical",
+                            limit: 1,
+                            title_font_size: 2,
+                            site_bg_color: "FFFFFF",
+                            title_color: "006699",
+                            url_color: "006699",
+                            all_color: "000000",
+                            text_color: "000000",
+                            hover_color: "6699CC"
+                        });
+                    });
+                    t = d.documentElement.firstChild;
+                    s = d.createElement("script");
+                    s.type = "text/javascript";
+                    s.src = "http://an.yandex.ru/system/context.js";
+                    s.setAttribute("async", "true");
+                    t.insertBefore(s, t.firstChild);
+                })(window, document, "yandex_context_callbacks");
+            </script>
+        <?php endif; ?>
     </div>
 </div>
 

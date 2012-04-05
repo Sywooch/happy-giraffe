4c551640bbcb962ae5bd1d4aaa570cbfb5e70dbb
ChooJoy <choojoy.work@gmail.com>
2012-04-05 18:09:39 +0400

    fix

--- site/console/commands/CommunityCommand.php
+++ site/console/commands/CommunityCommand.php
@@ -62,9 +62,9 @@ class CommunityCommand extends CConsoleCommand
 
         $criteria = new CDbCriteria;
         $criteria->compare('by_happy_giraffe', true);
-        $criteria->addInCondition('t.id', array(
-            6019,
-        ));
+        //$criteria->addInCondition('t.id', array(
+        //    6019,
+        //));
 
         $contents = CommunityContent::model()->full()->findAll($criteria);
 

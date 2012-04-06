7f42e7b99f5ad334097a65070824aaa8b1a133ac
Kireev Alex <alexk984@gmail.com>
2012-04-06 16:45:15 +0400

    home page

--- site/frontend/widgets/home/views/CommunityArticlesWidget.php
+++ site/frontend/widgets/home/views/CommunityArticlesWidget.php
@@ -4,10 +4,10 @@
  */
 ?>
 <ul>
-    <li><a href=""><img src="/images/<?=$image ?>"></a></li>
+    <li><a href="<?= Yii::app()->createUrl('/community/list', array('community_id' => $community_id))?>"><img src="/images/<?=$image ?>"></a></li>
     <?php foreach ($articles as $article): ?>
     <li><a href="<?=$article->getUrl() ?>"><?=$article->name ?></a></li>
     <?php endforeach; ?>
 </ul>
 
-<div class="all-link"><a href="">Все <?=$title ?> (<?=$count ?>)</a></div>
+<div class="all-link"><a href="<?= Yii::app()->createUrl('/community/list', array('community_id' => $community_id))?>">Все <?=$title ?> (<?=$count ?>)</a></div>

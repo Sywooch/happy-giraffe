9bf8c4e0770cfbd44c0bb09e80680d1352996281
ChooJoy <choojoy.work@gmail.com>
2012-04-08 16:42:29 +0400

    fix

--- site/console/commands/PurifyCommand.php
+++ site/console/commands/PurifyCommand.php
@@ -5,53 +5,39 @@
  */
 class PurifyCommand extends CConsoleCommand
 {
-    public function actionPurify()
+    public function actionIndex($cli = 0, $id = null, $by_happy_giraffe = 0)
     {
         Yii::import('site.frontend.extensions.ESaveRelatedBehavior');
         Yii::import('site.frontend.extensions.phpQuery.phpQuery');
-        Yii::import('site.frontend.helpers.*');
-        require_once(Yii::getPathOfAlias('site.frontend') . '/vendor/simplehtmldom_1_5/simple_html_dom.php');
 
         $criteria = new CDbCriteria;
-        $criteria->compare('by_happy_giraffe', true);
+        if ($id !== null) {
+            $criteria->compare('t.id', $id);
+        } else {
+            $criteria->compare('by_happy_giraffe', $by_happy_giraffe);
+        }
 
         $contents = CommunityContent::model()->full()->findAll($criteria);
 
         foreach ($contents as $c) {
-            echo $c->id . "\n";
-            $c->content->text = $this->_purify($c->content->text);
-            $c->content->save();
-        }
-    }
-
-    public function actionPurifyNonGiraffe()
-    {
-        Yii::import('site.frontend.extensions.ESaveRelatedBehavior');
-        Yii::import('site.frontend.extensions.phpQuery.phpQuery');
-        Yii::import('site.frontend.helpers.*');
-        require_once(Yii::getPathOfAlias('site.frontend') . '/vendor/simplehtmldom_1_5/simple_html_dom.php');
-
-        $perIteraion = 1000;
-
-        $criteria = new CDbCriteria;
-        $criteria->condition = 'by_happy_giraffe = 0 AND t.id > 12265';
-        $criteria->limit = $perIteraion;
-        $criteria->order = 't.id ASC';
-
-        while ($contents = CommunityContent::model()->full()->findAll($criteria)) {
-            foreach ($contents as $c) {
-                echo $c->id . "\n";
-                $c->content->text = $this->_purifyNonGiraffe($c->content->text);
+            $method = $c->by_happy_giraffe ? '_giraffe' : '_nonGiraffe';
+            $text = $this->$method($c->content->text);
+            if ($cli) {
+                $text = str_replace('/upload', 'http://www.happy-giraffe.ru/upload', $text);
+                echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n" . $text . "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+            } else {
+                $c->content->text = $text;
                 $c->content->save();
             }
-
-            $criteria->offset += $perIteraion;
         }
     }
 
-    private function _purify($html)
+    private function _giraffe($html)
     {
         $doc = phpQuery::newDocumentXHTML($html, $charset = 'utf-8');
+
+        //заменяем устаревшие теги
+        $this->_deprecated();
 
         //вычисляем максимальный размер шрифта
         $maxFontSize = 18;
@@ -68,53 +54,53 @@ class PurifyCommand extends CConsoleCommand
         foreach (pq('span') as $s) {
             if (preg_match('/font-size:?(\d+)px;/', pq($s)->attr('style'), $matches)) {
                 $fontSize = $matches[1];
-                if ($fontSize == $maxFontSize && mb_strlen(pq($s)->text(), 'utf-8') <= 70 && count(pq($s)->find('img')) == 0) {
+                if ($fontSize == $maxFontSize) {
                     pq($s)->replaceWith('<h2>'. pq($s)->html() . '</h2>');
                 }
             }
         }
 
-        //чистим атрибуты
-        foreach (pq('h2, h3, p, strong, b, em, u, s, li') as $e) {
-            pq($e)->removeAttr('style');
+        //заменяем длинные заголовки на p
+        $this->_extraHeaders();
+
+        //вычищаем всё из заголовков
+        foreach (pq(':header') as $h) {
+            pq($h)->html(pq($h)->text());
+            pq(pq($h)->parents(':last'))->replaceWith(pq($h));
+        }
+
+        //убираем лишние теги
+        while (count(pq('span')) > 0) {
+            foreach (pq('span') as $s) {
+                pq($s)->replaceWith((string) pq($s)->html());
+            }
+        }
+
+        //чистим стили
+        $this->_clearStyle();
+
+        //удаляем пустые параграфы
+        $this->_emptyParagraphs();
+
+        //правим картинки
+        $this->_images();
+
+        //выделяем лид
+        foreach (pq(':header:first')->prevAll('p') as $e) {
+            pq($e)->html('<em>' . pq($e)->text() . '</em>');
         }
 
         //чистим маркированные списки
-        foreach (pq('li') as $l) {
-            pq($l)->text(preg_replace('/^\s*-\s?/', '', pq($l)->text()));
-        }
-
-        //вычищаем всё из заголовков - РАБОТАЕТ ЧЕРЕЗ ЖОПУ
-        foreach (pq('h2, h3') as $h) {
-            pq($h)->html(pq($h)->text());
-            if (count(pq($h)->parents()) > 0) {
-                pq($h)->parent(':first')->replaceWith(pq($h));
-            }
-        }
-
-        //убираем спаны
-        while (count(pq('span')) > 0) {
-            foreach (pq('span') as $s) {
-                pq($s)->replaceWith(pq($s)->html());
-            }
-        }
-
-        //убираем фонты
-        foreach (pq('font') as $s) {
-            pq($s)->replaceWith(pq($s)->html());
-        }
+        $this->_lists();
 
         //убираем длинные стронги
-        foreach (pq('strong, b') as $s) {
-            if (mb_strlen(pq($s)->text(), 'utf-8') > 25 || mb_strlen(trim(pq($s)->parent('p')->text()), 'utf-8') == mb_strlen(trim(pq($s)->text()), 'utf-8')) {
-                if (mb_strlen(pq($s)->html(), 'utf-8') > 0) {
-                    pq($s)->replaceWith(pq($s)->html());
-                } else {
-                    pq($s)->remove();
-                }
+        foreach (pq('strong') as $s) {
+            if (mb_strlen($this->_htmltrim(pq($s)->parents('p')->text()), 'utf-8') == mb_strlen($this->_htmltrim(pq($s)->text()), 'utf-8')) {
+                pq($s)->replaceWith((string) pq($s)->html());
             }
         }
 
+        //убираем курсив с выделенного жирным
         foreach (pq('em') as $e) {
             if (count(pq($e)->parent('strong')) > 0 || count(pq($e)->children('strong')) > 0) {
 
@@ -125,18 +111,17 @@ class PurifyCommand extends CConsoleCommand
         return $doc;
     }
 
-    private function _purifyNonGiraffe($html)
+    private function _nonGiraffe($html)
     {
         $doc = phpQuery::newDocumentXHTML($html, $charset = 'utf-8');
 
-        $allowedTags = 'h2, h3, p, a, ul, ol, li, img, div';
+        $allowedTags = ':header, p, a, ul, ol, li, img, div';
 
-        //убираем лишние заголовки
-        foreach (pq('h2, h3') as $e) {
-            if (mb_strlen(pq($e)->text(), 'utf-8') > 70) {
-                pq($e)->replaceWith('<p>' . pq($e)->html() . '</p>');
-            }
-        }
+        //заменяем устаревшие теги
+        $this->_deprecated();
+
+        //заменяем длинные заголовки на p
+        $this->_extraHeaders();
 
         //убираем лишние теги
         while (count(pq(':not(' . $allowedTags .')')) > 0) {
@@ -145,11 +130,85 @@ class PurifyCommand extends CConsoleCommand
             }
         }
 
-        //чистим атрибуты
+        //чистим стили
+        $this->_clearStyle();
+
+        //удаляем пустые параграфы
+        $this->_emptyParagraphs();
+
+        //правим картинки
+        $this->_images();
+
+        //чистим маркированные списки
+        $this->_lists();
+
+        return $doc;
+    }
+
+    private function _htmltrim($string)
+    {
+        $pattern = '(?:[ \t\n\r\x0B\x00\x{A0}\x{AD}\x{2000}-\x{200F}\x{201F}\x{202F}\x{3000}\x{FEFF}]|&nbsp;|<br\s*\/?>)+';
+        return preg_replace('/^' . $pattern . '|' . $pattern . '$/u', '', $string);
+    }
+
+    private function _images()
+    {
+        foreach (pq('img') as $e) {
+            //убираем margin
+            $style = pq($e)->attr('style');
+            preg_match('/float:\s*(\w+)/', $style, $matches);
+            $float = $matches[1];
+            $style = preg_replace('/(?:margin|float):[^;]*;\s*/', '', $style);
+            pq($e)->attr('class', 'content-img-' . $float);
+            pq($e)->attr('style', $style);
+            if (pq($e)->attr('style') == '') {
+                pq($e)->removeAttr('style');
+            }
+
+            //выносим
+            pq($e)->insertBefore(pq($e)->parents(':last'));
+        }
+    }
+
+    private function _deprecated()
+    {
+        foreach (pq('b') as $e) {
+            $e->replaceWith('<strong>' . $e->html() . '</strong>');
+        }
+        foreach (pq('i') as $e) {
+            $e->replaceWith('<em>' . $e->html() . '</em>');
+        }
+    }
+
+    private function _extraHeaders()
+    {
+        foreach (pq(':header') as $e) {
+            if (mb_strlen(pq($e)->text(), 'utf-8') > 70) {
+                pq($e)->replaceWith('<p>' . pq($e)->html() . '</p>');
+            }
+        }
+    }
+
+    private function _emptyParagraphs()
+    {
+        foreach (pq('p') as $e) {
+            if ($this->_htmltrim(pq($e)->text()) == '') {
+                pq($e)->remove();
+            }
+        }
+    }
+
+    private function _clearStyle()
+    {
         foreach (pq(':not(img, div)') as $e) {
             pq($e)->removeAttr('style');
         }
+    }
 
-        return $doc;
+    private function _lists()
+    {
+        foreach (pq('li') as $l) {
+            pq($l)->text(preg_replace('/^\s*-\s?/', '', pq($l)->text()));
+        }
     }
 }

0d1d816ccdc1622837c09f07608a7b8276a31425
Kireev Alex <alexk984@gmail.com>
2012-04-09 12:52:24 +0400

    wisiwyg fix

--- site/common/models/AlbumPhoto.php
+++ site/common/models/AlbumPhoto.php
@@ -262,7 +262,11 @@ class AlbumPhoto extends CActiveRecord
                 mkdir($model_dir);
             Yii::import('ext.image.Image');
             $image = new Image($this->originalPath);
-            if ($master && $master == Image::WIDTH && $image->width < $width)
+
+            if ($image->width <= $width && $image->height <= $height){
+
+            }
+            elseif ($master && $master == Image::WIDTH && $image->width < $width)
                 $image->resize($image->width, $height, Image::WIDTH);
             elseif ($master && $master == Image::HEIGHT && $image->height < $height)
                 $image->resize($width, $image->height, Image::HEIGHT);

8d9e802a545c23e50a746566da6e44e7b9f0ce67
Kireev Alex <alexk984@gmail.com>
2012-04-11 11:40:55 +0400

    fixes

--- site/frontend/www/themes/happy_giraffe/views/layouts/new2.php
+++ site/frontend/www/themes/happy_giraffe/views/layouts/new2.php
@@ -1,7 +1,7 @@
 <?php $this->beginContent('//layouts/main'); ?>
 <?php Yii::app()->clientScript->registerCssFile('/stylesheets/baby.css'); ?>
     <div class="left-inner">
-
+        <a href="/contest/1/"><img src="/images/banner_03.png"></a>
     </div>
 
     <div class="right-inner">

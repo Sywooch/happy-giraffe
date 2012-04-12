278bf3936ab09e9e11697736c0d16c4e4ad85b8f
Kireev Alex <alexk984@gmail.com>
2012-04-12 21:11:57 +0400

    like buttons

--- site/frontend/www/themes/happy_giraffe/views/morning/view.php
+++ site/frontend/www/themes/happy_giraffe/views/morning/view.php
@@ -85,6 +85,59 @@
 
 </div>
 
+<div>
+
+    <?php
+    Yii::app()->clientScript
+        ->registerScriptFile('http://vk.com/js/api/share.js?11')
+        ->registerScriptFile('http://stg.odnoklassniki.ru/share/odkl_share.js')
+        ->registerCssFile('http://stg.odnoklassniki.ru/share/odkl_share.css')
+    ;
+
+    ?>
+
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+    <script type="text/javascript">
+        window.___gcfg = {lang: 'ru'};
+
+        (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
+    </script>
+
+    <table>
+        <tr>
+            <td>
+                <script type="text/javascript">document.write(VK.Share.button(false,{type: "round", text: "Сохранить"}));</script>
+            </td>
+            <td>
+                <div class="fb-like" data-send="false" data-layout="button_count" data-width="150" data-show-faces="false" data-action="recommend"></div>
+            </td>
+            <td>
+                <a class="odkl-klass-stat" href="<?php the_permalink() ?>" onclick="ODKL.Share(this);return false;" ><span>0</span></a>
+            </td>
+            <td>
+                <a href="https://twitter.com/share" class="twitter-share-button" data-lang="ru">Твитнуть</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </td>
+            <td>
+                <g:plusone annotation="inline" width="120"></g:plusone>
+            </td>
+        </tr>
+    </table>
+</div>
+
 <?php $this->widget('application.widgets.commentWidget.CommentWidget', array(
     'model' => $article,
 )); ?>

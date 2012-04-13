ed3fecf76b0af3777ba2fb82b48807a83cf3aeca
Kireev Alex <alexk984@gmail.com>
2012-04-13 15:20:20 +0400

    favourites fix

--- site/frontend/widgets/favoritesWidget/assets/favourites.js
+++ site/frontend/widgets/favoritesWidget/assets/favourites.js
@@ -4,15 +4,13 @@
  * Time: 13:23
  */
 var Favourites = {
-    toggle:function (el, num) {
+    toggle:function (el, num, param) {
         var entity = $(el).parent('div').find('input.entity').val();
         var entity_id = $(el).parent('div').find('input.entity_id').val();
-
-        $.post('/ajax/toggleFavourites/', {entity:entity, entity_id:entity_id, num:num}, function (response) {
+        $.post('/ajax/toggleFavourites/', {entity:entity, entity_id:entity_id, num:num, param:param}, function (response) {
             if (response.status) {
                 $(el).toggleClass('active');
                 if ($(el).attr('title') == '') $(el).attr('title', '');
-
             }
         }, 'json');
     }

7778daa22a57a5f58b39c502662747de81a8d521
Dmitriy Farafontov <farikd@gmail.com>
2012-04-17 18:28:19 +0300

    service repear/wallpapers

---
+++ site/frontend/modules/repair/controllers/WallpapersController.php
@@ -0,0 +1,70 @@
+<?php
+
+class WallpapersController extends Controller
+{
+    public function actionIndex()
+    {
+        $basePath = Yii::getPathOfAlias('repair') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'wallpapers' . DIRECTORY_SEPARATOR . 'assets';
+        $baseUrl = Yii::app()->getAssetManager()->publish($basePath, false, 1, YII_DEBUG);
+        Yii::app()->clientScript->registerScriptFile($baseUrl . '/script.js', CClientScript::POS_HEAD);
+        Yii::app()->clientScript->registerCssFile($baseUrl . '/style.css', 'all');
+        $session = new CHttpSession;
+        $session->open();
+        if (isset($session['wallpapersCalcAreas']))
+            unset($session['wallpapersCalcAreas']);
+        $this->pageTitle = 'Расчет обоев';
+        $this->render('index', array('model' => new WallpapersCalcForm(), 'emptyArea' => new WallpapersAreaForm()));
+    }
+
+    public function actionCalculate()
+    {
+        if (isset($_POST['WallpapersCalcForm'])) {
+            $model = new WallpapersCalcForm;
+            $model->attributes = $_POST['WallpapersCalcForm'];
+            $this->performAjaxValidation($model);
+            CActiveForm::validate($model);
+            $this->renderPartial('result', array('result' => $model->calculate()));
+        }
+    }
+
+    public function performAjaxValidation($model)
+    {
+        if (isset($_POST['ajax']) && $_POST['ajax'] == 'wallpapers-calculate-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
+    public function actionAddemptyarea()
+    {
+        if (isset($_POST['WallpapersAreaForm'])) {
+            $model = new WallpapersAreaForm();
+            $model->attributes = $_POST['WallpapersAreaForm'];
+            if (isset($_POST['ajax']) && $_POST['ajax'] == 'empty-area-form') {
+                echo CActiveForm::validate($model);
+                Yii::app()->end();
+            }
+            CActiveForm::validate($model);
+            $session = new CHttpSession;
+            $session->open();
+            $areas = $session['wallpapersCalcAreas'];
+            $areas[] = array('title' => $model->title, 'height' => $model->height, 'width' => $model->width);
+            $session['wallpapersCalcAreas'] = $areas;
+            print_r($session['wallpapersCalc']);
+            $this->renderPartial('emptyarea', array('areas' => $areas));
+            print_r(Yii::app()->session['wallpapersCalc']);
+        }
+    }
+
+    public
+    function actionRemovearea($id)
+    {
+        $session = new CHttpSession;
+        $session->open();
+        $areas = $session['wallpapersCalcAreas'];
+        if (isset($areas[$id]))
+            unset($areas[$id]);
+        $session['wallpapersCalcAreas'] = $areas;
+        $this->renderPartial('emptyarea', array('areas' => $areas));
+    }
+}

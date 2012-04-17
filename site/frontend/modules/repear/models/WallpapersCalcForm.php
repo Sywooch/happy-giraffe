1adafd9d4deaaff2bcdec68abf05bec5b69a2555
Dmitriy Farafontov <farikd@gmail.com>
2012-04-17 16:23:53 +0300

    service repear/wallpapers

--- site/frontend/modules/repear/models/WallpapersCalcForm.php
+++ site/frontend/modules/repear/models/WallpapersCalcForm.php
@@ -12,14 +12,16 @@ class WallpapersCalcForm extends CFormModel
     public function rules()
     {
         return array(
-            //array('room_length, room_width, room_height', 'normalizeLength'),
-            //array('room_length, room_width, room_height', 'filter', 'filter'=> 'normalizeLength'),
             array('room_length', 'required', 'message' => 'Укажите длину помещения'),
             array('room_width', 'required', 'message' => 'Укажите ширину помещения'),
             array('room_height', 'required', 'message' => 'Укажите высоту помещения'),
             array('wp_width', 'required', 'message' => 'Укажите ширину обоев'),
             array('wp_length', 'required', 'message' => 'Укажите длину рулона'),
-            array('repeat', 'required', 'message' => 'Укажите раппорт')
+            //array('repeat', 'required', 'message' => 'Укажите раппорт'),
+            //array('room_length, room_width, room_height, wp_width, wp_length, repeat', 'filter', 'filter'=>'trim'),
+            array('room_length, room_width, room_height, wp_width, wp_length, repeat', 'normalizeLength'),
+            array('room_length, room_width, room_height, wp_width, wp_length, repeat', 'numerical', 'message' => 'Введите число')
+
         );
     }
 
@@ -28,16 +30,28 @@ class WallpapersCalcForm extends CFormModel
         return array(
             'room_length' => 'Длина помещения',
             'room_width' => 'Ширина помещения',
-            'room_height'=>'Высота помещения',
-            'wp_width'=>'Ширина обоев',
-            'wp_length'=>'Длина рулона',
-            'repeat'=>'Раппорт'
+            'room_height' => 'Высота помещения',
+            'wp_width' => 'Ширина обоев',
+            'wp_length' => 'Длина рулона',
+            'repeat' => 'Раппорт'
         );
     }
 
     public function normalizeLength($attribute, $params)
     {
-        $this->$attribute = str_replace(',', '.', $this->$attribute);
-        //$this->$attribute = preg_replace('#^[0-9\.]+$#', '', $this->$attribute);
+        $this->$attribute = trim(str_replace(',', '.', $this->$attribute));
+        $this->$attribute = preg_replace('#[^0-9\.]+#', '', $this->$attribute);
+    }
+
+    public function normalizeLengths()
+    {
+        foreach ($this->rules() as $key => $rule) {
+            if ($rule[1] == "normalizeLength") {
+                $attributes = preg_split('/[\s,]+/', $rule[0], 0, PREG_SPLIT_NO_EMPTY);
+                foreach ($attributes as $attr)
+                    $this->normalizeLength($attr, array());
+            }
+        }
+
     }
 }

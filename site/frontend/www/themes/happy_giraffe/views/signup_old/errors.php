38749fcc0463c8dcb5e113a88af52806e98e29ce
Eugene <lnghost@hotmail.com>
2012-04-18 11:13:17 +0400

    Refactor step 2

--- site/frontend/www/themes/happy_giraffe/views/signup_old/errors.php
+++
@@ -1,8 +0,0 @@
-<div class="form-error-summary">
-	<big>Вы допустили ошибки при заполении формы:</big>
-	<ul>
-		<? foreach ($errors as $e): ?>
-			<li><?php echo $e; ?></li>
-		<? endforeach; ?>
-	</ul>
-</div>

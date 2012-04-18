174a6ee1f2dfee3aebc4b27b32136818c3eae71e
Kireev Alex <alexk984@gmail.com>
2012-04-18 18:45:24 +0400

    Merge branch 'develop' of github.com:beknown/happy-giraffe into develop
    Conflicts:
    	site/frontend/www/themes/happy_giraffe/views/blog/form.php

--- site/common/migrations/m120417_102959_rename_community_columns.php
+++ site/common/migrations/m120417_102959_rename_community_columns.php
@@ -5,12 +5,12 @@ class m120417_102959_rename_community_columns extends CDbMigration
     private $_table = 'community__travels';
     public function up()
     {
-//        $this->renameColumn('community__communities', 'name', 'title');
-//        $this->renameColumn('community__contents', 'name', 'title');
-//        $this->renameColumn('community__content_types', 'name', 'title');
-//        $this->renameColumn('community__content_types', 'name_plural', 'title_plural');
-//        $this->renameColumn('community__content_types', 'name_accusative', 'title_accusative');
-//        $this->renameColumn('community__rubrics', 'name', 'title');
+        $this->renameColumn('community__communities', 'name', 'title');
+        $this->renameColumn('community__contents', 'name', 'title');
+        $this->renameColumn('community__content_types', 'name', 'title');
+        $this->renameColumn('community__content_types', 'name_plural', 'title_plural');
+        $this->renameColumn('community__content_types', 'name_accusative', 'title_accusative');
+        $this->renameColumn('community__rubrics', 'name', 'title');
 
         $this->addForeignKey('fk_'.$this->_table.'_content', $this->_table, 'content_id', 'community__contents', 'id','CASCADE',"CASCADE");
         $this->_table = 'community__travel_images';

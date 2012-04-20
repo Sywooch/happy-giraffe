b4751d24c1f2d2bc22b059fb6a60d871e171fef4
Dmitriy Farafontov <farikd@gmail.com>
2012-04-20 21:29:00 +0300

    service repair/suspendedCeiling

--- site/frontend/modules/repair/controllers/FlooringController.php
+++ site/frontend/modules/repair/controllers/FlooringController.php
@@ -3,7 +3,6 @@
 class FlooringController extends Controller
 {
 
-
     public function actionIndex()
     {
         $this->pageTitle = 'Расчет напольного покрытия';
@@ -11,7 +10,6 @@ class FlooringController extends Controller
         $baseUrl = Yii::app()->getAssetManager()->publish($basePath, false, 1, YII_DEBUG);
         Yii::app()->clientScript->registerScriptFile($baseUrl . '/script.js', CClientScript::POS_HEAD);
         Yii::app()->clientScript->registerCssFile($baseUrl . '/style.css', 'all');
-        //Yii::app()->user->setState('repairTileAreas', array());
         $this->render('index', array('FlooringModel' => new FlooringForm()));
     }
 
@@ -27,31 +25,5 @@ class FlooringController extends Controller
             }
             $this->renderPartial('result', array('result' => $model->calculate()));
         }
-    }
-
-    public function actionAreaAdd()
-    {
-        if (isset($_POST['TileAreaForm'])) {
-            $model = new TileAreaForm();
-            $model->attributes = $_POST['TileAreaForm'];
-            if (isset($_POST['ajax']) && $_POST['ajax'] == 'area-form') {
-                echo CActiveForm::validate($model);
-                Yii::app()->end();
-            }
-            $model->validate();
-            $areas = Yii::app()->user->getState('repairTileAreas');
-            $areas[] = array('title' => $model->title, 'height' => $model->height, 'width' => $model->width);
-            Yii::app()->user->setState('repairTileAreas', $areas);
-            $this->renderPartial('emptyarea', array('areas' => $areas));
-        }
-    }
-
-    public function actionAreaDelete($id)
-    {
-        $areas = Yii::app()->user->getState('repairTileAreas');
-        if (isset($areas[$id]))
-            unset($areas[$id]);
-        Yii::app()->user->setState('repairTileAreas', $areas);
-        $this->renderPartial('emptyarea', array('areas' => $areas));
     }
 }

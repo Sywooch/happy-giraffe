890d91a2bbf1168ab98d43578fdc2c0e846d9d63
Dmitriy Farafontov <farikd@gmail.com>
2012-04-20 20:25:16 +0300

    service repair/flooring

--- site/frontend/modules/repair/models/TileForm.php
+++ site/frontend/modules/repair/models/TileForm.php
@@ -43,36 +43,11 @@ class TileForm extends CFormModel
         $tiles = ((ceil($this->roomLength / $this->tileLength) * ceil($this->roomHeight / $this->tileWidth)) * 2) +
             ((ceil($this->roomWidth / $this->tileLength) * ceil($this->roomHeight / $this->tileWidth)) * 2);
         $areas = Yii::app()->user->getState('repairTileAreas');
-        if(count($areas)){
-            foreach($areas as $area){
-                $tiles-=floor($area['height'] / $this->tileLength) * floor($area['width'] / $this->tileWidth);
+        if (count($areas)) {
+            foreach ($areas as $area) {
+                $tiles -= floor($area['height'] / $this->tileLength) * floor($area['width'] / $this->tileWidth);
             }
         }
         return $tiles;
-    }
-
-    public function calculate_old()
-    {
-
-        $areas = Yii::app()->user->getState('repairTileAreas');
-        $perimeter = ($this->room_length + $this->room_width) * 2;
-        $lines = ceil($perimeter / $this->wp_width);
-        if ($this->repeat) {
-            $tiles = (ceil($this->room_height / $this->repeat)) * $lines;
-            if (count($areas)) {
-                foreach ($areas as $area) {
-                    $tiles -= (floor($area['width'] / $this->wp_width) * floor($area['height'] / $this->repeat));
-                }
-            }
-            return ceil($tiles / (floor($this->wp_length / $this->repeat)));
-        } else {
-            $length = $lines * ($this->room_height + 0.1);
-            if (count($areas)) {
-                foreach ($areas as $area) {
-                    $length -= (floor($area['width'] / $this->wp_width) * $area['height']);
-                }
-            }
-            return ceil($length / $this->wp_length);
-        }
     }
 }

d39bca1cb1f69e4e0b0b6a76782510b5114fee5d
Dmitriy Farafontov <farikd@gmail.com>
2012-04-25 13:38:41 +0300

    services

--- site/frontend/modules/repair/controllers/SuspendedCeilingController.php
+++ site/frontend/modules/repair/controllers/SuspendedCeilingController.php
@@ -11,7 +11,6 @@ class SuspendedCeilingController extends Controller
         $basePath = Yii::getPathOfAlias('repair') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'suspendedCeiling' . DIRECTORY_SEPARATOR . 'assets';
         $baseUrl = Yii::app()->getAssetManager()->publish($basePath, false, 1, YII_DEBUG);
         Yii::app()->clientScript->registerScriptFile($baseUrl . '/script.js', CClientScript::POS_HEAD);
-        //Yii::app()->clientScript->registerCssFile($baseUrl . '/style.css', 'all');
         $this->render('index', array('SuspendedCeilingModel' => new SuspendedCeilingForm()));
     }
 

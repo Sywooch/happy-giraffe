01e04ad3d2abe1a1cf83e13b444af41fdb5d2041
Dmitriy Farafontov <farikd@gmail.com>
2012-04-27 08:52:43 +0300

    services

--- site/frontend/modules/repair/views/tile/assets/script.js
+++ site/frontend/modules/repair/views/tile/assets/script.js
@@ -4,13 +4,35 @@ var Tile = {
             url:$("#tile-calculate-form").attr('action'),
             data:$("#tile-calculate-form").serialize(),
             type:"POST",
+            dataType:'json',
             success:function (data) {
-                $("#result").fadeOut(100, function () {
-                    $("#result").html(data);
-                    $("#result").fadeIn(100);
+                $("div#repair-bathroom-tile div.recommendation").fadeOut(100, function () {
+                    $("div#repair-bathroom-tile div.recommendation div.left span").html(data.qty + ' ' + data.noun);
+                    $("div#repair-bathroom-tile div.recommendation").fadeIn(100);
                 });
             }
         });
         return false;
+    },
+    CalcSq:function(){
+        $('#tile-calculate-form div.form-in div.row div.img').each(function(){
+            a = $(this).next().find('input').val();
+            b = $(this).next().next().find('input').val();
+            sq = parseFloat(a) * parseFloat(b);
+            if (sq > 0) {
+                $(this).find('div.val').text(sq.toFixed(1));
+            } else {
+                $(this).find('div.val').text('');
+            }
+            console.log(a);
+            console.log(b);
+        })
     }
 }
+
+
+$(function(){
+    $('#tile-calculate-form .row input[type="text"]').bind('change', function(){
+        Tile.CalcSq()
+    });
+})

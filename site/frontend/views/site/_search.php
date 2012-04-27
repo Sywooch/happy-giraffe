035949a8931d56be9b659b2ef50433078efa05ee
ChooJoy <choojoy.work@gmail.com>
2012-04-27 17:37:31 +0400

    search fix

--- site/frontend/views/site/_search.php
+++ site/frontend/views/site/_search.php
@@ -4,7 +4,7 @@ $criteria->params[':id'] = $data->id;
 $c = CommunityContent::model()->full()->find($criteria);
 if(!$c)
     return;
-$name = Yii::app()->search->buildExcerpts(array($c->name), $search_index, $search_text);
+$name = Yii::app()->search->buildExcerpts(array($c->title), $search_index, $search_text);
 $c->name = $name[0];
 $text = Yii::app()->search->buildExcerpts(array($c->preview), $search_index, $search_text);
 $c->preview = $text[0];

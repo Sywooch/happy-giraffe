6f705c451d6893697640a143940e7e29d370f02b
Dmitriy Farafontov <farikd@gmail.com>
2012-04-27 12:31:35 +0300

    services

--- site/frontend/modules/repair/models/PaintForm.php
+++ site/frontend/modules/repair/models/PaintForm.php
@@ -5,6 +5,8 @@ class PaintForm extends CFormModel
     public $roomLength;
     public $roomWidth;
     public $roomHeight;
+    public $paintType;
+    public $surface;
 
     public $paints = array(1 => 'Водная краска', 2 => 'Эмаль', 3 => 'Силиконовая краска', 4 => 'Пластичная краска', 5 => 'Акриловая краска', 6 => 'Латексная, водонепроницаемая краска', 7 => 'Краска эластичная, противогрибковая', 8 => 'Пластиковая краска');
     public $paintsK = array(1 => 0.15, 2 => 0.12, 3 => 0.083, 4 => 0.083, 5 => 0.083, 6 => 0.1, 7 => 0.1, 8 => 0.125);
@@ -12,9 +14,11 @@ class PaintForm extends CFormModel
     public function rules()
     {
         return array(
+            array('surface', 'required'),
             array('roomLength', 'required', 'message' => 'Укажите длину помещения'),
             array('roomWidth', 'required', 'message' => 'Укажите ширину помещения'),
-            array('roomHeight', 'required', 'message' => 'Укажите высоту помещения'),
+            //array('roomHeight', 'required', 'message' => 'Укажите высоту помещения'),
+            array('roomHeight', 'validateRoomHeight'),
             array('paintType', 'required', 'message' => 'Укажите тип краски'),
 
             array('roomLength, roomWidth, roomHeight', 'normalizeLength'),
@@ -29,7 +33,8 @@ class PaintForm extends CFormModel
             'roomLength' => 'Длина помещения',
             'roomWidth' => 'Ширина помещения',
             'roomHeight' => 'Высота помещения',
-            'paintType' => 'Чем красим?'
+            'paintType' => 'Чем красим?',
+            'wall' => 'окрашиваемая поверхность'
         );
     }
 
@@ -39,7 +44,40 @@ class PaintForm extends CFormModel
         $this->$attribute = preg_replace('#[^0-9\.]+#', '', $this->$attribute);
     }
 
+    public function validateRoomHeight($attribute, $params)
+    {
+        if ($this->surface == "Стены") {
+
+            if (!$this->$attribute) {
+                $this->addError($attribute, 'Укажите высоту помещения');
+                return;
+            }
+            $this->normalizeLength($attribute, $params);
+            if (!preg_match('#^[0-9\.]+$#', $this->$attribute)) {
+                $this->addError($attribute, 'Введите число');
+            }
+        }
+    }
+
     public function calculate()
+    {
+        $sq = ($this->surface == "Стены") ? ($this->roomHeight * (($this->roomLength + $this->roomWidth) * 2)) : ($this->roomLength * $this->roomWidth);
+        $areas = Yii::app()->user->getState('wallpapersCalcAreas');
+        if ($this->surface == "Стены") {
+            $areas = Yii::app()->user->getState('emptyAreas');
+            if (count($areas)) {
+                foreach ($areas as $area) {
+                    $sq -= $area['height'] * $area['width'] * $area['qty'];
+                }
+            }
+        }
+        $result['volume'] = ceil($sq * $this->paintsK[$this->paintType]);
+        $result['noun'] = HDate::GenerateNoun(array('литр', 'литра', 'литров'),$result['volume']);
+
+        return $result;
+    }
+
+    public function calculate_old()
     {
 
         $areas = Yii::app()->user->getState('wallpapersCalcAreas');

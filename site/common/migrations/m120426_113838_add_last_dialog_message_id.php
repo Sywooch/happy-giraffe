8bbab56da18e2a81f394cb705411a9f1c978921d
Kireev Alex <alexk984@gmail.com>
2012-04-27 13:24:49 +0400

    migration fix

--- site/common/migrations/m120426_113838_add_last_dialog_message_id.php
+++ site/common/migrations/m120426_113838_add_last_dialog_message_id.php
@@ -6,24 +6,24 @@ class m120426_113838_add_last_dialog_message_id extends CDbMigration
 
     public function up()
     {
-        $this->addColumn($this->_table, 'last_message_id', 'int(10) UNSIGNED');
-
-        $dialog_ids = Yii::app()->db->createCommand('SELECT id FROM im__dialogs')->queryColumn();
-        foreach ($dialog_ids as $dialog_id) {
-            $last_message_id = Yii::app()->db->createCommand()
-                ->select('id')
-                ->from('im__messages')
-                ->where('dialog_id=:dialog_id', array(':dialog_id' => $dialog_id))
-                ->limit(1)
-                ->order('id desc')
-                ->queryScalar();
-
-            if ($last_message_id != null)
-                $this->update($this->_table, array('last_message_id' => $last_message_id), 'id='.$dialog_id);
-        }
-
-        $this->addForeignKey('fk_' . $this->_table . '_message', $this->_table, 'last_message_id', 'im__messages', 'id', 'CASCADE', "CASCADE");
-//        $this->execute('');
+//        $this->addColumn($this->_table, 'last_message_id', 'int(10) UNSIGNED');
+//
+//        $dialog_ids = Yii::app()->db->createCommand('SELECT id FROM im__dialogs')->queryColumn();
+//        foreach ($dialog_ids as $dialog_id) {
+//            $last_message_id = Yii::app()->db->createCommand()
+//                ->select('id')
+//                ->from('im__messages')
+//                ->where('dialog_id=:dialog_id', array(':dialog_id' => $dialog_id))
+//                ->limit(1)
+//                ->order('id desc')
+//                ->queryScalar();
+//
+//            if ($last_message_id != null)
+//                $this->update($this->_table, array('last_message_id' => $last_message_id), 'id='.$dialog_id);
+//        }
+//
+//        $this->addForeignKey('fk_' . $this->_table . '_message', $this->_table, 'last_message_id', 'im__messages', 'id', 'CASCADE', "CASCADE");
+        $this->execute('delete from im__deleted_messages');
         $this->execute('ALTER TABLE  `im__deleted_messages` ADD PRIMARY KEY (  `message_id` ,  `user_id` ) ;');
     }
 

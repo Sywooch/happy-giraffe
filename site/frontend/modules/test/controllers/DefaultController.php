c118201bc5a389890a77d25b1390032ff9536ab3
Dmitriy Farafontov <farikd@gmail.com>
2012-04-27 21:10:59 +0300

    test pregnancy code

--- site/frontend/modules/test/controllers/DefaultController.php
+++ site/frontend/modules/test/controllers/DefaultController.php
@@ -43,6 +43,22 @@ class DefaultController extends HController
         ));
     }
 
+    public function actionView2($slug)
+    {
+
+        $basePath = Yii::getPathOfAlias('test') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'assets';
+        $baseUrl = Yii::app()->getAssetManager()->publish($basePath, false, 1, YII_DEBUG);
+        //echo $baseUrl;
+        Yii::app()->clientScript->registerScriptFile($baseUrl . '/points.js', CClientScript::POS_HEAD);
+
+        $test = $this->LoadModelPoint($slug);
+        $this->pageTitle = $test->title;
+
+        $this->render('points', array(
+            'test' => $test
+        ));
+    }
+
     /**
      * @param $slug
      * @return Test
@@ -54,6 +70,19 @@ class DefaultController extends HController
             'testQuestions' => array('order' => 'testQuestions.number'),
             'testQuestions.testQuestionAnswers' => array('order' => 'testQuestionAnswers.number'),
             'testResults' => array('order' => 'testResults.number'),
+            'questionsCount'
+        ))->findByAttributes(array('slug' => $slug));
+        if ($model === null)
+            throw new CHttpException(404, 'Такого теста не существует.');
+        return $model;
+    }
+
+    public function LoadModelPoint($slug)
+    {
+        $model = Test::model()->with(array(
+            'testQuestions' => array('order' => 'testQuestions.number'),
+            'testQuestions.testQuestionAnswers' => array('order' => 'testQuestionAnswers.number'),
+            'testResults' => array('order' => 'testResults.points DESC'),
             'questionsCount'
         ))->findByAttributes(array('slug' => $slug));
         if ($model === null)

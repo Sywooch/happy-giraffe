021c3a9c87d22f2ddc0b8cb2d3640d660c019e15
Kireev Alex <alexk984@gmail.com>
2012-05-03 11:16:26 +0400

    stat info

--- site/backend/controllers/SiteController.php
+++ site/backend/controllers/SiteController.php
@@ -89,12 +89,6 @@ class SiteController extends BController
     public function actionStat(){
         Yii::import('site.frontend.modules.recipeBook.models.*');
 
-        echo 'Статей: '.CommunityContent::model()->count('type_id = 1 AND removed = 0').'<br>';
-        echo 'Видео: '.CommunityContent::model()->count('type_id = 2 AND removed = 0').'<br>';
-        echo 'Утро с Веселым Жирафом: '.CommunityContent::model()->count('type_id = 4 AND removed = 0').'<br>';
-        echo 'Путешествий: '.CommunityContent::model()->count('type_id = 3 AND removed = 0').'<br>';
-        echo 'Имен: '.Name::model()->count().'<br>';
-        echo 'Рецептов: '.RecipeBookRecipe::model()->count().'<br>';
-        echo 'Болезней: '.RecipeBookDisease::model()->count().'<br>';
+        $this->render('stat');
     }
 }

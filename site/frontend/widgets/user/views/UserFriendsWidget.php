811e9820dd3f5caf2dacb2a8135ba9106989741e
ChooJoy <choojoy.work@gmail.com>
2012-05-16 18:46:47 +0400

    live

--- site/frontend/widgets/user/views/UserFriendsWidget.php
+++ site/frontend/widgets/user/views/UserFriendsWidget.php
@@ -1,3 +1,7 @@
+<?php
+    Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/javascripts/wantToChat.js');
+?>
+
 <div class="user-friends clearfix">
 
     <div class="box-title">Друзья <?php echo CHtml::link('Все друзья (' . $user->getFriendsCount() . ')', array('user/friends', 'user_id' => $user->id)); ?></div>
@@ -7,5 +11,7 @@
             <li><?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array('user' => $f, 'small' => true)); ?></li>
         <?php endforeach; ?>
     </ul>
+
+    <div class="more-friends"><?=CHtml::link('Найти ещё друзей', '/activity/friends')?><?php if (! Yii::app()->user->isGuest && ! WantToChat::hasCooldown(Yii::app()->user->id)): ?> <a href="" class="wannachat" onclick="WantToChat.send(this); return false;">Хочу общаться!</a><?php endif; ?></div>
 
 </div>

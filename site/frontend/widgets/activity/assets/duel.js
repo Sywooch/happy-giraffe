143aa5aa2bb1948f0ea842a0f87f05e541718e08
ChooJoy <choojoy.work@gmail.com>
2012-05-17 08:32:06 +0400

    live

--- site/frontend/widgets/activity/assets/duel.js
+++ site/frontend/widgets/activity/assets/duel.js
@@ -3,5 +3,25 @@ var Duel = {
 }
 
 Duel.select = function(el, id) {
-    $('#duel_step_2 div.title').text(el.text());
+    var jEl = $(el);
+    $('span.num', jEl).remove();
+    $('#duel_step_2 div.title').text(jEl.text());
+    $('#DuelAnswer_question_id').val(id);
+    $('#duel_step_1').hide();
+    $('#duel_step_2').show();
+}
+
+Duel.submit = function(form) {
+    $.post(
+        $(form).attr('action'),
+        $(form).serialize(),
+        function(response) {
+            if (response.status) {
+                $('#duel_step_3').html(response.html);
+                $('#duel_step_2').hide();
+                $('#duel_step_3').show();
+            }
+        },
+        'json'
+    );
 }

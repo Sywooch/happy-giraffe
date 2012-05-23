918cb2fe5867aace5726e07fc2877c98bbbc05a5
ChooJoy <choojoy.work@gmail.com>
2012-05-23 20:30:03 +0400

    activity

--- site/frontend/views/activity/friends.php
+++ site/frontend/views/activity/friends.php
@@ -5,15 +5,9 @@
 
 <div id="find-friend" class="activity-find-friend">
 
-    <div class="title">Найти <span>друзей</span><?php if(count($friends) > 12): ?> <a href="javascript:void(0);" onclick="nextFriendsPage();"><span>Найти<br/>еще</span></a><?php endif; ?></div>
+    <div class="title">Найти <span>друзей</span> <a href="javascript:void(0);" onclick="nextFriendsPage(); return false;"><span>Найти<br/>еще</span></a></div>
 
     <div id="find-friend-wrapper">
-        <?php for($i = 0; $i < ceil(count($friends)/12); $i++): ?>
-            <ul class="clearfix"<?php if ($i == 0): ?> style="display:block;"<?php endif; ?>>
-                <?php for($j = $i * 12; ($j < ($i + 1) * 12) && $j < count($friends); $j++): ?>
-                    <?php $this->renderPartial('application.widgets.activity.views._friend', array('f' => $friends[$j])); ?>
-                <?php endfor; ?>
-            </ul>
-        <?php endfor; ?>
+        <?php $this->renderPartial('_friends', compact('friends')); ?>
     </div>
 </div>

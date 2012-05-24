c42b9e8868779d1d93a56dc9f2aa5791536f8458
ChooJoy <choojoy.work@gmail.com>
2012-05-24 09:08:04 +0400

    activity

--- site/frontend/modules/services/modules/repair/controllers/SuspendedCeilingController.php
+++ site/frontend/modules/services/modules/repair/controllers/SuspendedCeilingController.php
@@ -5,6 +5,9 @@ class SuspendedCeilingController extends HController
 
     public $layout = '//layouts/new';
 
+    /**
+     * @sitemap
+     */
     public function actionIndex()
     {
         $this->pageTitle = 'Расчет материалов для подвесного потолка';

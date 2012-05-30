ecce65586e28106eea4f2dd2c7b147991c2fe48f
Kireev Alex <alexk984@gmail.com>
2012-05-30 14:53:38 +0400

    yandex webmaster

--- site/seo/components/YandexWebmaster.php
+++ site/seo/components/YandexWebmaster.php
@@ -8,18 +8,12 @@ class YandexWebmaster extends CComponent
 
     public function login()
     {
-
-        $hostname = 'oauth.yandex.ru';
-        $ch = curl_init('https://' . $hostname . '/token');
+        $hostname = 'webmaster.yandex.ru';
+        $ch = curl_init('https://' . $hostname . '/api/me');
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=password&username=alex-kireev.volg&password=happyalex&client_id=d27687353e75454b9e31062755093f8d&client_secret=d1877be966034ce082d1542fe9b1eca3');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization'=>'OAuth 3c1b05e08f6647699ba611c9836a1b9e'));
         curl_exec($ch);
         $result = curl_multi_getcontent($ch);
         echo $result;

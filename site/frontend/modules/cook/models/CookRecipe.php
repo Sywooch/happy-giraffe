63618b1533773add23ac7239f0bb0b0069c7470d
ChooJoy <choojoy.work@gmail.com>
2012-06-04 17:46:40 +0400

    add_recipe

--- site/frontend/modules/cook/models/CookRecipe.php
+++ site/frontend/modules/cook/models/CookRecipe.php
@@ -95,6 +95,7 @@ class CookRecipe extends CActiveRecord
             array('servings', 'numerical', 'integerOnly' => true, 'min' => 1, 'max' => 10),
             array('preparation_duration, cooking_duration', 'numerical', 'integerOnly' => true, 'min' => 1, 'max' => 999),
             array('preparation_duration_h, preparation_duration_m, cooking_duration_h, cooking_duration_m', 'safe'),
+            array('cuisine_id', 'default', 'value' => null),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('id, title, photo_id, preparation_duration, cooking_duration, servings, text, cuisine_id, type, method, author_id', 'safe', 'on'=>'search'),
---
+++ site/frontend/modules/cook/views/recipe/_ingredient.php
@@ -0,0 +1,46 @@
+<tr<?php if ($n >= 3): ?> style="display:none;"<?php endif; ?>>
+    <td class="col-1">
+        <?php $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
+            'name' => 'CookRecipeIngredient[' . $n . '][title]',
+            'sourceUrl' => '/cook/recipe/ac/',
+            'options' => array(
+                'minLength' => '2',
+                'select' => 'js: function(event, ui) {inSelect(event, ui, this)}',
+            ),
+            'htmlOptions' => array(
+                'placeholder' => 'Название продукта',
+            ),
+        )); ?>
+        <?=$form->hiddenField($model, '[' . $n . ']' . 'ingredient_id')?>
+    </td>
+    <td class="col-2"><?=$form->textField($model, '[' . $n . ']' . 'value', array('placeholder' => 0))?></td>
+    <td class="col-3">
+        <div class="drp-list">
+            <?php $this->renderPartial('_units', array('units' => $units, 'unit' => $model->unit, 'n' => $n)); ?>
+            <?=$form->hiddenField($model, '[' . $n . ']' . 'unit_id')?>
+        </div>
+    </td>
+    <td class="col-4"><a href="javascript:void(0)" class="remove tooltip" title="Удалить ингредиент"></a></td>
+</tr>
+
+<?php if (false): ?>
+    <div>
+        <p>
+            Продукт:
+            <?php $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
+                'name' => 'CookRecipeIngredient[' . $n . '][title]',
+                'sourceUrl' => '/cook/recipe/ac/',
+                'options' => array(
+                    'minLength' => '2',
+                    'select' => 'js: function(event, ui) {
+                        $(\'#CookRecipeIngredient_' . $n . '_ingredient_id\').val(ui.item.id);
+                    }',
+                ),
+            )); ?>
+
+            <?=$form->hiddenField($model, '[' . $n . ']' . 'ingredient_id')?>
+        </p>
+        <p>Кол-во: <?=$form->textField($model, '[' . $n . ']' . 'value')?></p>
+        <p>Мера: <?=$form->dropDownList($model, '[' . $n . ']' . 'unit_id', CHtml::listData($units, 'id', 'title'), array('prompt' => 'не выбрано'))?></p>
+    </div>
+<?php endif; ?>

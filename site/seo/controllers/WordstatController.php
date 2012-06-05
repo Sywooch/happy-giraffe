df2e45fbf16d11ce2e0c7a1afb63a36fdb497e3a
Kireev Alex <alexk984@gmail.com>
2012-06-05 16:11:41 +0400

    wordstat parsing fixes

--- site/seo/controllers/WordstatController.php
+++ site/seo/controllers/WordstatController.php
@@ -43,10 +43,10 @@ class WordstatController extends SController
 
     public function actionAddCompetitors()
     {
-        $keywords = Yii::app()->db_seo->createCommand('select distinct(keyword_id) from baby_stats__key_stats')->queryColumn();
+        $keywords = Yii::app()->db_seo->createCommand('select keyword_id from baby_stats__key_stats')->queryColumn();
         $count = 0;
         foreach ($keywords as $keyword)
-            if (ParsingKeywords::model()->addKeywordById($keyword['id']))
+            if (ParsingKeywords::model()->addKeywordById($keyword['keyword_id']))
                 $count++;
 
         echo CJSON::encode(array(

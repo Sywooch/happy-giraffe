f0e3d12fcddc46186c320a27b9e32ac9f7294e05
Kireev Alex <alexk984@gmail.com>
2012-06-06 09:55:27 +0400

    delete article + article numbers

--- site/seo/views/existArticles/_article.php
+++ site/seo/views/existArticles/_article.php
@@ -1,5 +1,8 @@
 <tr>
-    <td class="col-1"><?=$model->id ?></td>
+    <td class="col-1"><?=$model->number ?></td>
     <td><b><?=$model->article->title ?></b><br/><a target="_blank" href="<?=$model->url ?>"><?=$model->url ?></a></td>
     <td><?=$model->getKeywords() ?></td>
+    <?php if (Yii::app()->user->checkAccess('admin')):?>
+        <td><a href="#" onclick="SeoModule.removeArticle(this, <?=$model->id ?>)">remove</a></td>
+    <?php endif ?>
 </tr>

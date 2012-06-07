8e7b1c7b7fcf8b051f671c5775bbd1a01544d0b6
Dmitriy Farafontov <farikd@gmail.com>
2012-06-07 11:04:05 +0300

    cookdecor

--- site/frontend/widgets/fileAttach/views/_cook_decor.php
+++ site/frontend/widgets/fileAttach/views/_cook_decor.php
@@ -1,9 +1,16 @@
 <div class="photo-title">
-    <label>Название блюда или оформления</label>
-    <input type="text" name="title" placeholder="Введите название ">
-    <label>Категория</label>
+
+    <div class="row">
+    <label>Тип блюда</label>
     <?php
     Yii::import('application.modules.cook.models.CookDecorationCategory');
-    echo CHtml::dropDownList('category', '', CHtml::listData(CookDecorationCategory::model()->findAll(), 'id', 'title'));
+    echo CHtml::dropDownList('category', '', CHtml::listData(CookDecorationCategory::model()->findAll(), 'id', 'title'), array('class'=>'chzn-select'));
     ?>
+    </div>
+
+    <div class="row">
+    <label>Название блюда или оформления</label>
+    <input type="text" name="title" placeholder="Введите название ">
+    </div>
+
 </div>

4962744d521a5203b220c3d3bf2ebb17538afe43
Kireev Alex <alexk984@gmail.com>
2012-06-07 17:19:09 +0400

    fixes

--- site/seo/controllers/TaskController.php
+++ site/seo/controllers/TaskController.php
@@ -123,12 +123,18 @@ class TaskController extends SController
             $article_keywords->entity_id = $article_id;
             $article_keywords->keyword_group_id = $task->keyword_group_id;
             $article_keywords->url = $url;
-            if (!$article_keywords->save()){
+            if (!$article_keywords->save()) {
+                $errorText = '';
+                foreach ($article_keywords->getErrors() as $error) {
+                    foreach($error as $errorPart)
+                        $errorText.= $errorPart.' ';
+                }
+
                 echo CJSON::encode(array(
                     'status' => false,
-                    'error' => 'Ошибка сохранения статьи'
+                    'error' => 'Ошибка сохранения статьи',
+                    'errorText' => $errorText
                 ));
-                var_dump($article_keywords->getErrors());
                 Yii::app()->end();
             }
 

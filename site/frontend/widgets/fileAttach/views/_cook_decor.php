1fa8f587704efa0417458fda3da1a12a591e5696
Dmitriy Farafontov <farikd@gmail.com>
2012-06-07 11:20:27 +0300

    cookdecor

--- site/frontend/widgets/fileAttach/views/_cook_decor.php
+++ site/frontend/widgets/fileAttach/views/_cook_decor.php
@@ -1,16 +1,22 @@
 <div class="photo-title">
 
     <div class="row">
-    <label>Тип блюда</label>
-    <?php
-    Yii::import('application.modules.cook.models.CookDecorationCategory');
-    echo CHtml::dropDownList('category', '', CHtml::listData(CookDecorationCategory::model()->findAll(), 'id', 'title'), array('class'=>'chzn-select'));
-    ?>
+        <label>Тип блюда</label>
+        <?php
+        Yii::import('application.modules.cook.models.CookDecorationCategory');
+        echo CHtml::dropDownList('category', '', CHtml::listData(CookDecorationCategory::model()->findAll(), 'id', 'title'), array('class' => 'chzn-select'));
+        ?>
     </div>
 
     <div class="row">
-    <label>Название блюда или оформления</label>
-    <input type="text" name="title" placeholder="Введите название ">
+        <label>Название блюда или оформления</label>
+        <input type="text" name="title" placeholder="Введите название ">
+    </div>
+
+    <div style="" id="save_attach_button" class="form-bottom">
+        <button onclick="Attach.insertToCookDecoration();" class="btn btn-green-medium">
+            <span><span>Завершить</span></span>
+        </button>
     </div>
 
 </div>

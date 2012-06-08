d3f85e961f05bbf70f24eb4c68c68d8655d20f8e
ChooJoy <choojoy.work@gmail.com>
2012-06-08 18:57:03 +0400

    recipes

--- site/frontend/components/HUrlManager.php
+++ site/frontend/components/HUrlManager.php
@@ -7,7 +7,7 @@ class HUrlManager extends CUrlManager
 {
     public function createUrl($route,$params=array(),$ampersand='&')
     {
-        if (strpos($route, 'community/view') !== FALSE && $params['community_id'] === null)
+        if (strpos($route, 'community/view') !== FALSE && (isset($params['community_id']) ? $params['community_id'] === null : true))
             Yii::log('Causing 404 errors' . "\n" . 'Controller: ' . Yii::app()->controller->id . "\n" . 'Action: ' . Yii::app()->controller->action->id . "\n", 'error');
 
         return parent::createUrl($route, $params, $ampersand);

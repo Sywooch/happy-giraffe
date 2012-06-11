954080bda85d6026fffcc9d1e46b34943089afe5
Kireev Alex <alexk984@gmail.com>
2012-06-11 17:09:54 +0400

    wordstat parsing features

--- site/seo/views/wordstat/index.php
+++ site/seo/views/wordstat/index.php
@@ -2,4 +2,5 @@
 <div><?= YandexPopularity::model()->count() ?> спарсено</div>
 Введите ключевое слово для парсинга Wordstat <input type="text" size="50">
 <a href="javascript:;" onclick="WordStat.addKeyword(this);">спарсить все c этим словом</a><br>
-<a href="javascript:;" onclick="WordStat.addCompetitors()">добавить фразы конкурентов</a>
+<a href="javascript:;" onclick="WordStat.addCompetitors()">добавить фразы конкурентов</a><br>
+<a href="javascript:;" onclick="WordStat.clearKeywords()">очистить список</a>

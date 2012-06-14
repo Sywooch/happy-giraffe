b47053248a1847b3a77dba6a276a8a4418b1e583
Kireev Alex <alexk984@gmail.com>
2012-06-14 12:20:08 +0400

    decoration admin

--- site/backend/views/club/cookDecoration/_form.php
+++ site/backend/views/club/cookDecoration/_form.php
@@ -11,12 +11,6 @@
     <?php echo $form->errorSummary($model); ?>
 
     <div class="row">
-        <?php echo $form->labelEx($model, 'photo_id'); ?>
-        <?php echo $form->textField($model, 'photo_id', array('size' => 11, 'maxlength' => 11)); ?>
-        <?php echo $form->error($model, 'photo_id'); ?>
-    </div>
-
-    <div class="row">
         <?php echo $form->labelEx($model, 'category_id'); ?>
         <?php echo $form->dropDownList($model, 'category_id', CHtml::listData(CookDecorationCategory::model()->findAll(), 'id', 'title'), array('empty' => ' '));?>
         <?php echo $form->error($model, 'category_id'); ?>

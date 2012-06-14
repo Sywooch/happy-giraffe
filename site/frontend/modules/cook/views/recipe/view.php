1f3b0eb292a27261f9c61a751dc6d3691d9e5fd1
ChooJoy <choojoy.work@gmail.com>
2012-06-14 22:34:03 +0400

    recipe view

--- site/frontend/modules/cook/views/recipe/view.php
+++ site/frontend/modules/cook/views/recipe/view.php
@@ -4,7 +4,7 @@
      */
 
     $this->widget('site.frontend.widgets.photoView.photoViewWidget', array(
-        'selector' => 'div.big > img, div.thumbs > img:not(.add)',
+        'selector' => 'div.big > a, div.thumbs > a:not(.add)',
         'entity' => 'CookRecipe',
         'entity_id' => $recipe->id,
     ));

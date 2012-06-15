e28c5077aaada3a3a4886ef0179c078d37766512
ChooJoy <choojoy.work@gmail.com>
2012-06-15 21:24:17 +0400

    cook main

--- site/frontend/modules/cook/controllers/DefaultController.php
+++ site/frontend/modules/cook/controllers/DefaultController.php
@@ -5,9 +5,6 @@ class DefaultController extends HController
 
     public function actionIndex()
     {
-        print_r(CookRecipe::model()->counts);
-        die;
-
         $this->pageTitle = 'Кулинария';
 
         $community = Community::model()->findByPk(22);

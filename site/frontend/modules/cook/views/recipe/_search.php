111d693ea10e023cce4fece8e9fa125bc5d0013c
Kireev Alex <alexk984@gmail.com>
2012-06-16 09:17:30 +0400

    cook search

--- site/frontend/modules/cook/views/recipe/_search.php
+++ site/frontend/modules/cook/views/recipe/_search.php
@@ -1,6 +1,11 @@
 <?php
 $criteria->addCondition('t.id = :id');
-$criteria->params[':id'] = $data->id;
-$data = CookRecipe::model()->find($criteria);
+if (isset($data['id'])) {
+    $criteria->params[':id'] = $data['id'];
+    if (isset($type) && $type !== null)
+        $criteria->compare('type', $type);
+    $data = CookRecipe::model()->find($criteria);
 
-$this->renderPartial('_recipe', compact('data'));
+    if ($data)
+        $this->renderPartial('_recipe', compact('data'));
+}

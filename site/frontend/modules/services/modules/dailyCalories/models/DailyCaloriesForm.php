023d583d5f1bdff076fdad2a27fc6eb7bc23af5a
Dmitriy Farafontov <farikd@gmail.com>
2012-06-22 17:46:01 +0300

    dailyCalories

---
+++ site/frontend/modules/services/modules/dailyCalories/models/DailyCaloriesForm.php
@@ -0,0 +1,30 @@
+<?php
+
+class DailyCaloriesForm extends HFormModel
+{
+    public $sex;
+    public $age;
+    public $growth;
+    public $weight;
+    public $activity;
+
+    public function rules()
+    {
+        return array(
+            array('sex, age, growth, weight, activity', 'required', 'message' => 'Укажите площадь потолка'),
+            array('age, growth, weight', 'numerical', 'message' => 'Введите число')
+            //array('growth, weight', 'normalizeLength')
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'sex' => 'Пол',
+            'age' => 'Возраст',
+            'growth' => 'Рост',
+            'weight' => 'Вес',
+            'activity' => 'Активность'
+        );
+    }
+}
---
+++ site/frontend/modules/services/modules/dailyCalories/views/index.php
@@ -0,0 +1,49 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'daily-calories-form-index-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'sex'); ?>
+		<?php echo $form->textField($model,'sex'); ?>
+		<?php echo $form->error($model,'sex'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'age'); ?>
+		<?php echo $form->textField($model,'age'); ?>
+		<?php echo $form->error($model,'age'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'growth'); ?>
+		<?php echo $form->textField($model,'growth'); ?>
+		<?php echo $form->error($model,'growth'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'weight'); ?>
+		<?php echo $form->textField($model,'weight'); ?>
+		<?php echo $form->error($model,'weight'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'activity'); ?>
+		<?php echo $form->textField($model,'activity'); ?>
+		<?php echo $form->error($model,'activity'); ?>
+	</div>
+
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->

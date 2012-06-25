1b4d6aa5747d06cb65aadd4c979bf6d62de01829
ChooJoy <choojoy.work@gmail.com>
2012-06-25 10:43:37 +0400

    seo fixes

--- site/frontend/views/layouts/main.php
+++ site/frontend/views/layouts/main.php
@@ -3,6 +3,9 @@
 <!--[if IE 7]>    <html xmlns="http://www.w3.org/1999/xhtml" class="ie7"> <![endif]-->
 <!--[if gt IE 7]><!--> <html xmlns="http://www.w3.org/1999/xhtml"> <!--<![endif]-->
 <head>
+    <?php if ($this->rssFeed !== null): ?>
+        <?=CHtml::linkTag('alternate', 'application/rss+xml', $this->rssFeed)?>
+    <?php endif; ?>
     <?=CHtml::linkTag('shortcut icon', null, '/favicon.bmp')?>
     <?=CHtml::metaTag('text/html; charset=utf-8', NULL, 'Content-Type')?>
     <title><?=CHtml::encode($this->pageTitle)?></title>

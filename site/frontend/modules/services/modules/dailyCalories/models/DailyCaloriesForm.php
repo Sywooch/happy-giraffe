40760734388b58f4eea8fb8a66c34ab7b603acdc
Dmitriy Farafontov <farikd@gmail.com>
2012-06-25 17:29:28 +0300

    dailyCalories

--- site/frontend/modules/services/modules/dailyCalories/models/DailyCaloriesForm.php
+++ site/frontend/modules/services/modules/dailyCalories/models/DailyCaloriesForm.php
@@ -7,6 +7,15 @@ class DailyCaloriesForm extends HFormModel
     public $growth;
     public $weight;
     public $activity;
+    public $activities = array(
+        '1.2' => 'Сидячий образ жизни',
+        '1.375' => 'Небольшая активность',
+        '1.55' => 'Умеренная активность',
+        '1.725' => 'Высокая активность',
+        '1.9' => 'Очень высокая активность'
+    );
+
+    public $sexes = array(0 => 'Ж', 1 => 'М');
 
     public function rules()
     {
@@ -33,6 +42,11 @@ class DailyCaloriesForm extends HFormModel
         $result = array();
         $result['calories'] = ($this->sex) ? 10 * $this->weight + 6.25 * $this->growth - 5 * $this->age + 5 : 10 * $this->weight + 6.25 * $this->growth - 5 * $this->age - 161;
         $result['calories'] *= $this->activity;
+        $result['calories'] = ceil($result['calories']);
+
+        $result['protein'] = ceil(($result['calories'] * (4 / 17)) * (1 / 6));
+        $result['fats'] = ceil(($result['calories'] * (9 / 17)) * (1 / 6));
+        $result['carbs'] = ceil(($result['calories'] * (4 / 17)) * (4 / 6));
 
         return $result;
     }

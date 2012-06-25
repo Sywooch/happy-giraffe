cd42517712bbb80613d00bd9ed6a3f435bcba99e
Eugene <lnghost@hotmail.com>
2012-06-25 19:08:11 +0400

    Рассылки

--- site/backend/modules/mail/views/index/create.php
+++ site/backend/modules/mail/views/index/create.php
@@ -6,6 +6,6 @@
 )); ?>
     <p>Тема письма: <input type="text" name="subject" value="" /></p>
     <p>Письмо:</p>
-    <p><textarea rows="10" cols="60" name="text"></textarea></p>
+    <p><textarea rows="10" cols="60" name="body"></textarea></p>
     <p><input type="submit" value="Начать рассылку" /></p>
 <?php $this->endWidget(); ?>

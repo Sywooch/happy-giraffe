c0383e19faa93eb4143acc2c12fb2b5205f407cd
ChooJoy <choojoy.work@gmail.com>
2012-06-26 21:56:45 +0400

    cook fix

--- site/frontend/controllers/RssController.php
+++ site/frontend/controllers/RssController.php
@@ -121,9 +121,6 @@ class RssController extends HController
             'with' => 'response',
         ));
 
-        if (! $comments)
-            throw new CHttpException(404, 'Такой записи не существует');
-
         Yii::import('ext.EFeed.*');
         $feed = new EFeed();
         $feed->link = $this->createAbsoluteUrl('blog/list', array('user_id' => $user->id));

b302d3ea76068df42e0bddab1eecee21b0afdee1
Eugene <lnghost@hotmail.com>
2012-06-26 13:03:08 +0400

    Change migration

--- site/common/migrations/m120626_072339_change_users.php
+++ site/common/migrations/m120626_072339_change_users.php
@@ -5,6 +5,8 @@ class m120626_072339_change_users extends CDbMigration
 	public function up()
 	{
         $this->execute("ALTER TABLE `users` ADD `remember_code` MEDIUMINT( 5 ) NOT NULL ");
+        $this->execute("INSERT INTO `mail__templates` (`id`, `title`, `action`, `subject`, `body`) VALUES
+        (2, 'Восстановление пароля', 'remember_password', 'Восстановление пароля', '');");
 	}
 
 	public function down()

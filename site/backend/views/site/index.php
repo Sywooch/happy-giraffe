7128536d566a274834bf03e84ee414356c9a6846
Kireev Alex <alexk984@gmail.com>
2012-06-27 07:40:13 +0400

    regs count

--- site/backend/views/site/index.php
+++ site/backend/views/site/index.php
@@ -2,7 +2,7 @@
     <h2>Регистрации за последние дни</h2>
     <?php for($i=0;$i<10;$i++){
     $date = date("Y-m-d", strtotime('-'.$i.' days'));
-    $regs_count = Yii::app()->db->createCommand('select count(id) from users where register_date >= "'.$date.' 00:00:00" AND register_date <= "'.$date.' 00:00:00";')->queryScalar();
+    $regs_count = Yii::app()->db->createCommand('select count(id) from users where register_date >= "'.$date.' 00:00:00" AND register_date <= "'.$date.' 23:59:59";')->queryScalar();
     ?><div>
         <span><?=$date ?>&nbsp;&nbsp;&nbsp;&nbsp;</span><span><?=$regs_count ?></span>
     </div><?php } ?>

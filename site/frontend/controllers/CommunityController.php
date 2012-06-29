dd30ad2278574d7a22ba8e9f01bf820563f28eb2
Eugene Podosenov <lnghost@hotmail.com>
2012-06-29 11:30:24 +0400

    Content gallery fixes

--- site/frontend/controllers/CommunityController.php
+++ site/frontend/controllers/CommunityController.php
@@ -230,7 +230,7 @@ class CommunityController extends HController
                     $gallery = new CommunityContentGallery;
                     $gallery->title = $_POST['CommunityContentGallery']['title'];
                     $gallery->content_id = $model->id;
-                    if($gallery->save() && $items = Yii::app()->request->getPost('CommunityContentGalleryItem'))
+                    if($gallery->save() && ($items = Yii::app()->request->getPost('CommunityContentGalleryItem')) != null)
                     {
                         foreach($items as $item)
                         {

59e75ee7e928ca5b701096ba1199099ee906b401
ChooJoy <choojoy.work@gmail.com>
2012-06-30 13:36:37 +0400

    release the kraken

--- site/frontend/www/javascripts/gallery.js
+++ site/frontend/www/javascripts/gallery.js
@@ -157,7 +157,7 @@ jQuery.fn.pGallery = function(options) {
     }
 
     plugin.getEntityUrl = function() {
-        return document.location.href.replace(/#1\/photo(.*)/, '');
+        return document.location.href.replace(/#!\/photo(.*)/, '');
     }
 
     if(/\/photo(\d+)/.test(document.location.href)) {

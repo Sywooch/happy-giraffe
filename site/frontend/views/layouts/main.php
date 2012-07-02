2259cd98744bc89850ecdd1c4310f3d2436fc722
Kireev Alex <alexk984@gmail.com>
2012-07-02 18:46:24 +0400

    ga check

--- site/frontend/views/layouts/main.php
+++ site/frontend/views/layouts/main.php
@@ -503,6 +503,7 @@
                 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
                 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
             })();
+
         </script>
     </noindex>
 

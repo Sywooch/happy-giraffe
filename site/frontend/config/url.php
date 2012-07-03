86c4245f9ea2188859dd4a2b8811f9b904d13c05
ChooJoy <choojoy.work@gmail.com>
2012-07-03 10:47:03 +0400

    release the kraken

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -5,6 +5,7 @@ return array(
     'urlFormat'=>'path',
     'showScriptName' => false,
     'urlSuffix' => '/',
+    'useStrictParsing' => true,
     'rules' => array(
         '/user/<user_id:\d+>' => 'user/profile',
         '/user/<user_id:\d+>/clubs' => 'user/clubs',

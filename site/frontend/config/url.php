ea635fbb790e7ff5472f58b2230815c25061c761
ChooJoy <choojoy.work@gmail.com>
2012-07-04 21:00:55 +0400

    release the kraken

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -66,12 +66,12 @@ return array(
         'community/<community_id:\d+>/forum' => 'community/list',
         'community/<community_id:\d+>/forum/<content_type_slug:\w+>/<content_id:\d+>' => 'community/view',
         'community/<community_id:\d+>/forum/<content_type_slug:\w+>/<content_id:\d+>/uploadImage' => 'community/uploadImage',
-        'community/<_a:(join|add)>' => 'community/<_a>',
 
         'community/add/community_id/<community_id:\d+>/rubric_id/<rubric_id:\d+>/content_type_slug/<content_type_slug:(post|video|travel)>' => 'community/add',
         'community/add/community_id/<community_id:\d+>/rubric_id/<rubric_id:\d+>' => 'community/add',
         'community/add/community_id/<community_id:\d+>/content_type_slug/<content_type_slug:(post|video|travel)>' => 'community/add',
         'community/add/community_id/<community_id:\d+>' => 'community/add',
+        'community/<_a:(join|add)>' => 'community/<_a>',
 
         //global
         '<_c:(activity|ajax|notification|signup|profile|friendRequests|communityRubric|family|morning)>/<_a>' => '<_c>/<_a>',

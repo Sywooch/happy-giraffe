80c8278ba506ef6923603f4848188dc969d011f3
Kireev Alex <alexk984@gmail.com>
2012-07-04 11:19:52 +0400

    add alt to photo

---
+++ site/common/behaviors/AddImageTagsBehavior.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Author: alexk984
+ * Date: 04.07.2012
+ */
+class AddImageTagsBehavior extends CActiveRecordBehavior
+{
+    public function addTags()
+    {
+        Yii::import('site.frontend.extensions.phpQuery.phpQuery');
+
+        $doc = phpQuery::newDocumentXHTML($this->owner->text, $charset = 'utf-8');
+        $i = 0;
+        foreach (pq('img') as $image) {
+            $alt = pq($image)->attr('alt');
+            $title = pq($image)->attr('title');
+
+            if (empty($alt) || empty($title)){
+                $i++;
+
+                if (empty($alt))
+                    pq($image)->attr('alt', $this->owner->content->title.' рисунок '.$i);
+
+                if (empty($title))
+                    pq($image)->attr('title', $this->owner->content->title.' рисунок '.$i);
+            }
+        }
+        $this->owner->text = $doc->html();
+    }
+
+    public function attach($owner)
+    {
+        parent::attach($owner);
+
+        $owner->attachEventHandler('onBeforeSave', array($this, 'addTags'));
+    }
+
+    public function detach($owner)
+    {
+        parent::detach($owner);
+
+        $owner->detachEventHandler('onBeforeSave', array($this, 'addTags'));
+    }
+}

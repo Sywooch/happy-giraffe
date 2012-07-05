ef2005eac9a88560b6bee5de53d3904cb4e9d752
Dmitriy Farafontov <farikd@gmail.com>
2012-07-05 17:01:44 +0300

    bodyFat

---
+++ site/frontend/modules/services/modules/bodyFat/models/BodyFatForm.php
@@ -0,0 +1,46 @@
+<?php
+
+class BodyFatForm extends HFormModel
+{
+    public $weight;
+    public $height;
+    public $waist;
+    public $sex;
+
+    public $sexes = array(0 => 'Ж', 1 => 'М');
+
+    public function rules()
+    {
+        return array(
+            array('weight, height, waist, sex', 'required', 'message' => 'Укажите {attribute}'),
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'height' => 'Рост, см',
+            'weight' => 'Вес, кг',
+            'waist' => 'Талия, см',
+            'sex' => 'Пол'
+        );
+    }
+
+    public function calculate()
+    {
+        $result = array();
+
+        if ($this->sex == 1) {
+            $result['fatPercent'] = 0.31457 * $this->waist - 0.10969 * $this->weight + 10.834;
+            $result['method'] = 'male';
+        } else {
+            $result['fatPercent'] = 100 - (0.11077 * $this->waist - 0.17666 * ($this->height / 100) + 0.14354 * $this->weight + 51.033);
+            $result['method'] = 'female';
+        }
+        $result['fatPercent'] = ceil($result['fatPercent']);
+        $result['fatWeight'] = ceil($this->weight * $result['fatPercent'] / 100);
+        $result['bodyWeight'] = $this->weight - $result['fatWeight'];
+
+        return $result;
+    }
+}

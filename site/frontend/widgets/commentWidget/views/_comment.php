c8f300d9cd752b17df560b318803a501b4ca0197
Kireev Alex <alexk984@gmail.com>
2012-07-07 22:19:10 +0400

    first giraffe comment fix

--- site/frontend/widgets/commentWidget/views/_comment.php
+++ site/frontend/widgets/commentWidget/views/_comment.php
@@ -4,8 +4,9 @@
  */
 if ($data->author_id == 1): ?>
     <?php if (!empty($data->photoAttaches)):?>
-        <?= $data->photoAttach->getContent(true); ?>
+        <?php $data->photoAttach->getContent(); ?>
     <?php endif ?>
+    <img src="/images/happy_giraffe_comment.jpg" alt="Веселый Жираф приветствует вас!">
 <?php else: ?>
 <?php if ($data->removed == 0): ?>
     <li class="clearfix item<?php if ($data->author_id == Yii::app()->user->id) echo ' author-comment' ?>" id="comment_<?php echo $data->id; ?>">

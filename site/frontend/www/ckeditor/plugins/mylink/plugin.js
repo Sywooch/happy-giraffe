d5cdccd33042770e7b053c43db0c436951b4cbc2
Dmitriy Farafontov <farikd@gmail.com>
2012-07-09 18:57:37 +0300

    fixes

--- site/frontend/www/ckeditor/plugins/mylink/plugin.js
+++ site/frontend/www/ckeditor/plugins/mylink/plugin.js
@@ -14,17 +14,28 @@ var MyLinkCommand = {
 }
 
 function epic_func_mylink(el) {
+
     var title = $(el).parents('.popup').find('.link-name').val();
     var href = $(el).parents('.popup').find('.link-address').val();
+
     var urlRegex = /^((?:http|https):\/\/)?(.*)$/;
+
+    if (href.substring(0, 14) == 'http://http://')
+        href = href.substring(7);
+    if (href.substring(0, 16) == 'https://https://')
+        href = href.substring(8);
 
     if (href.substring(0, 7) != 'http://' && href.substring(0, 8) != 'https://')
         href = 'http://' + href;
 
-    var urlMatch;
-    if (href && ( urlMatch = href.match(urlRegex) )) {
+    var regexp = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
+    var urlMatch = regexp.test(href);
+
+    if (href && urlMatch) {
         CKEDITOR.instances[cke_instance].insertHtml('<a href="' + href + '">' + $.trim(title) + '</a> ');
         $.fancybox.close();
+    } else {
+        alert('Введите корректный адрес ссылки');
     }
 }
 

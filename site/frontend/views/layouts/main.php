4bb7d078eeef78eaad4d8ee1e90e84ac04934633
Kireev Alex <alexk984@gmail.com>
2012-07-10 16:36:49 +0400

    pre release fixes

--- site/frontend/views/layouts/main.php
+++ site/frontend/views/layouts/main.php
@@ -14,18 +14,19 @@
         <title><?=CHtml::encode($this->pageTitle)?></title>
     <?php endif;
 
+    $release_id = 325;
     $cs = Yii::app()->clientScript;
     $cs
         ->registerScriptFile('/javascripts/comet.js')
         ->registerScriptFile('/javascripts/dklab_realplexor.js')
         ->registerScript('Realplexor-reg', 'comet.connect(\'http://' . Yii::app()->comet->host . '\', \'' . Yii::app()->comet->namespace . '\', \'' . UserCache::GetCurrentUserCache() . '\');')
 
-        ->registerCssFile('/stylesheets/common.css?r=351')
-        ->registerCssFile('/stylesheets/global.css?r=351')
+        ->registerCssFile('/stylesheets/common.css?r='.$release_id)
+        ->registerCssFile('/stylesheets/global.css?r='.$release_id)
         ->registerCssFile('/stylesheets/ie.css', 'screen')
         ->registerCoreScript('jquery')
         ->registerCssFile('/stylesheets/jquery.fancybox-1.3.4.css')
-        ->registerScriptFile('/javascripts/jquery.fancybox-1.3.4.pack.js')
+        ->registerScriptFile('/javascripts/jquery.fancybox-1.3.4.pack.js?r='.$release_id)
         ->registerScriptFile('/javascripts/jquery.iframe-post-form.js')
         ->registerScriptFile('/javascripts/jquery.placeholder.min.js')
         ->registerScriptFile('/javascripts/chosen.jquery.min.js')

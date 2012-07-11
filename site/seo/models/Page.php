78a658195b313a443f88dce7c0308448da57e584
Kireev Alex <alexk984@gmail.com>
2012-07-11 11:55:58 +0400

    seo fix

--- site/seo/models/Page.php
+++ site/seo/models/Page.php
@@ -118,7 +118,6 @@ class Page extends CActiveRecord
 
         return new CActiveDataProvider($this, array(
             'criteria' => $criteria,
-            'pagination' => array('pageSize' => 20),
         ));
     }
 

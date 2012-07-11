b973ed911b7b757e66313be1b331c550d6991483
Kireev Alex <alexk984@gmail.com>
2012-07-11 12:17:29 +0400

    baby gender index page

--- site/frontend/modules/services/modules/babySex/views/default/index.php
+++ site/frontend/modules/services/modules/babySex/views/default/index.php
@@ -1,1 +1,34 @@
-<?php $this->renderPartial('blood_refresh'); ?>
+<div id="baby-sex">
+
+    <div class="block-title"><h1>Определение пола ребенка</h1></div>
+
+    <div class="chart">
+
+        <a href="<?=$this->createUrl('china') ?>" class="link-1">
+            <i class="icon"></i>
+            Китайский метод
+        </a>
+
+        <a href="<?=$this->createUrl('japan') ?>" class="link-2">
+            <i class="icon"></i>
+            Японский метод
+        </a>
+
+        <a href="<?=$this->createUrl('bloodRefresh') ?>" class="link-3">
+            <i class="icon"></i>
+            По дням рождения родителей
+        </a>
+
+        <a href="<?=$this->createUrl('blood') ?>" class="link-4">
+            <i class="icon"></i>
+            По группе крови родителей
+        </a>
+
+        <a href="<?=$this->createUrl('ovulation') ?>" class="link-5">
+            <i class="icon"></i>
+            По овуляции
+        </a>
+
+    </div>
+
+</div>

0bcff485e0a8391d9eaffe2d6066bf0319f09c16
ChooJoy <choojoy.work@gmail.com>
2012-07-11 20:33:51 +0400

    interests

--- site/common/models/CommunityPost.php
+++ site/common/models/CommunityPost.php
@@ -133,7 +133,8 @@ class CommunityPost extends HActiveRecord
 
     protected function afterSave()
     {
-        echo CopyScape::getUniquenessByText($this->text);
+        $test = CopyScape::getUniquenessByText($this->text);
+        var_dump($test);
         die;
         if ($this->isNewRecord)
             $this->content->update(array('uniqueness' => CopyScape::getUniquenessByText($this->text)));

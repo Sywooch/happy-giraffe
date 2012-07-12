e0c101008744273996d15e60732eda286046b85e
ChooJoy <choojoy.work@gmail.com>
2012-07-12 14:52:49 +0400

    Merge branch 'master' of github.com:beknown/happy-giraffe
    Conflicts:
    	site/frontend/modules/services/modules/babySex/views/default/index.php

---
+++ site/frontend/views/albums/view.php
@@ -0,0 +1,231 @@
+<div id="user">
+
+    <div class="main">
+        <div class="main-in">
+
+            <div class="content-title-new">
+                Альбом «Зимние каникулы» <a href="#albumSettings" class="settings tooltip fancy" title="Настройки альбома"></a>
+                <span>Хлопоты понемногу утихли а значит можно хорошо провести зимние выходные. В Славяногороске погода зимняя. </span>
+            </div>
+
+            <div id="gallery" class="nopadding">
+
+                <div class="gallery-photos-new cols-3 clearfix">
+
+                    <ul>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_01.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_02.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_03.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_04.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_05.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_06.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_07.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_08.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_09.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_10.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_11.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+                        <li>
+                            <div class="img">
+                                <a href="">
+                                    <img src="/images/example/gallery_album_img_12.jpg" />
+                                    <span class="btn">Посмотреть</span>
+                                </a>
+                                <div class="actions">
+                                    <a href="#photoPick.v2" class="edit fancy tooltip" title="Редактировать"></a>
+                                    <a href="" class="remove tooltip" title="Удалить"></a>
+                                </div>
+                            </div>
+                            <div class="item-title">Разнообразие десертов сицилийского стиля</div>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="side-left gallery-sidebar">
+
+        <div class="clearfix">
+            <div class="user-info">
+                <div class="ava female"></div>
+                <div class="details">
+                    <span class="icon-status status-online"></span>
+                    <a href="" class="username">Александр Богоявленский</a>
+                    <div class="user-fast-buttons">
+                        <a href="" class="add-friend"><span class="tip">Пригласить в друзья</span></a>
+                        <a href="" class="new-message"><span class="tip">Написать сообщение</span></a>
+                    </div>
+                    <div class="user-fast-nav">
+                        <ul>
+                            <a href="">Анкета</a> &nbsp;|&nbsp; <a href="">Блог</a> &nbsp;|&nbsp; <span class="drp-list"><a href="" class="more">Еще</a><ul><li><a href="">Семья</a></li><li><a href="">Друзья</a></li></ul>
+                                            </span>
+                        </ul>
+                    </div>
+                </div>
+                <div class="text-status">
+                    <p>Привет всем! У меня все ok! Единственное, что имеет значение.</p>
+                    <span class="tale"></span>
+                </div>
+            </div>
+        </div>
+
+        <div class="upload-photo-btn">
+            <a href=""><img src="/images/btn_upload_photo.png" /></a>
+        </div>
+
+        <div class="club-topics-list-new">
+
+            <div class="block-title">Мои альбомы</div>
+
+            <ul>
+                <li><span><a href="">Все альбомы</a></span></li>
+                <li class="active"><span><a href="">Зимние каникулы</a></span><div class="count">15</div></li>
+                <li><span><a href="">Фотография</a></span><div class="count">15</div></li>
+                <li><span><a href="">Прикольное видео и другие фишки</a></span><div class="count">15</div></li>
+                <li><span><a href="">Школа восточного танца</a></span><div class="count">15</div></li>
+                <li><span><a href="">Футбол</a></span><div class="count">15</div></li>
+                <li><span><a href="">Детские передачи</a></span><div class="count">15</div></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>

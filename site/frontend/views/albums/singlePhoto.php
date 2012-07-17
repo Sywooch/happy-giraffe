cf9e80e29769620725b35b3bf30ea75dcab9bc48
ChooJoy <choojoy.work@gmail.com>
2012-07-17 15:46:44 +0400

    albums

--- site/frontend/views/albums/singlePhoto.php
+++ site/frontend/views/albums/singlePhoto.php
@@ -7,6 +7,72 @@
     ));
 ?>
 
+<div id="photo-inline" itemscope itemtype="http://schema.org/ImageObject">
+
+    <div class="meta">
+
+        <div class="clearfix">
+
+            <div class="count">
+                <?=$currentIndex?> фото из <?=count($collection['photos'])?> <a href="javascript:void(0)" class="btn btn-green-smedium" data-id="<?=$photo->id?>"><span><span>Смотреть весь альбом</span></span></a>
+            </div>
+
+            <div class="album-title">
+                <?=$collection['title']?>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="title"><h1 itemprop="name"><?=$photo->w_title?></h1></div>
+
+    <div class="img">
+
+        <div class="user clearfix">
+
+            <?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array(
+                'user' => $photo->author,
+                'size' => 'small',
+                'sendButton' => false,
+                'location' => false
+            )); ?>
+
+        </div>
+
+        <?=CHtml::image($photo->getPreviewUrl(960, 627, Image::HEIGHT, true), '', array('itemprop' => 'contentURL'))?>
+
+        <meta itemprop="width" content="250 px">
+        <meta itemprop="height" content="250 px">
+
+    </div>
+
+    <div class="photo-comment" itemprop="description"><?=$photo->w_description?></div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div id="photo-inline">
 
     <div class="meta">

72b7b8e5fad0cc1a1b10eea7c5db01cf4eccbe32
ChooJoy <choojoy.work@gmail.com>
2012-07-17 13:49:04 +0400

    recipes feed

--- site/frontend/modules/cook/controllers/RecipeController.php
+++ site/frontend/modules/cook/controllers/RecipeController.php
@@ -283,8 +283,8 @@ class RecipeController extends HController
     {
         header("Content-type: text/xml; charset=utf-8");
         $feed = Yii::app()->cache->get('recipesFeed');
-        if ($feed === false || true) {
-            $recipes = CookRecipe::model()->with('cuisine', 'author', 'ingredients.ingredient', 'ingredients.unit')->findAll(array('order' => 'created DESC', 'limit' => 1));
+        if ($feed === false) {
+            $recipes = CookRecipe::model()->with('cuisine', 'author', 'ingredients.ingredient', 'ingredients.unit')->findAll(array('order' => 'created DESC'));
 
             $xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><entities/>');
 
@@ -321,7 +321,7 @@ class RecipeController extends HController
                     $nutrition->addChild('value', $value);
                 }
 
-                $recipe->addChild('instructions', html_entity_decode(strip_tags($r->text)));
+                $recipe->addChild('instructions', html_entity_decode(strip_tags($r->text), ENT_COMPAT, 'utf-8'));
                 $recipe->addChild('calorie', $r->nutritionals['total']['nutritionals'][1] . ' ккал');
                 $recipe->addChild('weight', $r->nutritionals['total']['weight'] . ' г');
                 if ($r->mainPhoto !== null) {

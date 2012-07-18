bbd3598157c46fb8270793053ee94215af4caf29
ChooJoy <choojoy.work@gmail.com>
2012-07-18 10:19:06 +0400

    login bug

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -19,8 +19,8 @@ return array(
         'js_dynamics/<hash:\w+>.js' => 'site/seoHide',
         'search' => 'site/search',
         'site/rememberPassword/step/<step:\d+>' => 'site/rememberPassword',
-        'site/login/<service:(mailru|odnoklassniki|vkontakte|facebook|)>' => 'site/login',
-        'site/<_a:(logout|link|test|users)>' => 'site/<_a>',
+        'site/login/<service:(mailru|odnoklassniki|vkontakte|facebook)>' => 'site/login',
+        'site/<_a:(login|logout|link|test|users)>' => 'site/<_a>',
         'contest' => 'site/contest',
 
         // ajax controller

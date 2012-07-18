a4b5bf5f5f38e017ef9d85aad090cee466524541
ChooJoy <choojoy.work@gmail.com>
2012-07-18 20:42:35 +0400

    albums

--- site/frontend/widgets/photoView/photoViewWidget.php
+++ site/frontend/widgets/photoView/photoViewWidget.php
@@ -27,7 +27,7 @@ class photoViewWidget extends CWidget
         );
 
         Yii::app()->clientScript->registerScriptFile('/javascripts/history.js');
-        Yii::app()->clientScript->registerScriptFile('/javascripts/gallery.js?r=' . time());
+        Yii::app()->clientScript->registerScriptFile('/javascripts/gallery.js');
 
         $report = $this->beginWidget('site.frontend.widgets.reportWidget.ReportWidget');
         $report->registerScripts();

e2146e44754d4b8326ba02115564a207d304eb91
ChooJoy <choojoy.work@gmail.com>
2012-07-19 19:27:25 +0400

    fixes

--- site/frontend/views/albums/singlePhoto.php
+++ site/frontend/views/albums/singlePhoto.php
@@ -54,3 +54,19 @@
     <?php endif; ?>
 
 </div>
+
+<?php $this->widget('site.frontend.widgets.socialLike.SocialLikeWidget', array(
+        'title' => 'Вам понравилось фото?',
+        'notice' => '<big>Рейтинг фото</big><p>Он показывает, насколько нравится ваше фото другим пользователям. Если фото интересное, то пользователи его смотрят, комментируют, увеличивают лайки социальных сетей.</p>',
+        'model' => $photo,
+        'type' => 'simple',
+        'options' => array(
+            'title' => CHtml::encode($photo->w_title),
+            'image' => $photo->getPreviewUrl(180, 180),
+            'description' => $photo->w_description,
+        ),
+)); ?>
+
+<?php $this->widget('site.frontend.widgets.commentWidget.CommentWidget', array(
+    'model' => $photo,
+)); ?>

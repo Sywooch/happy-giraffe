953504775483beb1ef6f2313a57381415c2a0f6f
ChooJoy <choojoy.work@gmail.com>
2012-07-20 17:06:23 +0400

    fixes

--- site/frontend/views/albums/postLoad.php
+++ site/frontend/views/albums/postLoad.php
@@ -15,8 +15,8 @@
     prev : <?=($i != 0) ? $photos[$i - 1]->id : 'null'?>,
     next : <?=($i < $count - 1) ? $photos[$i + 1]->id : 'null'?>,
     src : '<?php echo $p->getPreviewUrl(960, 627, Image::HEIGHT, true); ?>',
-    title : <?=($p->w_title === null) ? 'null' : '\'' . $p->w_title . '\''?>,
-    description : <?=($p->w_description === null) ? 'null' : '\'' . $p->w_description . '\''?>,
+    title : <?=($p->w_title === null) ? 'null' : '\'' . CJavaScript::quote($p->w_title) . '\''?>,
+    description : <?=($p->w_description === null) ? 'null' : '\'' . CJavaScript::quote($p->w_description) . '\''?>,
     avatar : '<?php
         $this->widget('application.widgets.avatarWidget.AvatarWidget', array(
             'user' => $p->author,

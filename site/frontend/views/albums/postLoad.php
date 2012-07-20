2c4e0335999a4321cdb123ba1f79ae2a91a91719
ChooJoy <choojoy.work@gmail.com>
2012-07-20 12:45:36 +0400

    fixes

--- site/frontend/views/albums/postLoad.php
+++ site/frontend/views/albums/postLoad.php
@@ -4,7 +4,7 @@
      * Date: 20.07.2012
      */
     $collection = $model->photoCollection;
-    echo count($collection['photos']);
+    var_dump($model);
     $photos = $collection['photos'];
     $count = count($photos);
 ?>

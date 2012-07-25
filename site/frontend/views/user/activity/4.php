e637bf08c68728cc12596884e4156298701277a3
ChooJoy <choojoy.work@gmail.com>
2012-07-25 19:56:56 +0400

    fixes

---
+++ site/frontend/views/user/activity/4.php
@@ -0,0 +1,44 @@
+<?php
+    $interestsIds = array();
+    foreach ($action->data as $interest)
+        $interestsIds[] = $interest['id'];
+
+    $criteria = new CDbCriteria(array(
+        'with' => array(
+            'interests' => array(
+                'with' => 'usersCount',
+                'join' => 'JOIN interest__users_interests ON interest__users_interests.interest_id = interests.id',
+            ),
+        ),
+    ));
+    $criteria->addInCondition('interests.id', $interestsIds);
+
+    $categories = InterestCategory::model()->findAll($criteria);
+?>
+
+<div class="box user-interests-box list-item">
+    <div class="user-interests">
+
+        <div class="box-title">Добавил интересы</div>
+
+        <ul>
+            <?php foreach ($categories as $category): ?>
+            <li>
+                <div class="interest-cat">
+                    <a href="">
+                        <span class="img"><img src="/images/interest_icon_<?=$category->id?>.png" /></span>
+                        <span class="text"><?=$category->title?></span>
+                    </a>
+                </div>
+                <ul>
+                    <?php foreach ($category->interests as $interest): ?>
+                    <li><a class="interest"><?=$interest->title?><span><?=$interest->usersCount?></span></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </li>
+            <?php endforeach; ?>
+
+        </ul>
+
+    </div>
+</div>

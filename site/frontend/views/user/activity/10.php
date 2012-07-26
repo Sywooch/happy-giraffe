0cebd07ce28fdb38f6d9451947391eac3c7fd140
ChooJoy <choojoy.work@gmail.com>
2012-07-26 08:44:18 +0400

    user activity

--- site/frontend/views/user/activity/10.php
+++ site/frontend/views/user/activity/10.php
@@ -1,5 +1,47 @@
 <?php
-/**
- * Author: choo
- * Date: 25.07.2012
- */
+    $model = CActiveRecord::model($action->data['entity'])->findByPk($action->data['entity_id']);
+?>
+
+<?php if ($model !== null): ?>
+    <div class="user-family list-item">
+        <div class="box-title">Добавил в семью</div>
+        <div class="t"></div>
+        <div class="c">
+            <ul>
+                <?php if (get_class($model) == 'UserPartner'): ?>
+                    <li>
+                        <big><?=$model->name?> &nbsp; <small><?=$this->user->getPartnerTitle($this->user->relationship_status)?></small></big>
+                        <?php if (! empty($model->notice)): ?>
+                            <div class="comment purple">
+                                <?= $model->notice ?>
+                                <span class="tale"></span>
+                            </div>
+                        <?php endif; ?>
+                        <?php if (count($model->photos) != 0): ?>
+                            <div class="img">
+                                <img src="<?php echo $model->getRandomPhotoUrl() ?>">
+                            </div>
+                        <?php endif; ?>
+                    </li>
+                <?php elseif (get_class($model) == 'Baby'): ?>
+                    <li>
+                        <big><?=$model->name?><span><?php if (!empty($model->birthday)) echo ', '.$model->getTextAge(false) ?></span></big>
+                        <?php if (! empty($model->notice)): ?>
+                        <div class="comment <?= ($model->sex == 1)?'blue':'pink' ?>">
+                            <?=$model->notice?>
+                            <span class="tale"></span>
+                        </div>
+                        <?php endif; ?>
+                        <?php if (count($model->photos) != 0): ?>
+                        <div class="img">
+                            <img src="<?php echo $model->getRandomPhotoUrl() ?>">
+                        </div>
+                        <?php endif; ?>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+        <div class="b"></div>
+        <a href="" class="read-more">Смотреть семейный альбом<i class="icon"></i></a>
+    </div>
+<?php endif; ?>

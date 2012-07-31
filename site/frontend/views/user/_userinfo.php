421ac08307d429ca77a880f46182dc12a075c112
ChooJoy <choojoy.work@gmail.com>
2012-07-31 18:21:20 +0400

    fix

--- site/frontend/views/user/_userinfo.php
+++ site/frontend/views/user/_userinfo.php
@@ -7,30 +7,29 @@
         ));
     ?>
     <div class="user-fast-nav">
-        <div class="user-fast-nav">
-            <ul class="clearfix">
-                <?php if ($this->uniqueId != 'user/profile'): ?>
-                <li><?=CHtml::link('Анкета', array('user/profile', 'user_id' => $user->id))?></li>
-                <?php endif; ?>
-                <?php if ($this->uniqueId != 'blog/list'): ?>
-                <li><?=CHtml::link('Блог', array('blog/list', 'user_id' => $user->id))?></li>
-                <?php endif; ?>
-                <?php if ($this->uniqueId != 'albums/user'): ?>
-                <li><?=CHtml::link('Фото', array('albums/user', 'id' => $user->id))?></li>
-                <?php endif; ?>
-                <?php if ($this->uniqueId != 'user/activity'): ?>
-                <li><?=CHtml::link('Что нового', array('user/activity', 'user_id' => $user->id))?></li>
-                <?php endif; ?>
-                <li>
-                    <span class="drp-list">
-                        <a href="javascript:void(0)" class="more" onclick="$(this).next().toggle(); return false;">Еще</a>
-                        <ul style="display: none;">
-                            <li><?=CHtml::link('Друзья', array('user/friends', 'user_id' => $user->id))?></li>
-                        </ul>
-                    </span>
-                </li>
-            </ul>
-        </div>
+        <ul class="clearfix">
+            <?php if ($this->uniqueId != 'user/profile'): ?>
+            <li><?=CHtml::link('Анкета', array('user/profile', 'user_id' => $user->id))?></li>
+            <?php endif; ?>
+            <?php if ($this->uniqueId != 'blog/list'): ?>
+            <li><?=CHtml::link('Блог', array('blog/list', 'user_id' => $user->id))?></li>
+            <?php endif; ?>
+            <?php if ($this->uniqueId != 'albums/user'): ?>
+            <li><?=CHtml::link('Фото', array('albums/user', 'id' => $user->id))?></li>
+            <?php endif; ?>
+            <?php if ($this->uniqueId != 'user/activity'): ?>
+            <li><?=CHtml::link('Что нового', array('user/activity', 'user_id' => $user->id))?></li>
+            <?php endif; ?>
+            <li>
+                <span class="drp-list">
+                    <a href="javascript:void(0)" class="more" onclick="$(this).next().toggle(); return false;">Еще</a>
+                    <ul style="display: none;">
+                        <li><?=CHtml::link('Друзья', array('user/friends', 'user_id' => $user->id))?></li>
+                        <li><?=CHtml::link('Клубы', array('user/clubs', 'user_id' => $user->id))?></li>
+                    </ul>
+                </span>
+            </li>
+        </ul>
     </div>
     <?php if ($user->status): ?>
     <div class="text-status">

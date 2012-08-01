74533d9ff0c520fffc78d470a3995cba942dcdf0
ChooJoy <choojoy.work@gmail.com>
2012-08-01 11:45:37 +0400

    fix

--- site/frontend/views/user/activity/3.php
+++ site/frontend/views/user/activity/3.php
@@ -1,6 +1,6 @@
 <div class="user-clubs clearfix list-item">
 
-    <?=$this->render('_activity_friend', array('user_id' => $action['user_id'], 'type' => $type))?>
+    <?=$this->render('activity/_activity_friend', array('user_id' => $action['user_id'], 'type' => $type))?>
 
     <div class="box-title">Вступил в клубы</div>
 
--- site/frontend/views/user/activity/5.php
+++ site/frontend/views/user/activity/5.php
@@ -18,7 +18,7 @@
 
 <?php if ($album !== null && ! empty($album->photos)): ?>
     <div class="user-albums list-item">
-        <?=$this->render('_activity_friend', array('user_id' => $action['user_id'], 'type' => $type))?>
+        <?=$this->render('activity/_activity_friend', array('user_id' => $action['user_id'], 'type' => $type))?>
 
         <div class="box-title">Добавил новые фото</div>
 

a9048baccc694cd75e7a6ddc5fa45f3fdbc77977
ChooJoy <choojoy.work@gmail.com>
2012-08-03 12:19:54 +0400

    verification

--- site/frontend/modules/calendar/views/default/index.php
+++ site/frontend/modules/calendar/views/default/index.php
@@ -24,7 +24,7 @@
         $doc = phpQuery::newDocumentXHTML($period->text, $charset = 'utf-8');
         $paragraphs = $doc->find('p');
         $paragraphsCount = count($paragraphs);
-        $pI = ($period->calendar == 0) ? floor($paragraphsCount / 3) : floor($paragraphsCount / 4);
+        $pI = ($period->calendar == 0) ? floor($paragraphsCount / 3) : floor($paragraphsCount / 2);
         $p = $doc->find('p:eq(' . $pI . ')');
         $p->after($features);
         $period->text = $doc->html();

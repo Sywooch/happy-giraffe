0a4434865b1268ef91232bbb3fa65bed2129fe8a
Kireev Alex <alexk984@gmail.com>
2012-08-03 17:13:17 +0400

    fixes

--- site/frontend/widgets/home/views/BlogWidget.php
+++ site/frontend/widgets/home/views/BlogWidget.php
@@ -4,24 +4,26 @@
 
     <ul>
         <?php foreach ($contents as $c): ?>
-            <li>
-                <div class="clearfix">
-                    <?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array(
-                        'user' => $c->contentAuthor,
-                        'size' => 'small',
-                        'sendButton' => false,
-                        'location' => false,
-                    )); ?>
-                </div>
-                <b><?=CHtml::link($c->title, $c->url)?></b>
+        <li>
+            <div class="clearfix">
+                <?php $this->widget('application.widgets.avatarWidget.AvatarWidget', array(
+                'user' => $c->contentAuthor,
+                'size' => 'small',
+                'sendButton' => false,
+                'location' => false,
+            )); ?>
+            </div>
+            <b><?=CHtml::link($c->title, $c->url)?></b>
+            <div class="img">
                 <?php
                 $image = $c->getContentImage();
                 if ($image)
                     echo CHtml::link(CHtml::image($image, $c->title), $c->url);
                 else
-                    echo '<br><p>'.$c->getContentText(300).'</p>';
+                    echo '<p>' . $c->getContentText(250) . '</p>';
                 ?>
-            </li>
+            </div>
+        </li>
         <?php endforeach; ?>
     </ul>
 

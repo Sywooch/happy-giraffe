24f29ea6717535464ddfd4635ffd5239a2339ca1
Kireev Alex <alexk984@gmail.com>
2012-08-03 17:01:43 +0400

    fixes

--- site/frontend/widgets/home/views/BlogWidget.php
+++ site/frontend/widgets/home/views/BlogWidget.php
@@ -17,7 +17,7 @@
                 <?php
                 $image = $c->getContentImage();
                 if ($image)
-                    echo CHtml::link(CHtml::image($c, $c->title), $c->url);
+                    echo CHtml::link(CHtml::image($image, $c->title), $c->url);
                 else
                     echo  $c->getContentText(250);
                 ?>

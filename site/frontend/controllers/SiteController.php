34285a0f1bd1f13ae0bde6b43f8579c7ffb236e9
Kireev Alex <alexk984@gmail.com>
2012-08-04 11:19:54 +0400

    fix

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -408,7 +408,14 @@ class SiteController extends HController
     }
 
     public function actionTest2(){
-        $this->widget('site.common.widgets.mail.NewMessagesWidget', array('user'=>Yii::app()->user->model));
+        $unread = Im::model(Yii::app()->user->id)->getUnreadMessagesCount();
+        $dialogUsers = Im::model(Yii::app()->user->id)->getUsersWithNewMessages();
+
+        $this->renderFile(Yii::getPathOfAlias('site.common.tpl.newMessages').'.php', array(
+            'user'=>Yii::app()->user->model,
+            'unread'=>$unread,
+            'dialogUsers'=>$dialogUsers,
+        ));
 //        $articles = Favourites::model()->getWeekPosts();
 //        $this->renderFile(Yii::getPathOfAlias('site.common.tpl.weeklyNews').'.php', array('models'=>$articles));
     }

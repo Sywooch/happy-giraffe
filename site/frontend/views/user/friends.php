dd4f85d9b2868fe285b70f8ff0eaa7c51a1fb5b2
Kireev Alex <alexk984@gmail.com>
2012-08-06 17:44:16 +0400

    fixes

--- site/frontend/views/user/friends.php
+++ site/frontend/views/user/friends.php
@@ -9,6 +9,8 @@
     <div class="content-title-new"><?=($this->user->id == Yii::app()->user->id) ? 'Мои друзья' : 'Друзья'?></div>
 
     <?php
+    if ($dataProvider->itemCount >= 6 && $show == 'all' || $this->user->id !== Yii::app()->user->id):?>
+        <?php
         $this->widget('zii.widgets.CListView', array(
             'id' => 'friends',
             'ajaxUpdate' => true,
@@ -28,7 +30,57 @@
                 'class' => 'MyLinkPager',
                 'header' => '',
             ),
-        ));
-    ?>
+        ));?>
+        <?php elseif ($dataProvider->itemCount == 0):?>
+        <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/javascripts/wantToChat.js'); ?>
+        <div class="friends clearfix">
+            <ul>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+            </ul>
+        </div>
+        <div class="friends-total empty">
+            <span>У вас пока нет друзей.</span>
+            <a href="<?=$this->createUrl('/activity/friends/') ?>">Найти ещё друзей</a>
+            <a href="" class="wannachat" onclick="WantToChat.send(this); return false;">Хочу общаться!</a>
+        </div>
+        <?php else: ?>
+        <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/javascripts/wantToChat.js'); ?>
+        <div class="friends clearfix">
+            <ul>
+                <?php foreach ($dataProvider->data as $friend): ?>
+                <?php $this->renderPartial('_friend', array('data' => $friend)); ?>
+                <?php endforeach; ?>
+                <?php for ($i = $dataProvider->itemCount; $i < 6; $i++) { ?>
+                <li class="empty">
+                    <div class="img"></div>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
+        <div class="friends-total">
 
+            <span>У вас пока только <?=$dataProvider->itemCount . ' ' . HDate::GenerateNoun(array('друг', 'друга', 'друзей'), $dataProvider->itemCount) ?>.</span>
+
+            <a href="<?=$this->createUrl('/activity/friends/') ?>">Найти ещё друзей</a>
+            <a href="" class="wannachat" onclick="WantToChat.send(this); return false;">Хочу общаться!</a>
+
+        </div>
+        <?php   endif; ?>
 </div>

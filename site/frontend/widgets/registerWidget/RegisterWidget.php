8dc1cad9e5263718d43522b0c40aea8e7aa097de
Kireev Alex <alexk984@gmail.com>
2012-08-08 16:48:36 +0400

    horoscope register

--- site/frontend/widgets/registerWidget/RegisterWidget.php
+++ site/frontend/widgets/registerWidget/RegisterWidget.php
@@ -3,23 +3,56 @@ class RegisterWidget extends CWidget
 {
     public $show_form = false;
     public $odnoklassniki = false;
+    public $type = 'default';
+
+    public $template = array(
+        'default' => array(
+            'step1' => array(
+                'title1' => 'Регистрация на Веселом Жирафе',
+                'title2' => 'Стань полноправным участником сайта за 1 минуту!',
+            ),
+            'step2' => array(
+                'title1' => 'Вы уже почти с нами!',
+                'title2' => 'Осталось ввести ваши имя, фамилию, пол и пароль',
+            ),
+            'step3' => array(
+                'title1' => 'Вы готовим для вас личную страницу',
+            ),
+            'inputBirthday'=>false
+        ),
+
+        'horoscope' => array(
+            'step1' => array(
+                'title1' => 'Хочу гороскоп каждый день',
+                'title2' => 'Зарегистрируйтесь, чтобы получать гороскоп!',
+            ),
+            'step2' => array(
+                'title1' => 'Ваш гороскоп почти готов!',
+                'title2' => 'Осталось ввести ваши имя, фамилию, пол, дату рождения и пароль',
+            ),
+            'step3' => array(
+                'title1' => 'Вы готовим для вас гороскоп',
+            ),
+            'inputBirthday'=>true
+        ),
+    );
 
     public function run()
     {
         if (Yii::app()->user->isGuest) {
-            if (strpos(Yii::app()->getRequest()->urlReferrer, 'http://www.odnoklassniki.ru/') === 0) {
-                //чел пришел из одноклассников
-                $this->show_form = true;
+            if (Yii::app()->controller->uniqueId == 'services/horoscope/default'){
+                $this->type = 'horoscope';
+            }
+            elseif (strpos(Yii::app()->getRequest()->urlReferrer, 'http://www.odnoklassniki.ru/') === 0) {
                 $this->odnoklassniki = true;
-                Yii::app()->user->setState('redirectUrl', Yii::app()->request->getRequestUri());
             }
-            if (strpos(Yii::app()->getRequest()->urlReferrer, 'http://'.$_SERVER['HTTP_HOST']) === false) {
-                //чел пришел с другого сайта, предлагаем зарегаться
-                if (Yii::app()->controller->module->id == 'services/horoscope')
-                    $this->show_form = true;
-            }
+
             $model = new User;
-            $this->render('form', array('model' => $model, 'show_form' => $this->show_form, 'odnoklassniki'=>$this->odnoklassniki));
+            $this->render('form', array(
+                'model' => $model,
+                'odnoklassniki' => $this->odnoklassniki,
+                'type'=>$this->type
+            ));
         }
     }
 }

c0f1c2984e2dd839f084e4e35ab4822469f5ea3a
Kireev Alex <alexk984@gmail.com>
2012-08-13 17:42:33 +0400

    proxy refreshing

---
+++ site/seo/components/ProxyRefresher.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Author: alexk984
+ * Date: 13.08.12
+ */
+class ProxyRefresher
+{
+    public static function execute()
+    {
+        $str = file_get_contents('http://awmproxy.com/socks_proxy.txt?');
+        $i = 0;
+        if (strlen($str) > 10000) {
+
+            $proxies = explode("\n", $str);
+            $transaction = Yii::app()->db_seo->beginTransaction();
+
+            try {
+                foreach ($proxies as $proxy) {
+                    $proxy = trim($proxy);
+                    $model = Proxy::model()->find('value="' . $proxy . '"');
+                    if ($model === null) {
+                        $model = new Proxy;
+                        $model->value = $proxy;
+                        $model->save();
+                        $i++;
+                    }
+                }
+
+                $transaction->commit();
+            } catch (Exception $e) {
+                $transaction->rollback();
+                echo 'update proxy failed';
+            }
+        }
+
+        echo $i.' -new proxies';
+    }
+}

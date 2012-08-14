406e6f9e2dd4454a8ed8b61cf0e1fd36cedc9dd0
Kireev Alex <alexk984@gmail.com>
2012-08-14 13:16:14 +0400

    caching + optimization

--- site/frontend/widgets/activity/LiveWidget.php
+++ site/frontend/widgets/activity/LiveWidget.php
@@ -7,11 +7,24 @@ class LiveWidget extends CWidget
 {
     public function run()
     {
-        $live = CommunityContent::model()->full()->findAll(array(
-            'limit' => 5,
-            'order' => 'created DESC',
-            'condition' => 'type_id != 4',
-        ));
+        $criteria = new CDbCriteria;
+        $criteria->limit = 5;
+        $criteria->with = array('rubric' => array(
+            'select' => array('community_id', 'user_id'),
+            'condition'=>'user_id IS NOT NULL'
+        ), 'type' => array(
+            'select' => array('slug')
+        ), 'post' => array(
+            'select' => array('text')
+        ), 'author' => array(
+            'select' => array('id', 'first_name', 'last_name', 'online')
+        ),'video','travel');
+
+        $criteria->order='created DESC';
+        $criteria->select = array('t.id', 't.title', 't.type_id', 't.rubric_id', 't.author_id', 'created');
+        $criteria->condition = 'type_id != 4';
+
+        $live = CommunityContent::model()->findAll($criteria);
 
         $this->render('LiveWidget', compact('live'));
     }

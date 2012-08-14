8d9d76a86a253fa352e23f01e75c18662435c383
Kireev Alex <alexk984@gmail.com>
2012-08-14 16:17:11 +0400

    fixes

--- site/frontend/widgets/activity/LiveWidget.php
+++ site/frontend/widgets/activity/LiveWidget.php
@@ -9,18 +9,30 @@ class LiveWidget extends CWidget
     {
         $criteria = new CDbCriteria;
         $criteria->limit = 5;
-        $criteria->with = array('rubric' => array(
-            'select' => array('community_id', 'user_id'),
-            'condition'=>'user_id IS NOT NULL'
-        ), 'type' => array(
-            'select' => array('slug')
-        ), 'post' => array(
-            'select' => array('text')
-        ), 'contentAuthor' => array(
-            'select' => array('id', 'first_name', 'last_name', 'avatar_id', 'online', 'blocked', 'deleted')
-        ),'video','travel');
+        $criteria->with = array(
+            'rubric' => array(
+                'with' => array(
+                    'community' => array(
+                        'select' => 'id, title, position',
+                    )
+                ),
+                'select' => array('community_id', 'user_id'),
+                'condition' => 'user_id IS NOT NULL'
+            ),
+            'type' => array(
+                'select' => array('slug')
+            ),
+            'post' => array(
+                'select' => array('text')
+            ),
+            'contentAuthor' => array(
+                'select' => array('id', 'first_name', 'last_name', 'avatar_id', 'online', 'blocked', 'deleted')
+            ),
+            'video',
+            'travel'
+        );
 
-        $criteria->order='created DESC';
+        $criteria->order = 'created DESC';
         $criteria->select = array('t.id', 't.title', 't.type_id', 't.rubric_id', 't.author_id', 't.created');
         $criteria->condition = 'type_id != 4';
 

c2b8ef35757ac656ff7ad9a54bbb7cd6f17e28b1
Kireev Alex <alexk984@gmail.com>
2012-08-15 18:05:15 +0400

    fixes

--- site/frontend/components/HController.php
+++ site/frontend/components/HController.php
@@ -79,7 +79,8 @@ class HController extends CController
                 $('[hashString]').each(function(){
                     var key = $(this).attr('hashString');
                     if($(this).attr('hashType') == 'href'){
-                        $(this).attr('href', Base64.decode(seoHrefs[key]));
+                        if ((seoHrefs.indexOf(key) != -1)
+                            $(this).attr('href', Base64.decode(seoHrefs[key]));
                     }else{
                         $(this).replaceWith(Base64.decode(seoContent[key]));
                     }

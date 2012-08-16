7791e9bc7f3ef0dbc35a67b30efa36c0c2881a8a
Kireev Alex <alexk984@gmail.com>
2012-08-16 14:00:37 +0400

    fixes

---
+++ site/common/models/mongo/Service.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This is the model class for table "services".
+ *
+ * The followings are the available columns in table 'services':
+ * @property string $id
+ * @property string $title
+ * @property string $description
+ * @property string $url
+ *
+ * The followings are the available model relations:
+ * @property CalendarPeriodsServices[] $calendarPeriodsServices
+ */
+class Service extends HActiveRecord
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return Service the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'services';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('title, description, url', 'required'),
+            array('title, url', 'length', 'max'=>255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, title, description, url', 'safe', 'on'=>'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'calendarPeriodsServices' => array(self::HAS_MANY, 'CalendarPeriodsServices', 'service_id'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'title' => 'Название',
+            'description' => 'Описание',
+            'url' => 'Ссылка',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('id',$this->id,true);
+        $criteria->compare('title',$this->title,true);
+        $criteria->compare('description',$this->description,true);
+        $criteria->compare('url',$this->url,true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
+}

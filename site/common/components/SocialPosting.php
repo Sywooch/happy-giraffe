14ee934f83c0c6763b21e638b74681cb75056a4c
Kireev Alex <alexk984@gmail.com>
2012-08-22 17:16:40 +0400

    fix

--- site/common/components/SocialPosting.php
+++ site/common/components/SocialPosting.php
@@ -5,13 +5,25 @@
  */
 class SocialPosting
 {
-    const KEY = 'btRzsXd5ky9yCBzC6TDrW';
+    const APIKEY = 'btRzsXd5ky9yCBzC6TDrW';
     /**
      * @static
      * @param CommunityContent $model
      */
     public static function sendPost($model)
     {
-
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_USERPWD, 'email:password');
+        $data = array(
+            'apiKey' => self::APIKEY,
+            'message' => "this is a message",
+            //'socialNetworks' => array(12345),
+        );
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_URL, 'https://api.hootsuite.com/api/2/messages');
+        curl_exec($curl);
     }
 }

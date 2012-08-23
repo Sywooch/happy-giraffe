30cc6a7065091f6782ed160d749055171d0849c2
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-08-23 15:47:58 +0400

    menu fix

--- site/frontend/widgets/SeoContentWidget.php
+++ site/frontend/widgets/SeoContentWidget.php
@@ -14,7 +14,7 @@ class SeoContentWidget extends CWidget
     {
         $contents = ob_get_clean();
         $hashString = md5($contents);
-        echo CHtml::tag('span', array('data-type' => 'content', 'data-key' => md5($contents)));
+        echo CHtml::tag('span', array('data-type' => 'content', 'data-key' => md5($contents)), '');
         Yii::app()->controller->seoContent[$hashString] = base64_encode($contents);
     }
 }

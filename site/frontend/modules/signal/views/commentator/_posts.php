69204ab3cd29a143f2820098f899a89bac00c0fc
Kireev Alex <alexk984@gmail.com>
2012-08-24 17:09:50 +0400

    seo fix

---
+++ site/frontend/modules/signal/views/commentator/_posts.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this HController
+ * @var CommunityContent[] $posts
+ */
+?>
+
+1. Posts to comment
+<br>
+<?php foreach ($posts as $post): ?>
+<div>
+    <?=CHtml::link($post->title, $post->url, array('target'=>'_blank')) ?>
+</div>
+<?php endforeach; ?>

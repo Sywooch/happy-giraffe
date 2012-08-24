1e06332dd72e0ab69cabef0139b1a8e345e88c58
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-08-24 17:07:44 +0400

    im

--- site/frontend/modules/im/views/default/_contact.php
+++ site/frontend/modules/im/views/default/_contact.php
@@ -1,12 +1,12 @@
-<li class="active">
+<li>
 
-    <span class="ava small"></span>
+    <span class="ava small"><?=$contact->getAva('small') ? CHtml::image($contact->getAva('small')):''?></span>
 
     <div class="in">
 
-        <span class="icon-status status-online"></span>
+        <span class="icon-status status-<?=$contact->online == 1 ? 'online' : 'offline'?>"></span>
 
-        <span class="username">Александр Богоявленский</span>
+        <span class="username"><?=$contact->fullName?></span>
 
     </div>
 

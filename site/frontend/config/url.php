2c020499514940aca8ddb78a199551cb1e12e601
Kireev Alex <alexk984@gmail.com>
2012-09-02 22:04:22 +0400

    commentators fix

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -120,6 +120,7 @@ return array(
         '<_m:(geo|im|signal|scores|cook)>/' => '<_m>/default/index',
         '<_m:(geo|im|signal)>/<_a>' => '<_m>/default/<_a>',
         'commentator'=>'signal/commentator/index',
+        'commentator/statistic'=>'signal/commentator/statistic',
         'signal/commentator/<_a>'=>'signal/commentator/<_a>',
 
         //cook

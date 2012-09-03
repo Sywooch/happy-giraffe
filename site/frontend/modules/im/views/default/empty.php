935929d21191a7a18100dc407e2d0a13b3616a6f
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-09-03 14:48:16 +0400

    im fixes

--- site/frontend/modules/im/views/default/empty.php
+++ site/frontend/modules/im/views/default/empty.php
@@ -38,7 +38,7 @@
 <?php if ($friends): ?>
     <div class="friends clearfix">
 
-        <div class="block-title"><span>Друзья на сайте</span> <?=CHtml::link('Все друзья на сайте', 'javascript:void(0)', array('onclick' => 'Messages.openMessages(null, 3)'))?></div>
+        <div class="block-title"><span>Друзья на сайте</span> <?=CHtml::link('Все друзья на сайте', 'javascript:void(0)', array('onclick' => 'Messages.open(null, 3)'))?></div>
 
         <ul>
 

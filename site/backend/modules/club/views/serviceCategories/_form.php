214e3e0808e324ea5b54fcf399058ca6e2cb19d3
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-09-05 13:38:37 +0400

    family

--- site/backend/modules/club/views/serviceCategories/_form.php
+++ site/backend/modules/club/views/serviceCategories/_form.php
@@ -15,13 +15,6 @@
         <?php echo $form->error($model,'title'); ?>
     </div>
 
-    <div class="row">
-        <?php echo $form->labelEx($model, 'category_id'); ?>
-        <?php echo $form->dropDownList($model, 'category_id',
-        CHtml::listData(ServiceCategory::model()->findAll(), 'id', 'title')); ?>
-        <?php echo $form->error($model, 'category_id'); ?>
-    </div>
-
     <div class="row buttons">
         <input type="hidden" name="redirect_to" id="redirect_to" value="">
         <?= CHtml::submitButton('Сохранить'); ?>

68a37b964c2470c90634c4e13d26e7c7a25259e7
Kireev Alex <alexk984@gmail.com>
2012-09-07 17:28:49 +0400

    fixes

--- site/frontend/views/albums/index.php
+++ site/frontend/views/albums/index.php
@@ -69,7 +69,7 @@
 
                 <?php endforeach; ?>
 
-                <?php if ($model->author_id == Yii::app()->user->id) CHtml::link('', array('addPhoto'), array('class' => 'fancy btn-album-create'))?>
+                <?php if ($model->author_id == Yii::app()->user->id) echo CHtml::link('', array('addPhoto'), array('class' => 'fancy btn-album-create'))?>
 
             </div>
 

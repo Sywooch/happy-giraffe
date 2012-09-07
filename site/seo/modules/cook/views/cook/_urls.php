e7ee3f52b2bc92d09b975eebf4f946b002cd388e
Kireev Alex <alexk984@gmail.com>
2012-09-07 12:38:59 +0400

    fix

--- site/seo/modules/cook/views/cook/_urls.php
+++ site/seo/modules/cook/views/cook/_urls.php
@@ -1,4 +1,11 @@
 <?php foreach ($urls as $url): ?>
-<input name="urls[]" type="text" class="example" value="<?=$url->url?>" data-id="<?=$url->id?>"/><br/>
+<input name="urls[]" type="text" class="example" value="<?=$url->url?>" data-id="<?=$url->id?>" onkeyup="$(this).next().next().show().next().show();"/><br/>
 <?php endforeach; ?>
-<input name="urls[]" type="text" class="example"/><br/>
+<input name="urls[]" type="text" class="example" onkeyup="$(this).next().next().show().next().show();"/><br/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>
+<input name="urls[]" type="text" class="example" style="display: none;" onkeyup="$(this).next().next().show().next().show();"/><br style="display: none;"/>

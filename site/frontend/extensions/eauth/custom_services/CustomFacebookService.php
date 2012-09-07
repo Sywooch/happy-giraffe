8a7dd17be94d8bb1403bd912d73b557299233602
Kireev Alex <alexk984@gmail.com>
2012-09-07 18:59:57 +0400

    fixes

--- site/frontend/extensions/eauth/custom_services/CustomFacebookService.php
+++ site/frontend/extensions/eauth/custom_services/CustomFacebookService.php
@@ -12,5 +12,7 @@ class CustomFacebookService extends FacebookOAuthService
 	 */
 	protected function fetchAttributes() {
 		$this->attributes = (array) $this->makeSignedRequest('https://graph.facebook.com/me');
+        if(!isset($this->attributes['first_name']))
+            $this->attributes['first_name'] = $this->attributes['name'];
 	}
 }

77e7c95db98772b08e29274ef76fd8d42c29840b
Kireev Alex <alexk984@gmail.com>
2012-09-07 18:50:49 +0400

    fixes

--- site/frontend/extensions/eauth/custom_services/CustomOdnoklassnikiService.php
+++ site/frontend/extensions/eauth/custom_services/CustomOdnoklassnikiService.php
@@ -6,47 +6,40 @@
  * @license http://www.opensource.org/licenses/bsd-license.php
  */
 
-require_once dirname(dirname(__FILE__)) . '/services/OdnoklassnikiOAuthService.php';
+require_once dirname(dirname(__FILE__)).'/services/OdnoklassnikiOAuthService.php';
 
-class CustomOdnoklassnikiService extends OdnoklassnikiOAuthService
-{
+class CustomOdnoklassnikiService extends OdnoklassnikiOAuthService {
 
-    protected $scope = 'VALUABLE ACCESS';
-
-    protected function fetchAttributes()
-    {
+	protected $scope = 'VALUABLE ACCESS';
+	
+	protected function fetchAttributes() {
         parent::fetchAttributes();
         if ($this->scope == 'VALUABLE ACCESS')
             $this->getRealIdAndUrl();
-    }
+	}
 
     /**
      * Avable only if VALUABLE ACCESS is on
      * you should ask for enable this scope for odnoklassniki administration
      */
-    protected function getRealIdAndUrl()
-    {
-        $sig = strtolower(md5('application_key=' . $this->client_public . 'client_id=' . $this->client_id . 'format=JSONmethod=users.getCurrentUser' . md5($this->access_token . $this->client_secret)));
+    protected function getRealIdAndUrl() {
 
-        $info = $this->makeRequest('http://api.odnoklassniki.ru/fb.do', array(
+        $info = $this->makeSignedRequest('http://api.odnoklassniki.ru/fb.do', array(
             'query' => array(
-                'method' => 'users.getCurrentUser',
-                'sig' => $sig,
+                'method' => 'users.getInfo',
+                'uids' => $this->attributes['id'],
+                'fields' => 'url_profile',
                 'format' => 'JSON',
                 'application_key' => $this->client_public,
                 'client_id' => $this->client_id,
-                'access_token' => $this->access_token,
             ),
         ));
 
-        $this->attributes['id'] = $info->uid;
-        $this->attributes['name'] = $info->first_name . ' ' . $info->last_name;
-        $this->attributes['first_name'] = $info->first_name;
-        $this->attributes['last_name'] = $info->last_name;
-        if (isset($info->birthday))
-            $this->attributes['birthday'] = $info->birthday;
+        preg_match('/\d+\/{0,1}$/',$info[0]->url_profile, $matches);
+        $this->attributes['id'] = (int)$matches[0];
+        $this->attributes['url'] = $info[0]->url_profile;
 
-        if (isset($info->pic_1)) {
+        if (isset($info[0]->pic_1)) {
             $temp_file_name = md5(microtime()) . '.jpeg';
             $img = AlbumPhoto::model()->getTempPath() . $temp_file_name;
 
@@ -65,4 +58,26 @@ class CustomOdnoklassnikiService extends OdnoklassnikiOAuthService
             }
         }
     }
+    
+    public function wallPost($data)
+    {
+        $post = array(
+            'application_key'=> $this->client_public,
+            'method' => 'share.addLink',
+            'format' => 'JSON',
+        );
+        if(isset($data['link']))
+        {
+            $post['linkUrl'] = $data['link'];
+        }
+
+        if(isset($data['message']))
+        {
+            $post['comment'] = $data['message'];
+        }
+        $this->makeSignedRequest('http://api.odnoklassniki.ru/fb.do',array(
+            'query'=>$post
+        ));
+    }
+
 }

4321b6bfeb3bd057138cca6bad984d02a8f23e38
Kireev Alex <alexk984@gmail.com>
2012-09-10 16:54:40 +0400

    page view test

--- site/frontend/extensions/GoogleAnalytics.php
+++ site/frontend/extensions/GoogleAnalytics.php
@@ -110,7 +110,7 @@ class GoogleAnalytics {
             $params[] = $key.'='.$value;
         }
 		//compose the apiURL string
-        $apiUrl = 'https://www.google.com/analytics/feeds/data?ids='.$this->_profileId.'&start-date='.$this->_startDate.'&end-date='.$this->_endDate.'&'.implode('&', $params);
+        $apiUrl = 'https://www.google.com/analytics/feeds/data?ids='.$this->_profileId.'&start-date='.$this->_startDate.'&end-date='.$this->_endDate.'&'.implode('&', $params).'&project:happygirafferu';
         $results = array();
 		//call the API
 		$xml = $this->_callAPI($apiUrl);

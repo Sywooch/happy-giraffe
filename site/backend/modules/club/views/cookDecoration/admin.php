537d4ccc274a448f0ae191aa6d1f569cf4791a1c
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-09-17 19:04:53 +0400

    backend fixes

--- site/backend/modules/club/views/cookDecoration/admin.php
+++ site/backend/modules/club/views/cookDecoration/admin.php
@@ -11,8 +11,12 @@
 		    'value'=>'CHtml::image($data->photo->getPreviewUrl(70, 70))',
             'type'=>'raw'
 		),
-		'category.title',
 		'title',
+        'description',
+        array(
+            'value' => 'CHtml::link($data->photo->author->fullName, $data->photo->author->url)',
+            'type' => 'raw',
+        ),
 		'created',
 		array(
 			'class'=>'CButtonColumn',

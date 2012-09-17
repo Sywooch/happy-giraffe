4e59132b66693ad0b928365e8a75068957e97f0d
Kireev Alex <alexk984@gmail.com>
2012-09-17 12:34:37 +0400

    fix

--- site/seo/modules/externalLinks/views/forums/reports.php
+++ site/seo/modules/externalLinks/views/forums/reports.php
@@ -5,42 +5,17 @@
 ?>
 <div class="ext-links-add">
 
-    <div class="nav clearfix">
-        <?php
+    <?php $this->renderPartial('sub_menu')?>
 
-        $this->widget('zii.widgets.CMenu', array(
-            'items' => array(
-                array(
-                    'label' => 'Добавить',
-                    'url' => array('/externalLinks/sites/index')
-                ),
-                array(
-                    'label' => 'Отчеты',
-                    'url' => array('/externalLinks/sites/reports'),
-                ),
-            )));
-        ?>
-    </div>
 </div>
 
 <div class="seo-table">
     <div class="table-box table-grey">
         <table>
-            <colgroup>
-                <col>
-                <col width="300">
-                <col width="40">
-                <col width="100">
-                <col width="140">
-            </colgroup>
             <thead>
             <tr>
-                <th>Внешний сайт - адрес страницы</th>
+                <th>Форум - адрес страницы</th>
                 <th>Наш сайт - адрес статьи / сервиса</th>
-                <th class="ac"></th>
-                <th class="ac">Стоимость<br>
-                    <small>(руб.)</small>
-                </th>
                 <th class="ac">Дата,<br>исполнитель</th>
             </tr>
             </thead>
@@ -49,12 +24,6 @@
             <tr>
                 <td style="vertical-align: top;"><a href="<?=$model->url?>" target="_blank"><?=$model->getUrlWithEmphasizedHost() ?></a></td>
                 <td><a href="<?=$model->our_link?>" target="_blank"><?=$model->our_link?></a><br><?=$model->getPageTitle() ?></td>
-                <td class="ac">
-                    <?=$model->getLinkTypeText()?>
-                </td>
-                <td class="ac">
-                    <?=$model->getLinkPrice()?>
-                </td>
                 <td class="ac">
                     <span class="date"><?=Yii::app()->dateFormatter->format('d MMM yyyy',strtotime($model->created))?></span>
                     <?=$model->author->name ?>

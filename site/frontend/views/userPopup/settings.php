0abfdda60dbe8d9b49ccd5039165a80d63970ca9
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-09-20 14:07:01 +0400

    cook fix

--- site/frontend/views/userPopup/settings.php
+++ site/frontend/views/userPopup/settings.php
@@ -8,13 +8,13 @@
 
         <div class="nav">
             <ul>
-                <li><a href="">Личные данные</a></li>
-                <li><a href="">Социальные сети</a></li>
-                <li><a href="">Пароль</a></li>
+                <li><a href="javascript:void(0)" onclick="Settings.openTab(0)">Личные данные</a></li>
+                <li><a href="javascript:void(0)" onclick="Settings.openTab(1)">Социальные сети</a></li>
+                <li><a href="javascript:void(0)" onclick="Settings.openTab(2)">Пароль</a></li>
             </ul>
         </div>
 
-        <a href="" class="close">Закрыть настройки</a>
+        <a href="javascript:void(0)" onclick="Settings.close()" class="close">Закрыть настройки</a>
 
     </div>
 
@@ -22,7 +22,7 @@
 
         <span class="ava male"></span>
 
-        <div class="settings-in">
+        <div class="settings-in" style="display: none;">
 
             <div class="settings-form">
 
@@ -77,7 +77,7 @@
                     <div class="row-title">Дата рождения:</div>
 
                     <div class="row-elements">
-						
+
 							<span class="chzn-v2">
 								<select class="chzn w-1">
                                     <option>29</option>
@@ -158,6 +158,151 @@
                     </div>
 
                 </div>
+
+            </div>
+
+        </div>
+
+        <div class="settings-in" style="display: none;">
+
+            <div class="socials">
+
+                <p>Свяжите свой профиль с вашими аккаунтами на других сайтах. <br/>Это позволит входить на сайт, используя любой из привязанных аккаунтов.</p>
+
+                <div class="profiles-list">
+
+                    <div class="list-title clearfix">
+
+                        <div class="col col-1">Социальная сеть</div>
+                        <div class="col col-2">Имя</div>
+                        <div class="col col-3">Удалить профиль </div>
+
+                    </div>
+
+                    <ul>
+                        <li class="clearfix">
+                            <div class="col col-1"><span class="social-logo vkontakte"></span></div>
+                            <div class="col col-2"><a href="">Александр Кувыркин</a></div>
+                            <div class="col col-3"><a href="" class="btn-remove"><i class="icon"></i>Удалить профиль</a></div>
+                        </li>
+                        <li class="clearfix">
+                            <div class="col col-1"><span class="social-logo facebook"></span></div>
+                            <div class="col col-2"><a href="">Александр Кувыркин</a></div>
+                            <div class="col col-3"><a href="" class="btn-remove"><i class="icon"></i>Удалить профиль</a></div>
+                        </li>
+                        <li class="clearfix">
+                            <div class="col col-1"><span class="social-logo odnoklassniki"></span></div>
+                            <div class="col col-2"><a href="">Александр Кувыркин</a></div>
+                            <div class="col col-3"><a href="" class="btn-remove"><i class="icon"></i>Удалить профиль</a></div>
+                        </li>
+                        <li class="clearfix">
+                            <div class="col col-1"><span class="social-logo mailru"></span></div>
+                            <div class="col col-2"><a href="">Александр Кувыркин</a></div>
+                            <div class="col col-3"><a href="" class="btn-remove"><i class="icon"></i>Удалить профиль</a></div>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+                <div class="add-profile">
+
+                    <div class="block-title">Добавить профиль</div>
+
+                    <ul class="auth-services">
+                        <li class="auth-service mailru"><a href="" class="auth-link mailru"></a></li>
+                        <li class="auth-service odnoklassniki"><a href="" class="auth-link odnoklassniki"></a></li>
+                        <li class="auth-service vkontakte"><a href="" class="auth-link vkontakte"></a></li>
+                        <li class="auth-service facebook"><a href="" class="auth-link facebook"></a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="settings-in" style="display: none;">
+
+            <div class="settings-form password-form">
+
+                <?php $form = $this->beginWidget('CActiveForm', array(
+                    'id' => 'password-form',
+                    'enableAjaxValidation' => true,
+                    'enableClientValidation' => true,
+                    'action' => array('userPopup/changePassword'),
+                    'htmlOptions' => array(
+                        'onsubmit' => 'Settings.changePassword(this); return false;',
+                    ),
+                ));?>
+
+                    <div class="row clearfix">
+
+                        <div class="row-title">Текущий пароль:</div>
+
+                        <div class="row-elements">
+                            <?=$form->passwordField($model, 'current_password', array('class' => 'input-big'))?><br/>
+                            <?=$form->error($model, 'current_password')?>
+                        </div>
+
+                    </div>
+
+                    <div class="row clearfix">
+
+                        <div class="row-title">Новый пароль:</div>
+
+                        <div class="row-elements">
+                            <?=$form->passwordField($model, 'new_password', array('class' => 'input-big'))?><br/>
+                            <?=$form->error($model, 'new_password')?>
+                            <div class="small">Придумайте сложный пароль</div>
+                        </div>
+
+                    </div>
+
+                    <div class="row clearfix">
+
+                        <div class="row-title">Новый пароль<br/>еще раз:</div>
+
+                        <div class="row-elements">
+                            <?=$form->passwordField($model, 'new_password_repeat', array('class' => 'input-big'))?>
+                            <?=$form->error($model, 'new_password_repeat')?>
+                        </div>
+
+                    </div>
+
+                    <div class="row clearfix row-captcha">
+
+                        <div class="row-title">Код:</div>
+
+                        <div class="row-elements">
+
+                            <div class="col">
+                                <div class="captcha-img"><?php $this->widget('Captcha', array('showRefreshButton' => FALSE, 'selector' => '.refresh')); ?></div>
+                                <a href="javascript:void(0)" class="refresh">Обновить картинку<i class="icon"></i></a>
+                            </div>
+
+                            <div class="col">
+                                <?=$form->textField($model, 'verifyCode', array('class' => 'input-big'))?><br/>
+                                <div class="small">Введите цифры, которые Вы видите на картинке.</div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row clearfix row-captcha">
+
+                        <div class="row-title">&nbsp;</div>
+
+                        <div class="row-elements">
+
+                            <button class="btn-orange">Изменить</button>
+
+                        </div>
+
+                    </div>
+
+                <?php $this->endWidget(); ?>
 
             </div>
 
--- site/frontend/www/javascripts/settings.js
+++ site/frontend/www/javascripts/settings.js
@@ -9,11 +9,12 @@ Settings.open = function() {
         $('body').append('<div id="body-overlay"></div>');
         $('body').addClass('nav-fixed');
         $('#user-nav-settings').addClass('active');
+        Settings.openTab(0);
     });
 }
 
 Settings.close = function() {
-    $('#user-notifications').remove();
+    $('#user-settings').remove();
     $('body').css('overflow', '');
     $('#body-overlay').remove();
     $('body').removeClass('nav-fixed');
@@ -28,6 +29,18 @@ Settings.isActive = function() {
     return $('#user-settings:visible').length > 0;
 }
 
-Settings.switchTab = function() {
+Settings.openTab = function(index) {
+    $('#user-settings .nav ul li.active').removeClass('active');
+    $('#user-settings .settings-in:visible').hide();
+    $('#user-settings .nav ul li:eq(' + index + ')').addClass('active')
+    $('#user-settings .settings-in:eq(' + index + ')').show();
+}
 
+Settings.changePassword = function(form) {
+    $.post($(form).attr('action'), $(form).serialize(), function(data) {
+        if (data == 'true') {
+            $(form).find('input:text').val('');
+            $('.refresh').trigger('click');
+        }
+    });
 }

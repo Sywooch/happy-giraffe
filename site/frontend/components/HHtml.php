33288513ec1ba4dd82535208294ac8b3a5f092b9
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-09-27 18:36:40 +0400

    fixes

--- site/frontend/components/HHtml.php
+++ site/frontend/components/HHtml.php
@@ -7,14 +7,18 @@ class HHtml extends CHtml
 {
     public static function link($text, $url='#', $htmlOptions=array(), $seoHide = false)
     {
-        return Yii::app()->controller->renderDynamic(array('HHtml', 'renderLink'), $text, $url, $htmlOptions, $seoHide);
+        return ($seoHide && ! Yii::app()->request->isAjaxRequest && $url != '#') ?
+            Yii::app()->controller->renderDynamic(array('HHtml', 'renderLink'), $text, $url, $htmlOptions, $seoHide)
+            :
+            HHtml::renderLink($text, $url, $htmlOptions, $seoHide);
+        ;
     }
 
     public static function renderLink($text, $url='#', $htmlOptions=array(), $seoHide = false)
     {
         if ($url !== '') {
             $url = self::normalizeUrl($url);
-            if ($seoHide && !Yii::app()->request->isAjaxRequest && $url != '#') {
+            if ($seoHide && ! Yii::app()->request->isAjaxRequest && $url != '#') {
                 $hashString = md5($url);
                 $htmlOptions['data-key'] = $hashString;
                 $htmlOptions['data-type'] = 'href';

f8c2cb1ed5547e6fdd080ee5fe119b55b0fb922e
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-01 17:17:20 +0400

    fixes

--- site/frontend/views/blog/view.php
+++ site/frontend/views/blog/view.php
@@ -1,6 +1,8 @@
 <?php $this->renderPartial('/community/_post', array('data' => $data, 'full' => true)); ?>
 
+<?php if ($data->type_id != 5): ?>
 <?php $this->renderPartial('/community/_prev_next', array('data' => $data)); ?>
+<?php endif; ?>
 
 <?php $this->widget('application.widgets.commentWidget.CommentWidget', array(
     'model' => $data,

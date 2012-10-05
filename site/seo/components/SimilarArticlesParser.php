e3665e11ad4c818bfc1229f0002fc8b624108118
Kireev Alex <alexk984@gmail.com>
2012-10-05 16:29:48 +0400

    seo fixes

--- site/seo/components/SimilarArticlesParser.php
+++ site/seo/components/SimilarArticlesParser.php
@@ -14,7 +14,7 @@ class SimilarArticlesParser
     public function getArticles($keyword)
     {
         if ($this->startsWith($keyword, 'http://')) {
-            $content = $this->query('http://yandex.ru/sitesearch?text=site%3A' . urlencode($keyword) . '&searchid=1883818&reqenc=utf-8&l10n=ru&web=0&lr=38&numdoc=50');
+            $content = $this->query('http://yandex.ru/sitesearch?text=url%3A' . urlencode($keyword) . '*&searchid=1883818&reqenc=utf-8&l10n=ru&web=0&lr=38&numdoc=50');
         } else
             $content = $this->query('http://yandex.ru/sitesearch?text=' . urlencode($keyword) . '&searchid=1883818&reqenc=utf-8&l10n=ru&web=0&lr=38&numdoc=50');
 

40a46051bd33c6788eb5e1f2042cfcddfce04c55
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-08 17:40:50 +0400

    activity fix

--- site/frontend/widgets/activity/views/_live_entry.php
+++ site/frontend/widgets/activity/views/_live_entry.php
@@ -1,9 +1,7 @@
 <li>
     <div class="date"><?php echo Yii::app()->dateFormatter->format("d MMMM yyyy, H:mm", $data->created); ?></div>
     <div class="user">
-        <?php if ($data->author->id == User::HAPPY_GIRAFFE):?>
-            <span>Веселый Жираф</span>
-        <?php else: ?>
+        <?php if ($data->author->id != User::HAPPY_GIRAFFE):?>
             <span class="icon-status status-<?php echo $data->author->online == 1 ? 'online' : 'offline'; ?>"></span>
             <?=CHtml::link($data->author->fullName, $data->author->url)?>
         <?php endif ?>

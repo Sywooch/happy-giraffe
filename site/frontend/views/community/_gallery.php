aef7ca0ca372d92ea54c8829d22b4a00e439fc7d
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-10 19:07:20 +0400

    wysiwyg

--- site/frontend/views/community/_gallery.php
+++ site/frontend/views/community/_gallery.php
@@ -2,12 +2,12 @@
     <a class="img" data-id="<?=$data->gallery->items[0]->photo->id?>">
         <?php echo CHtml::image($data->gallery->items[0]->photo->getPreviewUrl(695, 463, Image::WIDTH)) ?>
 
-        <div class="title">
+        <span class="title">
             <?=CHtml::encode($data->gallery->title)?>
-        </div>
-        <div class="count">
+        </span>
+        <span class="count">
             смотреть <span><?=count($data->gallery->items)?> ФОТО</span>
-        </div>
+        </span>
         <i class="icon-play"></i>
     </a>
 </div>

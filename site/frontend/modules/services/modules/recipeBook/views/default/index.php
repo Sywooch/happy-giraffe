3ac824f31940996f7c3fc401f300820c475997c0
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-13 12:29:29 +0400

    wysiwyg

--- site/frontend/modules/services/modules/recipeBook/views/default/index.php
+++ site/frontend/modules/services/modules/recipeBook/views/default/index.php
@@ -1,7 +1,5 @@
 <?php
-    if (Yii::app()->request->getParam('Comment_page', 1) != 1) {
-        Yii::app()->clientScript->registerMetaTag('noindex', 'robots');
-    }
+    Yii::app()->clientScript->registerMetaTag('noindex', 'robots');
 
     $this->widget('zii.widgets.CListView', array(
         'ajaxUpdate' => false,

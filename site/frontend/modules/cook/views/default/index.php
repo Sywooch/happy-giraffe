c3c3a0bcdd1f01ebe5d3378f65d41fabba5202d0
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-13 12:49:28 +0400

    wysiwyg

--- site/frontend/modules/cook/views/default/index.php
+++ site/frontend/modules/cook/views/default/index.php
@@ -110,6 +110,15 @@
 
             </div>
 
+            <div class="recipe-banner recipe-banner-1">
+
+                <div class="block-title">
+                    <span>1500 рецептов<br/>для мультиварки</span>
+                    <a href="<?=$this->createUrl('/cook/recipe/index', array('section' => 1))?>">Готовим в мультиварке <i class="arr">&rarr;</i></a>
+                </div>
+
+            </div>
+
 
             <div class="dish-list">
 

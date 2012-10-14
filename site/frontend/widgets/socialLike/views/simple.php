f3ed7f570f7211973356c41ce172114197da0643
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-14 17:15:52 +0400

    contest

--- site/frontend/widgets/socialLike/views/simple.php
+++ site/frontend/widgets/socialLike/views/simple.php
@@ -1,6 +1,11 @@
 <?php
+
+$js = "
+    document.getElementById('vk_share_button').innerHTML = VK.Share.button(false,{type: 'round', text: 'Мне нравится'});
+";
+
 Yii::app()->clientScript
-    ->registerScriptFile('http://vk.com/js/api/share.js?11')
+    //->registerScriptFile('http://vk.com/js/api/share.js?11')
     //->registerScript('vk-init', "VK.init({apiId: " . Yii::app()->params['social']['vk']['api_id'] . ", onlyWidgets: true});", CClientScript::POS_HEAD)
     ->registerCssFile('http://stg.odnoklassniki.ru/share/odkl_share.css')
     ->registerScriptFile('http://stg.odnoklassniki.ru/share/odkl_share.js')
@@ -10,6 +15,7 @@ Yii::app()->clientScript
     ->registerMetaTag('article', null, null, array('property'=>'og:type') )
     ->registerMetaTag('Веселый Жираф', 'og:site_name')
     ->registerCss('vk_css', '#vk_like{width: 170px !important;}')
+    ->registerScript('vklike', $js);
 ;
 ?>
 <div class="like-block fast-like-block">
@@ -21,10 +27,8 @@ Yii::app()->clientScript
                     <td style="vertical-align:top;padding-right:0;text-align: left;">
                         <iframe src="//www.facebook.com/plugins/like.php?locale=ru_RU&amp;href=<?=urlencode('http://'.$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]) ?>&amp;send=false&amp;layout=button_count&amp;width=150&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>
                     </td>
-                    <td style="vertical-align:top;width: 150px;">
-                        <script type="text/javascript"><!--
-                        document.write(VK.Share.button(false,{type: "round", text: "Мне нравится"}));
-                        --></script>
+                    <td style="vertical-align:top;width: 150px;" id="vk_share_button">
+
                     </td>
                     <td style="vertical-align:top;padding-right:15px;text-align: left;">
                         <a class="odkl-klass-oc"

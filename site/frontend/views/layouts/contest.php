c389fe5ca176ad72429656d24fd655f05f529bbc
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-15 08:48:22 +0400

    contest

--- site/frontend/views/layouts/contest.php
+++ site/frontend/views/layouts/contest.php
@@ -11,10 +11,12 @@
 <div class="section-banner">
     <div class="contest-text">
         <div class="holder">
-            <img src="/images/contest/banner-mother-i-2.jpg" alt="" />
-            <div class="button-holder">
-                <a href="<?=$this->createUrl('/contest/default/statement', array('id' => $this->contest->id))?>" onclick="Contest.canParticipate(this, '<?=$this->createUrl('/contest/default/canParticipate', array('id' => $this->contest->id))?>'); return false;" class="btn-blue btn-blue-55">Участвовать!</a>
-            </div>
+            <img src="/images/contest/banner-mother-i-2.jpg" alt="<?=$this->contest->title?>" />
+            <?php if (Yii::app()->user->isGuest || Yii::app()->user->model->getContestWork($this->contest->id) === null): ?>
+                <div class="button-holder">
+                    <a href="<?=$this->createUrl('/contest/default/statement', array('id' => $this->contest->id))?>" onclick="Contest.canParticipate(this, '<?=$this->createUrl('/contest/default/canParticipate', array('id' => $this->contest->id))?>'); return false;" class="btn-blue btn-blue-55">Участвовать!</a>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
     <img src="/images/contest/banner-mother-i-1.jpg" />
@@ -54,6 +56,6 @@
 
 <script id="oopsTmpl" type="text/x-jquery-tmpl">
     <div class="contest-error-hint">
-        <h4>Oops!</h4><p>Что бы принять участие в конкурсе, вам нужно пройти <a href="<?=urldecode($this->createUrl('/user/profile', array('user_id' => '${id}', '#' => 'first-steps')))?>">Первые 6 шагов</a> в свой анкете </p>
+        <h4>Oops!</h4><p>Что бы принять участие в конкурсе, вам нужно пройти <a href="<?=urldecode($this->createUrl('/user/profile', array('user_id' => '${id}')))?>">первые 6 шагов</a> в свой анкете </p>
     </div>
 </script>

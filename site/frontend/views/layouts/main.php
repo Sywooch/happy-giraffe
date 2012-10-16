44e666b4a032acffe71ea3258f0f66a82c31cf36
Kireev Alex <alexk984@gmail.com>
2012-10-16 17:52:38 +0400

    rating facebook fix

--- site/frontend/views/layouts/main.php
+++ site/frontend/views/layouts/main.php
@@ -14,7 +14,7 @@
         <title><?=CHtml::encode($this->pageTitle)?></title>
     <?php endif;
 
-    $release_id = 339;
+    $release_id = 1;
     $cs = Yii::app()->clientScript;
     $cs
         ->registerPackage('comet')

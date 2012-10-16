5aca0bfec265ddf59d8e7b57eb34ab0bd539dc05
Kireev Alex <alexk984@gmail.com>
2012-10-16 17:51:46 +0400

    rating facebook fix

--- site/frontend/widgets/socialLike/views/simple.php
+++ site/frontend/widgets/socialLike/views/simple.php
@@ -16,7 +16,6 @@ Yii::app()->clientScript
 //->registerScript('vk-init', "VK.init({apiId: " . Yii::app()->params['social']['vk']['api_id'] . ", onlyWidgets: true});", CClientScript::POS_HEAD)
     ->registerCssFile('http://stg.odnoklassniki.ru/share/odkl_share.css')
     ->registerScriptFile('http://stg.odnoklassniki.ru/share/odkl_share.js')
-    ->registerScriptFile('//connect.facebook.net/ru_RU/all.js')
     ->registerMetaTag($this->options['title'], null, null, array('property' => 'og:title'))
     ->registerMetaTag($this->options['image'], null, null, array('property' => 'og:image'))
     ->registerMetaTag($this->options['description'], null, null, array('property' => 'og:description'))
@@ -32,10 +31,17 @@ Yii::app()->clientScript
             <table width="100%">
                 <tr>
                     <td style="vertical-align:top;padding-right:0;text-align: left;">
-                        <iframe src="//www.facebook.com/plugins/like.php?locale=ru_RU&amp;href=<?=urlencode($url) ?>&amp;send=false&amp;layout=button_count&amp;width=150&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21"
-                                scrolling="no" frameborder="0"
-                                style="border:none; overflow:hidden; width:150px; height:21px;"
-                                allowTransparency="true"></iframe>
+                        <div class="fb-custom-like">
+                            <a href="http://www.facebook.com/sharer/sharer.php?u=<?=urlencode($url) ?>"
+                               onclick="return Social.showFacebookPopup(this);" class="fb-custom-text">
+                                <i class="pluginButtonIcon img sp_like sx_like_fav"></i>Мне нравится</a>
+                            <div class="fb-custom-share-count">0</div>
+                            <script type="text/javascript">
+                                $.getJSON("http://graph.facebook.com", { id : document.location.href }, function(json){
+                                    $('.fb-custom-share-count').html(json.shares || '0');
+                                });
+                            </script>
+                        </div>
                     </td>
                     <td style="vertical-align:top;width: 150px;" class="vk_share_button">
 
@@ -65,11 +71,6 @@ Yii::app()->clientScript
             <script type="text/javascript">
                 $(function () {
                     //подписываемся на клик
-                    if (FB && FB.Event && FB.Event.subscribe)
-                        FB.Event.subscribe('edge.create', function (response) {
-                            console.log('update fb shares');
-                            Social.updateLikesCount('fb');
-                        });
                     if (VK && VK.Share && VK.Share.click) {
                         var oldShareClick = VK.Share.click;
                         VK.Share.click = function (index, el) {

18684b61b366beaf928fa7d8644f103b04dea307
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-17 10:28:26 +0400

    fix

--- site/console/commands/RatingCommand.php
+++ site/console/commands/RatingCommand.php
@@ -24,7 +24,7 @@ class RatingCommand extends CConsoleCommand
         }
     }
 
-    public function actionSync()
+    public function actionSync($social_key = null)
     {
         $models = ContestWork::model()->findAll();
         $count = count($models);
@@ -32,10 +32,14 @@ class RatingCommand extends CConsoleCommand
             $attach = AttachPhoto::model()->findByEntity('ContestWork', $model->id);
             $photo = $attach[0]->photo;
             $url = 'http://www.happy-giraffe.ru' . ltrim(Yii::app()->createUrl('albums/singlePhoto', array('entity' => 'Contest', 'contest_id' => $model->contest_id, 'photo_id' => $photo->id)), '.');
-            Rating::updateByApi($model, 'fb', $url);
-            Rating::updateByApi($model, 'tw', $url);
-            Rating::updateByApi($model, 'vk', $url);
-            Rating::updateByApi($model, 'ok', $url);
+            if ($social_key === null) {
+                Rating::updateByApi($model, 'fb', $url);
+                Rating::updateByApi($model, 'tw', $url);
+                Rating::updateByApi($model, 'vk', $url);
+                Rating::updateByApi($model, 'ok', $url);
+            } else {
+                Rating::updateByApi($model, $social_key, $url);
+            }
             echo ($i + 1) . '/' . $count . "\n";
         }
     }

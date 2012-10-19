649334e498a717cbb3b5b249adfa3683461aa0cd
Kireev Alex <alexk984@gmail.com>
2012-10-19 09:55:49 +0400

    mailru parsing

--- site/seo/modules/mailru/controllers/DefaultController.php
+++ site/seo/modules/mailru/controllers/DefaultController.php
@@ -4,10 +4,7 @@ class DefaultController extends SController
 {
 	public function actionIndex()
 	{
-        $parser = new MailRuUserParser;
-        //for($i=50;$i<90;$i++){
-            $parser->user = MailruUser::model()->findByPk(6);
-            $parser->parsePage();
-//        }
+//        $parser = new MailRuUserParser;
+//        $parser->start();
 	}
 }

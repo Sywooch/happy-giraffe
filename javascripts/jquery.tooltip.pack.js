760f9728b82db469b00178703be1800dcd940606
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-19 17:47:03 +0400

    fix

--- javascripts/jquery.tooltip.pack.js
+++ javascripts/jquery.tooltip.pack.js
@@ -1,4 +1,269 @@
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}(';(8($){j e={},9,m,B,A=$.2u.2g&&/29\\s(5\\.5|6\\.)/.1M(1H.2t),M=12;$.k={w:12,1h:{Z:25,r:12,1d:19,X:"",G:15,E:15,16:"k"},2s:8(){$.k.w=!$.k.w}};$.N.1v({k:8(a){a=$.1v({},$.k.1h,a);1q(a);g 2.F(8(){$.1j(2,"k",a);2.11=e.3.n("1g");2.13=2.m;$(2).24("m");2.22=""}).21(1e).1U(q).1S(q)},H:A?8(){g 2.F(8(){j b=$(2).n(\'Y\');4(b.1J(/^o\\(["\']?(.*\\.1I)["\']?\\)$/i)){b=1F.$1;$(2).n({\'Y\':\'1D\',\'1B\':"2r:2q.2m.2l(2j=19, 2i=2h, 1p=\'"+b+"\')"}).F(8(){j a=$(2).n(\'1o\');4(a!=\'2f\'&&a!=\'1u\')$(2).n(\'1o\',\'1u\')})}})}:8(){g 2},1l:A?8(){g 2.F(8(){$(2).n({\'1B\':\'\',Y:\'\'})})}:8(){g 2},1x:8(){g 2.F(8(){$(2)[$(2).D()?"l":"q"]()})},o:8(){g 2.1k(\'28\')||2.1k(\'1p\')}});8 1q(a){4(e.3)g;e.3=$(\'<t 16="\'+a.16+\'"><10></10><t 1i="f"></t><t 1i="o"></t></t>\').27(K.f).q();4($.N.L)e.3.L();e.m=$(\'10\',e.3);e.f=$(\'t.f\',e.3);e.o=$(\'t.o\',e.3)}8 7(a){g $.1j(a,"k")}8 1f(a){4(7(2).Z)B=26(l,7(2).Z);p l();M=!!7(2).M;$(K.f).23(\'W\',u);u(a)}8 1e(){4($.k.w||2==9||(!2.13&&!7(2).U))g;9=2;m=2.13;4(7(2).U){e.m.q();j a=7(2).U.1Z(2);4(a.1Y||a.1V){e.f.1c().T(a)}p{e.f.D(a)}e.f.l()}p 4(7(2).18){j b=m.1T(7(2).18);e.m.D(b.1R()).l();e.f.1c();1Q(j i=0,R;(R=b[i]);i++){4(i>0)e.f.T("<1P/>");e.f.T(R)}e.f.1x()}p{e.m.D(m).l();e.f.q()}4(7(2).1d&&$(2).o())e.o.D($(2).o().1O(\'1N://\',\'\')).l();p e.o.q();e.3.P(7(2).X);4(7(2).H)e.3.H();1f.1L(2,1K)}8 l(){B=S;4((!A||!$.N.L)&&7(9).r){4(e.3.I(":17"))e.3.Q().l().O(7(9).r,9.11);p e.3.I(\':1a\')?e.3.O(7(9).r,9.11):e.3.1G(7(9).r)}p{e.3.l()}u()}8 u(c){4($.k.w)g;4(c&&c.1W.1X=="1E"){g}4(!M&&e.3.I(":1a")){$(K.f).1b(\'W\',u)}4(9==S){$(K.f).1b(\'W\',u);g}e.3.V("z-14").V("z-1A");j b=e.3[0].1z;j a=e.3[0].1y;4(c){b=c.2o+7(9).E;a=c.2n+7(9).G;j d=\'1w\';4(7(9).2k){d=$(C).1r()-b;b=\'1w\'}e.3.n({E:b,14:d,G:a})}j v=z(),h=e.3[0];4(v.x+v.1s<h.1z+h.1n){b-=h.1n+20+7(9).E;e.3.n({E:b+\'1C\'}).P("z-14")}4(v.y+v.1t<h.1y+h.1m){a-=h.1m+20+7(9).G;e.3.n({G:a+\'1C\'}).P("z-1A")}}8 z(){g{x:$(C).2e(),y:$(C).2d(),1s:$(C).1r(),1t:$(C).2p()}}8 q(a){4($.k.w)g;4(B)2c(B);9=S;j b=7(2);8 J(){e.3.V(b.X).q().n("1g","")}4((!A||!$.N.L)&&b.r){4(e.3.I(\':17\'))e.3.Q().O(b.r,0,J);p e.3.Q().2b(b.r,J)}p J();4(7(2).H)e.3.1l()}})(2a);',62,155,'||this|parent|if|||settings|function|current||||||body|return|||var|tooltip|show|title|css|url|else|hide|fade||div|update||blocked|||viewport|IE|tID|window|html|left|each|top|fixPNG|is|complete|document|bgiframe|track|fn|fadeTo|addClass|stop|part|null|append|bodyHandler|removeClass|mousemove|extraClass|backgroundImage|delay|h3|tOpacity|false|tooltipText|right||id|animated|showBody|true|visible|unbind|empty|showURL|save|handle|opacity|defaults|class|data|attr|unfixPNG|offsetHeight|offsetWidth|position|src|createHelper|width|cx|cy|relative|extend|auto|hideWhenEmpty|offsetTop|offsetLeft|bottom|filter|px|none|OPTION|RegExp|fadeIn|navigator|png|match|arguments|apply|test|http|replace|br|for|shift|click|split|mouseout|jquery|target|tagName|nodeType|call||mouseover|alt|bind|removeAttr|200|setTimeout|appendTo|href|MSIE|jQuery|fadeOut|clearTimeout|scrollTop|scrollLeft|absolute|msie|crop|sizingMethod|enabled|positionLeft|AlphaImageLoader|Microsoft|pageY|pageX|height|DXImageTransform|progid|block|userAgent|browser'.split('|'),0,{}));
+/*
+ * jQuery Tooltip plugin 1.2
+ *
+ * http://bassistance.de/jquery-plugins/jquery-plugin-tooltip/
+ * http://docs.jquery.com/Plugins/Tooltip
+ *
+ * Copyright (c) 2006 - 2008 Jörn Zaefferer
+ *
+ * $Id$
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+;(function($) {
+
+    // the tooltip element
+    var helper = {},
+    // the current tooltipped element
+        current,
+    // the title of the current element, used for restoring
+        title,
+    // timeout id for delayed tooltips
+        tID,
+    // IE 5.5 or 6
+        IE = $.browser.msie && /MSIE\s(5\.5|6\.)/.test(navigator.userAgent),
+    // flag for mouse tracking
+        track = false;
+
+    $.tooltip = {
+        blocked: false,
+        defaults: {
+            delay: 200,
+            showURL: true,
+            extraClass: "",
+            top: 15,
+            left: 15,
+            id: "tooltip"
+        },
+        block: function() {
+            $.tooltip.blocked = !$.tooltip.blocked;
+        }
+    };
+
+    $.fn.extend({
+        tooltip: function(settings) {
+            settings = $.extend({}, $.tooltip.defaults, settings);
+            createHelper(settings);
+            return this.each(function() {
+                $.data(this, "tooltip-settings", settings);
+                // copy tooltip into its own expando and remove the title
+                this.tooltipText = this.title;
+                $(this).removeAttr("title");
+                // also remove alt attribute to prevent default tooltip in IE
+                this.alt = "";
+            })
+                .hover(save, hide)
+                .click(hide);
+        },
+        fixPNG: IE ? function() {
+            return this.each(function () {
+                var image = $(this).css('backgroundImage');
+                if (image.match(/^url\(["']?(.*\.png)["']?\)$/i)) {
+                    image = RegExp.$1;
+                    $(this).css({
+                        'backgroundImage': 'none',
+                        'filter': "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled=true, sizingMethod=crop, src='" + image + "')"
+                    }).each(function () {
+                            var position = $(this).css('position');
+                            if (position != 'absolute' && position != 'relative')
+                                $(this).css('position', 'relative');
+                        });
+                }
+            });
+        } : function() { return this; },
+        unfixPNG: IE ? function() {
+            return this.each(function () {
+                $(this).css({'filter': '', backgroundImage: ''});
+            });
+        } : function() { return this; },
+        hideWhenEmpty: function() {
+            return this.each(function() {
+                $(this)[ $(this).html() ? "show" : "hide" ]();
+            });
+        },
+        url: function() {
+            return this.attr('href') || this.attr('src');
+        }
+    });
+
+    function createHelper(settings) {
+        // there can be only one tooltip helper
+        if( helper.parent )
+            return;
+        // create the helper, h3 for title, div for url
+        helper.parent = $('<div id="' + settings.id + '"><h3></h3><div class="body"></div><div class="url"></div></div>')
+            // add to document
+            .appendTo(document.body)
+            // hide it at first
+            .hide();
+
+        // apply bgiframe if available
+        if ( $.fn.bgiframe )
+            helper.parent.bgiframe();
+
+        // save references to title and url elements
+        helper.title = $('h3', helper.parent);
+        helper.body = $('div.body', helper.parent);
+        helper.url = $('div.url', helper.parent);
+    }
+
+    function settings(element) {
+        return $.data(element, "tooltip-settings");
+    }
+
+    // main event handler to start showing tooltips
+    function handle(event) {
+        // show helper, either with timeout or on instant
+        if( settings(this).delay )
+            tID = setTimeout(show, settings(this).delay);
+        else
+            show();
+
+        // if selected, update the helper position when the mouse moves
+        track = !!settings(this).track;
+        $(document.body).bind('mousemove', update);
+
+        // update at least once
+        update(event);
+    }
+
+    // save elements title before the tooltip is displayed
+    function save() {
+        // if this is the current source, or it has no title (occurs with click event), stop
+        if ( $.tooltip.blocked || this == current || (!this.tooltipText && !settings(this).bodyHandler) )
+            return;
+
+        // save current
+        current = this;
+        title = this.tooltipText;
+
+        if ( settings(this).bodyHandler ) {
+            helper.title.hide();
+            var bodyContent = settings(this).bodyHandler.call(this);
+            if (bodyContent.nodeType || bodyContent.jquery) {
+                helper.body.empty().append(bodyContent)
+            } else {
+                helper.body.html( bodyContent );
+            }
+            helper.body.show();
+        } else if ( settings(this).showBody ) {
+            var parts = title.split(settings(this).showBody);
+            helper.title.html(parts.shift()).show();
+            helper.body.empty();
+            for(var i = 0, part; part = parts[i]; i++) {
+                if(i > 0)
+                    helper.body.append("<br/>");
+                helper.body.append(part);
+            }
+            helper.body.hideWhenEmpty();
+        } else {
+            helper.title.html(title).show();
+            helper.body.hide();
+        }
+
+        // if element has href or src, add and show it, otherwise hide it
+        if( settings(this).showURL && $(this).url() )
+            helper.url.html( $(this).url().replace('http://', '') ).show();
+        else
+            helper.url.hide();
+
+        // add an optional class for this tip
+        helper.parent.addClass(settings(this).extraClass);
+
+        // fix PNG background for IE
+        if (settings(this).fixPNG )
+            helper.parent.fixPNG();
+
+        handle.apply(this, arguments);
+    }
+
+    // delete timeout and show helper
+    function show() {
+        tID = null;
+        helper.parent.show();
+        update();
+    }
+
+    /**
+     * callback for mousemove
+     * updates the helper position
+     * removes itself when no current element
+     */
+    function update(event)  {
+        if($.tooltip.blocked)
+            return;
+
+        // stop updating when tracking is disabled and the tooltip is visible
+        if ( !track && helper.parent.is(":visible")) {
+            $(document.body).unbind('mousemove', update)
+        }
+
+        // if no current element is available, remove this listener
+        if( current == null ) {
+            $(document.body).unbind('mousemove', update);
+            return;
+        }
+
+        // remove position helper classes
+        helper.parent.removeClass("viewport-right").removeClass("viewport-bottom");
+
+        var left = helper.parent[0].offsetLeft;
+        var top = helper.parent[0].offsetTop;
+        if (event) {
+            // position the helper 15 pixel to bottom right, starting from mouse position
+            left = event.pageX + settings(current).left;
+            top = event.pageY + settings(current).top;
+            helper.parent.css({
+                left: left + 'px',
+                top: top + 'px'
+            });
+        }
+
+        var v = viewport(),
+            h = helper.parent[0];
+        // check horizontal position
+        if (v.x + v.cx < h.offsetLeft + h.offsetWidth) {
+            left -= h.offsetWidth + 20 + settings(current).left;
+            helper.parent.css({left: left + 'px'}).addClass("viewport-right");
+        }
+        // check vertical position
+        if (v.y + v.cy < h.offsetTop + h.offsetHeight) {
+            top -= h.offsetHeight + 20 + settings(current).top;
+            helper.parent.css({top: top + 'px'}).addClass("viewport-bottom");
+        }
+    }
+
+    function viewport() {
+        return {
+            x: $(window).scrollLeft(),
+            y: $(window).scrollTop(),
+            cx: $(window).width(),
+            cy: $(window).height()
+        };
+    }
+
+    // hide helper and restore added classes and the title
+    function hide(event) {
+        if($.tooltip.blocked)
+            return;
+        // clear timeout if possible
+        if(tID)
+            clearTimeout(tID);
+        // no more current element
+        current = null;
+
+        helper.parent.hide().removeClass( settings(this).extraClass );
+
+        if( settings(this).fixPNG )
+            helper.parent.unfixPNG();
+    }
+
+    $.fn.Tooltip = $.fn.tooltip;
+
+})(jQuery);
 
 function bindTooltips(){
     $('.tooltip').tooltip({

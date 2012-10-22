d85fb81afb99d1de87194addac59e35209162919
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-22 09:32:05 +0400

    noindex

--- site/frontend/widgets/socialLike/views/_yh_min.php
+++ site/frontend/widgets/socialLike/views/_yh_min.php
@@ -2,14 +2,19 @@
      <?php if (Yii::app()->user->isGuest):?>
          <a class="btn-icon heart heart2 fancy" href="#login" data-theme="white-square"></a>
      <?php else: ?>
-         <a class="btn-icon heart heart2<?=(! Yii::app()->user->isGuest && get_class($this->model) == 'ContestWork' && Yii::app()->user->model->scores->full != 2) ? ' yohoho_steps':''?><?php echo isset($this->model->author) && Yii::app()->user->id == $this->model->author->id ? ' yohoho_me ' : '' ?><?php echo RatingYohoho::model()->findByEntity($this->model) ? ' active' : ''; ?>" href="javascript:;" onclick="pushYohoho(this);"></a>
+         <a class="btn-icon heart heart2<?=(! Yii::app()->user->isGuest && get_class($this->model) == 'ContestWork' && Yii::app()->user->model->scores->full == 0) ? ' yohoho_steps':''?><?php echo isset($this->model->author) && Yii::app()->user->id == $this->model->author->id ? ' yohoho_me ' : '' ?><?php echo RatingYohoho::model()->findByEntity($this->model) ? ' active' : ''; ?>" href="javascript:;" onclick="pushYohoho(this);"></a>
+        <?php if (! Yii::app()->user->isGuest): ?>
+            <div class="contest-error-hint" style="display: none;">
+                <h4>Oops!</h4><p>Что бы принять участие в конкурсе, вам нужно пройти <a href="<?=$this->controller->createUrl('/user/profile', array('user_id' => Yii::app()->user->id))?>">первые 6 шагов</a> в свой анкете </p>
+            </div>
+        <?php endif; ?>
      <?php endif ?>
 
      <div class="count"><?php echo Rating::model()->countByEntity($this->model, 'yh') / 2; ?></div>
  </div>
 <script type="text/javascript">
     function pushYohoho(elem) {
-        <?php if(Yii::app()->user->isGuest || (!Yii::app()->user->isGuest && isset($this->model->author) && Yii::app()->user->id == $this->model->author->id)): ?>
+        <?php if(Yii::app()->user->isGuest || (! Yii::app()->user->isGuest && get_class($this->model) == 'ContestWork' && Yii::app()->user->model->scores->full == 0) || (!Yii::app()->user->isGuest && isset($this->model->author) && Yii::app()->user->id == $this->model->author->id)): ?>
             return false;
         <?php else: ?>
             if($(elem).hasClass("disabled"))

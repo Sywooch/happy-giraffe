c2b43dd6727f5049ff47c939b0bcad66c73ac605
Kireev Alex <alexk984@gmail.com>
2012-10-22 14:03:07 +0400

    community content rating fix

--- site/console/commands/UserScoresCommand.php
+++ site/console/commands/UserScoresCommand.php
@@ -151,10 +151,39 @@ class UserScoresCommand extends CConsoleCommand
                 $criteria->entity_name('==', 'ContestWork');
                 $model = Rating::model()->find($criteria);
                 if ($model !== null) {
-                    $model->ratings['yh'] = $count*2;
+                    $model->ratings['yh'] = $count * 2;
                     $model->save();
                 }
             }
+        }
+    }
+
+    public function actionCheckCommunity()
+    {
+        $models = Rating::model()->findAllByAttributes(array(
+            'entity_name' => 'CommunityContent',
+        ));
+
+        $i=0;
+        foreach ($models as $item) {
+            if (isset($item->ratings['fb']))
+                unset($item->ratings['fb']);
+            if (isset($item->ratings['vk']))
+                unset($item->ratings['vk']);
+            if (isset($item->ratings['tw']))
+                unset($item->ratings['tw']);
+            if (isset($item->ratings['ok']))
+                unset($item->ratings['ok']);
+            if (isset($item->ratings['mr']))
+                unset($item->ratings['mr']);
+            if (isset($item->ratings['gp']))
+                unset($item->ratings['gp']);
+
+            $item->save();
+
+            $i++;
+            if ($i % 10 == 0)
+                echo $i."\n";
         }
     }
 }

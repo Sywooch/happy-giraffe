866da28d25dcffed30a92c0ed109795cd3c405cc
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-24 10:42:32 +0400

    features

--- site/frontend/widgets/user/views/UserMoodWidget.php
+++ site/frontend/widgets/user/views/UserMoodWidget.php
@@ -69,12 +69,35 @@
         <?php endif; ?>
     </div>
     <?php if ($isMyProfile): ?>
-        <div class="moods-list">
-            <ul>
-                <?php $moods = UserMood::model()->findAll(); foreach($moods as $m): ?>
-                    <li><a href="" onclick="return updateMood(<?php echo $m->id; ?>)"><img src="/images/widget/mood/<?php echo $m->id; ?>.png" /><?php echo $m->title; ?></a></li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
+        <?php if ($this->user->hasFeature(4)): ?>
+            <div class="moods-list tabs">
+                <div class="moods-list-settings clearfix">
+                    <ul class="moods-list-settings-list">
+                        <li class=""><a onclick="setTab(this, 1);" href="javascript:void(0);">Обычные</a></li>
+                        <li class="new active"><a onclick="setTab(this, 2);" href="javascript:void(0);">Новые</a></li>
+                    </ul>
+                    <div class="slogan" style="display: block; ">Новые уровни - новые смайлы! Вперед!</div>
+                </div>
+
+                <ul class="tab-box tab-box-1" style="display: none; ">
+                    <?php $moods = UserMood::model()->findAll('id <= 35'); foreach($moods as $m): ?>
+                        <li><a href="" onclick="return updateMood(<?php echo $m->id; ?>)"><img src="/images/widget/mood/<?php echo $m->id; ?>.png" /><?php echo $m->title; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+                <ul class="tab-box tab-box-2" style="display: block; ">
+                    <?php $moods = UserMood::model()->findAll('id > 35'); foreach($moods as $m): ?>
+                        <li><a href="" onclick="return updateMood(<?php echo $m->id; ?>)"><img src="/images/widget/mood/<?php echo $m->id; ?>.png" /><?php echo $m->title; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php else: ?>
+            <div class="moods-list">
+                <ul>
+                    <?php $moods = UserMood::model()->findAll('id <= 35'); foreach($moods as $m): ?>
+                        <li><a href="" onclick="return updateMood(<?php echo $m->id; ?>)"><img src="/images/widget/mood/<?php echo $m->id; ?>.png" /><?php echo $m->title; ?></a></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
     <?php endif; ?>
 </div>

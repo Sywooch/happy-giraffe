24f467dab0168fa8dda6ed7453aef674aaebc3a0
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-24 15:23:19 +0400

    features

--- site/frontend/widgets/user/views/_status.php
+++ site/frontend/widgets/user/views/_status.php
@@ -1,2 +1,2 @@
 <div class="date"><?php if ($canUpdate): ?><a href="" class="a-right pseudo">Новый статус</a><?php endif; ?><?php echo Yii::app()->dateFormatter->format("dd MMMM yyyy, HH:mm", $status->created); ?></div>
-<p><?php echo $status->text; ?></p>
+<p><?php echo $status->purified->text; ?></p>

901f792faf02686cf4214ada7f130bf8d2866a7f
Kireev Alex <alexk984@gmail.com>
2012-10-25 20:46:09 +0400

    remove seo profiling

--- site/seo/views/layouts/main.php
+++ site/seo/views/layouts/main.php
@@ -138,8 +138,5 @@
     ?>
 </div>
 <?php endif; ?>
-<!--Отработало за <?=sprintf('%0.5f',Yii::getLogger()->getExecutionTime())?> с. Скушано памяти: <?=round(memory_get_peak_usage()/(1024*1024),2)."MB"?>-->
-<!--<?php $sql_stats = YII::app()->db->getStats();
-echo $sql_stats[0] . ' запросов к БД, время выполнения запросов - ' . sprintf('%0.5f', $sql_stats[1]) . ' c.'; ?>-->
 </body>
 </html>

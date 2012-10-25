5fb8fd131849603a687ef96acde2c296b5f09173
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-25 17:01:33 +0400

    rubrics

--- site/frontend/widgets/user/views/_mood.php
+++ site/frontend/widgets/user/views/_mood.php
@@ -2,3 +2,7 @@
     <span><?php echo $mood->title; ?></span>
 </div>
 Мое настроение <img id="userMood" src="/images/widget/mood/<?php echo $mood->id; ?>.png" /><?php if ($canUpdate): ?> <a href="" class="pseudo small">Изменить</a><?php endif; ?>
+
+<?php if ($this->isMyProfile && $this->user->hasFeature(4)): ?>
+    <a href="javascript:void(0)" class="tooltip-new">20 новых</a>
+<?php endif; ?>

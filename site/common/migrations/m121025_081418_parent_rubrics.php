8bb5a026d3932256522ad8946fc5c6a233811131
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-26 09:33:53 +0400

    rubrics

--- site/common/migrations/m121025_081418_parent_rubrics.php
+++ site/common/migrations/m121025_081418_parent_rubrics.php
@@ -9,18 +9,6 @@ class m121025_081418_parent_rubrics extends CDbMigration
         $this->execute("ALTER TABLE  `community__rubrics` ADD FOREIGN KEY (  `parent_id` ) REFERENCES  `happy_giraffe`.`community__rubrics` (
 `id`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;");
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Гладью', '24', NULL, '193'), (NULL, 'Крестиком', '24', NULL, '193'), (NULL, 'Лентами', '24', NULL, '193'), (NULL, 'Бисером', '24', NULL, '193');");
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Для мужчин', '24', NULL, '194'), (NULL, 'Для женщин', '24', NULL, '194');");
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Спицами', '24', NULL, '195'), (NULL, 'Крючком', '24', NULL, '195');");
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Мальчики', '25', NULL, '205'), (NULL, 'Девочки', '25', NULL, '205');");
-
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Роспись', '25', NULL, NULL);");
-        $lastId = PDO::lastInsertId();
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'По дереву', '25', NULL, $lastId), (NULL, 'По ткани', '25', NULL, $lastId), (NULL, 'По стеклу', '25', NULL, $lastId), (NULL, 'По керамике', '25', NULL, $lastId);");
-
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Плетение', '25', NULL, NULL);");
-        $lastId = PDO::lastInsertId();
-        $this->execute("INSERT INTO `happy_giraffe`.`community__rubrics` (`id`, `title`, `community_id`, `user_id`, `parent_id`) VALUES (NULL, 'Из лозы', '25', NULL, $lastId), (NULL, 'Из соломы', '25', NULL, $lastId), (NULL, 'Из газет', '25', NULL, $lastId), (NULL, 'Из лент', '25', NULL, $lastId);");
 	}
 
 	public function down()

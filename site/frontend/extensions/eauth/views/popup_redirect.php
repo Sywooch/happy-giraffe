d5b8bd486ef244da1cc450211968a06dc1f7b80f
Kireev Alex <alexk984@gmail.com>
2012-10-26 16:48:49 +0400

    social auth

--- site/frontend/extensions/eauth/views/popup_redirect.php
+++ site/frontend/extensions/eauth/views/popup_redirect.php
@@ -3,23 +3,16 @@
   <head>
 	<script type="text/javascript">
 		<?php 
-//			$code = 'if (window.opener) {';
-//			$code .= 'window.close();';
-//			if ($redirect)
-//				$code .= 'window.opener.location = \''.addslashes($url).'\';';
-//			$code .= '}';
-//			$code .= 'else {';
-//			if ($redirect)
-//				$code .= 'window.location = \''.addslashes($url).'\';';
-//			$code .= '}';
-//			echo $code;
+			if ($redirect)
+				$code .= 'window.location = \''.addslashes($url).'\';';
+			echo $code;
 		?>
 	</script>
   </head>
   <body>
-  <?=$url ?>
-  <?php var_dump($_GET) ?>
-  <?php var_dump($_POST) ?>
+    <?=$url ?>
+    <?php var_dump($_GET) ?>
+    <?php var_dump($_POST) ?>
 	<h2 id="title" style="display:none;">Redirecting back to the application...</h2>
 	<h3 id="link"><a href="<?php echo $url; ?>">Click here to return to the application.</a></h3>
 	<script type="text/javascript">

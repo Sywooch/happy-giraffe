39b324a349f5ada52864d46f5ec21227636e9369
Kireev Alex <alexk984@gmail.com>
2012-10-29 16:35:46 +0400

    profile enhance

--- site/frontend/views/user/profile.php
+++ site/frontend/views/user/profile.php
@@ -216,3 +216,6 @@
     $remove_tmpl->registerTemplates();
     $this->endWidget();
 ?>
+
+<?php $sql_stats = YII::app()->db->getStats();
+echo $sql_stats[0] . ' запросов к БД, время выполнения запросов - ' . sprintf('%0.5f', $sql_stats[1]) . ' c.'; ?>

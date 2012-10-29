1c6898f137ec749060aae0b561ecdfd511b741d7
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-29 17:23:02 +0400

    Лайки

---
+++ site/frontend/widgets/avatarWidget/views/hg.php
@@ -0,0 +1,15 @@
+<?php if ($this->size == 'ava'): ?>
+
+    <div class="user-info ava-happy-giraffe medium">
+
+        <a href="http://www.happy-giraffe.ru" class="ava male"><img src="/images/ava_happy_giraffe.png" alt="" /></a>
+
+        <div class="details">
+
+            <a href="http://www.happy-giraffe.ru" class="username">Веселый Жираф</a>
+
+        </div>
+
+    </div>
+
+<?php endif; ?>

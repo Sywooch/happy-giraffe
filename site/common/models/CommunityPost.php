4eae193e8d3712ba2941c9e06fa1cf7baa4de86d
Kireev Alex <alexk984@gmail.com>
2012-10-31 18:27:59 +0400

    home fix

--- site/common/models/CommunityPost.php
+++ site/common/models/CommunityPost.php
@@ -188,7 +188,6 @@ class CommunityPost extends HActiveRecord
 
     public function searchImage($author_id)
     {
-        echo 'search_image';
         if (preg_match('/http:\/\/img.happy-giraffe.ru\/thumbs\/[\d]+x[\d]+\/[\d]+\/([^\"]+)/', $this->text, $m)) {
             $photo = AlbumPhoto::model()->findByAttributes(array('fs_name' => $m[1]));
             if (isset($photo)) {

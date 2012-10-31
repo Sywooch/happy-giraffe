f9961d9c1e2009f7e81592aa6b20084ba1659753
Kireev Alex <alexk984@gmail.com>
2012-10-31 21:37:08 +0400

    fix

--- site/common/models/CommunityPost.php
+++ site/common/models/CommunityPost.php
@@ -213,7 +213,8 @@ class CommunityPost extends HActiveRecord
 
                 if ($image !== false) {
                     $photo = AlbumPhoto::createByUrl($image, $author_id, 6);
-                    $this->photo_id = $photo->id;
+                    if ($photo !== null)
+                        $this->photo_id = $photo->id;
                 }
             }
     }

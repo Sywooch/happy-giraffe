9cfbddb67904eb8a24ce1ad1146fe69b3d093815
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-10-31 14:20:42 +0400

    cache user_blog

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -247,11 +247,11 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-                array(
-                    'class'=>'CEmailLogRoute',
-                    'levels'=>'error, warning',
-                    'emails'=>'nikita@happy-giraffe.ru',
-                ),
+//                array(
+//                    'class'=>'CEmailLogRoute',
+//                    'levels'=>'error, warning',
+//                    'emails'=>'nikita@happy-giraffe.ru',
+//                ),
 //				array(
 //					'class'=>'CEmailLogRoute',
 //					'levels'=>'error, warning',

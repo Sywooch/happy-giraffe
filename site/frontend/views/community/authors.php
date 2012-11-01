1d56c05f980da58819ca34d4ca6f024ff281e470
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-01 20:28:34 +0400

    news rss

--- site/frontend/views/community/authors.php
+++ site/frontend/views/community/authors.php
@@ -5,13 +5,13 @@
         <li>
             <p>Главный редактор сайта «Веселый Жираф»<br />
                 Анастасия Рева      <br />
-                <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a></p>
+                <a href="mailto:redactor@happy-giraffe.ru">redactor@happy-giraffe.ru</a></p>
         </li>
         <li style="clear: both;">
             <img src="/images/authors_1.jpg" alt="Алиса Титко" style="width: 50px; float: right;" />
             <p>Редактор новостей от «Веселого Жирафа»<br />
             Алиса Титко  <br />
-            <a href="mailto:redactor@happy-giraffe.ru">redactor@happy-giraffe.ru</a></p>
+            <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a></p>
         </li>
     </ul>
 </div>

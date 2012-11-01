77f8cf770da1455e80d2651beb60dd092688a7a9
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-01 12:34:09 +0400

    news rss

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -46,6 +46,7 @@ return array(
         'rss/page<page:\d+>' => 'rss/index',
         'rss/social/' => 'rss/social',
         'rss/social/page<page:\d+>' => 'rss/social',
+        'news/rss/<for:\w+>' => 'rss/news',
         'news/rss' => 'rss/news',
 
         // morning controller

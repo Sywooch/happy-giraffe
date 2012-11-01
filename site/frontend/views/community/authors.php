5dd361af82d57b418b341cc3ccf4fbe883649b7b
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-01 20:17:35 +0400

    news rss

--- site/frontend/views/community/authors.php
+++ site/frontend/views/community/authors.php
@@ -10,8 +10,8 @@
         <li style="clear: both;">
             <img src="/images/authors_1.jpg" alt="Алиса Титко" style="width: 50px; float: right;" />
             <p>Редактор новостей от «Веселого Жирафа»<br />
-            ФОТО Алиса Титко  <br />
-            <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a></p>
+            Алиса Титко  <br />
+            <a href="mailto:redactor@happy-giraffe.ru">redactor@happy-giraffe.ru</a></p>
         </li>
     </ul>
 </div>

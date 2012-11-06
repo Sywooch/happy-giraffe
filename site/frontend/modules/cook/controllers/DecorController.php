e044ca8822f9518387753b2f73e78141b4159de3
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-06 18:56:18 +0400

    ias

--- site/frontend/modules/cook/controllers/DecorController.php
+++ site/frontend/modules/cook/controllers/DecorController.php
@@ -7,6 +7,14 @@ class DecorController extends HController
         $this->pageTitle = ($id) ? 'Оформление блюд: ' . $category->title : 'Оформление блюд';
         $dataProvider = CookDecoration::model()->indexDataProvider($id);
 
+        $this->breadcrumbs = array(
+            'Кулинария' => array('/cook/default/index'),
+            'Украшения блюд' => array('/cook/decor/index'),
+        );
+
+        if ($id !== false)
+            $this->breadcrumbs[$category->title] = $category->url;
+
         $this->render('index', compact('id', 'category', 'dataProvider'));
     }
 }

5c195cb12b2e0a6d70ac49d06b36189fb4b3a693
Kireev Alex <alexk984@gmail.com>
2012-11-12 20:01:13 +0400

    fix

--- site/common/tpl/innerLinks.php
+++ site/common/tpl/innerLinks.php
@@ -1,8 +1,9 @@
 <div class="interesting">
     <div class="block-title">Вам может быть интересно</div>
     <ul>
-        <?php foreach ($link_pages as $link_page): ?>
-            <li><?php echo CHtml::link($link_page->keyword->name, $link_page->pageTo->url); ?></li>
-        <?php endforeach; ?>
+        <?php foreach ($link_pages as $link_page)
+                if (isset($link_page->keyword->name)) {
+            ?><li><?php echo CHtml::link($link_page->keyword->name, $link_page->pageTo->url); ?></li>
+            <?php } ?>
     </ul>
 </div>

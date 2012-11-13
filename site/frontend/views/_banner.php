46fc91ab92b65683eca04149a2d84c7cf2699ef6
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-13 14:49:07 +0400

    contest

--- site/frontend/views/_banner.php
+++ site/frontend/views/_banner.php
@@ -8,4 +8,19 @@
     );
     $n = rand(0, count($banners) - 1);
 ?>
-<?=CHtml::link(CHtml::image('/images/banners/' . $n . '.jpg'), $banners[$n])?>
+<?//=CHtml::link(CHtml::image('/images/banners/' . $n . '.jpg'), $banners[$n])?>
+
+<?php
+//<!--/* OpenX Local Mode Tag v2.8.10 */-->
+
+// The MAX_PATH below should point to the base of your OpenX installation
+define('MAX_PATH', '/var/www/happy-giraffe.ru/openx');
+if (@include_once(MAX_PATH . '/www/delivery/alocal.php')) {
+    if (!isset($phpAds_context)) {
+        $phpAds_context = array();
+    }
+    // function view_local($what, $zoneid=0, $campaignid=0, $bannerid=0, $target='', $source='', $withtext='', $context='', $charset='')
+    $phpAds_raw = view_local('', 1, 0, 0, '', '', '0', $phpAds_context, '');
+}
+echo $phpAds_raw['html'];
+?>

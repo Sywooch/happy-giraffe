58430bd2772b95dc04b26b95957309f195e893e6
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-15 18:11:31 +0400

    photo replace/delete

---
+++ site/common/behaviors/ExternalImagesBehavior.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: solivager
+ * Date: 11/15/12
+ * Time: 4:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ExternalImagesBehavior extends CActiveRecordBehavior
+{
+    public $attributes = true;
+
+    public function beforeSave($event)
+    {
+        Yii::import('site.frontend.extensions.phpQuery.phpQuery');
+
+        $attributes = array_keys($this->owner->getAttributes($this->attributes));
+        foreach ($attributes as $attr) {
+            $doc = phpQuery::newDocumentXHTML($this->owner->$attr, $charset = 'utf-8');
+
+            foreach (pq('img') as $e) {
+                $src = pq($e)->attr('src');
+                if (strpos($src, Yii::app()->params['photos_url']) == 0) {
+                    $photo = AlbumPhoto::createByUrl($src, Yii::app()->user->id, 2);
+                    if ($photo !== false) {
+                        $newSrc = $photo->getPreviewUrl(700, 700, Image::WIDTH);
+                        pq($e)->attr('src', $newSrc);
+                        Yii::log(
+                            'Image was replaced' . "\n" .
+                            '------------------------------' . "\n" .
+                            'Old image: ' . $src . "\n" .
+                            'New image: ' . $newSrc . "\n" .
+                            'Entity: ' . get_class($this->owner) . "\n" .
+                            'Entity id: ' . $this->owner->id ."\n" .
+                            '------------------------------' . "\n"
+                        , 'warning');
+                    } else {
+                        pq($e)->remove();
+                        Yii::log(
+                            'Image was deleted' . "\n" .
+                                '------------------------------' . "\n" .
+                                'Old image: ' . $src . "\n" .
+                                'Entity: ' . get_class($this->owner) . "\n" .
+                                'Entity id: ' . $this->owner->id ."\n" .
+                                '------------------------------' . "\n"
+                        , 'warning');
+                    }
+                }
+            }
+
+            $this->owner->$attr = $doc->html();
+            $doc->unloadDocument();
+        }
+
+        parent::beforeSave($event);
+    }
+}

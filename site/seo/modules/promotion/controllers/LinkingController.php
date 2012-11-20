c5563c2e97c1ecf9db2af5f56cfec2d84dc42cff
Kireev Alex <alexk984@gmail.com>
2012-11-20 22:14:40 +0400

    fix

--- site/seo/modules/promotion/controllers/LinkingController.php
+++ site/seo/modules/promotion/controllers/LinkingController.php
@@ -147,9 +147,9 @@ class LinkingController extends SController
             $res = array();
             foreach ($pages as $page)
                 $res [] = $page->page;
-            TimeLogger::model()->startTimer('pages found - filter');
+//            TimeLogger::model()->startTimer('pages found - filter');
             $pages = $this->filterPages($phrase, $res);
-            TimeLogger::model()->endTimer();
+//            TimeLogger::model()->endTimer();
 
         } else {
             TimeLogger::model()->startTimer('pages not found - parsing');
@@ -213,10 +213,14 @@ class LinkingController extends SController
                 unset($pages[$key]);
 
         //удалим те с которых уже стоят ссылки на наш
+        TimeLogger::model()->startTimer('pages found - filter: check out links');
         foreach ($pages as $key => $page) {
             if (!empty($page->id) && InnerLink::model()->exists('page_id = ' . $page->id . ' and page_to_id=' . $phrase->page->id))
                 unset($pages[$key]);
         }
+        TimeLogger::model()->endTimer();
+
+        TimeLogger::model()->startTimer('pages found - filter: check next previous');
         //удалим те, с которых стоят ссылки на наш в разделе "Ещё статьи на эту тему"
         foreach ($pages as $key => $page) {
             $article = $phrase->page->getArticle();
@@ -232,6 +236,7 @@ class LinkingController extends SController
                 }
             }
         }
+        TimeLogger::model()->endTimer();
 
         return $pages;
     }

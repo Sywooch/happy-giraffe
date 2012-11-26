88007050d05bdd5630ca17d706d21e1b17271c28
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-26 18:13:50 +0400

    contest 2 remove href

--- site/frontend/modules/contest/views/default/prizes/2.php
+++ site/frontend/modules/contest/views/default/prizes/2.php
@@ -7,7 +7,7 @@
             </div>
             <div class="place place-1-1"></div>
             <div class="title">
-                <a href="">Фотоаппарат<br/><b>SONY Cyber-shot DSC-HX10</b></a>
+                Фотоаппарат<br/><b>SONY Cyber-shot DSC-HX10</b>
             </div>
         </li>
         <li>
@@ -16,7 +16,7 @@
             </div>
             <div class="place place-2-3"></div>
             <div class="title">
-                <a href="">Фоторамка<br/><b>SONY DPF-D830LB 8"</b></a>
+                Фоторамка<br/><b>SONY DPF-D830LB 8"</b>
             </div>
         </li>
 

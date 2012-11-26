3db6d01e4c0136e1d587508fd860834fe13f9db2
Kireev Alex <alexk984@gmail.com>
2012-11-26 14:20:43 +0400

    new register

--- javascripts/common.js
+++ javascripts/common.js
@@ -454,7 +454,7 @@ var Register = {
         Register.attributes['email']= email;
         Register.attributes['type']= type;
         console.log(Register.attributes);
-        $.post('/signup/showForm/', Register.attributes, function(response) {
+        $.post('/signup/showForm/', Register.attributes.serialize(), function(response) {
             var link = $('#hidden_register_link');
             link.attr('href', '#register');
             link.trigger('click');

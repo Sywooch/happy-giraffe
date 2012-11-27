b6bb9ba609c66212ac4a694cfedaf7dfadbfbe00
Kireev Alex <alexk984@gmail.com>
2012-11-27 12:13:09 +0400

    reports fix

---
+++ site/seo/modules/writing/views/editor/reports_3.php
@@ -0,0 +1,55 @@
+<div class="seo-table table-report tabs">
+    <div class="table-title">Список заданий</div>
+
+    <div class="table-nav nav">
+        <?php $this->renderPartial('reports_menu', array('criteria' => $criteria, 'status'=>$status)); ?>
+    </div>
+
+    <div class="tabs-container">
+        <div class="table-box tab-box tab-box-3" style="display: none; ">
+            <table>
+                <colgroup>
+                    <col width="400">
+                    <col>
+                    <col>
+                    <col>
+                    <col>
+                </colgroup>
+                <thead>
+                <tr>
+                    <th class="al">Ключевые слова и фразы</th>
+                    <th class="al">Название статьи</th>
+                    <th>Исполнитель</th>
+                    <th>Статус</th>
+                    <th>Действие</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($models as $task) { ?>
+                <tr>
+                    <td class="al"><?=$task->getText() ?></td>
+                    <td class="al"><?=$task->getArticleText() ?></td>
+                    <td><?=$task->getExecutor() ?></td>
+                    <td class="seo-status-publish-<?=($task->status == SeoTask::STATUS_CORRECTED)?1:2 ?>"><?=$task->statusText ?></td>
+                    <?php if ($task->status == SeoTask::STATUS_CORRECTED): ?>
+                    <td><a href="" class="btn-green-small" onclick="SeoTasks.ToPublishing(this, <?=$task->id ?>);return false;">На публикацию</a></td>
+                    <?php else: ?>
+                    <td></td>
+                    <?php endif ?>
+                </tr>
+                    <?php } ?>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?php if ($pages->pageCount > 1): ?>
+    <div class="pagination pagination-center clearfix">
+        <?php $this->widget('MyLinkPager', array(
+        'header'=>false,
+        'pages' => $pages,
+    )); ?>
+    </div>
+    <?php endif; ?>
+</div>

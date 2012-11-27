5ff42713f3d68daf5b8235db6f9d2ca5ed546261
Kireev Alex <alexk984@gmail.com>
2012-11-27 10:17:53 +0400

    linking fix

--- site/seo/modules/promotion/views/linking/check_links.php
+++ site/seo/modules/promotion/views/linking/check_links.php
@@ -1,12 +1,33 @@
 <div>
     <?php $this->widget('zii.widgets.CListView', array(
-        'dataProvider'=>$link->search(),
-        'itemView'=>'_link',
-        'sortableAttributes'=>array(
-            'title',
-            'create_time'=>'Post Time',
-        ),
-    ));
+    'dataProvider' => $link->search(),
+    'itemView' => '_link',
+    'sortableAttributes' => array(
+        'title',
+        'create_time' => 'Post Time',
+    ),
+));
 
     ?>
 </div>
+<script type="text/javascript">
+    var CheckLinks = {
+        remove:function (page_id, page_to_id, el) {
+            if (confirm("Точно удалить?")) {
+                $.post('/promotion/linking/remove/', {
+                    page_to_id:page_to_id,
+                    page_id:page_id
+                }, function (response) {
+                    if (response.status)
+                        $(el).parent().remove();
+                    else
+                        $.pnotify({
+                            pnotify_title:'Ошибка',
+                            pnotify_type:'error',
+                            pnotify_text:'Обратитесь к разработчикам'
+                        });
+                }, 'json');
+            }
+        }
+    }
+</script>

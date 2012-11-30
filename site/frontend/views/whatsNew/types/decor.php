307f06efc6de4ced9e1de8ed722f1ded0e9b4c2d
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-11-30 14:06:30 +0400

    live

--- site/frontend/views/whatsNew/types/decor.php
+++ site/frontend/views/whatsNew/types/decor.php
@@ -21,7 +21,7 @@
     <div class="masonry-news-list_content">
         <ul class="masonry-news-list_img-list clearfix">
             <?php foreach ($data->decorations as $d): ?>
-                <li><a href="javascript:void(0)" data-id="<?=$d->photo->id?>"><?=CHtml::image($d->photo->getPreviewUrl(64, 61, Image::HEIGHT, true, AlbumPhoto::CROP_SIDE_TOP), $d->title)?></a></li>
+                <li><a href="javascript:void(0)" data-id="<?=$d->photo->id?>"><?=CHtml::image($d->photo->getPreviewUrl(64, 61, Image::AUTO, true, AlbumPhoto::CROP_SIDE_TOP), $d->title)?></a></li>
             <?php endforeach; ?>
         </ul>
     </div>

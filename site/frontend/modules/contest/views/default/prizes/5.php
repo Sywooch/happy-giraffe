23e7871031464117195ac6595d991684cc9dfa55
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-03 09:37:07 +0400

    contest 5

---
+++ site/frontend/modules/contest/views/default/prizes/5.php
@@ -0,0 +1,26 @@
+<div class="contest-prizes-list contest-prizes-list-5 clearfix">
+
+    <ul>
+        <li>
+            <div class="img">
+                <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-1'))?>"><img src="/images/prize_14.jpg" /></a>
+            </div>
+            <div class="place place-1-1"></div>
+            <div class="title">
+                Увлажнитель воздуха<br/><b>TIGEX </b>
+            </div>
+            <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-1'))?>" class="all">Подробнее</a>
+        </li>
+        <li>
+            <div class="img">
+                <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-2'))?>"><img src="/images/prize_15.jpg" /></a>
+            </div>
+            <div class="place place-2-3"></div>
+            <div class="title">
+                Радионяня Я расту WT-448<br/><b>Я расту WT- 448</b>
+            </div>
+            <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-2'))?>" class="all">Подробнее</a>
+        </li>
+    </ul>
+
+</div>

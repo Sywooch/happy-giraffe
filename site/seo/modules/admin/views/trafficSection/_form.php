250c1548d2734f9c1b7a6fa34bf81cf0965a74d3
Kireev Alex <alexk984@gmail.com>
2012-12-05 10:17:53 +0400

    traffic admin

---
+++ site/seo/modules/admin/views/trafficSection/_form.php
@@ -0,0 +1,36 @@
+ <?php echo CHtml::link('К таблице', array('TrafficSection/admin')) ?><div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'traffic-section-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Поля с <span class="required">*</span> обязательны.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'url'); ?>
+		<?php echo $form->textField($model,'url',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'url'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>256)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'parent_id'); ?>
+		<?php echo $form->textField($model,'parent_id',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'parent_id'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->

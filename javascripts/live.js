37d5b7dcd7c7e16d13559f74684a7b196947fb86
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-05 19:37:20 +0400

    scroll

--- javascripts/live.js
+++ javascripts/live.js
@@ -1,11 +1,30 @@
 $(function() {
-    var $container = $("#liveList .items");
-    $container.imagesLoaded(function(){
-        $container.masonry({
-            itemSelector : ".masonry-news-list_item",
-            columnWidth : 240,
-            isAnimated: true
-        });
+//    var $container = $("#liveList .items");
+//    $container.imagesLoaded(function(){
+//        $container.masonry({
+//            itemSelector : ".masonry-news-list_item",
+//            columnWidth : 240,
+//            isAnimated: true
+//        });
+//    });
+//
+//    Comet.prototype.receiveEvent = function(result, id) {
+//        var old = $('.masonry-news-list_item:data(blockId=' + result.blockId + ')');
+//        var toRemove = (old.length > 0) ? old : $('.masonry-news-list_item:last');
+//        toRemove.remove();
+//
+//        var $boxes = $(result.code);
+//        $('#liveList .items').prepend($boxes).imagesLoaded(function() {
+//            $('#liveList .items').masonry('reload');
+//        });
+//    }
+//
+//    comet.addEvent(10000, 'receiveEvent');
+
+    $('#liveList .items').isotope({
+        // options
+        itemSelector : '.masonry-news-list_item',
+        layoutMode : 'masonry'
     });
 
     Comet.prototype.receiveEvent = function(result, id) {
@@ -13,10 +32,7 @@ $(function() {
         var toRemove = (old.length > 0) ? old : $('.masonry-news-list_item:last');
         toRemove.remove();
 
-        var $boxes = $(result.code);
-        $('#liveList .items').prepend($boxes).imagesLoaded(function() {
-            $('#liveList .items').masonry('reload');
-        });
+        $('#liveList .items').prepend($(result.code)).isotope('reloadItems').isotope({sortBy: 'original-order'});
     }
 
     comet.addEvent(10000, 'receiveEvent');

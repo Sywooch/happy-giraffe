73ba51ed88bf33e817e778b957a6ec3b456e9f3e
Kireev Alex <alexk984@gmail.com>
2012-12-06 10:03:45 +0400

    migration fix

--- site/common/migrations/m121123_090506_add_geo_region_center.php
+++ site/common/migrations/m121123_090506_add_geo_region_center.php
@@ -7,9 +7,9 @@ class m121123_090506_add_geo_region_center extends CDbMigration
 	public function up()
 	{
         $this->addForeignKey('fk_'.$this->_table.'_center', $this->_table, 'center_id', 'geo__city', 'id','CASCADE',"CASCADE");
-        $this->execute("INSERT INTO  `happy_giraffe`.`auth__items`
- (`name` ,`type` ,`description` ,`bizrule` ,`data`)
- VALUES ('geo',  '0',  'Редактирование географии', NULL , NULL);");
+//        $this->execute("INSERT INTO  `happy_giraffe`.`auth__items`
+// (`name` ,`type` ,`description` ,`bizrule` ,`data`)
+// VALUES ('geo',  '0',  'Редактирование географии', NULL , NULL);");
 
     }
 

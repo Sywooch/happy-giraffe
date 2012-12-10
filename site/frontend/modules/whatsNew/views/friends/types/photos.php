7e58c9faca1b74f8a36ea68246ad3ab64215a038
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-10 14:43:56 +0400

    exist

--- site/frontend/modules/whatsNew/views/friends/types/photos.php
+++ site/frontend/modules/whatsNew/views/friends/types/photos.php
@@ -1,3 +1,12 @@
+<?php
+    $this->widget('site.frontend.widgets.photoView.photoViewWidget', array(
+        'selector' => '.masonry-news-list_item:data(id=' . $data->_id . ') .title-img a',
+        'entity' => 'Album',
+        'entity_id' => $data->album->id,
+        'entity_url' => $data->album->url,
+    ));
+?>
+
 <h3 class="masonry-news-list_title">
     <?=CHtml::link($data->album->title, $data->album->url)?>
     <a href="<?=$data->album->url?>" class="icon-photo"></a>

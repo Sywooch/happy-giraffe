3df8cb32bbeeac87019088ca1d01876c4d233af4
Kireev Alex <alexk984@gmail.com>
2012-12-10 11:39:39 +0400

    tags alphabet

--- site/frontend/modules/cook/views/recipe/tag_list.php
+++ site/frontend/modules/cook/views/recipe/tag_list.php
@@ -3,6 +3,6 @@
  * Author: alexk984
  * Date: 06.12.12
  */
-$tags = CookRecipeTag::model()->findAll();
+$tags = CookRecipeTag::model()->alphabet()->findAll();
 foreach($tags as $tag)
     echo CHtml::link($tag->title, '/cook/recipe/tag/'.$tag->id.'/').'<br>';

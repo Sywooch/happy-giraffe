97407ed7c59d21aa63e8bdde6d78dca12264f234
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-10 14:28:06 +0400

    whatsNew module

--- site/frontend/modules/whatsNew/models/FriendEventPhotos.php
+++ site/frontend/modules/whatsNew/models/FriendEventPhotos.php
@@ -9,4 +9,83 @@
 class FriendEventPhotos extends FriendEvent
 {
     public $type = FriendEvent::TYPE_PHOTOS_ADDED;
+    public $album_id;
+
+    private $_album;
+    private $_photos;
+
+    public function init()
+    {
+        $this->_album = $this->_getAlbum();
+        $this->_photos = $this->_getPhotos();
+    }
+
+    public function getAlbum()
+    {
+        return $this->_album;
+    }
+
+    public function setAlbum($album)
+    {
+        $this->_album = $album;
+    }
+
+    private function _getAlbum()
+    {
+        return Album::model()->findByPk($this->album_id);
+    }
+
+    public function getPhotos()
+    {
+        return $this->_photos;
+    }
+
+    public function setPhotos($photos)
+    {
+        $this->_photos = $photos;
+    }
+
+    private function _getPhotos()
+    {
+        $criteria = new CDbCriteria(array(
+            'limit' => 9,
+            'condition' => 'album_id = :album_id',
+            'params' => array(':album_id' => $this->album_id),
+            'order' => 'created DESC',
+        ));
+
+        return AlbumPhoto::model()->findAll($criteria);
+    }
+
+    public function getLabel()
+    {
+        return HDate::simpleVerb('Добавил', $this->user->gender) . ' фото в альбом';
+    }
+
+    public function createBlock()
+    {
+        $this->album_id = (int) $this->params['album_id'];
+        $this->user_id = (int) $this->params['user_id'];
+
+        parent::createBlock();
+    }
+
+    public function updateBlock($new)
+    {
+        $this->updated = time();
+        $this->save();
+    }
+
+    public function getStack()
+    {
+        $criteria = new EMongoCriteria(array(
+            'conditions' => array(
+                'album_id' => array(
+                    'equals' => (int) $this->params['album_id'],
+                ),
+            ),
+        ));
+
+        return FriendEvent::model($this->type)->find($criteria);
+    }
 }

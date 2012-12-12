aa300fa7b39535a44963641d2bb1f67761f45b59
Kireev Alex <alexk984@gmail.com>
2012-12-12 14:19:27 +0400

    fix

--- site/frontend/modules/signal/views/commentator/_hint.php
+++ site/frontend/modules/signal/views/commentator/_hint.php
@@ -4,21 +4,22 @@
  */
 
 if ($task === null):?>
-<li><a href="/commentator/tasks/" onclick="CommentatorPanel.block=<?=$block ?>;" class="btn-g orange small fancy" data-theme="white-simple">Подсказка</a></li>
+<li><a href="/commentator/tasks/" onclick="CommentatorPanel.block=<?=$block ?>;" class="btn-g orange small fancy"
+       data-theme="white-simple">Подсказка</a></li>
 <?php else: ?>
-<?php if ($task->status == SeoTask::STATUS_TAKEN):?>
+<?php if ($task->status == SeoTask::STATUS_TAKEN): ?>
     <li>
         <strong><?=$task->keywordGroup->keywords[0]->name ?></strong>
         <a href="javascript:;" class="icon-close" onclick="CommentatorPanel.CancelTask(<?=$task->id ?>, this)"></a>
         <input type="text" name="" class="placing" placeholder="Введите URL статьи">
         <a href="javascript:;" onclick="CommentatorPanel.Written(<?=$task->id ?>, this)" class="btn-green-small">ok</a>
     </li>
-<?php else: ?>
+    <?php else: ?>
     <li>
         <b>По подсказке:</b>
         <a href="<?=$task->article->url ?>" target="_blank"><?= $task->article->getArticleTitle() ?></a>
         <span class="done"><i class="icon"></i>Сделано</span>
         <span class="date"><?=Yii::app()->dateFormatter->format('dd MMM yyyy, HH:mm', strtotime($task->article->getArticle()->created)) ?></span>
     </li>
-<?php endif ?>
+    <?php endif ?>
 <?php endif ?>

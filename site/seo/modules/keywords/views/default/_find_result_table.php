e5e7e55188a06077257e5a1abc60207cbe12038c
Kireev Alex <alexk984@gmail.com>
2012-12-12 17:07:30 +0400

    commentators fix

--- site/seo/modules/keywords/views/default/_find_result_table.php
+++ site/seo/modules/keywords/views/default/_find_result_table.php
@@ -3,13 +3,10 @@
  * @var $models Keyword[]
  */
 foreach ($models as $model):?>
-<tr id="key-<?=$model->id ?>" class="<?=$model->getClass() ?>">
-    <td class="col-1"><?=$model->getKeywordAndSimilarArticles() ?></td>
+<tr id="key-<?=$model->id ?>">
+    <td class="col-1"><?=$model->name ?></td>
     <td><?=$model->getFreqIcon() ?></td>
     <td><?= isset($model->yandex) ? $model->yandex->value : '' ?></td>
-    <td></td>
-    <td><?=$model->getStats(1) ?></td>
-    <td><?=$model->getStats(2) ?></td>
     <td>
         <?=$model->getButtons(); ?>
     </td>

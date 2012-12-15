2392696a6397b3af05cb69b50e7d130058db2379
Kireev Alex <alexk984@gmail.com>
2012-12-15 12:22:49 +0400

    li parsing fix

--- site/console/commands/SeoCommand.php
+++ site/console/commands/SeoCommand.php
@@ -395,7 +395,7 @@ class SeoCommand extends CConsoleCommand
     public function actionLi($site){
         Yii::import('site.seo.modules.competitors.components.*');
         if (empty($site)){
-            $sites = Site::model()->findAll('id > 3');
+            $sites = Site::model()->findAll('id > 57');
             foreach($sites as $site){
                 $parser = new LiParser;
                 $parser->start($site->id, 2012, 11, 12);

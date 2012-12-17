81e0fca0878e4ed50b85e3dc1c1b4ffb97074025
Kireev Alex <alexk984@gmail.com>
2012-12-17 12:49:47 +0400

    cook

---
+++ site/frontend/modules/cook/views/recipe/_recipe.php
@@ -0,0 +1,32 @@
+<?php
+/* @var $this Controller
+ * @var $data CookRecipe
+ */
+?><div class="entry recipe-article clearfix">
+
+    <?php $this->renderPartial('_recipe_parts/_header',array('recipe'=>$data)); ?>
+
+    <div class="entry-content">
+
+        <?php $this->renderPartial('_recipe_parts/_cook_book', array('recipe' => $data)); ?>
+
+        <div class="recipe-photo">
+            <div class="big">
+                <?php if ($data->mainPhoto) echo HHtml::image($data->mainPhoto->getPreviewUrl(460, null, Image::WIDTH), $data->mainPhoto->title, array('class' => 'photo'), true)?>
+            </div>
+        </div>
+
+        <div style="clear:left;"></div>
+
+        <?php $this->renderPartial('_recipe_parts/_recipe_description', array('recipe' => $data)); ?>
+
+        <h2>Приготовление</h2>
+
+        <div class="instructions wysiwyg-content">
+
+            <p><?=Str::truncate(strip_tags($data->text), 400)?> <?=HHtml::link('Весь рецепт<i class="icon"></i>', $data->url, array('class' => 'read-more'), true)?></p>
+        </div>
+
+    </div>
+
+</div>

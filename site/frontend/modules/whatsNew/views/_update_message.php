42fa36d4d00b79c9378a3261acf995bea5f6186a
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-17 18:00:27 +0400

    findFriends

---
+++ site/frontend/modules/whatsNew/views/_update_message.php
@@ -0,0 +1,13 @@
+<li class="masonry-news-list_item update-message">
+    <div class="icon-broadcast"></div>
+    <div class="desc">Если, пока вы читаете, <br />появятся новые события, <br />мы вам сообщим здесь.
+    </div>
+</li>
+<li class="masonry-news-list_item update-message new" style="display: none;">
+    <div class="count">0</div>
+    <div class="desc">
+        <span class="red">Внимание! </span> <br />
+        Появились новые события
+    </div>
+    <a href="<?=Yii::app()->request->requestUri?>" class="btn-update">Обновить</a>
+</li>

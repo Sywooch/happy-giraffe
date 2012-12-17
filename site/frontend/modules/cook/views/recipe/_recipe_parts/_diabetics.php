b15dc1b8282cd4c90a7dd81efa0bf8219d8fa03a
Kireev Alex <alexk984@gmail.com>
2012-12-17 15:19:50 +0400

    cook fix

--- site/frontend/modules/cook/views/recipe/_recipe_parts/_diabetics.php
+++ site/frontend/modules/cook/views/recipe/_recipe_parts/_diabetics.php
@@ -1,13 +1,8 @@
-<div class="cook-diabets">
-    <!--
-    Диаграмма для диабетиков имеет 4 состояния на сколько не подходит
-    val0 (по умолчанию даже без класса)
-    val33
-    val66
-    val100
-    -->
-    <div class="cook-diabets-chart <?=$recipe->getBakeryItemsCssClass() ?>">
-        <span class="text"><?=$recipe->bakeryItems?></span>
+<?php if (!empty($recipe->servings)):?>
+    <div class="cook-diabets">
+        <div class="cook-diabets-chart <?=$recipe->getBakeryItemsCssClass() ?>">
+            <span class="text"><?=$recipe->bakeryItems?></span>
+        </div>
+        <div class="cook-diabets-desc"><?=$recipe->getBakeryItemsText() ?></div>
     </div>
-    <div class="cook-diabets-desc"><?=$recipe->getBakeryItemsText() ?></div>
-</div>
+<?php endif ?>

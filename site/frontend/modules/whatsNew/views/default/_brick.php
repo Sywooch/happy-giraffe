aa7a6bb71d5741af247fa027218985adae14ce5e
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2012-12-17 18:44:26 +0400

    Merge branch 'develop' of github.com:beknown/happy-giraffe into develop
    Conflicts:
    	site/common/models/Album.php

--- site/frontend/modules/whatsNew/views/default/_brick.php
+++ site/frontend/modules/whatsNew/views/default/_brick.php
@@ -1,4 +1,4 @@
-<?php if ($this->module !== null && $this->module->id == 'whatsNew' && $index == 0): ?>
+<?php if (Yii::app()->controller->module !== null && Yii::app()->controller->module->id == 'whatsNew' && $index == 0): ?>
     <?php $this->renderPartial('/_update_message'); ?>
 <?php endif; ?>
 

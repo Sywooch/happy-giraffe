f169456a743cd4a86216fae6e1036c620f4e862e
Kireev Alex <alexk984@gmail.com>
2012-12-18 15:05:26 +0400

    fix

--- site/seo/modules/competitors/models/SiteKeywordVisit.php
+++ site/seo/modules/competitors/models/SiteKeywordVisit.php
@@ -198,17 +198,16 @@ class SiteKeywordVisit extends HActiveRecord
                 $this->temp_ids = Keyword::findSiteIdsByNameWithSphinx($this->key_name);
 
             if (empty($this->temp_ids))
-                $criteria->condition .= ' AND keyword.id = 0';
+                $criteria->addCondition('keyword.id = 0');
             else
-                $criteria->condition .= ' AND keyword.id IN (' . implode(',', $this->temp_ids) . ')';
+                $criteria->addCondition('keyword.id IN (' . implode(',', $this->temp_ids) . ')');
         }
 
         //search by site_id
         if (count($this->sites_id) == 1)
-            $condition = 'site_id = ' . $this->sites_id[0];
+            $criteria->addCondition('site_id = ' . $this->sites_id[0]);
         else
-            $condition = 'site_id IN (' . implode(',', $this->sites_id) . ') ';
-        $criteria->condition .= empty($criteria->condition) ? $condition : ' AND ' . $condition;
+            $criteria->addCondition('site_id IN (' . implode(',', $this->sites_id) . ') ');
 
         $criteria->compare('year', $this->year);
         $criteria->together = true;

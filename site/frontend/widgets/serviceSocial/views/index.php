8073462ec6c33f8e91b32fe01fa59de1b2d2faee
Kireev Alex <alexk984@gmail.com>
2012-12-19 17:50:33 +0400

    fix

--- site/frontend/widgets/serviceSocial/views/index.php
+++ site/frontend/widgets/serviceSocial/views/index.php
@@ -11,7 +11,4 @@ $this->render('_service_likes', array(
     <?php $this->render('_assistance_users', compact('service')); ?>
     <?php $this->render('_assistance_count', array('count' => $service->using_count)); ?>
 </div>
-<?php if ($service->id == 9 && Yii::app()->user->checkAccess('services'))
-    $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $service));
-elseif ($service->id != 9)
-    $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $service));
+<?php $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $service));

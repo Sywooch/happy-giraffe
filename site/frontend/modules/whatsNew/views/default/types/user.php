a4dd3b47aa8ba86d95d042507045a14d22b833fd
Kireev Alex <alexk984@gmail.com>
2012-12-21 15:09:26 +0400

    fixes

--- site/frontend/modules/whatsNew/views/default/types/user.php
+++ site/frontend/modules/whatsNew/views/default/types/user.php
@@ -1,6 +1,6 @@
 <li class="masonry-news-list_item" data-block-id="<?=$data->blockId?>">
     <h3 class="masonry-news-list_title textalign-c">
-        <a href="<?=$this->createUrl('/activity/friends')?>">Новые пользователи</a>
+        <a href="<?=$this->createUrl('/whatsNew/friends/index')?>">Новые пользователи</a>
     </h3>
     <div class="textalign-c clearfix">
         <span class="date"><?=HDate::GetFormattedTime($data->last_updated)?></span>
@@ -16,7 +16,7 @@
         <?php endforeach; ?>
     </ul>
     <div class="textalign-c clearfix">
-        <a href="<?=$this->createUrl('/activity/friends')?>" class="icon-friends"></a>
-        <a href="<?=$this->createUrl('/activity/friends')?>">Смотреть всех</a>
+        <a href="<?=$this->createUrl('/friends/find')?>" class="icon-friends"></a>
+        <a href="<?=$this->createUrl('/friends/find')?>">Смотреть всех</a>
     </div>
 </li>

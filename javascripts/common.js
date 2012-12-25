666b9d6b0697d434b86582a1de73d03599993c34
tantalid <tantalid@mail.ru>
2012-12-25 13:38:43 +0200

    ref

--- javascripts/common.js
+++ javascripts/common.js
@@ -23,12 +23,6 @@ function removeA(arr){
     return arr;
 }
 
-/* проверка версии ie 8-9 */
-if (jQuery.browser.msie) {
-	if(parseInt(jQuery.browser.version) == 9){$("html").addClass(" ie9")}
-	else if(parseInt(jQuery.browser.version) == 8){$("html").addClass(" ie8")}
-}
-
 $(document).ready(function () {
     $(".wysiwyg-content").addtocopy({htmlcopytxt: '<br /><br />Подробнее: <a href="'+window.location.href+'">'+window.location.href+'</a>'});
     

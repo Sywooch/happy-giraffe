2004a6da781652a7815cf419dae5be5403cbbd21
Kireev Alex <alexk984@gmail.com>
2013-01-09 10:21:05 +0400

    mail

--- site/common/extensions/mailchimp/MailChimp.php
+++ site/common/extensions/mailchimp/MailChimp.php
@@ -77,7 +77,7 @@ class MailChimp extends CApplicationComponent
     {
         $criteria = new CDbCriteria;
         $criteria->limit = 100;
-        $criteria->condition = 'id > 372533';
+        $criteria->condition = 'id > 484847';
 
         $users = array(1);
         $last_id = 0;

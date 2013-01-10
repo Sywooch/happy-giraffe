2bd582bd487e084c237251a219626ca8f4dfd369
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-01-10 09:27:47 +0400

    Merge branch 'develop' of github.com:beknown/happy-giraffe into develop
    Conflicts:
    	site/common/models/Album.php

---
+++ site/frontend/modules/contest/views/default/prizes/6.php
@@ -0,0 +1,36 @@
+<div class="contest-prizes-list contest-prizes-list-6 clearfix">
+
+    <ul>
+        <li>
+            <div class="img">
+                <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-1'))?>"><img src="/images/prize_16.jpg" /></a>
+            </div>
+            <div class="place place-1-1"></div>
+            <div class="title">
+                Детские электронные весы<br/><b>LAICA PS3003 (Италия)</b>
+            </div>
+            <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-1'))?>" class="all">Подробнее</a>
+        </li>
+        <li>
+            <div class="img">
+                <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-2'))?>"><img src="/images/prize_17.jpg" /></a>
+            </div>
+            <div class="place place-2"></div>
+            <div class="title">
+                Мини-блендер<br/><b>Philips AVENT SCF 860/22</b>
+            </div>
+            <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-2'))?>" class="all">Подробнее</a>
+        </li>
+        <li>
+            <div class="img">
+                <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-3'))?>"><img src="/images/prize_18.jpg" /></a>
+            </div>
+            <div class="place place-3"></div>
+            <div class="title">
+                Мини-комбайн<br/><b>Maman ЕС01М </b>
+            </div>
+            <a href="<?=$this->createUrl('/contest/default/prizes', array('id' => $this->contest->id, '#' => 'prize-3'))?>" class="all">Подробнее</a>
+        </li>
+    </ul>
+
+</div>

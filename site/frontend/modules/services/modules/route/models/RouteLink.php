4c770a05e12bf1c9ea2efba99f9ddbbf7c68ca49
Kireev Alex <alexk984@gmail.com>
2013-01-15 12:33:17 +0400

    route module

---
+++ site/frontend/modules/services/modules/route/models/RouteLink.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This is the model class for table "routes__links".
+ *
+ * The followings are the available columns in table 'routes__links':
+ * @property string $route_from_id
+ * @property string $route_to_id
+ * @property string $anchor
+ *
+ * The followings are the available model relations:
+ * @property Route $route2
+ * @property Route $route1
+ */
+class RouteLink extends HActiveRecord
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return RouteLink the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'routes__links';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('route_from_id, route_to_id, anchor', 'required'),
+            array('route_from_id, route_to_id', 'length', 'max' => 11),
+            array('anchor', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('route_from_id, route_to_id, anchor', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'route2' => array(self::BELONGS_TO, 'Route', 'route_to_id'),
+            'route1' => array(self::BELONGS_TO, 'Route', 'route_from_id'),
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'route_from_id' => 'Route1',
+            'route_to_id' => 'Route2',
+            'anchor' => 'Anchor',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('route_from_id', $this->route_from_id, true);
+        $criteria->compare('route_to_id', $this->route_to_id, true);
+        $criteria->compare('anchor', $this->anchor, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+}

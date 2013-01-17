1d2d5d232c0aa53dcdb76dfb7c2a4a45df4f8a5b
Kireev Alex <alexk984@gmail.com>
2013-01-17 13:53:07 +0400

    stop counter

--- site/frontend/controllers/AjaxSimpleController.php
+++ site/frontend/controllers/AjaxSimpleController.php
@@ -14,6 +14,8 @@ class AjaxSimpleController extends CController
     }
 
     public function actionCounter(){
+
+        return false;
         Yii::import('site.seo.models.*');
         $referrer = Yii::app()->request->getPost('referrer');
         $page_url = Yii::app()->request->urlReferrer;

6ec0c2b59665fa3b22b8f25f05c71d54639d3cae
Kireev Alex <alexk984@gmail.com>
2013-01-17 10:38:05 +0400

    status fix

--- site/frontend/views/community/_post.php
+++ site/frontend/views/community/_post.php
@@ -23,7 +23,7 @@
     <?php if (! $full): ?>
         <?php if ($data->type_id == 5): ?>
             <div class="entry-content user-status">
-                <?=CHtml::link($data->status->status->text, $data->url)?>
+                <?=CHtml::link(strip_tags($data->status->status->text), $data->url)?>
             </div>
         <?php else: ?>
             <div class="entry-content wysiwyg-content">
@@ -46,7 +46,7 @@
     <?php else: ?>
         <?php if ($data->type_id == 5): ?>
             <div class="entry-content user-status">
-                <?=$data->status->status->text?>
+                <?=$data->status->status->purified->text?>
             </div>
         <?php else: ?>
             <div class="entry-content">

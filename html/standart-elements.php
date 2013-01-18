a5f89a6b9b83411b47f395404815a0faeccb94e6
tantalid <tantalid@mail.ru>
2013-01-18 12:23:53 +0200

    standart-elements

--- html/standart-elements.php
+++ html/standart-elements.php
@@ -16,6 +16,7 @@
 		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/layout-header.php'; ?>
 			
 		<div id="content" class="layout-content clearfix">
+			<h1>Стандартные элементы</h1>
 			<h1>h1</h1>
 			<h2>h2</h2>
 			<h3>h3</h3>
@@ -33,12 +34,12 @@
 				<input type="search" value="search" >
 			</div>
 			<div class="clearfix margin-b10">
-				<label for="">Input: checkbox</label>
-				<input type="checkbox" >
+				<label for="checkbox">Input: checkbox</label>
+				<input type="checkbox" id="checkbox">
 			</div>
 			<div class="clearfix margin-b10">
-				<label for="">Input: radio</label>
-				<input type="radio" >
+				<label for="radio">Input: radio</label>
+				<input type="radio" id="radio" >
 			</div>
 			<div class="clearfix margin-b10">
 				<label for="">Input: submit</label>
@@ -48,7 +49,9 @@
 				<label for="">Button</label>
 				<button>button</button>
 			</div>
-			<hr>
+			<div class="clearfix margin-b10">
+				<textarea name="" id="" cols="30" rows="10">textarea</textarea>
+			</div>
 			<h1>Кнопки</h1>
 			<div class="clearfix margin-b10">
 				<a href="" class="btn-green">.btn-green</a>
@@ -147,34 +150,107 @@
 				<a href="" class="btn-yellow btn-big">.btn-yellow.btn-big</a>
 				<a href="" class="btn-yellow btn-large">.btn-yellow.btn-large</a>
 			</div>
-			<hr>
-			<h1>Аватары</h1>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava"></a> - .ava
+				<div class="logo-box">
+					<a title="happy-giraffe.ru &ndash; Домашняя страница" class="logo" href="/">Ключевые слова сайта</a>
+					<span>САЙТ ДЛЯ ВСЕЙ СЕМЬИ</span>
+				</div>
+				- 	<p><span style="white-space: pre">	</span>&lt;div class=&quot;logo-box&quot;&gt;</p>
+					<p><span style="white-space: pre">		</span>&lt;a title=&quot;happy-giraffe.ru &amp;ndash; Домашняя страница&quot; class=&quot;logo&quot; href=&quot;/&quot;&gt;Ключевые слова сайта&lt;/a&gt;</p>
+					<p><span style="white-space: pre">		</span>&lt;span&gt;САЙТ ДЛЯ ВСЕЙ СЕМЬИ&lt;/span&gt;</p>
+					<p><span style="white-space: pre">	</span>&lt;/div&gt;</p>
+			</div>
+		
+		<h1>Ссылки</h1>	
+			<div class="clearfix margin-b10">
+				<a href="">a - tag</a> 
+			</div>	
+			<div class="clearfix margin-b10">
+				<a href="" class="a-pseudo">.a-pseudo</a> - .a-pseudo
+			</div>
+			
+		<h1>Иконки</h1>
+		<div class="">
+			<div class="clearfix margin-b10">
+				<div class="a-info"></div> - .a-info
+			</div>
+			
+			<div class="clearfix margin-b10">
+				<i class="ico-friends-small"></i> - .ico-friends-small
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava female"></a> - .ava.female
+				<i class="ico-comment"></i> - .ico-comment
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava male"></a> - .ava.male
+				<i class="ico-pregnant-test"></i> - .ico-pregnant-test
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava"><img src="/images/example/ava.jpg" alt=""></a> - .ava
-		<pre>
-			&lt;a href="" class="ava"&gt;
-				&lt;img src="/images/example/ava.jpg" alt=""&gt;
-			&lt;/a&gt;
-		</pre>
+				<i class="ico-childbirth"></i> - .ico-childbirth
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava big"></a> - .ava.big
+				<i class="ico-age18"></i> - .ico-age18
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava big female"></a> - .ava.big.female
+				<i class="ico-child"></i> - .ico-child
 			</div>
 			<div class="clearfix margin-b10">
-				<a href="" class="ava big male"></a> - .ava.big.male
+				<i class="ico-child ico-child__baby-plan"></i> - .ico-child.ico-child__baby-plan
 			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__baby"></i> - .ico-child.ico-child__baby
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-wait"></i> - .ico-child.ico-child__boy-wait
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-wait"></i> - .ico-child.ico-child__girl-wait
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-small"></i> - .ico-child.ico-child__boy-small
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-small"></i> - .ico-child.ico-child__girl-small
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-3"></i> - .ico-child.ico-child__boy-3
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-3"></i> - .ico-child.ico-child__girl-3
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-5"></i> - .ico-child.ico-child__boy-5
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-5"></i> - .ico-child.ico-child__girl-5
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-8"></i> - .ico-child.ico-child__boy-8
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-8"></i> - .ico-child.ico-child__girl-8
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-14"></i> - .ico-child.ico-child__boy-14
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-14"></i> - .ico-child.ico-child__girl-14
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__boy-19"></i> - .ico-child.ico-child__boy-19
+			</div>
+			<div class="clearfix margin-b10">
+				<i class="ico-child ico-child__girl-19"></i> - .ico-child.ico-child__girl-19
+			</div>
+			<h1>Разделители</h1>
+			<div class="clearfix margin-b10">
+				<hr> - hr or div.hr
+			</div>
+			<div class="clearfix margin-b10">
+				<div class="hr-notches"></div> - hr-notches - зубчатая линия разделителя
+			</div>
+		</div>	
+		<h1>Аватары</h1>
+		<div class="">
 			<div class="clearfix margin-b10">
 				<a href="" class="ava small"></a> - .ava.small
 			</div>
@@ -186,15 +262,60 @@
 			</div>
 			
 			<div class="clearfix margin-b10">
-				<div class="logo-box">
-					<a title="happy-giraffe.ru &ndash; Домашняя страница" class="logo" href="/">Ключевые слова сайта</a>
-					<span>САЙТ ДЛЯ ВСЕЙ СЕМЬИ</span>
-				</div>
+				<a href="" class="ava"></a> - .ava
 			</div>
-			
-			
-			
-			
+			<div class="clearfix margin-b10">
+				<a href="" class="ava female"></a> - .ava.female
+			</div>
+			<div class="clearfix margin-b10">
+				<a href="" class="ava male"></a> - .ava.male
+			</div>
+			<div class="clearfix margin-b10">
+				<a href="" class="ava"><img src="/images/example/ava.jpg" alt=""></a> - .ava
+				<p><span style="white-space: pre">	</span>	&lt;a href="" class="ava"&gt;</p>
+				<p><span style="white-space: pre">	</span>		&lt;img src="/images/example/ava.jpg" alt=""&gt;</p>
+				<p><span style="white-space: pre">	</span>	&lt;/a&gt;</p>
+			</div>
+			<div class="clearfix margin-b10">
+				<a href="" class="ava big"></a> - .ava.big
+			</div>
+			<div class="clearfix margin-b10">
+				<a href="" class="ava big female"></a> - .ava.big.female
+			</div>
+			<div class="clearfix margin-b10">
+				<a href="" class="ava big male"></a> - .ava.big.male
+			</div>
+		</div>		
+		<h1>Js palugins</h1>
+		<div class="">
+			<div class="clearfix margin-b10">
+				плагин подсказок - <span class="js-tooltipsy" title="Title text">class="js-tooltipsy" title="Title text"</span>
+			</div>
+		</div>
+		<h1>Errors</h1>
+			<div class="clearfix margin-b10">
+				<input type="text" class="error" value="error"> - input[type=text].error или но менее предпочтительно .error input[type=text]
+			</div>
+			<div class="clearfix margin-b10">
+				<input type="password" class="error" value="error"> - input[type=password].error или но менее предпочтительно .error input[type=text]
+			</div>
+			<div class="clearfix margin-b10">
+				<textarea name="" id="" cols="30" rows="10" class="error">textarea.error или но менее предпочтительно .error textarea</textarea> - textarea.error или но менее предпочтительно .error textarea
+			</div>
+			<div class="clearfix margin-b10">
+				<span class="chzn-v2 error">
+					<select class="chzn">
+					  <option>1000</option>
+					</select>
+				</span>
+				<p>
+				<p>&lt;span class="chzn-v2 error"&gt;</p>
+				<p><span class="Apple-tab-span" style="white-space:pre">	</span>&lt;select class="chzn"&gt;</p>
+				<p><span class="Apple-tab-span" style="white-space:pre">		</span> &nbsp;&lt;option&gt;1000&lt;/option&gt;</p>
+				<p><span class="Apple-tab-span" style="white-space:pre">	</span>&lt;/select&gt;</p>
+				<p><span class="Apple-tab-span" style="white-space:pre"></span>&lt;/span&gt;</p>
+				</p>
+			</div>
 			
 
 		</div>  	

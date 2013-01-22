a395e1c2173d8e8e458a8c2049ab9cc49424d0a6
Kireev Alex <alexk984@gmail.com>
2013-01-22 10:33:55 +0400

    fix

--- site/frontend/components/HController.php
+++ site/frontend/components/HController.php
@@ -18,7 +18,7 @@ class HController extends CController
     public $pGallery = null;
     public $broadcast = false;
 
-    protected $r = 148;
+    protected $r = 149;
 
     public function filterAjaxOnly($filterChain)
     {

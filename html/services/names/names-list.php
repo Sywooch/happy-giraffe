7eeb71af66076f166d83e728b375b0ef9338b72a
tantalid <tantalid@mail.ru>
2013-01-22 10:14:01 +0200

    del not used images

---
+++ html/services/names/names-list.php
@@ -0,0 +1,257 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class=" ie7"> <![endif]-->
+<!--[if IE 8]>         <html class=" ie8"> <![endif]-->
+<!--[if IE 9]>         <html class=" ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/layout-header.php'; ?>
+			
+		<div id="content" class="layout-content clearfix">
+			
+			<div id="baby">
+				<div class="content-box clearfix">
+
+                    <div class="right_block">
+					    <div class="choice_name">
+					        <div class="title-in">Выбор имени <span>ребенка</span></div>
+					    </div>
+						    <ul class="choice_name_navi">
+						        <li class="active"><a href="#"><ins>По алфавиту</ins></a><span></span></li>
+						        <li><a href="#"><ins>ТОП-10</ins></a><span></span></li>
+						        <li><a href="#"><ins>По святцам</ins></a><span></span></li>
+						        <li class="like"><a href="#"><ins>Нравятся (<ins>0</ins>)</ins></a><span></span></li>
+						    </ul>
+					</div>
+					<ul class="letters">
+					    <li class="active"><a href="#">Все</a></li>
+					    <li><a href="#">А</a></li>
+					    <li><a href="#">Б</a></li>
+					    <li><a href="#">В</a></li>
+					    <li><a href="#">Г</a></li>
+					    <li><a href="#">Д</a></li>
+					    <li><a href="#">Е</a></li>
+					    <li><a href="#">Ж</a></li>
+					    <li><a href="#">З</a></li>
+					    <li><a href="#">И</a></li>
+					    <li><a href="#">К</a></li>
+					    <li><a href="#">Л</a></li>
+					    <li><a href="#">М</a></li>
+					    <li><a href="#">Н</a></li>
+					    <li><a href="#">О</a></li>
+					    <li><a href="#">П</a></li>
+					    <li><a href="#">Р</a></li>
+					    <li><a href="#">С</a></li>
+					    <li><a href="#">Т</a></li>
+					    <li><a href="#">У</a></li>
+					    <li><a href="#">Ф</a></li>
+					    <li><a href="#">Х</a></li>
+					    <li><a href="#">Ц</a></li>
+					    <li><a href="#">Ч</a></li>
+					    <li><a href="#">Э</a></li>
+					    <li><a href="#">Ю</a></li>
+					    <li><a href="#">Я</a></li>
+					</ul>
+					<div class="content_block">
+					    <div class="view_name gender-link">
+						    <p>Показывать</p>
+						    <a class="all_names active" href="#">Все имена</a>
+						    <a class="boy_names" href="#">Мальчики</a>
+						    <a class="girl_names" href="#">Девочки</a>
+						</div>
+				    <p class="names_header">Все имена</p>
+				    
+					<div class="clear"></div>
+
+						<div id="result" class="list_names">
+		 					<div id="result" class="list_names"><div class="clearfix">
+        <div class="name_block">
+    <a href="#">Аарон </a>
+    <a href="#" class="heart"></a>
+    <p>учитель, возвышенный</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="boy">Август</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>величественный, священный</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="boy">Авдей</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>служитель Бога</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Авдотья</a>
+    <a href="#" class="heart"></a>
+    <p>милая, благоволение, превосходная</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="boy">Авраам</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>отец народов</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Аврора</a>
+    <a href="#" class="heart"></a>
+    <p>предрассветный ветерок, утренняя заря, богиня зари</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Агата</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>хорошая, добрая, хорошо</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="girl">Агафья</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>добрая, благородная, хорошая</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Аглая </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>великолепная, прекрасная, блистающая</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Агнесса </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>чистая, непорочная, агнец</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="girl">Агния</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>огненная, невинность, богатая</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Агриппина</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>дикая лошадь, рожденная ногами вперед</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Ада</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>украшение, радость, бесстрашная, благородная</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="boy">Адам </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>мужчина, вылепленный из земли</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Аделаида</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>благородная, из благородного сословия</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="boy">Адольф </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>благородный волк</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="boy">Адриан </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>житель Адрии</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Азиза</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>ценная, уважаемая, дорогая, святая, ценная, редкостная</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Аида</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>соловей, возвращение к Богу, навещающая больных, праздничная, божественный праздник, королевская власть, первая дочь, божественная власть</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="girl">Айгуль</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>лунный цветок</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="boy">Акакий</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>беззлобный, хороший</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="boy">Аким</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>воздвигнутый Богом</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="girl">Аксинья</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>гостеприимная, гостья, чужестранка</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Акулина </a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>орлица, орлиная</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="girl">Алевтина</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>отражающая, свободная, защищающая</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="boy">Александр</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>защитник, мужчина</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Александра</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>защитница, защитница людей, защитница мужчин</p>
+</div>
+</div><div class="clearfix">        <div class="name_block">
+    <a href="#" class="boy">Алексей</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>защитник</p>
+</div>
+        <div class="name_block">
+    <a href="#" class="girl">Алина</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>другая, чужая, честная, чужестранка, солнечный луч, белая, светлая, благородная, великодушная, величественная, возвышенная</p>
+</div>
+        <div class="name_block last">
+    <a href="#" class="girl">Алиса</a>
+    <a href="#" class="heart empty_heart"></a>
+    <p>благородная, окрылённая, с крыльями, Бог есть спасение, истина, из благородного рода</p>
+</div>
+</div><div class="clearfix"></div>
+
+        <div class="pagination pagination-center clearfix">
+        <ul id="yw0" class="yiiPager"><li class="first hidden"><a href="#"></a></li>
+<li class="previous hidden"><a href="#"></a></li>
+<li class="page selected"><a href="#"></li>
+<li class="page"><a href="#">2</a></li>
+<li class="page"><a href="#">3</a></li>
+<li class="page"><a href="#">4</a></li>
+<li class="page"><a href="#">5</a></li>
+<li class="page"><a href="#">6</a></li>
+<li class="page"><a href="#">7</a></li>
+<li class="page"><a href="#">8</a></li>
+<li class="page"><a href="#">9</a></li>
+<li class="page"><a href="#">10</a></li>
+<li class="next"><a href="#">...</a></li>
+<li class="last"><a href="#"></a></li></ul>    </div>
+    </div>
+
+
+						</div>
+					</div>
+
+		</div>  	
+		
+		<div class="footer-push"></div>
+		
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</body>
+</html>

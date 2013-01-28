9b0f0cd6428b382c50c434150658228bf98187e2
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-01-28 10:25:17 +0400

    Merge branch 'master' of github.com:beknown/happy-giraffe
    Conflicts:
    	site/frontend/components/HController.php

--- site/frontend/modules/contest/views/default/prizes_desc/7.php
+++ site/frontend/modules/contest/views/default/prizes_desc/7.php
@@ -1,8 +1,51 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: solivager
- * Date: 1/28/13
- * Time: 10:12 AM
- * To change this template use File | Settings | File Templates.
- */
+<div class="contest-prizes-desc clearfix">
+
+    <ul>
+        <li class="clearfix" id="prize-1">
+            <div class="l-col">
+                <div class="img">
+                    <a href=""><img src="/images/prize_19.jpg" /></a>
+                </div>
+                <div class="place place-1-1"></div>
+            </div>
+            <div class="desc">
+                <div class="title">
+                    Комод для детских вещей <b>Chloe &amp; Ryan</b>
+                </div>
+                <p>Этот красивый комод украсит интерьер любой детской комнаты. Помимо того, что комод изготовлен <b>из натуральной ольхи</b>, он еще и очень вместителен - в <b>четыре ящика</b> наверняка поместится не только гардероб вашего малыша, но и все необходимые для ухода за младенцем аксессуары. </p>
+            </div>
+        </li>
+        <li class="clearfix" id="prize-2">
+            <div class="l-col">
+                <div class="img">
+                    <a href=""><img src="/images/prize_20.jpg" /></a>
+                </div>
+                <div class="place place-2"></div>
+            </div>
+            <div class="desc">
+                <div class="title">
+                    Развивающий коврик для малышей <b>Felice</b>
+                </div>
+                <p>Детский развивающий коврик “Теленок” от торговой марки Felice - это прекрасное развлечение для самых маленьких. Коврик изготовлен в <b>виде буренки</b> - когда малыш начинает трогать ее мордочку, коровка будет забавно мычать, а если кроха захочет подергать игрушку за лапку, он услышит шелест. Коврик помогает малышу развивать восприятие звуков, цветов, фактур, а также стимулировать хватательный рефлекс.</p>
+            </div>
+        </li>
+        <li class="clearfix" id="prize-3">
+            <div class="l-col">
+                <div class="img">
+                    <a href=""><img src="/images/prize_21.jpg" /></a>
+                </div>
+                <div class="place place-3"></div>
+            </div>
+            <div class="desc">
+                <div class="title">
+                    Подогреватель детского питания <b>Tigex</b>
+                </div>
+                <p>Этот современный девайс станет необходимым помощником для любой мамы!  Подогреватель детского питания Tigex <b>за полторы минуты</b> нагреет бутылочку с детским питанием до оптимальной температуры - быстро, равномерно, без перегрева, и сохраняя все необходимые витамины. Устройство особенно удобно для подогрева питания ночью. Им можно пользоваться как дома, так и в автомобиле. </p>
+            </div>
+        </li>
+
+    </ul>
+    <div class="be-active">
+        Будьте активны! <i class="icon"></i>
+    </div>
+</div>

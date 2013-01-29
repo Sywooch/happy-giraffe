38242f3850ef832e4a1618dd7580c1c27d11484d
Kireev Alex <alexk984@gmail.com>
2013-01-29 15:19:48 +0400

    wordstat debugging

--- site/frontend/config/assets.php
+++ site/frontend/config/assets.php
@@ -4,5 +4,5 @@ return array(
 	'class'=>'CAssetManager',
 	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'www-submodule' . DIRECTORY_SEPARATOR . 'assets',
 	'baseUrl' => '/assets',
-    'linkAssets' => false,
+    'linkAssets' => true,
 );

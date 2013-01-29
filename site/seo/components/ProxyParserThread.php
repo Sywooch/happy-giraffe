47410d6894162ab8e382342aa1e12ec3fb29bba1
Kireev Alex <alexk984@gmail.com>
2013-01-29 15:28:08 +0400

    wordstat fix

--- site/seo/components/ProxyParserThread.php
+++ site/seo/components/ProxyParserThread.php
@@ -201,15 +201,15 @@ class ProxyParserThread
 
     public function startTimer($title)
     {
-//        $this->_start_time = microtime(true);
-//        $this->_time_stamp_title = $title;
+        $this->_start_time = microtime(true);
+        $this->_time_stamp_title = $title;
     }
 
     public function endTimer()
     {
-//        $fh = fopen($dir = Yii::getPathOfAlias('application.runtime') . DIRECTORY_SEPARATOR . 'my_log.txt', 'a');
-//        $long_time = 1000 * (microtime(true) - $this->_start_time);
-//        fwrite($fh, $this->_time_stamp_title . ': ' . $long_time . "\n");
+        $fh = fopen($dir = Yii::getPathOfAlias('application.runtime') . DIRECTORY_SEPARATOR . 'my_log.txt', 'a');
+        $long_time = 1000 * (microtime(true) - $this->_start_time);
+        fwrite($fh, $this->_time_stamp_title . ': ' . $long_time . "\n");
     }
 
     protected function log($state)

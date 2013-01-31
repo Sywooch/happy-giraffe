7efcccd4b9c8511ef2b741420d3dd4de03bb699d
Kireev Alex <alexk984@gmail.com>
2013-01-31 16:15:23 +0400

    fix

--- site/frontend/modules/services/modules/route/views/default/map.php
+++ site/frontend/modules/services/modules/route/views/default/map.php
@@ -1,5 +1,7 @@
 <?php
-Yii::app()->clientScript->registerScriptFile('http://maps.googleapis.com/maps/api/js?key=' . Yii::app()->params['google_map_key'] . '&sensor=true');
+Yii::app()->clientScript
+    ->registerScriptFile('http://maps.googleapis.com/maps/api/js?key=' . Yii::app()->params['google_map_key'] . '&sensor=true')
+    ->registerCoreScript('jquery.ui');
 
 $start = 'Россия, Волгоград';
 $end = 'Россия, Москва';
@@ -11,8 +13,8 @@ Yii::app()->clientScript->registerScriptFile('http://maps.google.com/maps/api/js
 <div id="map_canvas" style="width:600px; height:600px"></div>
 <script type="text/javascript">
     var map;
-    var service;
     var infowindow;
+    var PlacesService;
 
     $(function () {
         var directionsService = new google.maps.DirectionsService();
@@ -21,75 +23,81 @@ Yii::app()->clientScript->registerScriptFile('http://maps.google.com/maps/api/js
         var mapOptions = {
             zoom:7,
             mapTypeId:google.maps.MapTypeId.ROADMAP
-        }
+        };
         map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
         directionsDisplay.setMap(map);
 
-        initializeAutoComplete();
+        PlacesService = new google.maps.places.PlacesService(map);
 
-<!--        var start = '--><?//=$start ?><!--';-->
-<!--        var end = '--><?//=$end ?><!--';-->
-<!--        var request = {-->
-<!--            origin:start,-->
-<!--            destination:end,-->
-<!--            travelMode:google.maps.DirectionsTravelMode.DRIVING,-->
-<!--            provideRouteAlternatives:true-->
-<!--        };-->
-<!--        directionsService.route(request, function (response, status) {-->
-<!--            if (status == google.maps.DirectionsStatus.OK) {-->
-<!--                directionsDisplay.setDirections(response);-->
-<!---->
-<!--                var rlegs = [];-->
-<!--                var steps = response.routes[0].legs[0].steps;-->
-<!--                for (var i = 0; i < steps.length; i++) {-->
-<!--                    rlegs[i] = {-->
-<!--                        distance:steps[i].distance.value,-->
-<!--                        duration:steps[i].duration.value,-->
-<!--                        t1_lat:steps[i].end_location.Ya,-->
-<!--                        t1_lng:steps[i].end_location.Za,-->
-<!--                        t2_lat:steps[i].end_location.Ya,-->
-<!--                        t2_lng:steps[i].end_location.Za-->
-<!--                    }-->
-<!---->
-<!--                }-->
-<!---->
-<!--                console.log(rlegs);-->
-<!--                $.ajax({-->
-<!--                    url:'/routes/getRoutes/',-->
-<!--                    data:{data:rlegs},-->
-<!--                    type:'POST',-->
-<!--                    success:function (response) {-->
-<!--                        $('#result').html(response);-->
-<!--                    }-->
-<!--                });-->
-<!--            }-->
-<!--        });-->
+        var start = '<?=$start ?>';
+        var end = '<?=$end ?>';
+        var request = {
+            origin:start,
+            destination:end,
+            travelMode:google.maps.DirectionsTravelMode.DRIVING,
+            provideRouteAlternatives:true
+        };
+        directionsService.route(request, function (response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+
+                var rlegs = [];
+                var steps = response.routes[0].legs[0].steps;
+                for (var i = 0; i < steps.length; i++) {
+                    rlegs[i] = {
+                        distance:steps[i].distance.value,
+                        duration:steps[i].duration.value,
+                        t1_lat:steps[i].end_location.Ya,
+                        t1_lng:steps[i].end_location.Za,
+                        t2_lat:steps[i].end_location.Ya,
+                        t2_lng:steps[i].end_location.Za
+                    }
+
+                }
+
+//                console.log(rlegs);
+//                $.ajax({
+//                    url:'/routes/getRoutes/',
+//                    data:{data:rlegs},
+//                    type:'POST',
+//                    success:function (response) {
+//                        $('#result').html(response);
+//                    }
+//                });
+            }
+        });
+
+        $('#searchTextField').autocomplete({
+            minLength:3,
+            source:function (request, response) {
+                console.log(request.term);
+
+                var defaultBounds = new google.maps.LatLngBounds(
+                        new google.maps.LatLng(51.734601, 33.178711),
+                        new google.maps.LatLng(62.536086, 137.504883)
+                );
+
+                PlacesService.search({
+                    name:request.term,
+                    bounds:defaultBounds
+                }, FoundPlaceCallback);
+            },
+            select:function (event, ui) {
+                $(this).next('input').val(ui.item.id);
+            }
+        });
     });
 
-    function initializeAutoComplete() {
+    function FoundPlaceCallback(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-        var input = document.getElementById('searchTextField');
-        var autocomplete = new google.maps.places.Autocomplete(input);
+        }
 
-        autocomplete.bindTo('bounds', map);
-
-        var infowindow = new google.maps.InfoWindow();
-        var marker = new google.maps.Marker({
-            map: map
-        });
-
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-
-            var place = autocomplete.getPlace();
-            if (!place.geometry) {
-                // Inform the user that the place was not found and return.
-                console.log('not found');
-                return;
-            }
-
-            console.log(place);
-        });
+        console.log(place);
+        console.log(status);
     }
+
+
 </script>
 
-    <div id="result"></div>
+<div id="result"></div>

a86ccba0eadb0cba4514aa68909eaf68a7253be3
tantalid <tantalid@mail.ru>
2013-02-05 12:07:12 +0200

    valentine

---
+++ html/valentine-day/valentine-day-start.php
@@ -0,0 +1,235 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+
+	
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+	<link rel="stylesheet" type="text/css" href="/stylesheets/valentine-day.css" />
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		
+		<?php 
+		$valentine = 1;
+		include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		
+		<div id="content" class="clearfix">
+			<h1 class="valentine-h1">День святого Валентина!</h1>
+			<div class="content-cols clearfix">
+				<div class="col-12">
+					<div class="valentine-spent">
+						<h2 class="valentine-spent_t">Как провести <br>День святого Валентина</h2>
+						<a href="" class="valentine-spent_img">
+							<img src="/images/valentine-day/valentine-spent_img.png" alt="">
+						</a>
+						<div class="textalign-c">
+							<a href="" class="valentine-spent_a">
+								<i class="ico-camera-big"></i>смотреть 25 фото
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="valentine-sms">
+						<a href="" class="valentine-sms_h"></a>
+						<div class="valentine-sms-b">
+							<div class="valentine-sms-b_t">«Мой любимый муж»</div>
+							<div class="valentine-sms-b_p">В День святого Валентина <br>Мой любимый муж, желаю, <br>Чтоб любовь – на все причина, <br>Счастьем сердце вдохновляла! </div>
+						</div>
+						<div class="valentine-sms-b">
+							<div class="valentine-sms-b_t">«Мой любимый муж»</div>
+							<div class="valentine-sms-b_p">В День святого Валентина <br>Мой любимый муж, желаю, <br>Чтоб любовь – на все причина, <br>Счастьем сердце вдохновляла! </div>
+						</div>
+						<div class="textalign-r">
+							<a href="" class="valentine-sms_more">Читать все SMS-ки</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="content-cols clearfix">
+				<div class="col-12">
+					<div class="valentines-best">
+						<h2 class="valentines-best_h">Лучшие валентинки</h2>
+	<script>
+		/* Пример: */
+		$(function(){
+			
+			$('.valentines-best_ul').isotope({
+			  // options
+			  itemSelector : '.valentines-best_li'
+			});
+			
+		})
+	
+	</script>
+						<ul class="valentines-best_ul clearfix">
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h309-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h309-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h164-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h309-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h164-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+							<li class="valentines-best_li">
+								<a href="" class="valentines-best_a">
+									<img src="/images/example/w220-h164-1.jpg" alt="">
+									<span class="valentines-best_btn">Отправить</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-3">
+					<div class="valentine-cook">
+						<h3 class="valentine-cook_t">Романтические блюда <br>к дню святого Валентина</h3>
+						<ul class="valentine-cook_ul">
+							<li class="valentine-cook_li clearfix">
+								<a href="" class="valentine-cook_a">
+									<span class="valentine-cook_img">
+										<img src="/images/example/w95-h71-1.jpg" alt="">
+									</span>
+									<span class="valentine-cook_desc">Салатик «Вкусная валентинка»</span>
+								</a>
+							</li>
+							<li class="valentine-cook_li clearfix">
+								<a href="" class="valentine-cook_a">
+									<span class="valentine-cook_img">
+										<img src="/images/example/w95-h71-1.jpg" alt="">
+									</span>
+									<span class="valentine-cook_desc">Салатик «Вкусная валентинка»</span>
+								</a>
+							</li>
+							<li class="valentine-cook_li clearfix">
+								<a href="" class="valentine-cook_a">
+									<span class="valentine-cook_img">
+										<img src="/images/example/w95-h71-1.jpg" alt="">
+									</span>
+									<span class="valentine-cook_desc">Салатик «Вкусная валентинка»</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+			<div class="valentine-recognition">
+				<h2 class="valentine-recognition_t">
+					<span class="valentine-recognition_t-big">Самые</span>романтичные признания
+				</h2>
+				<div class="margin-b30">
+					<img src="/images/example/w960-h537-1.jpg" alt="">
+				</div>
+				<div class="valentine-gallery">
+					<a href="" class="valentine-gallery_arrow valentine-gallery_arrow__prev disabled"></a>
+					<a href="" class="valentine-gallery_arrow valentine-gallery_arrow__next"></a>
+					<div class="valentine-gallery_hold">
+						<ul class="valentine-gallery_ul">
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li active">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+							<li class="valentine-gallery_li">
+								<a href="" class="valentine-gallery_a">
+									<span class="valentine-gallery_img">
+										<img src="/images/example/w152-h84-1.jpg" alt="">
+									</span>
+									<span class="valentine-gallery_desc">Известная история</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+		<a href="#layout" id="btn-up-page"></a>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+
+</body>
+</html>

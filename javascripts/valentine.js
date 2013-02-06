6aecf8cb8dd5e12e1b34fd7b15be0eafeada9db7
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-06 13:28:27 +0400

    sm

---
+++ javascripts/valentine.js
@@ -0,0 +1,17 @@
+var ValentineVideos = {
+    carousel : null
+}
+
+$(function() {
+    ValentineVideos.carousel = $('.valentine-gallery').jcarousel({
+        list:'.valentine-gallery_ul',
+        items:'.valentine-gallery_li'
+    });
+
+    $('.valentine-gallery_arrow__next').jcarouselControl({target:'+=1'});
+    $('.valentine-gallery_arrow__prev').jcarouselControl({target:'-=1'});
+});
+
+
+
+

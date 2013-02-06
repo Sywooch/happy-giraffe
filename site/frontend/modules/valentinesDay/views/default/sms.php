a9d2e73738b41397ddb499146517f2e0aa93f4ad
Kireev Alex <alexk984@gmail.com>
2013-02-06 13:02:26 +0400

    valentines day fixes

--- site/frontend/modules/valentinesDay/views/default/sms.php
+++ site/frontend/modules/valentinesDay/views/default/sms.php
@@ -20,7 +20,7 @@
         <?php if ($pages !== null): ?>
         <?php if ($pages->pageCount > 1): ?>
             <div class="pagination pagination-center clearfix">
-                <?php $this->widget('LinkPager', array('pages' => $pages)); ?>
+                <?php $this->widget('AlbumLinkPager', array('pages' => $pages)); ?>
             </div>
             <?php endif; ?>
         <?php endif; ?>

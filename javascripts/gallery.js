46ee9ecd857853c73a566878961a56f22d145a36
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-07 15:13:28 +0400

    sm

--- javascripts/gallery.js
+++ javascripts/gallery.js
@@ -179,7 +179,7 @@ jQuery.fn.pGallery = function(options) {
         /*$('#photo-window-in', this.window).append('<div id="loading"><div class="in"><img src="/images/test_loader.gif">Загрузка</div></div>');*/
 
         pGallery.currentPhoto = plugin.data.id;
-        $('#photo_share').attr('href', '/albums/share/id/' + pGallery.currentPhoto + '/');
+        $('#photo_download').attr('href', '/albums/download/id/' + pGallery.currentPhoto + '/');
         var newUrl = plugin.getEntityUrl() + 'photo' + plugin.data.id + '/';
         plugin.history.changeBrowserUrl(newUrl);
 

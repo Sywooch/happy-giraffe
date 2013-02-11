25fb4c787703789700b8f1928e4fd0bf87f30dc7
Kireev Alex <alexk984@gmail.com>
2013-02-11 15:32:52 +0400

    horoscope fixes

--- site/frontend/modules/services/modules/horoscope/views/default/_forecast_menu.php
+++ site/frontend/modules/services/modules/horoscope/views/default/_forecast_menu.php
@@ -11,22 +11,22 @@ $this->widget('HMenu', array(
     'seoHide'=>$hideLinks,
     'items' => array(
         array(
-            'label' =>  'на сегодня',
+            'label' =>  'На сегодня',
             'url' => $this->createUrl('today', array('zodiac' => Horoscope::model()->getZodiacSlug($model->zodiac))),
             'active' => Yii::app()->controller->action->id == 'today'
         ),
         array(
-            'label' =>  'на завтра',
+            'label' =>  'На завтра',
             'url' => $this->createUrl('tomorrow', array('zodiac' => Horoscope::model()->getZodiacSlug($model->zodiac))),
             'active' => Yii::app()->controller->action->id == 'tomorrow'
         ),
         array(
-            'label' =>  'на месяц',
+            'label' =>  'На месяц',
             'url' => $this->createUrl('month', array('zodiac' => Horoscope::model()->getZodiacSlug($model->zodiac))),
             'active' => Yii::app()->controller->action->id == 'month' && empty($_GET['month'])
         ),
         array(
-            'label' =>  'на 2013',
+            'label' =>  'На 2013',
             'url' => $this->createUrl('year', array('zodiac' => Horoscope::model()->getZodiacSlug($model->zodiac), 'year' => 2013)),
             'active' => Yii::app()->controller->action->id == 'year' && (isset($_GET['year']) && $_GET['year'] == 2013)
         ),

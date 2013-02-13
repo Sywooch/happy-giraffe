bd0588a9ad7a7926eb757fd9d81a19d8209295b5
tantalid <tantalid@mail.ru>
2013-02-13 12:47:30 +0200

    map-route

---
+++ html/services/map-route/map-route.php
@@ -0,0 +1,76 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class=" ie7"> <![endif]-->
+<!--[if IE 8]>         <html class=" ie8"> <![endif]-->
+<!--[if IE 9]>         <html class=" ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div id="content" class="layout-content clearfix">
+		
+			<div class="map-route-h">
+				<h1 class="map-route-h_t">Составь маршрут для автомобиля</h1>
+				<div class="map-route-h_form clearfix">
+					<form action="" class="">
+						<div class="map-route-h_start">A</div>
+						<input type="" class="map-route-h_itx itx-bluelight" placeholder="Откуда едем?">
+						<a href="" class="map-route-h_reverse"></a>
+						<div class="map-route-h_start map-route-h_start__b">B</div>
+						<input type="" class="map-route-h_itx itx-bluelight" placeholder="Куда едем?">
+						<button class="btn-green map-route-h_btn">Проложить <br> маршрут</button>
+					</form>
+				</div>
+			</div>
+			
+			<div class="map-route-desc">
+				<p>Одним из основных свидетельств правильного течения беременности является набор веса согласно принятым нормам. Оптимальный набор веса при беременности — это 10–14 кг. Набираемый вес при беременности складывается из нескольких показателей: вес ребенка, матки, околоплодных вод, плаценты, а также увеличиваются   молочные железы, объем циркулирующей крови, ну и, конечно, появляется запас жировой ткани. Желательно, чтобы набор веса при беременности происходил постепенно, без рывков.ткани. Желательно, чтобы набор веса при беременности</p>
+				<ul class="map-route-desc_ul clearfix">
+					<li class="map-route-desc_li">
+						<div class="map-route-desc_img">
+							<img src="/images/services/map-route/map-route-desc-1.jpg" alt="">
+						</div>
+						<strong class="map-route-desc_t">Расчет <br>расстояния</strong>
+						<div class="map-route-desc_tx">Узнавайте, сколько километров вам предстоит преодолеть</div>
+					</li>
+					<li class="map-route-desc_li">
+						<div class="map-route-desc_img">
+							<img src="/images/services/map-route/map-route-desc-2.jpg" alt="">
+						</div>
+						<strong class="map-route-desc_t">Время <br>в пути</strong>
+						<div class="map-route-desc_tx">Уточняйте время, <br>проведенное в пути	</div>
+					</li>
+					<li class="map-route-desc_li">
+						<div class="map-route-desc_img">
+							<img src="/images/services/map-route/map-route-desc-3.jpg" alt="">
+						</div>
+						<strong class="map-route-desc_t">Промежуточные <br>пункты маршрута</strong>
+						<div class="map-route-desc_tx">Смотрите населенные пункты <br>  на пути следования маршрута</div>
+					</li>
+					<li class="map-route-desc_li">
+						<div class="map-route-desc_img">
+							<img src="/images/services/map-route/map-route-desc-4.jpg" alt="">
+						</div>
+						<strong class="map-route-desc_t">Расход <br>бензина</strong>
+						<div class="map-route-desc_tx">Считайте, сколько бензина <br>вам понадобится для поездки</div>
+					</li>
+				</ul>
+			</div>
+		
+		</div>	
+		
+		<div class="footer-push"></div>
+		
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</body>
+</html>

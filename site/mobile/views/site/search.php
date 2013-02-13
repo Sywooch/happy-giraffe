4faf94db653aae1acf094fc093893d102d4902b1
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-13 16:37:51 +0400

    valentines

---
+++ site/mobile/views/site/search.php
@@ -0,0 +1,23 @@
+<div class="search-form clearfix">
+    <div class="clearfix">
+        <div class="search-form_t">Я ищу</div>
+        <div class="search-form_hold">
+            <?=CHtml::beginForm('/site/search', 'get')?>
+                <input type="text" placeholder="Поиск" class="search-form_itx" name="text">
+                <input type="submit" value="Поиск" class="search-form_btn btn-green">
+            <?=CHtml::endForm()?>
+        </div>
+    </div>
+    <div class="text-small">Всего нашлось <span class="search-highlight"><?=$allCount?></span> результатов</div>
+</div>
+
+<?php $this->widget('MListView', array(
+    'dataProvider' => $dp,
+    'itemView' => '_search',
+    'viewData' => array(
+        'full' => false,
+    ),
+    'pager' => array(
+        'class' => 'MPager',
+    ),
+)); ?>

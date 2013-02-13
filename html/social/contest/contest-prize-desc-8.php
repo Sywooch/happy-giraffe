945f96c18af58dee99072ccc4521b3bb90fa77b0
tantalid <tantalid@mail.ru>
2013-02-13 18:48:25 +0200

    contest 8

---
+++ html/social/contest/contest-prize-desc-8.php
@@ -0,0 +1,100 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div id="content">
+			
+			<div id="contest" class="contest contest-8">
+				<div class="section-banner">
+					<div class="button-holder">
+						<a href=""class="contest-button">Участвовать!</a>
+					</div>
+					<img src="/images/contest/banner-w1000-8.jpg" />
+				</div>
+				
+				<div class="contest-nav clearfix">
+					<ul>
+						<li><a href="">О конкурсе</a></li>
+						<li><a href="">Правила</a></li>
+						<li class="active"><a href="">Призы</a></li>
+						<li><a href="">Участники</a></li>
+					</ul>
+				</div>
+				
+				<div class="content-title">Вас ждут замечательные призы!</div>
+				
+				<div class="contest-prizes-desc clearfix">
+					
+					<ul>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_22.jpg" /></a>
+								</div>
+								<div class="place place-1-1"></div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Дорожная сумка  <b>Avent</b>
+								</div>
+								<p>Супер удобная сумка станет незаменимой помощницей для любой мамы. Изготовленная из легкой гигиеничной ткани с микрофиброй, оснащенная ремнем регулируемой длины и съемными карманами, сумка <b>Philips AVENT Urban Bag</b> создана для оптимального комфорта во время поездок. Уникальная функция сохранения изначальной температуры позволяет хранить сцеженное грудное молоко или молочную смесь, а также горячую кипяченую воду в течение <b>4 часов</b>. </p>
+							</div>
+						</li>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_23.jpg" /></a>
+								</div>
+								<div class="place place-2"></div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Устройство для приготовления детского питания <b>Maman</b>
+								</div>
+								<p>Устройство для приготовления детского питания Maman создано так, что смесь в керамической чаше объемом в <b>1 литр</b> не кипит, а томится, почти как в печке. Таким образом, при приготовлении еды для малыша сохраняется максимальное количество микроэлементов и витаминов. Чаша Slow cooker изготовлена из экологически чистого материала и питание в ней может оставаться теплым достаточно долгое время.</p>
+								<p>Подогреватель имеет три режима работы. </p>
+							</div>
+						</li>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_24.jpg" /></a>
+								</div>
+								<div class="place place-3"></div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Набор для пикника <b>Nuby</b>
+								</div>
+								<p>Идеальное решение для случаев, когда вам нужно выбраться из дома с ребенком и взять с собой все приспособления <b>для кормления</b>. Стильный и удобный набор посуды содержит в себе все необходимое, что только может понадобиться. В комплект включены: тренировочные ложка и вилка для малыша, поильник с носиком, секционная тарелка, контейнер с крышечкой и специальная упаковочная сумка. Набор для пикника Nuby  – удобно взять с собой все и сразу! </p>
+							</div>
+						</li>
+						
+					</ul>
+					<div class="be-active">
+						Будьте активны! <i class="icon"></i>
+					</div>
+				</div>
+
+            </div>
+				
+		</div>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</body>
+</html>

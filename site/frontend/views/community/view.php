d64a33d305d01aca8ea7d5a724b6677eefca53bb
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-14 12:25:55 +0400

    valentines

--- site/frontend/views/community/view.php
+++ site/frontend/views/community/view.php
@@ -6,6 +6,22 @@
 $this->renderPartial('_post', array('data' => $data, 'full' => true));
 
 $this->renderPartial('_prev_next', array('data' => $data));
+?>
+
+<div class="valentine-banner">
+    <div class="valentine-banner_hold">
+        <div class="valentine-banner_h">
+            <div class="valentine-banner_t">500</div>
+            валентинок
+        </div>
+        <a href="<?=Yii::app()->createUrl('/valentinesDay/valentines', array('open_photo_id' => 302388))?>" class="valentine-banner_btn btn-green btn-big">Выбери свою</a>
+    </div>
+    <a href="<?=Yii::app()->createUrl('/valentinesDay/valentines', array('open_photo_id' => 302686))?>" class="valentine-banner_img valentine-banner_img__1"></a>
+    <a href="<?=Yii::app()->createUrl('/valentinesDay/valentines', array('open_photo_id' => 302444))?>" class="valentine-banner_img valentine-banner_img__2"></a>
+    <a href="<?=Yii::app()->createUrl('/valentinesDay/valentines', array('open_photo_id' => 302437))?>" class="valentine-banner_img valentine-banner_img__3"></a>
+</div>
+
+<?php
 
 $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $data));
 

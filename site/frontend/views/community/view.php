f1aac1212ff7a8fe959e880f9379af812fb35f78
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-14 12:46:50 +0400

    valentines

--- site/frontend/views/community/view.php
+++ site/frontend/views/community/view.php
@@ -16,11 +16,11 @@ $this->renderPartial('_prev_next', array('data' => $data));
             <div class="valentine-banner_t">500</div>
             валентинок
         </div>
-        <a href="<?=Yii::app()->createUrl('/valentinesDay/default/valentines', array('open_photo_id' => 302388))?>" class="valentine-banner_btn btn-green btn-big">Выбери свою</a>
+        <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302388" class="valentine-banner_btn btn-green btn-big">Выбери свою</a>
     </div>
-    <a href="<?=Yii::app()->createUrl('/valentinesDay/default/valentines', array('open_photo_id' => 302686))?>" class="valentine-banner_img valentine-banner_img__1"></a>
-    <a href="<?=Yii::app()->createUrl('/valentinesDay/default/valentines', array('open_photo_id' => 302444))?>" class="valentine-banner_img valentine-banner_img__2"></a>
-    <a href="<?=Yii::app()->createUrl('/valentinesDay/default/valentines', array('open_photo_id' => 302437))?>" class="valentine-banner_img valentine-banner_img__3"></a>
+    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302686" class="valentine-banner_img valentine-banner_img__1"></a>
+    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302444" class="valentine-banner_img valentine-banner_img__2"></a>
+    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302437" class="valentine-banner_img valentine-banner_img__3"></a>
 </div>
 
 <?php endif; ?>

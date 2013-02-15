489c1f2657376a8ec78b5f46ec765ff4568fd348
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-15 23:40:15 +0400

    valentines

--- site/frontend/views/blog/view.php
+++ site/frontend/views/blog/view.php
@@ -4,19 +4,6 @@
 <?php $this->renderPartial('/community/_prev_next', array('data' => $data)); ?>
 <?php endif; ?>
 
-<div class="valentine-banner">
-    <div class="valentine-banner_hold">
-        <div class="valentine-banner_h">
-            <div class="valentine-banner_t">500</div>
-            валентинок
-        </div>
-        <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302388" class="valentine-banner_btn btn-green btn-big">Выбери свою</a>
-    </div>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302686" class="valentine-banner_img valentine-banner_img__1"></a>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302444" class="valentine-banner_img valentine-banner_img__2"></a>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302437" class="valentine-banner_img valentine-banner_img__3"></a>
-</div>
-
 <?php $this->widget('application.widgets.commentWidget.CommentWidget', array(
     'model' => $data,
 )); ?>
--- site/frontend/views/community/view.php
+++ site/frontend/views/community/view.php
@@ -8,19 +8,6 @@ $this->renderPartial('_post', array('data' => $data, 'full' => true));
 $this->renderPartial('_prev_next', array('data' => $data));
 ?>
 
-<div class="valentine-banner">
-    <div class="valentine-banner_hold">
-        <div class="valentine-banner_h">
-            <div class="valentine-banner_t">500</div>
-            валентинок
-        </div>
-        <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302388" class="valentine-banner_btn btn-green btn-big">Выбери свою</a>
-    </div>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302686" class="valentine-banner_img valentine-banner_img__1"></a>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302444" class="valentine-banner_img valentine-banner_img__2"></a>
-    <a href="http://www.happy-giraffe.ru/ValentinesDay/valentines/?open_photo_id=302437" class="valentine-banner_img valentine-banner_img__3"></a>
-</div>
-
 <?php
 
 $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $data));

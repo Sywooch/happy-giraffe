05d3ba97f2e17e4caa44e98616469b2834c5e766
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-02-15 17:29:56 +0400

    valentines

--- site/frontend/modules/contest/views/default/results.php
+++ site/frontend/modules/contest/views/default/results.php
@@ -121,6 +121,29 @@
                 'model' => 'Nuk',
             ),
         );
+    } elseif ($this->contest->id == 7) {
+        $prizes = array(
+            '1' => array(
+                'title' => 'Дорожная сумка',
+                'model' => 'Avent',
+            ),
+            '2' => array(
+                'title' => 'Устройство для приготовления детского питания',
+                'model' => 'Maman',
+            ),
+            '3' => array(
+                'title' => 'Набор для пикника',
+                'model' => 'Nuby',
+            ),
+            '4' => array(
+                'title' => 'Салфетка-игрушка',
+                'model' => 'Nuk',
+            ),
+            '5' => array(
+                'title' => 'Салфетка-игрушка',
+                'model' => 'Nuk',
+            ),
+        );
     }
 ?>
 
@@ -146,7 +169,7 @@
             <div class="contest-winners_frame clearfix">
                 <ul class="contest-winners_list clearfix">
                     <?php foreach ($this->contest->winners as $w): ?>
-                        <?php $this->renderPartial('_winner', array('data' => $w, 'prize' => $prizes[$w->place], 'isConsolationPrize' => $this->contest->id == 6 && ($w->place == 4 || $w->place == 5))); ?>
+                        <?php $this->renderPartial('_winner', array('data' => $w, 'prize' => $prizes[$w->place], 'isConsolationPrize' => ($this->contest->id == 6 || $this->contest->id == 7) && ($w->place == 4 || $w->place == 5))); ?>
                     <?php endforeach; ?>
                 </ul>
             </div>

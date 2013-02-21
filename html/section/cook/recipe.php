afcb708fa3701b5d0a51acf71647d3080af23837
tantalid <tantalid@mail.ru>
2013-02-21 15:23:36 +0200

    cook fix

--- html/section/cook/recipe.php
+++ html/section/cook/recipe.php
@@ -155,20 +155,20 @@
 										</a>
 										
 										<div class="big">
-											<img class="photo" src="/images/cook_recipe_img_01.jpg" width="460" />
+											<img class="result-photo" src="/images/cook_recipe_img_01.jpg" width="460" />
 										</div>
 										
 										<div class="thumbs clearfix">
 											
 											<ul class="clearfix">
 												<li>
-													<a href=""><img src="/images/cook_recipe_img_02.jpg" width="82" height="60" /></a>
+													<a href=""><img src="/images/cook_recipe_img_02.jpg" width="82" height="60"/></a>
 												</li>
-												<li><a href=""><img src="/images/cook_recipe_img_03.jpg" width="82"  height="60" /></a></li>
+												<li><a href=""><img src="/images/cook_recipe_img_03.jpg" width="82"  height="60" class="photo" /></a></li>
 												<li>
-													<a href=""><img src="/images/cook_recipe_img_02.jpg" width="82" height="60" /></a>
+													<a href=""><img src="/images/cook_recipe_img_02.jpg" width="82" height="60" class="photo" /></a>
 												</li>
-												<li><a href=""><img src="/images/cook_recipe_img_03.jpg" width="82"  height="60" /></a></li>
+												<li><a href=""><img src="/images/cook_recipe_img_03.jpg" width="82"  height="60" class="photo" /></a></li>
 												<li>
 												<a href="" class="add">
 													<span>Уже готовили</span>
@@ -200,7 +200,7 @@
 											</div>
 											<div class="recipe-description-item">
 												<div class="icon-yield tooltip" title="Количество порций"></div>
-												на 6 персон
+												на <span class="yeild">6 персон</span>
 											</div>
 										</div>
 									</div>
@@ -280,11 +280,11 @@
 									
 									<h2>Приготовление</h2>
 									
-									<div class="instructions wysiwyg-content">
+									<div class="cook-instructions wysiwyg-content">
 									
 										<p><em>Мои родители и я пошел в поход на Верхний полуостров штата Мичиган летом после моего старшего года средней школы. Это один из тех поездок, которые всегда будут оставаться со мной. Я был одним из тех больших основные переходные периоды в жизни (хотя, как всегда, я не узнал его в то время) и поездка с родителями в красивой части страны было только, что мне нужно, чтобы чувствовать себя в безопасности , безопасный и готовы отправиться в следующую главу моей жизни.</em></p>
 										
-										<ol class="cook-instructions">
+										<ol class="instructions">
 											<li class="clearfix">Курицу нарезать на кусочки, выложить в форму для запекания,
 												посолить, поперчить, добавить специи по вкусу.</li>
 											<li class="clearfix">Курицу залить пивом, поставить в духовку.</li>

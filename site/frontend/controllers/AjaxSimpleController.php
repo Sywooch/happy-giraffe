47741ec8a79cab0333a22b67931c726bfc3eb72f
Kireev Alex <alexk984@gmail.com>
2013-02-24 13:46:33 +0400

    counter fix

--- site/frontend/controllers/AjaxSimpleController.php
+++ site/frontend/controllers/AjaxSimpleController.php
@@ -17,6 +17,7 @@ class AjaxSimpleController extends CController
         Yii::import('site.seo.models.*');
 
         $referrer = Yii::app()->request->getPost('referrer');
+        $referrer = 'https://www.google.com/search?q=dgsjfgj&rlz=1C1CHHP_ruRU463RU463&oq=dgsjfgj&aqs=chrome.0.57j60j64j62l3.767&sourceid=chrome&ie=UTF-8';
         $page_url = Yii::app()->request->urlReferrer;
         if (empty($referrer) || empty($page_url) || strpos('http://www.happy-giraffe.ru/', $referrer) === 0)
             Yii::app()->end();
@@ -31,9 +32,11 @@ class AjaxSimpleController extends CController
         $se_list = SearchEngine::model()->cache(3600)->findAll();
         foreach($se_list as $se)
             if (strpos($referrer, $se->url) === 0){
-                $page = Page::getPage($page_url);
-                if ($page && in_array($page->entity , array('CommunityContent','BlogContent', 'CookRecipe')))
-                    SearchEngineVisits::addVisit($page->id);
+//                $page = Page::getPage($page_url);
+//                if ($page && in_array($page->entity , array('CommunityContent','BlogContent', 'CookRecipe')))
+//                    SearchEngineVisits::addVisit($page->id);
+
+                PageSearchView::model()->inc($page_url);
             }
     }
 }

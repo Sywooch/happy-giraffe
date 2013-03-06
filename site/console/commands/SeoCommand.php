f2008b7764153fb34745dd87ee0cf3d699280d5e
Kireev Alex <alexk984@gmail.com>
2013-03-06 21:25:21 +0400

    fix

--- site/console/commands/SeoCommand.php
+++ site/console/commands/SeoCommand.php
@@ -203,7 +203,7 @@ class SeoCommand extends CConsoleCommand
         $criteria->order = 'keyword_id ASC';
 
         $models = array(0);
-        $last_id = 348218944;
+        $last_id = 424051611;
         $i = 0;
         while (!empty($models)) {
             $criteria->condition = 'keyword_id > ' . $last_id;

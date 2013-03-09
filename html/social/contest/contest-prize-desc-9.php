004f40f8791647c8d6eac49d0fb6f23389b5d782
Tantalid <tantalid@mail.ru>
2013-03-09 13:22:05 +0200

    contest 9

---
+++ html/social/contest/contest-prize-desc-9.php
@@ -0,0 +1,107 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div id="content">
+			
+			<div id="contest" class="contest contest-9">
+				<div class="section-banner">
+					<div class="button-holder">
+						<a href=""class="contest-button">Участвовать!</a>
+					</div>
+					<img src="/images/contest/banner-w1000-9.jpg" />
+				</div>
+				
+				<div class="contest-nav clearfix">
+					<ul>
+						<li><a href="">О конкурсе</a></li>
+						<li><a href="">Правила</a></li>
+						<li class="active"><a href="">Призы</a></li>
+						<li><a href="">Участники</a></li>
+					</ul>
+				</div>
+				
+				<div class="contest-sponsor">
+					<img src="/images/contest/contest-sponsor.jpg" alt="" class="contest-sponsor_img">
+					
+					<div class="button-holder">
+						<a href=""class="contest-button">Принять участие</a>
+						<div class="contest-error-hint" style="display:block;">
+							<h4>Oops!</h4><p>Что бы проголосовать, вам нужно пройти <a href='#'>Первые 6 шагов</a> в свой анкете </p>
+						</div>
+					</div>
+				</div>
+				
+				<div class="content-title">Вас ждут замечательные призы!</div>
+				
+				<div class="contest-prizes-desc clearfix">
+					
+					<ul>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_26.jpg" /></a>
+								</div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Книга  <b>«Детство выдающихся людей»</b>
+								</div>
+								<p>Как воспитать великого математика, музыканта, оратора или спортсмена, рассказывают родители малышей со всей страны. В книге собраны самые интересные практические советы от мам и пап, которые щедро поделились своим личным опытом в воспитании, а также рекомендации специалистов. </p>
+							</div>
+						</li>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_27.jpg" /></a>
+								</div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Развивающая игра   <b>«Лабиринт»</b>
+								</div>
+								<p>Игрушка «Лабиринт» - это прекрасный способ развить в ребенке внимание, концентрацию, логику, пространственное мышление, мелкую моторику и сенсорику. Игрушка вовлекает ребенка в процесс размышления и анализа. Решив эту сложную для себя задачу, малыш почувствует гордость за результат. <br>А вы со своей стороны обязательно понаблюдайте за ним и похвалите за успехи! </p>
+							</div>
+						</li>
+						<li class="clearfix">
+							<div class="l-col">
+								<div class="img">
+									<a href=""><img src="/images/prize_28.jpg" /></a>
+								</div>
+							</div>
+							<div class="desc">
+								<div class="title">
+									Набор детского питания  <b>Heinz</b>
+								</div>
+								<p>Для полноценного здорового роста и развития малышей детская продукция Heinz дополнительно обогащена витаминами и минералами. </p>
+							</div>
+						</li>
+						
+					</ul>
+					<div class="be-active">
+						Будьте активны! <i class="icon"></i>
+					</div>
+				</div>
+
+            </div>
+				
+		</div>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</body>
+</html>

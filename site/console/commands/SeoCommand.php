7c2d90dfcf781db2349b679c43315bdfbbe22900
Kireev Alex <alexk984@gmail.com>
2013-03-10 13:25:40 +0400

    parsing fix

--- site/console/commands/SeoCommand.php
+++ site/console/commands/SeoCommand.php
@@ -203,7 +203,7 @@ class SeoCommand extends CConsoleCommand
         $criteria->order = 'keyword_id ASC';
 
         $models = array(0);
-        $last_id = 468972583;
+        $last_id = 474651339;
         $i = 0;
         while (!empty($models)) {
             $criteria->condition = 'keyword_id > ' . $last_id;

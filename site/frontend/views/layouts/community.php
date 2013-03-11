f1bbc8ceebe2102f7d4a8b2b3c71fb24f76a501f
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-03-11 14:59:28 +0400

    contest 9

--- site/frontend/views/layouts/community.php
+++ site/frontend/views/layouts/community.php
@@ -213,7 +213,7 @@
 
         <div class="box">
             <a href="<?=$this->createUrl('/contest/default/view', array('id' => 9)) ?>"><img
-                src="/images/contest/banner-w240-9.jpg"></a>
+                src="/images/contest/banner-w240-9-<?=mt_rand(1, 3)?>.jpg"></a>
         </div>
 
         <?php foreach ($this->community->banners as $b): ?>

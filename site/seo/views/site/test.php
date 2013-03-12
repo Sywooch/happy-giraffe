e0fde6f2c4d494ef6274f43250d4a674b90a386b
Kireev Alex <alexk984@gmail.com>
2013-03-12 17:43:30 +0400

    parsing fix

--- site/seo/views/site/test.php
+++ site/seo/views/site/test.php
@@ -6,24 +6,7 @@
  * @var $parser RouteChecker
  */
 
-?>
-<table>
-    <thead>
-    <tr>
-        <td></td>
-        <?php foreach ($parser->cities as $city): ?>
-            <td><?=$city[0] ?></td>
-        <?php endforeach; ?>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($parser->keywords as $keyword_id => $keyword): ?>
-        <tr>
-            <td><?=$keyword ?></td>
-            <?php foreach ($parser->cities as $city_id => $city): ?>
-                <td><?=$parser->getValue($city_id, $keyword_id) ?></td>
-            <?php endforeach; ?>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+Yii::import('site.seo.modules.promotion.components.*');
+$c = new CLinking();
+$c->start();
+

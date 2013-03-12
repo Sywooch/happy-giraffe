5a5f2997e605678ddd0f6543a55b1c215de7778f
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-03-12 23:40:24 +0400

    contest 9

--- site/frontend/components/HController.php
+++ site/frontend/components/HController.php
@@ -20,7 +20,7 @@ class HController extends CController
 
     public $body_class = 'body-club';
 
-    protected $r = 165;
+    protected $r = 166;
 
     public function filterAjaxOnly($filterChain)
     {

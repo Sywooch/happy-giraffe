46f4c22b54b94d0f06da44cc0024686c808b7f76
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-03-13 00:37:44 +0400

    sm

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -42,6 +42,7 @@
         ->registerScriptFile('/javascripts/jquery.jcarousel.js')
         ->registerScriptFile('/javascripts/jquery.jcarousel.control.js')
         ->registerScriptFile('/javascripts/jquery.tmpl.min.js')
+        ->registerScriptFile('/javascripts/jquery.lazyload.min.js')
         ->registerScriptFile('/javascripts/addtocopy.js')
         ->registerScriptFile('/javascripts/tooltipsy.min.js')
         ->registerScriptFile('http://vk.com/js/api/share.js?11')

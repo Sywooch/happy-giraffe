81346def54d0a1df67c910a3c992a56249ca789c
Kireev Alex <alexk984@gmail.com>
2013-03-18 11:53:59 +0400

    link fix

--- site/seo/modules/promotion/components/CRecipeLinking.php
+++ site/seo/modules/promotion/components/CRecipeLinking.php
@@ -371,4 +371,37 @@ class CRecipeLinking
         $link->save();
         $this->counts[0]++;
     }
+
+    public function deleteWithBadKeywords()
+    {
+        $bad_list = array('реферат', 'база отдыха', 'аллергия', 'ВИДЕО', 'кроссворд');
+        foreach ($bad_list as $bad) {
+            $criteria = new CDbCriteria;
+            $criteria->condition = 'date >= "2013-03-17"';
+            $criteria->compare('keyword.name', $bad, true);
+            $criteria->with = array('keyword');
+            $links = InnerLink::model()->findAll($criteria);
+            foreach ($links as $link) {
+                echo $link->keyword->name . '<br>';
+                $link->delete();
+            }
+
+            echo '<br><br>';
+        }
+    }
+
+    public function showResultLinks()
+    {
+        $criteria = new CDbCriteria;
+        $criteria->condition = 'date >= "2013-03-17"';
+        $criteria->order = 'rand()';
+        $criteria->limit = 10;
+        $links = InnerLink::model()->findAll($criteria);
+
+        foreach ($links as $link) {
+            echo CHtml::link($link->page->getArticleTitle(), $link->page->url) . ' - ';
+            echo $link->keyword->name . ' - ';
+            echo CHtml::link($link->pageTo->getArticleTitle(), $link->pageTo->url) . '<br>';
+        }
+    }
 }

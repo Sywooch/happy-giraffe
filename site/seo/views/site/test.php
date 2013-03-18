a01385defd2eb905a859d2c7eb531394827848f5
Kireev Alex <alexk984@gmail.com>
2013-03-18 11:48:02 +0400

    link test

--- site/seo/views/site/test.php
+++ site/seo/views/site/test.php
@@ -11,8 +11,19 @@ $criteria->order = 'rand()';
 $criteria->limit = 10;
 $links = InnerLink::model()->findAll($criteria);
 
-foreach($links as $link){
-    echo CHtml::link($link->page->getArticleTitle(), $link->page->url).' - ';
-    echo $link->keyword->name.' - ';
-    echo CHtml::link($link->pageTo->getArticleTitle(), $link->pageTo->url).'<br>';
+foreach ($links as $link) {
+    echo CHtml::link($link->page->getArticleTitle(), $link->page->url) . ' - ';
+    echo $link->keyword->name . ' - ';
+    echo CHtml::link($link->pageTo->getArticleTitle(), $link->pageTo->url) . '<br>';
+}
+
+$bad_list = array('реферат', 'база отдыха', 'аллергия', 'ВИДЕО', 'кроссворд');
+foreach ($bad_list as $bad) {
+    $criteria->compare('keyword.name', $bad, true);
+    $criteria->with = array('keyword');
+    $links = InnerLink::model()->findAll($criteria);
+    foreach ($links as $link)
+        echo $link->keyword->name . '<br>';
+
+    echo '<br><br>';
 }

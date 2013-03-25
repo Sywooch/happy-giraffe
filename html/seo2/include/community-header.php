565a7f0083de017e7812c5d193016b55cd0802f7
Tantalid <tantalid@mail.ru>
2013-03-25 15:06:40 +0200

    seo2

---
+++ html/seo2/include/community-header.php
@@ -0,0 +1,54 @@
+<div class="header-page">
+	<div class="clearfix">
+		<div class="header-page_user">
+			<a href="" class="header-page_logout"></a>
+			<div class="user-info clearfix">
+				<a href="" class="ava small"></a>
+				<div class="user-info_details">
+					<a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="header-page_logo-hold clearfix">
+			<a href="" class="header-page_logo"></a>
+			<div class="header-page_title">КОМЬЮНИТИ</div>
+			<div class="header-page_date">15 марта 2013</div>
+		</div>
+	</div>
+
+	<nav class="header-nav">
+		<ul class="header-nav_ul">
+			<li class="header-nav_li header-nav_li__tasks active">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Задания</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__links">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Ссылки</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__new-peoples">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Новые люди</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__reports">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Отчеты</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__award active">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Премия</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__secrets">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Секреты</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+</div>
---
+++ html/seo2/include/popup/popup-keyword.php
@@ -0,0 +1,15 @@
+
+    <div id="popup-keyword" class="popup popup-keyword">
+        <div class="popup_hold">
+            <div class="popup-keyword_i clearfix">
+                <a href="" class="btn-green popup-keyword_take">Взять </a>
+                <div class="keyword keyword__high">
+                    <div class="keyword_key">поющая кошка в африке</div>
+                    <div class="keyword_hold">
+                        <div class="keyword_count">4 956</div>
+                        <div class="keyword_cat">ВЧ</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

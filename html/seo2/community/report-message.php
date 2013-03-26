102492e3d5d7894336eafcbbd6fa3492165d0a02
Tantalid <tantalid@mail.ru>
2013-03-26 16:46:11 +0200

    seo2

---
+++ html/seo2/community/report-message.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+
+    <?php 
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    include $path.'/html/seo2/include/head.php'; ?>
+    
+</head>
+<body >
+<div class="layout-page">
+    <div class="layout-page_w1">
+        <?php include $path.'/html/seo2/include/community-header.php'; ?>
+        
+        <?php include $path.'/html/seo2/include/community-nav-hor.php'; ?>
+        
+        <div class="block">
+            
+            <?php include $path.'/html/seo2/include/month-list.php'; ?>
+            
+            <div class="report-icons">
+                <div class="report-icons_i">
+                    <img src="/images/seo2/ico/message-out.png" alt="" class="report-icons_img">
+                    <div class="report-icons_hold">
+                        <div class="report-icons_tx">Исх. сообщения</div>
+                        <div class="report-icons_count">87669</div>
+                    </div>
+                </div>
+                <div class="report-icons_i">
+                    <img src="/images/seo2/ico/respondent-out.png" alt="" class="report-icons_img">
+                    <div class="report-icons_hold">
+                        <div class="report-icons_tx">Исх. респонденты</div>
+                        <div class="report-icons_count">879</div>
+                    </div>
+                </div>
+                
+                <div class="report-icons_i">
+                    <img src="/images/seo2/ico/message-in.png" alt="" class="report-icons_img">
+                    <div class="report-icons_hold">
+                        <div class="report-icons_tx">Вх. сообщения</div>
+                        <div class="report-icons_count">87669</div>
+                    </div>
+                </div>
+                
+                <div class="report-icons_i">
+                    <img src="/images/seo2/ico/respondent-in.png" alt="" class="report-icons_img">
+                    <div class="report-icons_hold">
+                        <div class="report-icons_tx">Вх. респонденты</div>
+                        <div class="report-icons_count">879</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="report">
+            <table class="report_table">
+                <tr class="report_odd">
+                    <td class="report_td-date">
+                        <div class="b-date">2 МАРТА 2013</div>
+                    </td>
+                    <td class="report_td-empty"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">68654</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">54</div>
+                    </td>
+                    <td class="report_td-empty w-50"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">566654</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">56654</div>
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="report_td-date">
+                        <div class="b-date">2 МАРТА 2013</div>
+                    </td>
+                    <td class="report_td-empty"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">64</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">544357</div>
+                    </td>
+                    <td class="report_td-empty w-50"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">54</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">56654</div>
+                    </td>
+                </tr>
+                <tr class="report_odd">
+                    <td class="report_td-date">
+                        <div class="b-date">2 МАРТА 2013</div>
+                    </td>
+                    <td class="report_td-empty"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">8</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">5</div>
+                    </td>
+                    <td class="report_td-empty w-50"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">9</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">1</div>
+                    </td>
+                </tr>
+                <tr class="">
+                    <td class="report_td-date">
+                        <div class="b-date">2 МАРТА 2013</div>
+                    </td>
+                    <td class="report_td-empty"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">812367</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-out-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">567826</div>
+                    </td>
+                    <td class="report_td-empty w-50"></td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/message-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">912348</div>
+                    </td>
+                    <td class="report_td-count">
+                        <img src="/images/seo2/ico/respondent-in-small.png" alt="" class="report_count-ico">
+                        <div class="report_count">127973</div>
+                    </td>
+                </tr>
+            </table>
+            </div>
+        </div>
+
+    </div>
+</div>      
+<?php include $path.'/html/seo2/include/footer.php'; ?>
+
+
+</body>
+</html>

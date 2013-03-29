a645c801c864338f26b69fccfe9d71d76d4c8c05
Tantalid <tantalid@mail.ru>
2013-03-29 07:46:54 +0200

    seo2

---
+++ html/seo2/include/commentator-header.php
@@ -0,0 +1,44 @@
+<div class="header-page">
+	<div class="clearfix">
+		<div class="header-page_user">
+			<a href="" class="header-page_logout"></a>
+			<div class="user-info clearfix">
+				<a href="" class="ava small"></a>
+				<div class="user-info_details">
+					<a href="" class="user-info_username color-gray">Аллахвердиева Зульфия</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="header-page_logo-hold clearfix">
+			<a href="" class="header-page_logo"></a>
+			<div class="header-page_title">КОММЕНТАТОРЫ</div>
+			<div class="header-page_date">15 марта 2013</div>
+		</div>
+	</div>
+
+	<nav class="header-nav">
+		<ul class="header-nav_ul">
+			<li class="header-nav_li header-nav_li__tasks">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Задания</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__links">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Ссылки</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__reports">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Отчеты</span>
+				</a>
+			</li>
+			<li class="header-nav_li header-nav_li__award active">
+				<a href="" class="header-nav_i">
+					<span class="header-nav_tx">Премия</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+</div>

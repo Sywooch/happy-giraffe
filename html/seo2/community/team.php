94760ddd5ac03d0a4734d42abdd44302c7136cde
Tantalid <tantalid@mail.ru>
2013-03-29 14:57:51 +0200

    seo2

---
+++ html/seo2/community/team.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+
+    <?php 
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    include $path.'/html/seo2/include/head.php'; ?>
+    
+</head>
+<body >
+<div class="layout-page">
+    <div class="layout-page_w1">
+<div class="header-page">
+    <div class="clearfix">
+        <div class="header-page_user">
+            <a href="" class="header-page_logout"></a>
+            <div class="user-info clearfix">
+                <a href="" class="ava small"></a>
+                <div class="user-info_details">
+                    <a href="" class="user-info_username color-gray">Аллахвердиева Зульфия</a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="header-page_logo-hold clearfix">
+            <a href="" class="header-page_logo"></a>
+            <div class="header-page_title">КОМЬЮНИТИ</div>
+            <div class="header-page_date">15 марта 2013</div>
+        </div>
+    </div>
+
+    <nav class="header-nav">
+        <ul class="header-nav_ul">
+            <li class="header-nav_li header-nav_li__tasks">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Задания</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__links">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Ссылки</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__new-peoples">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Новые люди</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__reports">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Отчеты</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__award">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Премия</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__secrets">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Секреты</span>
+                </a>
+            </li>
+            <li class="header-nav_li header-nav_li__team active">
+                <a href="" class="header-nav_i">
+                    <span class="header-nav_tx">Команда</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+        
+        <?php include $path.'/html/seo2/include/commentator-nav-hor.php'; ?>
+        
+        <div class="block">
+
+            <?php include $path.'/html/seo2/include/month-list.php'; ?>
+            
+            <div class="report report__center">
+                <table class="report_table">
+                    <tr>
+                        <th>
+                            Комментаторы
+                        </th>
+                        <th>
+                            Записей в блог
+                        </th>
+                        <th>
+                            Записей в клуб
+                        </th>
+                        <th>
+                            Комментариев
+                        </th>
+                        <th>
+                            План
+                        </th>
+                    </tr>
+                    <tr class="report_odd">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <!-- 
+                                    Выполнено    - blog-green-small.png
+                                    Не выполнено - blog-red-small.png
+                                  -->
+                                <img src="/images/seo2/ico/blog-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/comment-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-green">Выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">4</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">684</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/club-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">654</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/comment-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-alizarin">Не выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="report_odd">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">54</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                20%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/comment-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-green">Выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">54</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                80%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">668654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
+                                100%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">6844</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/comment-red-small.png" alt="" class="report_count-ico">
+                                90%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-alizarin">Не выполнен</div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+
+    </div>
+</div>      
+<?php include $path.'/html/seo2/include/footer.php'; ?>
+
+
+</body>
+</html>
---
+++ html/seo2/include/popup/popup-commentator-task.php
@@ -0,0 +1,123 @@
+
+    <div id="popup-commentator-task" class="popup-commentator-task">
+        <div class="popup_hold tabs">
+            <ul class="popup-commentator-task_tabs clearfix">
+                <li class="popup-commentator-task_tabs-li active">
+                    <a href="javascript:void(0);" onclick="setTab(this, 1);" class="popup-commentator-task_tab popup-commentator-task_tab__donenone">Невыполнили (3)</a>
+                </li>
+                <li  class="popup-commentator-task_tabs-li">
+                    <a href="javascript:void(0);" onclick="setTab(this, 2);" class="popup-commentator-task_tab popup-commentator-task_tab__done">Выполнили (24)</a>
+                </li>
+            </ul>
+            <div class="popup-commentator-task_b-w tabs-container">
+                <div id="" class="popup-commentator-task_b tab-box tab-box-1" style="display:block">
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="" class="popup-commentator-task_b tab-box tab-box-2" >
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Аллахвердиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user popup-commentator-task_user__odd">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="popup-commentator-task_user">
+                        <div class="user-info clearfix">
+                            <a class="ava small" href=""></a>
+                            <div class="user-info_details">
+                                <a class="user-info_username" href="">Рдиева Зульфия</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

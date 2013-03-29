7762bc4ba744f80e582c69de7b618f517753b337
Tantalid <tantalid@mail.ru>
2013-03-29 12:04:19 +0200

    seo2

--- html/seo2/commentator/report-all.php
+++ html/seo2/commentator/report-all.php
@@ -14,6 +14,8 @@
     <div class="layout-page_w1">
         <?php include $path.'/html/seo2/include/commentator-header.php'; ?>
         
+        <?php include $path.'/html/seo2/include/commentator-nav-hor.php'; ?>
+        
         <div class="block">
 
             <?php include $path.'/html/seo2/include/month-list.php'; ?>
@@ -48,7 +50,7 @@
                         </td>
                         <td class="report_td-count">
                             <div class="report_count">68654</div>
-                            <div class="report_precent color-green">
+                            <div class="report_percent color-green">
                                 <!-- 
                                     Выполнено    - blog-green-small.png
                                     Не выполнено - blog-red-small.png
@@ -57,20 +59,124 @@
                                 120%
                             </div>
                         </td>
-                        
                         <td class="report_td-count">
                             <div class="report_count">68654</div>
-                            <div class="report_precent color-green">
+                            <div class="report_percent color-green">
                                 <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
                                 120%
                             </div>
                         </td>
                         <td class="report_td-count">
-                            <img src="/images/seo2/ico/comment-green-small.png" alt="" class="report_count-ico">
-                            <div class="report_count">566654</div>
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/comment-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
                         </td>
                         <td class="report_td-status">
                             <div class="report_status color-green">Выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">4</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">684</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/club-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">654</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/comment-red-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-alizarin">Не выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="report_odd">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">54</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                20%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">68654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/comment-green-small.png" alt="" class="report_count-ico">
+                                120%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-green">Выполнен</div>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td class="report_td-user">
+                            <div class="user-info clearfix">
+                                <a href="" class="ava small"></a>
+                                <div class="user-info_details">
+                                    <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">54</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/blog-red-small.png" alt="" class="report_count-ico">
+                                80%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">668654</div>
+                            <div class="report_percent color-green">
+                                <img src="/images/seo2/ico/club-green-small.png" alt="" class="report_count-ico">
+                                100%
+                            </div>
+                        </td>
+                        <td class="report_td-count">
+                            <div class="report_count">6844</div>
+                            <div class="report_percent color-alizarin">
+                                <img src="/images/seo2/ico/comment-red-small.png" alt="" class="report_count-ico">
+                                90%
+                            </div>
+                        </td>
+                        <td class="report_td-status">
+                            <div class="report_status color-alizarin">Не выполнен</div>
                         </td>
                     </tr>
                 </table>

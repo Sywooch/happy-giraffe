d5c594c81a566ab42ea3a6f8c6dfc06e55767a54
Tantalid <tantalid@mail.ru>
2013-04-01 11:08:33 +0300

    seo2

---
+++ html/seo2/best/social.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+
+    <?php 
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    include $path.'/html/seo2/include/head.php'; ?>
+    
+</head>
+<body >
+<div class="layout-page">
+    <div class="layout-page_w1">
+        <div class="header-page">
+            <div class="clearfix">
+                <div class="header-page_user">
+                    <a href="" class="header-page_logout"></a>
+                    <div class="user-info clearfix">
+                        <a href="" class="ava small"></a>
+                        <div class="user-info_details">
+                            <a href="" class="user-info_username color-gray">Аллахвердиева Зульфия</a>
+                            <div class="user-info_post">Гл.редактор </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="header-page_logo-hold clearfix">
+                    <a href="" class="header-page_logo"></a>
+                    <div class="header-page_title">Лучшее</div>
+                    <div class="header-page_date">15 марта 2013</div>
+                </div>
+            </div>
+        </div>
+        
+        <?php include $path.'/html/seo2/include/best-nav-hor.php'; ?>
+        
+        <div class="block">
+            <div class="clearfix">
+                <div class="b-best">
+                    <div class="clearfix margin-b20">
+                        <div class="b-date">22 ФЕВРАЛЯ 2013</div>
+                    </div>
+                    <div class="best-list">
+                        <ul class="best-list_ul">
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix">
+                <div class="b-best b-best__today">
+                    <div class="b-best_t">
+                        <span class="b-best_t-date">20 МАРТА 2013</span>
+                        СЕГОДНЯ
+                    </div>
+                    <div class="best-list">
+                        <ul class="best-list_ul">
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать растяжек во время беременности?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                            <li class="best-list_li b-best_i">
+                                <div class="user-info clearfix">
+                                    <a href="" class="ava small"></a>
+                                    <div class="user-info_details">
+                                        <a href="" class="user-info_username">Аллахвердиева Зульфия</a>
+                                    </div>
+                                </div>
+                                <b class="best-list_t"><a href="">Как избежать?</a></b>
+                                <div class="best-list_tx">Я, друзья давно женился! У меня семья! Но недавно вдруг влюбился, как мальчишка я! На цветок она похожа, лучше в мире нет! И она меня ...</div>
+                                <div class="b-best_overlay">
+                                    <div class="b-best_overlay-tx">Вы можете удалить</div>
+                                </div>
+                                <a href="" class="b-best_close"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>      
+<?php include $path.'/html/seo2/include/footer.php'; ?>
+
+
+</body>
+</html>
---
+++ html/seo2/include/best-nav-hor.php
@@ -0,0 +1,36 @@
+<div class="nav-hor clearfix">
+    <ul class="nav-hor_ul">
+        <li class="nav-hor_li">
+            <a href="" class="nav-hor_i">
+                <span class="nav-hor_tx">
+                <span class="ico-best ico-best__interest"></span>
+                Самое интересное
+                </span>
+            </a>
+        </li>
+        <li class="nav-hor_li active">
+            <a href="" class="nav-hor_i">
+                <span class="nav-hor_tx">
+                <span class="ico-best ico-best__blog"></span>
+                Блоги на главную
+                </span>
+            </a>
+        </li>
+        <li class="nav-hor_li">
+            <a href="" class="nav-hor_i">
+                <span class="nav-hor_tx">
+                <span class="ico-best ico-best__social"></span>
+                Посты в соцсети
+                </span>
+            </a>
+        </li>
+        <li class="nav-hor_li">
+            <a href="" class="nav-hor_i">
+                <span class="nav-hor_tx">
+                <span class="ico-best ico-best__mail"></span>
+                Посты в рассылку
+                </span>
+            </a>
+        </li>
+    </ul>
+</div>

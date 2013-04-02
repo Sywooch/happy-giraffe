59b93bac0972bf1ad21fd55f6bded7f7216080eb
Kireev Alex <alexk984@gmail.com>
2013-04-02 13:24:02 +0400

    fixes

--- javascripts/seo2/main.js
+++ javascripts/seo2/main.js
@@ -41,3 +41,9 @@ function initSelects(block) {
     }
 }
 /* /tabs */
+
+
+function refreshOdd(selecor, odd_class ){
+    $(selecor).removeClass(odd_class);
+    $(selecor+':even').addClass(odd_class);
+}

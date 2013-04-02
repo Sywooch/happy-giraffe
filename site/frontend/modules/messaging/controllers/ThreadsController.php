0a39245eccb27fa9421b5dc4326a15775fb2b3ea
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-04-02 15:04:40 +0400

    Скрыть/показать диалог

--- site/frontend/modules/messaging/controllers/ThreadsController.php
+++ site/frontend/modules/messaging/controllers/ThreadsController.php
@@ -21,7 +21,7 @@ class ThreadsController extends HController
 
         $thread = new MessagingThread();
         $threadUser1 = new MessagingThreadUser();
-        $threadUser1->user_id = 22;
+        $threadUser1->user_id = Yii::app()->user->id;
         $threadUser2 = new MessagingThreadUser();
         $threadUser2->user_id = $interlocutor_id;
         $thread->threadUsers = array($threadUser1, $threadUser2);
@@ -33,7 +33,33 @@ class ThreadsController extends HController
         echo CJSON::encode($response);
     }
 
-    public function actionLoadThread()
+    /**
+     * Изменяет статус видимости диалога
+     *
+     * @throws CHttpException
+     */
+    public function actionChangeThreadHiddenStatus()
+    {
+        $threadId = Yii::app()->request->getPost('threadId');
+        $hiddenStatus = Yii::app()->request->getPost('hiddenStatus');
+
+        $threadUser = MessagingThreadUser::model()->findByAttributes(array(
+            'thread_id' => $threadId,
+            'user_id' => Yii::app()->user->id,
+        ));
+
+        if ($threadUser === null)
+            throw new CHttpException(403, 'Thread does not exists.');
+
+        $threadUser->hidden = $hiddenStatus;
+        $success = $threadUser->save();
+        $response = array(
+            'success' => $success,
+        );
+        echo CJSON::encode($response);
+    }
+
+    public function actionChangeThreadReadStatus()
     {
 
     }

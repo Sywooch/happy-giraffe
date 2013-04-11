64afc9e3f3785217e7b596246c6b5a0fc7c6c64b
Kireev Alex <alexk984@gmail.com>
2013-04-11 17:57:57 +0400

    image optimisation

---
+++ site/console/commands/OptimisationCommand.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Class OptimisationCommand
+ *
+ * Консольные команды по различной оптимизации
+ *
+ * @author Alex Kireev <alexk984@gmail.com>
+ */
+class OptimisationCommand extends CConsoleCommand
+{
+    public function actionAvatars(){
+        Yii::import('site.frontend.extensions.EPhpThumb.*');
+        for($i=22214;$i<=22214;$i++){
+            $user = User::model()->findByPk($i);
+            if ($user !== null && !empty($user->avatar_id)){
+                $photo = $user->avatar;
+                $file_name = $photo->getAvatarPath('small');
+                $image = new EPhpThumb();
+                $image->init(); //this is needed
+                $image = $image->create($file_name);
+                $image->save($file_name);
+
+                $file_name = $photo->getAvatarPath('ava');
+                $image = new EPhpThumb();
+                $image->init(); //this is needed
+                $image = $image->create($file_name);
+                $image->save($file_name);
+            }
+
+            if ($i % 1000 == 0)
+                echo $i;
+        }
+    }
+}

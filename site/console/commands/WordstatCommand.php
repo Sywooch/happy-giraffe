6eb36a52cedad94be58d908bad79822f22c89064
Kireev Alex <alexk984@gmail.com>
2013-04-13 10:04:21 +0400

    wordstat parsing debug

--- site/console/commands/WordstatCommand.php
+++ site/console/commands/WordstatCommand.php
@@ -72,18 +72,20 @@ class WordstatCommand extends CConsoleCommand
     {
         for ($i = 0; $i < 515; $i++) {
             $ids = 1;
+            $j = 0;
             while (!empty($ids)) {
                 $ids = Yii::app()->db_keywords->createCommand()
                     ->select('id')
                     ->from('keywords')
                     ->where('wordstat >= 100 AND status IS NULL AND id >= '.($i*1000000).' AND id <'.(($i+1)*1000000))
                     ->limit(1000)
-                    ->offset($i * 1000)
+                    ->offset($j * 1000)
                     ->queryColumn();
 
                 if (!empty($ids))
                 Yii::app()->db_keywords->createCommand()->update('parsing_keywords', array('priority' => 201),
                     'keyword_id IN (' . implode(',', $ids) . ')');
+                $j++;
             }
             echo $i . "\n";
         }

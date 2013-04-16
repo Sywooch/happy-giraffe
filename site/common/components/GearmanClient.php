cf07c578beac2580fbbfa0e935e185e0753981fd
Kireev Alex <alexk984@gmail.com>
2013-04-16 16:58:42 +0400

    testing

---
+++ site/common/components/GearmanClient.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * insert Description
+ * 
+ * @author Alex Kireev <alexk984@gmail.com>
+ */
+
+class GearmanClient extends CApplicationComponent{
+    protected $client = null;
+
+
+    /**
+     * @brief Initialize component.
+     * @details in case fakeMode is enabled loading fake Queue and Exchange classes
+     */
+    public function init()
+    {
+        $this->client = new GearmanClient();
+        $this->client->addServer();
+
+        parent::init();
+    }
+
+    public function sender($text)
+    {
+        $this->client->addTask("reverse", $text, null, "1");
+    }
+
+    public function receiver()
+    {
+        $worker = new GearmanWorker();
+        $worker->addServer();
+        $worker->addFunction("reverse", array($this, "processMessage"));
+        while ($worker->work());
+    }
+
+    public function processMessage($job)
+    {
+        return strrev($job->workload());
+    }
+
+}

58dde573ebe5960e1f0f4f4e01ab4a3fcacc7e28
Kireev Alex <alexk984@gmail.com>
2013-04-16 14:37:55 +0400

    parsing

--- site/console/commands/SeoParsingCommand.php
+++ site/console/commands/SeoParsingCommand.php
@@ -63,12 +63,12 @@ class SeoParsingCommand extends CConsoleCommand
                 $sites = Site::model()->findAll('type = 1 AND url != ""');
 
             foreach ($sites as $site) {
-                $parser->start($site->id, 2013, 3, 3);
+                $parser->start($site->id, 2013, 3, 4);
                 SeoUserAttributes::setAttribute('last_li_parsed_'.date("Y-m") , $site->id, 1);
             }
         } else {
             $parser = new LiParser();
-            $parser->start($site, 2013, 1, 2);
+            $parser->start($site, 2013, 3, 4);
         }
     }
 

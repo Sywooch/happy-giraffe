05e5adc898ef9a1d7d2cc05984855be6abce44a7
Tantalid <tantalid@mail.ru>
2013-04-16 09:37:23 +0300

    scroll

---
+++ html/social/like-happy-giraffe.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		
+		<div id="content" class="layout-content clearfix">
+			
+      <div id="photo-inline" itemscope itemtype="http://schema.org/ImageObject">
+        
+        <div class="meta">
+          
+          <div class="clearfix">
+            
+            <div class="count">
+              3 фото из 158 <a href="" class="btn btn-green-smedium"><span><span>Смотреть весь альбом</span></span></a>
+            </div>
+            
+            <div class="album-title">
+              Фотоальбом к статье<br/><a href="">Прекрасные сады Болгарии</a>
+            </div>
+            
+          </div>
+          
+          <br/> <!-- <br/> для примера, нужна 1 из 3 шапок -->
+          
+          <div class="clearfix">
+            
+            <div class="count">
+              3 фото из 158 <a href="" class="btn btn-green-smedium"><span><span>Смотреть весь альбом</span></span></a>
+            </div>
+            
+            <div class="album-title">
+              Фотоальбом <a href="">Дачные вкусняшки</a>
+            </div>
+            
+          </div>
+          
+          <br/> <!-- <br/> для примера, нужна 1 из 3 шапок -->
+          
+          <div class="clearfix">
+            
+            <div class="count">
+              3 фото из 158 <a href="" class="btn btn-green-smedium"><span><span>Смотреть весь альбом</span></span></a>
+            </div>
+            
+            <div class="album-title">
+              Фотоальбом <a href="">Вторые блюда</a><br/>
+              к сервису <a href="">Оформление блюд</a>
+            </div>
+            
+          </div>
+          
+        </div>
+        
+        <div class="title"><h1 itemprop="name">Жареная картошка по-домашнему с беконом</h1></div>
+          
+        <div class="img">
+          
+          <div class="user clearfix">
+            
+            <div class="user-info clearfix">
+              <a class="ava female small"></a>
+              <div class="details">
+                <span class="icon-status status-online"></span>
+                <a href="" class="username">Александр Богоявленский</a>
+              </div>
+            </div>
+            <a href="" class="remove"><span class="icon"></span></a>
+          </div>
+          
+          <img src="/images/example/ex5.jpg" itemprop="contentURL">
+          
+          <meta itemprop="width" content="250 px">
+          <meta itemprop="height" content="250 px">
+          
+        </div>
+        
+        <div class="photo-comment" itemprop="description">Квашеная капуста с клюквой, грибами, соусом, зеленью и еще очень длинный комментарий про это оформление блюда, да нужно двести знаков для этого комментария, уже вроде набралось или нет кто будет считать</div>
+        
+      </div>
+			<div class="entry-nav clearfix">
+              <div class="next">
+                <span>Предыдущая статья</span>
+                <a href="">Лечение краснухи. Как избежать тяжелейших последствий</a>
+              </div>
+              <div class="prev">
+                <span>Следуюшая статья</span>
+                <a href="">Корь у ребенка. Симптомы кори. Лечение кори</a>
+              </div>
+            </div>
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/comment.php'; ?>
+			<a href="#like-hg-popup" class="fancy"  data-theme="white-square">Вам нравится Веселый Жираф?</a>
+		
+			<div class="fast-like-block fast-like-block__hg">
+								
+				<div class="box-2">
+					<span class="btn-icon heart active"></span>
+					<div class="fast-like-block_t-hg">Вам нравится Веселый Жираф?</div>
+				</div>
+				
+				<div class="box-1">
+
+		            <div class="share_button">
+		                <div class="fb-custom-like">
+		                    <a data-type="href" data-key="ca9f3639a03b213520dc32db0fbb4bac" onclick="return Social.showFacebookPopup(this);" class="fb-custom-text" href="http://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.happy-giraffe.ru%2Fuser%2F15835%2Fblog%2Fpost48317%2F"><i class="icon-fb"></i>Мне нравится</a>                    <div class="fb-custom-share-count">0</div>
+		                    <script type="text/javascript">
+		                        $.getJSON("http://graph.facebook.com", { id:'http://www.happy-giraffe.ru/user/15835/blog/post48317/' }, function (json) {
+		                            $('.fb-custom-share-count').html(json.shares || '0');
+		                        });
+		                    </script>
+		                </div>
+		            </div>
+
+		            <div class="share_button">
+		                <div class="vk_share_button"><table cellspacing="0" cellpadding="0" style="position: relative; width: auto; cursor: pointer; border: 0px;" id="vkshare0"><tbody><tr style="line-height: normal;"><td style="vertical-align: middle;"><a style="text-decoration:none;" onclick="return VK.Share.click(0, this);" href=""><div style="height: 21px; cursor:pointer; width: 2px; background: url(https://vk.com/images/btns.png) no-repeat -21px -42px;"></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="border: 1px solid #3b6798; cursor:pointer; border-left: 0px;"><div style="border: 1px solid #5c82ab; border-left: 0px; border-top-color: #7e9cbc; background-color: #6d8fb3; color: #fff; text-shadow: 0px 1px #45688E; height: 15px; padding: 2px 4px 0px 6px; font-family: tahoma, arial; font-size: 10px;">Мне нравится</div></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="background: url(https://vk.com/images/btns.png) 0px -21px no-repeat; cursor:pointer; width:22px; height: 21px"></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;"href=""><div style="border: 1px solid #a2b9d3; border-width: 1px 0px; cursor:pointer; background-color: #dee6f1; color:#000; height: 15px; padding: 2px 6px 0px 4px; font-size: 10px; font-weight: bold; font-family: tahoma, arial;display: none">0</div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="background: url(https://vk.com/images/btns.png) -27px -42px; cursor:pointer; width: 2px; height: 21px;display: none"></div></a></td></tr></tbody></table></div>
+		            </div>
+
+		            <div class="share_button">
+						<link rel="stylesheet" type="text/css" href="http://stg.odnoklassniki.ru/share/odkl_share.css" />
+						<a class="odkl-klass-stat" href=""><span id="odklock0">0</span></a>
+					</div>
+
+		        </div>
+				
+			</div>
+		
+		</div>
+		
+    <div class="push"></div>
+    
+	</div>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+	
+<div style="display:none">
+	<div id="like-hg-popup" class="like-hg-popup popup">
+		<div class="like-hg-popup_t">
+			<div class="like-hg-popup_heart"></div>
+			Вам нравится Веселый Жираф?
+		</div>
+		<div class="like-hg-popup_row">
+			<span class="like-hg-popup_row-i">Расскажите, пожалуйста, своим друзьям.</span>
+		</div>
+		
+		<div class="fast-like-block">
+
+				<div class="box-1">
+
+		            <div class="share_button">
+		                <div class="fb-custom-like">
+		                    <a data-type="href" data-key="ca9f3639a03b213520dc32db0fbb4bac" onclick="return Social.showFacebookPopup(this);" class="fb-custom-text" href="http://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.happy-giraffe.ru%2Fuser%2F15835%2Fblog%2Fpost48317%2F"><i class="icon-fb"></i>Мне нравится</a>                    <div class="fb-custom-share-count">0</div>
+		                    <script type="text/javascript">
+		                        $.getJSON("http://graph.facebook.com", { id:'http://www.happy-giraffe.ru/user/15835/blog/post48317/' }, function (json) {
+		                            $('.fb-custom-share-count').html(json.shares || '0');
+		                        });
+		                    </script>
+		                </div>
+		            </div>
+
+		            <div class="share_button">
+		                <div class="vk_share_button"><table cellspacing="0" cellpadding="0" style="position: relative; width: auto; cursor: pointer; border: 0px;" id="vkshare0"><tbody><tr style="line-height: normal;"><td style="vertical-align: middle;"><a style="text-decoration:none;" onclick="return VK.Share.click(0, this);" href=""><div style="height: 21px; cursor:pointer; width: 2px; background: url(https://vk.com/images/btns.png) no-repeat -21px -42px;"></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="border: 1px solid #3b6798; cursor:pointer; border-left: 0px;"><div style="border: 1px solid #5c82ab; border-left: 0px; border-top-color: #7e9cbc; background-color: #6d8fb3; color: #fff; text-shadow: 0px 1px #45688E; height: 15px; padding: 2px 4px 0px 6px; font-family: tahoma, arial; font-size: 10px;">Мне нравится</div></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="background: url(https://vk.com/images/btns.png) 0px -21px no-repeat; cursor:pointer; width:22px; height: 21px"></div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;"href=""><div style="border: 1px solid #a2b9d3; border-width: 1px 0px; cursor:pointer; background-color: #dee6f1; color:#000; height: 15px; padding: 2px 6px 0px 4px; font-size: 10px; font-weight: bold; font-family: tahoma, arial;display: none">0</div></a></td><td style="vertical-align: middle;"><a style="text-decoration:none;" href=""><div style="background: url(https://vk.com/images/btns.png) -27px -42px; cursor:pointer; width: 2px; height: 21px;display: none"></div></a></td></tr></tbody></table></div>
+		            </div>
+
+		            <div class="share_button">
+						<link rel="stylesheet" type="text/css" href="http://stg.odnoklassniki.ru/share/odkl_share.css" />
+						<a class="odkl-klass-stat" href=""><span id="odklock0">0</span></a>
+					</div>
+
+		        </div>
+				
+			</div>
+	</div>
+</div>
+</body>
+</html>
--- javascripts/im.js
+++ javascripts/im.js
@@ -1,43 +1,103 @@
+/* Высчитывание ширины контейнера .layout-container без браузерного скрола */
+function scrollFix() {
+    
+    var widthFixBlock = $('.layout-container_hold')
+    var widthFix = widthFixBlock.parent().width();
+    widthFixBlock.width(widthFix - getScrollBarWidth() + 'px'); 
+}
+
+var im = {};
+
+im.topLineMenuHeight = 85;
+im.tabsHeight = 53;
+im.userListIndentFix = 198;
+im.userListIndent = 189;
+im.minHeight = 460;
+
+im.viewHeight = function () {
+    return im.windowHeight - im.topHeight - im.bottomHeight - im.headerHeight - im.topLineMenuHeight;
+}
+
+/* Высота в sidebar списка собеседников */
+im.sidebarHeight = function () {
+        
+    /* Класс управления фиксед элементами */
+    im.im__fixed = $(".im__fixed");
+    if (im.im__fixed.length > 0) {
+        im.userList.height(im.sidebar.height() - im.userListIndentFix);
+    } else {
+        im.userList.height(im.windowHeight - im.headerHeight - im.userListIndent + im.containerScroll);
+    }
+
+}
+
+im.holdHeights = function  () {
+    im.height = im.wrapper.height();
+    if (im.height > im.viewHeight()) {
+        im.hold.height(im.height);
+    } else {
+        im.hold.height(im.viewHeight());
+    }
+}
+
+/* Поизиция скрола */
+im.scrollIm = function (){
+    if (im.containerScroll > im.headerHeight ) {
+        /* Класс управления фиксед элементами */
+        im.imBlock.addClass("im__fixed");
+        /* Позиция блока сообщений */
+        /*imTopScroll = -im.height + im.viewHeight() + im.containerScroll*2 - im.headerHeight;
+        im.wrapper.css('top', imTopScroll);*/
+
+    } else {
+        /* Класс управления фиксед элементами */
+        im.imBlock.removeClass("im__fixed");
+
+        /* Высота sidebar списка собеседников */
+        im.sidebarHeight();
+
+        /* Позиция блока сообщений */
+        /*imTopScroll = -im.height + im.viewHeight() + im.containerScroll;
+        im.wrapper.css('top', imTopScroll);*/
+
+         /* заглушка */
+         $('.im-cap').css('top', im.headerHeight + im.tabsHeight - im.containerScroll);
+    }
+}
+
 
 /* im - instant messeger for user */
 $(window).load(function() {
+    im.container = $('.layout-container');
+    im.imBlock = $(".im");
+    im.sidebar = $('.im-sidebar');
+    im.userList = $('.im-user-list');
+    im.hold = $('.im-center_middle-hold');
+    im.wrapper = $('.im-center_middle-w');
+    im.bottom = $('.im-center_bottom');
 
-    var topLineMenuHeight = 100;
-    var imTabsHeight = 53;
-    var imMinHeight = 50;
-    var imUserListIndentFix = 198;
-    var imUserListIndent = 189;
-    var imMinHeight = 460;
-
-    var container = $('.layout-container');
-    var im = $(".im");
-    var imSidebar = $('.im-sidebar');
-    var imUserList = $('.im-user-list');
-    var imHold = $('.im-center_middle-hold');
-    var imWrapper = $('.im-center_middle-w');
-    var imBottom = $('.im-center_bottom');
-    var im__fixed = $(".im__fixed");
-
-    var windowHeight = $(window).height();
-    var headerHeight = $('#header-new').height();
-    var imBottomHeight = $('.im-center_bottom').height();
-    var imTopHeight = $('.im-center_bottom').height();
-    var imHeight = imWrapper.height();
-    var imViewHeight = windowHeight - imTopHeight - imBottomHeight - headerHeight - topLineMenuHeight; 
+    im.windowHeight = $(window).height();
+    im.headerHeight = $('#header-new').height();
+    im.topHeight = $('.im-center_top').height();
+    im.bottomHeight = im.bottom.height();
+    im.height = im.wrapper.height();
+    im.containerScroll = im.container.scrollTop();
 
     /* Высчитывание ширины контейнера .layout-container без браузерного скрола */
     scrollFix();
 
-    imWrapper.css('top',  -imHeight + imViewHeight);
+    /*im.wrapper.css('top',  -im.height + im.viewHeight());*/
 
-    imHoldHeights ();
+    im.holdHeights ();
+    /* Прокручивание в конец страницы */
+    im.container.scrollTop(im.container.height());
 
     /* Высота sidebar списка собеседников */
-    imSidebarHeight(0, headerHeight);
+    im.sidebarHeight(0);
 
-    container.bind('scroll', function () {
-        var containerScroll = container.scrollTop();
-        scrollIm (containerScroll, headerHeight);
+    im.container.bind('scroll', function () {
+        im.containerScroll = im.container.scrollTop();
+        im.scrollIm ();
     });
 
 
@@ -51,7 +111,7 @@ $(window).load(function() {
 
     /* toggle блок инфо собедника .im-panel */
     var mousePosY = false;
-    im.on("mousedown", ".im-panel .im_toggle", function(e) {
+    im.imBlock.on("mousedown", ".im-panel .im_toggle", function(e) {
         e.preventDefault();
         mousePosY = e.pageY;
     }).on("mouseup", function(e) {
@@ -62,21 +122,21 @@ $(window).load(function() {
     });
 
     /* Изменение отступа от wysywig до конца стрнаицы */
-    im.on("mousedown", ".im-center_bottom .im_toggle", function(e) {
+    im.imBlock.on("mousedown", ".im-center_bottom .im_toggle", function(e) {
         e.preventDefault();
         var imBottomHold = $('.im-center_bottom-hold');
         var imMiddle = $('.im-center_middle');
-        im.on("mousemove", function(e) {
-            imBottomMarg = windowHeight - e.pageY - 15;
-            if (imBottomMarg > 11 && imBottomMarg < windowHeight - imBottomHold.height() - imMinHeight) {
+        im.imBlock.on("mousemove", function(e) {
+            imBottomMarg = im.windowHeight - e.pageY - 15;
+            if (imBottomMarg > 11 && imBottomMarg < im.windowHeight - imBottomHold.height() - im.minHeight) {
                 imBottomHold.stop().animate({'margin-bottom': imBottomMarg},10);
-                imBottomHeight = imBottom.height();
-                imMiddle.css('padding-bottom', imBottomHeight);
+                im.bottomHeight = im.bottom.height();
+                imMiddle.css('padding-bottom', im.bottomHeight);
             }
         });
 
     }).on("mouseup", function(e) {
-        im.off("mousemove");
+        im.imBlock.off("mousemove");
     });
 
     /* Список скрытых пользователей в сайдбаре */
@@ -88,72 +148,12 @@ $(window).load(function() {
     $(window).resize(function() {
         scrollFix();
 
-        containerScroll = container.scrollTop();
-        windowHeight = $(window).height();
-        imHoldHeights ();
-            /* Высота видимой части сообщений */
-            imViewHeight = windowHeight - imTopHeight - imBottomHeight - headerHeight - topLineMenuHeight;
+        im.containerScroll = im.container.scrollTop();
+        im.windowHeight = $(window).height();
+        im.holdHeights();
 
-        scrollIm (containerScroll);
-        imSidebarHeight(containerScroll);
+        im.scrollIm ();
+        im.sidebarHeight();
     });
-
-    /* Высота в sidebar списка собеседников */
-    function imSidebarHeight(containerScroll, headerHeight) {
-        
-        /* Класс управления фиксед элементами */
-        var im__fixed = $(".im__fixed");
-        if (im__fixed.length > 0) {
-            imUserList.height(imSidebar.height() - imUserListIndentFix);
-        } else {
-            imUserList.height(windowHeight - headerHeight - imUserListIndent + containerScroll);
-        }
-
-    }
-
-    /* Поизиция скрола */
-    function scrollIm (containerScroll){
-        if (containerScroll > headerHeight ) {
-            /* Класс управления фиксед элементами */
-            im.addClass("im__fixed");
-            /* Позиция блока сообщений */
-            imTopScroll = -imHeight + imViewHeight + containerScroll*2 - headerHeight;
-            imWrapper.css('top', imTopScroll);
-
-            /*imTopScroll = messagesHeight - messagesViewHeight - contanerScroll*2 - headerHeight - imBottomHeight;
-            imWrapper.css('bottom', imTopScroll);*/
-
-        } else {
-            /* Класс управления фиксед элементами */
-            im.removeClass("im__fixed");
-
-            /* Высота sidebar списка собеседников */
-            imSidebarHeight(containerScroll, headerHeight);
-
-            /* Позиция блока сообщений */
-            imTopScroll = -imHeight + imViewHeight + containerScroll;
-            imWrapper.css('top', imTopScroll);
-
-             /* заглушка */
-             $('.im-cap').css('top', headerHeight + imTabsHeight - containerScroll);
-        }
-    }
-
-    function imHoldHeights () {
-        if (imHeight > imViewHeight) {
-            imHold.height(imHeight);
-        } else {
-            imHold.height(imViewHeight);
-        }
-    }
-
-    /* Высчитывание ширины контейнера .layout-container без браузерного скрола */
-    function scrollFix() {
-        
-        var widthFixBlock = $('.layout-container_hold')
-        var widthFix = widthFixBlock.parent().width();
-        widthFixBlock.width(widthFix - getScrollBarWidth() + 'px'); 
-    }
-
 
 });

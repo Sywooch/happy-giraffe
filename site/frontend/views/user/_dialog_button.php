c8cddb364ab96dd5760f464977ca5413ec8733ce
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-04-17 22:56:10 +0400

    Баннеры

--- site/frontend/views/user/_dialog_button.php
+++ site/frontend/views/user/_dialog_button.php
@@ -1,7 +1,7 @@
 <?php if (Yii::app()->user->isGuest): ?>
 <?php echo CHtml::link('', '#login', array('class' => 'new-message fancy tooltip', 'title'=>'Написать сообщение', 'data-theme'=>"white-square")); ?>
 <?php else: ?>
-<?php if (Yii::app()->user->id != 12936 || Yii::app()->user->id != 22): ?>
+<?php if (Yii::app()->user->id != 12936 && Yii::app()->user->id != 22): ?>
 <?php echo CHtml::link('', 'javascript:void(0)', array('class' => 'new-message tooltip', 'title'=>'Написать сообщение', 'onclick' => 'Messages.open(' . $user->id . ')')); ?>
 <?php else: ?>
     <?php echo CHtml::link('', array('/messaging/default/index', 'interlocutorId' => $user->id), array('class' => 'new-message tooltip', 'title'=>'Написать сообщение')); ?>

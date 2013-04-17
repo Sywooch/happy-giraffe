fd22a624e0c4d3ab0afacce78cc9ddbee1b7a762
Kireev Alex <alexk984@gmail.com>
2013-04-17 11:59:51 +0400

    testing

--- site/seo/components/wordstat/WordstatTaskCreator.php
+++ site/seo/components/wordstat/WordstatTaskCreator.php
@@ -15,18 +15,21 @@ class WordstatTaskCreator
         for ($i = 0; $i < 20; $i++) {
             $text = 'task.' . $i;
             $job_handle = $client->doBackground("simple_parsing", serialize($text));
-            if ($client->returnCode() != GEARMAN_SUCCESS){
+            if ($client->returnCode() != GEARMAN_SUCCESS) {
                 echo 'send task fail';
                 Yii::app()->end();
             }
             $this->jobs [] = $job_handle;
         }
 
-        while(1){
+        while (1) {
             sleep(1);
-            foreach($this->jobs as $job_handle){
+            foreach ($this->jobs as $key => $job_handle) {
                 $stat = $client->jobStatus($job_handle);
-                var_dump($stat);
+                if ($stat[0] === false) {
+                    echo $job_handle . " ended\n";
+                    unset($this->jobs[$key]);
+                }
             }
         }
     }

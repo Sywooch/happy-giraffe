7280f55016415107dde5f4e6280c67d15612a8ba
Kireev Alex <alexk984@gmail.com>
2013-04-18 13:44:51 +0400

    promotion feature

--- site/seo/views/site/test.php
+++ site/seo/views/site/test.php
@@ -12,6 +12,7 @@ $keywords = $p->compareDates('2013-04-08', '2013-04-17');
     <thead>
         <tr>
             <th>Ключевое слово</th>
+            <th>Url</th>
             <th>20130408</th>
             <th>20130417</th>
             <th>Изменение %</th>
@@ -22,6 +23,7 @@ $keywords = $p->compareDates('2013-04-08', '2013-04-17');
         <?php $diff = round(100 * ($stat[1] - $stat[0]) / ($stat[0] + $stat[1])) ?>
         <tr>
             <td><?php $k = Keyword::model()->findByPk($keyword_id); echo $k->name; ?></td>
+            <td><?=$stat[3] ?></td>
             <td><?=$stat[0] ?></td>
             <td><?=$stat[1] ?></td>
             <td<?php if ($diff < -30) echo ' style="color:red;"' ?>><?= ($diff > 0) ? '+' . $diff : $diff ?> %</td>

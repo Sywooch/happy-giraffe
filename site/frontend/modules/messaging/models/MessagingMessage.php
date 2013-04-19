2f8c6209aa4d317b28056b3dfa08aeacb5675396
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-04-19 09:14:17 +0400

    Баннеры

--- site/frontend/modules/messaging/models/MessagingMessage.php
+++ site/frontend/modules/messaging/models/MessagingMessage.php
@@ -176,7 +176,7 @@ class MessagingMessage extends CActiveRecord
             'id' => $this->id,
             'author_id' => $this->author_id,
             'text' => $this->text,
-            'created' => Yii::app()->dateFormatter->format("d MMMM yyyy, H:mm", time()),
+            'created' => Yii::app()->dateFormatter->format("d MMMM yyyy, H:mm", is_numeric($this->created) ? $this->created : time()),
             'read' => $this->isReadByInterlocutor,
             'images' => $images,
         );

d45c2d53014bc7599350b647f42f832c25c057ef
Kireev Alex <alexk984@gmail.com>
2013-04-23 11:23:54 +0400

    photo gallery fix

--- site/frontend/views/community/_gallery.php
+++ site/frontend/views/community/_gallery.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * @var $data CommunityContent
+ */
+
+//только для новых галерей - скрываем их код
+$is_new_gallery = ($data->gallery->id > 1404);
+?><?php if ($is_new_gallery) Yii::app()->controller->beginWidget('SeoContentWidget'); ?>
 <div class="gallery-box">
     <a class="img" data-id="<?=$data->gallery->items[0]->photo->id?>">
         <?php echo CHtml::image($data->gallery->items[0]->photo->getPreviewUrl(695, 463, Image::WIDTH)) ?>
@@ -11,3 +19,4 @@
         <i class="icon-play"></i>
     </a>
 </div>
+<?php if ($is_new_gallery) Yii::app()->controller->endWidget(); ?>

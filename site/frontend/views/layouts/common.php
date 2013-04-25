7239447f4e53d8a90ca9a2024332b1fd6b440ee6
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-04-25 14:12:06 +0400

    Баннеры

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -104,7 +104,7 @@
                 <li class="i-broadcast new top-line-menu_nav_li js-tooltipsy" title="Что нового">
                     <a href="<?=$this->createUrl('/whatsNew/default/index')?>"><i class="icon-broadcast"></i></a>
                 </li>
-                <?php if (! in_array(Yii::app()->user->id, array(12936, 22, 9990, 83)) && ! Yii::app()->user->checkAccess('commentator_panel')): ?>
+                <?php if (! in_array(Yii::app()->user->id, array(12936, 22, 9990)) && ! Yii::app()->user->checkAccess('commentator_panel')): ?>
                 <li class="i-dialogs top-line-menu_nav_li js-tooltipsy<?php if ($imCount > 0): ?> new<?php endif; ?>" title="Мои диалоги">
                     <a href="javascript:void(0)" onclick="Messages.toggle()">
                         <i class="icon-dialogs"></i>

e4d9c820ecd316e09401c3434944c54f905dd577
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-04-25 17:29:04 +0400

    Баннеры

--- site/frontend/views/banners/community_24_700x346.php
+++ site/frontend/views/banners/community_24_700x346.php
@@ -1,23 +1,5 @@
 <!--/* OpenX Javascript Tag v2.8.10 */-->
 
-<!--/*
-  * The backup image section of this tag has been generated for use on a
-  * non-SSL page. If this tag is to be placed on an SSL page, change the
-  *   'http://openx.happy-giraffe.ru/www/delivery/...'
-  * to
-  *   'https://openx.happy-giraffe.ru/www/delivery/...'
-  *
-  * This noscript section of this tag only shows image banners. There
-  * is no width or height in these banners, so if you want these tags to
-  * allocate space for the ad before it shows, you will need to add this
-  * information to the <img> tag.
-  *
-  * If you do not want to deal with the intricities of the noscript
-  * section, delete the tag (from <noscript>... to </noscript>). On
-  * average, the noscript tag is called from less than 1% of internet
-  * users.
-  */-->
-
 <script type='text/javascript'><!--//<![CDATA[
 var m3_u = (location.protocol=='https:'?'https://openx.happy-giraffe.ru/www/delivery/ajs.php':'http://openx.happy-giraffe.ru/www/delivery/ajs.php');
 var m3_r = Math.floor(Math.random()*99999999999);
@@ -32,4 +14,4 @@ if (document.referrer) document.write ("&amp;referer=" + escape(document.referre
 if (document.context) document.write ("&context=" + escape(document.context));
 if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
 document.write ("'><\/scr"+"ipt>");
-//]]>--></script><noscript><a href='http://openx.happy-giraffe.ru/www/delivery/ck.php?n=a01faebf&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_top'><img src='http://openx.happy-giraffe.ru/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a01faebf' border='0' alt='' /></a></noscript>
+//]]>--></script><noscript><a href='http://openx.happy-giraffe.ru/www/delivery/ck.php?n=a888df5b&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_top'><img src='http://openx.happy-giraffe.ru/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a888df5b' border='0' alt='' /></a></noscript>

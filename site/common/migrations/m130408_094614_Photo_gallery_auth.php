009fcab8fa8f71e34dd9e2c07f9c44ab227b7ef7
Kireev Alex <alexk984@gmail.com>
2013-05-07 09:14:12 +0400

    migration fix

--- site/common/migrations/m130408_094614_Photo_gallery_auth.php
+++ site/common/migrations/m130408_094614_Photo_gallery_auth.php
@@ -5,16 +5,16 @@ class m130408_094614_Photo_gallery_auth extends CDbMigration
 
 	public function up()
 	{
-        $this->execute("
-        INSERT INTO `auth__items` (`name`, `type`, `description`, `bizrule`, `data`) VALUES ('photo_gallery', '0', 'Размещение фотогалерей', NULL, NULL);
-
-        INSERT INTO `auth__items_childs` (`parent`, `child`) VALUES
-        ('editor', 'photo_gallery'),
-        ('administrator', 'photo_gallery'),
-        ('moderator', 'photo_gallery'),
-        ('supermoderator', 'photo_gallery'),
-        ('virtual_user', 'photo_gallery');
-        ");
+//        $this->execute("
+//        INSERT INTO `auth__items` (`name`, `type`, `description`, `bizrule`, `data`) VALUES ('photo_gallery', '0', 'Размещение фотогалерей', NULL, NULL);
+//
+//        INSERT INTO `auth__items_childs` (`parent`, `child`) VALUES
+//        ('editor', 'photo_gallery'),
+//        ('administrator', 'photo_gallery'),
+//        ('moderator', 'photo_gallery'),
+//        ('supermoderator', 'photo_gallery'),
+//        ('virtual_user', 'photo_gallery');
+//        ");
 	}
 
 	public function down()

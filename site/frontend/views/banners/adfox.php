c780659d067934a9039dbcf3dfad42cffae2b67f
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-05-15 13:08:03 +0400

    Баннеры

---
+++ site/frontend/views/banners/adfox.php
@@ -0,0 +1,20 @@
+<!--AdFox START-->
+<!--giraffe-->
+<!--Площадка: Весёлый Жираф / * / *-->
+<!--Тип баннера: Безразмерный 240x400-->
+<!--Расположение: &lt;сайдбар&gt;-->
+<script type="text/javascript">
+    <!--
+    if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+    if (typeof(document.referrer) != 'undefined') {
+        if (typeof(afReferrer) == 'undefined') {
+            afReferrer = escape(document.referrer);
+        }
+    } else {
+        afReferrer = '';
+    }
+    var addate = new Date();
+    document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '"><\/scr' + 'ipt>');
+    // -->
+</script>
+<!--AdFox END-->

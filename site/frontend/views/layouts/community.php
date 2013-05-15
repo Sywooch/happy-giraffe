025787c4bb500316a5dd4524bfbd793442343316
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-05-15 12:51:42 +0400

    Баннеры

--- site/frontend/views/layouts/community.php
+++ site/frontend/views/layouts/community.php
@@ -206,34 +206,28 @@
             <?php endif; ?>
         <?php endif; ?>
 
-        <!--AdFox START-->
-        <!--giraffe-->
-        <!--Площадка: Весёлый Жираф / * / *-->
-        <!--Тип баннера: Безразмерный 240x400-->
-        <!--Расположение: &lt;сайдбар&gt;-->
-        <!-- ________________________AdFox Asynchronous code START__________________________ -->
-        <script type="text/javascript">
-            <!--
-            if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
-            if (typeof(document.referrer) != 'undefined') {
-                if (typeof(afReferrer) == 'undefined') {
-                    afReferrer = escape(document.referrer);
+        <div style="margin-bottom: 40px;">
+            <!--AdFox START-->
+            <!--giraffe-->
+            <!--Площадка: Весёлый Жираф / * / *-->
+            <!--Тип баннера: Безразмерный 240x400-->
+            <!--Расположение: &lt;сайдбар&gt;-->
+            <script type="text/javascript">
+                <!--
+                if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+                if (typeof(document.referrer) != 'undefined') {
+                    if (typeof(afReferrer) == 'undefined') {
+                        afReferrer = escape(document.referrer);
+                    }
+                } else {
+                    afReferrer = '';
                 }
-            } else {
-                afReferrer = '';
-            }
-            var addate = new Date();
-            var dl = escape(document.location);
-            var pr1 = Math.floor(Math.random() * 1000000);
-
-            document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
-            document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
-
-            AdFox_getCodeScript(1,pr1,'http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;dl='+dl+'&amp;pr1='+pr1);
-            // -->
-        </script>
-        <!-- _________________________AdFox Asynchronous code END___________________________ -->
-        <!--AdFox END-->
+                var addate = new Date();
+                document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '"><\/scr' + 'ipt>');
+                // -->
+            </script>
+            <!--AdFox END-->
+        </div>
 
         <?php if (false): ?>
             <?php if (in_array($this->community->id, array(1, 2))): ?>

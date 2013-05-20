9f64b36af0e598b3a3010930d7598f42c31625a2
choojoy-work <nikita@happy-giraffe.ru>
2013-05-20 17:21:19 +0400

    banners

--- site/frontend/views/albums/w_photo.php
+++ site/frontend/views/albums/w_photo.php
@@ -129,7 +129,7 @@ if (get_class($model) == 'Contest') {
             pGallery.start = <?=$photo->id?>;
         </script>
 
-        <?php if (in_array(get_class($model), array('CommunityContentGallery', 'Contest')) && false): ?>
+        <?php if (in_array(get_class($model), array('CommunityContentGallery', 'Contest'))): ?>
             <div class="content-cols clearfix">
                 <div class="col-12 photo-banner-hold ">
 
@@ -147,7 +147,7 @@ if (get_class($model) == 'Contest') {
                 </div>
 
                 <div class="col-3">
-                    <div class="margin-t60">
+                    <div class="margin-t60" id="AdfoxAjax">
                         <!--AdFox START-->
                         <!--giraffe-->
                         <!--Площадка: Весёлый Жираф / * / *-->
@@ -170,13 +170,14 @@ if (get_class($model) == 'Contest') {
                             var dl = escape(document.location);
                             var pr1 = Math.floor(Math.random() * 1000000);
 
-                            document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
-                            document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
+                            $('#AdfoxAjax').append('<div id="AdFox_banner_'+pr1+'"><\/div>');
+                            $('#AdfoxAjax').append('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
 
                             AdFox_getCodeScript(1,pr1,'http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;dl='+dl+'&amp;pr1='+pr1);
                             // -->
                         </script>
                         <!-- _________________________AdFox Asynchronous code END___________________________ -->
+
                     </div>
                 </div>
             </div>

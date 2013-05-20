505a5af29ef198dd4a07a436f7a54cd174445e43
Kireev Alex <alexk984@gmail.com>
2013-05-20 12:53:48 +0400

    notification fixes

--- site/frontend/modules/notifications/views/default/types/type_0.php
+++ site/frontend/modules/notifications/views/default/types/type_0.php
@@ -9,7 +9,7 @@
     <div class="user-notice-list_date"><?= HDate::GetFormattedTime($model->updated) ?></div>
     <div class="user-notice-list_deed">
         <span class="user-notice_ico user-notice_ico__comment"></span>
-        <a href="<?=$model->getUrl() ?>" class="user-notice-list_a-big"><?= $model->count ?></a>
+        <a href="<?=$model->getUrl() ?>" class="user-notice-list_a-big"><?= $model->getVisibleCount() ?></a>
         <span class="user-notice-list_deed-desc"><?= Yii::app()->user->getModel()->first_name ?>, к вашей записи написаны новые комментарии</span>
     </div>
     <?php $this->renderPartial('content_preview', array('entity' => $model->entity, 'entity_id' => $model->entity_id)); ?>
--- site/frontend/modules/notifications/views/default/types/type_1.php
+++ site/frontend/modules/notifications/views/default/types/type_1.php
@@ -9,7 +9,7 @@
     <div class="user-notice-list_date"><?= HDate::GetFormattedTime($model->updated) ?></div>
     <div class="user-notice-list_deed">
         <span class="user-notice_ico user-notice_ico__answer"></span>
-        <a href="<?=$model->getUrl() ?>" class="user-notice-list_a-big"><?= $model->count ?></a>
+        <a href="<?=$model->getUrl() ?>" class="user-notice-list_a-big"><?= $model->getVisibleCount() ?></a>
         <span class="user-notice-list_deed-desc">новые ответы на ваш комментарий</span>
     </div>
     <?php $this->renderPartial('comment_preview', array('comment_id' => $model->comment_id)); ?>

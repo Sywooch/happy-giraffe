443cd3718fabdcef6fe851a889bae4d725d08bbe
Tantalid <tantalid@mail.ru>
2013-05-21 15:24:13 +0300

    rambler

--- block/global/footer.php
+++ block/global/footer.php
@@ -1,12 +1,14 @@
 <div id="footer" class="layout-footer clearfix">
 	
-	<div class="clearfix">
+<!-- 	<div class="clearfix">
 		<a href="" class="mobile-version">Мобильная версия</a>
 	</div>
 	<div class="a-right">
 		<a href="">Политика конфедециальности</a> &nbsp; | &nbsp; <a href="">Пользовательское соглашение</a>
-	</div>
+	</div> -->
+	<div class="float-r margin-r20 color-gray">Написать нам: <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a></div>
 	
+	<div id="counter-rambler" class="float-r margin-r15" style=""></div>
 	<div class="copy">
 		<p>Весёлый жираф &nbsp; © 2012 &nbsp; Все права защищены 
 			<img src="/images/icon-18+.png" alt="" class="icon-18"/>
@@ -15,3 +17,22 @@
 	</div>
 	
 </div>
+
+<!-- Счетчик рамблер  -->
+<script type="text/javascript">
+var _top100q = _top100q || [];
+
+_top100q.push(["setAccount", "2900190"]);
+_top100q.push(["trackPageviewByLogo", document.getElementById("counter-rambler")]);
+
+
+(function(){
+ var top100 = document.createElement("script"); top100.type = "text/javascript";
+
+ top100.async = true;
+ top100.src = ("https:" == document.location.protocol ? "https:" : "http:") + "//st.top100.ru/top100/top100.js";
+ var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(top100, s);
+})();
+</script>
+
+<!-- /Счетчик рамблер  -->

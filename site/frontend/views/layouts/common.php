cfa8062e9925b93e79dd648da5064a426fc09759
nikita@happy-giraffe.ru <nikita@happy-giraffe.ru>
2013-05-21 16:50:35 +0400

    Баннеры

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -197,13 +197,18 @@
 <?php if (! $this->tempLayout): ?>
 <div id="footer" class="layout-footer clearfix">
 
-    <div class="a-right">
-        <!--Отработало за <?=sprintf('%0.5f',Yii::getLogger()->getExecutionTime())?> c -->
-        <!--<a>Политика конфиденциальности</a> &nbsp; | &nbsp; <a>Пользовательское соглашение</a> &nbsp; | &nbsp; -->
+    <div class="float-r margin-r20 color-gray">
+        Написать нам: <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a>
     </div>
-
+    <div style="float:right;margin:0 60px 0 10px;line-height:normal;">
+        <div id="counter-rambler" class="" style="display:inline-block;vertical-align:middle;margin:5px 10px 0 0;"></div>
+        <a href="http://www.rambler.ru/" >Партнер «Рамблера»</a>
+    </div>
     <div class="copy">
-        <p>Весёлый жираф &nbsp; © 2012 &nbsp; Все права защищены <img src="/images/icon-18+.png" alt="" class="icon-18"/><a href="<?=$this->createUrl('/site/moderationRules') ?>">Правила модерации</a><span style="float: right;margin-right:20px;">Написать нам: <a href="mailto:info@happy-giraffe.ru">info@happy-giraffe.ru</a></span></p>
+        <p>Весёлый жираф &nbsp; © 2012 &nbsp; Все права защищены
+            <img src="/images/icon-18+.png" alt="" class="icon-18"/>
+            <a href="">Правила модерации</a>
+        </p>
     </div>
 
 </div>
@@ -264,6 +269,22 @@
             var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+    <script type="text/javascript">
+        var _top100q = _top100q || [];
+
+        _top100q.push(["setAccount", "2900190"]);
+        _top100q.push(["trackPageviewByLogo", document.getElementById("counter-rambler")]);
+
+
+        (function(){
+            var top100 = document.createElement("script"); top100.type = "text/javascript";
+
+            top100.async = true;
+            top100.src = ("https:" == document.location.protocol ? "https:" : "http:") + "//st.top100.ru/top100/top100.js";
+            var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(top100, s);
         })();
     </script>
 

299939eda8455e40efe2ddd16dd267b47b06d14b
choojoy-work <nikita@happy-giraffe.ru>
2013-05-26 16:04:02 +0400

    view

--- site/frontend/modules/favourites/components/FavouritesManager.php
+++ site/frontend/modules/favourites/components/FavouritesManager.php
@@ -8,30 +8,44 @@
  */
 class FavouritesManager
 {
-    public static function getByUserId($userId, $entity, $tagId, $query)
+    const FAVOURITES_PER_PAGE = 15;
+
+    public static function getByUserId($userId, $entity, $tagId, $query, $offset)
     {
-        $criteria = new CDbCriteria(array(
-            'condition' => 'user_id = :user_id',
-            'params' => array(':user_id' => $userId),
-        ));
+        $criteria = self::getCriteria($userId, $entity, $tagId, $query);
+        $criteria->offset = $offset;
+        $criteria->limit = self::FAVOURITES_PER_PAGE;
 
-        if ($entity !== null)
-            $criteria->mergeWith(self::getEntityCriteria($entity));
+        $favourites = Favourite::model()->findAll($criteria);
 
-        if ($tagId !== null) {
-            $tagCriteria = new CDbCriteria(array(
-                'join' => 'INNER JOIN favourites__tags_favourites tf ON tf.favourite_id = t.id AND tf.tag_id = :tagId',
-                'params' => array(':tagId' => $tagId),
+        //получение необходимых id для выборки
+        $entities = array();
+        foreach ($favourites as $favourite)
+            $entities[$favourite->entity][$favourite->entity_id] = null;
+
+        //выборка и создание моделей
+        foreach ($entities as $entity => $ids) {
+            $criteria = new CDbCriteria(array(
+                'index' => 'id',
             ));
-            $criteria->mergeWith($tagCriteria);
+            $criteria->addInCondition('t.id', array_keys($ids));
+            if (isset(Yii::app()->controller->module->relatedModelCriteria[$entity]))
+                $criteria->mergeWith(new CDbCriteria(Yii::app()->controller->module->relatedModelCriteria[$entity]));
+            $models = CActiveRecord::model($entity)->findAll($criteria);
+            foreach ($models as $m)
+                $entities[$entity][$m->id] = $m;
         }
 
-        return new FavouritesDataProvider(array(
-            'criteria' => $criteria,
-            'pagination' => array(
-                'pageSize' => 20,
-            ),
-        ));
+        //присваивание моделей соответсвующим элементам избранного
+        foreach ($favourites as $favourite)
+            $favourite->relatedModel = $entities[$favourite->entity][$favourite->entity_id];
+
+        return $favourites;
+    }
+
+    public static function getCount($userId, $entity, $tagId, $query)
+    {
+        return Favourite::model()->count(self::getCriteria($userId, $entity, $tagId, $query));
     }
 
     public static function getCountByUserId($userId, $entity = null)
@@ -45,6 +59,28 @@ class FavouritesManager
             $criteria->mergeWith(self::getEntityCriteria($entity));
 
         return Favourite::model()->count($criteria);
+    }
+
+    protected static function getCriteria($userId, $entity, $tagId, $query)
+    {
+        $criteria = new CDbCriteria(array(
+            'condition' => 'user_id = :user_id',
+            'params' => array(':user_id' => $userId),
+            'order' => 't.id DESC',
+        ));
+
+        if ($entity !== null)
+            $criteria->mergeWith(self::getEntityCriteria($entity));
+
+        if ($tagId !== null) {
+            $tagCriteria = new CDbCriteria(array(
+                'join' => 'INNER JOIN favourites__tags_favourites tf ON tf.favourite_id = t.id AND tf.tag_id = :tagId',
+                'params' => array(':tagId' => $tagId),
+            ));
+            $criteria->mergeWith($tagCriteria);
+        }
+
+        return $criteria;
     }
 
     protected static function getEntityCriteria($entity)

d495438a65f06ee7320b2a82fedbe32656707042
choojoy-work <nikita@happy-giraffe.ru>
2013-05-27 15:17:29 +0400

    Редактирование

--- site/frontend/modules/favourites/views/default/index.php
+++ site/frontend/modules/favourites/views/default/index.php
@@ -54,7 +54,63 @@ Yii::app()->clientScript
         </div>
 
         <!-- ko foreach: favourites -->
-            <div data-bind="html: html"></div>
+            <div class="entry-hold">
+                <div data-bind="html: html"></div>
+
+                <div class="entry-tags">
+                    <div class="entry-tags_row" data-bind="visible: tags().length > 0">
+                        <span class="entry-tags_t">Теги:</span>
+                        <!-- ko foreach: tags -->
+                        <a href="javascript:void(0)" class="entry-tags_tag" data-bind="text: $data"></a>
+                        <!-- /ko -->
+                    </div>
+                    <div class="entry-tags_row">
+                        <div class="entry-tags_tx" data-bind="visible: note() != '', text: note"></div>
+                    </div>
+                    <div class="entry-tags_edit">
+                        <a href="javascript: void(0)" class="entry-tags_edit-a powertip" title="Редактировать" data-bind="click: showEditForm"></a>
+
+                        <!-- ko if: editing -->
+                            <div class="favorites-add-popup" data-bind="with: editing">
+                                <div class="favorites-add-popup_t">Редактировать</div>
+                                <div class="favorites-add-popup_i clearfix">
+                                    <img class="favorites-add-popup_i-img" data-bind="attr: { src : image, alt: title }">
+                                    <div class="favorites-add-popup_i-hold" data-bind="text: title"></div>
+                                </div>
+                                <div class="favorites-add-popup_row">
+                                    <label for="" class="favorites-add-popup_label">Теги:</label>
+                                    <!-- ko foreach: tags -->
+                                    <span class="favorites-add-popup_tag">
+                                        <a href="" class="favorites-add-popup_tag-a" data-bind="text: $data"></a>
+                                        <a href="javascript:void(0)" class="ico-close" data-bind="click: $parent.removeTag"></a>
+                                    </span>
+                                    <!-- /ko -->
+                                </div>
+                                <div class="favorites-add-popup_row margin-b10" data-bind="visible: ! tagsInputIsVisible(), click: showTagsForm">
+                                    <a class="textdec-none" href="">
+                                        <span class="ico-plus2 margin-r5"></span>
+                                        <span class="a-pseudo-gray color-gray">Добавить</span>
+                                    </a>
+                                </div>
+                                <div class="favorites-add-popup_row margin-b10" data-bind="visible: tagsInputIsVisible">
+                                    <input type="text" class="favorites-add-popup_itx-tag ui-autocomplete-input" placeholder="Вводите теги через запятую или Enter" data-bind="value: tagsInputValue, valueUpdate: 'keyup', event: { keypress : tagHandler }">
+                                </div>
+                                <div class="favorites-add-popup_row">
+                                    <label for="" class="favorites-add-popup_label">Комментарий</label>
+                                    <div class="float-r color-gray" data-bind="text: note().length + '/150'"></div>
+                                </div>
+                                <div class="favorites-add-popup_row">
+                                    <textarea name="" id="" cols="25" rows="2" class="favorites-add-popup_textarea" placeholder="Введите комментарий" data-bind="value: note, valueUpdate: 'keyup'" maxlength="150"></textarea>
+                                </div>
+                                <div class="favorites-add-popup_row textalign-c margin-t15">
+                                    <a href="javascript:void(0)" class="btn-gray-light" data-bind="click: $root.cancel">Отменить</a>
+                                    <a href="javascript:void(0)" class="btn-green" data-bind="click: $root.edit">Сохранить</a>
+                                </div>
+                            </div>
+                        <!-- /ko -->
+                    </div>
+                </div>
+            </div>
         <!-- /ko -->
     </div>
 </div>

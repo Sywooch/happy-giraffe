e6cad0de0be48bd510da316eb9670d46ddc91353
choojoy-work <nikita@happy-giraffe.ru>
2013-05-28 11:09:28 +0400

    menu

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -45,6 +45,7 @@
         ->registerScriptFile('/javascripts/jquery.tmpl.min.js')
         ->registerScriptFile('/javascripts/jquery.lazyload.min.js')
         ->registerScriptFile('/javascripts/jquery.powertip.js')
+        ->registerScriptFile('/javascripts/jquery.flydiv.js')
         ->registerScriptFile('/javascripts/addtocopy.js')
         ->registerScriptFile('/javascripts/tooltipsy.min.js')
         //->registerScriptFile('http://vk.com/js/api/share.js?11')

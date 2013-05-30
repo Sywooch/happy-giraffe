7c71675200dacd21aebb00aae1e1ffb48002e375
choojoy-work <nikita@happy-giraffe.ru>
2013-05-30 14:23:43 +0400

    accessFilter

--- site/mobile/views/layouts/main.php
+++ site/mobile/views/layouts/main.php
@@ -126,7 +126,7 @@
             })();
         </script>
 
-        <?php var_dump(Yii::app()->homeUrl); if ($this->route == 'site/index'): ?>
+        <?php var_dump(Yii::app()->request->pathInfo); if ($this->route == 'site/index'): ?>
             <a href="http://waplog.net/c.shtml?517512"><img src="http://c.waplog.net/517512.cnt" alt="waplog" /></a>
         <?php else: ?>
             <a href="http://waplog.net/c.shtml?517511"><img src="http://c.waplog.net/517511.cnt" alt="waplog" /></a>

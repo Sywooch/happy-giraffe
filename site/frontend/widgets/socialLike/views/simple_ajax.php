54dddce55b499a52be94908a1edfb60058ddb007
Kireev Alex <alexk984@gmail.com>
2013-05-31 14:13:23 +0400

    last gallery page

--- site/frontend/widgets/socialLike/views/simple_ajax.php
+++ site/frontend/widgets/socialLike/views/simple_ajax.php
@@ -6,6 +6,7 @@ if(isset($this->model) && method_exists($this->model, 'isValentinePost') && $thi
     $url = $this->url;
 }else
     $url = 'http://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+
 ?>
 <script type="text/javascript">
     $('.vk_share_button').html(VK.Share.button('<?= $url?>',{type: 'round', text: 'Мне нравится'}));
@@ -40,8 +41,23 @@ if(isset($this->model) && method_exists($this->model, 'isValentinePost') && $thi
         </div>
 
         <div class="share_button">
-            <a class="odkl-klass-oc" href="<?=$url?>"
-               onclick="Social.updateLikesCount('ok'); ODKL.Share(this);return false;"><span>0</span></a>
+            <div id="ok_shareWidget"></div>
+            <script>
+                !function (d, id, did, st) {
+                    var js = d.createElement("script");
+                    js.src = "http://connect.ok.ru/connect.js";
+                    js.onload = js.onreadystatechange = function () {
+                        if (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") {
+                            if (!this.executed) {
+                                this.executed = true;
+                                setTimeout(function () {
+                                    OK.CONNECT.insertShareWidget(id,did,st);
+                                }, 0);
+                            }
+                        }};
+                    d.documentElement.appendChild(js);
+                }(document,"ok_shareWidget","<?=$url ?>","{width:145,height:35,st:'straight',sz:20,ck:1}");
+            </script>
         </div>
 
         <div class="share_button">

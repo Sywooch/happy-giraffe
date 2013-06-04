7e47587c038f511d06cf578a3318964f0922a8c1
choojoy-work <nikita@happy-giraffe.ru>
2013-06-04 11:52:21 +0400

    search

--- site/frontend/modules/search/components/SearchManager.php
+++ site/frontend/modules/search/components/SearchManager.php
@@ -16,13 +16,56 @@ class SearchManager
         $entities = array();
         foreach ($raw->results as $document) {
             list($modelName, $modelId) = explode('_', $document->docid);
-            $entities[$modelName][] = $modelId;
+            $entities[$modelName][$modelId] = null;
+        }
+
+        //выборка и создание моделей
+        foreach ($entities as $entity => $ids) {
+            $criteria = new CDbCriteria(array(
+                'index' => 'id',
+            ));
+            $criteria->addInCondition('t.id', array_keys($ids));
+            if (isset(Yii::app()->controller->module->relatedModelCriteria[$entity]))
+                $criteria->mergeWith(new CDbCriteria(Yii::app()->controller->module->relatedModelCriteria[$entity]));
+            $models = CActiveRecord::model($entity)->findAll($criteria);
+            foreach ($models as $m)
+                $entities[$entity][$m->id] = $m;
+        }
+
+        $results = array();
+        foreach ($raw->results as $document) {
+            list($modelName, $modelId) = explode('_', $document->docid);
+            $results[] = $entities[$modelName][$modelId];
         }
 
         $data = array(
             'total' => $raw->matches,
+            'results' => $results,
         );
 
         return $data;
     }
 }
+
+
+//получение необходимых id для выборки
+$entities = array();
+foreach ($favourites as $favourite)
+    $entities[$favourite->model_name][$favourite->model_id] = null;
+
+//выборка и создание моделей
+foreach ($entities as $entity => $ids) {
+    $criteria = new CDbCriteria(array(
+        'index' => 'id',
+    ));
+    $criteria->addInCondition('t.id', array_keys($ids));
+    if (isset(Yii::app()->controller->module->relatedModelCriteria[$entity]))
+        $criteria->mergeWith(new CDbCriteria(Yii::app()->controller->module->relatedModelCriteria[$entity]));
+    $models = CActiveRecord::model($entity)->findAll($criteria);
+    foreach ($models as $m)
+        $entities[$entity][$m->id] = $m;
+}
+
+//присваивание моделей соответсвующим элементам избранного
+foreach ($favourites as $favourite)
+    $favourite->relatedModel = $entities[$favourite->model_name][$favourite->model_id];
--- site/frontend/modules/search/views/default/index.php
+++ site/frontend/modules/search/views/default/index.php
@@ -1,1 +1,5 @@
 Общее количесто результатов: <?=$total?>
+
+<?php foreach ($results as $r): ?>
+    <?php $this->render('_row', array('data' => $r)); ?>
+<?php endforeach; ?>

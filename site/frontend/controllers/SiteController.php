396c00bef4a2ede3c7a2b75d0bdd3c453f641db6
Kireev Alex <alexk984@gmail.com>
2013-06-04 11:40:58 +0400

    signup fix

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -327,11 +327,13 @@ class SiteController extends HController
     public function actionConfirmEmail($user_id, $code)
     {
         $user = User::model()->findByPk($user_id);
-        if ($user === null || $user->email_confirmed || $code != $user->confirmationCode)
+        if ($user === null || $code != $user->confirmationCode)
             throw new CHttpException(404);
 
-        $user->email_confirmed = 1;
-        $user->update(array('email_confirmed'));
+        if (!$user->email_confirmed){
+            $user->email_confirmed = 1;
+            $user->update(array('email_confirmed'));
+        }
 
         $identity = new SafeUserIdentity($user_id);
         if ($identity->authenticate())

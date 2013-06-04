b46aa3a4b7cdeb7acf587c2d13bfd4323481a130
choojoy-work <nikita@happy-giraffe.ru>
2013-06-04 18:03:45 +0400

    messaging fix

--- site/frontend/views/site/test.php
+++ site/frontend/views/site/test.php
@@ -1,24 +1,26 @@
-<div id="yandex_ad"></div>
+<div id="yandex_ad_2"></div>
 <script type="text/javascript">
     (function(w, d, n, s, t) {
         w[n] = w[n] || [];
         w[n].push(function() {
-            Ya.Direct.insertInto(87026, "yandex_ad", {
+            Ya.Direct.insertInto(87026, "yandex_ad_2", {
+                stat_id: 2,
                 site_charset: "utf-8",
                 ad_format: "direct",
                 font_size: 1,
-                type: "vertical",
-                limit: 1,
-                title_font_size: 2,
+                type: "horizontal",
+                limit: 3,
+                title_font_size: 3,
                 site_bg_color: "FFFFFF",
-                title_color: "006699",
-                url_color: "006699",
-                all_color: "000000",
+                header_bg_color: "FEEAC7",
+                title_color: "0000CC",
+                url_color: "006600",
                 text_color: "000000",
-                hover_color: "6699CC"
+                hover_color: "0066FF",
+                favicon: true
             });
         });
-        t = d.documentElement.firstChild;
+        t = d.getElementsByTagName('head')[0];
         s = d.createElement("script");
         s.type = "text/javascript";
         s.src = "http://an.yandex.ru/system/context.js";

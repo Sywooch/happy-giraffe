ae472b38d716bfca4dd299cb11bad0ec7716e2e4
Tantalid <tantalid@mail.ru>
2013-06-05 10:52:08 +0300

    search

---
+++ html/search/sarch_result-2.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-club">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+<div class="layout-container_hold">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div id="content" class="layout-content clearfix">
+			
+			<div class="content-search">
+				<div class="content-search_top clearfix">
+					<div class="content-search_t">Я ищу</div>
+					<div class="content-search_itx-hold">
+						<input type="text" name="" id="" class="content-search_itx" placeholder="Введите слово ">
+						<button class="content-search_btn"></button>
+					</div>
+				</div>
+				<div class="content-search_utility clearfix">
+					<div class="content-search_found">Найдено <span class="search-highlight">365</span></div>
+					<div class="content-search_select">
+						<div class="chzn-bluelight">
+							<select class="chzn">
+								<option>по дате добавления</option>
+								<option>по популярности</option>			
+							</select>
+						</div>
+					</div>
+					<div class="content-search_pager-control">
+						<span class="content-search_pager-control-tx">Показывать на странице </span>
+						<span class="content-search_pager-control-a">20</span>
+						<span class="content-search_pager-control-separator">|</span>
+						<a href="" class="content-search_pager-control-a">50</a>
+						<span class="content-search_pager-control-separator">|</span>
+						<a href="" class="content-search_pager-control-a">100</a>
+						
+					</div>
+				</div>
+			</div>
+			
+			<div class="content-cols clearfix">
+				<div class="col-1">
+					<div class="menu-list menu-list__favorites">
+						<a href="" class="menu-list_i menu-list_i__all2">
+							<span class="menu-list_ico"></span>
+							<span class="menu-list_tx">Все</span>
+							<span class="menu-list_count">6878995</span>
+						</a>
+						<a href="" class="menu-list_i menu-list_i__post">
+							<span class="menu-list_ico"></span>
+							<span class="menu-list_tx">Записи</span>
+							<span class="menu-list_count">685</span>
+						</a>
+						<a href="" class="menu-list_i menu-list_i__photo active">
+							<span class="menu-list_ico"></span>
+							<span class="menu-list_tx">Фото</span>
+							<span class="menu-list_count">685</span>
+						</a>
+						<a href="" class="menu-list_i menu-list_i__video">
+							<span class="menu-list_ico"></span>
+							<span class="menu-list_tx">Видео</span>
+							<span class="menu-list_count">685</span>
+						</a>
+					</div>
+				</div>
+				
+				<div class="col-23 clearfix">
+					<div class="entry-hold">
+						<div class="entry">
+								
+							<div class="entry-header">
+								<div class="entry-title_hold">
+									<a href="" class="entry-title">Необычная свадьба в <span class="search-highlight">народном стиле</span> с байкерами</a>
+								</div>
+
+								<noindex>
+								<div class="clearfix">
+								    <div class="user">
+									    <div class="user-info clearfix medium">
+											<a href="" class="ava female">
+												<span class="icon-status "></span>
+											</a>
+											<div class="details">
+											    <a href="" class="username">Ольга Богоявленская</a>
+											    <div class="user-fast-buttons">
+											    	<span class="friend">друг</span>
+											        <a href="" class="new-message tooltip"></a>
+											    </div>
+											</div>
+										</div>
+									</div>
+
+									<div class="favorites-control">
+										<!-- for active status addclass active <a href="" class="favorites-control_a active"> -->
+										<a href="" class="favorites-control_a powertip active" title="Удалить из избранного">
+											145
+										</a>
+									</div>
+									<div class="meta">
+										<div class="time">24 мая 2013, 1:50</div>
+										<div class="views"><span class="icon"></span> <span>18</span></div>
+										<div class="comments">
+	            							<a class="icon" href="#"></a>
+	                                        <a class="ava small male" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/16534/small/70b73fea2a74470ae721ebae1848f86b.jpg">
+	                                        </a>
+	                                        <a class="ava small female" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/15994/small/80c4ed185b865c375741adba8ca4367f.jpg">
+	                                        </a>
+	                                        <a class="ava small female" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/15545/small/63f3660625c513462a0ba3670dd1d031.jpg">
+	                                        </a>
+	                                        <a href="/user/10/blog/post79169/#comment_list">и еще 9</a>
+	                                    </div>
+									</div>
+								</div>
+								</noindex>
+							</div>
+								
+							<div class="entry-content">
+								
+								<div class="wysiwyg-content">
+									
+									<h2>Как выбрать <span class="search-highlight">детскую коляску</span></h2>
+									
+									<p>Практически нет девушки, которая не <span class="search-highlight">переживала бы за отношения героев</span>  "Сумерек" как в на экранах, так и в жизни. Но, к сожалению, даже несмотря на то, что недавно герои "Сумерек" радовали всех тем, что у них невероятный роман и в рельной жизни, а не только лишь на экране, все же Роберт Паттинсон и Кристен Стюарт расстались и пока решили взять паузу в своих отношениях.</p>
+									
+								</div>
+								
+							</div>
+
+						</div>
+							
+					</div>
+					<div class="entry-hold">
+						<div class="entry">
+								
+							<div class="entry-header">
+								<div class="entry-title_hold">
+									<a href="" class="entry-title">Топ - 8 модных вещей в горошек</a>
+								</div>
+
+								<noindex>
+								<div class="clearfix">
+								    <div class="user">
+									    <div class="user-info clearfix medium">
+											<a href="" class="ava female">
+												<span class="icon-status "></span>
+												<img alt="" src="http://img.happy-giraffe.ru/avatars/15426/ava/56e76f006e881353012beb7d9f6106f6.jpg">
+											</a>
+											<div class="details">
+											    <a href="" class="username">Таня Родионова (Рыбак)</a>
+											    <div class="user-fast-buttons">
+											    	<span class="friend">друг</span>
+											        <a href="" class="new-message tooltip"></a>
+											    </div>
+											</div>
+										</div>
+									</div>
+
+									<div class="favorites-control">
+										<!-- for active status addclass active <a href="" class="favorites-control_a active"> -->
+										<a href="" class="favorites-control_a powertip" title="В избранное">
+											145
+										</a>
+										
+									</div>
+									<div class="meta">
+										<div class="time">24 мая 2013, 1:50</div>
+										<div class="views"><span class="icon"></span> <span>18</span></div>
+										<div class="comments">
+	            							<a class="icon" href="#"></a>
+	                                        <a class="ava small male" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/16534/small/70b73fea2a74470ae721ebae1848f86b.jpg">
+	                                        </a>
+	                                        <a class="ava small female" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/15994/small/80c4ed185b865c375741adba8ca4367f.jpg">
+	                                        </a>
+	                                        <a class="ava small female" href="">
+	                                        	<img alt="" src="http://img.happy-giraffe.ru/avatars/15545/small/63f3660625c513462a0ba3670dd1d031.jpg">
+	                                        </a>
+	                                        <a href="/user/10/blog/post79169/#comment_list">и еще 9</a>
+	                                    </div>
+									</div>
+								</div>
+								</noindex>
+							</div>
+								
+							<div class="entry-content">
+								
+								<div class="wysiwyg-content">
+									
+									<h2>Как выбрать детскую коляску</h2>
+									
+									<p>Как правило, кроватку новорожденному приобретают незадолго до его появления на свет. При этом многие молодые <b>родители</b> обращают внимание главным <u>образом</u> на ее <strike>внешний</strike> вид. Но, прельстившись яркими красками, многие платят баснословные суммы, даже не поинтересовавшись, из чего сделано это покорившее вас чудо.</p>
+									
+									<p><span class="search-highlight">Атопический дерматит у детей до года локализуется в основном на щечках и ягодицах, реже на теле и конечностях, на коже головы возможно появление корочек. Когда малышу исполнится год, то места высыпаний меняются &ndash; теперь поражаются локтевые сгибы</span> Атопический дерматит у детей до года локализуется в основном на щечках и ягодицах, реже на теле и конечностях, на коже головы возможно появление корочек. Когда малышу исполнится год, то места высыпаний меняются &ndash; теперь поражаются локтевые сгибы (внутри и снаружи), подколенные впадины, шея. После трех лет высыпания начинают поражать также и кисти рук. <a class="read-more" href="">Читать всю запись<i class="icon"></i></a></p>
+									
+								</div>
+								
+							</div>
+						
+						</div>
+							
+					</div>
+					
+					<div class="pagination pagination-center clearfix">
+						<div class="pager">
+							<ul class="yiiPager" id="">
+								<li class="first hidden"><a href=""></a></li>
+								<li class="previous hidden"><a href=""></a></li>
+								<li class="page selected"><a href="">1</a><img src="/images/pagination_tale.png"></li>
+								<li class="page"><a href="">2</a></li>
+								<li class="page"><a href="">3</a></li>
+								<li class="page"><a href="">4</a></li>
+								<li class="page"><a href="">5</a></li>
+								<li class="page"><a href="">6</a></li>
+								<li class="page"><a href="">7</a></li>
+								<li class="page"><a href="">8</a></li>
+								<li class="page"><a href="">9</a></li>
+								<li class="page"><a href="">10</a></li>
+								<li class="next"><a href="">...</a></li>
+								<li class="last"><a href=""></a></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>  	
+		
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</div>
+
+
+</body>
+</html>

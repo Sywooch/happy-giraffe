9bc3993ca1b82b6aae68de99699ea6319fc06483
Kireev Alex <alexk984@gmail.com>
2013-06-10 12:24:33 +0400

    fix

--- site/common/migrations/m130610_041540_scores.php
+++ site/common/migrations/m130610_041540_scores.php
@@ -4,8 +4,9 @@ class m130610_041540_scores extends CDbMigration
 {
 	public function up()
 	{
-        $sql = <<<EOD
         //ALTER TABLE `score__user_scores` DROP `full`;
+
+        $sql = <<<EOD
         UPDATE `score__user_scores` set `scores` = 0;
         ALTER TABLE  `score__user_scores` ADD  `seen_scores` INT UNSIGNED NOT NULL DEFAULT  '0' AFTER  `scores`;
         DROP TABLE `score__actions`;

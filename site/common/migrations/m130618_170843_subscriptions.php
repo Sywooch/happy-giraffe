0ec55b6d81ffb8e74c7ab5fbb435c80a94ad5dbf
Kireev Alex <alexk984@gmail.com>
2013-06-19 11:36:50 +0400

    subscriptions

--- site/common/migrations/m130618_170843_subscriptions.php
+++ site/common/migrations/m130618_170843_subscriptions.php
@@ -2,24 +2,44 @@
 
 class m130618_170843_subscriptions extends CDbMigration
 {
-	public function up()
-	{
-	}
+    private $_table = '';
 
-	public function down()
-	{
-		echo "m130618_170843_subscriptions does not support migration down.\n";
-		return false;
-	}
+    public function up()
+    {
+        $this->_table = 'user__community_subscriptions';
+        $this->createTable($this->_table, array(
+            'user_id' => 'int(10) unsigned NOT NULL',
+            'community_id' => 'int(11) unsigned NOT NULL',
+            'PRIMARY KEY (`user_id`, `community_id`)'
+        ), 'ENGINE=Innodb DEFAULT CHARSET=utf8');
 
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
+        $this->addForeignKey('fk_'.$this->_table.'_user', $this->_table, 'user_id', 'users', 'id','CASCADE',"CASCADE");
+        $this->addForeignKey('fk_'.$this->_table.'_community', $this->_table, 'community_id', 'community__communities', 'id','CASCADE',"CASCADE");
 
-	public function safeDown()
-	{
-	}
-	*/
+        $this->_table = 'user__blog_subscriptions';
+        $this->createTable($this->_table, array(
+            'user_id' => 'int(10) unsigned NOT NULL',
+            'user2_id' => 'int(11) unsigned NOT NULL',
+            'PRIMARY KEY (`user_id`, `user2_id`)'
+        ), 'ENGINE=Innodb DEFAULT CHARSET=utf8');
+        $this->addForeignKey('fk_'.$this->_table.'_user', $this->_table, 'user_id', 'users', 'id','CASCADE',"CASCADE");
+        $this->addForeignKey('fk_'.$this->_table.'_user2', $this->_table, 'user2_id', 'users', 'id','CASCADE',"CASCADE");
+    }
+
+    public function down()
+    {
+        echo "m130618_170843_subscriptions does not support migration down.\n";
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
 }
---
+++ site/common/models/UserBlogSubscription.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * This is the model class for table "user__blog_subscriptions".
+ *
+ * The followings are the available columns in table 'user__blog_subscriptions':
+ * @property string $user_id
+ * @property string $user2_id
+ *
+ * The followings are the available model relations:
+ * @property User $user2
+ * @property User $user
+ */
+class UserBlogSubscription extends HActiveRecord
+{
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return UserBlogSubscription the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'user__blog_subscriptions';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        return array(
+            array('user_id, user2_id', 'required'),
+            array('user_id', 'length', 'max' => 10),
+            array('user2_id', 'length', 'max' => 11),
+            array('user_id, user2_id', 'safe', 'on' => 'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        return array(
+            'user' => array(self::BELONGS_TO, 'User', 'user_id'),
+            'user2' => array(self::BELONGS_TO, 'User', 'user2_id'),
+        );
+    }
+
+    /**
+     * возвращает всех подписчиков блога
+     *
+     * @param int $user_id id автора блога
+     * @return User[]
+     */
+    public function getSubscribers($user_id)
+    {
+        $criteria = new CDbCriteria;
+        $criteria->compare('user2_id', $user_id);
+        $criteria->with = array('subscriber');
+        $criteria->limit = 100;
+        return User::model()->findAll($criteria);
+    }
+
+    /**
+     * Возвращает список id юзеров, на блоги которых одписан
+     *
+     * @param int $user_id
+     * @return int[]
+     */
+    public function getSubUserIds($user_id)
+    {
+        return Yii::app()->db->createCommand()
+            ->select('user2_id')
+            ->from($this->tableName())
+            ->where('user_id=:user_id', array(':user_id' => $user_id))
+            ->queryColumn();
+    }
+
+    /**
+     * Меняет подписку на блог - если нет - добалвяет, если есть - удаляет
+     *
+     * @param int $user2_id id пользователя, на блог которого подписываемся
+     * @return bool успех
+     */
+    public static function toggle($user2_id)
+    {
+        $model = self::model()->find('user_id=:user_id AND user2_id=:user2_id', array(
+            ':user_id' => Yii::app()->user->id,
+            ':user2_id' => $user2_id,
+        ));
+        return $model !== null ? self::add($user2_id) : $model->delete();
+    }
+
+    /**
+     * Отписать пользователя user от блога user2
+     * @param int $user_id
+     * @param int $user2_id
+     * @return bool успех
+     */
+    public static function unSubscribe($user_id, $user2_id)
+    {
+        $model = self::model()->find('user_id=:user_id AND user2_id=:user2_id', array(
+            ':user_id' => $user_id,
+            ':user2_id' => $user2_id,
+        ));
+        return $model !== null ? true : $model->delete();
+    }
+
+    /**
+     * Подписан ли user на блог пользователя user2
+     *
+     * @param int $user_id
+     * @param int $user2_id
+     * @return bool успех
+     */
+    public static function subscribed($user_id, $user2_id)
+    {
+        $model = self::model()->find('user_id=:user_id AND user2_id=:user2_id', array(
+            ':user_id' => $user_id,
+            ':user2_id' => $user2_id,
+        ));
+        return $model !== null;
+    }
+
+    /**
+     * Добавляет подписку на блог
+     *
+     * @param int $user2_id id пользователя, на блог которого подписываемся
+     * @return bool успех
+     */
+    public static function add($user2_id)
+    {
+        $model = new self;
+        $model->user_id = Yii::app()->user->id;
+        $model->user2_id = $user2_id;
+        return $model->save();
+    }
+}

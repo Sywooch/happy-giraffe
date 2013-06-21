633578afca6f93dca05393a6e96bc19cebd76b08
Tantalid <tantalid@mail.ru>
2013-06-21 13:57:48 +0300

    blog2

---
+++ html/social/user/user_blog2_list.php
@@ -0,0 +1,336 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="top-nav-fixed ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="top-nav-fixed ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="top-nav-fixed ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="top-nav-fixed"> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+	<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,100&amp;subset=latin,cyrillic-ext,cyrillic">
+
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		
+		<div class="layout-content clearfix">
+		<div class="content-cols clearfix">
+			<div class="col-1">
+				<!-- <div class="b-ava-large">
+					<div class="b-ava-large_ava-hold clearfix">
+						<a class="ava large" href="">
+							<img alt="" src="/images/example/ava-large.jpg">
+						</a>
+						<span class="b-ava-large_online">На сайте</span>
+						<a class="b-ava-large_bubble b-ava-large_bubble__dialog powertip" href="">
+							<span class="b-ava-large_ico b-ava-large_ico__mail"></span>
+							<span class="b-ava-large_bubble-tx">+5</span>
+						</a>
+						<a class="b-ava-large_bubble b-ava-large_bubble__photo powertip" href="">
+							<span class="b-ava-large_ico b-ava-large_ico__photo"></span>
+							<span class="b-ava-large_bubble-tx">+50</span>
+						</a>
+						<a class="b-ava-large_bubble b-ava-large_bubble__blog powertip" href="">
+							<span class="b-ava-large_ico b-ava-large_ico__blog"></span>
+							<span class="b-ava-large_bubble-tx">+999</span>
+						</a>
+					</div>
+					<a class="b-ava-large_a" href="">Валерия Петроградская</a>
+				</div> -->
+				<div class="b-ava-large">
+					<div class="b-ava-large_ava-hold clearfix">
+						<a class="ava large" href="">
+							<img alt="" src="/images/example/ava-large.jpg">
+						</a>
+						<span class="b-ava-large_online">На сайте</span>
+						<a href="" class="b-ava-large_bubble b-ava-large_bubble__dialog powertip" title="Начать диалог">
+							<span class="b-ava-large_ico b-ava-large_ico__mail"></span>
+							<span class="b-ava-large_bubble-tx">+5</span>
+						</a>
+						<a href="" class="b-ava-large_bubble b-ava-large_bubble__photo powertip" title="Фотографии">
+							<span class="b-ava-large_ico b-ava-large_ico__photo"></span>
+							<span class="b-ava-large_bubble-tx">+50</span>
+						</a>
+						<a href="" class="b-ava-large_bubble b-ava-large_bubble__blog powertip" title="Записи в блоге">
+							<span class="b-ava-large_ico b-ava-large_ico__blog"></span>
+							<span class="b-ava-large_bubble-tx">+999</span>
+						</a>
+						<a href="" class="b-ava-large_bubble b-ava-large_bubble__friend-add powertip" title="Добавить в друзья">
+							<span class="b-ava-large_ico b-ava-large_ico__friend-add"></span>
+						</a>
+					</div>
+					<div class="textalign-c">
+						<a href="" class="b-ava-large_a">Александр Богоявленский</a>
+					</div>
+				</div>
+				
+				<div class="aside-blog-desc">
+					<div class="aside-blog-desc_tx">
+						Пусть кто-то будет безумно красивый Пусть кто-то будет богаче и круче Мне наплевать, ведь ты - мой любимый.	Ты навсегда. Ты мой. Самый лучший.
+					</div>
+				</div>
+				
+				<div class="readers2">
+					<a href="" class="btn-green btn-medium">Подписаться</a>
+					<ul class="readers2_ul clearfix">
+						<li class="readers2_li clearfix">
+							<a href="" class="ava female small">
+								<span class="icon-status status-online"></span>
+								<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a href="" class="ava female small">
+								<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a href="" class="ava female small">
+								<span class="icon-status status-online"></span>
+								<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a href="" class="ava female small">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a href="" class="ava female small">
+								<span class="icon-status status-online"></span>
+								<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a href="" class="ava male small">
+								<span class="icon-status status-online"></span>
+							</a>
+						</li>
+					</ul>
+					<div class="clearfix">
+						<div class="readers2_count">Все подписчики (129)</div>
+					</div>
+				</div>
+				
+				<div class="menu-simple">
+					<ul class="menu-simple_ul">
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Обо всем</a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Свадьба - прекрасный миг</a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Прикольное видео </a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Школа восточного танца  </a>
+						</li>
+						<li class="menu-simple_li active">
+							<a href="" class="menu-simple_a">Мой мужчина </a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Детские передачи </a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Свадьбы </a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Кормление ребенка </a>
+						</li>
+						<li class="menu-simple_li">
+							<a href="" class="menu-simple_a">Воспитание детей </a>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="fast-articles2">
+					<div class="fast-articles2_t-ico"></div>
+					<div class="fast-articles2_i">
+						<div class="fast-articles2_header clearfix">
+						
+							<div class="meta-gray">
+								<a href="" class="meta-gray_comment">
+									<span class="ico-comment ico-comment__gray"></span>
+									<span class="meta-gray_tx">35</span>
+								</a>
+								<div class="meta-gray_view">
+									<span class="ico-view ico-view__gray"></span>
+									<span class="meta-gray_tx">305</span>
+								</div>
+							</div>
+							
+							<div class="float-l">
+								<span class="font-smallest color-gray">Сегодня 13:25</span>
+							</div>
+						</div>
+						<div class="fast-articles2_i-t">
+							<a href="" class="fast-articles2_i-t-a"> О моем первом бойфренде</a>
+						</div>
+						<div class="fast-articles2_i-desc">Практически нет девушки, которая не переживала </div>
+						<div class="fast-articles2_i-img-hold">
+							<a href=""><img src="/images/example/w190-h166.jpg" alt="" class="fast-articles2_i-img"></a>
+						</div>
+					</div>
+					<div class="fast-articles2_i">
+						<div class="fast-articles2_header clearfix">
+						
+							<div class="meta-gray">
+								<a href="" class="meta-gray_comment">
+									<span class="ico-comment ico-comment__gray"></span>
+									<span class="meta-gray_tx">35</span>
+								</a>
+								<div class="meta-gray_view">
+									<span class="ico-view ico-view__gray"></span>
+									<span class="meta-gray_tx">305</span>
+								</div>
+							</div>
+							
+							<div class="float-l">
+								<span class="font-smallest color-gray">Сегодня 13:25</span>
+							</div>
+						</div>
+						<div class="fast-articles2_i-t">
+							<a href="" class="fast-articles2_i-t-a"> Как мне предлагали руку и сердце</a>
+						</div>
+						<div class="fast-articles2_i-desc">Практически нет девушки, которая не переживала </div>
+						<div class="fast-articles2_i-img-hold">
+							<a href=""><img src="/images/example/w190-h166.jpg" alt="" class="fast-articles2_i-img"></a>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-23 col-23__gray">
+				<div class="blog-title-b">
+					<div class="blog-title-b_img-hold">
+						<img src="/images/example/w720-h128.jpg" alt="" class="blog-title-b_img">
+					</div>
+					<h1 class="blog-title-b_t">Блог о красивой любви </h1>
+				</div>
+				
+				<div class="b-article b-article__preview clearfix">
+					<div class="float-l">
+						<div class="b-article_control clearfix">
+							<a href="" class="ava male">
+								<span class="icon-status status-online"></span>
+								<img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+							</a>
+						</div>
+						<div class="b-article_control b-article_control__pinned clearfix">
+							<a href="" class="b-article_control-ico b-article_control-ico__like">865</a>
+							<a href="" class="b-article_control-ico b-article_control-ico__repost">5</a>
+							<a href="" class="b-article_control-ico b-article_control-ico__favorites active">123865</a>
+						</div>
+					</div>
+					<div class="b-article_cont clearfix">
+						<div class="b-article_cont-tale"></div>
+						<div class="b-article_header clearfix">
+							<div class="meta-gray">
+								<a href="" class="meta-gray_comment">
+									<span class="ico-comment ico-comment__gray"></span>
+									<span class="meta-gray_tx">35</span>
+								</a>
+								<div class="meta-gray_view">
+									<span class="ico-view ico-view__gray"></span>
+									<span class="meta-gray_tx">305</span>
+								</div>
+							</div>
+							<div class="float-l">
+								<a href="" class="b-article_author">Ангелина Богоявленская</a>
+								<span class="font-smallest color-gray">Сегодня 13:25</span>
+							</div>
+						</div>
+						<h2 class="b-article_t">
+							<a href="" class="b-article_t-a">Самое лучшее утро - просыпаюсь, а ты рядом</a>
+						</h2>
+						<div class="b-article_in clearfix">
+							<div class="wysiwyg-content clearfix">
+								<p>	Недавно посмотрел фильм "Убить Дракона" снятый в 1988 году с Абдуловым в главной роли. По мотивам пьесы Евгения Шварца «Дракон».</p>
+								<p><img title="Убить Дракона. Фантасмагория или сказка для взрослых. фото 1" src="http://img.happy-giraffe.ru/thumbs/700x700/16534/3733dd340b221ac3052b5cef11288870.jpg" class="content-img" alt="Убить Дракона. Фантасмагория или сказка для взрослых. фото 1"></p>
+							</div>
+						</div>
+						<div class="textalign-r margin-15">					
+							<a href="" class="b-article_more">Смотреть далее</a>
+						</div>
+						
+						<div class="comments-gray">
+							<div class="comments-gray_t">
+								<a href="" class="comments-gray_t-a">
+									<span class="ico-comment ico-comment__gray"></span>
+									<span class="comments-gray_t-a-tx">Все комментарии (28)</span>
+								</a>
+							</div>
+							<div class="comments-gray_hold">
+								<div class="comments-gray_i">
+									<div class="comments-gray_ava">
+										<a href="" class="ava small male"></a>
+									</div>
+									<div class="comments-gray_frame">
+										<div class="comments-gray_header clearfix">
+											<a href="" class="comments-gray_author">Ангелина Богоявленская </a>
+											<span class="font-smallest color-gray">Сегодня 13:25</span>
+										</div>
+										<div class="comments-gray_cont wysiwyg-content">
+											<p>	Мне безумно жалко всех женщин, но особенно Тину Кароль, я просто представить себе не могу как она все это переживет(</p>
+											<p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту и подцепляет естественно студийные версии песен вместо нужных.  я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически </p>
+										</div>
+									</div>
+								</div>
+								<div class="comments-gray_i">
+									<div class="comments-gray_ava">
+										<a href="" class="ava small female"></a>
+									</div>
+									<div class="comments-gray_frame">
+										<div class="comments-gray_header clearfix">
+											<a href="" class="comments-gray_author">Анг Богоявлен </a>
+											<span class="font-smallest color-gray">Сегодня 14:25</span>
+										</div>
+										<div class="comments-gray_cont wysiwyg-content">
+											<p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту и подцепляет естественно студийные версии песен вместо нужных.  я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически </p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="comments-gray_add clearfix">
+								
+								<div class="comments-gray_ava">
+									<a href="" class="ava small female"></a>
+								</div>
+								<div class="comments-gray_frame">
+									<input type="text" name="" id="" class="comments-gray_add-itx itx-gray" placeholder="Ваш комментарий">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="yiipagination">
+                    <div class="pager">
+                        <ul class="yiiPager" id="yw1">
+                            <li class="page"><a href="">1</a></li>
+                            <li class="page"><a href="">2</a></li>
+                            <li class="page selected"><a href="">3</a></li>
+                            <li class="page"><a href="">4</a></li>
+                            <li class="page"><a href="">5</a></li>
+                            <li class="page"><a href="">6</a></li>
+                            <li class="page"><a href="">7</a></li>
+                            <li class="page"><a href="">8</a></li>
+                            <li class="page"><a href="">9</a></li>
+                            <li class="page"><a href="">10</a></li>
+                        </ul>
+                    </div>
+                </div>
+			</div>
+		</div>
+		</div>
+		
+		<a href="#layout" id="btn-up-page"></a>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+
+</body>
+</html>

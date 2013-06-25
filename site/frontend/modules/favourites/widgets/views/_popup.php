25f3ff56b4fdd033b6d6d653efe1dbca9f8f9e5d
Kireev Alex <alexk984@gmail.com>
2013-06-25 15:18:40 +0400

    comments + blog

--- site/frontend/modules/favourites/widgets/views/_popup.php
+++ site/frontend/modules/favourites/widgets/views/_popup.php
@@ -1,5 +1,5 @@
 <!-- ko if: adding -->
-<div class="favorites-add-popup" data-bind="with: adding">
+<div class="favorites-add-popup<?php if ($this->right) echo ' favorites-add-popup__right' ?>" data-bind="with: adding">
     <div class="favorites-add-popup_t">Добавить запись в избранное</div>
     <div class="favorites-add-popup_i clearfix">
         <img class="favorites-add-popup_i-img" data-bind="visible: image !== false, attr: { src : image, alt: title }">

910244fe44f4866256e163d0f646177f6454de00
choojoy-work <nikita@happy-giraffe.ru>
2013-06-26 13:11:27 +0400

    settings

--- javascripts/ko_blog.js
+++ javascripts/ko_blog.js
@@ -12,6 +12,7 @@ var BlogSettingsViewModel = function(data) {
     self.descriptionValue = ko.observable(data.description);
     self.title = ko.observable(data.title);
     self.description = ko.observable(data.description);
+    self.photo = ko.observable(data.photo === null ? null : new Photo(data.photo));
 
     self.setTitle = function() {
         self.title(self.titleValue());
@@ -29,12 +30,39 @@ var BlogSettingsViewModel = function(data) {
     }
 
     self.save = function() {
-        $.post(data.updateUrl, { blog_title : self.title(), blog_description : self.description() }, function(response) {
+        $.post(data.updateUrl, { blog_title : self.title(), blog_description : self.description(), blog_position : position }, function(response) {
             blogInfo.title(self.title());
             blogInfo.description(self.description());
             $.fancybox.close();
         }, 'json');
     }
+
+    self.photoSrc = ko.computed(function() {
+        return self.photo() === null ? '/images/jcrop-blog.jpg' : self.photo().src();
+    });
+
+    self.photoSrc.subscribe(function() {
+        jcrop_api.destroy();
+        $('.popup-blog-set_jcrop-img').Jcrop({
+            setSelect: [ 0, 0, 100, 100 ],
+            onChange: showPreview,
+            onSelect: showPreview,
+            aspectRatio: 720 / 128,
+            boxWidth: 320
+        });
+    });
+
+    $('#upload-target').on('load', function() {
+        var response = $(this).contents().find('#response').text();
+        if (response.length > 0)
+            blogSettings.photo(new Photo($.parseJSON(response)));
+    });
+}
+
+var Photo = function(data) {
+    var self = this;
+    self.id = ko.observable(data.id);
+    self.src = ko.observable(data.src);
 }
 
 var BlogInfoViewModel = function(data) {

edda337e32dc32686881249bff07c4a5841b70ce
choojoy-work <nikita@happy-giraffe.ru>
2013-06-28 11:47:21 +0400

    google adsense

---
+++ site/frontend/components/ClientScript.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 6/28/13
+ * Time: 11:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ClientScript extends CClientScript
+{
+    public function getHasNoindex()
+    {
+        $robotsTxt = array(
+            'albums',
+            'signup',
+            'search',
+            'messaging',
+        );
+
+        foreach ($robotsTxt as $segment)
+            if (strpos(Yii::app()->request->requestUri, '/' . $segment) === 0)
+                return true;
+
+
+        foreach ($this->metaTags as $tag)
+            if ($tag['name'] == 'robots' && $tag['content'] == 'noindex')
+                return true;
+
+        return false;
+    }
+}

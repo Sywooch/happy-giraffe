2602cd46fa132cd93cdc9dcbdaf28da7d25ebb7f
choojoy-work <nikita@happy-giraffe.ru>
2013-07-04 12:13:45 +0400

    удалить/восстановить запись

--- site/frontend/views/albums/w_photo_banner.php
+++ site/frontend/views/albums/w_photo_banner.php
@@ -35,8 +35,8 @@
                                 </td></tr></table>
                     </div>
 
-                    <a href="javascript:void(0)" class="prev" onclick="(adsbygoogle = window.adsbygoogle || []).push({});"><i class="icon"></i>предыдушая</a>
-                    <a href="javascript:void(0)" class="next" onclick="(adsbygoogle = window.adsbygoogle || []).push({});"><i class="icon"></i>следующая</a>
+                    <a href="javascript:void(0)" class="prev"><i class="icon"></i>предыдушая</a>
+                    <a href="javascript:void(0)" class="next"><i class="icon"></i>следующая</a>
 
                 </div>
 
@@ -48,11 +48,17 @@
 
             <div class="col-3">
                 <div class="margin-t145" id="AdfoxAjax">
-                    <script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <ins class="adsbygoogle"
-                         style="display:inline-block;width:240px;height:400px"
-                         data-ad-client="ca-pub-3807022659655617"
-                         data-ad-slot="3100101685"></ins>
+                    <script type="text/javascript"><!--
+                        google_ad_client = "ca-pub-3807022659655617";
+                        /* Google-gallery */
+                        google_ad_slot = "3100101685";
+                        google_ad_width = 240;
+                        google_ad_height = 400;
+                        //-->
+                    </script>
+                    <script type="text/javascript"
+                            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+                    </script>
                 </div>
             </div>
         </div>

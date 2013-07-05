fb67f4d96cef19460c9c2d000cd5ad441daf07ce
choojoy-work <nikita@happy-giraffe.ru>
2013-07-05 18:08:21 +0400

    удалить/восстановить запись

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -297,6 +297,32 @@
         })();
     </script>
 
+    <!-- tns-counter.ru -->
+    <script type="text/javascript">
+        (function(win, doc, cb){
+            (win[cb] = win[cb] || []).push(function() {
+                try {
+                    tnsCounterHappygiraffe_ru = new TNS.TnsCounter({
+                        'account':'happygiraffe_ru',
+                        'tmsec': 'happygiraffe_total'
+                    });
+                } catch(e){}
+            });
+
+            var tnsscript = doc.createElement('script');
+            tnsscript.type = 'text/javascript';
+            tnsscript.async = true;
+            tnsscript.src = ('https:' == doc.location.protocol ? 'https:' : 'http:') +
+                '//www.tns-counter.ru/tcounter.js';
+            var s = doc.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(tnsscript, s);
+        })(window, this.document,'tnscounter_callback');
+    </script>
+    <noscript>
+        <img src="//www.tns-counter.ru/V13a****happygiraffe_ru/ru/UTF-8/tmsec=happygiraffe_total/" width="0" height="0" alt="" />
+    </noscript>
+    <!--/ tns-counter.ru -->
+
     <?php if (!Yii::app()->user->isGuest && Yii::app()->user->id == 10):?>
         <script type="text/javascript">
             var _bdWidgetConfig = {

c04c76d44df74ac86d459a99dbdcf4c2869c87f5
Tantalid <tantalid@mail.ru>
2013-07-10 12:24:18 +0300

    profile

--- html/social/user/user_photoalbum_list.php
+++ html/social/user/user_photoalbum_list.php
@@ -57,51 +57,52 @@
                 <div class="col-gray">
                 
                     <div class="photo-preview-row clearfix">
-                        
-                        <div class="photo-grid clearfix">
-                            <div class="photo-grid_row clearfix" >
-                                <!-- Ловить клик на photo-grid_i для показа увеличенного фото -->
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-7.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                        <div class="photo-preview-row_hold2">
+                            <div class="photo-grid clearfix">
+                                <div class="photo-grid_row clearfix" >
+                                    <!-- Ловить клик на photo-grid_i для показа увеличенного фото -->
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-7.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-8.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-8.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-9.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-9.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-10.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-10.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-11.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-11.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="photo-grid_i">
-                                    <img class="photo-grid_img" src="/images/example/photo-grid-12.jpg" alt="">
-                                    <div class="photo-grid_overlay">
-                                        <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                    <div class="photo-grid_i">
+                                        <img class="photo-grid_img" src="/images/example/photo-grid-12.jpg" alt="">
+                                        <div class="photo-grid_overlay">
+                                            <span class="photo-grid_zoom powertip" title="Смотреть фото"></span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="photo-preview-row_last">
-                            <div class="font-small color-gray margin-b5">смотреть <br> все фото</div>
-                            <a href="" class="photo-preview-row_a">58 054</a>
+                            <div class="photo-preview-row_last">
+                                <div class="font-small color-gray margin-b5">смотреть <br> все фото</div>
+                                <a href="" class="photo-preview-row_a">58 054</a>
+                            </div>
                         </div>
                     </div>
                     

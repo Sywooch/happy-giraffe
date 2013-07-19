b1f191b39710d128dd53696619ef9f143a44d8b5
Kireev Alex <alexk984@gmail.com>
2013-07-19 18:03:39 +0400

    add photos to album

--- site/frontend/modules/blog/views/default/form/3.php
+++ site/frontend/modules/blog/views/default/form/3.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * @var CommunityContent $model
+ * @var HActiveRecord $slaveModel
+ * @var $json
+ */
+?>
 <div class="b-settings-blue b-settings-blue__photo">
     <div class="b-settings-blue_tale"></div>
     <div class="clearfix">
@@ -8,7 +15,9 @@
                 <img src="/images/b-settings-blue_photo-record-img1.png" alt="" class="">
             </div>
             <div class="clearfix">
-                <a href="javascript:;" class="btn-blue btn-h46" onclick="$(this).parents('.b-settings-blue').hide();$('#popup-user-add-photo').show();">Загрузить фото</a>
+                <a href="javascript:;" class="btn-blue btn-h46"
+                   onclick="$(this).parents('.b-settings-blue').hide();$('#popup-user-add-photo').show();">Загрузить
+                    фото</a>
             </div>
         </div>
 
@@ -18,7 +27,9 @@
                 <img src="/images/b-settings-blue_photo-record-img2.png" alt="" class="">
             </div>
             <div class="clearfix">
-                <a href="javascript:;" class="btn-blue btn-h46" onclick="$(this).parents('.b-settings-blue').hide();$('#popup-user-add-photo-post').show();">Создать фотопост</a>
+                <a href="javascript:;" class="btn-blue btn-h46"
+                   onclick="$(this).parents('.b-settings-blue').hide();$('#popup-user-add-photo-post').show();">Создать
+                    фотопост</a>
             </div>
         </div>
 
@@ -26,38 +37,32 @@
 </div>
 
 <div class="b-settings-blue b-settings-blue__photo" id="popup-user-add-photo-post" style="display: none;">
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'blog-form',
+        'action' => $model->isNewRecord ? array('save') : array('save', 'id' => $model->id),
+        'enableAjaxValidation' => true,
+        'enableClientValidation' => true,
+        'clientOptions' => array(
+            'validateOnSubmit' => true,
+        ),
+    )); ?>
     <div class="b-settings-blue_tale"></div>
     <div class="b-settings-blue_head">
         <div class="b-settings-blue_row clearfix">
             <div class="clearfix">
                 <div class="float-r font-small color-gray margin-3">0/50</div>
             </div>
-            <label for="" class="b-settings-blue_label">Заголовок</label>
-            <input type="text" name="" id="" class="itx-simple w-400" placeholder="Введите заголовок видео">
+            <?= $form->label($model, 'title', array('class' => 'b-settings-blue_label')) ?>
+            <?= $form->textField($model, 'title', array('class' => 'itx-simple w-400', 'placeholder' => 'Введите заголовок статьи', 'data-bind' => 'value: title, valueUpdate: \'keyup\'')) ?>
+            <?= $form->error($model, 'title') ?>
         </div>
         <div class="b-settings-blue_row clearfix">
             <label for="" class="b-settings-blue_label">Рубрика</label>
+
             <div class="w-400 float-l">
                 <div class="chzn-itx-simple">
-                    <select class="chzn chzn-done" id="selK3R" style="display: none;">
-                        <option selected="selected">0</option>
-                        <option>Россия</option>
-                        <option>2</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>132</option>
-                        <option>132</option>
-                        <option>132</option>
-                    </select><div id="selK3R_chzn" class="chzn-container chzn-container-single" style="width: 2px;"><a href="javascript:void(0)" class="chzn-single" tabindex="-1"><span>0</span><div><b></b></div></a><div class="chzn-drop" style="left: -9000px; width: 2px; top: 0px;"><div class="chzn-search" style=""><input type="text" autocomplete="off" style=""></div><ul class="chzn-results"><li id="selK3R_chzn_o_0" class="active-result result-selected" style="">0</li><li id="selK3R_chzn_o_1" class="active-result" style="">Россия</li><li id="selK3R_chzn_o_2" class="active-result" style="">2</li><li id="selK3R_chzn_o_3" class="active-result" style="">32</li><li id="selK3R_chzn_o_4" class="active-result" style="">32</li><li id="selK3R_chzn_o_5" class="active-result" style="">32</li><li id="selK3R_chzn_o_6" class="active-result" style="">32</li><li id="selK3R_chzn_o_7" class="active-result" style="">132</li><li id="selK3R_chzn_o_8" class="active-result" style="">132</li><li id="selK3R_chzn_o_9" class="active-result" style="">132</li></ul></div></div>
-                    <div class="chzn-itx-simple_add">
-                        <div class="chzn-itx-simple_add-hold">
-                            <input type="text" name="" id="" class="chzn-itx-simple_add-itx">
-                            <a href="" class="chzn-itx-simple_add-del" onclick="$.fancybox.close();return false;"></a>
-                        </div>
-                        <button class="btn-green">Ok</button>
-                    </div>
+                    <?= $form->dropDownList($model, 'rubric_id', CHtml::listData($this->user->blog_rubrics, 'id', 'title'), array('class' => 'chzn')) ?>
+                    <?= $form->error($model, 'rubric_id') ?>
                 </div>
             </div>
         </div>
@@ -80,10 +85,11 @@
         <div class="textalign-c clearfix">
             <!-- ko with: upload -->
             <!-- ko foreach: photos -->
-            <div class="b-add-img_i" data-bind="attr: {id: 'uploaded_photo' + uid}">
+            <div class="b-add-img_i" data-bind="attr: {id: 'uploaded_photo_' + uid}">
+                <div class="js-image" style="opacity: 0.2"></div>
                 <div class="b-add-img_i-vert"></div>
                 <div class="b-add-img_i-load">
-                    <div class="b-add-img_i-load-progress" data-bind="css: {width: progress}"></div>
+                    <div class="b-add-img_i-load-progress" data-bind="style: {width: progress}"></div>
                 </div>
                 <div class="b-add-img_i-overlay">
                     <a href="" class="b-add-img_i-del ico-close4" data-bind="click: remove"></a>
@@ -93,44 +99,49 @@
             <!-- /ko -->
         </div>
 
+        <!-- ko if: upload().photos().length == 0 -->
         <div class="b-add-img_html5-tx">или перетащите фото сюда</div>
+        <!-- /ko -->
     </div>
 
     <div class="b-settings-blue_row clearfix">
-        <textarea name="" id="" cols="80" rows="5" class="b-settings-blue_textarea itx-simple" placeholder="Ваш текст к фотопосту "></textarea>
+        <textarea name="" id="" cols="80" rows="5" class="b-settings-blue_textarea itx-simple"
+                  placeholder="Ваш текст к фотопосту "></textarea>
     </div>
     <div class=" clearfix">
-        <a href="" class="btn-blue btn-h46 float-r btn-inactive">Добавить</a>
-        <a href="" class="btn-gray-light btn-h46 float-r margin-r15" onclick="$.fancybox.close();return false;">Отменить</a>
+        <a href="" class="btn-blue btn-h46 float-r" data-bind="click: add, css: {'btn-inactive': upload().photos().length == 0}">Добавить</a>
+        <a href="" class="btn-gray-light btn-h46 float-r margin-r15"
+           onclick="$.fancybox.close();return false;">Отменить</a>
 
         <div class="float-l">
             <div class="privacy-select clearfix">
+                <?= $form->hiddenField($model, 'privacy', array('data-bind' => 'value: selectedPrivacyOption().value()')) ?>
                 <div class="privacy-select_hold clearfix">
                     <div class="privacy-select_tx">Для кого:</div>
                     <div class="privacy-select_drop-hold">
-                        <a href="" class="privacy-select_a">
-                            <span class="ico-users ico-users__friend active"></span>
-                            <span class="privacy-select_a-tx">только <br>друзьям</span>
+                        <a class="privacy-select_a"
+                           data-bind="click: $root.toggleDropdown, with: selectedPrivacyOption()">
+                            <span class="ico-users active" data-bind="css: 'ico-users__' + cssClass()"></span>
+                            <span class="privacy-select_a-tx" data-bind="html: title"></span>
                         </a>
                     </div>
-                    <div class="privacy-select_drop">
+                    <div class="privacy-select_drop" data-bind="css: { 'display-b' : showDropdown}">
+                        <!-- ko foreach: privacyOptions -->
                         <div class="privacy-select_i">
-                            <a href="" class="privacy-select_a">
-                                <span class="ico-users ico-users__all"></span>
-                                <span class="privacy-select_a-tx">для <br>всех</span>
+                            <a class="privacy-select_a" data-bind="click: select">
+                                <span class="ico-users" data-bind="css: 'ico-users__' + cssClass()"></span>
+                                <span class="privacy-select_a-tx" data-bind="html: title"></span>
                             </a>
                         </div>
-                        <div class="privacy-select_i">
-                            <a href="" class="privacy-select_a">
-                                <span class="ico-users ico-users__friend"></span>
-                                <span class="privacy-select_a-tx">только <br>друзьям</span>
-                            </a>
-                        </div>
+                        <!-- /ko -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <?php $this->endWidget(); ?>
+
 </div>
 
 
@@ -139,27 +150,10 @@
     <div class="b-settings-blue_head">
         <div class="b-settings-blue_row clearfix">
             <label for="" class="b-settings-blue_label">Фотоальбом</label>
+
             <div class="w-400 float-l">
                 <div class="chzn-itx-simple">
-                    <select class="chzn chzn-done" id="selY9Q" style="display: none;">
-                        <option selected="selected">0</option>
-                        <option>Россия</option>
-                        <option>2</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>32</option>
-                        <option>132</option>
-                        <option>132</option>
-                        <option>132</option>
-                    </select><div id="selY9Q_chzn" class="chzn-container chzn-container-single" style="width: 2px;"><a href="javascript:void(0)" class="chzn-single" tabindex="-1"><span>0</span><div><b></b></div></a><div class="chzn-drop" style="left: -9000px; width: 2px; top: 0px;"><div class="chzn-search" style=""><input type="text" autocomplete="off" style=""></div><ul class="chzn-results"><li id="selY9Q_chzn_o_0" class="active-result result-selected" style="">0</li><li id="selY9Q_chzn_o_1" class="active-result" style="">Россия</li><li id="selY9Q_chzn_o_2" class="active-result" style="">2</li><li id="selY9Q_chzn_o_3" class="active-result" style="">32</li><li id="selY9Q_chzn_o_4" class="active-result" style="">32</li><li id="selY9Q_chzn_o_5" class="active-result" style="">32</li><li id="selY9Q_chzn_o_6" class="active-result" style="">32</li><li id="selY9Q_chzn_o_7" class="active-result" style="">132</li><li id="selY9Q_chzn_o_8" class="active-result" style="">132</li><li id="selY9Q_chzn_o_9" class="active-result" style="">132</li></ul></div></div>
-                    <div class="chzn-itx-simple_add">
-                        <div class="chzn-itx-simple_add-hold">
-                            <input type="text" name="" id="" class="chzn-itx-simple_add-itx">
-                            <a href="" class="chzn-itx-simple_add-del"></a>
-                        </div>
-                        <button class="btn-green">Ok</button>
-                    </div>
+                    <?= CHtml::dropDownList('album_id', '', CHtml::listData(Album::model()->findAllByAttributes(array('author_id' => Yii::app()->user->id, 'type' => 0)), 'id', 'title'), array('class' => 'chzn', 'data-bind' => 'value:id')) ?>
                 </div>
             </div>
         </div>
@@ -173,16 +167,17 @@
             </div>
             <div class="file-fake">
                 <button class="btn-green btn-medium file-fake_btn">Обзор</button>
-                <input type="file" class="js-upload-files-multiple" multiple />
+                <input type="file" class="js-upload-files-multiple" multiple/>
             </div>
         </div>
         <div class="textalign-c clearfix">
             <!-- ko with: upload -->
             <!-- ko foreach: photos -->
             <div class="b-add-img_i" data-bind="attr: {id: 'uploaded_photo_' + uid}">
+                <div class="js-image" style="opacity: 0.2"></div>
                 <div class="b-add-img_i-vert"></div>
                 <div class="b-add-img_i-load">
-                    <div class="b-add-img_i-load-progress" data-bind="css: {width: progress}"></div>
+                    <div class="b-add-img_i-load-progress" data-bind="style: {width: progress}"></div>
                 </div>
                 <div class="b-add-img_i-overlay">
                     <a href="" class="b-add-img_i-del ico-close4" data-bind="click: remove"></a>
@@ -191,60 +186,61 @@
             <!-- /ko -->
             <!-- /ko -->
         </div>
-        <!-- Текст приглашения для перетаскивания можно скрыть или удалить при наличии фото -->
+
+        <!-- ko if: upload().photos().length == 0 -->
         <div class="b-add-img_html5-tx">или перетащите фото сюда</div>
+        <!-- /ko -->
     </div>
 
     <div class=" clearfix">
-        <a href="" class="btn-blue btn-h46 float-r">Добавить</a>
-        <a href="" class="btn-gray-light btn-h46 float-r margin-r15">Отменить</a>
+        <a href="" class="btn-blue btn-h46 float-r" data-bind="click: add, css: {'btn-inactive': upload().photos().length == 0}">Добавить</a>
+        <a href="javascript:;" class="btn-gray-light btn-h46 float-r margin-r15"
+           onclick="$.fancybox.close()">Отменить</a>
 
         <div class="float-l">
             <div class="privacy-select clearfix">
-                <div class="privacy-select_hold clearfix">
-                    <div class="privacy-select_tx">Для кого:</div>
-                    <div class="privacy-select_drop-hold">
-                        <a href="" class="privacy-select_a">
-                            <span class="ico-users ico-users__friend active"></span>
-                            <span class="privacy-select_a-tx">только <br>друзьям</span>
-                        </a>
-                    </div>
-                    <div class="privacy-select_drop display-b">
-                        <div class="privacy-select_i">
-                            <a href="" class="privacy-select_a">
-                                <span class="ico-users ico-users__all"></span>
-                                <span class="privacy-select_a-tx">для <br>всех</span>
-                            </a>
-                        </div>
-                        <div class="privacy-select_i">
-                            <a href="" class="privacy-select_a">
-                                <span class="ico-users ico-users__friend"></span>
-                                <span class="privacy-select_a-tx">только <br>друзьям</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
 </div>
 
 
-
 <script type="text/javascript">
-    $(function() {
-        var PhotoPostViewModel = function() {
+    $(function () {
+        var PhotoPostViewModel = function (data) {
             var self = this;
+            ko.utils.extend(self, new BlogFormViewModel(data));
             self.upload = ko.observable(new UploadPhotos());
-            self.title = ko.observable('');
-            self.text = ko.observable('');
+
+            self.add = function () {
+                if (self.upload().photos().length > 0)
+                    var a = 0;
+            }
         };
-        var formVM1 = new PhotoPostViewModel();
+        var formVM1 = new PhotoPostViewModel(<?=CJSON::encode($json)?>);
         ko.applyBindings(formVM1, document.getElementById('popup-user-add-photo-post'));
 
-        var PhotoAlbumViewModel = function() {
+        var PhotoAlbumViewModel = function () {
             var self = this;
+            self.id = 0;
             self.upload = ko.observable(new UploadPhotos());
+
+            self.add = function () {
+                if (self.upload().photos().length > 0){
+                    var photo_ids = [];
+                    var a = self.upload().photos();
+                    for (var i = 0; i < a.length; i++){
+                        photo_ids.push(a[i].id());
+                    }
+
+                    console.log(photo_ids);
+                    $.post('/ajaxSimple/addPhoto/', {album_id: self.id, photo_ids: photo_ids}, function (response) {
+                        if (response.status)
+                            $.fancybox.close();
+                    }, 'json');
+                }
+            }
         };
         var formVM2 = new PhotoAlbumViewModel();
         ko.applyBindings(formVM2, document.getElementById('popup-user-add-photo'));

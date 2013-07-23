6367a7dc446a3c89d934e269e781ded4848ffd58
Kireev Alex <alexk984@gmail.com>
2013-07-23 17:33:08 +0400

    commentators module - team awards

--- site/frontend/modules/signal/views/commentator/award/me.php
+++ site/frontend/modules/signal/views/commentator/award/me.php
@@ -4,10 +4,8 @@
  * @var $month string
  * @author Alex Kireev <alexk984@gmail.com>
  */
-
 $commentatorMonth = CommentatorsMonth::get($month);
 ?><div class="block">
-
     <?php $this->renderPartial('_month_list', array('month' => $month, 'params'=>array('type'=>'me'))); ?>
 
     <div class="award-me clearfix">
@@ -23,6 +21,8 @@ $commentatorMonth = CommentatorsMonth::get($month);
                 <a href="/commentator/secrets/#friends">Как завести наибольшее количество дружеских связей (болше всего друзей на сайте)</a>
             </div>
         </div>
+
+
         <?php $place = $commentatorMonth->getPlace(Yii::app()->user->id, CommentatorsMonth::IM_MESSAGES) ?>
         <div class="award-me_i award-me_i__3<?php if ($place < 4) echo ' win' ?>">
             <div class="award-me_t">Личная <br> переписка</div>
@@ -33,6 +33,36 @@ $commentatorMonth = CommentatorsMonth::get($month);
             <div class="award-me_desc">
                 <div class="ico-info"></div> <br>
                 <a href="/commentator/secrets/#messages">Самый коммуникабельный сотрудник (тот, кто больше всего отправил сообщений по внутренней почте) – входящие и исходящие сообщения</a>
+            </div>
+        </div>
+
+
+        <?php $place = $commentatorMonth->getPlace(Yii::app()->user->id, CommentatorsMonth::RECORDS_COUNT) ?>
+        <div class="award-me_i award-me_i__2<?php if ($place < 4) echo ' win' ?>">
+            <div class="award-me_t">Количество <br> записей</div>
+            <div class="award-me_value"><?=$commentatorMonth->getStatValue(Yii::app()->user->id, CommentatorsMonth::RECORDS_COUNT) ?></div>
+            <div class="award-me_place">
+                <?=$commentatorMonth->getPlaceView(Yii::app()->user->id, CommentatorsMonth::RECORDS_COUNT) ?>
+            </div>
+        </div>
+
+
+        <?php $place = $commentatorMonth->getPlace(Yii::app()->user->id, CommentatorsMonth::MOST_COMMENTED_POST) ?>
+        <div class="award-me_i award-me_i__2<?php if ($place < 4) echo ' win' ?>">
+            <div class="award-me_t">Наибольшее кол-во <br> комментариев <br> к посту</div>
+            <div class="award-me_value"><?=$commentatorMonth->getStatValue(Yii::app()->user->id, CommentatorsMonth::MOST_COMMENTED_POST) ?></div>
+            <div class="award-me_place">
+                <?=$commentatorMonth->getPlaceView(Yii::app()->user->id, CommentatorsMonth::MOST_COMMENTED_POST) ?>
+            </div>
+        </div>
+
+
+        <?php $place = $commentatorMonth->getPlace(Yii::app()->user->id, CommentatorsMonth::GOOD_COMMENTS_COUNT) ?>
+        <div class="award-me_i award-me_i__2<?php if ($place < 4) echo ' win' ?>">
+            <div class="award-me_t">Кол-во <br>развернутых<br> комментариев</div>
+            <div class="award-me_value"><?=$commentatorMonth->getStatValue(Yii::app()->user->id, CommentatorsMonth::GOOD_COMMENTS_COUNT) ?></div>
+            <div class="award-me_place">
+                <?=$commentatorMonth->getPlaceView(Yii::app()->user->id, CommentatorsMonth::GOOD_COMMENTS_COUNT) ?>
             </div>
         </div>
     </div>

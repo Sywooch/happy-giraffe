6128adacdaf9f57513d98cf7bb24ef2f9ee5e533
Kireev Alex <alexk984@gmail.com>
2013-07-25 11:40:23 +0400

    layout fix

--- site/frontend/modules/notifications/views/default/list.php
+++ site/frontend/modules/notifications/views/default/list.php
@@ -5,8 +5,17 @@
  * @author Alex Kireev <alexk984@gmail.com>
  */
 ?>
-<?php foreach ($list as $model) :?>
-<div class="user-notice-list_i">
-    <?php $this->renderPartial('types/type_' . $model->type, compact('model', 'check')); ?>
+<?php if (!empty($list)):?>
+    <?php foreach ($list as $model) :?>
+    <div class="user-notice-list_i">
+        <?php $this->renderPartial('types/type_' . $model->type, compact('model', 'check')); ?>
+    </div>
+    <?php endforeach; ?>
+<?php else: ?>
+<div class="cap-empty cap-empty__rel">
+    <div class="cap-empty_hold">
+        <div class="cap-empty_tx">У вас пока нет новых уведомлений.</div>
+        <span class="cap-empty_gray" href="">Будьте активны! Добавляйте записи, фото, видео.</span>
+    </div>
 </div>
-<?php endforeach; ?>
+<?php endif; ?>

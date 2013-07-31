9b1c4b607f01e2b6655fb08654dfd130c2fe82f6
choojoy-work <nikita@happy-giraffe.ru>
2013-07-31 09:27:45 +0400

    Merge branch 'gallery' into blog
    Conflicts:
    	site/frontend/config/main.php

---
+++ site/frontend/modules/gallery/components/AlbumPhotoCollection.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 7/30/13
+ * Time: 5:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AlbumPhotoCollection extends PhotoCollection
+{
+    public $albumId;
+
+    public function generateIds()
+    {
+        return Yii::app()->db->createCommand("SELECT id FROM album__photos WHERE album_id = :album_id")->queryColumn(array(':album_id' => $this->albumId));
+    }
+
+    protected function getIdsCacheDependency()
+    {
+        $dependency = new CDbCacheDependency("SELECT COUNT(*) FROM album__photos WHERE album_id = :album_id");
+        $dependency->params = array(':album_id' => $this->albumId);
+        return $dependency;
+    }
+
+    protected function generateModels($ids)
+    {
+        $criteria = new CDbCriteria(array(
+            'with' => array(
+                'photo' => array(
+                    'with' => array('author'),
+                ),
+            ),
+            'order' => new CDbExpression('FIELD(t.photo_id, ' . implode(',', $ids) . ')')
+        ));
+        $criteria->addInCondition('t.id', $ids);
+        return AlbumPhoto::model()->findAll($criteria);
+    }
+
+    protected function toJSON($model)
+    {
+        return array(
+            'id' => $model->id,
+            'title' => $model->title,
+            'description' => '',
+            'src' => $model->getPreviewUrl(804, null, Image::WIDTH),
+            'date' => HDate::GetFormattedTime($model->created),
+            'user' => array(
+                'id' => $model->author->id,
+                'firstName' => $model->author->first_name,
+                'lastName' => $model->author->last_name,
+                'gender' => $model->author->gender,
+                'ava' => $model->author->getAva('small'),
+                'url' => $model->author->url,
+            ),
+        );
+    }
+}

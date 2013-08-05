d69e5e54ec2f01a6a02945992da667c86d2b4361
Tantalid <tantalid@mail.ru>
2013-08-05 14:17:52 +0300

    fix

--- html/sign/sign-add-club.php
+++ html/sign/sign-add-club.php
@@ -11,16 +11,111 @@
 
 <div class="layout-container">
 	<div class="layout-container_hold">
-		<div class="clearfix">
-			<div class="logo-box">
-				<a title="Веселый жираф - Happy-giraffe.ru" class="logo" href="/">Веселый жираф</a>
-				<span>САЙТ ДЛЯ ВСЕЙ СЕМЬИ</span>
+		<div class="layout-wrapper">
+			<div class="clearfix">
+				<div class="logo-box">
+					<a title="Веселый жираф - Happy-giraffe.ru" class="logo" href="/">Веселый жираф</a>
+					<span>САЙТ ДЛЯ ВСЕЙ СЕМЬИ</span>
+				</div>
 			</div>
+			
+			<div class="b-registration">
+				<div class="b-registration_row clearfix">
+					<div class="float-r">
+						<a href="" class="b-registration_skip">Пропустить этот шаг</a>
+						<a href="" class="btn-green btn-h46">Готово, продолжить</a>
+					</div>
+					<div class="b-registration_t">Александра, выберите клубы</div>
+				</div>
+				
+				<div class="content-cols">
+					<div class="col-white padding-20">
+						<div class="margin-20">
+							<span class="i-highlight i-highlight__big font-big">Веселый Жираф вам рекомендует</span>
+						</div>
+						<div class="club-list clearfix">
+							<ul class="club-list_ul clearfix">
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/1.png">
+										</span>
+										<span class="club-list_i-name">Украшаем дом</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li club-list_li__in">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/2.png">
+										</span>
+										<span class="club-list_i-name">Дети</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/3.png">
+										</span>
+										<span class="club-list_i-name">Цветы в доме</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/4.png">
+										</span>
+										<span class="club-list_i-name">Свадьба</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/5.png">
+										</span>
+										<span class="club-list_i-name">Наши питомцы</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/6.png">
+										</span>
+										<span class="club-list_i-name">Образование</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/7.png">
+										</span>
+										<span class="club-list_i-name">Выходные с семьей</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+								<li class="club-list_li">
+									<a class="club-list_i" href="">
+										<span class="club-list_img-hold">
+											<img class="club-list_img" alt="" src="/images/club/8.png">
+										</span>
+										<span class="club-list_i-name">Красота и мода</span>
+									</a>
+									<a class="club-list_check powertip" href=""></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			 	
+			
 		</div>
-		 	
-		
 	</div>
-
 </div>
 
 </body>

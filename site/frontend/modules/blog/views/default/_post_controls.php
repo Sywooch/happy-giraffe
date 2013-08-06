f6f39f3e814d49f8544c03ae08751ef3da17664d
Kireev Alex <alexk984@gmail.com>
2013-08-06 12:48:02 +0400

    bug fixes

--- site/frontend/modules/blog/views/default/_post_controls.php
+++ site/frontend/modules/blog/views/default/_post_controls.php
@@ -18,17 +18,17 @@ $ViewModelData = $model->getSettingsViewModel();
     <?php if ($model->author_id == Yii::app()->user->id):?>
         <div class="article-settings">
             <div class="article-settings_i">
-                <a href="javascript:;" class="article-settings_a article-settings_a__settings powertip" data-bind='css: {active: displayOptions}, click: show'></a>
+                <a href="javascript:;" class="article-settings_a article-settings_a__settings powertip" data-bind='css: {active: displayOptions}, click: show' title="Настройки"></a>
             </div>
             <div class="article-settings_hold" data-bind="slideVisible: displayOptions()">
                 <div class="article-settings_i">
-                    <a href="" class="article-settings_a article-settings_a__pin powertip" data-bind="click: attach, css: {active: attached}"></a>
+                    <a href="" class="article-settings_a article-settings_a__pin powertip" data-bind="click: attach, css: {active: attached}" title="Прикрепить сверху"></a>
                 </div>
                 <div class="article-settings_i">
-                    <a href="<?=$this->createUrl('form', array('id' => $model->id))?>" data-theme="transparent" class="article-settings_a article-settings_a__edit powertip fancy"></a>
+                    <a href="<?=$this->createUrl('form', array('id' => $model->id))?>" data-theme="transparent" class="article-settings_a article-settings_a__edit powertip fancy" title="Редактировать"></a>
                 </div>
                 <div class="article-settings_i">
-                    <a href="javascript:;" class="ico-users powertip" data-bind="css: {active: displayPrivacy, 'ico-users__friend': privacy() == 1, 'ico-users__all': privacy() == 0}, click: showPrivacy"></a>
+                    <a href="javascript:;" class="ico-users powertip" data-bind="css: {active: displayPrivacy, 'ico-users__friend': privacy() == 1, 'ico-users__all': privacy() == 0}, click: showPrivacy" title="Приватность"></a>
                     <div class="article-settings_drop" data-bind="toggleVisible: displayPrivacy()">
                         <div class="article-settings_drop-i">
                             <a href="" class="article-settings_drop-a" data-bind="click: function(data, event) { setPrivacy(0, data, event)}">
@@ -45,7 +45,7 @@ $ViewModelData = $model->getSettingsViewModel();
                     </div>
                 </div>
                 <div class="article-settings_i">
-                    <a class="article-settings_a article-settings_a__delete powertip" data-bind="click: remove"></a>
+                    <a class="article-settings_a article-settings_a__delete powertip" data-bind="click: remove" title="Удалить"></a>
                 </div>
             </div>
         </div>

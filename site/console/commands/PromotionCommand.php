e28290e037c77448fccc0c88b019192b0c25c9da
Kireev Alex <alexk984@gmail.com>
2013-08-07 17:45:37 +0400

    fixes

--- site/console/commands/PromotionCommand.php
+++ site/console/commands/PromotionCommand.php
@@ -7,6 +7,7 @@
 Yii::import('site.seo.models.*');
 Yii::import('site.seo.models.mongo.*');
 Yii::import('site.seo.components.*');
+Yii::import('site.seo.components.wordstat.*');
 Yii::import('site.seo.modules.competitors.models.*');
 Yii::import('site.seo.modules.writing.models.*');
 Yii::import('site.seo.modules.promotion.models.*');

5fd9cc42fd1920e729b059ba186e8965b217ccbe
choojoy-work <nikita@happy-giraffe.ru>
2013-08-07 10:15:25 +0400

    search

--- site/frontend/modules/blog/views/settings/settings.php
+++ site/frontend/modules/blog/views/settings/settings.php
@@ -146,8 +146,23 @@
 
             $('#upload-target').on('load', function() {
                 var response = $(this).contents().find('#response').text();
-                if (response.length > 0)
+                if (response.length > 0) {
                     blogVM.draftPhoto(new Photo($.parseJSON(response)));
+                    jcrop_api.destroy();
+                    var x = self.draftPhoto().width()/2 - 720/2;
+                    var y = self.draftPhoto().height()/2 - 128/2;
+                    var x2 = x + 720;
+                    var y2 = y + 128;
+                    $('.popup-blog-set_jcrop-img').Jcrop({
+                        setSelect: [ x, y, x2, y2 ],
+                        onChange: showPreview,
+                        onSelect: showPreview,
+                        aspectRatio: 720 / 128,
+                        boxWidth: 320
+                    }, function(){
+                        jcrop_api = this;
+                    });
+                }
             });
         });
     </script>

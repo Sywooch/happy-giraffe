c23e7dba0b28d51e4cb48e6a8cf2de0d30dce046
choojoy-work <nikita@happy-giraffe.ru>
2013-08-07 14:52:47 +0400

    search

--- site/frontend/modules/gallery/controllers/UserController.php
+++ site/frontend/modules/gallery/controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends HController
 {
     public $layout = '//layouts/common_new';
 
-    public function actionIndex($userId = 12936)
+    public function actionIndex($userId)
     {
         $scopes = !Yii::app()->user->isGuest && Yii::app()->user->id == $userId ? array() : array('noSystem');
         $dataProvider = Album::model()->findByUser($userId, false, false, $scopes);

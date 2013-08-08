acaa1d604efc6bbd89c17106cc218c42f28a3a43
Kireev Alex <alexk984@gmail.com>
2013-08-08 13:04:27 +0400

    convert images fix

--- site/frontend/views/albums/_widget.php
+++ site/frontend/views/albums/_widget.php
@@ -8,9 +8,9 @@ if (isset($title))
 if (isset($alt))
     $add = ' alt="'.$alt.'"';
 
-?><!-- widget: { entity : 'AlbumPhoto', entity_id : '<?=$model->id ?>' } --><?php
+?><!-- widget: { entity : 'AlbumPhoto', entity_id : '<?php echo $model->id ?>' } --><?php
 if ($model->width >= 580):
-    ?><div class="b-article_in-img"><img src="<?=$model->getPreviewUrl(580, 1000) ?>" class="content-img"<?=$add ?>></div><?php
+    ?><div class="b-article_in-img"><img src="<?php echo $model->getPreviewUrl(580, 1000) ?>" class="content-img"<?php echo $add ?>></div><?php
 else:
-    ?><div class="clearfix"><img src="<?=$model->getPreviewUrl(540, 1000) ?>" class="content-img"<?=$add ?>></div><?php
+    ?><div class="clearfix"><img src="<?php echo $model->getPreviewUrl(540, 1000) ?>" class="content-img"<?php echo $add ?>></div><?php
 endif; ?><!-- /widget -->

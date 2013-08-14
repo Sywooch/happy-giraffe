af5dad4b02c76532dde128d42c249d5ecb023453
choojoy-work <nikita@happy-giraffe.ru>
2013-08-14 16:05:30 +0400

    gallery history API

--- site/frontend/views/albums/w_photo_banner.php
+++ site/frontend/views/albums/w_photo_banner.php
@@ -48,7 +48,25 @@
 
             <div class="col-3">
                 <div class="margin-t145" id="AdfoxAjax">
-                    <iframe id="direct" src="http://www.happy-giraffe.ru/rtb2.html" width="200" height="300" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"></iframe>
+                    <div id="yandex_ad_R-87026-4"></div>
+                    <script type="text/javascript">
+                        (function(w, d, n, s, t) {
+                            w[n] = w[n] || [];
+                            w[n].push(function() {
+                                Ya.Context.AdvManager.render({
+                                    blockId: "R-87026-4",
+                                    renderTo: "yandex_ad_R-87026-4",
+                                    async: true
+                                });
+                            });
+                            t = d.getElementsByTagName("script")[0];
+                            s = d.createElement("script");
+                            s.type = "text/javascript";
+                            s.src = "//an.yandex.ru/system/context.js";
+                            s.async = true;
+                            t.parentNode.insertBefore(s, t);
+                        })(this, this.document, "yandexContextAsyncCallbacks");
+                    </script>
                 </div>
             </div>
         </div>
@@ -68,9 +86,11 @@
 
     <script type="text/javascript">
         function rtb_refresh() {
-            var r = Math.floor(Math.random() * 1000000) + 1;
-            var url = 'http://www.happy-giraffe.ru/rtb2.html?r=' + r;
-            $('#direct').attr('src', url);
+            return Ya.Context.AdvManager.render({
+                blockId: "R-87026-4",
+                renderTo: "yandex_ad_R-87026-4",
+                async: true
+            });
         }
     </script>
 

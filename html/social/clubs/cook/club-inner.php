637bc49e15ab802ea6818fdb9c7d5c4eb5c93257
Tantalid <tantalid@mail.ru>
2013-08-20 12:39:15 +0300

    Club + fix

---
+++ html/social/clubs/cook/club-inner.php
@@ -0,0 +1,212 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+	
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		
+		<div class="layout-content clearfix">
+		<div class="content-cols clearfix">
+			<div class="col-1">
+				<div class="sidebar-search clearfix">
+					<input type="text" placeholder="Поиск по сайту" class="sidebar-search_itx" id="" name="">
+					<!-- 
+					В начале ввода текста, скрыть sidebar-search_btn добавить класс active"
+					 -->
+					<button class="sidebar-search_btn"></button>
+				</div>
+			</div>
+			<div class="col-23-middle">
+				<div class="user-add-record user-add-record__small clearfix">
+					<div class="user-add-record_ava-hold">
+						<a href="" class="ava male middle">
+							<span class="icon-status status-online"></span>
+							<img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+						</a>
+					</div>
+					<div class="user-add-record_hold">
+						<div class="user-add-record_tx">Я хочу добавить</div>
+						<a href="#popup-user-add-article"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__article fancy powertip" title="Статью"></a>
+						<a href="#popup-user-add-photo"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__photo fancy powertip" title="Фото"></a>
+						<a href="#popup-user-add-video"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__video fancy active powertip" title="Видео"></a>
+						<a href="#popup-user-add-status"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__status fancy powertip" title="Статус"></a>
+					</div>
+				</div>
+				<div class="padding-l20">
+					<div class="crumbs-small clearfix">
+						<ul class="crumbs-small_ul">  
+							<li class="crumbs-small_li">Я здесь:</li>
+							<li class="crumbs-small_li"><a href="" class="crumbs-small_a">Главная</a></li> &gt;
+							<li class="crumbs-small_li"><a href="" class="crumbs-small_a">Наш дом</a></li> &gt;
+							<li class="crumbs-small_li"><a href="" class="crumbs-small_a">Готовим на кухне </a></li> &gt;
+							<li class="crumbs-small_li"><span class="crumbs-small_last">Кулинарные рецепты</span></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="b-section">
+			<div class="b-section_hold">
+				<div class="content-cols clearfix">
+					<div class="col-1">
+						<div class="club-list club-list__big clearfix">
+							<ul class="club-list_ul textalign-c clearfix">
+								<li class="club-list_li">
+									<a href="" class="club-list_i">
+										<span class="club-list_img-hold">
+											<img src="/images/club/9-w130.png" alt="" class="club-list_img">
+										</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-23-middle">
+						<div class="padding-l20">
+							<h1 class="b-section_t"><a href="">Готовим на кухне</a></h1>
+							<div class="clearfix">
+								<ul class="b-section_ul clearfix">
+									<li class="b-section_li"><span class="b-section_li-a active">Рецепты</span></li>
+									<li class="b-section_li"><a href="" class="b-section_li-a">Для детей</a></li>
+									<li class="b-section_li"><a href="" class="b-section_li-a">Для мультиварки</a></li>
+									<li class="b-section_li"><a href="" class="b-section_li-a">Форум</a></li>
+									<li class="b-section_li"><a href="" class="b-section_li-a">Сервисы</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="content-cols clearfix">
+			<div class="col-1">
+			
+				<div class="readers2">
+					<a class="btn-green btn-medium" href="">Вступить в клуб</a>
+					<ul class="readers2_ul clearfix">
+						<li class="readers2_li clearfix">
+							<a class="ava female small" href="">
+								<span class="icon-status status-online"></span>
+								<img src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG" alt="">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a class="ava female small" href="">
+								<img src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG" alt="">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a class="ava female small" href="">
+								<span class="icon-status status-online"></span>
+								<img src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG" alt="">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a class="ava female small" href="">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a class="ava female small" href="">
+								<span class="icon-status status-online"></span>
+								<img src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG" alt="">
+							</a>
+						</li>
+						<li class="readers2_li clearfix">
+							<a class="ava male small" href="">
+								<span class="icon-status status-online"></span>
+							</a>
+						</li>
+					</ul>
+					<div class="clearfix">
+						<div class="readers2_count">Все участники клуба (129)</div>
+					</div>
+				</div>
+				
+				<div class="sidebar-search sidebar-search__gray clearfix">
+					<input type="text" placeholder="Поиск из 15611 рецептов" class="sidebar-search_itx" id="" name="">
+					<!-- 
+					В начале ввода текста, скрыть sidebar-search_btn добавить класс active"
+					 -->
+					<button class="sidebar-search_btn"></button>
+				</div>
+					
+				<div class="menu-simple">
+					<ul class="menu-simple_ul">
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Обо всем</a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Свадьба - прекрасный миг</a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Прикольное видео </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Школа восточного танца  </a>
+						</li>
+						<li class="menu-simple_li active">
+							<a class="menu-simple_a" href="">Мой мужчина </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Детские передачи </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Свадьбы </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Кормление ребенка </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Воспитание детей </a>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="banner">
+					<a href="">
+						<img src="/images/banners/6.jpg" alt="">
+					</a>
+				</div>
+			</div>
+			<div class="col-23-middle ">
+			
+			
+				<div class="clearfix margin-r20 margin-b20">
+					<a href="" class="btn-blue btn-h46 float-r">Добавить рецепт</a>
+					<div class="heading-title margin-b5 clearfix">
+						Торты и пирожные
+					</div>
+					<p class="font-middle margin-l20">Одним из основных свидетельств правильного течения <br> беременности является набор веса согласно принятым нормам. </p>
+				</div>
+				<div class="col-gray">
+					<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/articles.php'; ?>
+
+				</div>
+			</div>
+		</div>
+		</div>
+		
+		<a href="#layout" id="btn-up-page"></a>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+
+<div class="display-n">
+	
+</div>
+</body>
+</html>
---
+++ html/social/clubs/handiwork/club-member.php
@@ -0,0 +1,261 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+	
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		
+		<div class="layout-content clearfix">
+		<div class="content-cols clearfix">
+			<div class="col-1">
+				<div class="sidebar-search clearfix">
+					<input type="text" placeholder="Поиск по сайту" class="sidebar-search_itx" id="" name="">
+					<!-- 
+					В начале ввода текста, скрыть sidebar-search_btn добавить класс active"
+					 -->
+					<button class="sidebar-search_btn"></button>
+				</div>
+			</div>
+			<div class="col-23-middle">
+				<div class="user-add-record user-add-record__small clearfix">
+					<div class="user-add-record_ava-hold">
+						<a href="" class="ava male middle">
+							<span class="icon-status status-online"></span>
+							<img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+						</a>
+					</div>
+					<div class="user-add-record_hold">
+						<div class="user-add-record_tx">Я хочу добавить</div>
+						<a href="#popup-user-add-article"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__article fancy powertip" title="Статью"></a>
+						<a href="#popup-user-add-photo"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__photo fancy powertip" title="Фото"></a>
+						<a href="#popup-user-add-video"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__video fancy active powertip" title="Видео"></a>
+						<a href="#popup-user-add-status"  data-theme="transparent" class="user-add-record_ico user-add-record_ico__status fancy powertip" title="Статус"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="b-section b-section__club b-section__club-handiwork">
+			<div class="b-section_hold">
+				<div class="content-cols clearfix">
+					<div class="col-1">
+						<div class="club-list club-list__large clearfix">
+							<ul class="club-list_ul textalign-c clearfix">
+								<li class="club-list_li">
+									<a href="" class="club-list_i">
+										<span class="club-list_img-hold">
+											<img src="/images/club/24-w240.jpg" alt="" class="club-list_img">
+										</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-23-middle clearfix">
+						<div class="b-section_transp">
+							<h1 class="b-section_transp-t">Рукоделие</h1>
+							<div class="b-section_transp-desc">Здесь собрано все что нужно для цветоводов. Растения, удобрения <br>чувство юмора имеется, на шею не сажусь, проблемами не загружаю. </div>
+							<div class="b-section_club-moder">
+								<span class="b-section_club-moder-tx">Модераторы <br> клуба</span>
+								<a href="" class="ava"><img src="/images/user_friends_img.jpg"></a>
+								<a class="ava male" href="">
+									<span class="icon-status status-online"></span>
+									<img src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" alt="">
+								</a>
+								<a href="" class="ava female"></a>
+							</div>
+						</div>
+						<ul class="b-section_ul b-section_ul__white margin-l30 clearfix">
+							<li class="b-section_li"><a href="" class="b-section_li-a">Форум</a></li>
+							<li class="b-section_li"><a href="" class="b-section_li-a">Маршруты</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="content-cols clearfix">
+			<div class="col-1">
+			
+				<div class="widget-friends clearfix">
+					<div class="clearfix">
+						<span class="heading-small">Участники клуба <span class="color-gray">(1876)</span> </span>
+						
+					</div>
+					<ul class="widget-friends_ul clearfix">
+						<li class="widget-friends_i">
+							<a class="ava male" href="">
+								<span class="icon-status status-online"></span>
+								<img src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" alt="">
+							</a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava female"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"><img src="/images/user_friends_img.jpg"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"><img src="/images/user_friends_img.jpg"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"><img src="/images/user_friends_img.jpg"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"><img src="/images/user_friends_img.jpg"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a class="ava male" href="">
+								<span class="icon-status status-online"></span>
+								<img src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" alt="">
+							</a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"></a>
+						</li>
+						<li class="widget-friends_i">
+							<a href="" class="ava"></a>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="menu-simple">
+					<ul class="menu-simple_ul">
+						<li class="menu-simple_li menu-simple_li__with-drop">
+							<a class="menu-simple_a" href="">Вышивка</a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Шитье</a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Вязание </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Бижутерия  </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Скрапбукинг </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Квиллинг </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Декупаж </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Бисероплетение </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Мыловарение </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Декоративно-прикладное творчество </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Фотокружок </a>
+						</li>
+						<li class="menu-simple_li menu-simple_li__with-drop active">
+							<a class="menu-simple_a" href="">Роспись </a>
+							<ul class="menu-simple_ul">
+								<li class="menu-simple_li">
+									<a class="menu-simple_a" href="">По дереву </a>
+								</li>
+								<li class="menu-simple_li active">
+									<a class="menu-simple_a" href="">По ткани </a>
+								</li>
+								<li class="menu-simple_li">
+									<a class="menu-simple_a" href="">По стеклу </a>
+								</li>
+								<li class="menu-simple_li">
+									<a class="menu-simple_a" href="">По керамике </a>
+								</li>
+							</ul>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Плетение </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Валяние из шерсти </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Декор </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Новая жизнь старых вещей </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Куклы своими руками </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Упаковка подарков </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Сувениры и поделки из теста </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Кружево </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Оригами </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Пэчворк </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Канзаши </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Картонаж </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Макраме </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Кофейные зерна </a>
+						</li>
+						<li class="menu-simple_li">
+							<a class="menu-simple_a" href="">Свечи и свечеварение </a>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="banner">
+					<a href="">
+						<img src="/images/banners/8.jpg" alt="">
+					</a>
+				</div>
+			</div>
+			<div class="col-23-middle ">
+				<div class="clearfix margin-r20 margin-b20">
+					<a href="" class="btn-blue btn-h46 float-r">Добавить в клуб</a>
+				</div>
+				<div class="col-gray">
+					<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/articles.php'; ?>
+
+				</div>
+			</div>
+		</div>
+		</div>
+		
+		<a href="#layout" id="btn-up-page"></a>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+
+<div class="display-n">
+	
+</div>
+</body>
+</html>

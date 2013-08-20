b38971a334c20f4731a938c97e5c4e18b6c9c944
choojoy-work <nikita@happy-giraffe.ru>
2013-08-20 18:52:30 +0400

    step 3

--- site/frontend/views/signup/family.php
+++ site/frontend/views/signup/family.php
@@ -53,67 +53,18 @@
                     <div class="b-family b-family__bg-none margin-b10">
                         <ul class="b-family_ul">
                         <!-- Объект для перетаскивания .b-family_li -->
-                            <li class="b-family_li">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__husband"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Муж</span>
-                                </div>
-                            </li>
-                            <li class="b-family_li">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__fiance"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Жених</span>
-                                </div>
-                            </li>
-                            <li class="b-family_li">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__boy-friend"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Друг</span>
-                                </div>
-                            </li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[0], template: { name : 'element-template', data : partnerModels[0] }"></li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[1], template: { name : 'element-template', data : partnerModels[1] }"></li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[1], template: { name : 'element-template', data : partnerModels[2] }"></li>
                         </ul>
                     </div>
 
                     <div class="b-family b-family__bg-none margin-b10">
                         <ul class="b-family_ul">
-                            <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, null, 1)">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__boy-wait"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Ждем мальчика</span>
-                                </div>
-                            </li>
-                            <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, null, 1)">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__girl-wait"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Ждем девочку</span>
-                                </div>
-                            </li>
-                            <li class="b-family_li" data-bind="draggable: new FamilyBaby(2, null, 1)">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__baby"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Ждем ребенка</span>
-                                </div>
-                            </li>
-                            <li class="b-family_li" data-bind="draggable: new FamilyBaby(2, null, 3)">
-                                <div class="b-family_img-hold">
-                                    <div class="ico-family ico-family__baby-two"></div>
-                                </div>
-                                <div class="b-family_tx">
-                                    <span>Ждем двойню</span>
-                                </div>
-                            </li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[0], template: { name : 'element-template', data : childrenModels[0] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[1], template: { name : 'element-template', data : childrenModels[1] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[2], template: { name : 'element-template', data : childrenModels[2] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[3], template: { name : 'element-template', data : childrenModels[3] }"></li>
                         </ul>
                     </div>
 
@@ -122,22 +73,8 @@
                             <div class="color-gray font-small">Дети от 0 до 1 года</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 0)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-small"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 0)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-small"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[4], template: { name : 'element-template', data : childrenModels[4] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[5], template: { name : 'element-template', data : childrenModels[5] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -146,22 +83,8 @@
                             <div class="color-gray font-small">Дети от 1 до 3 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 1)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-3"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 1)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-3"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[6], template: { name : 'element-template', data : childrenModels[6] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[7], template: { name : 'element-template', data : childrenModels[7] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -170,22 +93,8 @@
                             <div class="color-gray font-small">Дети от 3 до 6 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 2)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-5"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 2)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-5"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[8], template: { name : 'element-template', data : childrenModels[8] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[9], template: { name : 'element-template', data : childrenModels[9] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -194,22 +103,8 @@
                             <div class="color-gray font-small">Дети от 6 до 12 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 3)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-8"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 3)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-8"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[10], template: { name : 'element-template', data : childrenModels[10] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[11], template: { name : 'element-template', data : childrenModels[11] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -218,22 +113,8 @@
                             <div class="color-gray font-small">Дети от 12 до 18 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 4)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-14"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 4)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-14"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[12], template: { name : 'element-template', data : childrenModels[12] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[13], template: { name : 'element-template', data : childrenModels[13] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -242,22 +123,8 @@
                             <div class="color-gray font-small">Старше 18 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(0, 5)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__girl-19"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Дочь</span>
-                                        </div>
-                                    </li>
-                                    <li class="b-family_li" data-bind="draggable: new FamilyBaby(1, 5)">
-                                        <div class="b-family_img-hold">
-                                            <div class="ico-family ico-family__boy-19"></div>
-                                        </div>
-                                        <div class="b-family_tx">
-                                            <span>Сын</span>
-                                        </div>
-                                    </li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[14], template: { name : 'element-template', data : childrenModels[14] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[15], template: { name : 'element-template', data : childrenModels[15] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -277,6 +144,15 @@
         </div>
     </div>
 </div>
+
+<script type="text/html" id="element-template">
+    <div class="b-family_img-hold">
+        <div class="ico-family" data-bind="css: cssClass()"></div>
+    </div>
+    <div class="b-family_tx">
+        <span data-bind="text: title()"></span>
+    </div>
+</script>
 
 <script type="text/javascript">
     familyVM = new FamilyViewModel(<?=CJSON::encode($json)?>);

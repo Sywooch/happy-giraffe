2a43776f2d4955c22f9aa443865f1be97773e9be
choojoy-work <nikita@happy-giraffe.ru>
2013-08-22 13:48:19 +0400

    blog fixes

--- site/frontend/modules/blog/views/default/types/type_2.php
+++ site/frontend/modules/blog/views/default/types/type_2.php
@@ -28,7 +28,7 @@ $post = $data->post;
     <div class="b-article_in clearfix">
         <div class="wysiwyg-content clearfix">
             <div class="b-article_in-img">
-                <?=CHtml::link(CHtml::image($data->video->photo->getPreviewUrl(580, null, Image::WIDTH), '', array('class' => 'video-preview')), $data->url)?>
+                <?=CHtml::link(CHtml::image($data->video->photo->getPreviewUrl(580, null, Image::WIDTH), ''), $data->url, array('class' => 'video-preview'))?>
             </div>
             <p><?=$data->getContentText(500, '') ?></p>
         </div>

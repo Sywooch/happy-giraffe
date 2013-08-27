609cc47a8b0571399bbf322f1f076e1778fef1b8
Kireev Alex <alexk984@gmail.com>
2013-08-27 17:23:26 +0400

    community fix

--- site/frontend/views/layouts/community.php
+++ site/frontend/views/layouts/community.php
@@ -43,7 +43,54 @@ $this->beginContent('//layouts/main'); ?>
             <?php endif ?>
         </div>
     </div>
-    <?=$content ?>
+
+    <div class="b-section">
+        <div class="b-section_hold">
+            <div class="content-cols clearfix">
+
+                <div class="col-1">
+                    <div class="club-list club-list__large clearfix">
+                        <ul class="club-list_ul textalign-c clearfix">
+                            <li class="club-list_li">
+                                <a href="<?=$this->createUrl('/community/default/community', array('community_id'=>$this->community->id)) ?>" class="club-list_i">
+                                    <span class="club-list_img-hold">
+                                        <img src="/images/club/<?=$this->community->id ?>-w130.jpg" class="club-list_img">
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-23-middle">
+                    <div class="padding-l20">
+                        <h1 class="b-section_t"><a href=""><?=$this->community->title ?></a></h1>
+
+                        <div class="clearfix">
+                            <ul class="b-section_ul margin-l30 clearfix<?php if (isset($root)) echo ' b-section_ul__white'; ?>">
+                                <li class="b-section_li">
+                                    <a href="<?=$this->createUrl('/community/default/forum', array('community_id'=>$this->community->id)) ?>"
+                                       class="b-section_li-a<?php if (isset($forum)) echo ' active' ?>">Форум</a>
+                                </li>
+                                <?php foreach($this->community->services as $service):?>
+                                    <li class="b-section_li">
+                                        <a href="<?=$service->getUrl() ?>" class="b-section_li-a<?php if (isset($service_id) && $service_id = $service->id) echo ' active' ?>"><?=$service->title ?></a>
+                                    </li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-cols clearfix">
+        <?=$content ?>
+    </div>
+
+
     <script type="text/javascript">
         $(function() {
             vm = new CommunitySubscription(<?=CJSON::encode(UserCommunitySubscription::subscribed(Yii::app()->user->id, $this->community->id))?>, <?=$this->community->id ?>);

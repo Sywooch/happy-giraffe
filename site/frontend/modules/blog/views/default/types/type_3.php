c777e8b6d82ecf9183a24f6628dfe604338fcdcb
Kireev Alex <alexk984@gmail.com>
2013-08-27 09:41:35 +0400

    fixes

--- site/frontend/modules/blog/views/default/types/type_3.php
+++ site/frontend/modules/blog/views/default/types/type_3.php
@@ -3,6 +3,7 @@
  * @var $this DefaultController
  * @var $data BlogContent
  * @var $full bool
+ * @var bool $showTitle
  */
 
 $post = $data->photoPost;
@@ -25,9 +26,12 @@ $collection = new PhotoPostPhotoCollection(array('contentId' => $data->id));
         ?>
     </div>
 <?php else: ?>
-    <h2 class="b-article_t">
-        <a href="<?=$data->getUrl() ?>" class="b-article_t-a"><?=$data->title ?></a>
-    </h2>
+    <?php if ($showTitle):?>
+        <h2 class="b-article_t">
+            <a href="<?=$data->getUrl() ?>" class="b-article_t-a"><?=$data->title ?></a>
+        </h2>
+    <?php endif ?>
+
 
     <div class="b-article_in clearfix">
         <div class="wysiwyg-content clearfix">

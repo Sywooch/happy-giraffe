8e1fe2d4a8c0ee5adc583b26abd4b1596dcd48ae
Kireev Alex <alexk984@gmail.com>
2013-08-29 09:55:39 +0400

    fixes

--- site/frontend/modules/routes/views/default/view.php
+++ site/frontend/modules/routes/views/default/view.php
@@ -70,151 +70,127 @@ Yii::app()->clientScript
     ->registerScriptFile($baseUrl . '/routes.js');
 
 ?>
+<div class="margin-l-20">
+    <div class="col-white padding-20 clearfix">
+
+        <div id="crumbs"><a href="/">Главная</a> &gt; <a href="/auto/">Авто</a> &gt; <span>Маршруты</span></div>
+
+        <div class="map-route-search">
+            <a href="javascript:;" class="map-route-search_new a-pseudo" onclick="$('form.map-route-search_form').toggle();">Новый маршрут</a>
+
+            <h1 class="map-route-search_h1"><?=$texts[0] ?></h1>
+
+            <form action="" class="map-route-search_form clearfix" style="display: none;">
+                <input id="city_from" type="text" class="map-route-search_itx itx-bluelight" placeholder="Откуда">
+                <a href="javascript:;" class="map-route-search_reverse" onclick="Routes.reversePlaces()"></a>
+                <input id="city_to" type="text" class="map-route-search_itx itx-bluelight" placeholder="Куда">
+                <button class="btn-green map-route-search_btn" onclick="Routes.go();return false;">Проложить <br> маршрут</button>
+            </form>
+            <p><?=$texts[1] ?></p>
+
+        </div>
+        <div class="margin-b30 map-canvas">
+            <div id="map_canvas" style="width:960px; height:389px;"></div>
+            <div id="waitLoader" class="map-canvas_overlay" style="display:none;">
+                Подождите. Мы формируем для вас маршрут.
+                <div id="infscr-loading"><img src="/images/ico/ajax-loader.gif" alt="Loading..."><div>Загрузка</div></div>
+            </div>
+            <div id="badRoute" class="map-canvas_overlay map-canvas_overlay__error" style="display:none;">
+                Извините. Этот маршрут проложить невозможно.
+            </div>
+        </div>
+        <div class="content-cols clearfix">
+            <div class="col-1">
+                <div class="map-route-calc">
+                    <div class="map-route-calc_item">
+                        <div class="clearfix">
+                            <div class="map-route-calc_img">
+                                <img src="/images/services/map-route/map-route-car.png" alt="">
+                            </div>
+                            <div class="map-route-calc_t clearfix"><?=$texts[3] ?></div>
+                        </div>
+                        <div class="map-route-calc_value"><span data-bind="text: distance"></span> <span
+                                class="map-route-calc_units">км</span></div>
+                        <div class="map-route-calc_desc"><?=$texts[4] ?></div>
+                    </div>
+
+                    <div class="map-route-calc_item">
+                        <div class="clearfix">
+                            <div class="map-route-calc_img">
+                                <img src="/images/services/map-route/map-route-time.png" alt="">
+                            </div>
+                            <div class="map-route-calc_t clearfix"><?=$texts[5] ?></div>
+                        </div>
+                        <div class="map-route-calc_row clearfix">
+                            <input type="text" class="map-route-calc_itx map-route-calc_itx__speed itx-bluelight"
+                                   data-bind="value: speed, valueUpdate: 'afterkeydown'">
+                            <label class="map-route-calc_label">Ср. скорость <br> км / ч</label>
+                        </div>
+                        <div class="map-route-calc_value">
+                            <span data-bind="text: DurationHours"></span> <span class="map-route-calc_units">ч</span>
+                            <span data-bind="text: DurationMinutes"></span> <span class="map-route-calc_units">м</span>
+                        </div>
+                        <div class="map-route-calc_desc"><?=$texts[6] ?></div>
+                    </div>
+
+                    <div class="map-route-calc_item">
+                        <div class="clearfix">
+                            <div class="map-route-calc_img">
+                                <img src="/images/services/map-route/map-route-fuel-cost.png" alt="">
+                            </div>
+                            <div class="map-route-calc_t clearfix">Расход и стоимость топлива</div>
+                        </div>
+                        <div class="map-route-calc_row clearfix">
+                            <input type="text" name="" class="map-route-calc_itx map-route-calc_itx__fuel itx-bluelight"
+                                   data-bind="value: fuelConsumption, valueUpdate: 'afterkeydown'">
+                            <label class="map-route-calc_label">л. / 100 км</label>
+                        </div>
+                        <div class="map-route-calc_row clearfix">
+                            <input type="text" name="" class="map-route-calc_itx map-route-calc_itx__cost itx-bluelight"
+                                   data-bind="value: fuelCost, valueUpdate: 'afterkeydown'">
+                            <label class="map-route-calc_label">
+                                <div class="chzn-v2">
+                                    <?= CHtml::dropDownList('currency', 1, CHtml::listData(FuelCost::model()->findAll(), 'currency_id', 'title'),
+                                        array('class' => 'chzn w-85', 'data-bind' => 'value: currentCurrency')); ?>
+                                </div>
+                            </label>
+                        </div>
+                        <div class="map-route-calc_value">
+                            <span data-bind="text: fuelNeeds"></span> <span class="map-route-calc_units">л</span>
+                        </div>
+                        <div class="map-route-calc_value">
+                            <span data-bind="text: summaryCost"></span> <span class="map-route-calc_units" data-bind="text: currencySign">руб.</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="watchers">
+                    <div class="watchers_t">Маршрут <br>просмотрели</div>
+                    <div class="watchers_eye"></div>
+                    <div class="watchers_count"><?=PageView::model()->viewsByPath($route->url)?></div>
+                </div>
+
+                <?php $this->renderPartial('send',array('route'=>$route, 'texts' => $texts)); ?>
+
+            </div>
+
+
+            <div class="col-23">
+                <?php $this->renderPartial('_transit_points', array('route' => $route, 'texts' => $texts, 'points' => $points)); ?>
+
+                <div class="map-route-other" style="display: none;">
+
+                    <?php //$this->renderPartial('links', array('route' => $route)); ?>
+
+                </div>
+
+                <?php $this->widget('application.widgets.newCommentWidget.NewCommentWidget', array('model' => $route, 'full' => true, 'notice' => $texts[8])); ?>
+
+            </div>
+        </div>
+    </div>
+</div>
 <style type="text/css">
     .pac-container:after {content: none !important;}
     #map_canvas img{max-width:none;}
 </style>
-<?php if (Yii::app()->user->checkAccess('routes')):?>
-    <a href="/routes/<?=Route::model()->find(new CDbCriteria(array('order'=>'rand()', 'condition'=>'status=4')))->id ?>/">случайно google</a><br>
-    <a href="/routes/<?=Route::model()->find(new CDbCriteria(array('order'=>'rand()', 'condition'=>'status=2')))->id ?>/">случайно rosneft</a><br>
-    <a href="/routes/reparseGoogle/<?=$route->id ?>/">перепарсить в гугл</a><br>
-    <a href="/routes/reparseRosneft/<?=$route->id ?>/">перепарсить в роснефть</a><br>
-<?php endif ?>
-<div class="map-route-search">
-    <a href="#" class="map-route-search_new a-pseudo" onclick="$('form.map-route-search_form').toggle();">Новый
-        маршрут</a>
-
-    <h1 class="map-route-search_h1"><?=$texts[0] ?></h1>
-
-    <form action="" class="map-route-search_form clearfix" style="display: none;">
-        <input id="city_from" type="text" class="map-route-search_itx itx-bluelight" placeholder="Откуда">
-        <a href="javascript:;" class="map-route-search_reverse" onclick="Routes.reversePlaces()"></a>
-        <input id="city_to" type="text" class="map-route-search_itx itx-bluelight" placeholder="Куда">
-        <button class="btn-green map-route-search_btn" onclick="Routes.go();return false;">Проложить <br> маршрут</button>
-    </form>
-    <p><?=$texts[1] ?></p>
-
-</div>
-<div class="margin-b30 map-canvas">
-    <div id="map_canvas" style="width:960px; height:389px;"></div>
-    <div id="waitLoader" class="map-canvas_overlay" style="display:none;">
-        Подождите. Мы формируем для вас маршрут.
-        <div id="infscr-loading"><img src="/images/ico/ajax-loader.gif" alt="Loading..."><div>Загрузка</div></div>
-    </div>
-    <div id="badRoute" class="map-canvas_overlay map-canvas_overlay__error" style="display:none;">
-        Извините. Этот маршрут проложить невозможно.
-    </div>
-</div>
-<div class="content-cols clearfix">
-    <div class="col-1">
-        <div class="map-route-calc">
-            <div class="map-route-calc_item">
-                <div class="clearfix">
-                    <div class="map-route-calc_img">
-                        <img src="/images/services/map-route/map-route-car.png" alt="">
-                    </div>
-                    <div class="map-route-calc_t clearfix"><?=$texts[3] ?></div>
-                </div>
-                <div class="map-route-calc_value"><span data-bind="text: distance"></span> <span
-                        class="map-route-calc_units">км</span></div>
-                <div class="map-route-calc_desc"><?=$texts[4] ?></div>
-            </div>
-
-            <div class="map-route-calc_item">
-                <div class="clearfix">
-                    <div class="map-route-calc_img">
-                        <img src="/images/services/map-route/map-route-time.png" alt="">
-                    </div>
-                    <div class="map-route-calc_t clearfix"><?=$texts[5] ?></div>
-                </div>
-                <div class="map-route-calc_row clearfix">
-                    <input type="text" class="map-route-calc_itx map-route-calc_itx__speed itx-bluelight"
-                           data-bind="value: speed, valueUpdate: 'afterkeydown'">
-                    <label class="map-route-calc_label">Ср. скорость <br> км / ч</label>
-                </div>
-                <div class="map-route-calc_value">
-                    <span data-bind="text: DurationHours"></span> <span class="map-route-calc_units">ч</span>
-                    <span data-bind="text: DurationMinutes"></span> <span class="map-route-calc_units">м</span>
-                </div>
-                <div class="map-route-calc_desc"><?=$texts[6] ?></div>
-            </div>
-
-            <div class="map-route-calc_item">
-                <div class="clearfix">
-                    <div class="map-route-calc_img">
-                        <img src="/images/services/map-route/map-route-fuel-cost.png" alt="">
-                    </div>
-                    <div class="map-route-calc_t clearfix">Расход и стоимость топлива</div>
-                </div>
-                <div class="map-route-calc_row clearfix">
-                    <input type="text" name="" class="map-route-calc_itx map-route-calc_itx__fuel itx-bluelight"
-                           data-bind="value: fuelConsumption, valueUpdate: 'afterkeydown'">
-                    <label class="map-route-calc_label">л. / 100 км</label>
-                </div>
-                <div class="map-route-calc_row clearfix">
-                    <input type="text" name="" class="map-route-calc_itx map-route-calc_itx__cost itx-bluelight"
-                           data-bind="value: fuelCost, valueUpdate: 'afterkeydown'">
-                    <label class="map-route-calc_label">
-                        <div class="chzn-v2">
-                            <?= CHtml::dropDownList('currency', 1, CHtml::listData(FuelCost::model()->findAll(), 'currency_id', 'title'),
-                                array('class' => 'chzn w-85', 'data-bind' => 'value: currentCurrency')); ?>
-                        </div>
-                    </label>
-                </div>
-                <div class="map-route-calc_value">
-                    <span data-bind="text: fuelNeeds"></span> <span class="map-route-calc_units">л</span>
-                </div>
-                <div class="map-route-calc_value">
-                    <span data-bind="text: summaryCost"></span> <span class="map-route-calc_units"
-                                                                      data-bind="text: currencySign">руб.</span>
-                </div>
-            </div>
-        </div>
-
-        <div class="watchers">
-            <div class="watchers_t">Маршрут <br>просмотрели</div>
-            <div class="watchers_eye"></div>
-            <div class="watchers_count"><?=PageView::model()->viewsByPath($route->url)?></div>
-        </div>
-
-        <?php $this->renderPartial('send',array('route'=>$route, 'texts' => $texts)); ?>
-
-    </div>
-
-
-    <div class="col-23">
-        <?php $this->renderPartial('_transit_points', array('route' => $route, 'texts' => $texts, 'points' => $points)); ?>
-
-        <div class="map-route-other" style="display: none;">
-
-            <?php //$this->renderPartial('links', array('route' => $route)); ?>
-
-        </div>
-
-        <?php $this->widget('application.widgets.commentWidget.CommentWidget', array('model' => $route, 'notice' => $texts[8])); ?>
-
-        <?php
-        $remove_tmpl = $this->beginWidget('site.frontend.widgets.removeWidget.RemoveWidget');
-        $remove_tmpl->registerTemplates();
-        $this->endWidget();
-        ?>
-    </div>
-</div>
-
-<script type="text/javascript">
-    function SendRoute() {
-        $.post('/routes/sendEmail/', $('#send-route-form').serialize(), function (response) {
-            if (response.status) {
-                $('#send-route-form').hide();
-                $('#send-success').show();
-
-                setTimeout(function() {
-                    $('#send-route-form').show();
-                    $('#send-success').hide();
-                    $('#SendRoute_friend_email').val('');
-                    $('#SendRoute_verifyCode').val('');
-                }, 2000)
-            }
-        }, 'json');
-    }
-</script>

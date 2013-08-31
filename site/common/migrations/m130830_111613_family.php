e7076324fe6b5d94c437442ce1475e5c9b49e0d1
Kireev Alex <alexk984@gmail.com>
2013-08-31 14:24:56 +0400

    migration fix

--- site/common/migrations/m130830_111613_family.php
+++ site/common/migrations/m130830_111613_family.php
@@ -8,7 +8,7 @@ class m130830_111613_family extends CDbMigration
         $this->execute("ALTER TABLE `user__users_partners` ADD INDEX `Photo` (`main_photo_id`);");
         $this->execute("ALTER TABLE `user__users_partners` ADD CONSTRAINT `Photo` FOREIGN KEY (`main_photo_id`) REFERENCES `album__photos` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;");
 
-        $this->execute("ALTER TABLE `user__users_babies` ADD `main_photo_id` INT(11)  UNSIGNED  NULL  DEFAULT NULL`;");
+        $this->execute("ALTER TABLE `user__users_babies` ADD `main_photo_id` INT(11)  UNSIGNED  NULL  DEFAULT NULL;");
         $this->execute("ALTER TABLE `user__users_babies` ADD INDEX `Photo` (`main_photo_id`);");
         $this->execute("ALTER TABLE `user__users_babies` ADD FOREIGN KEY (`main_photo_id`) REFERENCES `album__photos` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;");
 

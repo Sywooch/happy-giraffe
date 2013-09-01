bef9b94e5b720d9fe38f3b0525ad548c4f110df8
choojoy-work <nikita@happy-giraffe.ru>
2013-09-01 19:13:15 +0400

    Тексты

---
+++ site/frontend/modules/cook/views/recipe/index.php
@@ -0,0 +1,185 @@
+<div class="heading-title margin-b10 margin-t15 clearfix">
+    Блюда из рыбы
+</div>
+<p class="margin-l20 margin-r40 color-gray-dark">Одним из основных свидетельств правильного течения  беременности является набор веса согласно принятым нормам. </p>
+<div class="b-article clearfix">
+    <div class="float-l">
+        <div class="like-control like-control__small-indent clearfix">
+            <a href="" class="ava male">
+                <span class="icon-status status-online"></span>
+                <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+            </a>
+        </div>
+        <div class="like-control clearfix">
+            <a href="" class="like-control_ico like-control_ico__like">865</a>
+            <a href="" class="like-control_ico like-control_ico__repost active">5</a>
+            <a href="" class="like-control_ico like-control_ico__cook ">123865</a>
+        </div>
+    </div>
+    <div class="b-article_cont clearfix">
+        <div class="b-article_cont-tale"></div>
+        <div class="b-article_header clearfix">
+            <div class="meta-gray">
+                <a href="" class="meta-gray_comment">
+                    <span class="ico-comment ico-comment__gray"></span>
+                    <span class="meta-gray_tx">35</span>
+                </a>
+                <div class="meta-gray_view">
+                    <span class="ico-view ico-view__gray"></span>
+                    <span class="meta-gray_tx">305</span>
+                </div>
+            </div>
+            <div class="float-l">
+                <a href="" class="b-article_author">Ангелина Богоявленская</a>
+                <span class="font-smallest color-gray">Сегодня 13:25</span>
+            </div>
+        </div>
+        <h2 class="b-article_t">
+            <a href="" class="b-article_t-a">Самое лучшее утро - просыпаюсь, а ты рядом</a>
+        </h2>
+        <div class="b-article_in clearfix">
+            <div class="wysiwyg-content clearfix">
+                <p>	Недавно посмотрел фильм "Убить Дракона" снятый в 1988 году с Абдуловым в главной роли. По мотивам пьесы Евгения Шварца «Дракон».</p>
+                <div class="b-article_in-img">
+                    <img title="Убить Дракона. Фантасмагория или сказка для взрослых. фото 1" src="http://img.happy-giraffe.ru/thumbs/700x700/16534/3733dd340b221ac3052b5cef11288870.jpg" class="content-img" alt="Убить Дракона. Фантасмагория или сказка для взрослых. фото 1" width="800">
+                </div>
+            </div>
+        </div>
+        <div class="textalign-r">
+            <a href="" class="b-article_more">Смотреть далее</a>
+        </div>
+
+        <div class="comments-gray">
+            <div class="comments-gray_t">
+                <a href="" class="comments-gray_t-a">
+                    <span class="comments-gray_t-a-tx">Все комментарии (28)</span>
+                </a>
+            </div>
+            <div class="comments-gray_hold">
+                <div class="comments-gray_i comments-gray_i__self">
+                    <div class="comments-gray_ava">
+                        <a href="" class="ava small male"></a>
+                    </div>
+                    <div class="comments-gray_frame">
+                        <div class="comments-gray_header clearfix">
+                            <a href="" class="comments-gray_author">Ангелина Богоявленская </a>
+                            <span class="font-smallest color-gray">Сегодня 13:25</span>
+                        </div>
+                        <div class="comments-gray_cont wysiwyg-content">
+                            <p>	<a href="">Вася Пупкин,</a> Мне безумно жалко всех женщин, но особенно Тину Кароль, я просто представить себе не могу <a href="">как она все это переживет</a> как она все это переживет(</p>
+                            <p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту и подцепляет естественно студийные версии песен вместо нужных.  я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически </p>
+                        </div>
+                    </div>
+                    <div class="comments-gray_control comments-gray_control__self">
+                        <div class="comments-gray_control-hold">
+                            <div class="clearfix">
+                                <a href="" class="message-ico message-ico__edit powertip" title="Редактировать"></a>
+                            </div>
+                            <div class="clearfix">
+                                <a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="comments-gray_i">
+                    <a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">7918</a>
+                    <div class="comments-gray_ava">
+                        <a href="" class="ava small female"></a>
+                    </div>
+                    <div class="comments-gray_frame">
+                        <div class="comments-gray_header clearfix">
+                            <a href="" class="comments-gray_author">Анг Богоявлен </a>
+                            <span class="font-smallest color-gray">Сегодня 14:25</span>
+                        </div>
+                        <div class="comments-gray_cont wysiwyg-content">
+                            <p>я не нашел, где можно поменять название трека. </p>
+                        </div>
+                    </div>
+
+                    <div class="comments-gray_control">
+                        <div class="comments-gray_control-hold">
+                            <div class="clearfix">
+                                <a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
+                            </div>
+                            <div class="clearfix">
+                                <a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
+                            </div>
+                        </div>
+                        <div class="clearfix">
+                            <a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="comments-gray_i">
+                    <a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
+                    <div class="comments-gray_ava">
+                        <a href="" class="ava small female"></a>
+                    </div>
+                    <div class="comments-gray_frame">
+                        <div class="comments-gray_header clearfix">
+                            <a href="" class="comments-gray_author">Анг Богоявлен </a>
+                            <span class="font-smallest color-gray">Сегодня 14:25</span>
+                        </div>
+                        <div class="comments-gray_cont wysiwyg-content">
+                            <p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту </p>
+                            <p>
+                                <a href="" class="comments-gray_cont-img-w">
+                                    <!--    max-width: 170px;  max-height: 110px; -->
+                                    <img src="/images/example/w170-h110.jpg" alt="">
+                                </a>
+                                <a href="" class="comments-gray_cont-img-w">
+                                    <img src="/images/example/w220-h309-1.jpg" alt="">
+                                </a>
+                                <a href="" class="comments-gray_cont-img-w">
+                                    <img src="/images/example/w200-h133-1.jpg" alt="">
+                                </a>
+                            </p>
+                            <p>и подцепляет естественно студийные версии песен вместо нужных.  я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически </p>
+                        </div>
+                    </div>
+
+                    <div class="comments-gray_control">
+                        <div class="comments-gray_control-hold">
+                            <div class="clearfix">
+                                <a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
+                            </div>
+                        </div>
+                        <div class="clearfix">
+                            <a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="comments-gray_add clearfix">
+
+                <div class="comments-gray_ava">
+                    <a href="" class="ava small female"></a>
+                </div>
+
+                <div class="comments-gray_frame">
+                    <!-- input hidden -->
+                    <input type="text" name="" id="" class="comments-gray_add-itx itx-gray" placeholder="Ваш комментарий">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="yiipagination">
+    <div class="pager">
+        <ul class="yiiPager" id="yw1">
+            <li class="page"><a href="">1</a></li>
+            <li class="page"><a href="">2</a></li>
+            <li class="page selected"><a href="">3</a></li>
+            <li class="page"><a href="">4</a></li>
+            <li class="page"><a href="">5</a></li>
+            <li class="page"><a href="">6</a></li>
+            <li class="page"><a href="">7</a></li>
+            <li class="page"><a href="">8</a></li>
+            <li class="page"><a href="">9</a></li>
+            <li class="page"><a href="">10</a></li>
+        </ul>
+    </div>
+</div>

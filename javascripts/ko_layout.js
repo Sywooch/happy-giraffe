42f56eae93e451564529284232de893750633d81
choojoy-work <nikita@happy-giraffe.ru>
2013-09-02 16:12:51 +0400

    Тексты

--- javascripts/ko_layout.js
+++ javascripts/ko_layout.js
@@ -3,4 +3,14 @@ var LayoutViewModel = function(data) {
     self.newNotificationsCount = ko.observable(data.newNotificationsCount);
     self.newMessagesCount = ko.observable(data.newMessagesCount);
     self.newFriendsCount = ko.observable(data.newFriendsCount);
+
+    self.inc = function(observable) {
+        observable(observable() + 1);
+    }
+
+    Comet.prototype.incNewFriendsCount = function(result, id) {
+        self.inc(self.newFriendsCount());
+    };
+
+    comet.addEvent(1001, 'incNewFriendsCount');
 }

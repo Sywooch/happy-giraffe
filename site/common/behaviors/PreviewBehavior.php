8b3d2996945d169f7249abe0a005e528be149d60
Kireev Alex <alexk984@gmail.com>
2013-09-03 20:15:04 +0400

    processing images

--- site/common/behaviors/PreviewBehavior.php
+++ site/common/behaviors/PreviewBehavior.php
@@ -22,6 +22,7 @@ class PreviewBehavior extends CActiveRecordBehavior
     private function generatePreview($text)
     {
         include_once Yii::getPathOfAlias('site.frontend.vendor.simplehtmldom_1_5') . DIRECTORY_SEPARATOR . 'simple_html_dom.php';
+        $text = trim(preg_replace('/\t+/', ' ', $text));
         $doc = str_get_html($text);
 
         if (Str::htmlTextLength($text) == 0)
@@ -30,20 +31,25 @@ class PreviewBehavior extends CActiveRecordBehavior
         if ($this->small_preview || isset($this->owner->photo_id) && !empty($this->owner->photo_id) && $this->owner->photo->width >= 580) {
             //если есть фото или известно что нужно показаться короткое превью, берем первый параграф
             $p_list = $doc->find('p');
+            echo '1';
             if (count($p_list) == 0)
-                return '<p>' . Str::getDescription($text, self::LIMIT_SMALL * 2) . '</p>';
+                return '<p>' . Str::truncate($text, self::LIMIT_SMALL * 2) . '</p>';
 
+            echo '2';
             foreach ($p_list as $p) {
+                echo $p->plaintext."\n";
                 $p_text = $this->getParagraphText($p);
+                echo $p_text."\n";
+                //Yii::app()->end();
                 if (!empty($p_text))
-                    return '<p>' . Str::getDescription($p_text, self::LIMIT_SMALL * 2) . '</p>';
+                    return '<p>' . Str::truncate($p_text, self::LIMIT_SMALL * 2) . '</p>';
             }
             return '';
         } else {
             $preview = '';
             $p_list = $doc->find('p');
             if (count($p_list) == 0)
-                return '<p>' . Str::getDescription($text, self::LIMIT_BIG * 2) . '</p>';
+                return '<p>' . Str::truncate($text, self::LIMIT_BIG * 2) . '</p>';
 
             foreach ($p_list as $p) {
                 $p_text = $this->getParagraphText($p);
@@ -53,7 +59,7 @@ class PreviewBehavior extends CActiveRecordBehavior
                 if (self::LIMIT_BIG - Str::htmlTextLength($preview) < 100)
                     return $preview;
 
-                $preview .= '<p>' . Str::getDescription($p_text, (self::LIMIT_BIG - Str::htmlTextLength($preview)) * 2) . '</p>';
+                $preview .= '<p>' . Str::truncate($p_text, (self::LIMIT_BIG - Str::htmlTextLength($preview)) * 2) . '</p>';
 
                 if (self::LIMIT_BIG - Str::htmlTextLength($preview) < 10)
                     return $preview;
@@ -66,6 +72,11 @@ class PreviewBehavior extends CActiveRecordBehavior
     private function getParagraphText($p)
     {
         #TODO смайлы тоже удаляет из превью
-        return trim(str_replace('&nbsp;', '', strip_tags($p->plaintext, '<a><br><br/><strong><b><i><u><strike><h2><h3><ul><ol><li>')));
+        $text = trim(str_replace('&nbsp;', ' ', $p->plaintext));
+        if (empty($text))
+            return '';
+
+        $text = strip_tags($text, '<a><br><br/><strong><b><i><u><strike><h2><h3><ul><ol><li>');
+        return trim($text);
     }
 }

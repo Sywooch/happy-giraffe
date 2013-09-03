04f8ef96566f06561ef375ff02446786a7012e3d
choojoy-work <nikita@happy-giraffe.ru>
2013-09-03 13:01:05 +0400

    Тексты

--- site/frontend/modules/community/views/default/_links.php
+++ site/frontend/modules/community/views/default/_links.php
@@ -24,3 +24,12 @@
         <a href="<?=$this->createUrl('/community/default/services', array('club_id'=>$this->club->id)) ?>" class="b-section_li-a<?php if (Yii::app()->controller->action->id == 'services' ) echo ' active' ?>">Сервисы</a>
     </li>
 <?php endif ?>
+
+<?php if ($this->club->id == 7): ?>
+    <li class="b-section_li">
+        <a href="<?=$this->createUrl('/cook/recipe/index', array('section' => 0))?>" class="b-section_li-a<?php if (Yii::app()->controller->id == 'recipe' && $this->section == 0) echo ' active' ?>">Рецепты</a>
+    </li>
+    <li class="b-section_li">
+        <a href="<?=$this->createUrl('/cook/recipe/index', array('section' => 1))?>" class="b-section_li-a<?php if (Yii::app()->controller->id == 'recipe' && $this->section == 1) echo ' active' ?>">Для мультиварки</a>
+    </li>
+<?php endif; ?>

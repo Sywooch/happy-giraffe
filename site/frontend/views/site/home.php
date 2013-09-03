248231fa29aa5cc99d469b0b1d8570aea5ffa2d3
choojoy-work <nikita@happy-giraffe.ru>
2013-09-03 19:46:00 +0400

    Merge remote-tracking branch 'origin/blog' into blog
    Conflicts:
    	site/frontend/www-submodule

--- site/frontend/views/site/home.php
+++ site/frontend/views/site/home.php
@@ -1,6 +1,8 @@
 <?php
-    Yii::app()->clientScript->registerMetaTag('NWGWm2TqrA1HkWzR8YBwRT08wX-3SRzeQIBLi1PMK9M', 'google-site-verification');
-    Yii::app()->clientScript->registerMetaTag('41ad6fe875ade857', 'yandex-verification');
+Yii::app()->clientScript->registerMetaTag('NWGWm2TqrA1HkWzR8YBwRT08wX-3SRzeQIBLi1PMK9M', 'google-site-verification');
+Yii::app()->clientScript->registerMetaTag('41ad6fe875ade857', 'yandex-verification');
+$this->widget('application.widgets.registerWidget.RegisterWidget');
+$this->widget('application.widgets.loginWidget.LoginWidget');
 ?>
 
 <div class="start-page">
@@ -9,36 +11,15 @@
 		<div class="start-page_hold">
 			<div class="start-page_head clearfix">
 				<h1 class="logo logo__big">
-					<a href="/" class="logo_i" title="Веселый жираф - сайт для все семьи">Веселый жираф - сайт для все семьи</a>
+					<span class="logo_i" title="Веселый жираф - сайт для все семьи">Веселый жираф - сайт для все семьи</span>
 					<strong class="logo_slogan">САЙТ ДЛЯ ВСЕЙ СЕМЬИ</strong>
 				</h1>
 				<div class="start-page_head-desc">
-                    <a class="btn-green btn-big" href="">Присоединяйтесь!</a>
+                    <a class="btn-green btn-big fancy" href="#register" data-theme="transparent">Присоединяйтесь!</a>
                     <div class="clearfix">
-                        <a class="display-ib verticalalign-m fancy" href="#login">Войти</a>
+                        <a class="display-ib verticalalign-m fancy" href="#login" data-theme="transparent">Войти</a>
                         <span class="i-or">или</span>
-                        <ul class="display-ib verticalalign-m">
-							<li class="display-ib">
-								<a class="custom-like" href="">
-									<span class="custom-like_icon odnoklassniki"></span>
-								</a>
-							</li>
-							<li class="display-ib">
-								<a class="custom-like" href="">
-									<span class="custom-like_icon vkontakte"></span>
-								</a>
-							</li>
-							<li class="display-ib">
-								<a class="custom-like" href="">
-									<span class="custom-like_icon facebook"></span>
-								</a>
-							</li>
-							<li class="display-ib">
-								<a class="custom-like" href="">
-									<span class="custom-like_icon twitter"></span>
-								</a>
-							</li>
-						</ul>
+                        <?php Yii::app()->eauth->renderWidget(array('action' => 'site/login', 'mode' => 'home', 'predefinedServices' => array('odnoklassniki', 'vkontakte', 'facebook', 'twitter'))); ?>
                     </div>
                 </div>
 
@@ -52,16 +33,6 @@
 				<div class="start-page_counter-desc">
 					Нас посетило уже
 				</div>
-
-		<script language="javascript" type="text/javascript">
-			jQuery(document).ready(function() {
-				$('#counter-users').countUp({
-					numberSet : 21789298,
-					randomNumberMax : 45
-				});
-
-			});
-		</script>
 
 				<!-- Countdown dashboard start -->
 				<div id="counter-users" class="counter-users">
@@ -357,37 +328,37 @@
 				<h2 class="start-page_club-t">Выбирайте клубы по интересам</h2>
 				<ul class="start-page_club-ul clearfix">
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>1)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/1.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Беременность <br>и дети</span>
 						</a>
 					</li>
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>2)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/2.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Наш дом</span>
 						</a>
 					</li>
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>3)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/4.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Муж и жена</span>
 						</a>
 					</li>
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>4)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/3.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Красота <br> и здоровье</span>
 						</a>
 					</li>
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>5)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/5.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Интересы <br> и увлечения</span>
 						</a>
 					</li>
 					<li class="start-page_club-li">
-						<a href="" class="start-page_club-i">
+						<a href="<?=$this->createUrl('/community/default/section', array('section_id'=>6)) ?>" class="start-page_club-i">
 							<img src="/images/club/collection/6.png" alt="" class="start-page_club-img">
 							<span class="start-page_club-tx">Семейный <br> отдых</span>
 						</a>
@@ -400,31 +371,10 @@
 	<div class="start-page_row start-page_row__join">
 		<div class="start-page_hold">
 			<div class="start-page_join">
-				<a class="btn-green start-page_join-btn" href="">Присоединяйтесь!</a>
+				<a class="btn-green start-page_join-btn fancy" href="#register" data-theme="transparent">Присоединяйтесь!</a>
                 <div class="clearfix">
                     <span class="i-or">войти через</span>
-                    <ul class="display-ib verticalalign-m">
-						<li class="display-ib">
-							<a class="custom-like" href="">
-								<span class="custom-like_icon odnoklassniki"></span>
-							</a>
-						</li>
-						<li class="display-ib">
-							<a class="custom-like" href="">
-								<span class="custom-like_icon vkontakte"></span>
-							</a>
-						</li>
-						<li class="display-ib">
-							<a class="custom-like" href="">
-								<span class="custom-like_icon facebook"></span>
-							</a>
-						</li>
-						<li class="display-ib">
-							<a class="custom-like" href="">
-								<span class="custom-like_icon twitter"></span>
-							</a>
-						</li>
-					</ul>
+                    <?php Yii::app()->eauth->renderWidget(array('action' => 'site/login', 'mode' => 'home', 'predefinedServices' => array('odnoklassniki', 'vkontakte', 'facebook', 'twitter'))); ?>
                 </div>
 			</div>
 		</div>

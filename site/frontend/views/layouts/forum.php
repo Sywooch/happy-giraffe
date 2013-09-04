4e07b3423015a97430f504d21266e9b31bdf37c0
Kireev Alex <alexk984@gmail.com>
2013-09-05 00:32:01 +0400

    fix

--- site/frontend/views/layouts/forum.php
+++ site/frontend/views/layouts/forum.php
@@ -7,10 +7,12 @@
 
     <div class="col-23-middle ">
 
-        <div class="clearfix margin-r20 margin-b20 js-community-subscription" data-bind="visible: active">
-            <a href="<?= $this->createUrl('/blog/default/form', array('type' => 1, 'club_id' => $this->forum->id)) ?>"
-               class="btn-blue btn-h46 float-r fancy">Добавить в клуб</a>
-        </div>
+        <?php if (!Yii::app()->user->isGuest):?>
+            <div class="clearfix margin-r20 margin-b20 js-community-subscription" data-bind="visible: active">
+                <a href="<?= $this->createUrl('/blog/default/form', array('type' => 1, 'club_id' => $this->forum->id)) ?>"
+                   class="btn-blue btn-h46 float-r fancy">Добавить в клуб</a>
+            </div>
+        <?php endif ?>
 
         <div class="col-gray">
             <?=$content ?>

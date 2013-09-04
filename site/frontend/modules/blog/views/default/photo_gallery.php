cc934c36a8a704726d44f541a71923eb14cef7ba
choojoy-work <nikita@happy-giraffe.ru>
2013-09-04 16:16:03 +0400

    Merge remote-tracking branch 'origin/blog' into blog
    Conflicts:
    	site/frontend/www-submodule

--- site/frontend/modules/blog/views/default/photo_gallery.php
+++ site/frontend/modules/blog/views/default/photo_gallery.php
@@ -2,7 +2,7 @@
 /**
  * @var $data BlogContent
  */
-$collection = new ContentGalleryPhotoCollection(array('contentId' => $data->id));
+$collection = new PhotoPostPhotoCollection(array('contentId' => $data->id));
 
 ?><div class="b-photopost">
     <h2 class="b-photopost_t"><?=CHtml::encode($data->gallery->title) ?></h2>

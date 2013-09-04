5bcadefc16ef3760c1108a662d7c87d0df6ac4b2
choojoy-work <nikita@happy-giraffe.ru>
2013-09-04 16:40:33 +0400

    Merge remote-tracking branch 'origin/blog' into blog
    Conflicts:
    	site/frontend/www-submodule

--- site/frontend/views/_footer.php
+++ site/frontend/views/_footer.php
@@ -10,14 +10,16 @@
             <li class="footer-list_li"><a href="" class="footer-list_a">Контакты </a></li>
             <li class="footer-list_li"><a href="" class="footer-list_a">Партнер "Рамблера"</a></li>
         </ul>
-        <ul class="footer-ul-bold">
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>1)) ?>" class="footer-ul-bold_a">Беременность и дети</a></li>
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>2)) ?>" class="footer-ul-bold_a">Наш дом</a></li>
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>3)) ?>" class="footer-ul-bold_a">Красота и здоровье</a></li>
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>4)) ?>" class="footer-ul-bold_a">Муж и жена</a></li>
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>5)) ?>" class="footer-ul-bold_a">Интересы и увлечения</a></li>
-            <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>6)) ?>" class="footer-ul-bold_a">Семейный отдых</a></li>
-        </ul>
+        <?php if ($this->route != 'site/index'): ?>
+            <ul class="footer-ul-bold">
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>1)) ?>" class="footer-ul-bold_a">Беременность и дети</a></li>
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>2)) ?>" class="footer-ul-bold_a">Наш дом</a></li>
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>3)) ?>" class="footer-ul-bold_a">Красота и здоровье</a></li>
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>4)) ?>" class="footer-ul-bold_a">Муж и жена</a></li>
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>5)) ?>" class="footer-ul-bold_a">Интересы и увлечения</a></li>
+                <li class="footer-ul-bold_li"><a href="<?=$this->createUrl('/community/default/section', array('section_id'=>6)) ?>" class="footer-ul-bold_a">Семейный отдых</a></li>
+            </ul>
+        <?php endif; ?>
 
         <div class="layout-footer_tx"> &copy; 2012-2013 Веселый Жираф. Социальная сеть для всей семьи. Использование редакционных материалов happy-giraffe.ru возможно только <br> с письменного разрешения редакции и/или при наличии активной ссылки на источник. Все права на пользовательские картинки и тексты принадлежат их авторам.
             Сайт предназначен для лиц старше 16 лет.</div>

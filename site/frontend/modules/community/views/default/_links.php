951d847aec5cfdcfdb507e4f19257f74c45b9b2a
Kireev Alex <alexk984@gmail.com>
2013-09-04 08:12:31 +0400

    cook fix

--- site/frontend/modules/community/views/default/_links.php
+++ site/frontend/modules/community/views/default/_links.php
@@ -1,35 +1,30 @@
-<?php if (count($this->club->communities) == 1):?>
-    <li class="b-section_li">
-        <a href="<?=$this->createUrl('/community/default/forum', array('forum_id'=>$this->club->communities[0]->id)) ?>"
-           class="b-section_li-a<?php if (isset($this->forum) && $this->forum !== null) echo ' active' ?>">Форум</a>
-    </li>
-<?php else: ?>
-    <?php foreach ($this->club->communities as $community): ?>
+<?php if (!in_array($this->club->id, array(7))):?>
+    <?php if (count($this->club->communities) == 1):?>
         <li class="b-section_li">
-            <a href="<?=$this->createUrl('/community/default/forum', array('forum_id'=>$community->id)) ?>"
-               class="b-section_li-a<?php if (isset($this->forum) && $this->forum !== null && $this->forum->id == $community->id) echo ' active' ?>"><?=$community->title ?></a>
+            <a href="<?=$this->createUrl('/community/default/forum', array('forum_id'=>$this->club->communities[0]->id)) ?>"
+               class="b-section_li-a<?php if (isset($this->forum) && $this->forum !== null) echo ' active' ?>">Форум</a>
         </li>
-    <?php endforeach; ?>
-<?php endif ?>
+    <?php else: ?>
+        <?php foreach ($this->club->communities as $community): ?>
+            <li class="b-section_li">
+                <a href="<?=$this->createUrl('/community/default/forum', array('forum_id'=>$community->id)) ?>"
+                   class="b-section_li-a<?php if (isset($this->forum) && $this->forum !== null && $this->forum->id == $community->id) echo ' active' ?>"><?=$community->title ?></a>
+            </li>
+        <?php endforeach; ?>
+    <?php endif ?>
 
 
-<?php if (count($this->club->services) < 2):?>
-    <?php foreach($this->club->services as $service):?>
+    <?php if (count($this->club->services) < 2):?>
+        <?php foreach($this->club->services as $service):?>
+            <li class="b-section_li">
+                <a href="<?=$service->getUrl() ?>" class="b-section_li-a<?php if (isset($service_id) && $service_id = $service->id) echo ' active' ?>"><?=$service->title ?></a>
+            </li>
+        <?php endforeach ?>
+    <?php else: ?>
         <li class="b-section_li">
-            <a href="<?=$service->getUrl() ?>" class="b-section_li-a<?php if (isset($service_id) && $service_id = $service->id) echo ' active' ?>"><?=$service->title ?></a>
+            <a href="<?=$this->createUrl('/community/default/services', array('club_id'=>$this->club->id)) ?>" class="b-section_li-a<?php if (Yii::app()->controller->action->id == 'services' ) echo ' active' ?>">Сервисы</a>
         </li>
-    <?php endforeach ?>
+    <?php endif ?>
 <?php else: ?>
-    <li class="b-section_li">
-        <a href="<?=$this->createUrl('/community/default/services', array('club_id'=>$this->club->id)) ?>" class="b-section_li-a<?php if (Yii::app()->controller->action->id == 'services' ) echo ' active' ?>">Сервисы</a>
-    </li>
+    <?php $this->renderPartial('application.modules.community.views.default.club_specific._links_' . $this->club->id); ?>
 <?php endif ?>
-
-<?php if ($this->club->id == 7): ?>
-    <li class="b-section_li">
-        <a href="<?=$this->createUrl('/cook/recipe/index', array('section' => 0))?>" class="b-section_li-a<?php if (Yii::app()->controller->id == 'recipe' && $this->section == 0) echo ' active' ?>">Рецепты</a>
-    </li>
-    <li class="b-section_li">
-        <a href="<?=$this->createUrl('/cook/recipe/index', array('section' => 1))?>" class="b-section_li-a<?php if (Yii::app()->controller->id == 'recipe' && $this->section == 1) echo ' active' ?>">Для мультиварки</a>
-    </li>
-<?php endif; ?>

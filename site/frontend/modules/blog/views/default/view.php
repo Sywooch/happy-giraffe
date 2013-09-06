5d15edd156cd3a2fc44d60e94f76c3ab0f4e79f1
choojoy-work <nikita@happy-giraffe.ru>
2013-09-06 19:47:57 +0400

    bugfixes

--- site/frontend/modules/blog/views/default/view.php
+++ site/frontend/modules/blog/views/default/view.php
@@ -42,7 +42,35 @@ else
         <?php if ($full): ?>
             <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
             <div id="yandex_ad" style="padding: 20px 20px 30px; background: #fffff0; margin-top: -20px;">
-                123
+                <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
+                <div id="yandex_ad"></div>
+                <script type="text/javascript">
+                    (function(w, d, n, s, t) {
+                        w[n] = w[n] || [];
+                        w[n].push(function() {
+                            Ya.Direct.insertInto(87026, "yandex_ad", {
+                                site_charset: "utf-8",
+                                ad_format: "direct",
+                                font_size: 1.2,
+                                type: "vertical",
+                                limit: 2,
+                                title_font_size: 3,
+                                site_bg_color: "FFFFFF",
+                                title_color: "0066CC",
+                                url_color: "006600",
+                                text_color: "000000",
+                                hover_color: "0066FF",
+                                favicon: true
+                            });
+                        });
+                        t = d.getElementsByTagName('head')[0];
+                        s = d.createElement("script");
+                        s.type = "text/javascript";
+                        s.src = "http://an.yandex.ru/system/context.js";
+                        s.setAttribute("async", "true");
+                        t.insertBefore(s, t.firstChild);
+                    })(window, document, "yandex_context_callbacks");
+                </script>
             </div>
         <?php endif; ?>
 

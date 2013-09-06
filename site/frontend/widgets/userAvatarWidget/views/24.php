74931c6f56442bacfb45cc74609c761a9739cfda
Kireev Alex <alexk984@gmail.com>
2013-09-06 19:17:15 +0400

    fixes

--- site/frontend/widgets/userAvatarWidget/views/24.php
+++ site/frontend/widgets/userAvatarWidget/views/24.php
@@ -2,7 +2,7 @@
 /**
  * @var $this Avatar
  */
-?><a href="<?=$this->url ?>" class="ava <?=($this->user->gender == 0)?'female':'male' ?> small">
+?><a href="<?=$this->user->getUrl() ?>" class="ava <?=($this->user->gender == 0)?'female':'male' ?> small">
     <?php if ($this->user->online):?>
         <span class="icon-status status-online"></span>
     <?php endif ?>

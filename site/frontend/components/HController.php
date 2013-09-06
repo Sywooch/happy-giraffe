7a1f5daf5cc227144e8cf25acff2fd0f2bab6768
choojoy-work <nikita@happy-giraffe.ru>
2013-09-06 16:30:33 +0400

    bugfixes

--- site/frontend/components/HController.php
+++ site/frontend/components/HController.php
@@ -190,7 +190,7 @@ class HController extends CController
 
     protected function combineStatic()
     {
-        if (YII_DEBUG === false && false) {
+        if (YII_DEBUG === false) {
             $wwwPath = Yii::getPathOfAlias('application.www-submodule');
 
             foreach (Yii::app()->params['combineMap'] as $all => $filesArray) {

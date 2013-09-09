31e89547982f92ba0672f4544f523ed413dec9eb
Kireev Alex <alexk984@gmail.com>
2013-09-09 10:17:30 +0400

    seo fix

--- site/seo/modules/commentators/views/default/_user.php
+++ site/seo/modules/commentators/views/default/_user.php
@@ -5,7 +5,7 @@
  */
 
 ?><div class="user-info clearfix">
-    <a href="http://www.happy-giraffe.ru/user/<?=$user->id ?>/" class="ava small" target="_blank"><?=CHtml::image(isset($user->avatar)?implode('/', array(Yii::app()->params['photos_url'],'thumbs','24x24',$user->id,$user->avatar->fs_name)):'') ?></a>
+    <a href="http://www.happy-giraffe.ru/user/<?=$user->id ?>/" class="ava small" target="_blank"><?=CHtml::image(SeoUser::getAvatarUrlForUser($user, 24))?></a>
     <div class="user-info_details">
         <a href="http://www.happy-giraffe.ru/user/<?=$user->id ?>/" class="user-info_username" target="_blank"><?=$user->fullName ?></a>
     </div>

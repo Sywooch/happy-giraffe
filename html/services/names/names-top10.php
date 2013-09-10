b49e863b4a03e4d500c9b851e4492f17bdbd80fe
Tantalid <tantalid@mail.ru>
2013-09-10 15:21:38 +0300

    fix

---
+++ html/services/names/names-top10.php
@@ -0,0 +1,219 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class=" ie7"> <![endif]-->
+<!--[if IE 8]>         <html class=" ie8"> <![endif]-->
+<!--[if IE 9]>         <html class=" ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/layout-header.php'; ?>
+        
+        <div class="layout-content clearfix">
+        
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/club-top.php'; ?>
+
+        <div class="content-cols margin-l-20">
+            <div class="col-white padding-20 clearfix">
+                
+			<div id="baby">
+				<div class="content-box clearfix">
+
+                    <div class="right_block">
+					    <div class="choice_name">
+					        <div class="title-in">Выбор имени <span>ребенка</span></div>
+					    </div>
+					    <ul class="choice_name_navi">
+					        <li><a href="#"><ins>По алфавиту</ins></a><span></span></li>
+					        <li class="active"><a href="#"><ins>ТОП-10</ins></a><span></span></li>
+					        <li><a href="#"><ins>По святцам</ins></a><span></span></li>
+					        <li class="like"><a href="#"><ins>Нравятся (<ins>0</ins>)</ins></a><span></span></li>
+					    </ul>
+					</div>
+
+
+                    <div class="top_boys">
+                        <div class="name_block">
+                            <div class="heart_like">150</div>
+                            <a href="/names/Artem/" class="man_names_lk">Артем</a>
+                            <a rel="168" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>невредимый, безупречного здоровья</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">121</div>
+                            <a href="/names/Maksim/" class="man_names_lk">Максим</a>
+                            <a rel="111" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>величайший</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">120</div>
+                            <a href="/names/Dmitrii/" class="man_names_lk">Дмитрий</a>
+                            <a rel="16" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>посвященный богине Деметре</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">114</div>
+                            <a href="/names/Nikita/" class="man_names_lk">Никита</a>
+                            <a rel="11" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>победитель</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">112</div>
+                            <a href="/names/Matvei/" class="man_names_lk">Матвей</a>
+                            <a rel="231" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>Божий дар</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">107</div>
+                            <a href="/names/Daniil/" class="man_names_lk">Даниил</a>
+                            <a rel="7" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>божий суд</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">107</div>
+                            <a href="/names/Kirill/" class="man_names_lk">Кирилл</a>
+                            <a rel="127" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>господин</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">99</div>
+                            <a href="/names/Roman/" class="man_names_lk">Роман</a>
+                            <a rel="91" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>римлянин</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">97</div>
+                            <a href="/names/Aleksandr/" class="man_names_lk">Александр</a>
+                            <a rel="30" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>защитник, мужчина</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">88</div>
+                            <a href="/names/Ivan/" class="man_names_lk">Иван</a>
+                            <a rel="8" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>Божий дар</p>
+
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                    
+                    <div class="top_girls">
+                        <div class="name_block">
+                            <div class="heart_like">173</div>
+                            <a href="/names/Sofiya/" class="woman_names_lk">София</a>
+                            <a rel="175" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>мудрая</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">120</div>
+                            <a href="/names/Arina/" class="woman_names_lk">Арина</a>
+                            <a rel="461" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>мирная, спокойная, солнечная богиня, яростная, яркая, гора света, высокая, гора, учительница, просвещенная, жизнерадостная</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">111</div>
+                            <a href="/names/Milana/" class="woman_names_lk">Милана</a>
+                            <a rel="530" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>милая</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">102</div>
+                            <a href="/names/Anastasiya/" class="woman_names_lk">Анастасия</a>
+                            <a rel="15" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>возрождение, воскрешение, бессмертная</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">99</div>
+                            <a href="/names/Polina/" class="woman_names_lk">Полина</a>
+                            <a rel="90" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>сильная, разрушающая, светящаяся</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">98</div>
+                            <a href="/names/Darya/" class="woman_names_lk">Дарья</a>
+                            <a rel="191" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>победительница, добрая, море, Божий дар</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">97</div>
+                            <a href="/names/Viktoriya/" class="woman_names_lk">Виктория</a>
+                            <a rel="300" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>победительница</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">96</div>
+                            <a href="/names/Varvara/" class="woman_names_lk">Варвара</a>
+                            <a rel="49" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>иностранка, странница, балаболка, тарабарщина</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">87</div>
+                            <a href="/names/Kira/" class="woman_names_lk">Кира</a>
+                            <a rel="187" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>госпожа</p>
+
+                            <div class="clear"></div>
+                        </div>
+                            <div class="name_block">
+                            <div class="heart_like">82</div>
+                            <a href="/names/Mariya/" class="woman_names_lk">Мария</a>
+                            <a rel="37" href="#" onclick="return NameModule.like(this);" class="heart empty_heart"></a>
+                            <p>горькая, любимая, госпожа</p>
+
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+		</div>
+
+		</div>  	
+		<a href="#layout" id="btn-up-page"></a>
+		<div class="footer-push"></div>
+		
+	</div>
+</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</body>
+</html>

12eaf5206a5376c2e388be173a6d3eaa795c2266
choojoy-work <nikita@happy-giraffe.ru>
2013-09-11 11:12:22 +0400

    bugfixes

--- site/frontend/modules/family/views/default/_add_element.php
+++ site/frontend/modules/family/views/default/_add_element.php
@@ -7,3 +7,12 @@
     </div>
     <a class="ico-close b-family_del" data-bind="click: remove"></a>
 </script>
+
+<script type="text/html" id="fake-element-template">
+    <div class="b-family_img-hold">
+        <div class="ico-family" data-bind="css: cssClass()"></div>
+    </div>
+    <div class="b-family_tx">
+        <span data-bind="text: title()"></span>
+    </div>
+</script>

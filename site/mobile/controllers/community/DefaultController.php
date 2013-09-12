4e046dfcec42c80faf2d888c22bb61378caff468
choojoy-work <nikita@happy-giraffe.ru>
2013-09-12 17:37:08 +0400

    bugfixes

--- site/mobile/controllers/community/DefaultController.php
+++ site/mobile/controllers/community/DefaultController.php
@@ -6,7 +6,7 @@
  * Time: 5:06 PM
  * To change this template use File | Settings | File Templates.
  */
-class CommunityController extends MController
+class DefaultController extends MController
 {
     public function actionIndex()
     {
@@ -38,14 +38,14 @@ class CommunityController extends MController
         $this->render('list', compact('dp'));
     }
 
-    public function actionView($community_id, $content_type_slug, $content_id)
+    public function actionView($forum_id, $content_type_slug, $content_id)
     {
         $content = CommunityContent::model()->active()->full()->findByPk($content_id);
 
         $next = CommunityContent::model()->active()->community()->findAll(array(
             'order' => 't.id DESC',
             'condition' => 't.id < :current_id AND rubric.community_id = :community_id',
-            'params' => array(':current_id' => $content_id, ':community_id' => $community_id),
+            'params' => array(':current_id' => $content_id, ':community_id' => $forum_id),
             'limit' => 3,
         ));
 

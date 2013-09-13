9097a00574c5b4c13338e318d12ec4c25f02ad62
choojoy-work <nikita@happy-giraffe.ru>
2013-09-13 13:23:16 +0400

    bugfixes

--- site/frontend/modules/profile/views/settings/social.php
+++ site/frontend/modules/profile/views/settings/social.php
@@ -2,5 +2,5 @@
     Свяжите свой профиль с вашими аккаунтами в других социальных сетях. <br>Это позволит входить на сайт, используя любой из привязанных аккаунтов.
 </div>
 <!-- Пока нет связаных соц. сетей, таблицы тоже нет -->
-<?php Yii::app()->eauth->renderWidget(array('mode' => 'profile', 'action' => 'site/login', 'predefinedServices' => array('odnoklassniki', 'facebook', 'vkontakte', 'twitter'))); ?>
+<?php Yii::app()->eauth->renderWidget(array('mode' => 'profile', 'action' => 'site/login')); ?>
 

79a5dc27a81d5c997532f096bfde6b72305c0a82
choojoy-work <nikita@happy-giraffe.ru>
2013-09-13 17:02:55 +0400

    bugfixes

--- site/frontend/views/albums/_widget.php
+++ site/frontend/views/albums/_widget.php
@@ -11,7 +11,7 @@ $add = empty($model->title) ? '' : ' title="' . $model->title . '" alt="' . $mod
 if (!$edit) echo '<!-- widget: { entity : "AlbumPhoto", entity_id : "' . $model->id . '" } -->';
 
 if (isset($parentModel) && in_array(get_class($parentModel), array('Comment', 'MessagingMessage')))
-    echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' })"><img src="' . $model->getPreviewUrl(485, 110, Image::HEIGHT) . '"></a>';
+    echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img src="' . $model->getPreviewUrl(485, 110, Image::HEIGHT) . '"></a>';
 else {
     if ($model->width >= 580) {
         if (!$edit)

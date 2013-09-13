bab8b37984e6b719d26c20814fd2e167589f7b44
choojoy-work <nikita@happy-giraffe.ru>
2013-09-13 13:52:27 +0400

    bugfixes

--- site/frontend/modules/profile/views/settings/social.php
+++ site/frontend/modules/profile/views/settings/social.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @var SettingsController $this
+ */
+Yii::app()->clientScript->registerScriptFile('/javascripts/ko_settings.js'); ?>
 <div class="margin-b30 clearfix">
     Свяжите свой профиль с вашими аккаунтами в других социальных сетях. <br>Это позволит входить на сайт, используя любой из привязанных аккаунтов.
 </div>

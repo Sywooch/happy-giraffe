c7b0d10f2a7b463f16ca52f8ee77b3d7670f7a46
choojoy-work <nikita@happy-giraffe.ru>
2013-09-13 14:42:33 +0400

    bugfixes

--- site/frontend/modules/family/views/default/signup.php
+++ site/frontend/modules/family/views/default/signup.php
@@ -30,7 +30,7 @@ Yii::app()->clientScript
                                 </div>
                             </li>
                             <!-- ko foreach: family -->
-                                <li class="b-family_li" data-bind="droppable: $root.drop, css: { 'b-family_li__empty' : isEmpty }, template: { name : 'element-template', if: ! isEmpty() }"></li>
+                                <li class="b-family_li" data-bind="droppable: $root.drop, css: { 'b-family_li__empty' : isEmpty }, template: { name : 'fake-element-template', if: ! isEmpty() }"></li>
                             <!-- /ko -->
                         </ul>
                     </div>
@@ -46,18 +46,19 @@ Yii::app()->clientScript
                     <div class="b-family b-family__bg-none margin-b10">
                         <ul class="b-family_ul">
                         <!-- Объект для перетаскивания .b-family_li -->
-                            <li class="b-family_li" data-bind="draggable: partnerModels[0], template: { name : 'element-template', data : partnerModels[0] }"></li>
-                            <li class="b-family_li" data-bind="draggable: partnerModels[1], template: { name : 'element-template', data : partnerModels[1] }"></li>
-                            <li class="b-family_li" data-bind="draggable: partnerModels[2], template: { name : 'element-template', data : partnerModels[2] }"></li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[0], template: { name : 'fake-element-template', data : partnerModels[0] }"></li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[1], template: { name : 'fake-element-template', data : partnerModels[1] }"></li>
+                            <li class="b-family_li" data-bind="draggable: partnerModels[2], template: { name : 'fake-element-template', data : partnerModels[2] }"></li>
                         </ul>
                     </div>
 
                     <div class="b-family b-family__bg-none margin-b10">
                         <ul class="b-family_ul">
-                            <li class="b-family_li" data-bind="draggable: childrenModels[0], template: { name : 'element-template', data : childrenModels[0] }"></li>
-                            <li class="b-family_li" data-bind="draggable: childrenModels[1], template: { name : 'element-template', data : childrenModels[1] }"></li>
-                            <li class="b-family_li" data-bind="draggable: childrenModels[2], template: { name : 'element-template', data : childrenModels[2] }"></li>
-                            <li class="b-family_li" data-bind="draggable: childrenModels[3], template: { name : 'element-template', data : childrenModels[3] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[0], template: { name : 'fake-element-template', data : childrenModels[0] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[0], template: { name : 'fake-element-template', data : childrenModels[1] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[1], template: { name : 'fake-element-template', data : childrenModels[2] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[2], template: { name : 'fake-element-template', data : childrenModels[3] }"></li>
+                            <li class="b-family_li" data-bind="draggable: childrenModels[3], template: { name : 'fake-element-template', data : childrenModels[4] }"></li>
                         </ul>
                     </div>
 
@@ -66,8 +67,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Дети от 0 до 1 года</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[4], template: { name : 'element-template', data : childrenModels[4] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[5], template: { name : 'element-template', data : childrenModels[5] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[4], template: { name : 'fake-element-template', data : childrenModels[5] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[5], template: { name : 'fake-element-template', data : childrenModels[6] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -76,8 +77,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Дети от 1 до 3 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[6], template: { name : 'element-template', data : childrenModels[6] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[7], template: { name : 'element-template', data : childrenModels[7] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[6], template: { name : 'fake-element-template', data : childrenModels[7] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[7], template: { name : 'fake-element-template', data : childrenModels[8] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -86,8 +87,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Дети от 3 до 6 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[8], template: { name : 'element-template', data : childrenModels[8] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[9], template: { name : 'element-template', data : childrenModels[9] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[8], template: { name : 'fake-element-template', data : childrenModels[9] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[9], template: { name : 'fake-element-template', data : childrenModels[10] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -96,8 +97,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Дети от 6 до 12 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[10], template: { name : 'element-template', data : childrenModels[10] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[11], template: { name : 'element-template', data : childrenModels[11] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[10], template: { name : 'fake-element-template', data : childrenModels[11] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[11], template: { name : 'fake-element-template', data : childrenModels[12] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -106,8 +107,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Дети от 12 до 18 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[12], template: { name : 'element-template', data : childrenModels[12] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[13], template: { name : 'element-template', data : childrenModels[13] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[12], template: { name : 'fake-element-template', data : childrenModels[13] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[13], template: { name : 'fake-element-template', data : childrenModels[14] }"></li>
                                 </ul>
                             </div>
                         </div>
@@ -116,8 +117,8 @@ Yii::app()->clientScript
                             <div class="color-gray font-small">Старше 18 лет</div>
                             <div class="b-family b-family__bg-none margin-b10">
                                 <ul class="b-family_ul">
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[14], template: { name : 'element-template', data : childrenModels[14] }"></li>
-                                    <li class="b-family_li" data-bind="draggable: childrenModels[15], template: { name : 'element-template', data : childrenModels[15] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[14], template: { name : 'fake-element-template', data : childrenModels[15] }"></li>
+                                    <li class="b-family_li" data-bind="draggable: childrenModels[15], template: { name : 'fake-element-template', data : childrenModels[16] }"></li>
                                 </ul>
                             </div>
                         </div>

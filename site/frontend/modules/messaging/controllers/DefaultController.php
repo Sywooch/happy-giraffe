86557dd33d24b80ff431b8785da1d3036859cd9e
Kireev Alex <alexk984@gmail.com>
2013-09-17 08:52:33 +0400

    fixes

--- site/frontend/modules/messaging/controllers/DefaultController.php
+++ site/frontend/modules/messaging/controllers/DefaultController.php
@@ -12,6 +12,7 @@ class DefaultController extends HController
 
     //public $layout = '//layouts/common_new';
     public $tempLayout = true;
+    public $showAddBlock = false;
 
     public function filters()
     {

6716ff5787ae1c1606a5a03783e77b61be854e07
choojoy-work <nikita@happy-giraffe.ru>
2013-09-17 16:39:47 +0400

    bugfixes

--- site/frontend/modules/gallery/components/PhotoCollection.php
+++ site/frontend/modules/gallery/components/PhotoCollection.php
@@ -87,7 +87,6 @@ abstract class PhotoCollection extends CComponent
 
     protected function getPhotoIds() {
         $value = Yii::app()->cache->get($this->getIdsCacheKey());
-        $value = false;
         if ($value === false) {
             $value = $this->generateIds();
             Yii::app()->cache->set($this->getIdsCacheKey(), $value, 0, $this->getIdsCacheDependency());

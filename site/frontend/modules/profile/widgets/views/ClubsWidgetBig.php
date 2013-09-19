0333c0e2779b66000486cbc421208d75dc1e809e
Kireev Alex <alexk984@gmail.com>
2013-09-19 11:16:53 +0400

    fixes

--- site/frontend/modules/profile/widgets/views/ClubsWidgetBig.php
+++ site/frontend/modules/profile/widgets/views/ClubsWidgetBig.php
@@ -3,7 +3,7 @@
  * @var $this ClubsWidget
  */
 ?>
-<div class="club-list club-list__big clearfix" id="user-clubs-<?=$this->uniqid?>">
+<div class="club-list club-list__big clearfix" id="user-clubs-<?=$this->uniqid?>" style="display: none" data-bind="visible: true">
     <ul class="club-list_ul clearfix">
         <!-- ko foreach: TopClubs -->
         <li class="club-list_li" data-bind="css: {'club-list_li__in': have()}">

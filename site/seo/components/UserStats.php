bfff5db73f9c0d8cb7c9bf9fa6084a1ac441e9f1
Kireev Alex <alexk984@gmail.com>
2013-09-20 11:28:12 +0400

    commenting

--- site/seo/components/UserStats.php
+++ site/seo/components/UserStats.php
@@ -62,12 +62,6 @@ class UserStats
         $criteria->compare('entity', 'CommunityContent');
         $criteria->compare('removed', 0);
 
-/*        if ($this->group == UserGroup::USER) {
-            $test_data = Comment::model()->findAll($criteria);
-            foreach ($test_data as $comment) {
-                echo '<a href="http://www.happy-giraffe.ru'.$comment->getUrl().'" target="_blank">link</a><br>';
-            }
-        }*/
         return Comment::model()->count($criteria);
     }
 

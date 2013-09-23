6858803821f5be96c82502c56db39ccede26d055
choojoy-work <nikita@happy-giraffe.ru>
2013-09-23 18:49:41 +0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	site/frontend/www-submodule

--- site/console/commands/CacheCommand.php
+++ site/console/commands/CacheCommand.php
@@ -13,12 +13,16 @@ class CacheCommand extends CConsoleCommand
     {
         $dp = new CActiveDataProvider('CommunityContent');
         $iterator = new CDataProviderIterator($dp);
-        foreach ($iterator as $c)
-            $c->purified->clearCache();
+        foreach ($iterator as $c) {
+            $c->text = str_replace('/img(?:\d+).happy-giraffe.ru/', 'img.happy-giraffe.ru', $c->text);
+            $c->save(false, array('text'));
+        }
 
         $dp = new CActiveDataProvider('Comment');
         $iterator = new CDataProviderIterator($dp);
-        foreach ($iterator as $c)
-            $c->purified->clearCache();
+        foreach ($iterator as $c) {
+            $c->text = str_replace('/img(?:\d+).happy-giraffe.ru/', 'img.happy-giraffe.ru', $c->text);
+            $c->save(false, array('text'));
+        }
     }
 }

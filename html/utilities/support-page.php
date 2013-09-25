e049648f342304722887958d77459d6f46945de9
Tantalid <tantalid@mail.ru>
2013-09-25 17:35:02 +0300

    fix

--- html/utilities/support-page.php
+++ html/utilities/support-page.php
@@ -9,33 +9,140 @@
 
 </head>
 <body class="body-gray">
-
-	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
 	
 <div class="layout-container">
 	<div class="layout-wrapper">
-		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		<div class="layout-header layout-header__nologin clearfix">
+			<div class="content-cols clearfix">
+				<div class="col-1">
+					<h1 class="logo">
+						<a href="/" class="logo_i" title="Веселый жираф - сайт для все семьи">Веселый жираф - сайт для все семьи</a>
+						<strong class="logo_slogan">САЙТ ДЛЯ ВСЕЙ СЕМЬИ</strong>
+					</h1>
+					<div class="sidebar-search clearfix">
+						<input type="text" placeholder="Поиск по сайту" class="sidebar-search_itx" id="" name="">
+						<!-- 
+						В начале ввода текста, скрыть sidebar-search_btn добавить класс active"
+						 -->
+						<button class="sidebar-search_btn"></button>
+					</div>
+				</div>
+				<div class="col-23">
+					<div class="b-join clearfix">
+						<div class="b-join_left">
+							<div class="b-join_tx"> Более <span class="b-join_tx-big"> 20 000 000</span> мам и пап</div>
+							<div class="b-join_slogan">уже посетили Веселый Жираф!</div>
+						</div>
+						<div class="b-join_right">
+							<a href="" class="btn-green btn-big">Присоединяйтесь!</a>
+							<div class="clearfix">
+								<a href="" class="display-ib verticalalign-m">Войти</a>
+								<span class="i-or">или</span>
+								<ul class="display-ib verticalalign-m">
+									<li class="display-ib">
+										<a class="custom-like" href="">
+											<span class="custom-like_icon odnoklassniki"></span>
+										</a>
+									</li>
+									<li class="display-ib">
+										<a class="custom-like" href="">
+											<span class="custom-like_icon vkontakte"></span>
+										</a>
+									</li>
+									<li class="display-ib">
+										<a class="custom-like" href="">
+											<span class="custom-like_icon facebook"></span>
+										</a>
+									</li>
+									<li class="display-ib">
+										<a class="custom-like" href="">
+											<span class="custom-like_icon twitter"></span>
+										</a>
+									</li>
+								</ul>
+								
+							
+								
+							
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<script>
+			$(window).load(function() {
+				/*
+				block - элемент, что фиксируется
+				elementStop - до какого элемента фиксируется
+				blockIndent - отступ
+				*/
+				function bJoinRowFixed() {
+
+					var block = $('.js-b-join-row');
+					var blockTop = block.offset().top;
+
+					var startTop = $('.layout-header').height();
+					
+
+					$(window).scroll(function() {
+				        var windowScrollTop = $(window).scrollTop();
+				        if (windowScrollTop > startTop) {
+				        	block.fadeIn();
+				        } else {
+
+							block.fadeOut();
+
+				        }
+				    });
+				}
+
+				bJoinRowFixed('.js-b-join-row');
+			})
+			</script>
+			<div class="b-join-row js-b-join-row">
+				<div class="b-join-row_hold">
+					<div class="b-join-row_logo"></div>
+					<div class="b-join-row_tx">Более <span class="b-join-row_tx-big"> 20 000 000</span> мам и пап</div>
+					<div class="b-join-row_slogan">уже посетили Веселый Жираф!</div>
+					<a href="" class="btn-green btn-h46">Присоединяйтесь!</a>
+				</div>
+			</div>
+
+
+		</div>
+
 
 		<div class="layout-content clearfix">
 			<div class="content-cols padding-t20">
 				<div class="col-1"> 
-					<div class="menu-list menu-list__favorites margin-t10">
-						<a href="" class="menu-list_i">
-							<span class="menu-list_tx">О проекте</span>
-						</a>
-						<a href="" class="menu-list_i">
-							<span class="menu-list_tx">Правила</span>
-						</a>
-						<a href="" class="menu-list_i">
-							<span class="menu-list_tx">Задать вопрос</span>
-						</a>
-						<a href="" class="menu-list_i active">
-							<span class="menu-list_tx">Реклама на сайте</span>
-						</a>
-						<a href="" class="menu-list_i">
-							<span class="menu-list_tx">Контакты </span>
-						</a>
-					</div>
+					<ul class="menu-list menu-list__favorites margin-t10">
+						<li class="menu-list_li">
+							<a href="" class="menu-list_i">
+								<span class="menu-list_tx">О проекте</span>
+							</a>
+						</li>
+						<li class="menu-list_li">
+							<a href="" class="menu-list_i">
+								<span class="menu-list_tx">Правила</span>
+							</a>
+						</li>
+						<li class="menu-list_li">
+							<a href="" class="menu-list_i">
+								<span class="menu-list_tx">Задать вопрос</span>
+							</a>
+						</li>
+						<li class="menu-list_li">
+							<a href="" class="menu-list_i active">
+								<span class="menu-list_tx">Реклама на сайте</span>
+							</a>
+						</li>
+						<li class="menu-list_li">
+							<a href="" class="menu-list_i">
+								<span class="menu-list_tx">Контакты </span>
+							</a>
+							<div class="menu-list_overlay"></div>
+						</li>
+					</ul>
 				</div>
 				<div class="col-23-middle">
 					<div class="col-white-hoar">

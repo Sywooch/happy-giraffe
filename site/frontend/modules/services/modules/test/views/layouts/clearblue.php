d555afebc2ebeb267ea1c794360e7996e15a9758
choojoy-work <nikita@happy-giraffe.ru>
2013-10-02 11:13:20 +0400

    trim name

---
+++ site/frontend/modules/services/modules/test/views/layouts/clearblue.php
@@ -0,0 +1,31 @@
+<?php $this->beginContent('//layouts/community'); ?>
+    <div class="col-1">
+        <div class="clearblue-aside1">
+            <a href="" class="clearblue-aside1_t"></a>
+            <div class="clearblue-aside1_tx">
+                Цифровой тест Clearblue самый <br>точный и самый простой в использовании
+            </div>
+            <div class="clearfix">
+                <div class="clearblue-aside1_count-hold">
+                    ТОЧНОСТЬ
+                    <div class="clearblue-aside1_count">99%</div>
+                </div>
+                <div class="clearblue-aside1_btn-hold">
+                    <a href="" class="btn-green">Узнать <br>больше</a>
+                </div>
+            </div>
+        </div>
+        <div class="clearblue-aside2">
+            <div class="clearblue-aside2_tx">
+                Цифровой тест Clearblue с определением срока беременности сообщит, беременны ли вы, и на каком сроке
+            </div>
+            <a href="" class="btn-green">Узнать больше</a>
+        </div>
+
+    </div>
+    <div class="col-23-middle ">
+
+        <?=$content?>
+
+    </div>
+<?php $this->endContent(); ?>

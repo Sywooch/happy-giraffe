c819a378ba87a2c7bb92ccae0c6919fca4b7ef44
choojoy-work <nikita@happy-giraffe.ru>
2013-10-02 12:03:24 +0400

    trim name

--- site/frontend/widgets/serviceSocial/views/_assistance_count.php
+++ site/frontend/widgets/serviceSocial/views/_assistance_count.php
@@ -1,7 +1,7 @@
 <div class="assistance-count">
     <div class="assistance_text"><?=$this->counter_title[0] ?></div>
     <div class="assistance-count_all clearfix">
-        <?php for ($i = 8; $i >= 0; $i--)
+        <?php for ($i = 5; $i >= 0; $i--)
             $this->render('__assistance_count', array('count' => $count, 'exp' => $i)); ?>
     </div>
     <div class="assistance_text"><?=Str::GenerateNoun($this->counter_title[1], $count) ?></div>

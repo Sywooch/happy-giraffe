c1091532528103c9d47a5ccfa72d51f593f6ef6e
choojoy-work <nikita@happy-giraffe.ru>
2013-10-03 15:48:51 +0400

    trim name

--- site/frontend/widgets/newCommentWidget/views/gallery_view.php
+++ site/frontend/widgets/newCommentWidget/views/gallery_view.php
@@ -1,26 +1,7 @@
 <?php $comments = $this->getComments(); ?><div class="photo-window_right">
 
     <div class="photo-window_banner-hold clearfix">
-        <!-- R-87026-5 ﬂÌ‰ÂÍÒ.RTB-·ÎÓÍ  -->
-        <div id="yandex_ad_R-87026-5"></div>
-        <script type="text/javascript">
-            (function(w, d, n, s, t) {
-                w[n] = w[n] || [];
-                w[n].push(function() {
-                    Ya.Context.AdvManager.render({
-                        blockId: "R-87026-5",
-                        renderTo: "yandex_ad_R-87026-5",
-                        async: true
-                    });
-                });
-                t = d.getElementsByTagName("script")[0];
-                s = d.createElement("script");
-                s.type = "text/javascript";
-                s.src = "//an.yandex.ru/system/context.js";
-                s.async = true;
-                t.parentNode.insertBefore(s, t);
-            })(this, this.document, "yandexContextAsyncCallbacks");
-        </script>
+        <iframe src="/rtb3.html" width="300" height="250" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"></iframe>
     </div>
 
     <?php $this->render('view', array('comments' => $comments)); ?>

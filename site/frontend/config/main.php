4275dab90c455ed21944eebff8ec781a7831c0e2
choojoy-work <nikita@happy-giraffe.ru>
2013-10-04 22:15:09 +0400

    trim name

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -221,7 +221,7 @@ return array(
 		),
 		'urlManager'=>require_once(dirname(__FILE__).'/url.php'),
 		'db' => array(
-            'schemaCachingDuration' => 3600,
+            'schemaCachingDuration' => 300,
             'tablePrefix'=> '',
         ),
         'db_seo' => array(

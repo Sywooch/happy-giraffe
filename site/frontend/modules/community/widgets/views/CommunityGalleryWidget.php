b1bfb124fed3b73645ff50a7ba5d9ecd7a38a7b2
choojoy-work <nikita@happy-giraffe.ru>
2013-10-05 00:08:21 +0400

    trim name

--- site/frontend/modules/community/widgets/views/CommunityGalleryWidget.php
+++ site/frontend/modules/community/widgets/views/CommunityGalleryWidget.php
@@ -2,6 +2,7 @@
 /**
  * @var CommunityContentGalleryWidget $widgetToShow
  */
+$item = $widgetToShow->item === null ? $widgetToShow->gallery->items[0] : $widgetToShow->item;
 ?>
 
 <div class="gallery-widget">
@@ -9,13 +10,13 @@
         <div class="gallery-widget_top-tx">еще рекомендуем посмотреть</div>
         <div class="gallery-widget_t"><?=$widgetToShow->gallery->content->title?></div>
     </div>
-    <a href="javascript:void(0)" class="gallery-widget_img" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode('PhotoPostPhotoCollection')?>, <?=CJavaScript::encode(array('contentId' => $widgetToShow->gallery->content->id))?>, <?=CJavaScript::encode($widgetToShow->item->photo_id)?>, <?=CJavaScript::encode(array('exitUrl' => $widgetToShow->gallery->content->getUrl()))?>)">
-        <?=CHtml::image($widgetToShow->item->photo->getPreviewUrl(580, null, Image::WIDTH), $widgetToShow->gallery->content->title)?>
+    <a href="javascript:void(0)" class="gallery-widget_img" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode('PhotoPostPhotoCollection')?>, <?=CJavaScript::encode(array('contentId' => $widgetToShow->gallery->content->id))?>, <?=CJavaScript::encode($item->photo_id)?>, <?=CJavaScript::encode(array('exitUrl' => $widgetToShow->gallery->content->getUrl()))?>)">
+        <?=CHtml::image($item->photo->getPreviewUrl(580, null, Image::WIDTH), $widgetToShow->gallery->content->title)?>
         <span class="ico-play-big"></span>
         <span class="gallery-widget_count"><?=$widgetToShow->gallery->count?> ФОТО</span>
     </a>
     <div class="gallery-widget_bottom clearfix">
-        <a href="javascript:void(0)" class="float-r btn-blue-light btn-medium" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode('PhotoPostPhotoCollection')?>, <?=CJavaScript::encode(array('contentId' => $widgetToShow->gallery->content->id))?>, <?=CJavaScript::encode($widgetToShow->item->photo_id)?>, <?=CJavaScript::encode(array('exitUrl' => $widgetToShow->gallery->content->getUrl()))?>)">Смотреть галерею</a>
+        <a href="javascript:void(0)" class="float-r btn-blue-light btn-medium" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode('PhotoPostPhotoCollection')?>, <?=CJavaScript::encode(array('contentId' => $widgetToShow->gallery->content->id))?>, <?=CJavaScript::encode($item->photo_id)?>, <?=CJavaScript::encode(array('exitUrl' => $widgetToShow->gallery->content->getUrl()))?>)">Смотреть галерею</a>
         <div class="meta-gray">
             <a href="<?=$widgetToShow->gallery->content->getUrl(true)?>" class="meta-gray_comment">
                 <span class="ico-comment ico-comment__white"></span>

4b389fec84fb3d30d5ef6d84ec236ca6a9da6e3d
choojoy-work <nikita@happy-giraffe.ru>
2013-10-08 18:51:18 +0400

    trim name

---
+++ site/frontend/modules/community/views/contest/_prizes.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @var CommunityContest $contest
+ */
+?>
+
+<div id="popup-contest-prize" class="popup-contest popup-contest__pets1">
+    <a class="popup-transparent-close" onclick="$.fancybox.close();" href="javascript:void(0);" title="Закрыть"></a>
+    <div class="clearfix">
+
+        <div class="b-settings-blue">
+
+            <div class="contest-prizes">
+                <div class="heading-title">Призы конкурса</div>
+                <div class="contest-prizes_i clearfix">
+                    <div class="contest-prizes_l">
+                        <div class="contest-prizes_img">
+                            <img src="/images/contest/club/pets1/prize-1-big.jpg" alt="">
+                        </div>
+                        <div class="place place-1-1"></div>
+                    </div>
+                    <div class="contest-prizes_r">
+                        <div class="contest-prizes_i-top">
+                            <span class="contest-prizes_heart"></span>
+                            <span class="contest-prizes_i-t">Лежак-Домик "Hilla" Trixie( 35х30х40 см) </span>
+                        </div>
+                        <div class="contest-prizes_desc">
+                            <p>Благодаря интересной и забавной форме и ткани под замшу такой домик для Вашего питомца станет отличным ярким дополнением в интерьере. Он подойдет как ля кошек, так и для маленьких собак.Trixie Лежак-Домик "Hilla"  может стать отличным подарком, как для домашнего питомца, так и для любого владельца маленького четвероного друга, ведь этот оригинальный домик - все, что нужно питомцу для отдыха и сна.</p>
+                            <p>Он выполнен из качественного полиэстера, который не потускнеет и не деформируется от времени, благодаря поролону стенки отлично держат форму,  а внутренняя двусторонняя  подушка с наполнением из полиэфирной ваты подарит Вашей кошечке или собачке невероятный комфорт и уют. Trixie Лежак-Домик "Hilla" надежно защитит Вашего питомца от сквозняков на холодном полу и поможет уберечь вашу мебель от ненужной шерсти, ведь теперь у животного будет свой собственный дом.За таким домиком очень легко ухаживать, ведь передняя и задняя стенки прикреплены застежкой молнией, а при необходимости Вы без труда даже можете постирать его.</p>
+                            <p>Этот подарок станет незаменимым для вашего домашнего любимца!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contest-prizes_i clearfix">
+                    <div class="contest-prizes_l">
+                        <div class="contest-prizes_img">
+                            <img src="/images/contest/club/pets1/prize-2-1-big.jpg" alt="">
+                        </div>
+                        <div class="clearfix">
+                            <a href="" class="contest-prizes_img">
+                                <img src="/images/contest/club/pets1/prize-2-1-small.jpg" alt="">
+                            </a>
+                            <a href="" class="contest-prizes_img">
+                                <img src="/images/contest/club/pets1/prize-2-2-small.jpg" alt="">
+                            </a>
+                            <a href="" class="contest-prizes_img">
+                                <img src="/images/contest/club/pets1/prize-2-3-small.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="place place-2-3"></div>
+                    </div>
+                    <div class="contest-prizes_r">
+                        <div class="contest-prizes_i-top">
+                            <span class="contest-prizes_heart"></span>
+                            <span class="contest-prizes_i-t">Автоматическая поилка фонтан "Original" Drinkwell </span>
+                        </div>
+                        <div class="contest-prizes_desc">
+                            <p>Поилка Drinkwell Original разработана ветеринарами и поможет обеспечить вашего домашнего питомца свежей фильтрованной водой в объеме 1,5л. Запатентованный ручеёк привлекает кошек и собак пить больше и постоянно обогащает воду кислородом. Угольный фильтр удаляет неприятный привкус и запах. Ваш любимец всегда будет иметь запас свежей воды.</p>
+                            <p>
+                                - Ручеек привлекает животных и приглашает их пить больше <br>
+                                - Специальный выступ снижает брызги от падающей воды <br>
+                                - Регулируемая подача воды <br>
+                                - Возможность полной разборки для легкого мытья <br>
+                                - Для использования внутри помещений <br>
+                                - Для кошек и собак мелких и средних пород <br>
+                                - Объем 1,5л. <br>
+                                - Питание от сети.</p>
+                            <p>Этот подарок станет незаменимым для вашего домашнего любимца!</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contest-prizes_bottom clearfix">
+                    <div class="contest-prizes_bottom-tx clearfix">
+                        <span class="ico-giraffe-r"></span>
+								<span class="">
+									Будьте активны! Расскажите о ваших питомцах
+								</span>
+                    </div>
+                    <a href="<?=$contest->getParticipateUrl()?>" class="btn-green btn-h46 fancy">Принять участие!</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>

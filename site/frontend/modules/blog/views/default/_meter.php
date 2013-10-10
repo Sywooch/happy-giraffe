99decd081fc615f6be9ffab5013e82f7972d01b3
choojoy-work <nikita@happy-giraffe.ru>
2013-10-10 13:57:57 +0400

    trim name

--- site/frontend/modules/blog/views/default/_meter.php
+++ site/frontend/modules/blog/views/default/_meter.php
@@ -10,7 +10,7 @@
         <div class="contest-meter_count-tx"><?=Str::GenerateNoun(array('балл', 'балла', 'баллов'), $contestWork->rate)?></div>
     </div>
     <a href="javascript:void(0)" class="contest-meter_a-vote" onclick="$(this).next().toggleClass('display-b')">Голосовать</a>
-    <div class="contest-meter_vote">
+    <div class="contest-meter_vote" onmouseleave="$(this).removeClass('display-b')">
         <div class="contest-meter_vote-tx">Вы можете проголосовать за участника нажав на кнопки соцсетей</div>
         <div class="contest-meter_vote-hold">
             <div class="like-block fast-like-block">

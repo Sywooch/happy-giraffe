0b62c9edcd938594d451960ce9dddf9adc1ccbcc
choojoy-work <nikita@happy-giraffe.ru>
2013-10-10 16:21:15 +0400

    trim name

--- site/frontend/modules/gallery/widgets/views/PhotoCollectionViewWidget.php
+++ site/frontend/modules/gallery/widgets/views/PhotoCollectionViewWidget.php
@@ -1,9 +1,9 @@
 <?php if ($this->href === null): ?>
 <div class="photo-grid clearfix">
     <?php foreach ($grid as $row): ?>
-    <div class="photo-grid_row clearfix" >
+    <div class="photo-grid_row clearfix" style="margin-bottom: <?=$borderSize?>px;">
         <?php foreach ($row['photos'] as $p): ?>
-        <div class="photo-grid_i" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode(get_class($this->collection))?>, <?=CJavaScript::encode($this->collection->options)?>, <?=CJavaScript::encode($p->id)?><?php if ($this->windowOptions !== null): ?>, <?=CJavaScript::encode($this->windowOptions)?><?php endif; ?>)">
+        <div class="photo-grid_i" style="margin-right: <?=$borderSize?>px;" onclick="PhotoCollectionViewWidget.open(<?=CJavaScript::encode(get_class($this->collection))?>, <?=CJavaScript::encode($this->collection->options)?>, <?=CJavaScript::encode($p->id)?><?php if ($this->windowOptions !== null): ?>, <?=CJavaScript::encode($this->windowOptions)?><?php endif; ?>)">
             <?=CHtml::image($p->getPreviewUrl(null, $row['height'], Image::HEIGHT), '', array('class' => 'photo-grid_img'))?>
             <div class="photo-grid_overlay">
                 <span class="photo-grid_zoom"></span>
@@ -16,9 +16,9 @@
 <?php else: ?>
 <div class="photo-grid clearfix">
     <?php foreach ($grid as $row): ?>
-        <div class="photo-grid_row clearfix" >
+        <div class="photo-grid_row clearfix" style="margin-bottom: <?=$borderSize?>px;">
             <?php foreach ($row['photos'] as $p): ?>
-                <div class="photo-grid_i">
+                <div class="photo-grid_i" style="margin-right: <?=$borderSize?>px;">
                     <a href="<?=$this->href?>">
                         <?=CHtml::image($p->getPreviewUrl(null, $row['height'], Image::HEIGHT), '', array('class' => 'photo-grid_img'))?>
                     </a>

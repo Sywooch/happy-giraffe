1b8081fed402ea585b8ae8de6a88b7fc75e33b6d
choojoy-work <nikita@happy-giraffe.ru>
2013-10-17 17:51:42 +0400

    trim name

---
+++ site/frontend/modules/community/widgets/views/CommunityMoreWidget.php
@@ -0,0 +1,184 @@
+<div class="such-post">
+    <div class="such-post_i such-post_i__photopost">
+        <a href="" class="such-post_img-hold">
+            <img src="/images/example/w335-h230.jpg" alt="" class="such-post_img">
+            <span class="such-post_img-overlay"></span>
+            <span class="such-post_tip">25 фото</span>
+        </a>
+        <div class="such-post_type-hold">
+            <div class="such-post_type such-post_type__photopost"></div>
+        </div>
+        <div class="such-post_cont">
+            <div class="clearfix">
+                <div class="float-l">
+                    <div class="meta-gray clearfix">
+                        <div class="meta-gray_view">
+                            <span class="ico-view ico-view__white"></span>
+                            <span class="meta-gray_tx color-gray-light">305</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="meta-gray">
+                    <a class="meta-gray_comment" href="">
+                        <span class="ico-comment ico-comment__white"></span>
+                        <span class="meta-gray_tx color-gray-light">35</span>
+                    </a>
+                </div>
+                <div class="such-post_author">
+                    <a href="" class="ava male small">
+                        <span class="icon-status status-online"></span>
+                        <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+                    </a>
+                    <a href="" class="such-post_author-name">Татьяна</a>
+                </div>
+                <div class="such-post_date">Сегодня 13:25</div>
+            </div>
+            <a href="" class="such-post_t">Креативная сервисная</a>
+        </div>
+    </div>
+    <div class="such-post_i such-post_i__photopost">
+        <a href="" class="such-post_img-hold">
+            <img src="/images/example/w335-h230.jpg" alt="" class="such-post_img">
+            <span class="such-post_img-overlay"></span>
+            <span class="such-post_tip">50 фото</span>
+        </a>
+        <div class="such-post_type-hold">
+            <div class="such-post_type such-post_type__photopost"></div>
+        </div>
+        <div class="such-post_cont">
+            <div class="clearfix">
+                <div class="float-l">
+                    <div class="meta-gray clearfix">
+                        <div class="meta-gray_view">
+                            <span class="ico-view ico-view__white"></span>
+                            <span class="meta-gray_tx color-gray-light">305</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="meta-gray">
+                    <a class="meta-gray_comment" href="">
+                        <span class="ico-comment ico-comment__white"></span>
+                        <span class="meta-gray_tx color-gray-light">35</span>
+                    </a>
+                </div>
+                <div class="such-post_author">
+                    <a href="" class="ava male small">
+                        <span class="icon-status status-online"></span>
+                        <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+                    </a>
+                    <a href="" class="such-post_author-name">Татьяна</a>
+                </div>
+                <div class="such-post_date">Сегодня 13:25</div>
+            </div>
+            <a href="" class="such-post_t">Креативная сервировка стола - 50 замечательных примеров</a>
+        </div>
+    </div>
+</div>
+
+<div class="such-post">
+    <div class="such-post_i such-post_i__medium">
+        <a href="" class="such-post_img-hold">
+            <img src="/images/example/w220-h165-2.jpg" alt="" class="such-post_img">
+        </a>
+        <div class="such-post_type-hold">
+            <div class="such-post_type such-post_type__post"></div>
+        </div>
+        <div class="such-post_cont">
+            <div class="clearfix">
+                <div class="float-l">
+                    <div class="meta-gray clearfix">
+                        <div class="meta-gray_view">
+                            <span class="ico-view ico-view__gray"></span>
+                            <span class="meta-gray_tx">305</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="meta-gray">
+                    <a href="" class="meta-gray_comment">
+                        <span class="ico-comment ico-comment__gray"></span>
+                        <span class="meta-gray_tx">35</span>
+                    </a>
+                </div>
+                <div class="such-post_author">
+                    <a href="" class="ava male small">
+                        <span class="icon-status status-online"></span>
+                        <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+                    </a>
+                    <a href="" class="such-post_author-name">Татьяна</a>
+                </div>
+                <div class="such-post_date">Сегодня 13:25</div>
+            </div>
+            <a href="" class="such-post_t">Самое лучшее утро - просыпаюсь, а ты рядом</a>
+        </div>
+    </div>
+    <div class="such-post_i such-post_i__medium">
+        <a href="" class="such-post_img-hold">
+            <img src="/images/example/w220-h165-1.jpg" alt="" class="such-post_img">
+        </a>
+        <div class="such-post_type-hold">
+            <div class="such-post_type such-post_type__post"></div>
+        </div>
+        <div class="such-post_cont">
+            <div class="clearfix">
+                <div class="float-l">
+                    <div class="meta-gray clearfix">
+                        <div class="meta-gray_view">
+                            <span class="ico-view ico-view__gray"></span>
+                            <span class="meta-gray_tx">305</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="meta-gray">
+                    <a href="" class="meta-gray_comment">
+                        <span class="ico-comment ico-comment__gray"></span>
+                        <span class="meta-gray_tx">35</span>
+                    </a>
+                </div>
+                <div class="such-post_author">
+                    <a href="" class="ava male small">
+                        <span class="icon-status status-online"></span>
+                        <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+                    </a>
+                    <a href="" class="such-post_author-name">Татьяна</a>
+                </div>
+                <div class="such-post_date">Сегодня 13:25</div>
+            </div>
+            <a href="" class="such-post_t">Печеньки</a>
+        </div>
+    </div>
+    <div class="such-post_i such-post_i__medium">
+        <a href="" class="such-post_img-hold">
+            <img src="/images/example/w220-h165-2.jpg" alt="" class="such-post_img">
+        </a>
+        <div class="such-post_type-hold">
+            <div class="such-post_type such-post_type__video"></div>
+        </div>
+        <div class="such-post_cont">
+            <div class="clearfix">
+                <div class="float-l">
+                    <div class="meta-gray clearfix">
+                        <div class="meta-gray_view">
+                            <span class="ico-view ico-view__gray"></span>
+                            <span class="meta-gray_tx">305</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="meta-gray">
+                    <a href="" class="meta-gray_comment">
+                        <span class="ico-comment ico-comment__gray"></span>
+                        <span class="meta-gray_tx">35</span>
+                    </a>
+                </div>
+                <div class="such-post_author">
+                    <a href="" class="ava male small">
+                        <span class="icon-status status-online"></span>
+                        <img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+                    </a>
+                    <a href="" class="such-post_author-name">Татьяна</a>
+                </div>
+                <div class="such-post_date">Сегодня 13:25</div>
+            </div>
+            <a href="" class="such-post_t">Креативная сервировка стола - 50 замечательных примеров</a>
+        </div>
+    </div>
+</div>

004a1a61732fb6227e647ab9e9ed46215975e2ae
choojoy-work <nikita@happy-giraffe.ru>
2013-10-18 17:18:43 +0400

    trim name

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -337,7 +337,7 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-        'releaseId' => 10,
+        'releaseId' => 11,
         'valentinesAlbum' => '41340',
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',

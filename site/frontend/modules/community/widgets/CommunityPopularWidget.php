dc24f9f560bae575ce0b1345f6187f45281e4d6a
choojoy-work <nikita@happy-giraffe.ru>
2013-10-21 16:37:13 +0400

    trim name

--- site/frontend/modules/community/widgets/CommunityPopularWidget.php
+++ site/frontend/modules/community/widgets/CommunityPopularWidget.php
@@ -13,18 +13,24 @@ class CommunityPopularWidget extends CWidget
 
     public function run()
     {
+        echo '<!-- popular club ' . $this->club->id . ' -->';
+
         $popular = ClubPopular::model()->findByAttributes(array(
             'date' => date('Y-m-d'),
             'clubId' => (int) $this->club->id,
         ));
 
         if ($popular !== null) {
+            echo '<!-- popular found -->';
+
             $criteria = new CDbCriteria();
             $criteria->addInCondition('t.id', $popular->contents);
             $contents = CommunityContent::model()->findAll($criteria);
 
-            if ($contents)
+            if ($contents) {
+                echo '<!-- popular render -->';
                 $this->render('CommunityPopularWidget', compact('contents'));
+            }
         }
     }
 }

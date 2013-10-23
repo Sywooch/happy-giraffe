63530f1ee562798d245313ed03e640525a27b7fa
Tantalid <tantalid@mail.ru>
2013-10-23 09:54:58 +0300

    comments

--- block/global/comments-gray.php
+++ block/global/comments-gray.php
@@ -1,89 +1,111 @@
 		<div class="comments-gray">
 			<div class="comments-gray_t">
-				<a href="" class="comments-gray_t-a">
-					<span class="comments-gray_t-a-tx">Все комментарии (28)</span>
-				</a>
+				<span class="comments-gray_t-tx">Все комментарии <span class="color-gray">(28)</span></span>
+				<a href="" class="a-pseudo font-small">Показать все  </a>
 			</div>
 			<div class="comments-gray_hold">
 				<div class="comments-gray_i comments-gray_i__self">
 					<div class="comments-gray_ava">
-						<a href="" class="ava small male"></a>
+						<a href="" class="ava middle male"></a>
+					</div>
+					<div class="comments-gray_r">
+						<div class="font-smallest color-gray">Сегодня 13:25</div>
+						<div class="comments-gray_control">
+							<div class="comments-gray_control-hold">
+								<a href="" class="message-ico message-ico__edit powertip" title="Редактировать"></a>
+								<a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
+							</div>
+						</div>
+						
 					</div>
 					<div class="comments-gray_frame">
 						<div class="comments-gray_header clearfix">
 							<a href="" class="comments-gray_author">Ангелина Богоявленская </a>
-							<span class="font-smallest color-gray">Сегодня 13:25</span>
+							<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
 						</div>
 						<div class="comments-gray_cont wysiwyg-content">
 							<p>	<a href="">Вася Пупкин,</a> Мне безумно жалко всех женщин, но особенно Тину Кароль, я просто представить себе не могу <a href="">как она все это переживет</a> как она все это переживет(</p>
 							<p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту и подцепляет естественно студийные версии песен вместо нужных.  я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически </p>
 						</div>
 					</div>
-					<div class="comments-gray_control comments-gray_control__self">
-						<div class="comments-gray_control-hold">
-							<div class="clearfix">
-								<a href="" class="message-ico message-ico__edit powertip" title="Редактировать"></a>
-							</div>
-							<div class="clearfix">
-								<a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
-							</div>
-						</div>
-					</div>
 				</div>
 				<div class="comments-gray_i">
-					<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">7918</a>
 					<div class="comments-gray_ava">
-						<a href="" class="ava small female"></a>
+						<a href="" class="ava middle female"></a>
 					</div>
+
+					<div class="comments-gray_r">
+						<div class="font-smallest color-gray">Сегодня 13:25</div>
+											
+						<div class="comments-gray_control">
+							<div class="comments-gray_control-hold">
+								<a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
+								<a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
+								<a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
+							</div>
+						</div>
+						
+					</div>
+
 					<div class="comments-gray_frame">
 						<div class="comments-gray_header clearfix">
 							<a href="" class="comments-gray_author">Анг Богоявлен </a>
-							<span class="font-smallest color-gray">Сегодня 14:25</span>
+							<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
 						</div>
 						<div class="comments-gray_cont wysiwyg-content">
 							<p>я не нашел, где можно поменять название трека. </p>
 						</div>
 					</div>
-					
-					<div class="comments-gray_control">
-						<div class="comments-gray_control-hold">
-							<div class="clearfix">
-								<a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
-							</div>
-							<div class="clearfix">
-								<a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
-							</div>
-						</div>
-						<div class="clearfix">
-							<a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
-						</div>
-					</div>
+
 				</div>
 				
 				<div class="comments-gray_i comments-gray_i__recovery">
 					<div class="comments-gray_ava">
-						<a href="" class="ava small female"></a>
+						<a href="" class="ava middle female"></a>
+					</div>
+					<div class="comments-gray_r">
+						<div class="font-smallest color-gray">Сегодня 13:25</div>
+						
+						<!-- В удаленном сообщении не должно быть кнопок управления -->				
+						<!-- <div class="comments-gray_control">
+							<div class="comments-gray_control-hold">
+								<a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
+								<a href="" class="message-ico message-ico__del powertip" title="Удалить"></a>
+								<a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
+							</div>
+						</div> -->
+						
 					</div>
 					<div class="comments-gray_frame">
 						<div class="comments-gray_header clearfix">
 							<a href="" class="comments-gray_author">Анг Богоявлен </a>
-							<span class="font-smallest color-gray">Сегодня 14:25</span>
+							<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
 						</div>
 						<div class="comments-gray_cont wysiwyg-content">
-							<p>Комментарий успешно удален.<a href="" class="comments-gray_a-recovery">Восстановить?</a> </p>
+							<p>Комментарий успешно удален. <a href="" class="comments-gray_a-recovery"> Восстановить?</a> </p>
 						</div>
 					</div>
 				</div>
 				
 				<div class="comments-gray_i">
-					<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
 					<div class="comments-gray_ava">
-						<a href="" class="ava small female"></a>
+						<a href="" class="ava middle female"></a>
+					</div>
+					<div class="comments-gray_r">
+						<div class="font-smallest color-gray">Сегодня 13:25</div>
+						
+						<div class="comments-gray_control">
+							<div class="comments-gray_control-hold">
+								<a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
+								<a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
+							</div>
+						</div>
+						
 					</div>
 					<div class="comments-gray_frame">
 						<div class="comments-gray_header clearfix">
 							<a href="" class="comments-gray_author">Анг Богоявлен </a>
-							<span class="font-smallest color-gray">Сегодня 14:25</span>
+							<a href="" class="comments-gray_like like-hg-small powertip" title="Нравится">78</a>
 						</div>
 						<div class="comments-gray_cont wysiwyg-content">
 							<p>я не нашел, где можно поменять название трека. Меняя название трека в альбоме он автоматически производит поиск по сайту </p>
@@ -103,28 +125,18 @@
 						</div>
 					</div>
 					
-					<div class="comments-gray_control">
-						<div class="comments-gray_control-hold">
-							<div class="clearfix">
-								<a href="" class="comments-gray_quote-ico powertip" title="Ответить"></a>
-							</div>
-						</div>
-						<div class="clearfix">
-							<a href="" class="message-ico message-ico__warning powertip" title="Пожаловаться"></a>
-						</div>
-					</div>
 				</div>
 			</div>
 			
 			<div class="comments-gray_add clearfix">
 				
 				<div class="comments-gray_ava">
-					<a href="" class="ava small female"></a>
+					<a href="" class="ava middle female"></a>
 				</div>
 				
 				<div class="comments-gray_frame">
 					<!-- input hidden -->
-					<input type="text" name="" id="" class="comments-gray_add-itx itx-gray" placeholder="Ваш комментарий">
+					<input type="text" name="" id="" class="comments-gray_add-itx itx-gray" placeholder="Добавьте комментарий">
 		
 				</div>
 			</div>

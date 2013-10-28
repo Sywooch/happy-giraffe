e995b4a1f7f733385bc8e3f5aeed59ded085bd9c
Tantalid <tantalid@mail.ru>
2013-10-28 08:07:12 +0200

    start

--- block/global/header-new.php
+++ block/global/header-new.php
@@ -5,6 +5,7 @@
 				<a href="/" class="logo_i" title="Веселый жираф - сайт для все семьи">Веселый жираф - сайт для всей семьи</a>
 				<span class="logo_slogan">стайт для всей семьи</span>
 			</div>
+
 			<div class="header-menu">
 				<ul class="header-menu_ul clearfix">
 					<li class="header-menu_li">
@@ -20,27 +21,7 @@
 							<span class="header-menu_count">256</span>
 						</a>
 					</li>
-					<li class="header-menu_li">
-						<a href="" class="header-menu_a">
-							<span class="header-menu_ico header-menu_ico__favorite"></span>
-							<span class="header-menu_tx">Мое<br>избранное</span>
-						</a>
-					</li>
-					<li class="header-menu_li header-menu_li__sepor"></li>
-					<li class="header-menu_li">
-						<a href="" class="header-menu_a">
-							<span class="ava middle">
-								<img src="http://img.happy-giraffe.ru/avatars/12963/ava/8d26a6f4dbae0536f8dbec37c0b5e5f8.jpg" alt="">
-							</span>
-							<span class="header-menu_tx">Моя<br>страница</span>
-						</a>
-					</li>
-					<li class="header-menu_li">
-						<a href="" class="header-menu_a">
-							<span class="header-menu_ico header-menu_ico__family"></span>
-							<span class="header-menu_tx">Моя<br>семья</span>
-						</a>
-					</li>
+
 					<li class="header-menu_li active">
 						<a href="" class="header-menu_a">
 							<span class="header-menu_ico header-menu_ico__im"></span>
@@ -48,6 +29,7 @@
 							<span class="header-menu_count">25456</span>
 						</a>
 					</li>
+
 					<li class="header-menu_li active">
 						<a href="" class="header-menu_a">
 							<span class="header-menu_ico header-menu_ico__friend"></span>
@@ -55,15 +37,54 @@
 							<span class="header-menu_count">2</span>
 						</a>
 					</li>
+
 					<li class="header-menu_li">
 						<a href="" class="header-menu_a">
 							<span class="header-menu_ico header-menu_ico__award"></span>
 							<span class="header-menu_tx">Мои<br>успехи</span>
 						</a>
 					</li>
+					<li class="header-menu_li header-menu_li__ava">
+						<a href="" class="header-menu_a">
+							<span class="ava middle">
+								<img src="http://img.happy-giraffe.ru/avatars/12963/ava/8d26a6f4dbae0536f8dbec37c0b5e5f8.jpg" alt="">
+							</span>
+						</a>
+					</li>
+					<li class="header-menu_li header-menu_li__dropin">
+						<a href="" class="header-menu_a">
+							<span class="header-menu_ico header-menu_ico__dropin"></span>
+						</a>
+						<div class="header-drop">
+							<div class="header-menu">
+								<ul class="header-menu_ul">
+									<li class="header-menu_li">
+										<a href="" class="header-menu_a">
+											<span class="header-menu_ico header-menu_ico__favorite"></span>
+											<span class="header-menu_tx">Мое<br>избранное</span>
+										</a>
+									</li>
+
+									
+									<li class="header-menu_li">
+										<a href="" class="header-menu_a">
+											<span class="header-menu_ico header-menu_ico__family"></span>
+											<span class="header-menu_tx">Моя<br>семья</span>
+										</a>
+									</li>
+									
+								</ul>
+							</div>
+							<div class="header-drop_b">
+								
+							</div>
+						</div>
+
+					</li>
+
+
 				</ul>
 			</div>
-			<a href="" class="layout-header_logout">Выход</a>
 		</div>
 		
 	</div>

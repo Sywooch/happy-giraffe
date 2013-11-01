096290d034b93d92745c95100c879a5442475d9a
choojoy-work <nikita@happy-giraffe.ru>
2013-11-01 14:20:28 +0400

    trim name

--- site/frontend/modules/blog/views/default/view.php
+++ site/frontend/modules/blog/views/default/view.php
@@ -109,6 +109,33 @@ switch ($data->type_id) {
     </div>
 </div>
 
+<?php if ($full): ?>
+    <div style="margin: 40px 20px;">
+        <script type="text/javascript">
+            <!--
+            if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+            if (typeof(document.referrer) != 'undefined') {
+                if (typeof(afReferrer) == 'undefined') {
+                    afReferrer = escape(document.referrer);
+                }
+            } else {
+                afReferrer = '';
+            }
+            var addate = new Date();
+
+
+            var dl = escape(document.location);
+            var pr1 = Math.floor(Math.random() * 1000000);
+
+            document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
+            document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
+
+            AdFox_getCodeScript(1,pr1,'http://ads.adfox.ru/211012/prepareCode?pp=i&amp;ps=bkqy&amp;p2=evor&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;dl='+dl+'&amp;pr1='+pr1);
+            // -->
+        </script>
+    </div>
+<?php endif; ?>
+
 <?php if ($full && ! $data->getIsFromBlog() && in_array($data->rubric->community->club_id, array(8, 10, 14))): ?>
     <?php $this->widget('CommunityMoreWidget', array('content' => $data)); ?>
 <?php endif; ?>

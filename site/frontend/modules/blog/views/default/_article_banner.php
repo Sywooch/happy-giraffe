789884ab6ffbe8a521408c2d650d737ce0e1f837
choojoy-work <nikita@happy-giraffe.ru>
2013-11-06 13:16:07 +0400

    trim name

--- site/frontend/modules/blog/views/default/_article_banner.php
+++ site/frontend/modules/blog/views/default/_article_banner.php
@@ -1,4 +1,5 @@
 <div class="article-banner">
+    <?php if (false): ?>
     <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
     <div id="yandex_ad" style="padding: 20px 20px 30px; background: #fffff0;">
         <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
@@ -31,4 +32,46 @@
             })(window, document, "yandex_context_callbacks");
         </script>
     </div>
+    <?php endif; ?>
+
+    <!--AdFox START-->
+    <!--giraffe-->
+    <!--Площадка: Весёлый Жираф / * / *-->
+    <!--Тип баннера: Безразмерный 680х470-->
+    <!--Расположение: <низ страницы>-->
+    <!-- ________________________AdFox Asynchronous code START__________________________ -->
+    <script type="text/javascript">
+        <!--
+        if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+        if (typeof(document.referrer) != 'undefined') {
+            if (typeof(afReferrer) == 'undefined') {
+                afReferrer = escape(document.referrer);
+            }
+        } else {
+            afReferrer = '';
+        }
+        var addate = new Date();
+
+        var scrheight = '', scrwidth = '';
+        if (self.screen) {
+            scrwidth = screen.width;
+            scrheight = screen.height;
+        } else if (self.java) {
+            var jkit = java.awt.Toolkit.getDefaultToolkit();
+            var scrsize = jkit.getScreenSize();
+            scrwidth = scrsize.width;
+            scrheight = scrsize.height;
+        }
+
+        var dl = escape(document.location);
+        var pr1 = Math.floor(Math.random() * 1000000);
+
+        document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
+        document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
+
+        AdFox_getCodeScript(1,pr1,'http://ads.adfox.ru/211012/prepareCode?pp=i&amp;ps=bkqy&amp;p2=evor&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '&amp;dl='+dl+'&amp;pr1='+pr1);
+        // -->
+    </script>
+    <!-- _________________________AdFox Asynchronous code END___________________________ -->
+
 </div>

8b686ec7d66b350491fd2cc60fdda12b60f59512
Tantalid <tantalid@mail.ru>
2013-11-11 10:21:23 +0200

    contest

---
+++ html/social/contest/contest-blogger/contest-about.php
@@ -0,0 +1,98 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="">
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+
+	</div>
+
+	<div class="contest-h">
+		<div class="contest-h_hold">
+			<div class="contest-h_contest">КОНКУРС</div>
+			<h1 class="contest-h_t">Комментатор и блоггер месяца</h1>
+			<ul class="contest-h_ul">
+				<li class="contest-h_li active">
+					<a href="" class="contest-h_a">О конкурсе</a>
+				</li>
+				<li class="contest-h_li">
+					<a href="" class="contest-h_a">Тор 100</a>
+				</li>
+			</ul>
+			<div class="contest-h_cont">Уважаемые мамы и папы стартовал новый конкурс! Все очень легко  вы больше пишите записей в свои блоги и добавляете свои <br> комментарии ко всему что вам интересно. Желаем вам выйграть достойные призы легко и с интересом!</div>
+		</div>
+	</div>
+
+	<div class="layout-wrapper">
+		<div class="layout-content margin-l0 clearfix">
+		<div class="content-cols ">
+			<div class="col-white">	
+				<div id="contest" class="contest contest-11">
+					<div class="contest-about clearfix">
+						<div class="contest-participant">
+							<img src="/images/contest/widget-11.jpg" alt="" calss="contest-title" />
+							<div class="img">
+								<a href="">
+									<img src="/images/example/gallery_album_img_01.jpg">
+									<span class="btn">Посмотреть</span>
+								</a>
+							<div class="item-title">Разнообразие десертов сицилийского стиля</div>
+							</div>
+							<div class="clearfix">
+								<div class="position">
+									<strong>18</strong> место
+								</div>
+								<div class="ball">
+									<div class="ball-count">186</div>
+									<div class="ball-text">баллов</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="sticker">
+							<big>Для участия в конкурсе Вам необходимо:</big>
+							<p>Для того, чтобы принять участие в конкурсе, вы должны заполнить <a href="#">свой профиль</a> и информацию о членах своей семьи!</p>
+							<center>
+								<a href="#takeapartPhotoContest" class="btn-green btn-green-medium fancy">Участвовать<i class="arr-r"></i></a>
+							</center>
+						</div>
+						
+						<div class="content-title">О конкурсе</div>
+						
+						<p>Летняя прогулка с ребенком всегда превращается в целое приключение. Маленькие сорванцы ни секунды не могут усидеть на месте, и постоянно чем-то заняты: катанием на самокатах, машинках, роликах.</p>
+						<p>Пришлите нам фотографию своего ребенка с его любимым транспортным средством и получите один из трех отличных призов! </p>
+						
+					</div>
+					
+	            </div>
+
+				
+			</div>
+		</div>
+		</div>
+	</div>
+
+	<div class="contest-prize-sber">
+		<div class="contest-prize-sber_hold">
+			<div class="contest-prize-sber_t">Призы победителям! </div>
+			<div class="contest-prize-sber_img">
+				<img src="/images/contest-prize-sber_img.png" alt="">
+			</div>
+			<div class="contest-prize-sber_cont">Первых 5 блоггеров и 5 комментаторов ждет подарочная олимпийская карта VISA от Сбербанка с <span class="contest-prize-sber_numb">1 000</span> рублями!
+			</div>
+			<a href="" class="contest-prize-sber_more">Узнать подробнее</a>
+		</div>
+	</div>
+	<div class="footer-push"></div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</body>
+</html>

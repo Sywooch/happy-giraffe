6e03a5cce92c6eaf5751cee864e40ba55edd16c3
choojoy-work <nikita@happy-giraffe.ru>
2013-11-14 12:44:40 +0400

    trim name

--- site/frontend/modules/contest/views/default/prizes_desc/12.php
+++ site/frontend/modules/contest/views/default/prizes_desc/12.php
@@ -4,7 +4,7 @@
         <li class="clearfix" id="prize-1">
             <div class="l-col">
                 <div class="img">
-                    <a href=""><img src="/images/prize_42.jpg" /></a>
+                    <a href="http://www.silver-care.ru"><img src="/images/prize_42.jpg" /></a>
                 </div>
             </div>
             <div class="desc">
@@ -21,13 +21,13 @@
                     <li class="contest-prizes-desc_li">Спрей для полости рта без фтора</li>
                 </ul>
                 <br>
-                <p><span class="color-gray">Подробнее:</span> <a href=""> www.silver-care.ru</a> </p>
+                <p><span class="color-gray">Подробнее:</span> <a href="http://www.silver-care.ru"> www.silver-care.ru</a> </p>
             </div>
         </li>
         <li class="clearfix" id="prize-2">
             <div class="l-col">
                 <div class="img">
-                    <a href=""><img src="/images/prize_43.jpg" /></a>
+                    <a href="http://www.silver-care.ru"><img src="/images/prize_43.jpg" /></a>
                 </div>
             </div>
             <div class="desc">
@@ -42,13 +42,13 @@
                     <li class="contest-prizes-desc_li">Набор для чистки языка</li>
                 </ul>
                 <br>
-                <p><span class="color-gray">Подробнее:</span> <a href=""> www.silver-care.ru</a> </p>
+                <p><span class="color-gray">Подробнее:</span> <a href="http://www.silver-care.ru"> www.silver-care.ru</a> </p>
             </div>
         </li>
         <li class="clearfix" id="prize-3">
             <div class="l-col">
                 <div class="img">
-                    <a href=""><img src="/images/prize_44.jpg" /></a>
+                    <a href="http://www.silver-care.ru"><img src="/images/prize_44.jpg" /></a>
                 </div>
             </div>
             <div class="desc">
@@ -62,7 +62,7 @@
                     <li class="contest-prizes-desc_li">Спрей для полости рта (со фтором или без фтора)</li>
                 </ul>
                 <br>
-                <p><span class="color-gray">Подробнее:</span> <a href=""> www.silver-care.ru</a> </p>
+                <p><span class="color-gray">Подробнее:</span> <a href="http://www.silver-care.ru"> www.silver-care.ru</a> </p>
             </div>
         </li>
 
@@ -75,13 +75,13 @@
 
     <div class="contest-brand">
         <div class="contest-brand_logo">
-            <a href="" class="">
+            <a href="http://www.silver-care.ru" class="">
                 <img src="/images/contest/contest-brand_logo_12.jpg" alt="">
             </a>
         </div>
         <div class="contest-brand_hold">
             <p>Это бренд всемирно известного итальянского производителя Piave, который был основан еще 150 лет тому назад.</p>
-            <p>Piave производит зубные щетки и аксессуары для полости рта для крупнейших фармацевтических компаний. А уникальные антибактериальные зубные щетки с серебряным покрытием — под своей собственной торговой маркой <a href="">Silver Care</a>.</p>
+            <p>Piave производит зубные щетки и аксессуары для полости рта для крупнейших фармацевтических компаний. А уникальные антибактериальные зубные щетки с серебряным покрытием — под своей собственной торговой маркой <a href="http://www.silver-care.ru">Silver Care</a>.</p>
             <p>Антибактериальное серебряное покрытие — это концептуально новое, оригинальное и эффективное решение проблемы размножения бактерий на поверхности зубных щеток и аксессуаров.</p>
         </div>
     </div>

c040e82ec5b321c2d3b0c79eef2460792d8c2464
choojoy-work <nikita@happy-giraffe.ru>
2013-11-20 17:19:04 +0400

    trim name

--- site/frontend/modules/blog/views/default/form/contest/2.php
+++ site/frontend/modules/blog/views/default/form/contest/2.php
@@ -46,7 +46,7 @@ $form = $this->beginWidget('CActiveForm', array(
                     <div class="wysiwyg-v wysiwyg-blue clearfix">
                         <label for="" class="b-settings-blue_label">Рассказ</label>
                         <?php $slaveModel->text = $slaveModel->forEdit->text ?>
-                        <?=$form->textArea($slaveModel, 'text', array('class' => 'wysiwyg-redactor-v'))?>
+                        <?=$form->textArea($slaveModel, 'text', array('class' => 'wysiwyg-redactor-v', 'placeholder' => $contest->textHint))?>
                         <?=$form->error($slaveModel, 'text')?>
                     </div>
 

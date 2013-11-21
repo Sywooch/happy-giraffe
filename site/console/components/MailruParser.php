9a281486b6bf027e034e56711911fc8f54caef76
choojoy-work <nikita@happy-giraffe.ru>
2013-11-21 17:59:49 +0400

    trim name

--- site/console/components/MailruParser.php
+++ site/console/components/MailruParser.php
@@ -48,11 +48,11 @@ class MailruParser extends ProxyParserThread
             $email = preg_replace('#^\/(.*)\/(.*)\/$#', '$2@$1.ru', $innerUrl);
             $gender = (strpos($user->class, 'b-mypage-social__card_male')) ? 1 : 0;
             $age = intval($user->find('.b-mypage-social__card__date', 1)->plaintext);
-            $type = $this->type;
+            $list = $this->type;
             $pregnancyWeek = (($blob = $user->find('.i-blob', 0)) !== null) ? $blob->plaintext : null;
             $kidsElement = $user->find('.b-user__kids', 0);
             if ($kidsElement !== null) {
-                $kids = array_map(function($a) {
+                $children = array_map(function($a) {
                     return array(
                         'name' => $a->find('.link-ico__text', 0)->plaintext,
                         'url' => 'http://deti.mail.ru' . $a->href,
@@ -60,14 +60,14 @@ class MailruParser extends ProxyParserThread
                     );
                 }, $kidsElement->find('a'));
                 foreach ($kidsElement->find('.b-user__kids__age') as $k => $v)
-                    $kids[$k]['age'] = $v->plaintext;
+                    $children[$k]['age'] = $v->plaintext;
             }
 
-            $attributes = compact('firstName', 'lastName', 'geo', 'url', 'email', 'gender', 'age', 'type', 'pregnancyWeek');
+            $attributes = compact('firstName', 'lastName', 'geo', 'url', 'email', 'gender', 'age', 'list', 'pregnancyWeek', 'children');
+            $model = new MailruUser();
             foreach ($attributes as $k => $v)
-                echo $k . ': ' . $v . "\n";
-
-            echo "\n";
+                $model->$k = $v;
+            $model->save();
             break;
         }
     }

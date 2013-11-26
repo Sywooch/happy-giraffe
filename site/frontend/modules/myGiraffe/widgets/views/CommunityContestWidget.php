a9c6c8ed265c3140057b60aa1dbb5455b4373d66
choojoy-work <nikita@happy-giraffe.ru>
2013-11-26 12:27:26 +0400

    trim name

--- site/frontend/modules/myGiraffe/widgets/views/CommunityContestWidget.php
+++ site/frontend/modules/myGiraffe/widgets/views/CommunityContestWidget.php
@@ -6,7 +6,7 @@
 
 <div class="b-article-conversion b-article-conversion__contest b-article-conversion__<?=$contest->cssClass?> clearfix">
     <a href="javascript:void(0)" class="a-pseudo b-article-conversion_hide" onclick="$(this).parent().hide();">Скрыть</a>
-    <div class="b-article-conversion_tx-top">Внимание! С 11 октября стартовал <br>конкурс рассказов о вашем домашнем питомце!     </div>
+    <div class="b-article-conversion_tx-top">Внимание! С 20 ноября стартовал <br>конкурс рассказов о вашем домашнем питомце!     </div>
     <div class="heading-title textalign-c clearfix"> <img src="/images/contest/club/<?=$contest->cssClass?>/small.png" alt=""><?=$contest->title?></div>
     <div class="textalign-c font-middle">
         <a href="<?=$contest->url?>">Участники конкурса (<?=$contest->contestWorksCount?>)</a>

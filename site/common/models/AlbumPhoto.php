961476e729088bed33d84b5a46a34b9a131e2432
choojoy-work <nikita@happy-giraffe.ru>
2013-11-30 10:45:49 +0400

    trim name

--- site/common/models/AlbumPhoto.php
+++ site/common/models/AlbumPhoto.php
@@ -730,7 +730,7 @@ class AlbumPhoto extends HActiveRecord
     public function getWidget($edit = false, $parentModel = null)
     {
         if (get_class(Yii::app()) == 'CConsoleApplication')
-            return Yii::app()->command->renderFile('site.frontend.albums.views._widget', array(
+            return Yii::app()->command->renderFile('site.frontend.views.albums._widget', array(
                 'model' => $this,
                 'edit' => $edit,
                 'parentModel' => $parentModel

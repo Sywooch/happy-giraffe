14b54b23233eddf781d15fed9b5d111e85231b8c
choojoy-work <nikita@happy-giraffe.ru>
2013-12-01 22:24:45 +0400

    trim name

--- site/console/commands/AlbumsCommand.php
+++ site/console/commands/AlbumsCommand.php
@@ -37,6 +37,7 @@ class AlbumsCommand extends CConsoleCommand
     {
         Yii::import('site.frontend.modules.gallery.components.*');
         Yii::import('site.frontend.modules.notifications.components.*');
+        Yii::import('site.frontend.modules.notifications.models.*');
         $criteria = new CDbCriteria();
         $criteria->order = 't.id ASC';
         $criteria->compare('t.type_id', CommunityContent::TYPE_PHOTO_POST);

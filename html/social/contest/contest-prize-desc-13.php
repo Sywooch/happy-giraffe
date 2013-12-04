077777b16fc60cdfd2c1a17486b641209a14358e
Tantalid <tantalid@mail.ru>
2013-12-04 12:35:50 +0200

    contest + fix

---
+++ html/social/contest/contest-prize-desc-13.php
@@ -0,0 +1,131 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-gray">
+	
+<div class="layout-container">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div class="layout-content margin-l0 clearfix">
+		<div class="content-cols ">
+			<div class="col-white">
+			
+				<div id="contest" class="contest contest-13">
+					<div class="section-banner">
+						<div class="button-holder">
+							<a href=""class="contest-button">Участвовать!</a>
+						</div>
+						<img src="/images/contest/banner-w1000-13.jpg" />
+					</div>
+					
+					<div class="contest-nav clearfix">
+						<ul>
+							<li><a href="">О конкурсе</a></li>
+							<li><a href="">Правила</a></li>
+							<li class="active"><a href="">Призы</a></li>
+							<li><a href="">Участники</a></li>
+						</ul>
+					</div>
+					
+					<div class="contest-sponsor">
+						<a href="http://www.neopod.ru"><img src="/images/contest/contest-sponsor-13-1.jpg" alt="" class="contest-sponsor_img"></a>
+						
+					</div>
+					
+					<div class="content-title">Вас ждут замечательные призы!</div>
+					
+					<div class="contest-prizes-desc clearfix">
+						
+						<ul>
+							<li class="clearfix">
+								<div class="l-col">
+									<div class="img">
+										<a href=""><img src="/images/prize_50.jpg" /></a>
+									</div>
+								</div>
+								<div class="desc">
+									<div class="title">
+										Детский электромобиль  <b>NeoTrike Beetle</b>
+									</div>
+									<p>NeoTrike Beetle - электромобиль с пультом управления и красивыми аппликациями на всем корпусе машинки. Дизайн автомобиля повторяет дизайн настоящего Фольцваген "Жук". NeoTrike Beetle может двигаться вперед или назад, имеет совсем несложное управление: чтобы двигаться вперед, нужно надавить на педаль газа, дабы приостановить электромобиль, довольно её отпустить. Пластмассовые колеса машинки имеют накладки из прочной резины, за счёт чего сцепление с поверхностью улучшается в разы.</p>
+									<br>
+									<p><span class="color-gray">Подробнее:</span> <a href="http://www.neopod.ru/products/neotrike_A19_green/">www.neopod.ru/products/neotrike_A19_green/</a> </p>
+								</div>
+							</li>
+							<li class="clearfix">
+								<div class="l-col">
+									<div class="img">
+										<a href=""><img src="/images/prize_51.jpg" /></a>
+									</div>
+								</div>
+								<div class="desc">
+									<div class="title">
+										Обучающий компьютер <b>Винни VTech</b> 
+									</div>
+									<p>Обучающий компьютер Винни VTech оснащен 11 обучающими программами из разных областей знаний. Он научит малыша буквам и цифрам, разовьет чувство ритма, поставит произношение и научит счету. Встроенная система вопросов закрепит полученные знания.</p>
+									<br>
+									<p><span class="color-gray">Подробнее:</span> <a href="http://www.neopod.ru/products/vtech_winnie/">www.neopod.ru/products/vtech_winnie/</a> </p>
+								</div>
+							</li>
+							<li class="clearfix">
+								<div class="l-col">
+									<div class="img">
+										<a href=""><img src="/images/prize_52.jpg" /></a>
+									</div>
+								</div>
+								<div class="desc">
+									<div class="title">
+										Детский электромобиль <b>NeoTrike Mini Mercedes Benz</b>
+									</div>
+									<p>Развивающий глобус от Vtech может помочь молодым исследователям и искателям приключений, не выходя из жилищу, осуществить кругосветное странствие. Занятный самолёт, джойстик и лупа будут его ассистентами в этом странствии. Ребенок сумеет узреть новые города и державы, он познакомится с традициями различных народов и выяснит о нашем мире большое количество нового и увлекательного. <br>
+									5 обучающих программ познакомят молодого странника с океанами, континентами, народами различных государств и наиболее знаменитыми достопримечательностями. Режим вопросов может помочь малышу укрепить познания. </p>
+									<br>
+									<p><span class="color-gray">Подробнее:</span> <a href="http://www.neopod.ru/products/vtech_globus/">www.neopod.ru/products/vtech_globus/</a> </p>
+								</div>
+							</li>
+							
+						</ul>
+						<div class="textalign-c margin-r40 clearfix">
+							<div class="be-active">
+								Будьте активны! <i class="icon"></i>
+							</div>
+							<a href=""class="contest-button margin-l150">Принять участие!</a>
+						</div>
+
+						<div class="contest-brand">
+							<div class="float-r margin-l30">
+								<a href="" class="">
+									<img src="/images/contest/contest-brand__13.jpg" alt=""> 
+								</a>
+							</div>
+							<div class="contest-brand_hold">
+								<a href="" class="">
+									<img src="/images/contest/contest-brand_logo_13.jpg" alt=""> 
+								</a><br>
+								<p><strong>Neopod</strong> работает напрямую с производителями и за счет постоянного наличия товаров обеспечивает очень быструю доставку и отличные цены.</p>
+								<p>Все товары сертифицированы и тщательно отбираются по качеству и безопасности. </p>
+								<p>Развивающие игрушки, детские ноутбуки, куклы, конструкторы из экологически чистых материалов, детский транспорт, одежда и средства гигиены для малышей, лучшие европейские производители.</p>
+								<p><strong>Все игрушки и товары для детей – в одном магазине!</strong></p>
+								<p><span class="color-gray">Подробнее:</span> <a href="http://www.neopod.ru">www.neopod.ru</a> </p>
+							</div>
+						</div>
+					</div>
+
+	            </div>
+				
+			</div>
+		</div>
+		</div>
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</body>
+</html>

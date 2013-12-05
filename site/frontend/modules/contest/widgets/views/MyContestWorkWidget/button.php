9a9a19cbb99aa41ecc49c321afe8cf24cab18d7a
choojoy-work <nikita@happy-giraffe.ru>
2013-12-05 13:42:28 +0400

    trim name

---
+++ site/frontend/modules/contest/widgets/views/MyContestWorkWidget/button.php
@@ -0,0 +1,5 @@
+<?php if (Yii::app()->user->isGuest): ?>
+    <a href="#login" class="contest-button fancy">Принять участие!</a>
+<?php else: ?>
+    <a href="<?=Yii::app()->controller->createUrl('/contest/default/statement', array('id' => $this->contestId))?>" class="contest-button fancy">Принять участие!</a>
+<?php endif; ?>

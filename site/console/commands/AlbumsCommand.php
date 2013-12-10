d1439063bab220105b6b18c144beb832c2492d40
choojoy-work <nikita@happy-giraffe.ru>
2013-12-10 18:13:18 +0400

    trim name

--- site/console/commands/AlbumsCommand.php
+++ site/console/commands/AlbumsCommand.php
@@ -88,6 +88,7 @@ class AlbumsCommand extends CConsoleCommand
         Yii::import('site.frontend.modules.notifications.models.*');
         Yii::import('site.frontend.modules.scores.components.*');
         Yii::import('site.frontend.modules.scores.models.*');
+        Yii::import('site.frontend.modules.scores.models.input.*');
         $criteria = new CDbCriteria();
         $criteria->order = 't.id DESC';
         $dp = new CActiveDataProvider('Album', array('criteria' => $criteria));

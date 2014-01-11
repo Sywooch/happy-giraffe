6572e31cf12771df73ffd39e58d08f5603d70c92
choojoy-work <nikita@happy-giraffe.ru>
2014-01-11 16:22:37 +0400

    adfox remove

--- site/frontend/modules/community/views/default/club.php
+++ site/frontend/modules/community/views/default/club.php
@@ -56,7 +56,7 @@ Yii::app()->clientScript->registerPackage('ko_community');
 
         <?php if (!Yii::app()->user->isGuest && count($this->club->communities) == 1):?>
             <div class="clearfix margin-r20 margin-b20 js-community-subscription" data-bind="visible: active">
-                <a href="<?= $this->createUrl('/blog/default/form', array('type' => 1, 'club_id' => $this->club->communities[0]->id)) ?>"
+                <a href="<?=true ? 'javascript:void(0)' : $this->createUrl('/blog/default/form', array('type' => 1, 'club_id' => $this->club->communities[0]->id)) ?>"
                    class="btn-blue btn-h46 float-r fancy-top">Добавить в клуб</a>
             </div>
         <?php endif ?>

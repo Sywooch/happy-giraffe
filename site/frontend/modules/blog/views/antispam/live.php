f68bfb3c96827d2c034a64e0447961aa6e2121f9
choojoy-work <nikita@happy-giraffe.ru>
2014-01-13 12:42:19 +0400

    architecture

--- site/frontend/modules/blog/views/antispam/live.php
+++ site/frontend/modules/blog/views/antispam/live.php
@@ -2,7 +2,6 @@
 $this->widget('zii.widgets.CMenu', array(
     'items' => array(
         array('label' => 'Прямой эфир', 'url' => array('index')),
-        array('label' => 'ТОП50', 'url' => array('index', 'type' => AntispamController::TYPE_TOP50)),
         array('label' => 'От старых пользователей', 'url' => array('index', 'type' => AntispamController::TYPE_OLDUSERS)),
     ),
 ));

bd23644f487f7c891dfea8002585eda074d1c63c
choojoy-work <nikita@happy-giraffe.ru>
2014-01-14 17:48:47 +0400

    architecture

---
+++ site/frontend/modules/antispam/components/AntispamStatusManager.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 14/01/14
+ * Time: 16:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AntispamStatusManager
+{
+    const STATUS_UNDEFINED = 0;
+    const STATUS_WHITE = 1;
+    const STATUS_BLACK = 2;
+    const STATUS_SPAMER = 3;
+
+    static function setUserStatus($user, $status)
+    {
+        return true;
+    }
+
+    static function getUserStatus($user)
+    {
+
+    }
+}

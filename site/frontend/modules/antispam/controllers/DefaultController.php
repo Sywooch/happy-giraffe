14ddc7a87b6fd899d081c8050a83effaee79f91c
choojoy-work <nikita@happy-giraffe.ru>
2014-01-15 16:52:31 +0400

    architecture

--- site/frontend/modules/antispam/controllers/DefaultController.php
+++ site/frontend/modules/antispam/controllers/DefaultController.php
@@ -6,4 +6,46 @@ class DefaultController extends HController
 	{
 
 	}
+
+    public function actionRemoved($entity)
+    {
+
+    }
+
+    public function actionQuestionable($entity)
+    {
+
+    }
+
+    public function actionAnalysis($userId, $checkId)
+    {
+
+    }
+
+    public function actionMarkGood($checkId)
+    {
+        $this->changeStatus($checkId, AntispamCheck::STATUS_GOOD);
+    }
+
+    public function actionMarkDelete($checkId)
+    {
+        $this->changeStatus($checkId, AntispamCheck::STATUS_BAD);
+    }
+
+    public function actionMarkQuestionable($checkId)
+    {
+        $this->changeStatus($checkId, AntispamCheck::STATUS_QUESTIONABLE);
+    }
+
+    public function actionMarkPending($checkId)
+    {
+        $this->changeStatus($checkId, AntispamCheck::STATUS_PENDING);
+    }
+
+    protected function changeStatus($checkId, $newStatus)
+    {
+        $check = AntispamCheck::model()->findByPk($checkId);
+        $success = $check->changeStatus($newStatus);
+        echo CJSON::encode(array('success' => $success));
+    }
 }

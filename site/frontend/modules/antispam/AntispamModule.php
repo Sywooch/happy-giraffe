7315eeb3570de60997b8db20861f6c9b00a1d42d
choojoy-work <nikita@happy-giraffe.ru>
2014-01-17 13:48:59 +0400

    architecture

--- site/frontend/modules/antispam/AntispamModule.php
+++ site/frontend/modules/antispam/AntispamModule.php
@@ -11,6 +11,7 @@ class AntispamModule extends CWebModule
 		$this->setImport(array(
 			'antispam.models.*',
 			'antispam.components.*',
+            'antispam.widgets.*',
 		));
 	}
 
--- site/frontend/modules/antispam/views/default/_check.php
+++ site/frontend/modules/antispam/views/default/_check.php
@@ -11,10 +11,6 @@
             <?php $this->renderPartial('site.frontend.modules.blog.views.default.view', array('data' => $data->relatedModel, 'full' => false)); ?>
         <?php endif; ?>
     </div>
-    <div class="antispam_control">
-        <div class="margin-b5 clearfix"><a class="ico-del margin-r5"></a><a href="" class="ava ava__small"><span class="ico-status"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" class="ava_img"/></a>
-        </div>
-        <div class="color-gray font-sx">Сегодня  23:15</div>
-    </div>
+    <?php $this->widget('MarkWidget', array('check' => $data)); ?>
 </div>
 <!-- /antispam_i-->

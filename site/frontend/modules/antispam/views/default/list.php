3c0fc9661054b32b8250ec98eee1fe2b5d192a45
choojoy-work <nikita@happy-giraffe.ru>
2014-01-17 11:23:50 +0400

    architecture

--- site/frontend/modules/antispam/views/default/list.php
+++ site/frontend/modules/antispam/views/default/list.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @var CActiveDataProvider $dp
+ */
+?>
+
 <div class="page-col">
     <div class="page-col_top">
         <div class="page-col_t"><a href="" class="page-col_t-a active">Записи 54</a><a href="" class="page-col_t-a">Комментарии 165</a><a href="" class="page-col_t-a">Фотографии 65</a></div>
@@ -5,92 +11,15 @@
     <div class="page-col_cont page-col_cont__gray">
         <!-- antispam-->
         <div class="antispam">
-            <!-- antispam_i-->
-            <div class="antispam_i clearfix">
-                <div class="antispam_cont">
-                    <!-- b-article-->
-                    <article class="b-article clearfix">
-                        <div class="float-l">
-                            <div class="like-control like-control__small-indent clearfix"><a href="" class="ava"><span class="ico-status"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" class="ava_img"/></a>
-                            </div>
-                            <div class="like-control clearfix"><a href="" title="Нравится" class="like-control_ico like-control_ico__like powertip">865</a>
-                                <div class="position-rel"><a href="" title="Репост" class="like-control_ico like-control_ico__repost powertip">5</a></div>
-                                <div class="position-rel"><a href="" title="В избранное" class="favorites-control_a active powertip">123865</a></div>
-                            </div>
-                        </div>
-                        <div class="b-article_cont clearfix">
-                            <div class="b-article_cont-tale"></div>
-                            <div class="b-article_header clearfix">
-                                <div class="meta-gray"><a href="" class="meta-gray_comment"><span class="ico-comment ico-comment__gray"></span><span class="meta-gray_tx">35</span></a>
-                                    <div class="meta-gray_view"><span class="ico-view ico-view__gray"></span><span class="meta-gray_tx">305</span></div>
-                                </div>
-                                <div class="float-l"><a href="" class="b-article_author">Ангелина Богоявленская</a><span class="b-article_date">Сегодня 13:25</span></div>
-                            </div>
-                            <h2 class="b-article_t"><a href="" class="b-article_t-a">Самое лучшее утро - просыпаюсь, а ты рядом</a></h2>
-                            <div class="b-article_in clearfix">
-                                <div class="wysiwyg-content clearfix">
-                                    <p>Недавно посмотрел фильм "Убить Дракона" снятый в 1988 году с Абдуловым в главной роли. По мотивам пьесы Евгения Шварца «Дракон».</p>
-                                    <div class="b-article_in-img"><a href=""><img alt="" src="http://img.dev.happy-giraffe.ru/thumbs/580x/16534/f464362269821285b5b2aa67855267da.jpeg"/></a></div>
-                                </div>
-                            </div>
-                            <div class="textalign-r"><a href="" class="b-article_more">Смотреть далее</a></div>
-                        </div>
-                    </article>
-                    <!-- /b-article-->
-                </div>
-                <div class="antispam_control">
-                    <div class="margin-b5 clearfix"><a class="ico-del margin-r5"></a><a href="" class="ava ava__small"><span class="ico-status"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" class="ava_img"/></a>
-                    </div>
-                    <div class="color-gray font-sx">Сегодня  23:15</div>
-                </div>
-            </div>
-            <!-- /antispam_i-->
-            <!-- antispam_i-->
-            <div class="antispam_i clearfix">
-                <div class="antispam_cont">
-                    <!-- b-article-->
-                    <article class="b-article b-article__photopost clearfix">
-                        <div class="float-l">
-                            <div class="like-control like-control__small-indent clearfix"><a href="" class="ava"><span class="ico-status"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" class="ava_img"/></a>
-                            </div>
-                            <div class="like-control clearfix"><a href="" title="Нравится" class="like-control_ico like-control_ico__like powertip">865</a>
-                                <div class="position-rel"><a href="" title="Репост" class="like-control_ico like-control_ico__repost powertip">5</a></div>
-                                <div class="position-rel"><a href="" title="В избранное" class="favorites-control_a active powertip">123865</a></div>
-                            </div>
-                        </div>
-                        <div class="b-article_cont clearfix">
-                            <div class="b-article_cont-tale"></div>
-                            <div class="b-article_header clearfix">
-                                <div class="meta-gray"><a href="" class="meta-gray_comment"><span class="ico-comment ico-comment__white"></span><span class="meta-gray_tx color-gray-light">35</span></a>
-                                    <div class="meta-gray_view"><span class="ico-view ico-view__white"></span><span class="meta-gray_tx color-gray-light">305</span></div>
-                                </div>
-                                <div class="float-l"><a href="" class="b-article_author">Ангелин Богоявленский</a><span class="b-article_date">Сегодня 13:25</span></div>
-                            </div>
-                            <h2 class="b-article_t"><a href="" class="b-article_t-a">Как менялась мода на прически</a></h2>
-                            <div class="b-article_in clearfix">
-                                <div class="wysiwyg-content clearfix">
-                                    <p>Недавно посмотрел фильм "Убить Дракона" снятый в 1988 году с Абдуловым в главной роли. По мотивам пьесы Евгения Шварца «Дракон».</p>
-                                </div>
-                                <div class="photo-grid clearfix">
-                                    <div class="photo-grid_row clearfix">
-                                        <div class="photo-grid_i"><img src="http://img.happy-giraffe.ru/thumbs/580x/220231/d2b4677dc8cdaf8012799a3f252afdc8.jpg" alt="Как поддерживают себя в форме и худеют в разных странах"/>
-                                            <div class="photo-grid_tip">20 фото</div><span class="ico-play-big"></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="textalign-r"><a href="" class="b-article_more b-article_more__white">Смотреть далее</a></div>
-                        </div>
-                    </article>
-                    <!-- /b-article-->
-                </div>
-                <div class="antispam_control">
-                    <div class="margin-b5 clearfix"><a class="ico-del margin-r5"></a><a href="" class="ava ava__small"><span class="ico-status"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg" class="ava_img"/></a>
-                    </div>
-                    <div class="color-gray font-sx">Сегодня  23:15</div>
-                </div>
-            </div>
-            <!-- /antispam_i-->
+            <?php
+            $this->widget('zii.widgets.CListView', array(
+                'dataProvider' => $dp,
+                'itemView' => '_check',
+                'summaryText' => '',
+                'cssFile' => false,
+            ));
+            ?>
+            <?php if (false): ?>
             <!-- antispam_i-->
             <div class="antispam_i clearfix">
                 <div class="antispam_cont">
@@ -115,6 +44,7 @@
                 </div>
                 <div class="antispam_control"></div>
             </div>
+            <?php endif; ?>
         </div>
         <!-- /antispam-->
     </div>

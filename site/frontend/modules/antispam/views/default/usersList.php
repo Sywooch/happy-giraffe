2cac485cc7508c448779f2c48a864311fe0b7a2f
choojoy-work <nikita@happy-giraffe.ru>
2014-01-20 14:10:15 +0400

    architecture

--- site/frontend/modules/antispam/views/default/usersList.php
+++ site/frontend/modules/antispam/views/default/usersList.php
@@ -42,31 +42,6 @@
             </ul>
         </div>
         <!-- antispam_i-->
-        <?php if (false): ?>
-            <div class="antispam_i clearfix">
-                <div class="antispam_cont">
-                    <!-- paginator-->
-                    <div class="yiipagination margin-l100">
-                        <div class="pager">
-                            <ul class="yiiPager">
-                                <li class="page"><a href="">1</a></li>
-                                <li class="page"><a href="">2</a></li>
-                                <li class="page selected"><a href="">3</a></li>
-                                <li class="page"><a href="">4</a></li>
-                                <li class="page"><a href="">5</a></li>
-                                <li class="page"><a href="">6</a></li>
-                                <li class="page"><a href="">7</a></li>
-                                <li class="page"><a href="">8</a></li>
-                                <li class="page"><a href="">9</a></li>
-                                <li class="page"><a href="">10</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- /paginator-->
-                </div>
-                <div class="antispam_control"></div>
-            </div>
-        <?php endif; ?>
     </div>
     <!-- /antispam-->
     </div>

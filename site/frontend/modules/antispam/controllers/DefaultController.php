123c7169f89d0d5ccbfe8fd8d25a4b2bd4d49945
choojoy-work <nikita@happy-giraffe.ru>
2014-01-20 12:17:44 +0400

    architecture

--- site/frontend/modules/antispam/controllers/DefaultController.php
+++ site/frontend/modules/antispam/controllers/DefaultController.php
@@ -38,9 +38,9 @@ class DefaultController extends HController
     public function actionList($status, $entity = AntispamCheck::ENTITY_POSTS)
     {
         $counts = array(
-            AntispamCheck::ENTITY_POSTS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_POSTS)->count(),
-            AntispamCheck::ENTITY_COMMENTS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_COMMENTS)->count(),
-            AntispamCheck::ENTITY_PHOTOS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_PHOTOS)->count(),
+            AntispamCheck::ENTITY_POSTS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_POSTS)->status($status)->count(),
+            AntispamCheck::ENTITY_COMMENTS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_COMMENTS)->status($status)->count(),
+            AntispamCheck::ENTITY_PHOTOS => AntispamCheck::model()->entity(AntispamCheck::ENTITY_PHOTOS)->status($status)->count(),
         );
         $dp = AntispamCheck::getDp($entity, $status);
         $this->render('list', compact('dp', 'status', 'counts'));

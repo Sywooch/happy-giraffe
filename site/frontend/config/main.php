a76ba16c07df4b91df103c7a69db2ef8598f15a8
choojoy-work <nikita@happy-giraffe.ru>
2014-01-21 12:04:22 +0400

    architecture

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -353,7 +353,7 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-        'releaseId' => 49,
+        'releaseId' => 50,
         'valentinesAlbum' => '41340',
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',

3b904cdb2be1b49b88d13507d435f60cfe57abfb
Kirill <kirill@happy-giraffe.ru>
2014-01-21 17:11:43 +0900

    Главное меню + небольшой фикс

--- site/frontend/modules/messaging/views/layouts/new_main.php
+++ site/frontend/modules/messaging/views/layouts/new_main.php
@@ -37,6 +37,18 @@
           <!-- logo-->
           <div class="logo"><a title="Веселый жираф - сайт для всей семьи" href="" class="logo_i">Веселый жираф - сайт для всей семьи</a><span class="logo_slogan">САЙТ ДЛЯ ВСЕЙ СЕМЬИ</span></div>
           <!-- /logo-->
+          <!-- header-menu-->
+          <div class="header-menu">
+            <ul class="header-menu_ul clearfix">
+              <li class="header-menu_li"><a href="" class="header-menu_a"><span class="header-menu_ico header-menu_ico__giraffe"></span><span class="header-menu_tx">Мой Жираф</span></a></li>
+              <li class="header-menu_li"><a href="" class="header-menu_a"><span class="header-menu_ico header-menu_ico__im"></span><span class="header-menu_tx">Диалоги</span><span class="header-menu_count">25456</span></a></li>
+              <li class="header-menu_li"><a href="" class="header-menu_a"><span class="header-menu_ico header-menu_ico__friend"></span><span class="header-menu_tx">Друзья</span><span class="header-menu_count">2</span></a></li>
+              <li class="header-menu_li active"><a href="" class="header-menu_a"><span class="header-menu_ico header-menu_ico__notice"></span><span class="header-menu_tx">Сигналы</span><span class="header-menu_count">2</span></a></li>
+              <li class="header-menu_li"><a href="" class="header-menu_a"><span class="header-menu_ico header-menu_ico__award"></span><span class="header-menu_tx">Успехи</span></a></li>
+              <li class="header-menu_li header-menu_li__dropin"><a href="/my" class="header-menu_a"><span href="" class="ava ava__middle ava__female"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/avatars/12963/ava/8d26a6f4dbae0536f8dbec37c0b5e5f8.jpg" class="ava_img"/></span><span class="header-menu_tx">Я<span class="header-menu_i-arrow"></span></span></a></li>
+            </ul>
+          </div>
+          <!-- /header-menu-->
         </div>
       </div>
       <!-- /header-->

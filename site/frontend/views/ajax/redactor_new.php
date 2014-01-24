b830caa15f72b146268316cf2cef6a141d285565
choojoy-work <nikita@happy-giraffe.ru>
2014-01-24 19:00:25 +0400

    Визивиг в редакторе

--- site/frontend/views/ajax/redactor_new.php
+++ site/frontend/views/ajax/redactor_new.php
@@ -139,47 +139,4 @@
             <!-- /ko -->
         </div>
     </div>
-
-    <!-- ko stopBinding: true -->
-    <div class="redactor-popup redactor-popup_b-photo display-n" id="redactor-popup_b-photo">
-        <a href="" class="redactor-popup_close ico-close3 powertip" data-bind="click: close"></a>
-        <div class="redactor-popup_tale"></div>
-        <div class="redactor-popup_t">Загрузите фото</div>
-
-        <?php $this->renderPartial('application.views.upload_image_popup'); ?>
-
-        <div class="textalign-c margin-t15">
-            <a href="javascript:;" class="btn-gray-light btn-medium margin-r10" onclick="$(this).parents('.redactor-popup').addClass('display-n');">Отменить</a>
-            <a href="" class="btn-green btn-medium" data-bind="click: add, css: {'btn-inactive': !upload().addActive()}">Добавить фото</a>
-        </div>
-    </div>
-    <!-- /ko -->
-
-    <!-- ko stopBinding: true -->
-    <div class="redactor-popup redactor-popup_b-link display-n" id="redactor-popup_b-link">
-        <a class="redactor-popup_close ico-close3 powertip" title="Закрыть" data-bind="click: close"></a>
-        <div class="redactor-popup_tale"></div>
-        <div class="redactor-popup_t">Ссылка</div>
-
-        <div class="redactor-popup_holder-blue">
-            <div class="margin-b10 clearfix">
-                <label class="redactor-popup_label" for="">Отображаемый текст</label>
-                <div class="float-l w-350">
-                    <input type="text" placeholder="Введите текст" class="itx-simple" data-bind="value: text">
-                </div>
-            </div>
-            <div class="clearfix">
-                <label class="redactor-popup_label" for="">Ссылка на</label>
-                <div class="float-l w-350">
-                    <input type="text" placeholder="Введите URL страницы" class="itx-simple" data-bind="value: url, hasfocus: url().length == 0">
-                </div>
-            </div>
-        </div>
-
-        <div class="textalign-c margin-t15">
-            <a class="btn-gray-light btn-medium margin-r10" data-bind="click: close">Отменить</a>
-            <a class="btn-green btn-medium" data-bind="click: processLink">Добавить ссылку</a>
-        </div>
-    </div>
-    <!-- /ko -->
 </div>

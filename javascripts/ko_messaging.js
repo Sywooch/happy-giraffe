a7013061ff53302896e784cc5e227f99427c6b01
choojoy-work <nikita@happy-giraffe.ru>
2014-01-28 20:58:32 +0400

    Merge remote-tracking branch 'origin/develop' into develop
    Conflicts:
    	site/frontend/www-submodule

--- javascripts/ko_messaging.js
+++ javascripts/ko_messaging.js
@@ -104,6 +104,17 @@ MessagingUser.prototype = {
                 });
             };
             comet.addEvent(4010, 'friendAdded');
+            Comet.prototype.requestDeclined = function(result, id) {
+                ko.utils.arrayForEach(self.objects, function(obj) {
+                    if (obj.id == result.fromId) {
+                        obj.hasIncomingRequest(false);
+                    }
+                    if (obj.id == result.toId) {
+                        obj.hasOutgoingRequest(false);
+                    }
+                });
+            };
+            comet.addEvent(4000, 'requestDeclined');
             Comet.prototype.avatarUploaded = function(result, id) {
                 ko.utils.arrayForEach(self.objects, function(obj) {
                     if (obj.id == result.userId) {
@@ -201,7 +212,8 @@ function MessagingUser(viewModel, model) {
     }
 
     self.friendsAccept = function() {
-        $.post('/friends/requests/accept/', { fromId : self.id });
+        alert('123');
+        //$.post('/friends/requests/accept/', { fromId : self.id });
     }
 
     self.friendsDecline = function() {

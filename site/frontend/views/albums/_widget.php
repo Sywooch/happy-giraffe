15a0dd9be673f58a4012cd304a8d0c955dd40619
choojoy-work <nikita@happy-giraffe.ru>
2014-01-31 13:33:08 +0400

    Баги

--- site/frontend/views/albums/_widget.php
+++ site/frontend/views/albums/_widget.php
@@ -12,7 +12,7 @@ if (!$edit) echo '<!-- widget: { entity : "AlbumPhoto", entity_id : "' . $model-
 
 if (isset($parentModel) && in_array(get_class($parentModel), array('Comment', 'MessagingMessage')))
     //echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img src="' . $model->getPreviewUrl(485, 110, Image::HEIGHT) . '"></a>';
-    echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img src="' . $model->getPreviewUrl(395, 400, Image::AUTO) . '"></a>';
+    echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img width="' . $model->width . '" height="' . $model->height . '" src="' . $model->getPreviewUrl(395, 400, Image::AUTO) . '"></a>';
 else {
     if ($model->width >= 580) {
         if (!$edit)

2d2ad51fe6555e9a2698527663cc1501de5b0bfa
choojoy-work <nikita@happy-giraffe.ru>
2014-02-07 10:17:38 +0400

    Небольшой фикс

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -355,7 +355,7 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-        'releaseId' => 101,
+        'releaseId' => 102,
         'valentinesAlbum' => '41340',
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',

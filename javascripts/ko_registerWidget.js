78435d4fdf7502527819a4298305faaca92c12d9
choojoy-work <nikita@happy-giraffe.ru>
2014-02-20 16:12:34 +0400

    Директ

--- javascripts/ko_registerWidget.js
+++ javascripts/ko_registerWidget.js
@@ -1,10 +1,14 @@
 function RegisterWidgetViewModel() {
     var self = this;
 
-    self.STEP_REG1 = 0;
-    self.STEP_REG2 = 1;
-    self.STEP_EMAIL1 = 2;
-    self.STEP_EMAIL2 = 3;
+    self.STEP_REG1 = 'signupStep1';
+    self.STEP_REG2 = 'signupStep2';
+    self.STEP_EMAIL1 = 'signupEmail1';
+    self.STEP_EMAIL2 = 'signupEmail2';
 
     self.currentStep = ko.observable(self.STEP_REG1);
+
+    self.email = ko.observable('');
+    self.first_name = ko.observable('');
+    self.last_name = ko.observable('');
 }

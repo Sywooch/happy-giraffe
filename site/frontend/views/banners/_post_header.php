88064be8864ea5fccf76d20d7a6c3c26ee0daa71
choojoy-work <nikita@happy-giraffe.ru>
2014-02-20 20:52:10 +0400

    Директ

--- site/frontend/views/banners/_post_header.php
+++ site/frontend/views/banners/_post_header.php
@@ -5,37 +5,33 @@
 ?>
 
 <div style="margin: 15px;">
-    <?php if ($data->getIsFromBlog()): ?>
-
-    <?php else: ?>
-        <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
-        <div id="yandex_ad_post_header"></div>
-        <script type="text/javascript">
-            (function(w, d, n, s, t) {
-                w[n] = w[n] || [];
-                w[n].push(function() {
-                    Ya.Direct.insertInto(87026, "yandex_ad_post_header", {
-                        ad_format: "direct",
-                        font_size: 1.1,
-                        type: "flat",
-                        limit: 1,
-                        title_font_size: 3,
-                        site_bg_color: "FFFFFF",
-                        title_color: "289FD7",
-                        url_color: "006600",
-                        text_color: "333333",
-                        hover_color: "2289BA",
-                        favicon: true,
-                        no_sitelinks: true
-                    });
+    <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
+    <div id="yandex_ad_post_header"></div>
+    <script type="text/javascript">
+        (function(w, d, n, s, t) {
+            w[n] = w[n] || [];
+            w[n].push(function() {
+                Ya.Direct.insertInto(87026, "yandex_ad_post_header", {
+                    ad_format: "direct",
+                    font_size: 1.1,
+                    type: "flat",
+                    limit: 1,
+                    title_font_size: 3,
+                    site_bg_color: "FFFFFF",
+                    title_color: "289FD7",
+                    url_color: "006600",
+                    text_color: "333333",
+                    hover_color: "2289BA",
+                    favicon: true,
+                    no_sitelinks: true
                 });
-                t = d.getElementsByTagName('head')[0];
-                s = d.createElement("script");
-                s.type = "text/javascript";
-                s.src = "http://an.yandex.ru/system/context.js";
-                s.setAttribute("async", "true");
-                t.insertBefore(s, t.firstChild);
-            })(window, document, "yandex_context_callbacks");
-        </script>
-    <?php endif; ?>
+            });
+            t = d.getElementsByTagName('head')[0];
+            s = d.createElement("script");
+            s.type = "text/javascript";
+            s.src = "http://an.yandex.ru/system/context.js";
+            s.setAttribute("async", "true");
+            t.insertBefore(s, t.firstChild);
+        })(window, document, "yandex_context_callbacks");
+    </script>
 </div>

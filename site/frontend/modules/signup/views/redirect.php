f78e8c314356a9d7fb6a409426dfd38189073cd2
choojoy-work <nikita@happy-giraffe.ru>
2014-02-24 09:59:13 +0400

    Директ

--- site/frontend/modules/signup/views/redirect.php
+++ site/frontend/modules/signup/views/redirect.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <script type="text/javascript">
+            document.domain = document.domain;
             window.close();
             var registerVm = window.opener.registerVm;
             <?php foreach ($this->params['attributes'] as $k => $v): ?>

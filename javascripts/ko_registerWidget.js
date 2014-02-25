e47ded93d9f8c39ca157b8d9af24234cf0bd858c
choojoy-work <nikita@happy-giraffe.ru>
2014-02-25 18:29:12 +0400

    Директ

--- javascripts/ko_registerWidget.js
+++ javascripts/ko_registerWidget.js
@@ -6,40 +6,28 @@ function RegisterWidgetViewModel(data, form) {
 
     self.social = ko.observable(false);
     self.currentStep = ko.observable(self.STEP_REG1);
-    self.currentStep.subscribe(function(val) {
-        if (val == self.STEP_REG2) {
-            setTimeout(function() {
-                var settings = form.data("settings");
-                $.each(settings.attributes, function () {
-                    this.status = 2;
-                });
-                form.data("settings", settings);
-
-                $.fn.yiiactiveform.validate(form, function (data) {
-                    $.each(settings.attributes, function () {
-                        $.fn.yiiactiveform.updateInput(this, data, form);
-                    });
-                    $('.popup-sign_attr:has(.success):visible').hide();
-                    form.triggerHandler('reset');
-                });
-            }, 1);
-        }
-    });
 
     self.id = ko.observable('');
-    self.email = ko.observable('');
-    self.first_name = ko.observable('');
-    self.last_name = ko.observable('');
+    self.email = new RegisterUserAttribute('');
+    self.first_name = new RegisterUserAttribute('');
+    self.last_name = new RegisterUserAttribute('');
     self.country = ko.observable('');
     self.city = ko.observable('');
-    self.birthday_year = ko.observable('');
-    self.birthday_month = ko.observable('');
-    self.birthday_day = ko.observable('');
-    self.gender = ko.observable('');
+    self.birthday_year = new RegisterUserAttribute('');
+    self.birthday_month = new RegisterUserAttribute('');
+    self.birthday_day = new RegisterUserAttribute('');
+    self.gender = new RegisterUserAttribute('');
 
     self.daysRange = DateRange.days();
     self.monthesRange = DateRange.months();
     self.yearsRange = DateRange.years(data.minYear, data.maxYear);
+}
+
+function RegisterUserAttribute(value)
+{
+    var self = this;
+    self.val = ko.observable(value);
+    self.show = ko.observable(true);
 }
 
 function LoginWidgetViewModel() {

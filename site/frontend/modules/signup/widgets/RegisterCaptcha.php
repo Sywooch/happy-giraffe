f8848de2490c40a20417c5493f8248617fe2e068
choojoy-work <nikita@happy-giraffe.ru>
2014-02-26 13:54:51 +0400

    Активация

---
+++ site/frontend/modules/signup/widgets/RegisterCaptcha.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 26/02/14
+ * Time: 13:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RegisterCaptcha extends CCaptcha
+{
+    public $imageOptions = array(
+        'class' => 'popup-sign_capcha',
+    );
+
+    public $buttonOptions = array(
+        'class' => 'popup-sign_tx-help',
+    );
+
+    public $buttonLabel = '<div class="ico-refresh"></div>Обновить';
+
+    public $clickableImage = true;
+}
--- site/frontend/modules/signup/widgets/views/email2.php
+++ site/frontend/modules/signup/widgets/views/email2.php
@@ -1,13 +1,36 @@
+<?php
+/**
+ * @var CActiveForm $form
+ * @var ResendConfirmForm $model
+ */
+?>
+
 <div class="popup-sign_top">
     <div class="popup-sign_t">Добро пожаловать Александр! <br> Теперь вы с Веселым Жирафом!</div>
 </div>
 <div class="popup-sign_cont">
     <div class="popup-sign_col">
+        <?php $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'registerFormStep1',
+            'action' => array('/signup/register/resend'),
+            'enableAjaxValidation' => true,
+            'enableClientValidation' => true,
+            'clientOptions' => array(
+                'inputContainer' => 'div.inp-valid',
+                'validateOnSubmit' => true,
+                'afterValidate' => 'js:afterValidateStep1',
+            ),
+        )); ?>
         <div class="popup-sign_row margin-t5">
             <div class="inp-valid inp-valid__abs success">
                 <input type="text" placeholder="E-mail" class="itx-gray popup-sign_itx">
+                <?php $form->textField($model, 'email', array(
+                    'placeholder' => 'E-mail',
+                    'class' => 'itx-gray popup-sign_itx',
+                    'data-bind' => 'value: email.val',
+                ))?>
                 <div class="inp-valid_error">
-                    <div class="errorMessage">Не заполнено поле</div>
+                    <?=$form->error($model, 'email')?>
                 </div>
                 <div class="inp-valid_success inp-valid_success__ico-check"></div>
             </div>
@@ -18,15 +41,15 @@
         </div>
         <div class="popup-sign_row margin-b30">
             <div class="popup-sign_capcha-hold">
-                <div class="margin-b3"><img src="/images/captcha.png" class="popup-sign_capcha">
-                </div><a class="popup-sign_tx-help">
-                    <div class="ico-refresh"></div>Обновить</a>
+                <?php $this->widget('RegisterCaptcha'); ?>
             </div>
             <div class="popup-sign_capcha-inp">
                 <div class="inp-valid inp-valid__abs error">
-                    <input type="text" class="itx-gray popup-sign_itx">
+                    <?php $form->textField($model, 'verifyCode', array(
+                        'class' => 'itx-gray popup-sign_itx',
+                    ))?>
                     <div class="inp-valid_error">
-                        <div class="errorMessage">Неправильный код проверки</div>
+                        <?=$form->error($model, 'verifyCode')?>
                     </div>
                     <div class="inp-valid_success inp-valid_success__ico-check"></div>
                 </div>
@@ -35,10 +58,11 @@
         </div>
         <div class="popup-sign_row">
             <div class="display-ib textalign-c">
-                <div class="btn-green-simple btn-l margin-b5">Отправить письмо еще раз</div>
+                <button class="btn-green-simple btn-l margin-b5">Отправить письмо еще раз</button>
                 <div class="popup-sign_tx-help">Письмо должно прийти в течении 10 мин.</div>
             </div>
         </div>
+        <?php $this->endWidget(); ?>
     </div>
     <div class="popup-sign_col-wide-r">
         <div class="margin-b15">Письмо может попасть в папку "Спам" вашего почтового ящика. Если это так, пожалуйста, отметьте его как <strong class="color-gray-darken">"Не спам"</strong>.</div><a href="" target="_blank" class="popup-sign_a-big">Открыть почту Mail.ru</a>

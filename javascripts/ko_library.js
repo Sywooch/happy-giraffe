d99ee9922ccf4a0ba10db8e400fea588e733eb6f
Kirill <kirill@happy-giraffe.ru>
2014-02-28 11:32:12 +0900

    select2 binding added

--- javascripts/ko_library.js
+++ javascripts/ko_library.js
@@ -106,6 +106,19 @@ ko.bindingHandlers.chosen =
     }
 };
 
+ko.bindingHandlers.select2 = {
+    init: function(element, valueAccessor) {
+        $(element).select2(valueAccessor());
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $(element).select2('destroy');
+        });
+    },
+    update: function(element) {
+        $(element).trigger('change');
+    }
+};
+
 ko.bindingHandlers.selectize = {
     init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
 

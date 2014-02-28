8b9e0d410f36c6e1028aafacda7eab74c0a8eb5d
Kirill <kirill@happy-giraffe.ru>
2014-02-28 14:39:14 +0900

    Включение ajax-фильтра

--- site/frontend/modules/friends/controllers/SearchController.php
+++ site/frontend/modules/friends/controllers/SearchController.php
@@ -14,7 +14,7 @@ class SearchController extends HController
     {
         return array(
             'accessControl',
-           // 'ajaxOnly - index',
+            'ajaxOnly - index',
         );
     }
 

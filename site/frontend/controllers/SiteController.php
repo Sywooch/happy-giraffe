e44bed1c13f80450e6506a89b66d8bd413ca00d6
choojoy-work <nikita@happy-giraffe.ru>
2014-03-12 13:17:38 +0400

    wysiwyg

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -82,14 +82,14 @@ class SiteController extends HController
 	/**
 	 * @sitemap changefreq=daily
 	 */
-	public function actionIndex()
+	public function actionIndex($openLogin = false)
 	{
         if (! Yii::app()->user->isGuest)
             $this->redirect(array('myGiraffe/default/index', 'type' => 1));
 
         $this->layout = '//layouts/common';
 		$this->pageTitle = 'Веселый Жираф - сайт для всей семьи';
-        $this->render('home');
+        $this->render('home', compact('openLogin'));
 	}
 
 	/**

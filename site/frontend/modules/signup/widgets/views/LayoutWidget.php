49f361671dbac33e404f4807be639a8cc2266b9c
choojoy-work <nikita@happy-giraffe.ru>
2014-03-13 12:44:06 +0400

    wysiwyg

--- site/frontend/modules/signup/widgets/views/LayoutWidget.php
+++ site/frontend/modules/signup/widgets/views/LayoutWidget.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="/new/javascript/common.js?r=113"></script>
+<script type="text/javascript" src="/javascripts/base64.js?r=113"></script>
+
 <?php $this->widget('signup.widgets.RegisterWidget'); ?>
 <?php $this->widget('signup.widgets.LoginWidget'); ?>
 <?php $this->widget('signup.widgets.PasswordRecoveryWidget'); ?>

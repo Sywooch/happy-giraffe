86d62299cfe8fdf2693b56ccef0c8e07129559bb
choojoy-work <nikita@happy-giraffe.ru>
2014-03-13 12:42:16 +0400

    wysiwyg

--- site/frontend/modules/signup/widgets/LayoutWidget.php
+++ site/frontend/modules/signup/widgets/LayoutWidget.php
@@ -8,8 +8,8 @@ class LayoutWidget extends CWidget
     public function run()
     {
         Yii::app()->clientScript->registerPackage('ko_registerWidget');
-        Yii::app()->clientScript->registerScriptFile('new/javascript/common.js');
-        Yii::app()->clientScript->registerScriptFile('javascripts/base64.js');
+        Yii::app()->clientScript->registerScriptFile('/new/javascript/common.js');
+        Yii::app()->clientScript->registerScriptFile('/javascripts/base64.js');
         $this->render('LayoutWidget');
     }
 }

a8b69b7c452bf0e313b350327279dafe9fa4cbe6
choojoy-work <nikita@happy-giraffe.ru>
2014-03-13 12:19:02 +0400

    wysiwyg

--- site/common/models/CommunityContent.php
+++ site/common/models/CommunityContent.php
@@ -1003,7 +1003,7 @@ class CommunityContent extends HActiveRecord
     {
         return array(
             'id' => $this->id,
-            'attached' => (bool) $this->pinned,
+            'attached' => false, //(bool) $this->pinned,
             'privacy' => (int) $this->privacy,
             'created' => strtotime($this->created),
         );

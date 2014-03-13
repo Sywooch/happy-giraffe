fdc311a5abe3181516dd3fd973093df3bde90852
choojoy-work <nikita@happy-giraffe.ru>
2014-03-13 16:51:48 +0400

    wysiwyg

--- site/frontend/modules/signup/widgets/views/LoginWidget.php
+++ site/frontend/modules/signup/widgets/views/LoginWidget.php
@@ -36,7 +36,7 @@
                             <?=$form->textField($model, 'email', array(
                                 'placeholder' => 'E-mail',
                                 'class' => 'itx-gray popup-sign_itx',
-                                'data-bind' => 'value: email, valueUpdate: "input"',
+                                'data-bind' => 'value: email, valueUpdate: \'input\'',
                             ))?>
                             <div class="inp-valid_error">
                                 <div class="inp-valid_error-tx"><?=$form->error($model, 'email'); ?></div>

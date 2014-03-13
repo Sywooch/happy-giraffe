1308ce21075d845c1d55410e315a0e9d2678eda9
choojoy-work <nikita@happy-giraffe.ru>
2014-03-13 11:20:07 +0400

    wysiwyg

--- site/frontend/modules/signup/controllers/RegisterController.php
+++ site/frontend/modules/signup/controllers/RegisterController.php
@@ -117,7 +117,7 @@ class RegisterController extends HController
         $identity = new ActivationUserIdentity($activationCode);
         if ($identity->authenticate()) {
             Yii::app()->user->login($identity, 3600*24*30);
-            $this->redirect(array('/signup/reguster/clubs/'));
+            $this->redirect(array('/signup/register/clubs/'));
         } elseif ($identity->errorCode == ActivationUserIdentity::ERROR_CODE_USED)
             $this->redirect($url);
         else

58cef3139e1ccfdb1fd4835ad2a847c6b1ef79f4
Kirill <kirill@happy-giraffe.ru>
2014-03-13 13:00:06 +0900

    Некоторые фиксы

--- site/frontend/config/clientScript.php
+++ site/frontend/config/clientScript.php
@@ -2,12 +2,9 @@
 
 return array(
     'class' => 'application.components.ClientScript',
-    'amdFile' => '/new/javascript/modules/require2.1.11-jquery.1.10.2.js',
+    'amdFile' => '/new/javascript/modules/require2.1.11-jquery1.10.2.js',
     'amd' => array(
         'baseUrl' => '/new/javascript/modules',
-        'paths' => array(
-            'jquery' => '/new/javascript/jquery-1.10.2.min'
-        ),
         'shim' => array(
         ),
     ),

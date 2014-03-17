b3965869444a4a0c286146928ab91e1a2974d484
choojoy-work <nikita@happy-giraffe.ru>
2014-03-17 10:54:53 +0400

    wysiwyg

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -111,7 +111,7 @@ class SiteController extends HController
             {
                 $viewFile = Yii::app()->getSystemViewPath() . DIRECTORY_SEPARATOR . 'error' . $error['code'] . '.php';
                 if (is_file($viewFile))
-                    $this->render('//system/' . $error['code'], $error);
+                    Yii::app()->errorHandler->render('//system/' . 'error' . $error['code'], $error);
             }
 	    }
 	}

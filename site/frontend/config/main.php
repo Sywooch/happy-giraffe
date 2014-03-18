66f1ecfb7c155844a2f69bd64d5176a93d5219ab
choojoy-work <nikita@happy-giraffe.ru>
2014-03-18 11:45:48 +0400

    wysiwyg

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -283,11 +283,11 @@ return array(
 //                    'levels'=>'error, warning',
 //                    'emails'=>'nikita@happy-giraffe.ru',
 //                ),
-//				array(
-//					'class'=>'CEmailLogRoute',
-//					'levels'=>'error, warning',
-//					'emails'=>'choojoy.work@gmail.com',
-//				),
+				array(
+					'class'=>'CEmailLogRoute',
+					'levels'=>'info',
+					'emails'=>'nikita@happy-giraffe.ru',
+				),
 //				array(
 //					'class'=>'CWebLogRoute',
 //					'categories'=>'system.db.CDbCommand',

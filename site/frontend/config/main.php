4ad63e7765cf8fc87c803b124472bf6a6d723224
choojoy-work <nikita@happy-giraffe.ru>
2014-03-18 14:05:42 +0400

    wysiwyg

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -365,7 +365,7 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-        'releaseId' => 120,
+        'releaseId' => 121,
         'valentinesAlbum' => '41340',
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',

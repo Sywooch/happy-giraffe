72c7e4c566dc454d6b75f7afbd2d4f4986480c0a
choojoy-work <nikita@happy-giraffe.ru>
2014-03-19 14:53:03 +0400

    Автоматический сброс кеша браузера при обновлении

--- site/frontend/components/ClientScript.php
+++ site/frontend/components/ClientScript.php
@@ -32,54 +32,6 @@ class ClientScript extends CClientScript
         return false;
     }
 
-//    public function registerScriptFile($url,$position=null,array $htmlOptions=array())
-//    {
-//        return parent::registerScriptFile($this->addReleaseId($url), $position, $htmlOptions);
-//    }
-//
-//    public function registerCssFile($url,$media='')
-//    {
-//        return parent::registerCssFile($this->addReleaseId($url), $media);
-//    }
-//
-//    public function renderCoreScripts()
-//    {
-//        if($this->coreScripts===null)
-//            return;
-//        $cssFiles=array();
-//        $jsFiles=array();
-//        foreach($this->coreScripts as $name=>$package)
-//        {
-//            $baseUrl=$this->getPackageBaseUrl($name);
-//            if(!empty($package['js']))
-//            {
-//                foreach($package['js'] as $js)
-//                    $jsFiles[$baseUrl.'/'.$this->addReleaseId($js)]=$baseUrl.'/'.$js;
-//            }
-//            if(!empty($package['css']))
-//            {
-//                foreach($package['css'] as $css)
-//                    $cssFiles[$baseUrl.'/'.$css]='';
-//            }
-//        }
-//        // merge in place
-//        if($cssFiles!==array())
-//        {
-//            foreach($this->cssFiles as $cssFile=>$media)
-//                $cssFiles[$cssFile]=$media;
-//            $this->cssFiles=$cssFiles;
-//        }
-//        if($jsFiles!==array())
-//        {
-//            if(isset($this->scriptFiles[$this->coreScriptPosition]))
-//            {
-//                foreach($this->scriptFiles[$this->coreScriptPosition] as $url => $value)
-//                    $jsFiles[$url]=$value;
-//            }
-//            $this->scriptFiles[$this->coreScriptPosition]=$jsFiles;
-//        }
-//    }
-
     protected function addReleaseId($url)
     {
         $r = $this->getReleaseId();

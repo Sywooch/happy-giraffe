f3414f35ae3e4bb30e423e4d2c8e8144a4c243fe
choojoy-work <nikita@happy-giraffe.ru>
2014-03-20 17:21:46 +0400

    #96

--- site/frontend/modules/signup/widgets/views/RegisterWidget.php
+++ site/frontend/modules/signup/widgets/views/RegisterWidget.php
@@ -28,6 +28,7 @@
         </div>
         <div class="popup-sign_b clearfix margin-t20" data-bind="visible: currentStep() == STEP_PHOTO">
             <div class="float-r">
+                <div class="btn-gray-simple btn-l" data-bind="click: cancelAvatar">Отменить</div>
                 <div class="btn-green-simple btn-l" data-bind="click: saveAvatar">Сохранить</div>
             </div>
         </div>

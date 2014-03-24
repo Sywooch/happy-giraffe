acde68885e3c60454d226c35d754acac2296ed4e
choojoy-work <nikita@happy-giraffe.ru>
2014-03-24 13:45:40 +0400

    #96

--- site/frontend/modules/signal/models/CommentatorWork.php
+++ site/frontend/modules/signal/models/CommentatorWork.php
@@ -306,7 +306,7 @@ class CommentatorWork extends EMongoDocument
      */
     public function incCommentsCount($next = true)
     {
-        $this->$this->getOrCreateDay("Y-m-d")->incComments($this);
+        $this->getOrCreateDay("Y-m-d")->incComments($this);
         if ($next) {
             if ($this->calculateNextComment()) {
                 return true;

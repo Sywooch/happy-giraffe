a511f22507d00ba83d0f80d47ff1e38472df9dad
Andrii <tantalid@mail.ru>
2014-03-27 14:50:55 +0200

    rambler couter ico footer

--- block/global/footer.php
+++ block/global/footer.php
@@ -8,7 +8,11 @@
 			<li class="footer-list_li"><a href="" class="footer-list_a">Задать вопрос</a></li>
 			<li class="footer-list_li"><a href="" class="footer-list_a">Реклама </a></li>
 			<li class="footer-list_li"><a href="" class="footer-list_a">Контакты </a></li>
-			<li class="footer-list_li"><a href="" class="footer-list_a">Партнер "Рамблера"</a></li>
+			<li class="footer-list_li">
+				<a href="" class="footer-list_a">Партнер "Рамблера"</a>
+
+				<span id="counter-rambler" class="footer-list_rambler-count"><a href="http://top100.rambler.ru/home?id=2892367" target="_blank"><img src="http://counter.rambler.ru/top100.scn?2892367&amp;rn=1382511841&amp;v=0.3&amp;bs=1680x983&amp;ce=1&amp;rf=http%3A%2F%2Fwww.happy-giraffe.ru%2Fmy%2F&amp;en=UTF-8&amp;pt=%D0%92%D0%B5%D1%81%D0%B5%D0%BB%D1%8B%D0%B9%20%D0%96%D0%B8%D1%80%D0%B0%D1%84%20-%20%D1%81%D0%B0%D0%B9%D1%82%20%D0%B4%D0%BB%D1%8F%20%D0%B2%D1%81%D0%B5%D0%B9%20%D1%81%D0%B5%D0%BC%D1%8C%D0%B8&amp;cd=32-bit&amp;sr=1680x1050&amp;la=ru&amp;ja=1&amp;acn=Mozilla&amp;an=Netscape&amp;pl=Win32&amp;tz=-120&amp;fv=12.0%20r0&amp;sv&amp;le=0" title="Rambler's Top100" alt="Rambler's Top100" border="0"></a></span>
+			</li>
 		</ul>
 		<ul class="footer-ul-bold">
 			<li class="footer-ul-bold_li"><a href="" class="footer-ul-bold_a">Беременность и дети</a></li>

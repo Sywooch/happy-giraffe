3a81af49d15c7346bce7ac5a985b6221f0c66bd4
choojoy-work <nikita@happy-giraffe.ru>
2014-04-08 15:48:23 +0400

    Формирование ссылок

--- site/frontend/modules/mail/components/senders/MailSender.php
+++ site/frontend/modules/mail/components/senders/MailSender.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: mikita
- * Date: 04/04/14
- * Time: 10:40
- * To change this template use File | Settings | File Templates.
+ * Рассыльщик
+ *
+ * Отвечает главным образом за то, КОМУ отправлять письма, собирает необходимые для генерации данные и передает их
+ * в модель сообщения
  */
 
 abstract class MailSender extends CComponent
@@ -12,9 +11,18 @@ abstract class MailSender extends CComponent
     const FROM_NAME = 'Весёлый Жираф';
     const FROM_EMAIL = 'noreply@happy-giraffe.ru';
 
+    /**
+     * Отправить рассылку всем пользователям, для которых она может быть отправлена
+     *
+     * @return mixed
+     */
     abstract public function sendAll();
-    abstract public function send($userId);
 
+    /**
+     * Отправляет сообщение, в случае успеха помечает модель доставки как успешно отправленную
+     *
+     * @param MailMessage $message
+     */
     protected function sendInternal(MailMessage $message)
     {
         if (ElasticEmail::send($message->user->email, $message->getSubject(), $message->getBody(), self::FROM_EMAIL, self::FROM_NAME)) {

ee3153243076eb57a311056a4db979f9214f892a
choojoy-work <nikita@happy-giraffe.ru>
2014-04-10 16:03:48 +0400

    Рассылка о новых непрочитанных сообщениях

--- site/frontend/modules/messaging/models/MessagingMessage.php
+++ site/frontend/modules/messaging/models/MessagingMessage.php
@@ -258,6 +258,9 @@ class MessagingMessage extends HActiveRecord
         $scopes['orderDesc'] = array(
             'order' => $this->getTableAlias(true) . '.`created` DESC',
         );
+        $scopes['orderAsc'] = array(
+            'order' => $this->getTableAlias(true) . '.`created` ASC',
+        );
 
         return $scopes;
     }

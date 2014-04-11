06a0e1d22a6e24360660194f2720163495e8c570
choojoy-work <nikita@happy-giraffe.ru>
2014-04-11 12:57:03 +0400

    Ежедневная рассылка

---
+++ site/common/components/JabberLogRoute.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 11/04/14
+ * Time: 12:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+class JabberLogRoute extends CLogRoute
+{
+    public $room;
+    public $name;
+    public $file;
+
+    protected function processLogs($logs)
+    {
+        foreach ($logs as $log) {
+            $this->sendMessage($log[0]);
+        }
+    }
+
+    protected function sendMessage($message)
+    {
+        $str = 'echo "message"  | sendxmpp -f /etc/sendxmpprc_www -r Warner --chatroom hg-tech-warnings@conference.jabber.ru'
+
+        $params = array(
+            '-f' => $this->file,
+            '-r' => $this->name,
+            '--chatroom' => $this->room,
+        );
+
+        $str = 'echo "' . $message . '" | sendxmpp ';
+        foreach ($params as $k => $v) {
+            $str .= $k . ' ' . $v;
+        }
+
+        shell_exec($str);
+    }
+}

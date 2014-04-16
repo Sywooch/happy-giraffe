5fb0814d14d2ccfff2d870546aaf89647b6b9ae7
choojoy-work <nikita@happy-giraffe.ru>
2014-04-16 15:43:36 +0400

    Ежедневная рассылка

--- site/frontend/modules/mail/tpls/daily.php
+++ site/frontend/modules/mail/tpls/daily.php
@@ -8,10 +8,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <!-- script for develop livereload -->
-        <script src="//localhost:35729/livereload.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Заголовок</title>
+        <title><?php echo $message->getSubject(); ?></title>
 
     </head>
     <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="background: #f0f0f0;">

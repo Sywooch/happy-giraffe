7ff6288080ad7581531374483309ee0f7cfa3653
choojoy-work <nikita@happy-giraffe.ru>
2014-04-22 18:07:40 +0400

    Ежедневная рассылка

--- site/frontend/components/WebUser.php
+++ site/frontend/components/WebUser.php
@@ -30,13 +30,13 @@ class WebUser extends CWebUser
         Yii::app()->request->cookies['not_guest'] = new CHttpCookie('not_guest', '1', array('expire' => time() + 3600*24*100));
     }
 
-    protected function beforeLogout()
+    protected function afterLogout()
     {
         $model = $this->getModel();
-        $model->offline();
-        unset(Yii::app()->request->cookies['not_guest']);
 
-        return parent::beforeLogout();
+        $model->offline();
+
+        unset(Yii::app()->request->cookies['not_guest']);
     }
 
     protected function beforeLogin($id, $states, $fromCookie)

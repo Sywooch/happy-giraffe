2bf1ccfc3af307ee98088fefc4f8cbe73d09b258
choojoy-work <nikita@happy-giraffe.ru>
2014-04-26 12:04:47 +0400

    Правки от Андрея

--- site/frontend/modules/blog/views/default/_likes.php
+++ site/frontend/modules/blog/views/default/_likes.php
@@ -1,3 +1,10 @@
+<?php
+/**
+ * @var HController $this
+ * @var CommunityContent $data
+ */
+?>
+
 <?php if (! Yii::app()->user->checkAccess('tester')): ?>
     <noindex>
         <?php $this->widget('site.frontend.widgets.socialLike.SocialLikeWidget', array(
@@ -12,7 +19,6 @@
     </noindex>
 <?php else: ?>
     <div style="text-align: center; margin-bottom: 10px;">
-        <script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
-        <div class="yashare-auto-init" data-yashareL10n="ru" data-yashareQuickServices="vkontakte,odnoklassniki,facebook,twitter,moimir,gplus" data-yashareTheme="counter"></div>
+        <?php $this->widget('application.widgets.yandexShareWidget.YandexShareWidget', array('model' => $data)); ?>
     </div>
 <?php endif; ?>
---
+++ site/frontend/widgets/yandexShareWidget/YandexShareWidget.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mikita
+ * Date: 26/04/14
+ * Time: 11:19
+ * To change this template use File | Settings | File Templates.
+ */
+
+class YandexShareWidget extends CWidget
+{
+    /**
+     * @var CommunityContent
+     */
+    public $model;
+
+    public function run()
+    {
+        $this->registerScript();
+        $json = CJSON::encode(array(
+            'element' => $this->getElementId(),
+            'theme' => 'counter',
+            'elementStyle' => array(
+                'quickServices' => array(
+                    'vkontakte',
+                    'odnoklassniki',
+                    'facebook',
+                    'twitter',
+                    'moimir',
+                    'gplus',
+                ),
+            ),
+            'link' => $this->model->getUrl(false, true),
+            'title' => $this->model->title,
+            'description' => $this->model->getContentText(),
+            'image' => $this->getImage(),
+        ));
+        $this->render('view', compact('json'));
+    }
+
+    public function getDefaultImage()
+    {
+        return Yii::app()->request->hostInfo . '/new/images/base/logo.png';
+    }
+
+    public function getElementId()
+    {
+        return 'ya_share' . $this->model->id;
+    }
+
+    public function registerScript()
+    {
+        /** @var ClientScript $cs */
+        $cs = Yii::app()->clientScript;
+        $cs->registerScriptFile('//yandex.st/share/share.js');
+    }
+
+    protected function getImage()
+    {
+        $photo = $this->model->getPhoto();
+        $avatar = $this->model->author->getAvatarUrl(200);
+        if ($photo !== null) {
+            return $photo->getOriginalUrl();
+        } elseif ($avatar !== false) {
+            return $avatar;
+        } else {
+            return $this->getDefaultImage();
+        }
+    }
+}

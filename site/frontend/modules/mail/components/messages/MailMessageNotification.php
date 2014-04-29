876fd6667c8da98abe458e57a4eac5cbccc3b62a
choojoy-work <nikita@happy-giraffe.ru>
2014-04-29 09:31:59 +0400

    Рассылки по уведомлениям

---
+++ site/frontend/modules/mail/components/messages/MailMessageNotification.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 28/04/14
+ * Time: 17:04
+ */
+
+class MailMessageNotification extends MailMessage
+{
+    const COMMENT_LENGTH = 80;
+
+    public $type = 'notificationDiscuss';
+
+    /**
+     * @property HActiveRecord $model
+     */
+    public $model;
+
+    /**
+     * @property Comment[] $commentsToShow
+     */
+    public $commentsToShow;
+
+    /**
+     * @property int $commentsCount
+     */
+    public $totalCommentsCount;
+
+    public function getSubject()
+    {
+        return time();
+    }
+
+    public function getMoreCount()
+    {
+        return $this->totalCommentsCount - count($this->commentsToShow);
+    }
+} 
--- site/frontend/modules/mail/components/messages/MailMessageNotificationDiscuss.php
+++ site/frontend/modules/mail/components/messages/MailMessageNotificationDiscuss.php
@@ -7,36 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class MailMessageNotificationDiscuss extends MailMessage
+class MailMessageNotificationDiscuss extends MailMessageNotification
 {
-    const COMMENT_LENGTH = 80;
-
-    public $type = 'notificationDiscuss';
-
-    /**
-     * @property CommunityContent $model
-     */
-    public $model;
-
-    /**
-     * @property Comment[] $commentsToShow
-     */
-    public $commentsToShow;
-
-    /**
-     * @property int $commentsCount
-     */
-    public $totalCommentsCount;
-
-    public function getSubject()
-    {
-        return time();
-    }
-
-    public function getMoreCount()
-    {
-        return $this->totalCommentsCount - count($this->commentsToShow);
-    }
-
 
 }

f193eb6a0196f50ca43c15656b3696f7f5462320
choojoy-work <nikita@happy-giraffe.ru>
2014-04-29 16:15:33 +0400

    Рассылки по уведомлениям

--- site/frontend/modules/mail/tpls/notification/notificationComment.php
+++ site/frontend/modules/mail/tpls/notification/notificationComment.php
@@ -13,8 +13,8 @@
         <table style="" cellpadding="0" cellspacing="0" border="0" width="100%">
             <tr>
                 <td style="font-size: 16px;color: #111111">
-                    У вас 15 новых комментариев  <br />
-                    к  вашей записи <a href=""  style="color:#3482e2;text-decoration:underline;">Крем-сыворотка</a>
+                    У вас <?php echo $message->totalCommentsCount; ?> новых комментариев  <br />
+                    к  вашей записи <a href="<?php echo $message->createUrl($message->model->getUrl(true, true)); ?>"  style="color:#3482e2;text-decoration:underline;"><?php echo $message->model->title; ?></a>
                 </td>
             </tr>
         </table>

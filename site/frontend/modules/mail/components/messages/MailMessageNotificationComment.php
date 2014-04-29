a9d8900b34bd0d8cfe0557851b9c56a644b95e88
choojoy-work <nikita@happy-giraffe.ru>
2014-04-29 14:52:20 +0400

    Рассылки по уведомлениям

---
+++ site/frontend/modules/mail/components/messages/MailMessageNotificationComment.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 29/04/14
+ * Time: 14:51
+ */
+
+class MailMessageNotificationComment extends MailMessageNotification
+{
+    public $type = 'notificationComment';
+} 

592f58a9f9aea1449ac125ed2d92bbf5673185d2
choojoy-work <nikita@happy-giraffe.ru>
2014-04-29 17:10:27 +0400

    Рассылки по уведомлениям

--- site/frontend/modules/mail/components/messages/MailMessage.php
+++ site/frontend/modules/mail/components/messages/MailMessage.php
@@ -117,10 +117,15 @@ abstract class MailMessage extends CComponent
         /**
          * @var CConsoleApplication $app
          */
-        $app = Yii::app();
-        $data['message'] = $this;
-        $runner = $app instanceof CConsoleApplication ? $app->getCommandRunner()->getCommand() : $app->controller;
-        $output = $runner->renderFile($this->getTemplateInternal($file), $data, true);
+        try {
+            $app = Yii::app();
+            $data['message'] = $this;
+            $runner = $app instanceof CConsoleApplication ? $app->getCommandRunner()->getCommand() : $app->controller;
+            $output = $runner->renderFile($this->getTemplateInternal($file), $data, true);
+        } catch (Exception $e) {
+            ob_end_clean();
+            echo $e->getMessage();
+        }
         if ($return) {
             return $output;
         } else {

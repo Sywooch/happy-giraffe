a6a87931ceefc27946f6cddf3d453de179746acf
Andrii <tantalid@mail.ru>
2014-05-06 11:52:56 +0300

    branding zirtec

---
+++ html/branding/zirtec/user_blog2.php
@@ -0,0 +1,354 @@
+﻿<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-gray theme theme__adfox theme__zirtec" style="">
+<!--AdFox START-->
+
+<div id="AdFox_banner_761862"><div style="height:0px;"></div><div id="adfoxBanner610364" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 1460px; z-index: -1;"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="100%" height="1460" id="1000x320_shapka" align="middle">
+				<param name="movie" value="1000x320_shapka.swf">
+				<param name="quality" value="high">
+				<param name="bgcolor" value="#ffffff">
+				<param name="play" value="true">
+				<param name="loop" value="true">
+				<param name="wmode" value="transparent">
+				<param name="scale" value="noScale">
+				<param name="menu" value="true">
+				<param name="devicefont" value="false">
+				<param name="salign" value="">
+				<param name="allowScriptAccess" value="sameDomain">
+				<!--[if !IE]>-->
+				<object type="application/x-shockwave-flash" data="http://adflasher.ru/www/banners/Zyrtec/4/1000x320_shapka.swf" align="middle"   width="100%" height="1460">
+					<param name="movie" value="1000x320_shapka.swf">
+					<param name="quality" value="high">
+					<param name="bgcolor" value="#ffffff">
+					<param name="play" value="true">
+					<param name="loop" value="true">
+					<param name="wmode" value="transparent">
+					<param name="scale" value="noScale">
+					<param name="menu" value="true">
+					<param name="devicefont" value="false">
+					<param name="salign" value="">
+
+					<param name="allowScriptAccess" value="sameDomain">
+				<!--<![endif]-->
+					<a href="http://www.adobe.com/go/getflash">
+						<img src="./1000x320_shapka_files/get_flash_player.gif" alt="Get Adobe Flash player">
+					</a>
+				<!--[if !IE]>-->
+				</object>
+				<!--<![endif]-->
+			</object>
+
+
+		</div></div>
+<div class="layout-w1">
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-fix.php'; ?>
+	<div class="layout-container">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+		<div class="layout-wrapper">
+		<div class="layout-wrapper_hold">
+			
+			<div class="layout-content clearfix">
+			<div class="content-cols clearfix">
+				<div class="col-1">
+					<div class="b-ava-large">
+						<div class="b-ava-large_ava-hold clearfix">
+							<a href="" class="ava large female">
+							</a>
+							<span class="b-ava-large_online">На сайте</span>
+							<a class="b-ava-large_bubble b-ava-large_bubble__dialog powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__mail"></span>
+								<span class="b-ava-large_bubble-tx">+5</span>
+							</a>
+							<a class="b-ava-large_bubble b-ava-large_bubble__photo powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__photo"></span>
+								<span class="b-ava-large_bubble-tx">+50</span>
+							</a>
+							<a class="b-ava-large_bubble b-ava-large_bubble__blog powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__blog"></span>
+								<span class="b-ava-large_bubble-tx">+999</span>
+							</a>
+							<a class="b-ava-large_bubble b-ava-large_bubble__friend-add-onhover powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__friend-add"></span>
+							</a>
+						</div>
+						<div class="textalign-c">
+							<a class="b-ava-large_a" href="">Александр Богоявленский</a>
+							<span class="font-smallest color-gray"> 39 лет</span>
+						</div>
+						<div class="b-ava-large_location">
+							<div class="flag flag-ua" title="Украина"></div>
+	               				Украина, Астраханская область
+						</div>
+					</div>
+					
+					<div class="aside-blog-desc">
+						<div class="aside-blog-desc_tx">
+							Пусть кто-то будет безумно красивый Пусть кто-то будет богаче и круче Мне наплевать, ведь ты - мой любимый.	Ты навсегда. Ты мой. Самый лучший.
+						</div>
+					</div>
+					
+					<div class="readers2">
+						<a href="" class="btn-green btn-medium">Подписаться</a>
+						<ul class="readers2_ul clearfix">
+							<li class="readers2_li clearfix">
+								<a href="" class="ava female small">
+									<span class="icon-status status-online"></span>
+									<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+								</a>
+							</li>
+							<li class="readers2_li clearfix">
+								<a href="" class="ava female small">
+									<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+								</a>
+							</li>
+							<li class="readers2_li clearfix">
+								<a href="" class="ava female small">
+									<span class="icon-status status-online"></span>
+									<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+								</a>
+							</li>
+							<li class="readers2_li clearfix">
+								<a href="" class="ava female small">
+								</a>
+							</li>
+							<li class="readers2_li clearfix">
+								<a href="" class="ava female small">
+									<span class="icon-status status-online"></span>
+									<img alt="" src="http://img.happy-giraffe.ru/avatars/34531/small/2fd2c2d5e773c3cb8a36ce231fbc6ce0.JPG">
+								</a>
+							</li>
+							<li class="readers2_li clearfix">
+								<a href="" class="ava male small">
+									<span class="icon-status status-online"></span>
+								</a>
+							</li>
+						</ul>
+						<div class="clearfix">
+							<div class="readers2_count">Все подписчики (129)</div>
+						</div>
+					</div>
+					
+					<div class="menu-simple">
+						<ul class="menu-simple_ul">
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Обо всем</a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Свадьба - прекрасный миг</a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Прикольное видео </a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Школа восточного танца  </a>
+							</li>
+							<li class="menu-simple_li active">
+								<a href="" class="menu-simple_a">Мой мужчина </a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Детские передачи </a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Свадьбы </a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Кормление ребенка </a>
+							</li>
+							<li class="menu-simple_li">
+								<a href="" class="menu-simple_a">Воспитание детей </a>
+							</li>
+						</ul>
+					</div>
+					
+				</div>
+				<div class="col-23-middle">
+					<div class="blog-title-b">
+						<div class="blog-title-b_img-hold">
+							<img src="/images/example/w720-h128.jpg" alt="" class="blog-title-b_img">
+						</div>
+						<h1 class="blog-title-b_t">Блог о красивой любви </h1>
+					</div>
+					
+					<div class="b-article clearfix">
+						<div class="float-l">
+							<div class="like-control like-control__small-indent clearfix">
+								<a href="" class="ava male">
+									<span class="icon-status status-online"></span>
+									<img alt="" src="http://img.happy-giraffe.ru/avatars/10/ava/f4e804935991c0792e91c174e83f3877.jpg">
+								</a>
+							</div>
+							<script>
+							$(window).load(function() {
+								/*
+								block - элемент, что фиксируется
+								elementStop - до какого элемента фиксируется
+								blockIndent - отступ
+								*/
+								function likeControlFixedInBlock(block, inBlock, blockIndent) {
+
+									var block = $(block);
+									var blockTop = block.offset().top;
+									var blockHeight = block.outerHeight();
+									/*
+										var stopTop = $(elementStop).offset().top;
+										var blockStopTop = stopTop - blockTop - blockHeight - blockIndent;
+									*/
+									var inBlock = $(inBlock);
+									var blockStopBottom = inBlock.offset().top + inBlock.outerHeight();
+
+									if (blockStopBottom-blockTop-blockHeight-blockIndent > 20) {
+
+										$(window).scroll(function() {
+									        var windowScrollTop = $(window).scrollTop();
+									        if (
+									        	windowScrollTop > blockTop-blockIndent && 
+									        	windowScrollTop + blockHeight < blockStopBottom - blockIndent
+									        	) {
+									        	block.css({
+													'position': 'fixed', 
+													'top'     : blockIndent+'px'
+												});
+									        } else {
+
+												block.css({
+													'position': 'relative', 
+													'top'     : 'auto'
+												});
+
+									        	if (windowScrollTop + blockHeight > blockStopBottom - blockIndent) {
+									        		block.css({ 
+									        			/* 89 - высота блока над едущими лайками */
+														'top'     : inBlock.outerHeight() - blockHeight - 89 
+													});
+									        	}
+									        }
+									    });
+									}
+								}
+
+								likeControlFixedInBlock('.js-like-control', '.b-article', 80);
+							})
+							</script>
+							<div class="js-like-control" >
+								<div class="like-control like-control__self clearfix">
+									<div class="position-rel">
+										<a href="" class="like-control_ico like-control_ico__like">865</a>
+										<div class="favorites-add-popup favorites-add-popup__right">
+											<div class="">Вы не можете ставить "Нравиться" к своей записи</div>
+										</div>
+									</div>
+									<div class="position-rel">
+										<a href="" class="like-control_ico like-control_ico__repost">5</a>
+										<div class="favorites-add-popup favorites-add-popup__right">
+											<div class="">Вы не можете делать репост своей записи</div>
+										</div>
+									</div>
+									<div class="position-rel">
+										<a href="" class="favorites-control_a">123865</a>
+										<div class="favorites-add-popup favorites-add-popup__right">
+											<div class="">Вы не можете добавить свою запись в Избранное</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="b-article_cont clearfix">
+							<div class="b-article_cont-tale"></div>
+							<div class="b-article_header clearfix">
+								<div class="meta-gray">
+									<a href="" class="meta-gray_comment">
+										<span class="ico-comment ico-comment__gray"></span>
+										<span class="meta-gray_tx">35</span>
+									</a>
+									<div class="meta-gray_view">
+										<span class="ico-view ico-view__gray"></span>
+										<span class="meta-gray_tx">305</span>
+									</div>
+								</div>
+								<div class="float-l">
+									<a href="" class="b-article_author">Ангелина Богоявленская</a>
+									<span class="font-smallest color-gray">Сегодня 13:25</span>
+								</div>
+							</div>
+							<h2 class="b-article_t">
+								Самое лучшее утро - просыпаюсь, а ты рядом
+							</h2>
+							<div class="b-article_in clearfix">
+								<div class="wysiwyg-content clearfix">
+									<p>	Недавно посмотрел фильм "Убить Дракона" снятый в 1988 году с Абдуловым в главной роли. По мотивам пьесы Евгения Шварца «Дракон».</p>
+								</div>
+							</div>
+
+							<div class="custom-likes-b">
+								<div class="custom-likes-b_slogan">Поделитесь с друзьями!</div>
+								<a href="" class="custom-like">
+									<span class="custom-like_icon odnoklassniki"></span>
+									<span class="custom-like_value">0</span>
+								</a>
+								<a href="" class="custom-like">
+									<span class="custom-like_icon vkontakte"></span>
+									<span class="custom-like_value">1900</span>
+								</a>
+							
+								<a href="" class="custom-like">
+									<span class="custom-like_icon facebook"></span>
+									<span class="custom-like_value">150</span>
+								</a>
+							
+								<a href="" class="custom-like">
+									<span class="custom-like_icon twitter"></span>
+									<span class="custom-like_value">10</span>
+								</a>
+							</div>
+						</div>
+					</div>
+					<!-- Для примера, так не прикручивать верстку -->
+					<div class="margin-20">
+						<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/comments-gray-empty.php'; ?>
+					</div>
+
+					<table class="article-nearby clearfix" ellpadding="0" cellspacing="0">
+						<tr>
+							<td>
+								<div class="article-nearby_hint">Предыдущая запись</div>
+							</td>
+							<td class="article-nearby_r">
+								<div class="article-nearby_hint">Следующая запись</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<a href="" class="article-nearby_a clearfix">
+									<span class="article-nearby_img-hold">
+										<img src="/images/example/w64-h61-2.jpg" alt="">
+									</span>
+									<span class="article-nearby_tx">Как приготовить Монастыпскую избу</span>
+								</a>
+							</td>
+							<td class="article-nearby_r">
+								<a href="" class="article-nearby_a clearfix">
+									<span class="article-nearby_tx">Готовим  Торт Сметанник в домашних условиях</span>
+								</a>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			</div>
+			</div>
+			
+		</div>
+	</div>
+	<div class="footer-push"></div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+
+</body>
+</html>

298e2e8275f9798535a591018a652d6223a53e3f
smaragdinus <kirill@happy-giraffe.ru>
2014-05-22 12:07:36 +0900

    Большой комит сигналов

--- new/javascript/common.js
+++ new/javascript/common.js
@@ -152,3 +152,8 @@ $(function() {
         new HgWysiwyg(this);
     };
 });
+
+function declOfNum(number, titles) {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
+}
--- new/javascript/modules/ko_notifications.js
+++ new/javascript/modules/ko_notifications.js
@@ -1,7 +1,34 @@
-define('ko_notifications', ['knockout', 'comet'], function(ko, comet) {
+define('ko_notifications', ['knockout', 'comet', 'ko_library', 'common'], function(ko, comet) {
+    var types = {
+        0: 'comment',
+        1: 'reply',
+        2: 'discuss',
+        5: 'like',
+        6: 'favorite',
+        7: 'post',
+    };
+    
+    function Notify(data) {
+        ko.utils.extend(this, data);
+        var self = this;
+        self.count = ko.observable(self.count);
+        self.visibleCount = ko.observable(self.visibleCount);
+        self.type = types[self.type];
+        self.readed = ko.observable(false);
+        self.setReaded = function() {
+            self.readed(true);
+        };
+    }
     function ViewModel(data) {
         var self = this;
-        self.notifications = ko.observableArray(data.notifications);
+        self.notifications = ko.observableArray(ko.utils.arrayMap(data.list, function(item) {
+            return new Notify(item);
+        }));
+        self.markAllAsReaded = function() {
+            ko.utils.arrayForEach(self.notifications(), function(item) {
+                item.setReaded();
+            });
+        };
     }
     
     return ViewModel;

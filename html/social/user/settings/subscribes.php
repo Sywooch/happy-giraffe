efbac3f4d17730522fca16b16ccac85bbbbc0a27
Andrii <tantalid@mail.ru>
2014-05-30 11:00:05 +0300

    натсройки рассылок

---
+++ html/social/user/settings/subscribes.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<!--[if lt IE 8]>      <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if IE 9]>         <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class=""> <!--<![endif]-->
+<head>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/head.php'; ?>
+
+</head>
+<body class="body-gray">
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/top-line-menu.php'; ?>
+	
+<div class="layout-container">
+<div class="layout-container_hold">
+	<div class="layout-wrapper">
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/header-new.php'; ?>
+			
+		<div id="content" class="layout-content clearfix">
+			
+			<div class="content-cols">
+				<div class="col-1">
+					<div class="b-ava-large">
+						<div class="b-ava-large_ava-hold clearfix">
+							<a href="" class="ava large female">
+							</a>
+							<span class="b-ava-large_online">На сайте</span>
+							<a class="b-ava-large_bubble b-ava-large_bubble__dialog powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__mail"></span>
+								<span class="b-ava-large_bubble-tx">+5</span>
+							</a>
+							<a class="b-ava-large_bubble b-ava-large_bubble__photo powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__photo"></span>
+								<span class="b-ava-large_bubble-tx">+50</span>
+							</a>
+							<a class="b-ava-large_bubble b-ava-large_bubble__blog powertip" href="">
+								<span class="b-ava-large_ico b-ava-large_ico__blog"></span>
+								<span class="b-ava-large_bubble-tx">+999</span>
+							</a>
+						</div>
+						<div class="textalign-c">
+							<a class="b-ava-large_a" href="">Александр Богоявленский</a>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-23-middle clearfix">
+	                <div class="heading-title clearfix">
+						Мои настройки 
+					</div>
+	                <div class="col-gray"> 
+						<ul class="cont-nav textalign-l">
+							<li class="cont-nav_i">
+								<a class="cont-nav_a" href="">Личные данные</a>
+							</li>
+							<li class="cont-nav_i">
+								<a class="cont-nav_a" href="">Социальные сети</a>
+							</li>
+							<li class="cont-nav_i">
+								<a class="cont-nav_a" href="">Пароль </a>
+							</li>
+							<li class="cont-nav_i active">
+								<a class="cont-nav_a" href="">Рассылки </a>
+							</li>
+						</ul>
+						
+						<div class="form-settings">
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Ежедневная рассылка о новом о сайте
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Новые сообщения в диалогах
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Новые комментарии к вашим записям, фото, видео
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Новые ответы на ваш комментарий
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Новые ответы на заданый вами вопрос
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+							<div class="form-settings_label-row clearfix">
+								<div class="form-settings_label form-settings_label__long">
+									Новые комментарии к записи в обсуждении которой вы принимали участие
+								</div>
+								<div class="display-ib">
+									<a href="" class="a-checkbox active"></a>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>  	
+		
+		<div class="footer-push"></div>
+	</div>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/block/global/footer.php'; ?>
+</div>
+</div>
+
+
+</body>
+</html>

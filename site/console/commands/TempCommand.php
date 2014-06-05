1f016342d0a72fe4a6f9ae14295fdc04a672e694
choojoy-work <nikita@happy-giraffe.ru>
2014-06-05 12:21:24 +0400

    Анонсы

--- site/console/commands/TempCommand.php
+++ site/console/commands/TempCommand.php
@@ -578,7 +578,7 @@ http://www.happy-giraffe.ru/community/22/forum/post/159657/";
         echo $doc->save();
 
         $post->updateByPk($post->id, array('text' => $doc->save()));
-        echo $post->text;
+        var_dump($post->text);
 
         die;
 

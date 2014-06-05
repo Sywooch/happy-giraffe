7ea5f0f38a3eb8c0f82d852b5834c1b32fbf755e
choojoy-work <nikita@happy-giraffe.ru>
2014-06-05 12:15:28 +0400

    Анонсы

--- site/console/commands/TempCommand.php
+++ site/console/commands/TempCommand.php
@@ -578,7 +578,7 @@ http://www.happy-giraffe.ru/community/22/forum/post/159657/";
         echo $doc->save();
 
         $post->updateByPk($post->id, array('text' => $doc->save()));
-        Yii::app()->cache->delete('CommunityVideo_' . $post->id . '_text');
+        echo Yii::app()->cache->get('CommunityVideo_' . $post->id . '_text');
 
         die;
 

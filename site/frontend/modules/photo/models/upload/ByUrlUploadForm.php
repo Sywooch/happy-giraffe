bf0d39f4d758f3ea7f988109a1a6384edba5beed
choojoy-work <nikita@happy-giraffe.ru>
2014-06-11 17:27:57 +0400

    last-modified

---
+++ site/frontend/modules/photo/models/upload/ByUrlUploadForm.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 11/06/14
+ * Time: 15:45
+ */
+
+namespace site\frontend\modules\photo\models\upload;
+
+
+class ByUrlUploadForm extends UploadForm
+{
+
+} 

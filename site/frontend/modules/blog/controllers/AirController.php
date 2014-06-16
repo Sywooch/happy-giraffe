f5fa73d51742b1c363ecc18e7f174ec0ff7751d5
choojoy-work <nikita@happy-giraffe.ru>
2014-06-16 16:02:32 +0400

    Загрузка фото

--- site/frontend/modules/blog/controllers/AirController.php
+++ site/frontend/modules/blog/controllers/AirController.php
@@ -10,12 +10,14 @@ class AirController extends HController
 {
     public function actionIndex()
     {
+        Yii::app()->clientScript->registerMetaTag('noindex', 'robots');
         $dp = new CActiveDataProvider('CommunityContent', array(
             'criteria' => array(
                 'order' => 't.created DESC',
             ),
         ));
         $onlineUsers = User::model()->findAll('online = 1 AND avatar_id IS NOT NULL');
+        $this->pageTitle = 'Прямой эфир';
         $this->render('index', compact('dp', 'onlineUsers'));
     }
 } 

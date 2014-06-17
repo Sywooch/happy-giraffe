769c4b2fdb19a380b36b8bd1144219e82c182cd6
choojoy-work <nikita@happy-giraffe.ru>
2014-06-17 19:42:10 +0400

    Прямой эфир

--- site/frontend/modules/seo/components/YandexOriginalText.php
+++ site/frontend/modules/seo/components/YandexOriginalText.php
@@ -102,7 +102,7 @@ class YandexOriginalText
 
         $responseXml = new \SimpleXMLElement($response);
 
-        var_dump($responseXml);
+        var_dump($response);
         die;
 
         $model->added = new \CDbExpression('NOW()');

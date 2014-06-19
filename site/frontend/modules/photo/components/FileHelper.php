c3dde14153de586ce28df2e13f614cfc28f91b86
choojoy-work <nikita@happy-giraffe.ru>
2014-06-19 13:19:32 +0400

    Прямой эфир

--- site/frontend/modules/photo/components/FileHelper.php
+++ site/frontend/modules/photo/components/FileHelper.php
@@ -29,23 +29,4 @@ class FileHelper
             return '';
         }
     }
-
-    public static function getMimeType($path)
-    {
-        if (function_exists('finfo_open'))
-        {
-            $mimeType = false;
-            if ($info = finfo_open(defined('FILEINFO_MIME_TYPE') ? FILEINFO_MIME_TYPE : FILEINFO_MIME)) {
-                $mimeType = finfo_file($info, $path);
-            }
-        }
-        elseif (function_exists('mime_content_type')) {
-            $mimeType = mime_content_type($path);
-        }
-        else {
-            throw new \CException(\Yii::t('yii','In order to use MIME-type validation provided by CFileValidator fileinfo PECL extension should be installed.'));
-        }
-
-        return $mimeType;
-    }
 } 
--- site/frontend/modules/photo/models/PhotoCreate.php
+++ site/frontend/modules/photo/models/PhotoCreate.php
@@ -1,78 +1,91 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mikita
- * Date: 09/06/14
- * Time: 18:57
+ * Модель для создания моделей фотографий
+ *
+ * В эту модель вынесена логика валидации и создания новой модели фотографии
  */
 
 namespace site\frontend\modules\photo\models;
 
 
+use site\frontend\modules\photo\components\FileHelper;
 use site\frontend\modules\photo\components\PathManager;
 
-class PhotoCreate extends Photo
+class PhotoCreate extends \CFormModel
 {
     const FS_NAME_LEVELS = 2;
     const FS_NAME_SYMBOLS_PER_LEVEL = 2;
 
     public $path;
     public $original_name;
+    public $width;
+    public $height;
+    public $type;
 
     protected $imageSize;
 
     public function __construct($path, $originalName)
     {
-        $this->path = $path;
         $this->original_name = $originalName;
+        $this->path = $path;
+
+        try {
+            $this->imageSize = getimagesize($this->path);
+            $this->width = $this->imageSize[0];
+            $this->height = $this->imageSize[1];
+            $this->type = $this->imageSize[2];
+        } catch (\Exception $e) {
+            $this->addError($this->path, 'Некорректный файл изображения');
+        }
     }
 
     public function rules()
     {
         return array(
-            array('path', 'canGetImageSize'),
+            array('type', 'validType')
         );
     }
 
-    public function canGetImageSize($attribute, $params)
+    public function save()
+    {
+        $photo = new Photo();
+        $photo->setAttributes($this->attributes);
+        $photo->fs_name = $this->getFsName();
+        if (! copy($this->path, $photo->getImagePath())) {
+            throw new \CException('Невозможно скопировать файл');
+        }
+        $photo->save();
+        return $photo;
+    }
+
+    protected function setImageSize()
     {
         try {
-            $this->imageSize = getimagesize($this->$attribute);
+            $this->imageSize = getimagesize($this->path);
+            $this->width = $this->imageSize[0];
+            $this->height = $this->imageSize[1];
+            $this->type = $this->imageSize[2];
         } catch (\Exception $e) {
-            $this->addError('path', 'Некорректный файл изображения');
+            $this->addError($this->path, 'Некорректный файл изображения');
         }
     }
 
-    public function imageType($attribute, $params)
+    public function validType($attribute)
     {
-
-    }
-
-    public function create()
-    {
-        try {
-            $imageSize = getimagesize($this->path);
-            $this->width = $imageSize[0];
-            $this->height = $imageSize[1];
-            $type = $imageSize[2];
-            if (! in_array($type, array(IMAGETYPE_JPEG, IMAGETYPE_GIF, IMAGETYPE_PNG))) {
-                $this->addError('path', 'Изображение должно быть изображение в формате JPEG, PNG или GIF');
-            } else {
-                $typeToExtension = array(IMAGETYPE_JPEG => 'jpg', IMAGETYPE_GIF => 'gif', IMAGETYPE_PNG => 'png');
-                $this->fs_name = $this->generateFsName() . '.' . $typeToExtension[$type];
-                if (! copy($this->path, $this->getImagePath())) {
-                    $this->addError('path', 'Не удалось скопировать изображение');
-                }
-                $this->save();
-            }
-        } catch (\Exception $e) {
-            $this->addError('path', 'Некорректный файл изображения');
+        if (! in_array($this->$attribute, array(IMAGETYPE_JPEG, IMAGETYPE_GIF, IMAGETYPE_PNG))) {
+            $this->addError($attribute, 'Изображение должно быть изображение в формате JPEG, PNG или GIF');
         }
     }
 
-    protected function generateFsName()
+    protected function getExtension()
     {
-        $hash = md5(uniqid($this->original_name . microtime(), true));
+        $typeToExtension = array(IMAGETYPE_JPEG => 'jpg', IMAGETYPE_GIF => 'gif', IMAGETYPE_PNG => 'png');
+        return $typeToExtension[$this->type];
+    }
+
+    protected function getFsName()
+    {
+        $hash = md5(uniqid($this->originalName . microtime(), true));
 
         $path = '';
         for ($i = 0; $i < self::FS_NAME_LEVELS; $i++) {
@@ -81,13 +94,15 @@ class PhotoCreate extends Photo
         }
 
         if (! mkdir(PathManager::getOriginalsPath() . DIRECTORY_SEPARATOR . $path, 0777, true)) {
-            throw new \CException('Can\'t create dir');
+            throw new \CException('Невозможно создать папку');
         }
 
-        $path .= substr($hash, self::FS_NAME_LEVELS * self::FS_NAME_SYMBOLS_PER_LEVEL);
+        $path .= substr($hash, self::FS_NAME_LEVELS * self::FS_NAME_SYMBOLS_PER_LEVEL) . '.' . $this->getExtension();
 
         return $path;
     }
+
+
 
     protected function beforeValidate()
     {

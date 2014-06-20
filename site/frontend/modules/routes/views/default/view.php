acbcfefb1fe18ba70dd347b22c4efbd48b0c8457
choojoy-work <nikita@happy-giraffe.ru>
2014-06-20 14:20:02 +0400

    Прямой эфир

--- site/frontend/modules/routes/views/default/view.php
+++ site/frontend/modules/routes/views/default/view.php
@@ -189,6 +189,7 @@ $this->breadcrumbs = array(
                 <!--Площадка: Весёлый Жираф / * / *-->
                 <!--Тип баннера: Безразмерный 680х470-->
                 <!--Расположение: <низ страницы>-->
+                <!-- ________________________AdFox Asynchronous code START__________________________ -->
                 <script type="text/javascript">
                     <!--
                     if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
@@ -200,6 +201,7 @@ $this->breadcrumbs = array(
                         afReferrer = '';
                     }
                     var addate = new Date();
+
                     var scrheight = '', scrwidth = '';
                     if (self.screen) {
                         scrwidth = screen.width;
@@ -210,10 +212,18 @@ $this->breadcrumbs = array(
                         scrwidth = scrsize.width;
                         scrheight = scrsize.height;
                     }
-                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=i&amp;ps=bkqy&amp;p2=evor&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
+
+                    var dl = escape(document.location);
+                    var pr1 = Math.floor(Math.random() * 1000000);
+
+                    document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
+                    document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
+
+                    AdFox_getCodeScript(1,pr1,'//ads.adfox.ru/211012/prepareCode?pp=i&amp;ps=bkqy&amp;p2=evor&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '&amp;dl='+dl+'&amp;pr1='+pr1);
                     // -->
                 </script>
-                <!--AdFox END-->
+                <!-- _________________________AdFox Asynchronous code END___________________________ -->
+
 
                 <div class="map-route-other" style="display: none;">
 

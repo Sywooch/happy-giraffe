ad22850afe7485d9f252822ac2f3b9a7e91c4678
choojoy-work <nikita@happy-giraffe.ru>
2014-06-23 15:04:16 +0400

    Прямой эфир

--- site/frontend/modules/photo/views/upload/_fromComputer.php
+++ site/frontend/modules/photo/views/upload/_fromComputer.php
@@ -18,7 +18,7 @@
                 <ul class="popup-add_multi scroll_cont">
                     <!-- ko foreach: photos -->
                     <li class="popup-add_multi-li">
-                        <div class="i-photo" data-bind="css: cssClass"><a href="" class="ico-close5"></a>
+                        <div class="i-photo" data-bind="css: cssClass"><a href="" class="ico-close5" data-bind="click: $parent.removePhoto"></a>
                             <!-- ko if: status() == $root.STATUS_SUCCESS -->
                             <div class="i-photo_hold">
                                 <div class="i-photo_img-hold"><img src="/new/images/example/w155-h110.jpg" alt="" class="i-photo_img">
@@ -40,7 +40,7 @@
                             <!-- /ko -->
 
                             <!-- ko if: status() == $root.STATUS_FAIL -->
-                            <div class="i-photo i-photo__error"><a href="" class="ico-close5"></a>
+                            <div class="i-photo i-photo__error">
                                 <div class="i-photo_hold error">
                                     <div class="tx-hint">IMG_5295.JPG</div>
                                     <div class="tx-hint">Ошибка загрузки</div>
@@ -60,11 +60,16 @@
         </div>
     </div>
     <div class="popup-add_footer clearfix" data-bind="visible: photos().length > 0">
-        <div class="float-r"><a href="" class="btn btn-success" data-bind="disabled: loading">Добавить</a></div>
+        <div class="float-r"><button href="" class="btn btn-success" data-bind="disable: loading">Добавить</button></div>
         <div class="popup-add_footer-l">
             <span class="color-gray-light">Загружено
-                <span class="popup-add_footer-count">7 </span>из <span class="popup-add_footer-count" data-bind="text: photos().length"></span></span>
-            <!-- ko if: $root.loading -->
+                <span class="popup-add_footer-count" data-bind="text: successPhotos().length"></span> из <span class="popup-add_footer-count" data-bind="text: photos().length"></span></span>
+            <!-- ko if: ! loading() -->
+            <div class="file-fake">
+                <input type="file" class="file-fake_inp" data-bind="fileUpload: fileUploadSettings" multiple><a href="" class="file-fake_btn btn btn-primary btn-s"><span class="ico-plus ico-plus__s ico-plus__white"></span>Загрузить еще</a>
+            </div>
+            <!-- /ko -->
+            <!-- ko if: loading -->
             <div class="progress progress-striped active progress__cont progress__inline">
                 <div role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%" class="progress-bar progress-bar__cont"></div>
             </div><a href="" class="btn-link btn-link-gray btn btn-s">Отменить загрузку </a>

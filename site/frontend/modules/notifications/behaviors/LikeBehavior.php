d6bfcd5028abaa756c5d73a55cf31efcf6a89c0d
smaragdinus <kirill@happy-giraffe.ru>
2014-06-26 19:02:51 +0900

    Дебаг поведения лайков и избранного

--- site/frontend/modules/notifications/behaviors/LikeBehavior.php
+++ site/frontend/modules/notifications/behaviors/LikeBehavior.php
@@ -18,6 +18,11 @@ class LikeBehavior extends BaseBehavior
         return parent::afterSave($event);
     }
 
+    public function afterDelete($event)
+    {
+        parent::afterDelete($event);
+    }
+
     /**
      * 
      * @param \HGLike $model
@@ -25,11 +30,23 @@ class LikeBehavior extends BaseBehavior
     protected function addNotification($model)
     {
         $entity = \CommunityContent::model($model->entity_name)->findByPk($model->entity_id);
-        $notification = $this->findOrCreateNotification($model->entity_name, (int) $model->entity_id, $entity->author_id, \site\frontend\modules\notifications\models\Notification::TYPE_NEW_LIKE);
-        $like = new \site\frontend\modules\notifications\models\Entity($model);
-        $like->id = $model->_id;
-        $notification->unreadEntities[] = $like;
-        $notification->save();
+        $notification = $this->findOrCreateNotification($model->entity_name, (int) $model->entity_id, (int) $entity->author_id, \site\frontend\modules\notifications\models\Notification::TYPE_NEW_LIKE);
+
+        $exists = false;
+        if ($notification->unreadEntities)
+            foreach ($notification->unreadEntities as $unreadLike)
+            {
+                if (!$exists && $unreadLike->userId == $model->user_id && $unreadLike->class == get_class($model))
+                    $exists = true;
+            }
+        if (!$exists)
+        {
+            $like = new \site\frontend\modules\notifications\models\Entity($model);
+            $like->id = $model->_id;
+            $like->userId = (int) $model->user_id;
+            $notification->unreadEntities[] = $like;
+            $notification->save();
+        }
     }
 
 }

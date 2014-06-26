f0b054bc99dd585e427682302a257613e1092f55
choojoy-work <nikita@happy-giraffe.ru>
2014-06-26 15:28:37 +0400

    Загрузка фото

--- javascripts/ko_photo.js
+++ javascripts/ko_photo.js
@@ -2,17 +2,29 @@ ko.bindingHandlers.photoUpload = {
     init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
         var value = valueAccessor();
         var data = value.data;
-        var callback = value.callback;
+        var observable = value.observable;
+
+        var defaultCallback = function(photo) {
+            if (observable() instanceof Array) {
+                observable.push(photo);
+            } else {
+                observable(photo);
+            }
+        }
+
+        var callback = value.callback || defaultCallback;
+
         ko.bindingHandlers.photoUpload.callback = function(photo) {
-            viewModel.photo(photo);
+            callback(photo);
             $.magnificPopup.close();
         };
+
         $(element).magnificPopup({
             type: 'ajax',
             ajax: {
                 settings: {
                     url: '/photo/upload/form/',
-                    data : valueAccessor()
+                    data : data
                 }
             }
         });

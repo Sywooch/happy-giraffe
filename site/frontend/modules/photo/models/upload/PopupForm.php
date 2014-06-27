63d82fe59d65ae046c5a663002668162d8260353
choojoy-work <nikita@happy-giraffe.ru>
2014-06-27 11:35:31 +0400

    Загрузка фото

--- site/frontend/modules/photo/models/upload/PopupForm.php
+++ site/frontend/modules/photo/models/upload/PopupForm.php
@@ -1,18 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mikita
- * Date: 25/06/14
- * Time: 09:59
+ * Модель для формирования попапа загрузки фото
+ *
+ * Занимается валидацией и обработкой данных, необходимых для формирования попапа загрузки фото
  */
 
 namespace site\frontend\modules\photo\models\upload;
 
-
 class PopupForm extends \CFormModel
 {
+    /**
+     * @var bool мультизагрузочная форма
+     */
     public $multiple;
 
+    /**
+     * Фильтр преобразования строки в логической значение
+     * @param $val string
+     * @return bool
+     */
     public function setMultiple($val)
     {
         return ($val === 'true') ? true : false;
@@ -26,6 +32,10 @@ class PopupForm extends \CFormModel
         );
     }
 
+    /**
+     * Формирует JSON для передачи обработанных данных с клиента
+     * @return string JSON для клиента
+     */
     public function toJSON()
     {
         return \CJSON::encode($this->attributes);
--- site/frontend/modules/photo/models/upload/UploadForm.php
+++ site/frontend/modules/photo/models/upload/UploadForm.php
@@ -1,25 +1,22 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mikita
- * Date: 05/06/14
- * Time: 16:05
+ * Форма загрузки фото
+ *
+ * Абстрактная модель загрузки фото, от нее необходимо наследовать конкретные модели загрузки
  */
 
 namespace site\frontend\modules\photo\models\upload;
-
-use site\frontend\modules\photo\components\FileHelper;
-use site\frontend\modules\photo\components\PathManager;
-use site\frontend\modules\photo\models\Photo;
 use site\frontend\modules\photo\models\PhotoCreate;
-
 
 abstract class UploadForm extends \CFormModel
 {
+    /**
+     * @return PhotoCreate возвращает модель создаваемой фотографии
+     */
     abstract public function populate();
 
     /**
-     * @var PhotoCreate
+     * @var PhotoCreate модель создаваемой фотографии
      */
     protected $photo;
 
@@ -37,16 +34,20 @@ abstract class UploadForm extends \CFormModel
         );
     }
 
+    /**
+     * Валидирует текущую форму и модель создания фото, генерирует ответ для клиента
+     * @return string JSON для клиента
+     */
     public function save()
     {
-        $this->populate();
+        $this->photo = $this->populate();
 
         $success = $this->validate() && $this->photo->save();
         $data = compact('success');
         if ($success) {
             $data['attributes'] = \CMap::mergeArray($this->photo->attributes, array(
                 'imageUrl' => $this->photo->getImageUrl(),
-                'previewUrl' => $this->getPreview(),
+                'previewUrl' => $this->getPreviewUrl(),
             ));
         } else {
             $data['error'] = $this->getFirstError();
@@ -55,13 +56,22 @@ abstract class UploadForm extends \CFormModel
         return \CJSON::encode($data);
     }
 
+    /**
+     * Выбирает первую ошибку из текущей формы и модели создания фотографии
+     * @return string текст первой ошибки
+     */
     protected function getFirstError()
     {
         $errors = \CMap::mergeArray($this->getErrors(), $this->photo->getErrors());
         return $errors[key($errors)][0];
     }
 
-    protected function getPreview()
+    /**
+     * Создает миниатюру фотографии и возвращает ее URL
+     * @return string URL миниатюры фотографии
+     * @todo временный метод, убрать после разработки механизма создания миниатюр
+     */
+    protected function getPreviewUrl()
     {
         /** @var \GdThumb $image */
         $image = \Yii::app()->phpThumb->create($this->photo->getImagePath());

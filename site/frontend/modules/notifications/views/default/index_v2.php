cf368d2cb753655caa7cee794ea5e23e7550f21e
smaragdinus <kirill@happy-giraffe.ru>
2014-06-27 14:53:51 +0900

    Рефакторинг контроллеров и представлений

--- site/frontend/modules/notifications/views/default/index_v2.php
+++ site/frontend/modules/notifications/views/default/index_v2.php
@@ -42,7 +42,7 @@
                 <div class="side-menu_t side-menu_t__notice"></div>
                 <ul class="side-menu_ul">
                     <li class="side-menu_li" data-bind="css: {active: tab() == 0}"><a href="?read=0" class="side-menu_i" data-bind="/*click: function() { changeTab(0); }*/"><span class="side-menu_i-hold"><span class="side-menu_ico side-menu_ico__notice"></span><span class="side-menu_tx">Новые </span><span class="side-menu_count" data-bind="text: unreadCount, visible: unreadCount()"></span></span><span class="verticalalign-m-help"></span></a></li>
-                    <li class="side-menu_li" data-bind="css: {active: tab() == 1}"><a href="?read=1" class="side-menu_i" data-bind="/*click: function() { changeTab(1); }*/"><span class="side-menu_i-hold"><span class="side-menu_ico side-menu_ico__notice-arhive"></span><span class="side-menu_tx">Прочитанные</span></span><span class="verticalalign-m-help"></span></a></li>
+                    <li class="side-menu_li" data-bind="css: {active: tab() == 1}"><a href="?read=1" class="side-menu_i" data-bind="/*click: function() { changeTab(1); }*/"><span class="side-menu_i-hold"><span class="side-menu_ico side-menu_ico__notice-arhive"></span><span class="side-menu_tx">Архив</span></span><span class="verticalalign-m-help"></span></a></li>
                 </ul>
             </div>
         </div>

6c701105884ecd20fe2a831bdd04c318e03f0e3a
choojoy-work <nikita@happy-giraffe.ru>
2014-06-27 18:00:34 +0400

    Загрузка фото

--- site/frontend/modules/photo/controllers/TestController.php
+++ site/frontend/modules/photo/controllers/TestController.php
@@ -60,19 +60,38 @@ class TestController extends \HController
 
     public function actionFlysystem()
     {
+        header('Content-Type: text/html; charset=utf-8');
+
         $client = S3Client::factory(array(
             'key'    => 'AKIAIRCLO4AYJCJRTV4Q',
             'secret' => '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',
         ));
 
-        \Yii::beginProfile('block1');
         $filesystem = new Filesystem(new Adapter($client, 'test-happygiraffe'));
+        $filesystem2 = new Filesystem(new Adapter2(\Yii::getPathOfAlias('webroot')));
+
+        \Yii::beginProfile('read-s3');
         $file = $filesystem->read('1344242897872.jpg');
         \Yii::endProfile('block1');
 
-        \Yii::beginProfile('block2');
-        $filesystem2 = new Filesystem(new Adapter2(\Yii::getPathOfAlias('webroot')));
-        $filesystem2->read('1344242897872.jpg');
+        \Yii::beginProfile('read-local');
+        $file2 = $filesystem2->read('1344242897872.jpg');
+        \Yii::endProfile('block2');
+
+        \Yii::beginProfile('write-s3');
+        $filesystem->write(md5(microtime()), $file);
+        \Yii::endProfile('block1');
+
+        \Yii::beginProfile('write-local');
+        $filesystem2->write(md5(microtime()), $file);
+        \Yii::endProfile('block2');
+
+        \Yii::beginProfile('exists-s3');
+        $filesystem->has(md5(microtime()));
+        \Yii::endProfile('block1');
+
+        \Yii::beginProfile('exists-local');
+        $filesystem2->has(md5(microtime()));
         \Yii::endProfile('block2');
     }
 

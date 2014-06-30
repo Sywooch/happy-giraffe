4bcb70d517d81e7773112fa76ab745ac2f605700
choojoy-work <nikita@happy-giraffe.ru>
2014-06-30 12:40:39 +0400

    Добавление EventDispatcher

---
+++ site/common/components/flysystem/Local.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 30/06/14
+ * Time: 10:18
+ */
+
+namespace site\common\components\flysystem;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local as Adapter;
+
+class Local extends Base
+{
+    public $pathAlias;
+
+    protected function getAdapter()
+    {
+        return new Filesystem(new Adapter(\Yii::getPathOfAlias($this->pathAlias)));
+    }
+} 

7c278f6ca71c59354a8e23dc20c4f58ecd3b80b5
choojoy-work <nikita@happy-giraffe.ru>
2014-07-02 12:04:16 +0400

    Переход на Gaufrette

--- site/frontend/modules/photo/components/InlinePhotoModifier.php
+++ site/frontend/modules/photo/components/InlinePhotoModifier.php
@@ -9,13 +9,19 @@
 namespace site\frontend\modules\photo\components;
 
 
+use Imagine\Gmagick\Imagine;
+use site\frontend\modules\photo\models\Photo;
+
 class InlinePhotoModifier extends \CComponent
 {
     const ROTATE_LEFT = 0;
     const ROTATE_RIGHT = 1;
 
-    public static function rotate($side)
+    public static function rotate(Photo $photo, $side)
     {
-
+        $imagine = new Imagine();
+        $image = $imagine->load($photo->getOriginalFsPath());
+        $image->rotate($side == self::ROTATE_LEFT ? -90 : 90);
+        \Yii::app()->getModule('photo')->fs->write($photo->getOriginalFsPath(), $image->get('jpg'));
     }
 } 

18d44f24b656e1b92c2b488b06ad2a810d430a3e
choojoy-work <nikita@happy-giraffe.ru>
2014-07-04 18:21:28 +0400

    Переход на Gaufrette

--- site/common/vendor/League/Flysystem/Cache/Adapter.php
+++ site/common/vendor/League/Flysystem/Cache/Adapter.php
@@ -81,8 +81,6 @@ class Adapter extends AbstractCache
         if (($file = $this->adapter->read($this->file)) !== null) {
             $this->setFromStorage($file['contents']);
         }
-
-        $this->adapter
     }
 
     /**

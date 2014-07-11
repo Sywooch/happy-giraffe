d1292008fe8d975050e30c785c3fa3c6af4bf7c3
choojoy-work <nikita@happy-giraffe.ru>
2014-07-11 17:18:37 +0400

    Загрузка фото

--- new/javascript/modules/ko_photo.js
+++ new/javascript/modules/ko_photo.js
@@ -111,7 +111,7 @@ define('ko_photoUpload', ['knockout', 'knockout.mapping', 'ko_photo', 'bootstrap
             function update() {
                 var src = 'http://img.virtual-giraffe.ru/proxy_public_file/thumbs/' + preset + '/' + photo.fs_name();
                 src = 'http://img2.dev.happy-giraffe.ru/thumbs/' + preset + '/' + photo.fs_name();
-                src = 'https://test-happygiraffe.s3.amazonaws.com/thumbs/' + preset + '/' + photo.fs_name();
+                //src = 'https://test-happygiraffe.s3.amazonaws.com/thumbs/' + preset + '/' + photo.fs_name();
                 $(element).attr('src', src);
             }
 

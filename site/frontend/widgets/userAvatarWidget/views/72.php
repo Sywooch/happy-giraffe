179b88cf8ccf9af5ab0c554fcc15e87a13d53d60
choojoy-work <nikita@happy-giraffe.ru>
2014-07-18 19:01:46 +0400

    Загрузка фото

--- site/frontend/widgets/userAvatarWidget/views/72.php
+++ site/frontend/widgets/userAvatarWidget/views/72.php
@@ -2,9 +2,18 @@
 /**
  * @var $this Avatar
  */
-?><a href="<?=$this->user->getUrl() ?>" class="ava ava__<?=($this->user->gender == 0)?'female':'male' ?>">
+$tag = $this->user->deleted == 1 ? 'span' : 'a';
+$class = 'ava ava__' . (($this->user->gender == 0) ? 'female' : 'male');
+$options = array(
+    'class' => $class,
+);
+if ($this->user->deleted == 0) {
+    $options['href'] = $this->user->getUrl();
+}
+?>
+<?=CHtml::openTag($tag, $options)?>
     <?php if ($this->user->online):?>
         <span class="ico-status ico-status__online"></span>
     <?php endif ?>
     <?=CHtml::image($this->user->getAvatarUrl(), '', array('class' => 'ava_img')) ?>
-</a>
+<?=CHtml::closeTag($tag); ?>

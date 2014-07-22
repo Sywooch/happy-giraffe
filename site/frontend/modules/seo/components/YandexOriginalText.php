47ae1c1fa3df5e6992ebf0c4ac0ade1cbc30d512
choojoy-work <nikita@happy-giraffe.ru>
2014-07-22 18:11:51 +0400

    Убрать изображения из описания украшений блюд в карте сайта

--- site/frontend/modules/seo/components/YandexOriginalText.php
+++ site/frontend/modules/seo/components/YandexOriginalText.php
@@ -101,7 +101,7 @@ class YandexOriginalText
 //        }
 
         echo $model->id . "\n";
-        var_dump((string) $xml);
+        var_dump($xml->asXML());
         die;
 
         $responseXml = new \SimpleXMLElement($response);

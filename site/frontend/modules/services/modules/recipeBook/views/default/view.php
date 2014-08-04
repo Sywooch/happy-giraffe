0d23992b7da5adbaa0cce5aeccea5b024c0a7492
choojoy-work <nikita@happy-giraffe.ru>
2014-08-04 14:46:23 +0400

    Народные рецепты

--- site/frontend/modules/services/modules/recipeBook/views/default/view.php
+++ site/frontend/modules/services/modules/recipeBook/views/default/view.php
@@ -80,8 +80,10 @@ $commentsWidget = $this->createWidget('site\frontend\modules\comments\widgets\Co
             <div class="comments-menu">
                 <ul data-tabs="tabs" class="comments-menu_ul">
                     <li class="comments-menu_li active"><a href="#commentsList" data-toggle="tab" class="comments-menu_a comments-menu_a__comments">Комментарии <?=$commentsWidget->count?></a></li>
+                    <?php if (false): ?>
                     <li class="comments-menu_li"><a href="#likesList" data-toggle="tab" class="comments-menu_a comments-menu_a__likes">Нравится 865</a></li>
                     <li class="comments-menu_li"><a href="#favoritesList" data-toggle="tab" class="comments-menu_a comments-menu_a__favorites">Закладки 865</a></li>
+                    <?php endif; ?>
                 </ul>
             </div>
             <div class="tab-content">

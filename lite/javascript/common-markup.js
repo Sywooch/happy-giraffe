e8dac56585ce9fe966fbb7e1b94b2a789bc12dff
Andrii <tantalid@mail.ru>
2014-08-04 11:24:14 +0300

    Поиск от Яндекс

--- lite/javascript/common-markup.js
+++ lite/javascript/common-markup.js
@@ -6,20 +6,20 @@ $(function() {
 
 
     // Стандартные подсказки
-    $('.powertip, .redactor_toolbar li a, [data-tooltip]').tooltipster({
-        trigger: 'hover',
-        offsetY: -6,
-        delay: 200,
-        maxWidth: 200,
-        arrowColor: '#5C4B86',
-        onlyOne: false,
-        touchDevices: false,
-        theme: '.tooltipster-default',
-        functionReady: function(origin, continueTooltip) {},
-        functionInit: function(origin, content) {
-            return origin.data('tooltip');
-        }
-    });
+    // $('.powertip, .redactor_toolbar li a, [data-tooltip]').tooltipster({
+    //     trigger: 'hover',
+    //     offsetY: -6,
+    //     delay: 200,
+    //     maxWidth: 200,
+    //     arrowColor: '#5C4B86',
+    //     onlyOne: false,
+    //     touchDevices: false,
+    //     theme: '.tooltipster-default',
+    //     functionReady: function(origin, continueTooltip) {},
+    //     functionInit: function(origin, content) {
+    //         return origin.data('tooltip');
+    //     }
+    // });
 
 
 });

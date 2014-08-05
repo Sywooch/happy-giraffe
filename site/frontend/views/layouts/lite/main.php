01ada1ae83a2167393fcb45c4bc4f9f539779d9c
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-08-05 10:39:34 +0200

    fix search

--- site/frontend/views/layouts/lite/main.php
+++ site/frontend/views/layouts/lite/main.php
@@ -52,7 +52,7 @@
                             'tagName' => 'ul',
                             'separator' => ' &nbsp; ',
                             'htmlOptions' => array('class' => 'b-crumbs_ul'),
-                            'homeLink' => '<li class="b-crumbs_li"><a href="' . $this->createUrl('/site/index') . '" class="b-crumbs_a">Главная </a></li>',
+                            'homeLink' => '<li class="b-crumbs_li"><a href="' . $this->createUrl('/site/index') . '" class="b-crumbs_a">Главная</a> </li>',
                             'activeLinkTemplate' => '<li class="b-crumbs_li"><a href="{url}" class="b-crumbs_a">{label}</a></li>',
                             'inactiveLinkTemplate' => '<li class="b-crumbs_li b-crumbs_li__last"><span class="b-crumbs_last">{label}</span></li>',
                             'links' => $this->breadcrumbs,

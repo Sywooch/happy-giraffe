624326579b935a33205e4e5634864732e8ffe2ee
choojoy-work <nikita@happy-giraffe.ru>
2014-08-08 16:40:06 +0400

    Маршруты

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -362,9 +362,12 @@ return array(
 //        'auto/routes/<_a>/<id:[\d]+>' => 'routes/default/<_a>',
 
         'auto/routes/<id:[\d]+>' => 'routes/default/index',
-        'auto/routes' => 'routes/default/index',
+        'auto/routes' => 'routes/defaultOld/index',
         'auto/routes/cities/<letter:[А-Я]>' => 'routes/default/cities',
         'auto/routes/city<cityId:[\d]+>' => 'routes/default/city',
+
+        'auto/routes/<_a>' => 'routes/defaultOld/<_a>',
+        'auto/routes/<_a>/<id:[\d]+>' => 'routes/defaultOld/<_a>',
 
         'ValentinesDay' => 'valentinesDay/default/index',
         'ValentinesDay/<_a>' => 'valentinesDay/default/<_a>',

1ec10ce03508449e0c2466ff1991f2f840016af1
choojoy-work <nikita@happy-giraffe.ru>
2014-08-15 10:47:30 +0400

    Народные рецепты

---
+++ site/frontend/modules/blog/views/calendar/index.php
@@ -0,0 +1,184 @@
+<div class="b-main_cont">
+    <div class="b-main_col-hold clearfix">
+        <div class="b-main_col-article">
+            <h1 class="heading-link-xxl">Записи от 11.08.2014</h1>
+        </div>
+    </div>
+    <div class="b-calendar">
+        <div class="b-calendar_in b-calendar_in__years">
+            <div class="b-calendar_t">Год</div>
+            <ul class="b-calendar_ul">
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">2011</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">2012</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">2013</a></li>
+                <li class="b-calendar_li active"><a href="#" class="b-calendar_a">2014</a></li>
+            </ul>
+        </div>
+        <div class="b-calendar_in b-calendar_in__months">
+            <div class="b-calendar_t">Месяц</div>
+            <ul class="b-calendar_ul">
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">янв</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">фев</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">мар</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">апр</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">май</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">июн</a></li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">июл</a></li>
+                <li class="b-calendar_li active"><a href="#" class="b-calendar_a">авг</a></li>
+                <li class="b-calendar_li"><span class="b-calendar_a">сен</span></li>
+                <li class="b-calendar_li"><span class="b-calendar_a">окт</span></li>
+                <li class="b-calendar_li"><span class="b-calendar_a">ноя</span></li>
+                <li class="b-calendar_li"><span class="b-calendar_a">дек</span></li>
+            </ul>
+        </div>
+        <div class="b-calendar_in b-calendar_in__days">
+            <div class="b-calendar_t">День</div>
+            <ul class="b-calendar_ul">
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">1</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">2</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">3</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">4</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">5</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">6</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">7</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">8</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">9</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">10</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">11</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">12</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">13</a>
+                </li>
+                <li class="b-calendar_li active"><a href="#" class="b-calendar_a">14</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">15</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">16</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">17</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">18</a>
+                </li>
+                <li class="b-calendar_li"><a href="#" class="b-calendar_a">19</a>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">20</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">21</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">22</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">23</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">24</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">25</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">26</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">27</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">28</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">29</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">30</span>
+                </li>
+                <li class="b-calendar_li"><span class="b-calendar_a">31</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="post-list-simple">
+        <!-- Количество элементов списка 50-100 на странице-->
+        <ul class="post-list-simple_ul">
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Теща ненавидит зятя. Внук растет без бабушки. Грустно... </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Теща ненавидит зятя. Внук растет без бабушки. Грустно... </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Про энцефалопатию. неужели эта болезнь никак не поддается лечению? или просто люди живут как овощи? </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Марафон женственности, кто нибудь участвовал?</a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+            <li class="post-list-simple_li">
+                <div class="post-list-simple_top"><a href="#" class="a-light">Ангелина Богоявленская</a>
+                    <time datetime="2012-12-23" class="tx-date">11 августа</time>
+                </div>
+                <div class="post-list-simple_t"><a href="#" class="post-list-simple_t-a">Так хорошо, когда папа выходной!  </a></div>
+            </li>
+        </ul>
+    </div>
+    <!-- paginator-->
+    <div class="yiipagination">
+        <div class="pager">
+            <ul class="yiiPager">
+                <li class="page"><a href="">1</a></li>
+                <li class="page"><a href="">2</a></li>
+                <li class="page"><a href="">5</a></li>
+                <li class="page"><a href="">6</a></li>
+                <li class="page selected"><a href="">7</a></li>
+                <li class="page"><a href="">8</a></li>
+                <li class="page"><a href="">9</a></li>
+                <li class="page"><a href="">10</a></li>
+                <li class="page"><a href="">11</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /paginator-->
+</div>

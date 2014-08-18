ef0cf7d526373f1b08f72469201b82168ecfcade
choojoy-work <nikita@happy-giraffe.ru>
2014-08-18 11:05:55 +0400

    Народные рецепты

--- site/frontend/components/LitePager.php
+++ site/frontend/components/LitePager.php
@@ -11,6 +11,15 @@ class LitePager extends CLinkPager
     public $cssFile = false;
     public $header = '';
 
+    public function init()
+    {
+        $this->appendTitle();
+        $this->appendDescription();
+        $this->setLinks();
+
+        parent::init();
+    }
+
     protected function createPageButtons()
     {
         if(($pageCount=$this->getPageCount())<=1)
@@ -25,5 +34,55 @@ class LitePager extends CLinkPager
             $buttons[]=$this->createPageButton($i+1,$i,$this->internalPageCssClass,false,$i==$currentPage);
 
         return $buttons;
+    }
+
+    protected function appendTitle()
+    {
+        if ($this->currentPage != 0) {
+            Yii::app()->controller->pageTitle .= ', страница ' . ($this->currentPage + 1);
+        }
+    }
+
+    protected function appendDescription()
+    {
+        if (! empty(Yii::app()->controller->meta_description)) {
+            Yii::app()->controller->meta_description .= ', страница ' . ($this->currentPage + 1);
+        }
+    }
+
+    protected function setLinks()
+    {
+        /**
+         * @var ClientScript $cs
+         */
+        $cs = Yii::app()->clientScript;
+        $currentPage = $this->getCurrentPage();
+        $pageCount = $this->getPageCount();
+
+        // prev
+        if (($page = $currentPage - 1) < 0) {
+            $page = 0;
+        }
+        if ($currentPage > 0) {
+            $cs->registerLinkTag('prev', null, $this->createPageUrl($page));
+        }
+
+        //next
+        if (($page = $currentPage + 1) >= $pageCount - 1) {
+            $page = $pageCount - 1;
+        }
+        if ($currentPage < $pageCount - 1) {
+            $cs->registerLinkTag('next', null, $this->createPageUrl($page));
+        }
+
+        // first
+        if ($currentPage > 0) {
+            $cs->registerLinkTag('first', null, $this->createPageUrl(0));
+        }
+
+        // last
+        if ($currentPage < $pageCount-1) {
+            $cs->registerLinkTag('last', null, $this->createPageUrl($pageCount - 1));
+        }
     }
 } 

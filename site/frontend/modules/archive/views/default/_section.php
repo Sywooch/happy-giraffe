df966dff7958128223c426a779ea4bbcdb44723c
choojoy-work <nikita@happy-giraffe.ru>
2014-08-18 15:55:38 +0400

    Народные рецепты

--- site/frontend/modules/archive/views/default/_section.php
+++ site/frontend/modules/archive/views/default/_section.php
@@ -6,7 +6,7 @@
 ?>
 
 <div class="site-map_section">
-    <div class="site-map_i"><a href="<?=$section->getUrl()?>" class="site-map_a site-map_a__<?=$class?>"><?=$section->title?></a></div>
+    <h3 class="site-map_i"><a href="<?=$section->getUrl()?>" class="site-map_a site-map_a__<?=$class?>"><?=$section->title?></a></h3>
     <ul class="menu-simple">
         <?php foreach ($section->clubs as $club): ?>
             <li class="menu-simple_li"><a href="<?=$club->getUrl()?>" class="a-light fontweight-b"><?=$club->title?></a></li>

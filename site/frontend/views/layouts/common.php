3aa4cf0399d8320cdc1ebabd019f83fde8571b41
choojoy-work <nikita@happy-giraffe.ru>
2014-08-20 15:06:44 +0400

    Маршруты

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -102,8 +102,7 @@
         <!--giraffe-->
         <!--Площадка: Весёлый Жираф / * / *-->
         <!--Тип баннера: Брендирование-->
-        <!--Расположение: <верх страницы>-->
-        <!-- ________________________AdFox Asynchronous code START__________________________ -->
+        <!--Расположение: бэкграунд-->
         <script type="text/javascript">
             <!--
             if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
@@ -115,7 +114,6 @@
                 afReferrer = '';
             }
             var addate = new Date();
-
             var scrheight = '', scrwidth = '';
             if (self.screen) {
                 scrwidth = screen.width;
@@ -126,17 +124,10 @@
                 scrwidth = scrsize.width;
                 scrheight = scrsize.height;
             }
-
-            var dl = escape(document.location);
-            var pr1 = Math.floor(Math.random() * 1000000);
-
-            document.write('<div id="AdFox_banner_'+pr1+'"><\/div>');
-            document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_'+pr1+'" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
-
-            AdFox_getCodeScript(1,pr1,'//ads.adfox.ru/211012/prepareCode?pp=g&amp;ps=bkqy&amp;p2=ewfb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '&amp;dl='+dl+'&amp;pr1='+pr1);
+            document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/211012/prepareCode?pp=dtx&amp;ps=bkqy&amp;p2=ewfb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
             // -->
         </script>
-        <!-- _________________________AdFox Asynchronous code END___________________________ -->
+        <!--AdFox END-->
 
         <?php if (false): ?>
         <!-- Soloway Javascript code START-->

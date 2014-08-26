c4ae6a1ede6cac029f7e87df2e7ba3c77f63408d
smaragdinus <kirill@happy-giraffe.ru>
2014-08-26 17:52:31 +0900

    muscula update

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -11,16 +11,16 @@
         <?php if (! YII_DEBUG): ?>
         <script type='text/javascript'>
             window.Muscula = { settings:{
-                logId:"VwXATrD-QRwMP", suppressErrors: false
+                logId:"ff8cf948-b188-4eb0-b8b9-bd8f006d5e51", suppressErrors: false
             }};
             (function () {
                 var m = document.createElement('script'); m.type = 'text/javascript'; m.async = true;
                 m.src = (window.location.protocol == 'https:' ? 'https:' : 'http:') +
-                    '//musculahq.appspot.com/Muscula6.js';
+                    '//musculahq.appspot.com/Muscula7.js';
                 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(m, s);
                 window.Muscula.run=function(){var a;eval(arguments[0]);window.Muscula.run=function(){};};
                 window.Muscula.errors=[];window.onerror=function(){window.Muscula.errors.push(arguments);
-                    return window.Muscula.settings.suppressErrors===undefined;}
+                return window.Muscula.settings.suppressErrors===undefined;}
             })();
         </script>
         <?php endif; ?>

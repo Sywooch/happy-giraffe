39d47e5e70c64f723b81e0ea525a25c32b408960
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-08-28 01:46:11 +0200

    fix services calendar

--- site/frontend/modules/calendar/views/default/view_v2.php
+++ site/frontend/modules/calendar/views/default/view_v2.php
@@ -14,17 +14,15 @@ if ($period->features && $period->features_heading)
     ?>
     <div class="age-features<?php if ($period->calendar == 1): ?> age-features-pregnancy<?php endif; ?>">
     </div>
-    <div class="b-main_row calendar-serv-note calendar-serv-note__blue-light margin-t40">
-        <div class="b-main_cont">
-            <div class="b-main_col-article b-main_col-article__center">
-                <h2 class="calendar-serv-note_t"><?= preg_replace('#(\d+)#', '<span>$1</span>', $period->features_heading) ?></h2>
-                <ul class="calendar-serv-note_ul">
-                    <?php
-                    foreach ($features as $f)
-                        echo CHtml::tag('li', array('class' => 'calendar-serv-note_li'), $f);
-                    ?>
-                </ul>
-            </div>
+    <div class="b-main_row calendar-serv-note">
+        <div class="b-main_col-article b-main_col-article__center">
+            <h2 class="calendar-serv-note_t"><?= preg_replace('#(\d+)#', '<span>$1</span>', $period->features_heading) ?></h2>
+            <ul class="calendar-serv-note_ul">
+                <?php
+                foreach ($features as $f)
+                    echo CHtml::tag('li', array('class' => 'calendar-serv-note_li'), $f);
+                ?>
+            </ul>
         </div>
     </div>
     <?php
@@ -40,53 +38,58 @@ if ($period->features && $period->features_heading)
     $doc->unloadDocument();
 }
 ?>
-<div class="b-main_cont">
-    <div class="b-main_col-wide">   
-        <h1 class="heading-link-xxl heading-link-xxl__center"><?= $period->heading ?></h1>
+<!-- В зависимости от сервиса есть класс services-calendars__pregnancy или services-calendars__child -->
+<div class="services-calendars services-calendars__child">
+    <div class="b-main_cont">
+        <div class="b-main_col-wide">   
+            <h1 class="heading-link-xxl heading-link-xxl__center"><?= $period->heading ?></h1>
+        </div>
+        <table class="article-nearby clearfix">
+            <tr>
+                <?php
+                $i = 0;
+                while ($periods[$i]->id !== $period->id)
+                    $i++;
+                ?>
+                <td><?php if(isset($periods[$i-1])) echo CHtml::link (CHtml::tag ('span', array('class' => 'article-nearby_tx'), $periods[$i-1]->title), $periods[$i-1]->url, array('class' => 'article-nearby_a article-nearby_a__l')); ?></td>
+                <td><?php if(isset($periods[$i+1])) echo CHtml::link (CHtml::tag ('span', array('class' => 'article-nearby_tx'), $periods[$i+1]->title), $periods[$i+1]->url, array('class' => 'article-nearby_a article-nearby_a__r')); ?></td>
+            </tr>
+        </table>
     </div>
-    <table class="article-nearby clearfix">
-        <tr>
+    <div class="b-main_cont">
+        <div class="b-main_col-article b-main_col-article__center wysiwyg-content">
             <?php
-            $i = 0;
-            while ($periods[$i]->id !== $period->id)
-                $i++;
+            echo $period->text;
             ?>
-            <td><?php if(isset($periods[$i-1])) echo CHtml::link (CHtml::tag ('span', array('class' => 'article-nearby_tx'), $periods[$i-1]->title), $periods[$i-1]->url, array('class' => 'article-nearby_a article-nearby_a__l')); ?></td>
-            <td><?php if(isset($periods[$i+1])) echo CHtml::link (CHtml::tag ('span', array('class' => 'article-nearby_tx'), $periods[$i+1]->title), $periods[$i+1]->url, array('class' => 'article-nearby_a article-nearby_a__r')); ?></td>
-        </tr>
-    </table>
-</div>
-<div class="b-main_cont">
-    <div class="b-main_col-article b-main_col-article__center">
-        <?php
-        echo $period->text;
-        ?>
+        </div>
     </div>
-</div>
-<?php if ($period->communities): ?>
-    <div class="b-main_row b-main_row__blue-light services-fast margin-b0">
-        <div class="b-main_cont">
-            <div class="services-fast_hold">
-                <div class="b-main_col-article b-main_col-article__center">
-                    <div class="services-fast_t">Полезные сервисы. Попробуйте!</div>
-                    <ul class="services-fast_ul">
-                        <?php foreach ($period->communities as $c): ?>
-                            <li class="services-fast_li">
-                                <a class="services-fast_a" href="<?= $c->url ?>">
-                                    <div class="services-fast_ico">
-                                        <?= CHtml::image('/images/club_img_' . $c->id . '.png') ?>
-                                    </div>
-                                    <div class="services-fast_tx">
-                                        <?= $c->title ?>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
+    <?php if ($period->communities): ?>
+        <!-- /Яндрекс реклама/ -->
+        <div class="b-main_row services-fast margin-b0">
+            <div class="b-main_cont">
+                <div class="services-fast_hold">
+                    <div class="b-main_col-article b-main_col-article__center">
+                        <div class="services-fast_t heading-m">Полезные сервисы. Попробуйте!</div>
+                        <ul class="services-fast_ul">
+                            <?php foreach ($period->communities as $c): ?>
+                                <li class="services-fast_li">
+                                    <a class="services-fast_a" href="<?= $c->url ?>">
+                                        <div class="services-fast_ico">
+                                            <?= CHtml::image('/images/club_img_' . $c->id . '.png') ?>
+                                        </div>
+                                        <div class="services-fast_tx">
+                                            <?= $c->title ?>
+                                        </div>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
 
-                    </ul>
+                        </ul>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-<?php endif; ?>
+    <?php endif; ?>
+    <!-- /Баннер о следующей статье, появляется у планшета и меньших экранов/ -->
+</div>

33806032746f3ff63f44d3f4ba8acd445b1fa2d0
smaragdinus <kirill@happy-giraffe.ru>
2014-08-28 12:51:58 +0900

    Изменения в ClientScript

--- site/frontend/config/clientScript.php
+++ site/frontend/config/clientScript.php
@@ -3,6 +3,7 @@
 return array(
     'class' => 'application.components.ClientScript',
     'amdFile' => '/new/javascript/modules/require2.1.11-jquery1.10.2.js',
+    'amdFilePos' => CClientScript::POS_HEAD,
     'amd' => array(
         'baseUrl' => '/new/javascript/modules',
         'waitSeconds' => 0,
@@ -39,6 +40,51 @@ return array(
       ko.amdTemplateEngine.defaultPath = "/new/javascript/modules";
       ko.amdTemplateEngine.defaultSuffix = ".tmpl.html";
       ', */
+    ),
+    'litePackages' => array(
+        'default' => array(
+            'baseUrl' => '/lite/css/dev/',
+            'guest' => array(
+                'all.css' => array(
+                    'pos' => CClientScript::POS_HEAD,
+                    'inline' => false,
+                ),
+            ),
+            'user' => array(
+                'all.css' => array(
+                    'pos' => CClientScript::POS_HEAD,
+                    'inline' => false,
+                ),
+            ),
+        ),
+        'services' => array(
+            'baseUrl' => '/lite/css/min/',
+            'guest' => array(
+                'services.css' => array(
+                    'pos' => CClientScript::POS_END,
+                    'inline' => false,
+                ),
+            ),
+            'user' => array(
+                'services-user.css' => array(
+                    'pos' => CClientScript::POS_END,
+                    'inline' => false,
+                ),
+            ),
+            'depends' => array('default'),
+        ),
+        'routes' => array(
+            'depends' => array('services'),
+        ),
+        'calendar' => array(
+            'depends' => array('services'),
+        ),
+        'recipes' => array(
+            'depends' => array('services'),
+        ),
+        'archive' => array(
+            'depends' => array('services'),
+        ),
     ),
     'packages' => array(
         'userSettings' => array(
--- site/frontend/modules/calendar/CalendarModule.php
+++ site/frontend/modules/calendar/CalendarModule.php
@@ -2,31 +2,36 @@
 
 class CalendarModule extends CWebModule
 {
-	public function init()
-	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
 
-		// import the module-level models and components
-		$this->setImport(array(
-			'calendar.models.*',
-			'calendar.components.*',
-		));
-        
+    public function init()
+    {
+        // this method is called when the module is being created
+        // you may place code here to customize the module or the application
+        // import the module-level models and components
+        $this->setImport(array(
+            'calendar.models.*',
+            'calendar.components.*',
+        ));
+
+        //Yii::app()->clientScript->useAMD = true;
+        //Yii::app()->clientScript->registerLitePackage('calendar');
+
+
         $package = Yii::app()->user->isGuest ? 'lite_calendar' : 'lite_calendar_user';
         Yii::app()->clientScript->registerPackage($package);
         Yii::app()->clientScript->useAMD = true;
-	}
+    }
 
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			return true;
-		}
-		else
-			return false;
-	}
+    public function beforeControllerAction($controller, $action)
+    {
+        if (parent::beforeControllerAction($controller, $action))
+        {
+            // this method is called before any module controller action is performed
+            // you may place customized code here
+            return true;
+        }
+        else
+            return false;
+    }
+
 }

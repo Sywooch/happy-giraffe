3e4ab6c560a6b10c911359d6672a558b5a4c6b05
Nikita Izygin <calacitizen@gmail.com>
2014-09-04 17:10:30 +0400

    merge BUGS-3105

--- site/frontend/modules/community/widgets/CommunityQuestionWidget.php
+++ site/frontend/modules/community/widgets/CommunityQuestionWidget.php
@@ -16,6 +16,35 @@ class CommunityQuestionWidget extends CWidget
         if ($this->forumId == 2) {
             $model = new CommunityQuestionForm();
             $this->render('CommunityQuestionWidget', compact('model'));
+       if (Yii::app()->clientScript->useAMD)
+           Yii::app()->clientScript->registerAMD('FormScript#focus', array('ko' => 'knockout', '$' => 'jquery', 'waitUntilExists' => 'waitUntilExistsRe'), "
+
+                $.fn.waitUntilExists = waitUntilExists;
+
+                $('#question-form').waitUntilExists(function () {
+                  var CommunityQuestion = function() {
+                     var self = this;
+                     self.title = ko.observable('');
+                  }
+                  var model = new CommunityQuestion();
+                  ko.applyBindings(model, document.getElementById('question-form'));
+                });
+
+           ");
+       else
+           Yii::app()->clientScript
+               ->registerCoreScript('jquery', 'knockout')
+               ->registerScript('FormScript#focus', "
+                   var CommunityQuestion = function() {
+                       var self = this;
+                       self.title = ko.observable('');
+                   }
+
+                   $(function() {
+                       var model = new CommunityQuestion();
+                       ko.applyBindings(model, document.getElementById('question-form'));
+                   });
+           ");
         }
     }
 }

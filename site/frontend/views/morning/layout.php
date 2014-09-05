4175029c3ce1bd672425007175f10efb1aaada7e
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-09-05 08:34:31 +0200

    fix sidebar

--- site/frontend/views/morning/layout.php
+++ site/frontend/views/morning/layout.php
@@ -1,9 +1,10 @@
 <?php $this->beginContent('//layouts/main'); ?>
 
-<div id="morning" class="clearfix">
+<!-- <div id="morning" class="clearfix">
 
-    <div class="main-right morning-main">
-
+    <div class="main-right morning-main"> -->
+<div id="morning" class="content-cols clearfix">
+    <div class="col-23-middle morning-main">
         <?=$content ?>
 
     </div>

4c5ef21fe857759d0514c08e3300fd46d4db28e7
choojoy-work <nikita@happy-giraffe.ru>
2014-09-09 16:51:56 +0400

    seo

--- site/frontend/modules/photo/controllers/DefaultController.php
+++ site/frontend/modules/photo/controllers/DefaultController.php
@@ -9,10 +9,20 @@
 namespace site\frontend\modules\photo\controllers;
 
 
+use site\frontend\modules\photo\models\PhotoAlbum;
+
 class DefaultController extends \HController
 {
+    public $layout = '//layouts/new/mainNew';
+
     public function actionPresets()
     {
         echo \CJSON::encode(\Yii::app()->thumbs->presets);
+    }
+
+    public function actionUser($userId = null)
+    {
+        $albums = PhotoAlbum::model()->user($userId)->findAll();
+        $this->render('user');
     }
 } 

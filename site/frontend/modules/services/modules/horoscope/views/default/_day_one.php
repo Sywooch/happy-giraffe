e6b2ae89221aa2ebaed3d351fc062526322fa69e
smaragdinus <kirill@happy-giraffe.ru>
2014-09-11 21:01:19 +0900

    fix

--- site/frontend/modules/services/modules/horoscope/views/default/_day_one.php
+++ site/frontend/modules/services/modules/horoscope/views/default/_day_one.php
@@ -6,8 +6,6 @@
 </div>
 <div class="wysiwyg-content clearfix">
     <?php
-    $this->beginWidget('SeoContentWidget');
     echo Str::strToParagraph($model->text);
-    $this->endWidget();
     ?>
 </div>

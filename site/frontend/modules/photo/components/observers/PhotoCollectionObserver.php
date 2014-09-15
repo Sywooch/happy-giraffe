0260872010ebc3f018656a4190b2ad529480fde3
choojoy-work <nikita@happy-giraffe.ru>
2014-09-15 15:53:00 +0400

    kinder

---
+++ site/frontend/modules/photo/components/observers/PhotoCollectionObserver.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 15/09/14
+ * Time: 14:43
+ */
+
+namespace site\frontend\modules\photo\components\observers;
+
+
+use site\frontend\modules\photo\models\PhotoCollection;
+
+abstract class PhotoCollectionObserver extends \CComponent
+{
+    const ORDER = 't.position ASC, t.id ASC';
+
+    protected $model;
+
+    public function __construct(PhotoCollection $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getCount()
+    {
+        return $this->model->attachesCount;
+    }
+
+    protected function getDefaultCriteria()
+    {
+        $criteria = new \CDbCriteria();
+        $criteria->order = self::ORDER;
+        $criteria->with = 'photo';
+        return $criteria;
+    }
+
+    abstract public function getSingle($offset);
+    abstract public function getSlice($length, $offset);
+} 

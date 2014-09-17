2db753acf3cd781ac2ac3110056101e7a2518ec9
choojoy-work <nikita@happy-giraffe.ru>
2014-09-17 18:16:32 +0400

    attaching process

--- new/javascript/modules/photo/MyPhotosAlbumViewModel.js
+++ new/javascript/modules/photo/MyPhotosAlbumViewModel.js
@@ -1,7 +1,7 @@
 define('photo/MyPhotosAlbumViewModel', ['knockout', 'photo/PhotoAlbum'], function(ko, PhotoAlbum) {
     function MyPhotosAlbumViewModel(data) {
         var self = this;
-        self.album = new PhotoAlbum(data.album);
+        self.album = new Album(data.album);
 
         self.editingHeader = ko.observable(false);
         self.editingDescription = ko.observable(false);
@@ -9,6 +9,17 @@ define('photo/MyPhotosAlbumViewModel', ['knockout', 'photo/PhotoAlbum'], functio
         self.editHeader = function() {
             self.editingHeader(self.album.title());
         };
+
+        self.removeAlbum = function() {
+            self.album.remove(function() {
+                document.location.href = data.returnUrl;
+            });
+        };
+    }
+
+    function Album(data) {
+        var self = this;
+        PhotoAlbum.apply(self, arguments);
     }
 
     return MyPhotosAlbumViewModel;

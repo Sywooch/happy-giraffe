cdd79ac97b1dc08a4ca13be7619107988feb4811
choojoy-work <nikita@happy-giraffe.ru>
2014-09-18 15:51:58 +0400

    sorting

--- site/frontend/modules/services/modules/test/views/assets/points.js
+++ site/frontend/modules/services/modules/test/views/assets/points.js
@@ -59,6 +59,6 @@ var Test = {
             yaCounter11221648.hit(Test.path + '?step=' + Test.step);
         }
         $('#pix').attr('src', 'http://ad.adriver.ru/cgi-bin/rle.cgi?sid=1&bt=21&ad=420214&pid=1313272&bid=2833663&bn=2833663&rnd=' + Math.floor((Math.random()*9999999999)+1000000000));
-
+        adfox_reloadBanner('bn-1');
     }
 }

86d45a99bb5c58590b14e325c99d6d6180f182a8
choojoy-work <nikita@happy-giraffe.ru>
2014-09-18 14:46:00 +0400

    sorting

--- javascripts/ko_gallery.js
+++ javascripts/ko_gallery.js
@@ -80,7 +80,7 @@
         ga('send', 'pageview', self.currentPhoto().url());
                 yaCounter11221648.hit(self.currentPhoto().url());
                 self.setLikesPosition();
-        $('#photo-window_banner iframe').attr('src', '/direct4.html?' + Math.floor(Math.random() * 9999999999) + 1000000000);
+        //$('#photo-window_banner iframe').attr('src', '/direct4.html?' + Math.floor(Math.random() * 9999999999) + 1000000000);
                 if (self.collectionClass == 'ContestPhotoCollection')
                     self.loadContestData();
 

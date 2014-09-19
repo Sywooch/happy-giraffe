b20582260cc503a3929ac047a8b2a94a7715b88a
choojoy-work <nikita@happy-giraffe.ru>
2014-09-19 13:20:39 +0400

    sorting

--- site/frontend/modules/services/modules/test/views/assets/points.js
+++ site/frontend/modules/services/modules/test/views/assets/points.js
@@ -53,7 +53,7 @@ var Test = {
                     if(defaultLoad) {
                         tgNS.loadBanner(initData.pr1, requestSrc, initData.sessionId);
                     }
-                })('bn-1', 'http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a', true);
+                })('bn-2', 'http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a', true);
             }
         });
 

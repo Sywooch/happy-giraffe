958bf2ae8ac5a8a50fbd454a636f26a843f68b2b
choojoy-work <nikita@happy-giraffe.ru>
2014-09-19 14:01:56 +0400

    sorting

--- site/frontend/modules/services/modules/test/views/assets/points.js
+++ site/frontend/modules/services/modules/test/views/assets/points.js
@@ -48,7 +48,7 @@ var Test = {
                         tgNS = window.ADFOX.RELOAD_CODE,
                         initData = tgNS.initBanner(bannerPlaceId,requestSrc);
 
-                    $('.result').after(initData.html);
+                    $('.result:last').after(initData.html);
 
                     if(defaultLoad) {
                         tgNS.loadBanner(initData.pr1, requestSrc, initData.sessionId);

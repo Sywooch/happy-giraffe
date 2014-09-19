e48c33172c35f2451430c138636341d89ebcdadb
choojoy-work <nikita@happy-giraffe.ru>
2014-09-19 11:25:53 +0400

    sorting

--- site/frontend/views/counters/counters.php
+++ site/frontend/views/counters/counters.php
@@ -1,3 +1,8 @@
+<script type="text/javascript">
+    isAmd = <?=CJavaScript::encode(Yii::app()->clientScript->useAMD)?>;
+    isGuest = <?=CJavaScript::encode(Yii::app()->user->isGuest)?>;
+</script>
+
 <?php Yii::app()->controller->renderPartial('//counters/_google_tag'); ?>
 <?php Yii::app()->controller->renderPartial('//counters/_metrika'); ?>
 <?php //Yii::app()->controller->renderPartial('//counters/_ga'); ?>

9b9c929b5f2d05f34717540613a8f41298292fe6
smaragdinus <kirill@happy-giraffe.ru>
2014-09-22 18:29:33 +0900

    Играем в шахматы с сео-текстом

--- site/frontend/modules/services/modules/horoscope/views/default/list.php
+++ site/frontend/modules/services/modules/horoscope/views/default/list.php
@@ -51,18 +51,19 @@
                 }
                 ?>
             </h1>
-            <div class="wysiwyg-content visible-md-block">
-                <?php if ($this->alias == 'today'): ?>
+            <?php if ($this->alias == 'today'): ?>
+                <div class="wysiwyg-content visible-md-block">
                     <p>Нравится ли вам возможность ежедневно советоваться со звёздами? Наверняка вы не раз читали гороскоп для
                         своего знака Зодиака. Что-то в нём вам казалось смешным, а что-то – полезным. Действительно, в то время как
                         одни считают гороскоп сегодня руководством к действию, другие относятся к нему как к пустой забаве.
                         Наверное, это связано с качеством гороскопа. Оцените работу нашего эксклюзивного астролога – прочитайте, что
                         он написал для вас сегодня, нажав на нужный знак Зодиака.</p>
-                <?php endif ?>
-            </div>
-            <div class="wysiwyg-content visible-md-block">
-                <?= ServiceText::getText('horoscope', $this->period == 'day' ? $this->alias : $this->period) ?>
-            </div>
+                </div>
+            <?php else: ?>
+                <div class="wysiwyg-content visible-md-block">
+                    <?= ServiceText::getText('horoscope', $this->period == 'day' ? $this->alias : $this->period) ?>
+                </div>
+            <?php endif; ?>
             <div class="zodiac-list">
                 <?php $this->renderPartial('_zodiac_list'); ?>
             </div>
@@ -70,6 +71,11 @@
                 <div class="menu-link-simple_t">Узнайте гороскоп</div>
                 <?php $this->renderPartial('_menu'); ?>
             </div>
+            <?php if ($this->alias == 'today'): ?>
+                <div class="seo-desc wysiwyg-content visible-md-block">
+                    <?= ServiceText::getText('horoscope', $this->period == 'day' ? $this->alias : $this->period) ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </div>

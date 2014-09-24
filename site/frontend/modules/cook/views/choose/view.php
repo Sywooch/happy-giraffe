3668b1f2cdd4c162f89984eb8689a00a3cd2c109
smaragdinus <kirill@happy-giraffe.ru>
2014-09-24 20:02:26 +0900

    init

--- site/frontend/modules/cook/views/choose/view.php
+++ site/frontend/modules/cook/views/choose/view.php
@@ -1,3 +1,13 @@
+<?php
+$this->pageTitle = 'Как выбрать ' . $model->title_accusative;
+
+$this->breadcrumbs = array(
+    'Кулинария' => array('/cook/default/index'),
+    'Как выбрать продукты?' => array('/cook/choose/index'),
+    $model->category->title => $model->category->url,
+    $model->title,
+);
+?>
 <div id="product-choose">
 
     <div class="title">
@@ -10,17 +20,17 @@
 
         <div class="product-choose-in">
 
-            <h1>Как выбрать <?=$model->title_accusative;?></h1>
+            <h1>Как выбрать <?= $model->title_accusative; ?></h1>
 
             <div class="product-choose-img">
 
-                <img src="<?=isset($model->photo) ? $model->photo->getPreviewUrl(350, 350, Image::WIDTH) : '' ?>"/>
+                <img src="<?= isset($model->photo) ? $model->photo->getPreviewUrl(350, 350, Image::WIDTH) : '' ?>"/>
 
             </div>
 
             <div class="wysiwyg-content clearfix">
 
-                <?=$model->desc;?>
+                <?= $model->desc; ?>
                 <br/>
 
                 <?php
@@ -28,17 +38,17 @@
                 $kak_viglyadat = (mb_substr($model->title_quality, -2, 2) == 'ки') ? 'Как выглядят' : 'Как выглядит';
                 ?>
 
-                <h2><?=$kak_viglyadat?> <?=$model->title_quality;?></h2>
+                <h2><?= $kak_viglyadat ?> <?= $model->title_quality; ?></h2>
 
-                <?=$model->desc_quality;?>
+                <?= $model->desc_quality; ?>
 
-                <h2><?=$kak_viglyadat?> <?=$model->title_defective;?></h2>
+                <h2><?= $kak_viglyadat ?> <?= $model->title_defective; ?></h2>
 
-                <?=$model->desc_defective;?>
+                <?= $model->desc_defective; ?>
 
-                <h2>Как проверить <?=$model->title_check;?></h2>
+                <h2>Как проверить <?= $model->title_check; ?></h2>
 
-                <?=$model->desc_check;?>
+                <?= $model->desc_check; ?>
 
             </div>
 

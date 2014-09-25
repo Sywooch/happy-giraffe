84794cb99e511dff80081b04f488585612d71087
smaragdinus <kirill@happy-giraffe.ru>
2014-09-25 20:35:42 +0900

    bug fix

--- site/frontend/components/api/SoftRestoreAction.php
+++ site/frontend/components/api/SoftRestoreAction.php
@@ -21,6 +21,11 @@ class SoftRestoreAction extends \CAction
         /** @todo Проверить доступ */
         $class = $this->modelName;
         $model = $class::model()->resetScope(true)->findByPk($id);
+        if (is_null($model))
+            throw new \CHttpException(404);
+
+        if ($model instanceof \IHToJSON)
+            $this->controller->data = $model->toJSON();
         $this->controller->success = $model->restore();
     }
 

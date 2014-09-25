9b45e31ba5c3ad9edaf2f0cd7b52c75f2f1cc10b
choojoy-work <nikita@happy-giraffe.ru>
2014-09-25 14:08:18 +0400

    sorting

---
+++ site/frontend/modules/photo/tests/ApiTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikita
+ * Date: 25/09/14
+ * Time: 01:16 PM
+ */
+
+namespace site\frontend\modules\photo\tests;
+
+
+class ApiTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCanBeNegated()
+    {
+        $this->
+    }
+
+} 

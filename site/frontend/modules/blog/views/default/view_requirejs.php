a0498409b469505e50966fa87782622ccba88b4e
smaragdinus <kirill@happy-giraffe.ru>
2014-09-26 17:19:21 +0900

    up

--- site/frontend/modules/blog/views/default/view_requirejs.php
+++ site/frontend/modules/blog/views/default/view_requirejs.php
@@ -140,10 +140,22 @@ if ($full) {
 <?php
 if ($full)
 {
-    echo CHtml::openTag('div', array('class' => 'section comments comments__buble'));
-    $this->widget('site\frontend\modules\comments\widgets\CommentWidget', array('model' => $data));
-    echo CHtml::closeTag('div');
-    //$this->widget('application.widgets.newCommentWidget.NewCommentWidget', array('model' => $data, 'full' => $full));
+    $commentsWidget = $this->createWidget('site\frontend\modules\comments\widgets\CommentWidget', array('model' => $data))
+    ?>
+    <div class="comments-menu">
+        <ul class="comments-menu_ul" data-tabs="tabs">
+            <li class="comments-menu_li active"><a class="comments-menu_a comments-menu_a__comments" data-toggle="tab" href="#commentsList">Комментарии <?= $commentsWidget->count ?></a></li>
+            <!--<li class="comments-menu_li"><a class="comments-menu_a comments-menu_a__likes" data-toggle="tab" href="#likesList">Нравится 865</a></li>
+            <li class="comments-menu_li"><a class="comments-menu_a comments-menu_a__favorites" data-toggle="tab" href="#favoritesList">Закладки 865</a></li>-->
+        </ul>
+    </div>
+    <section class="comments comments__buble">
+        <?php
+        $commentsWidget->run();
+        //$this->widget('application.widgets.newCommentWidget.NewCommentWidget', array('model' => $data, 'full' => $full));
+        ?>
+    </section>
+    <?php
 }
 ?>
 

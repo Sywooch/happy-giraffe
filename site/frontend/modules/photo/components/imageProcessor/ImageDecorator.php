9a1d3ac0fdfd4df075e65977a0b3925afbbf4ae7
choojoy-work <nikita@happy-giraffe.ru>
2014-09-30 13:08:46 +0400

    sorting

--- site/frontend/modules/photo/components/imageProcessor/ImageDecorator.php
+++ site/frontend/modules/photo/components/imageProcessor/ImageDecorator.php
@@ -18,41 +18,60 @@ class ImageDecorator
     protected $image;
     protected $format;
     protected $animated;
+    protected $options;
 
     public function __construct(ImageInterface $image, FilterInterface $filter, $format, $animated)
     {
         $this->format = $format;
         $this->image = $image;
         $this->animated = $animated;
+
+        $this->options['animated'] = $animated;
         switch ($this->format) {
             case 'gif':
             case 'png':
-            if ($this->animated) {
-                $_filter = new AnimatedGifProcessor($filter);
-            } else {
-                $_filter = new StaticGifProcessor();
-            }
-            $_filter->apply($this->image);
+                if ($this->animated) {
+                    $_filter = new AnimatedGifProcessor($filter);
+                } else {
+                    $_filter = new StaticGifProcessor();
+                }
+                $_filter->apply($this->image);
+            case 'jpg':
+                $this->options['jpeg_quality'] = $this->getJpegQuality($this->image);
+                break;
+            default:
+                throw new \CException('Неподдерживаемый формат');
         }
     }
 
     public function get()
     {
-        return $this->image->get($this->format);
+        return $this->image->get($this->format, $this->options);
     }
 
     public function show()
     {
-        return $this->image->show($this->format);
+        return $this->image->show($this->format, $this->options);
     }
 
     public function save($path)
     {
-        return $this->image->save($path);
+        return $this->image->save($path, $this->options);
     }
 
-    protected function getOptions()
+    protected function getJpegQuality(\Imagine\Imagick\Image $image)
     {
-        if ($this->)
+        $width = $this->filter->getWidth($image->getSize()->getWidth(), $image->getSize()->getHeight());
+        $config = \Yii::app()->thumbs->quality;
+        $q = array_pop($config);
+        $config = array_reverse($config, true);
+        foreach ($config as $minWidth => $quality) {
+            if ($width <= $minWidth) {
+                $q = $quality;
+            } else {
+                break;
+            }
+        }
+        return $q;
     }
 } 

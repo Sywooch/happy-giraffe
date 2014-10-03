666f069327d7bd0a2a4b302ecdb0dda0032b29a9
Nikita Izygin <calacitizen@gmail.com>
2014-10-03 17:19:31 +0400

    adding redactor

--- site/frontend/modules/editorialDepartment/views/redactor/index.php
+++ site/frontend/modules/editorialDepartment/views/redactor/index.php
@@ -17,17 +17,85 @@ $form = $this->beginWidget('site\frontend\components\requirejsHelpers\ActiveForm
 $form->hiddenField($model, 'clubId');
 ?>
 
-Заголовок статьи <?=$form->textField($model, 'title') ?><br />
-Превью (md) <?=$form->textArea($model, 'markDownPreview') ?><br />
-Превью (html) <?=$form->textArea($model, 'htmlTextPreview') ?><br />
-Текст (md) <?=$form->textArea($model, 'markDown') ?><br />
-Текст (html) <?=$form->textArea($model, 'htmlText') ?><br />
-meta.title <?=$form->textField($model, 'meta[title]') ?><br />
-meta.keywords <?=$form->textArea($model, 'meta[keywords]') ?><br />
-meta.description <?=$form->textArea($model, 'meta[description]') ?><br />
-social.title <?=$form->textField($model, 'social[title]') ?><br />
-social.text <?=$form->textArea($model, 'social[text]') ?><br />
-social.image <?=$form->hiddenField($model, 'social[image]') ?><br />
+    <?=$form->hiddenField($model, 'markDownPreview',  array('id' => 'markDownPreview'));?>
+    <?=$form->hiddenField($model, 'htmlTextPreview',  array('id' => 'htmlTextPreview'));?>
+    <?=$form->hiddenField($model, 'markDown',  array('id' => 'markDown'));?>
+    <?=$form->hiddenField($model, 'htmlText',  array('id' => 'htmlText'));?>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">Заголовок</label>
+        <div class="w-400 float-l">
+            <?=$form->textField($model, 'title', array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">Превью</label>
+    </div>
+
+
+    <md-redactor style="display: block; border: 1px solid #e0e1e2; border-radius: 3px;" params="id: 'md-redactor-1', textareaId: 'markDownPreview', htmlId: 'htmlTextPreview'"></md-redactor>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">Текст</label>
+    </div>
+
+    <md-redactor style="display: block; border: 1px solid #e0e1e2; border-radius: 3px;" params="id: 'md-redactor-2', textareaId: 'markDown', htmlId: 'htmlText'"></md-redactor>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">meta.title</label>
+        <div class="w-400 float-l">
+            <?=$form->textArea($model, 'meta[title]',  array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">meta.keywords</label>
+        <div class="w-400 float-l">
+            <?=$form->textArea($model, 'meta[keywords]',  array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">meta.description</label>
+        <div class="w-400 float-l">
+            <?=$form->textArea($model, 'meta[description]',  array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">social.title</label>
+        <div class="w-400 float-l">
+            <?=$form->textArea($model, 'social[title]',  array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+    <div class="b-settings-blue_row clearfix">
+        <div class="clearfix">
+        </div>
+        <label class="b-settings-blue_label" for="BlogContent_title">social.title</label>
+        <div class="w-400 float-l">
+            <?=$form->textArea($model, 'social[text]',  array('class' => 'itx-simple w-400')) ?>
+        </div>
+    </div>
+
+    social.image <?=$form->hiddenField($model, 'social[image]') ?>
+
 <?=CHtml::submitButton('save') ?>
 
 <?php
@@ -37,4 +105,5 @@ $this->endWidget();
  * @var ClientScript $cs
  */
 $cs = Yii::app()->clientScript;
+$cs->registerAMD("md-redactor", array("kow"));
 ?>

31aab132e2af2a656027cb84d911ab851db806cf
choojoy-work <nikita@happy-giraffe.ru>
2014-10-03 18:42:50 +0400

    Чистка

--- site/frontend/modules/developers/controllers/DefaultController.php
+++ site/frontend/modules/developers/controllers/DefaultController.php
@@ -32,4 +32,27 @@ class DefaultController extends HController
 
 		$this->render('index');
 	}
+
+    public function actionTestWeekly()
+    {
+        Yii::import('site.frontend.extensions.YiiMongoDbSuite.*');
+        Yii::import('site.frontend.extensions.*');
+        Yii::import('site.frontend.components.*');
+        Yii::import('site.frontend.helpers.*');
+        Yii::import('site.frontend.modules.messaging.models.*');
+        Yii::import('site.frontend.modules.messaging.components.*');
+        Yii::import('site.frontend.modules.geo.models.*');
+        Yii::import('site.frontend.widgets.userAvatarWidget.Avatar');
+        Yii::import('site.common.models.mongo.*');
+
+        $articles = Favourites::model()->getWeekPosts();
+        if (empty($articles))
+            $articles = CommunityContent::model()->findAll(array(
+                'limit' => 6,
+                'order' => 'id DESC',
+            ));
+        $contents = $this->renderFile(Yii::getPathOfAlias('site.common.tpl.weeklyNews') . '.php', array('models' => $articles), true);
+
+        Yii::app()->email->sendCampaign($contents, HEmailSender::LIST_TEST_LIST);
+    }
 }

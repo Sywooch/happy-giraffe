47f182599b8a45db60e92168b108f81e07150f06
Nikita Izygin <calacitizen@gmail.com>
2014-10-13 12:24:14 +0400

    typo

--- new/javascript/modules/photo/baseUrlCreator.js
+++ new/javascript/modules/photo/baseUrlCreator.js
@@ -1,4 +1,4 @@
-define('baseUrlCreator', ['photo-config'] ,function (photoConfig) {
+define('baseUrlCreator', ['photo-config'], function (photoConfig) {
     var thumb = '/v2/thumbs/';
     if (photoConfig.hostName !== undefined) {
         return photoConfig.hostName + thumb;

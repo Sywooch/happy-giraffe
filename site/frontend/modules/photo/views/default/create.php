9b7d5dceead85f2757a84d7f71c8fe4c25b4bd81
Nikita Izygin <calacitizen@gmail.com>
2014-10-13 18:23:29 +0400

    create model

--- site/frontend/modules/photo/views/default/create.php
+++ site/frontend/modules/photo/views/default/create.php
@@ -1,39 +1,11 @@
 <?php
-$this->pageTitle = 'Как выбрать ' ;
+$this->pageTitle = 'Создание фотоальбома' ;
 
 $this->breadcrumbs = array(
     'мои фото' => array('/photo/default/create'),
     'Создание фотоальбома',
 );
+$cs = Yii::app()->clientScript;
+$cs->registerAMD('photo-albums-create', array('kow'));
 ?>
-
-        <!-- Добавление -->
-        <div class="postAdd">
-            <div class="postAdd_hold margin-t40">
-                <div class="postAdd_row">
-                    <div class="postAdd_count">1</div>
-                    <div class="postAdd_cont">
-                        <div class="inp-valid inp-valid__abs">
-                            <input type="text" placeholder="Заголовок альбома" class="itx-gray">
-                            <div class="inp-valid_count">150</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="postAdd_row">
-                    <div class="postAdd_count">2</div>
-                    <div class="postAdd_cont">
-                        <div class="inp-valid inp-valid__abs">
-                            <textarea rows="5" cols="40" placeholder="Описание альбома" class="itx-gray"></textarea>
-                            <div class="inp-valid_count">450</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="postAdd_row">
-                    <div class="postAdd_count"></div>
-                    <div class="postAdd_cont">
-                        <div class="postAdd_btns-hold"><a href="" class="btn btn-link-gray margin-r15">Отменить</a><a href="" class="btn btn-success btn-xm disabled">Создать альбом</a></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /Добавление -->
+<photo-albums-create></photo-albums-create>

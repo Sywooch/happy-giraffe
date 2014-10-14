0dd16696431f899eb7bca71e5d53940c44180b1a
choojoy-work <nikita@happy-giraffe.ru>
2014-10-14 09:34:54 +0400

    фикс

--- site/frontend/modules/gallery/views/default/window.php
+++ site/frontend/modules/gallery/views/default/window.php
@@ -126,7 +126,9 @@ $model = AlbumPhoto::model()->findByPk($json['initialPhotoId']);
             </div>
 
             <div id="photo-window_banner" class="photo-window_banner clearfix">
-                <iframe src="/google.html?<?=mt_rand(1000000000, 9999999999)?>" width="300" height="250" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"></iframe>
+                <div class="display-ib">
+
+                </div>
             </div>
         </div>
 

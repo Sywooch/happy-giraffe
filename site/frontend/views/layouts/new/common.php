7097959e908a7c551965409d9bdd793e2431a04d
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-10-14 22:45:47 +0200

    fix Roboto font delete

--- site/frontend/views/layouts/new/common.php
+++ site/frontend/views/layouts/new/common.php
@@ -37,7 +37,6 @@ if (! Yii::app()->user->isGuest)
     <?=CHtml::linkTag('shortcut icon', null, '/favicon.bmp')?>
     <!-- including .css-->
     <link rel="stylesheet" type="text/css" href="/new/css/all1.css" />
-    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300&amp;subset=latin,cyrillic-ext,cyrillic">
     <script src="/new/javascript/jquery.tooltipster.js"></script>
     <script src="/new/javascript/modernizr-2.7.1.min.js"></script>
     <!-- wisywig-->

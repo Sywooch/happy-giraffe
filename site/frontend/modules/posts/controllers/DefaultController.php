5f11bdb631433f8e9b82cc87d0124912ca4a84fc
smaragdinus <kirill@happy-giraffe.ru>
2014-10-17 20:50:45 +0900

    ApiModel - добавлено добавление, обновление и кеширование

--- site/frontend/modules/posts/controllers/DefaultController.php
+++ site/frontend/modules/posts/controllers/DefaultController.php
@@ -15,7 +15,9 @@ class DefaultController extends PostController
 
     public function actionIndex()
     {
+        $t = microtime(true);
         var_dump(User::model()->findByPk(83)->attributes);
+        echo microtime(true) - $t;
         $text = ob_get_clean();
         $this->renderText('<pre>' . htmlspecialchars($text) . '</pre>');
     }

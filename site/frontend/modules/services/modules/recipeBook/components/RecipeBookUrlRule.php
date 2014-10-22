12d0ca9a999198f29b7b237269fe09da947a688a
choojoy-work <nikita@happy-giraffe.ru>
2014-10-22 17:21:23 +0400

    Семья

--- site/frontend/modules/services/modules/recipeBook/components/RecipeBookUrlRule.php
+++ site/frontend/modules/services/modules/recipeBook/components/RecipeBookUrlRule.php
@@ -31,13 +31,6 @@ class RecipeBookUrlRule extends \CBaseUrlRule
     {
         Yii::import('site.frontend.modules.services.modules.recipeBook.models.*');
 
-        /** @todo Убрать этот блок через месяц после его появления */
-        if (preg_match('#^recipeBook\/category\/(\w+)$#', $pathInfo, $matches))
-        {
-            $slug = $matches[1];
-            Yii::app()->request->redirect(Yii::app()->createUrl('services/recipeBook/default/category', array('slug' => $slug)));
-        }
-
         $route = false;
 
         if (preg_match('#^recipeBook\/(\w+)$#', $pathInfo, $matches))

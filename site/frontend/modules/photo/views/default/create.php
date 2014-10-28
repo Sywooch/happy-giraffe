66ac6466ac521e0ee7f74eda39bb685ce695ddae
choojoy-work <nikita@happy-giraffe.ru>
2014-10-28 17:27:34 +0300

    Получение URL фотоальбома

--- site/frontend/modules/photo/views/default/create.php
+++ site/frontend/modules/photo/views/default/create.php
@@ -1,10 +1,5 @@
 <?php
 $this->pageTitle = 'Создание фотоальбома' ;
-
-$this->breadcrumbs = array(
-    'мои фото' => array('/photo/default/create'),
-    'Создание фотоальбома',
-);
 $cs = Yii::app()->clientScript;
 $cs->registerAMD('photo-albums-create', array('kow'));
 ?>

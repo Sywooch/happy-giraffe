3c442b30fb4ac19dddf67b51b94f2bb45654e36b
choojoy-work <nikita@happy-giraffe.ru>
2014-10-29 10:59:17 +0300

    Исправление багов

--- site/frontend/modules/profile/widgets/views/UserSectionWidget.php
+++ site/frontend/modules/profile/widgets/views/UserSectionWidget.php
@@ -7,11 +7,18 @@
 <section class="userSection visible-md-block">
     <div class="userSection_hold">
         <div class="userSection_left">
-            <h2 class="userSection_name">Ангелина Богоявленская</h2>
-            <div class="margin-b5 clearfix">33 года (28 января)</div>
-            <div class="location locationsmall clearfix"><span class="flag flag-ua"></span><span class="location_tx">
-                      Ярославская обл.
-                      Переславль-Залесский</span></div>
+            <h2 class="userSection_name"><?=$this->user->getFullName()?></h2>
+            <?php if ($this->user->birthday): ?>
+                <div class="margin-b5 clearfix"><?=$this->user->getNormalizedAge() ?>, <?=$this->user->birthdayString?></div>
+            <?php endif; ?>
+            <?php if (!empty($this->user->address->country_id)): ?>
+            <div class="location locationsmall clearfix">
+                <?=$this->user->address->getFlag(true, 'span')?>
+                <?php if (!empty($user->address->city_id) || !empty($user->address->region_id)): ?>
+                    <span class="location_tx"><?=$this->user->address->getUserFriendlyLocation()?></span>
+                <?php endif; ?>
+            </div>
+            <?php endif; ?>
             <div class="userSection_btn-hold clearfix">
                 <!--
                 виды кнопок друзья
@@ -19,37 +26,57 @@
                 userSection_btn__friend-add
                 userSection_btn__friend-added
                 userSection_btn__friend-append
-                --><a href="" class="userSection_btn userSection_btn__friend-add powertip"><span class="userSection_ico"></span></a><a href="" class="userSection_btn userSection_btn__dialog powertip"><span class="userSection_ico"></span></a>
+                -->
+
+                <!--<a href="" class="userSection_btn userSection_btn__friend-add powertip"><span class="userSection_ico"></span></a>-->
+                <a href="<?=Yii::app()->user->isGuest ? '#loginWidget' : Yii::app()->createUrl('/messaging/default/index', array('interlocutorId' => $user->id))?>" class="userSection_btn userSection_btn__dialog powertip popup-a"><span class="userSection_ico"></span></a>
             </div>
         </div>
         <div class="userSection_center">
-            <div class="userSection_center-reg">с Веселым Жирафом 2 года 5 месяцев 203 дня</div>
+            <div class="userSection_center-reg">с Веселым Жирафом <?=$this->user->withUs()?></div>
             <div class="b-ava-large b-ava-large__nohover">
                 <div class="b-ava-large_ava-hold">
-                    <!-- ava--><a href="#" class="ava ava__large ava__male"><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a>
-                </div><span class="b-ava-large_online">На сайте</span>
+                    <?php $this->widget('Avatar', array('user' => $this->user, 'size' => Avatar::SIZE_LARGE, 'largeAdvanced' => false)); ?>
+                </div>
+                <?php if ($this->user->online): ?>
+                    <span class="b-ava-large_online">На сайте</span>
+                <?php endif; ?>
             </div>
         </div>
         <div class="userSection_right">
-            <!-- b-family-->
-            <div class="b-family">
-                <div class="b-family_top b-family_top__white"></div>
-                <ul class="b-family_ul">
-                    <li class="b-family_li"><a href="" class="b-family_a"><span class="b-family_img-hold"><span class="ico-family ico-family__wife"></span></span><span class="b-family_tx"><span>Жена</span></span></a></li>
-                    <li class="b-family_li"><a href="" class="b-family_a"><span class="b-family_img-hold"><span class="ico-family ico-family__girl-8"></span></span><span class="b-family_tx"><span>Дочь</span><br><span>8 лет</span></span></a></li>
-                    <li class="b-family_li"><a href="" class="b-family_a"><span class="b-family_img-hold"><span class="ico-family ico-family__girl-5"></span></span><span class="b-family_tx"><span>Дочь</span><br><span>5 лет</span></span></a></li>
-                    <li class="b-family_li"><a href="" class="b-family_a"><span class="b-family_img-hold"><span class="ico-family ico-family__boy-small"></span></span><span class="b-family_tx"><span>Сын</span><br><span>2,5 года</span></span></a></li>
-                </ul>
-            </div>
-            <!-- /b-family-->
+            <?php $this->widget('profile.widgets.FamilyWidget', array('user' => $this->user)); ?>
         </div>
     </div>
     <div class="userSection_panel">
-        <ul class="userSection_panel-ul">
-            <li class="userSection_panel-li"><a href="" class="userSection_panel-a">Анкета</a></li>
-            <li class="userSection_panel-li"><a href="" class="userSection_panel-a">Семья </a></li>
-            <li class="userSection_panel-li active"><a href="" class="userSection_panel-a">Блог </a></li>
-            <li class="userSection_panel-li"><a href="" class="userSection_panel-a">Фото</a></li>
-        </ul>
+        <?php
+        $this->widget('zii.widgets.CMenu', array(
+            'htmlOptions' => array(
+                'class' => 'userSection_panel-ul',
+            ),
+            'itemCssClass' => 'userSection_panel-li',
+            'items' => array(
+                array(
+                    'label' => 'Анкета',
+                    'url' => array('/profile/default/index', 'user_id' => $this->user->id),
+                    'linkOptions' => array('class' => 'userSection_panel-a'),
+                ),
+                array(
+                    'label' => 'Семья',
+                    'url' => array('/family/default/index', 'userId' => $this->user->id),
+                    'linkOptions' => array('class' => 'userSection_panel-a'),
+                ),
+                array(
+                    'label' => 'Блог',
+                    'url' => array('/blog/default/index', 'user_id' => $this->user->id),
+                    'linkOptions' => array('class' => 'userSection_panel-a'),
+                ),
+                array(
+                    'label' => 'Фото',
+                    'url' => array('/photo/default/index', 'userId' => $this->user->id),
+                    'linkOptions' => array('class' => 'userSection_panel-a'),
+                ),
+            ),
+        ));
+        ?>
     </div>
 </section>

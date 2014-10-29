816873ae92bca4e332be65bc16a95602c6372599
smaragdinus <kirill@happy-giraffe.ru>
2014-10-29 13:56:52 +0800

    ап

---
+++ site/frontend/modules/posts/controllers/ListController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace site\frontend\modules\posts\controllers;
+
+/**
+ * Description of ListController
+ *
+ * @author Кирилл
+ */
+class ListController extends \LiteController
+{
+    public function actionIndex() {
+        echo 1;
+    }
+}
+
+?>

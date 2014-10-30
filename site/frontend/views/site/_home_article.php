cb5d08c9671b5208fcb6b0916bf9876f46d9b146
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-10-30 15:00:46 +0100

    fix homepage

--- site/frontend/views/site/_home_article.php
+++ site/frontend/views/site/_home_article.php
@@ -6,22 +6,33 @@
     <?php if ($model->type_id == CommunityContent::TYPE_POST): ?>
         <?php if (($photo = $model->getPhoto()) !== null): ?>
             <div class="article-similar_img-hold">
-                <?= CHtml::image($photo->getPreviewUrl(235, null, Image::WIDTH), $photo->title, array('class' => 'content-ing')) ?>
+                <?= CHtml::image($photo->getPreviewUrl(240, null, Image::WIDTH), $photo->title, array('class' => 'article-similar_img')) ?>
             </div>
         <?php elseif ($model->getAttributePhotoCollection('preview', false) && $model->getAttributePhotoCollection('preview')->attachesCount > 0): ?>
             <div class="article-similar_img-hold">
                 <?= CHtml::image(\Yii::app()->thumbs->getThumb($model->getAttributePhotoCollection('preview')->attaches[0]->photo, 'postPreviewSmall')->getUrl(), $model->getAttributePhotoCollection('preview')->attaches[0]->photo->title, array('class' => 'content-ing')) ?>
             </div>
         <?php else: ?>
-            <p><?= $model->getContentText(256) ?></p>
+            <div class="article-similar_text">
+                <p><?= $model->getContentText(256) ?></p>
+            </div>
         <?php endif; ?>
     <?php elseif ($model->type_id == CommunityContent::TYPE_VIDEO): ?>
         <div class="article-similar_img-hold">
-            <?= $model->video->embed ?>
+            <div class="video-container">
+                <?= $model->video->embed ?>
+            </div>
         </div>
     <?php elseif ($model->type_id == CommunityContent::TYPE_PHOTO_POST): ?>
         <div class="article-similar_img-hold">
-            <?php $this->widget('PhotoCollectionViewWidget', array('width' => 235, 'maxHeight' => 100, 'borderSize' => 1, 'href' => $model->url, 'maxRows' => 3, 'minPhotos' => 1, 'collection' => new PhotoPostPhotoCollection(array('contentId' => $model->id)))); ?>
+            <div class="article-similar_count-hold">
+                <!-- КОличество фото в фотопосте -->
+                <?= $model->getPhoto() ?>
+                <div class="article-similar_count-t"> </div>
+                <div class="article-similar_count-tx">фото</div>
+            </div>
+
+            <?php $this->widget('PhotoCollectionViewWidget', array('width' => 240, 'maxHeight' => 100, 'borderSize' => 1, 'href' => $model->url, 'maxRows' => 3, 'minPhotos' => 1, 'collection' => new PhotoPostPhotoCollection(array('contentId' => $model->id)))); ?>
         </div>
     <?php endif; ?>
 </div>

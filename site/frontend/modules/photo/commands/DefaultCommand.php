ce058bb0d0b5d95268c9ee9e7c77ab2780878b0a
choojoy-work <nikita@happy-giraffe.ru>
2014-10-30 09:54:14 +0300

    Исправление багов

--- site/frontend/modules/photo/commands/DefaultCommand.php
+++ site/frontend/modules/photo/commands/DefaultCommand.php
@@ -25,9 +25,7 @@ class DefaultCommand extends \CConsoleCommand
         \Yii::app()->gearman->worker()->addFunction('deferredWrite', array($this, 'deferredWrite'));
         \Yii::app()->gearman->worker()->addFunction('createThumbs', array($this, 'createThumbs'));
 
-        while (\Yii::app()->gearman->worker()->work()) {
-            echo "OK\n";
-        }
+        while (\Yii::app()->gearman->worker()->work());
     }
 
     /**
@@ -36,11 +34,10 @@ class DefaultCommand extends \CConsoleCommand
      */
     public function deferredWrite(\GearmanJob $job)
     {
-//        $data = unserialize($job->workload());
-//        $key = $data['key'];
-//        $content = $data['content'];
-//        \Yii::app()->fs->getAdapter()->getSource()->write($key, $content);
-//        echo "deferredWrite:\n$key\n\n";
+        $data = unserialize($job->workload());
+        $key = $data['key'];
+        $content = $data['content'];
+        \Yii::app()->fs->getAdapter()->getSource()->write($key, $content);
     }
 
     /**
@@ -54,12 +51,33 @@ class DefaultCommand extends \CConsoleCommand
         if ($photo !== null) {
             \Yii::app()->thumbs->createAll($photo);
         }
-        echo "createThumbs\n";
     }
 
     public function actionMigrate()
     {
         $mm = new MigrateManager();
         $mm->moveUserAlbumsPhotos();
+    }
+
+    public function actionSync()
+    {
+        $local = \Yii::app()->fs->getAdapter()->getCache();
+        $source = \Yii::app()->fs->getAdapter()->getSource();
+        $dp = new \CActiveDataProvider('site\frontend\modules\photo\models\Photo');
+        $iterator = new \CDataProviderIterator($dp, 1000);
+        /** @var \site\frontend\modules\photo\models\Photo $photo */
+        foreach ($iterator as $photo) {
+            $fsPath = $photo->getImageFile()->getOriginalFsPath();
+            if ($local->exists($fsPath)) {
+                $data = array(
+                    'key' => $fsPath,
+                    'content' => $local->read($fsPath),
+                );
+                \Yii::app()->gearman->client()->doBackground('deferredWrite', serialize($data));
+                \Yii::app()->gearman->client()->doBackground('createThumbs', $photo->id);
+            } else {
+                echo $photo->id . "\n";
+            }
+        }
     }
 } 

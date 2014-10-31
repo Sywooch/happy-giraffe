500a82f57edba222a188176bf5a57a7ce445fde7
choojoy-work <nikita@happy-giraffe.ru>
2014-10-31 10:40:19 +0300

    Исправление багов

--- site/frontend/widgets/home/views/CounterWidget.php
+++ site/frontend/widgets/home/views/CounterWidget.php
@@ -11,8 +11,8 @@ $visitorsLength = strlen($visitors);
         <div id="counter-users" class="counter-users">
             <?php
                 for ($i = 0; $i < $visitorsLength; $i++):
-                    $showOpening = ($i == 0) || (($visitorsLength - $i) % 3 == 0);
-                    $showClosing = ($visitorsLength - $i - 1) % 3 == 0;
+                    $showOpening = ($i == 0) || (($visitorsLength - $i) % CounterWidget::DIGITS_PER_SECTION == 0);
+                    $showClosing = ($visitorsLength - $i - 1) % CounterWidget::DIGITS_PER_SECTION == 0;
             ?>
                 <?php if ($showOpening): ?><div class="counter-users_dash"><?php endif; ?>
                         <div class="counter-users_digit"><?=substr($visitors, $i, 1)?></div>

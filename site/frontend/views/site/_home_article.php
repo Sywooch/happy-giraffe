94eb6e8193eacb67718bffebe3246fdb2379da6f
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-10-31 13:09:13 +0100

    fix homepage

--- site/frontend/views/site/_home_article.php
+++ site/frontend/views/site/_home_article.php
@@ -1,6 +1,9 @@
 <div class="article-similar article-similar__<?= $color ?>">
     <div class="article-similar_row">
-        <? $this->widget('Avatar', array('user' => $model->author, 'size' => 40)) ?><a href="#" class="article-similar_author"> <?=$model->author->fullName ?></a>
+        <a href="<?=$model->author->url ?>" class="article-similar_header-a">
+            <? $this->widget('Avatar', array('user' => $model->author, 'tag' => 'span', 'size' => 40)) ?>
+            <span class="article-similar_author"> <?=$model->author->fullName ?></span>
+        </a>
     </div>
     <div class="article-similar_row"><a href="<?= $model->url ?>" class="article-similar_t"><?= $model->title ?></a></div>
     <?php if ($model->type_id == CommunityContent::TYPE_POST): ?>
@@ -26,15 +29,13 @@
     <?php elseif ($model->type_id == CommunityContent::TYPE_PHOTO_POST): ?>
         <div class="article-similar_img-hold">
             <div class="article-similar_count-hold">
-                <div class="article-similar_count-t"><?=$model->getPhotoCollection()->attachesCount?></div>
+                <!-- КОличество фото в фотопосте -->
+                <?= $model->getPhoto() ?>
+                <div class="article-similar_count-t"> </div>
                 <div class="article-similar_count-tx">фото</div>
             </div>
 
-            <?php if (false): ?>
-                <?php $this->widget('PhotoCollectionViewWidget', array('width' => 240, 'maxHeight' => 100, 'borderSize' => 1, 'href' => $model->url, 'maxRows' => 3, 'minPhotos' => 1, 'collection' => new PhotoPostPhotoCollection(array('contentId' => $model->id)))); ?>
-            <?php else: ?>
-                тут должен быть виджет фотопоста
-            <?php endif; ?>
+            <?php $this->widget('PhotoCollectionViewWidget', array('width' => 240, 'maxHeight' => 100, 'borderSize' => 1, 'href' => $model->url, 'maxRows' => 3, 'minPhotos' => 1, 'collection' => new PhotoPostPhotoCollection(array('contentId' => $model->id)))); ?>
         </div>
     <?php endif; ?>
 </div>

93480054ec1db22e9d9395eb055926bd7ab0c46c
smaragdinus <kirill@happy-giraffe.ru>
2014-11-10 22:21:29 +0800

    API для вывода количества лайков

--- site/frontend/config/main.php
+++ site/frontend/config/main.php
@@ -126,6 +126,9 @@ return array(
         'posts' => array(
             'class' => 'site\frontend\modules\posts\PostsModule',
         ),
+        'like' => array(
+            'class' => 'site\frontend\modules\like\LikeModule',
+        ),
         'friends',
         'favourites',
         'scores',

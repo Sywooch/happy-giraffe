22020c54608d1c82103601e31c995a75850a0950
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-11-10 12:01:07 +0100

    fix

--- site/frontend/modules/services/modules/recipeBook/views/default/_recipe.php
+++ site/frontend/modules/services/modules/recipeBook/views/default/_recipe.php
@@ -7,8 +7,10 @@
 
 <li class="traditional-recipes_li">
     <div class="traditional-recipes_author">
-        <?php $this->widget('Avatar', array('user' => $data->author, 'size' => Avatar::SIZE_MICRO, 'htmlOptions' => array('class' => 'visible-md-inline-block'))); ?>
-        <a href="<?=$data->author->getUrl()?>" class="a-light"><?=$data->author->getFullName()?></a>
+        <a href="<?=$data->author->getUrl()?>">
+            <?php $this->widget('Avatar', array('user' => $data->author, 'size' => Avatar::SIZE_MICRO, 'tag' => 'span', 'htmlOptions' => array('class' => 'visible-md-inline-block'))); ?>
+            <span  class="a-light"><?=$data->author->getFullName()?></span>
+        </a>
     </div>
     <div class="traditional-recipes_t"><a href="<?=$data->getUrl()?>" class="traditional-recipes_t-a"><?=$data->title?></a></div>
     <?php if ($showDisease): ?>

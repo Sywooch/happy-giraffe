85b1d422c3ec5e492e1e5a7fcb50a6b763f85502
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-11-10 11:19:50 +0100

    fix banner

--- site/frontend/views/banners/_post_footer.php
+++ site/frontend/views/banners/_post_footer.php
@@ -1,4 +1,4 @@
-<div style="margin: 15px 0;">
+<div style="margin: 15px;">
     <?php if ($data instanceof CommunityContent): ?>
     <?php $this->beginWidget('AdsWidget'); ?>
     <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->

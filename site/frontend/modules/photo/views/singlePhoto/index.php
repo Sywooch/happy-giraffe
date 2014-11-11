fdbdb1182a56899a6b0de53727c5308790ef7f2b
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-11-11 14:20:41 +0100

    fix photo link page

--- site/frontend/modules/photo/views/singlePhoto/index.php
+++ site/frontend/modules/photo/views/singlePhoto/index.php
@@ -46,103 +46,99 @@ $commentsWidget = $this->createWidget('site\frontend\modules\comments\widgets\Co
                     </div>
                     <h1 class="b-article_t"><?=$attach->getTitle()?></h1>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="b-main_cont b-main_cont__wide">
-    <!-- b-album-->
-    <section class="b-album b-album__photolink">
-        <div class="b-album_img-hold">
-            <div class="b-album_img-a">
-                <div class="verticalalign-m-help"></div>
-                <div class="verticalalign-m-el">
-                    <?=CHtml::image(Yii::app()->thumbs->getThumb($photo, 'myPhotosAlbumCover')->getUrl(), $attach->getTitle())?>
-                </div>
-            </div>
+                <!-- b-album-->
+                <section class="b-album b-album__photolink">
+                    <div class="b-album_img-hold">
+                        <div class="b-album_img-a">
+                            <div class="verticalalign-m-help"></div>
+                            <div class="verticalalign-m-el">
+                                <?=CHtml::image(Yii::app()->thumbs->getThumb($photo, 'myPhotosAlbumCover')->getUrl(), $attach->getTitle())?>
+                            </div>
+                        </div>
 
-            <div class="b-album_img-hold-ovr">
-                <div class="ico-zoom ico-zoom__abs"></div>
-            </div>
+                        <div class="b-album_img-hold-ovr">
+                            <div class="ico-zoom ico-zoom__abs"></div>
+                        </div>
 
-            <?php if ($attachPrev !== null): ?>
-                <a href="<?=$attachPrev->getUrl()?>" class="i-photo-arrow i-photo-arrow__l i-photo-arrow__abs"></a>
-            <?php endif; ?>
+                        <?php if ($attachPrev !== null): ?>
+                            <a href="<?=$attachPrev->getUrl()?>" class="i-photo-arrow i-photo-arrow__l i-photo-arrow__abs"></a>
+                        <?php endif; ?>
 
-            <?php if ($attachNext !== null): ?>
-                <a href="<?=$attachNext->getUrl()?>" class="i-photo-arrow i-photo-arrow__r i-photo-arrow__abs"></a>
-            <?php endif; ?>
-        </div>
-    </section>
-</div>
-
-<div class="b-main_cont">
-    <div class="b-main_col-hold clearfix">
-        <div class="b-main_col-article">
-        <!-- b-article-->
-        <div class="b-article clearfix">
-            <div class="wysiwyg-content">
-                <p><?=$photo->description?></p>
-            </div>
-        </div>
-        <!--<div class="textalign-c visible-md-block">
-            <div class="like-control like-control__line">
-                <div class="like-control_hold"><a href="" title="Нравится" class="like-control_i like-control_i__like powertip">
-                        <div class="like-control_t">Мне нравится!</div>
-                        <div class="ico-action-hg ico-action-hg__like"></div>
-                        <div class="like-control_tx">865</div></a></div>
-                <div class="like-control_hold"><a href="" title="В избранное" class="like-control_i like-control_i__idea powertip">
-                        <div class="like-control_t">В закладки</div>
-                        <div class="ico-action-hg ico-action-hg__favorite"></div>
-                        <div class="like-control_tx">863455</div></a></div>
-            </div>
-         /div>-->
-        <?php $this->widget('application.widgets.yandexShareWidget.YandexShareWidget', array('model' => $photo, 'lite' => true)); ?>
-        <!-- Реклама яндекса-->
-        <?php $this->renderPartial('//banners/_direct_others'); ?>
-        <!-- comments-->
-        <section class="comments comments__buble">
-            <div class="comments-menu">
-                <ul data-tabs="tabs" class="comments-menu_ul">
-                    <li class="comments-menu_li active"><a href="#commentsList" data-toggle="tab" class="comments-menu_a comments-menu_a__comments">Комментарии <?=$commentsWidget->count?> </a></li>
-                    <!--<li class="comments-menu_li"><a href="#likesList" data-toggle="tab" class="comments-menu_a comments-menu_a__likes">Нравится 865</a></li>
-                    <li class="comments-menu_li"><a href="#favoritesList" data-toggle="tab" class="comments-menu_a comments-menu_a__favorites">Закладки 865</a></li>-->
-                </ul>
-            </div>
-            <div class="tab-content">
-                <?php $commentsWidget->run(); ?>
-                <!--<div id="likesList" class="comments_hold tab-pane">
-                    <div class="list-subsribe-users">
-                        <ul class="list-subsribe-users_ul">
-                            <li class="list-subsribe-users_li">
-                                <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
-                                <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-secondary btn-sl">Читаю</a>
+                        <?php if ($attachNext !== null): ?>
+                            <a href="<?=$attachNext->getUrl()?>" class="i-photo-arrow i-photo-arrow__r i-photo-arrow__abs"></a>
+                        <?php endif; ?>
+                    </div>
+                    <div class="b-album_overlay">
+                        <a class="b-album_r"><div class="b-album_tx">Смотреть  <br> все фото &nbsp;</div><div class="b-album_ico-album"></div><div class="b-album_arrow-all"></div></a>
+                        <ul class="b-album_prev clearfix visible-md-block">
+                            
+                            <li class="b-album_prev-li">
+                                <a href="#" class="b-album_prev-a"><img src="/lite/images/example/w104-h70-1.jpg" alt="" class="b-album_prev-img"><div class="b-album_prev-hold"></div></a>
                             </li>
-                            <li class="list-subsribe-users_li">
-                                <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
-                                <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-success btn-sl"><span class="ico-plus"></span>Подписаться</a>
+                            <li class="b-album_prev-li">
+                                <a href="#" class="b-album_prev-a"><img src="/lite/images/example/w46-h70-1.jpg" alt="" class="b-album_prev-img"><div class="b-album_prev-hold"></div></a>
+                            </li>
+                            <li class="b-album_prev-li">
+                                <a href="#" class="b-album_prev-a"><img src="/lite/images/example/w104-h70-2.jpg" alt="" class="b-album_prev-img"><div class="b-album_prev-hold"></div></a>
                             </li>
                         </ul>
                     </div>
+                </section>
+
+                <!-- b-article-->
+                <div class="b-article clearfix">
+                    <div class="wysiwyg-content">
+                        <p><?=$photo->description?></p>
+                    </div>
                 </div>
-                <div id="favoritesList" class="comments_hold tab-pane">
-                    <div class="list-subsribe-users">
-                        <ul class="list-subsribe-users_ul">
-                            <li class="list-subsribe-users_li">
-                                <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
-                                <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-success btn-sl"><span class="ico-plus"></span>Подписаться</a>
-                            </li>
-                            <li class="list-subsribe-users_li">
-                                <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
-                                <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-secondary btn-sl">Читаю</a>
-                            </li>
+
+                <?php $this->widget('application.widgets.yandexShareWidget.YandexShareWidget', array('model' => $photo, 'lite' => true)); ?>
+                <!-- Реклама яндекса-->
+                <?php $this->renderPartial('//banners/_direct_others'); ?>
+                <!-- comments-->
+                <section class="comments comments__buble">
+                    <div class="comments-menu">
+                        <ul data-tabs="tabs" class="comments-menu_ul">
+                            <li class="comments-menu_li active"><a href="#commentsList" data-toggle="tab" class="comments-menu_a comments-menu_a__comments">Комментарии <?=$commentsWidget->count?> </a></li>
+                            <!--<li class="comments-menu_li"><a href="#likesList" data-toggle="tab" class="comments-menu_a comments-menu_a__likes">Нравится 865</a></li>
+                            <li class="comments-menu_li"><a href="#favoritesList" data-toggle="tab" class="comments-menu_a comments-menu_a__favorites">Закладки 865</a></li>-->
                         </ul>
                     </div>
-                </div>-->
+                    <div class="tab-content">
+                        <?php $commentsWidget->run(); ?>
+                        <!--<div id="likesList" class="comments_hold tab-pane">
+                            <div class="list-subsribe-users">
+                                <ul class="list-subsribe-users_ul">
+                                    <li class="list-subsribe-users_li">
+                                        <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
+                                        <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-secondary btn-sl">Читаю</a>
+                                    </li>
+                                    <li class="list-subsribe-users_li">
+                                        <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
+                                        <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-success btn-sl"><span class="ico-plus"></span>Подписаться</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div id="favoritesList" class="comments_hold tab-pane">
+                            <div class="list-subsribe-users">
+                                <ul class="list-subsribe-users_ul">
+                                    <li class="list-subsribe-users_li">
+                                        <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
+                                        <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-success btn-sl"><span class="ico-plus"></span>Подписаться</a>
+                                    </li>
+                                    <li class="list-subsribe-users_li">
+                                        <a href="#" class="ava ava__middle"><span class="ico-status ico-status__online"></span><img alt="" src="http://img.happy-giraffe.ru/thumbs/200x200/167771/ava9a3e33bd8a5a29146175425a5281390d.jpg" class="ava_img"></a><a class="a-light">Ангелина Богоявленская</a>
+                                        <time datetime="1957-10-04" class="tx-date">Сегодня 13:25</time><a class="btn btn-secondary btn-sl">Читаю</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>-->
+                    </div>
+                </section>
+                <!-- /comments-->
             </div>
-        </section>
-        <!-- /comments-->
         </div>
     </div>
 </div>

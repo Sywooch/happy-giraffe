10af6d78c5cb4ae3b5028673fdaa1b726ea91535
choojoy-work <nikita@happy-giraffe.ru>
2014-11-13 12:45:38 +0300

    Главная страница

---
+++ site/seo/modules/competitors/components/LiSitesManager.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Никита
+ * @date 13/11/14
+ */
+
+class LiSitesManager
+{
+    public static function getData()
+    {
+        $sites = Site::model()->findAll(array(
+            'order' => 'id ASC',
+        ));
+        foreach ($sites as $site) {
+            echo self::getKeywordsCount($site);
+            die;
+        }
+    }
+
+    protected static function getKeywordsCount(Site $site)
+    {
+        $criteria = new CDbCriteria();
+        $criteria->select = new CDbExpression('COUNT(DISTINCT keyword_id)');
+        $criteria->compare('site_id', $site->id);
+        return SiteKeywordVisit::model()->count($criteria);
+    }
+} 

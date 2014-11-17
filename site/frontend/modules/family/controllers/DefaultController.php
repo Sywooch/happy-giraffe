4a9c1cc69656e254d15f0e3fcc4659df2f1734f9
choojoy-work <nikita@happy-giraffe.ru>
2014-11-17 17:41:00 +0300

    Шаблоны

--- site/frontend/modules/family/controllers/DefaultController.php
+++ site/frontend/modules/family/controllers/DefaultController.php
@@ -8,13 +8,39 @@ class DefaultController extends \LiteController
 {
     public $litePackage = 'family';
 
-	public function actionIndex($userId)
+    public function filters()
+    {
+        return array(
+            'accessControl - index',
+        );
+    }
+
+    public function accessRules()
+    {
+        return array(
+            array('deny',
+                'users' => array('?'),
+            ),
+        );
+    }
+
+    public function actionIndex($userId)
 	{
+        $this->render('empty');
+
         /** @var \site\frontend\modules\family\models\Family $family */
         $family = Family::model()->with('members')->hasMember($userId)->find();
-        if ($family === null) {
+        if ($family !== null) {
+            $this->render('index', compact('family',  'userId'));
+        } elseif (\Yii::app()->user->id == $userId) {
+            $this->render('empty');
+        } else {
             throw new \CHttpException(404);
         }
-		$this->render('index', compact('family',  'userId'));
 	}
+
+    public function actionFill()
+    {
+        $this->render('fill');
+    }
 }

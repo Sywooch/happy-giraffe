ffda2402412c5d845f500e8317b11a04d200cbc3
choojoy-work <nikita@happy-giraffe.ru>
2014-11-17 16:41:47 +0300

    Lazy ads

---
+++ site/frontend/widgets/views/AdsWidget.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var string $contents
+ */
+?>
+
+<div class="ad" data-lazyad data-matchmedia="only screen and (min-width: 800px)">
+    <script type="text/lazyad">
+        <?=$contents?>
+    </script>
+</div>

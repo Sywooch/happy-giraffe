f3e69c2d6f8cb02dd79c0e6157308a6c2725f75e
choojoy-work <nikita@happy-giraffe.ru>
2014-11-18 16:45:40 +0300

    Шаблоны

--- site/frontend/views/layouts/forum.php
+++ site/frontend/views/layouts/forum.php
@@ -89,11 +89,9 @@
         <?php $this->renderPartial('_rubrics', array('rubrics'=>$this->forum->rootRubrics)); ?>
 
         <?php if ($this->action->id == 'view'): ?>
-            <?php $this->beginWidget('AdsWidget'); ?>
             <div class="banner">
                 <?php $this->renderPartial('//banners/_sidebar'); ?>
             </div>
-            <?php $this->endWidget(); ?>
         <?php endif; ?>
 
         <?php if (false): ?>

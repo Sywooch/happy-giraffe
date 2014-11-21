754061f69b60abe815432dc956082e18f5266eae
choojoy-work <nikita@happy-giraffe.ru>
2014-11-21 10:51:08 +0300

    Миграция

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,19 +1,9 @@
 <div class="article-banner">
 <?php
-if (Yii::app()->controller instanceof LiteController) {
-    $this->widget('AdsWidget', array(
-        'dummyTag' => 'adfox',
-        'responsiveConfig' => array(
-            AdsWidget::VERSION_TABLET => 'adfox/680x470',
-            AdsWidget::VERSION_DESKTOP => 'adfox/680x470',
-        ),
-    ));
-} else {
     $this->beginWidget('AdsWidget', array(
         'dummyTag' => 'adfox',
     ));
     echo $this->renderPartial('site.frontend.widgets.views.ads.adfox.680x470');
     $this->endWidget();
-}
 ?>
 </div>

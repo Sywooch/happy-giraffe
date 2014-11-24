3f1a263dede7727c007d294f0286117b1b836e07
choojoy-work <nikita@happy-giraffe.ru>
2014-11-24 17:56:50 +0300

    Тизер

--- site/frontend/modules/posts/views/post/view.php
+++ site/frontend/modules/posts/views/post/view.php
@@ -161,6 +161,38 @@ $comments = $this->createWidget('site\frontend\modules\comments\widgets\CommentW
                 <!-- _________________________AdFox Asynchronous code END___________________________ -->
             </div>
             <?php $this->endWidget(); ?>
+            <div class="teaser">
+                <!--AdFox START-->
+                <!--giraffe-->
+                <!--Площадка: Весёлый Жираф / * / *-->
+                <!--Тип баннера: Тексто-графические-->
+                <!--Расположение: &lt;сайдбар&gt;-->
+                <script type="text/javascript">
+                    <!--
+                    if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+                    if (typeof(document.referrer) != 'undefined') {
+                        if (typeof(afReferrer) == 'undefined') {
+                            afReferrer = escape(document.referrer);
+                        }
+                    } else {
+                        afReferrer = '';
+                    }
+                    var addate = new Date();
+                    var scrheight = '', scrwidth = '';
+                    if (self.screen) {
+                        scrwidth = screen.width;
+                        scrheight = screen.height;
+                    } else if (self.java) {
+                        var jkit = java.awt.Toolkit.getDefaultToolkit();
+                        var scrsize = jkit.getScreenSize();
+                        scrwidth = scrsize.width;
+                        scrheight = scrsize.height;
+                    }
+                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=exim&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
+                    // -->
+                </script>
+                <!--AdFox END-->
+            </div>
             <?php /*
               <div class="adv-banner adv-banner__bd"><a href=""><img src="/lite/images/example/w240-h400.jpg" alt=""></a></div>
               <!-- Варианты цветов блока

b843dda311bdc7dbdd6ab695092a71e6e584c5e8
Nikita Izygin <calacitizen@gmail.com>
2014-11-27 18:26:45 +0300

    edit family view

--- site/frontend/modules/family/views/default/fill.php
+++ site/frontend/modules/family/views/default/fill.php
@@ -2,4 +2,12 @@
 /**
  * @author Никита
  * @date 17/11/14
- */ 
+ */
+$cs = Yii::app()->clientScript;
+$cs->registerAMD('family-user', array('kow'));
+?>
+<div class="page-col page-col__user">
+    <div class="page-col_cont page-col_cont__in">
+        <family-user></family-user>
+    </div>
+</div>

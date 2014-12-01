698e2329776ff3ee7b52dfa162b0742a6d120b15
choojoy-work <nikita@happy-giraffe.ru>
2014-12-01 19:25:15 +0300

    Фикс

--- site/frontend/modules/photo/views/default/album.php
+++ site/frontend/modules/photo/views/default/album.php
@@ -17,7 +17,7 @@ $cs = Yii::app()->clientScript;
 $cs->registerAMD('photo-album', array('kow'));
 ?>
 
-<?php $this->widget('profile.widgets.UserSectionWidget', array('user' => $this->user)); ?>
+<?php $this->widget('profile.widgets.UserSectionWidget', array('user' => $this->owner)); ?>
 
 <div class="b-main_cont">
     <photo-album params="userId: <?= $userId ?>, albumId: <?= $id ?>"></photo-album>

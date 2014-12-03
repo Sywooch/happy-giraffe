f7f99b82bff352d0ba3717756a70b40ca501b512
choojoy-work <nikita@happy-giraffe.ru>
2014-12-03 10:54:57 +0300

    Фикс

--- site/frontend/modules/family/controllers/DefaultController.php
+++ site/frontend/modules/family/controllers/DefaultController.php
@@ -34,7 +34,7 @@ class DefaultController extends \LiteController
         $this->owner = $user;
 
         /** @var \site\frontend\modules\family\models\Family $family */
-        $family = Family::model()->with('members')->findByPk(152202);
+        $family = Family::model()->with('members')->find(array('limit' => 1));
 
         die($family->id);
 

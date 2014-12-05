65f2715364f67463d9ad3c07c02015380508d038
choojoy-work <nikita@happy-giraffe.ru>
2014-12-05 15:27:07 +0300

    Фикс

--- site/frontend/modules/photo/commands/DefaultCommand.php
+++ site/frontend/modules/photo/commands/DefaultCommand.php
@@ -15,32 +15,34 @@ use site\frontend\modules\photo\models\PhotoAlbum;
 use site\frontend\modules\photo\models\PhotoAttach;
 use site\frontend\modules\photo\models\PhotoCollection;
 
-function fatal_handler()
-{
-    $errfile = "unknown file";
-    $errstr  = "shutdown";
-    $errno   = E_CORE_ERROR;
-    $errline = 0;
 
-    $error = error_get_last();
 
-    if( $error !== NULL) {
-        $errno   = $error["type"];
-        $errfile = $error["file"];
-        $errline = $error["line"];
-        $errstr  = $error["message"];
-
-        echo $errno; echo "\n";
-        echo $errfile; echo "\n";
-        echo $errline; echo "\n";
-        echo $errstr; echo "\n";
-    }
-}
-
-register_shutdown_function("site\frontend\modules\photo\commands\fatal_handler" );
+register_shutdown_function("site\frontend\modules\photo\commands\DefaultCommand::fatal_handler");
 
 class DefaultCommand extends \CConsoleCommand
 {
+    public static function fatal_handler()
+    {
+        $errfile = "unknown file";
+        $errstr  = "shutdown";
+        $errno   = E_CORE_ERROR;
+        $errline = 0;
+
+        $error = error_get_last();
+
+        if( $error !== NULL) {
+            $errno   = $error["type"];
+            $errfile = $error["file"];
+            $errline = $error["line"];
+            $errstr  = $error["message"];
+
+            echo $errno; echo "\n";
+            echo $errfile; echo "\n";
+            echo $errline; echo "\n";
+            echo $errstr; echo "\n";
+        }
+    }
+
     /**
      * Основной воркер, должен быть всегда запущен для корректной работы приложения.
      */

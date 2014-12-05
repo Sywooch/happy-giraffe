522bfbaa25e21d1313c16dd728092b274bd81e3c
choojoy-work <nikita@happy-giraffe.ru>
2014-12-05 10:58:43 +0300

    #FAMILY-29

--- site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_info.php
+++ site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_info.php
@@ -8,7 +8,7 @@
 <div class="family-member_about-in">
     <div class="family-member_about-hold">
         <div class="ico-family-big ico-family-big__<?=$member->getViewData()->getCssClass()?>"></div>
-        <div class="family-member_about-name"><?=$member->getViewData()->getTitle()?></div>
+        <div class="family-member_about-name"><?=$member->getViewData()->getAsString()?></div>
     </div>
     <?php if ($member->description): ?>
         <div class="family-member_about-tx-hold">

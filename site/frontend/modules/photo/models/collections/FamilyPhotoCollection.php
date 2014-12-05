b9c07b4ac5bbff4bcb7dd86f58ebd766e288dba0
choojoy-work <nikita@happy-giraffe.ru>
2014-12-05 15:04:51 +0300

    Фикс

--- site/frontend/modules/photo/models/collections/FamilyPhotoCollection.php
+++ site/frontend/modules/photo/models/collections/FamilyPhotoCollection.php
@@ -1,34 +1,19 @@
 <?php
 /**
  * @author Никита
- * @date 11/11/14
+ * @date 05/12/14
  */
 
 namespace site\frontend\modules\photo\models\collections;
 
 
 use site\frontend\modules\family\models\FamilyMember;
-use site\frontend\modules\notifications\models\User;
-use site\frontend\modules\photo\components\IPublicPhotoCollection;
-use site\frontend\modules\photo\models\PhotoAttach;
 use site\frontend\modules\photo\models\PhotoCollection;
 
-class FamilyPhotoCollection extends PhotoCollection implements IPublicPhotoCollection
+class FamilyPhotoCollection extends PhotoCollection
 {
-    public function getTitle()
-    {
-        $owner = $this->getOwner();
-        if ($owner->id == \Yii::app()->user->id) {
-            return 'Моя семья';
-        } else {
-            return 'Семья - ' . $owner->fullName;
-        }
-    }
-
     public function getOwner()
     {
-        $family = $this->RelatedModelBehavior->relatedModel;
-        $familyMember = FamilyMember::model()->family($family->id)->real()->find();
-        return \User::model()->findByPk($familyMember->user->id);
+        return FamilyMember::model()->family($this->owner->RelatedModelBehavior->relatedModel->id)->real()->find();
     }
 } 

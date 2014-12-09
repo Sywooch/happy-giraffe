8196a7dd265a9cb1bdceba1d9353ac32bb52d3b9
choojoy-work <nikita@happy-giraffe.ru>
2014-12-09 17:17:50 +0300

    RSS + карта

--- site/frontend/modules/rss/controllers/DefaultController.php
+++ site/frontend/modules/rss/controllers/DefaultController.php
@@ -3,6 +3,8 @@
 namespace site\frontend\modules\rss\controllers;
 use site\frontend\modules\rss\components\FeedGenerator;
 
+\Yii::import('ext.EFeed.*');
+
 /**
  * @author Никита
  * @date 28/11/14
@@ -10,8 +12,15 @@ use site\frontend\modules\rss\components\FeedGenerator;
 
 class DefaultController extends \HController
 {
-    public function actionUser($userId)
+    public function actionUser($userId, $page = 0)
     {
+        /** @var \User $user */
+        $user = \User::model()->findByPk($userId);
+        if ($user === null || $user->id == \User::HAPPY_GIRAFFE) {
+            throw new \CHttpException(404);
+        }
+
+        $feed = new \EFeed();
         $dataProvider = new \CActiveDataProvider('site\frontend\modules\posts\models\Content', array(
             'criteria' => new \CDbCriteria(array(
                 'condition' => 'authorId = :authorId',
@@ -19,12 +28,22 @@ class DefaultController extends \HController
                 'order' => 'dtimeCreate DESC',
             )),
         ));
-        $feed = new FeedGenerator($dataProvider);
-        $feed->run();
+        FeedGenerator::fill($feed, $dataProvider, $page);
+        $feed->setTitle('Блог пользователя ' . $user->getFullName());
+        $feed->setLink($this->createAbsoluteUrl('/blog/default/index', array('user_id' => $user->id)));
+        $feed->setDescription(($user->blog_title === null) ? 'Блог - ' . $user->getFullName() : $user->blog_title);
+        $feed->addChannelTag('generator', 'MyBlogEngine 1.1');
+        $feed->addChannelTag('wfw:commentRss', $this->createAbsoluteUrl('/rss/default/comments', array('userId' => $user->id)));
+        if ($dataProvider->pagination->pageCount > ($page + 1)) {
+            $feed->addChannelTag('ya:more', $this->createAbsoluteUrl('/rss/default/user', array('userId' => $user->id, 'page' => $page + 1)));
+        }
+        $feed->addChannelTag('image', array('url' => $user->getAvatarUrl(), 'width' => 72, 'height' => 72));
+        $feed->generateFeed();
     }
 
     public function actionComments($userId, $page = 0)
     {
+        $feed = new \EFeed();
         $dataProvider = new \CActiveDataProvider('site\frontend\modules\comments\models\Comment', array(
             'criteria' => new \CDbCriteria(array(
                 'join' => 'LEFT OUTER JOIN community__contents c ON c.id = t.entity_id AND (t.entity = "CommunityContent" OR t.entity = "BlogContent")',
@@ -33,14 +52,13 @@ class DefaultController extends \HController
                 'order' => 't.created DESC',
             )),
         ));
-        $feed = new FeedGenerator();
-        $feed->fill($dataProvider, $page);
-        $feed->feed->setLink($this->createAbsoluteUrl('/blog/default/index', array('user_id' => $userId)));
-        $feed->feed->addChannelTag('generator', 'MyBlogEngine 1.1:comments');
+        FeedGenerator::fill($feed, $dataProvider, $page);
+        $feed->setLink($this->createAbsoluteUrl('/blog/default/index', array('user_id' => $userId)));
+        $feed->addChannelTag('generator', 'MyBlogEngine 1.1:comments');
         if ($dataProvider->pagination->pageCount > ($page + 1)) {
-            $feed->feed->addChannelTag('ya:more', $this->createAbsoluteUrl('/' . $this->route, array('userId' => $userId, 'page' => $page + 1)));
+            $feed->addChannelTag('ya:more', $this->createAbsoluteUrl('/rss/default/comments', array('userId' => $userId, 'page' => $page + 1)));
         }
-        $feed->feed->addChannelTag('category', 'ya:comments');
-        $feed->feed->generateFeed();
+        $feed->addChannelTag('category', 'ya:comments');
+        $feed->generateFeed();
     }
 } 

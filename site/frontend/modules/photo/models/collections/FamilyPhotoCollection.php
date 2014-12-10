b77cb21ecf39fa5b7015cc9a719be2d6d1bc330a
choojoy-work <nikita@happy-giraffe.ru>
2014-12-10 11:45:14 +0300

    #FAMILY-10

--- site/frontend/modules/photo/models/collections/FamilyPhotoCollection.php
+++ site/frontend/modules/photo/models/collections/FamilyPhotoCollection.php
@@ -12,6 +12,13 @@ use site\frontend\modules\photo\models\PhotoCollection;
 
 class FamilyPhotoCollection extends PhotoCollection
 {
+    public function getRelatedCollections()
+    {
+        return array(
+            $this->owner->getPhotoCollection('all'),
+        );
+    }
+
     public function getOwner()
     {
         $family = $this->RelatedModelBehavior->relatedModel;

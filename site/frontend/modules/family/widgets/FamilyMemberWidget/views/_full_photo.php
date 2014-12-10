6ecfcd0779f53b9416467102c8562ada840aaca9
choojoy-work <nikita@happy-giraffe.ru>
2014-12-10 11:05:14 +0300

    #FAMILY-10

--- site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_photo.php
+++ site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_photo.php
@@ -14,7 +14,7 @@ $cs = Yii::app()->clientScript;
 $cs->registerAMD($id, array('ko' => 'knockout', 'kow' => 'kow', 'sliderBinding' => 'extensions/sliderBinding'), 'ko.applyBindings({}, document.getElementById("' . $id . '"));');
 ?>
 
-<a href="#" class="family-member_i" data-bind="photoSlider: { photo: <?=$widgetAttach?>, collectionId: <?=$familyCollection?> }" id="<?=$id?>">
+<a href="#" class="family-member_i" data-bind="photoSlider: { photo: <?=$widgetAttach->id?>, collectionId: <?=$familyCollection?> }" id="<?=$id?>">
     <img src="<?=Yii::app()->thumbs->getThumb($photo, 'familyMemberImage')?>" alt="" class="family-member_img">
     <div class="family-member_overlay"><span class="ico-zoom ico-zoom__abs"></span></div>
 </a>

054860de5713a036701bedea45f1e9e80f13b69a
choojoy-work <nikita@happy-giraffe.ru>
2014-12-10 10:16:24 +0300

    #FAMILY-26

--- site/frontend/modules/family/views/default/empty.php
+++ site/frontend/modules/family/views/default/empty.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var LiteController $this
+ * @var integer $userId
  */
 $this->pageTitle = 'Моя семья';
 ?>
@@ -15,7 +16,7 @@ $this->pageTitle = 'Моя семья';
             <div class="cap-empty_hold">
                 <div class="cap-empty_img cap-empty_img__family-big"></div>
                 <div class="cap-empty_t">У вас не заполнена информация о вашей семье</div>
-                <div class="cap-empty_tx-sub"><a href='<?=$this->createUrl('/family/default/fill')?>' class='btn btn-success btn-xm'>Заполнить</a></div>
+                <div class="cap-empty_tx-sub"><a href='<?=$this->createUrl('/family/default/fill', array('userId' => $userId))?>' class='btn btn-success btn-xm'>Заполнить</a></div>
             </div>
         </div>
         <!-- /cap-empty-->

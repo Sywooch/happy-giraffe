bf602195ccacfe41ee55b725d2cfdf9e9170b6d5
choojoy-work <nikita@happy-giraffe.ru>
2014-12-10 09:47:20 +0300

    #FAMILY-10

--- site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_photo.php
+++ site/frontend/modules/family/widgets/FamilyMemberWidget/views/_full_photo.php
@@ -3,10 +3,20 @@
  * @var site\frontend\modules\family\widgets\MembersListWidget\MembersListWidget $this
  * @var site\frontend\modules\family\models\FamilyMemberAbstract $member
  */
-$photo = $member->photoCollection->observer->getSingle(0)->photo;
+$attach = $member->photoCollection->observer->getSingle(0);
+$photo = $attach->photo;
+$id = 'familyMember' . $member->id;
+
+/** @var \ClientScript $cs */
+$cs = Yii::app()->clientScript;
+$cs->registerAMD($id, array('ko' => 'knockout', 'kow' => 'kow', 'sliderBinding' => 'extensions/sliderBinding'), 'ko.applyBindings({}, document.getElementById("' . $id . '"));')
 ?>
 
-<a href="#" class="family-member_i">
+<a href="#" class="family-member_i" data-bind="photoSlider: { photoId: <?=$attach->id?>, collectionId: <?=$member->photoCollection->id?> }" id="<?=$id?>">
     <img src="<?=Yii::app()->thumbs->getThumb($photo, 'familyMemberImage')?>" alt="" class="family-member_img">
     <div class="family-member_overlay"><span class="ico-zoom ico-zoom__abs"></span></div>
 </a>
+
+<script>
+
+</script>

514bd01ea226b9c9851b4245522a1abae47b1773
choojoy-work <nikita@happy-giraffe.ru>
2014-12-11 16:06:05 +0300

    creative

--- site/frontend/modules/family/widgets/FamilyMemberWidget/views/index.php
+++ site/frontend/modules/family/widgets/FamilyMemberWidget/views/index.php
@@ -7,17 +7,17 @@ $attach = $familyCollection->observer->getByAttach($this->model->photoCollection
 ?>
 
 <div class="family-member_row clearfix">
-    <?php if ($attach !== null): ?>
+    <?php if ($this->model->photoCollection->observer->getCount() > 0): ?>
         <?php if ($this->getPhotoSide() == $this::PHOTO_SIDE_LEFT): ?>
             <?php $this->render('_full_photo', array('member' => $this->model)); ?>
             <div style="width: <?=$this->getInfoWidth()?>px;" class="family-member_about family-member_about__<?=$this->getColor()?> family-member_about__right">
-                <?php $this->render('_full_info', array('member' => $this->model)); ?>
+                <?php $this->render('_full_info', array('member' => $this->model, 'attach' => $attach)); ?>
             </div>
         <?php else: ?>
             <div style="width: <?=$this->getInfoWidth()?>px;" class="family-member_about family-member_about__<?=$this->getColor()?> family-member_about__left">
                 <?php $this->render('_full_info', array('member' => $this->model)); ?>
             </div>
-            <?php $this->render('_full_photo', array('member' => $this->model)); ?>
+            <?php $this->render('_full_photo', array('member' => $this->model, 'attach' => $attach)); ?>
         <?php endif; ?>
     <?php else: ?>
         <div style="width: 100%;" class="family-member_about family-member_about__<?=$this->getColor()?>">

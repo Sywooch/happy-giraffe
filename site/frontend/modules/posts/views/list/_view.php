a5bc6b78c6b7912d26041ca77b5812743da3fb16
Nikita Izygin <calacitizen@gmail.com>
2014-12-16 15:06:04 +0300

    php article-settings widget

--- site/frontend/modules/posts/views/list/_view.php
+++ site/frontend/modules/posts/views/list/_view.php
@@ -1,3 +1,7 @@
+<?php
+$cs = Yii::app()->clientScript;
+$cs->registerAMD('BlogRecordSettings', array('kow'));
+?>
 <article class="b-article b-article__list clearfix">
     <div class="b-article_cont clearfix">
         <div class="b-article_header clearfix">
@@ -88,24 +92,27 @@
           </div>
           </section> */ ?>
         <!-- /comments-->
-        <div class="article-also">
-            <div class="article-also_row like-control-hold">
-                <?php
-                if ($data->authorId == Yii::app()->user->id) {
-                    ?>
-                    <div class="article-settings">
-                        <div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__settings powertip"></a></div>
-                        <div class="article-settings_hold display-b">
-                            <!--<div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__pin powertip"></a></div>-->
-                            <div class="article-settings_i"><a href="<?= Yii::app()->createUrl('/blog/tmp/index', array('id' => $data->originEntityId)) ?>" class="article-settings_a article-settings_a__edit powertip"></a></div>
-                            <div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__delete powertip"></a></div>
-                        </div>
-                    </div>
-                    <?php
-                }
+        <div class="b-article_like clearfix">
+
+            <?php
+            if ($data->authorId == Yii::app()->user->id) {
                 ?>
-                <!-- при маленьком размере в мобильном исчезают только лайки и избранное-->
-                <!--<div class="like-control like-control__small">
+                <article-settings params="articleId: <?=$data->originEntityId?>, editUrl: <?= Yii::app()->createUrl('/blog/tmp/index', array('id' => $data->originEntityId)) ?>"></article-settings>
+<!--                <div class="article-settings">-->
+<!--                    <div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__settings powertip"></a></div>-->
+<!--                    <div class="article-settings_hold display-b">-->
+<!--                        <!--<div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__pin powertip"></a></div>-->-->
+<!--                        <div class="article-settings_i"><a href="--><?//= Yii::app()->createUrl('/blog/tmp/index', array('id' => $data->originEntityId)) ?><!--" class="article-settings_a article-settings_a__edit powertip"></a></div>-->
+<!--                        <div class="article-settings_i"><a href="#" class="article-settings_a article-settings_a__delete powertip"></a></div>-->
+<!--                    </div>-->
+<!--                </div>-->
+            <?php
+            }
+            ?>
+            <div class="article-also">
+                <div class="article-also_row like-control-hold">
+                    <!-- при маленьком размере в мобильном исчезают только лайки и избранное-->
+                    <!--<div class="like-control like-control__small">
                     <div class="like-control_hold like-control_hold__comment"><a href="<?= $data->commentsUrl ?>" title="Комментировать" class="like-control_i powertip">
                             <div href="#" class="ico-action-hg ico-action-hg__comment"> </div>
                             <div class="like-control_tx">&nbsp;</div></a></div>
@@ -119,14 +126,16 @@
                             <div class="like-control_tx">&nbsp;</div></a>
                     </div>
                 </div>-->
-            </div>
+                </div>
 
-            <!--<div class="article-also_row">
+                <!--<div class="article-also_row">
                 <div class="article-also_tx">Смотреть все <a href="<?= $data->commentsUrl ?>">комментарии</a>
                     <div class="visible-md-inline-block">,<a href="#"> нравится, </a></div>
                     <div class="visible-md-inline-block"><a href="#"> закладки </a></div>.
                 </div>
             </div>-->
+            </div>
         </div>
+
     </div>
 </article>

219cf72bbbfe6213c0652c06f7a931b9d3d9fe11
Nikita Izygin <calacitizen@gmail.com>
2014-12-16 13:51:11 +0300

    article settings showable

--- new/javascript/modules/post-settings/article-settings.js
+++ new/javascript/modules/post-settings/article-settings.js
@@ -2,11 +2,18 @@ define(['jquery', 'knockout', 'text!article-settings/article-settings.html', 'mo
     function ArticleSettings(params) {
         this.removeBlogUrl = '/newblog/remove/';
         this.restoreBlogUrl = '/newblog/restore/';
+        this.settingsClicker = 'a.article-settings_a__settings';
+        this.settingsBlock = 'div.article-settings_hold';
         this.articleId = params.articleId;
         this.editUrl = params.editUrl;
         this.removePost = function removePost() {
             Model.get(this.removeBlogUrl, { id: this.articleId });
         };
+        this.settingsShowHandler = function settingsShowHandler(e) {
+            e.preventDefault();
+            $(this.settingsBlock).toggle();
+        };
+        $(this.settingsClicker).on('click', this.settingsShowHandler.bind(this));
     };
 
     return {

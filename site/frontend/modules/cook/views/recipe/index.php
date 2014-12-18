a20094e96f5caa86b5a8b2613706475534dfba70
smaragdinus <kika.goth@gmail.com>
2014-12-18 13:05:34 +0800

    Фиккс в отображении списка.

--- site/frontend/modules/cook/views/recipe/index.php
+++ site/frontend/modules/cook/views/recipe/index.php
@@ -1,27 +1,25 @@
 <div class="heading-title margin-b10 margin-t15 clearfix">
     <?=CookRecipe::model()->getTypeString($type) ?>
 </div>
-<div class="b-main_cont">
-    <div class="b-main_col-hold clearfix">
-        <?php
-        $this->widget('LiteListView', array(
-            'dataProvider' => $dp,
-            'itemView' => '_recipe',
-            'tagName' => 'div',
-            'htmlOptions' => array(
-                'class' => 'b-main_col-article'
-            ),
-            'itemsTagName' => 'div',
-            'template' => '{items}<div class="yiipagination yiipagination__center">{pager}</div>',
-            'pager' => array(
-                'class' => 'LitePager',
-                'maxButtonCount' => 10,
-                'prevPageLabel' => '&nbsp;',
-                'nextPageLabel' => '&nbsp;',
-                'showPrevNext' => true,
-            ),
-        ));
-        ?>
-        <aside class="b-main_col-sidebar visible-md"></aside>
-    </div>
+<div class="b-main_col-hold clearfix">
+    <?php
+    $this->widget('LiteListView', array(
+        'dataProvider' => $dp,
+        'itemView' => '_recipe',
+        'tagName' => 'div',
+        'htmlOptions' => array(
+            'class' => 'b-main_col-article'
+        ),
+        'itemsTagName' => 'div',
+        'template' => '{items}<div class="yiipagination yiipagination__center">{pager}</div>',
+        'pager' => array(
+            'class' => 'LitePager',
+            'maxButtonCount' => 10,
+            'prevPageLabel' => '&nbsp;',
+            'nextPageLabel' => '&nbsp;',
+            'showPrevNext' => true,
+        ),
+    ));
+    ?>
+    <aside class="b-main_col-sidebar visible-md"></aside>
 </div>

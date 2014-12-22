90907c4786781d34925d32bddfb69c4d447f9bc0
choojoy-work <nikita@happy-giraffe.ru>
2014-12-22 17:04:21 +0300

    Регистрация

---
+++ site/frontend/modules/signup/controllers/TestControl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Никита
+ * @date 22/12/14
+ */
+
+namespace site\frontend\modules\signup\controllers;
+
+
+class TestController
+{
+    public function actionReg()
+    {
+
+    }
+} 

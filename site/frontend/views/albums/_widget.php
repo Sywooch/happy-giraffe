5c436e3e767d18e2880e1ebde9cbfde5bd1d8f15
smaragdinus <kika.goth@gmail.com>
2014-12-24 20:37:24 +0800

    Добавление класса на изображения шириной 580 пикселей, для того, что бы в новом дизайне растянуть их до 600.

--- site/frontend/views/albums/_widget.php
+++ site/frontend/views/albums/_widget.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var AlbumPhoto $model
  * @var bool $edit редактирование
@@ -8,24 +9,26 @@ if (!isset($edit))
 
 $add = empty($model->title) ? '' : ' title="' . $model->title . '" alt="' . $model->title . '"';
 
-if (!$edit) echo '<!-- widget: { entity : "AlbumPhoto", entity_id : "' . $model->id . '" } -->';
+if (!$edit)
+    echo '<!-- widget: { entity : "AlbumPhoto", entity_id : "' . $model->id . '" } -->';
 
 if (isset($parentModel) && in_array(get_class($parentModel), array('Comment', 'MessagingMessage')))
-    //echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img src="' . $model->getPreviewUrl(485, 110, Image::HEIGHT) . '"></a>';
+//echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')"><img src="' . $model->getPreviewUrl(485, 110, Image::HEIGHT) . '"></a>';
     echo '<a class="comments-gray_cont-img-w" onclick="PhotoCollectionViewWidget.open(\'AttachPhotoCollection\', { entityName : \'' . get_class($parentModel) . '\', entityId : \'' . $parentModel->id . '\' }, \'' . $model->id . '\')">' . $model->getPreviewHtml(395, 400, Image::AUTO) . '</a>';
 else {
     if ($model->width >= 580) {
         if (!$edit)
-            echo '<div class="b-article_in-img">';
+            echo '<div class="b-article_in-img b-article_in-img__l">';
         echo '<img src="' . $model->getPreviewUrl(580, 1000) . '" class="content-img"' . $add . '>';
         if (!$edit)
             echo '</div>';
     } else {
         if (!$edit)
-            echo '<div class="clearfix">';
+            echo '<div class="b-article_in-img">';
         echo '<img src="' . $model->getPreviewUrl(580, 1000) . '" class="content-img"' . $add . '>';
         if (!$edit)
             echo '</div>';
     }
 }
-if (!$edit) echo '<!-- /widget -->';
+if (!$edit)
+    echo '<!-- /widget -->';

025902da0fb45662cc6556ac2b21b8d7d09d07b2
smaragdinus <kika.goth@gmail.com>
2014-12-24 11:59:50 +0800

    Хлебные крошки в клубах

--- site/frontend/modules/posts/controllers/CommunityController.php
+++ site/frontend/modules/posts/controllers/CommunityController.php
@@ -15,15 +15,25 @@ class CommunityController extends PostController
     public $litePackage = 'clubs';
     public $layout = '/layouts/newCommunityPost';
     protected $_club = null;
+    protected $_forum = null;
 
     public function getClub()
     {
         if (is_null($this->_club)) {
             $id = \Yii::app()->request->getParam('forum_id');
-            $this->_club = \Community::model()->with('club')->findByPk($id)->club;
+            $this->_forum = \Community::model()->with('club')->findByPk($id);
+            $this->_club = $this->_forum->club;
         }
-        
+
         return $this->_club;
+    }
+
+    public function getForum()
+    {
+        if (is_null($this->_forum)) {
+            $this->getClub();
+        }
+        return $this->_forum;
     }
 
 }
--- site/frontend/modules/posts/views/community/view.php
+++ site/frontend/modules/posts/views/community/view.php
@@ -1,13 +1,18 @@
 <?php
+
 /** @todo перенести обработку $this->post->metaObject в контроллер */
 $this->pageTitle = $this->post->title;
 $this->metaDescription = $this->post->metaObject->description;
 $this->metaNoindex = $this->post->isNoindex;
+
+// Скопировано из site\frontend\modules\community\controllers\DefaultController::actionView
 $this->breadcrumbs = array(
-    '<span class="ava ava__small">' . CHtml::image($this->user->avatarUrl, $this->user->fullName, array('class' => 'ava_img')) . "</span>" => $this->user->profileUrl,
-    'Блог' => Yii::app()->createUrl('/blog/default/index', array('user_id' => $this->user->id)),
+    $this->club->section->title => $this->club->section->getUrl(),
+    $this->club->title => $this->club->getUrl(),
+    (isset($this->club->communities) && count($this->club->communities) > 1) ? $this->forum->title : 'Форум' => $this->forum->getUrl(),
     $this->post->title,
 );
+
 $comments = $this->createWidget('site\frontend\modules\comments\widgets\CommentWidget', array('model' => array(
         /** @todo Исправить класс при конвертации */
         'entity' => 'BlogContent', //$this->post->originEntity,

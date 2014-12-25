fdc57a1865cec59cf14d6fb9b5ada9dffeca42c2
smaragdinus <kika.goth@gmail.com>
2014-12-25 17:56:33 +0800

    Добавлено сообщение об обработке статьи при её добавлении.

--- site/frontend/modules/posts/controllers/PostController.php
+++ site/frontend/modules/posts/controllers/PostController.php
@@ -12,10 +12,11 @@ use site\frontend\modules\posts\models\Content;
  */
 class PostController extends \LiteController
 {
+
     public $litePackage = 'posts';
     public $layout = '/layouts/newBlogPost';
     public $post = null;
-    public $hideUserAdd = false;
+    public $hideUserAdd = true;
     protected $_user = null;
     protected $_leftPost = null;
     protected $_rightPost = null;
@@ -25,21 +26,24 @@ class PostController extends \LiteController
      */
     public function actionView($content_id)
     {
-        // Отработка сигналов
-        \site\frontend\modules\notifications\behaviors\ContentBehavior::$active = true;
         /** @todo добавить условие byService для полноценного использования индекса */
         $this->post = Content::model()->byEntity('CommunityContent', $content_id)->find();
-        // Отключим обработку сигналов, что бы на следующий и предыдущий пост сигналы оставались непрочитанными
-        \site\frontend\modules\notifications\behaviors\ContentBehavior::$active = true;
-        if (!$this->post || $this->post->parsedUrl !== \Yii::app()->request->requestUri)
-            throw new \CHttpException(404);
-        $this->render('view');
+        if (!$this->post || $this->post->parsedUrl !== \Yii::app()->request->requestUri) {
+            // Временная заглушка, если пост ещё не сконвертировался
+            if (\Yii::app()->user->getState('newPost' . $content_id)) {
+                $this->layout = '//layouts/lite/main';
+                $this->render('notYet');
+            } else {
+                throw new \CHttpException(404);
+            }
+        } else {
+            $this->render('view');
+        }
     }
 
     public function getUser()
     {
-        if (is_null($this->_user))
-        {
+        if (is_null($this->_user)) {
             $this->_user = \site\frontend\components\api\models\User::model()->query('get', array(
                 'id' => $this->post->authorId,
                 'avatarSize' => \Avatar::SIZE_MEDIUM,
@@ -51,8 +55,7 @@ class PostController extends \LiteController
 
     public function getLeftPost()
     {
-        if (is_null($this->_leftPost))
-        {
+        if (is_null($this->_leftPost)) {
             $this->_leftPost = Content::model()->cache(3600)->byService('oldBlog')->leftFor($this->post)->find();
         }
 
@@ -61,8 +64,7 @@ class PostController extends \LiteController
 
     public function getRightPost()
     {
-        if (is_null($this->_rightPost))
-        {
+        if (is_null($this->_rightPost)) {
             $this->_rightPost = Content::model()->cache(3600)->byService('oldBlog')->rightFor($this->post)->find();
         }
 
@@ -87,6 +89,7 @@ class PostController extends \LiteController
             );
         }, $models);
     }
+
 }
 
 ?>

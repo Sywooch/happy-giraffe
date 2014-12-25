6362972e070f8e9fb2318c77d05f146ff77d8d69
Nikita Izygin <calacitizen@gmail.com>
2014-12-25 18:23:06 +0300

    view

--- site/frontend/modules/signup/views/test/reg.php
+++ site/frontend/modules/signup/views/test/reg.php
@@ -1,10 +1,15 @@
 <div id="href">
-    <a  data-bind="register: {}">Регистрация</a><br>
-    <a  data-bind="login: {}">Вход</a>
+    <a class="registration-button" data-bind="follow: {}">Регистрация</a><br>
+    <a class="login-button" data-bind="follow: {}">Вход</a>
 </div>
 
 <script type="text/javascript">
-    require(['knockout', 'signup/bindings'], function(ko) {
-        ko.applyBindings({}, document.getElementById('href'));
+    require(['extensions/proceedAuthForms'], function RegLogRem(proceedAuthForms) {
+        proceedAuthForms();
     });
+//    require(['knockout', 'signup/register-form', 'signup/login-form', 'signup/password-recovery-form', 'signup/bindings'], function(ko, RegisterForm, LoginForm, PasswordRecoveryForm) {
+//        ko.applyBindings(RegisterForm.viewModel.prototype, document.getElementById('register-button'));
+//        ko.applyBindings(LoginForm.viewModel.prototype, document.getElementById('login-button'));
+//        ko.applyBindings(PasswordRecoveryForm.viewModel.prototype, document.getElementById('password-recovery-form'));
+//    });
 </script>

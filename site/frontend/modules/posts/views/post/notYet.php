7d1c4cb09b136007054cf73abc092c3e98462487
Happy Giraffe <giraffe@dev.happy-giraffe.ru>
2014-12-25 12:22:55 +0100

    page loaded post

--- site/frontend/modules/posts/views/post/notYet.php
+++ site/frontend/modules/posts/views/post/notYet.php
@@ -1,1 +1,16 @@
-Немного подождите, пост ещё в обработке.
+<!-- Немного подождите, пост ещё в обработке. -->
+
+<div class="b-main_cont b-main_cont__wide">
+    <div class="postAdd b-main_col-hold clearfix">
+      <!-- cap-empty-->
+      <div class="cap-empty cap-empty__post-loaded">
+        <div class="verticalalign-m-help"></div>
+        <div class="cap-empty_hold">
+          <div class="cap-empty_img cap-empty_img__post-loaded"></div>
+          <div class="cap-empty_t">Ваша статья ещё обрабатывается, подождите ещё немного</div>
+          <div class="cap-empty_tx-sub"><a href="#" class="">Вернуться в блог</a></div>
+        </div>
+      </div>
+      <!-- /cap-empty-->
+    </div>
+</div>

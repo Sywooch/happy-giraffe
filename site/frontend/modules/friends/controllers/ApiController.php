f97edd3fff8d56d84909687aa182a00e2682d868
smaragdinus <kika.goth@gmail.com>
2014-12-26 16:30:06 +0800

    API для получения списка друзей

---
+++ site/frontend/modules/friends/controllers/ApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Description of ApiController
+ *
+ * @author Кирилл
+ */
+class ApiController extends \site\frontend\components\api\ApiController
+{
+
+    public function actionList($userId, $limit = 20)
+    {
+        if ($limit > 100)
+            $limit = 20;
+
+        $friends = Friend::model()->with('friend')->together()->findAllByAttributes(array(
+            'user_id' => $userId,
+                ), array(
+            'select' => 'friend_id',
+            'limit' => $limit,
+            'order' => 'friend.online DESC, friend.id ASC'
+        ));
+        
+        $this->data = array_map(function($friend) {
+            return array('id' => $friend->friend_id);
+        }, $friends);
+        
+        $this->success = true;
+    }
+
+}

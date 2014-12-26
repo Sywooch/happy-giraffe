f89e6000ca3533a83d6b2f6f0cdbe981d487f7de
Nikita Izygin <calacitizen@gmail.com>
2014-12-26 15:28:03 +0300

    пограничный случай

--- site/frontend/modules/signup/views/redirect.php
+++ site/frontend/modules/signup/views/redirect.php
@@ -4,11 +4,19 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <script type="text/javascript">
         document.domain = document.domain;
-        registerForm = window.opener.registerForm;
+//        require = window.opener.require
+//        registerForm = window.opener.registerForm;
+        window.opener.require(['signup/register-form'], function (Register) {
+            registerForm = new Register.viewModel();
+            registerForm.social(<?=CJSON::encode($this->params['attributes'])?>);
+            registerForm.__proto__.open(registerForm);
+        });
+//        registerForm = window.opener.registerForm;
+//        console.log(registerForm);
         <?php if ($this->params['fromLogin']): ?>
-        registerForm.prototype.open();
+//        registerForm.prototype.open();
         <?php endif; ?>
-        registerForm.social(<?=CJSON::encode($this->params['attributes'])?>);
+//        registerForm.social(<?//=CJSON::encode($this->params['attributes'])?>//)
         window.close();
     </script>
 </head>

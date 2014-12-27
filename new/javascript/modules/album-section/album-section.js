482a3ef1c7bacc80937fb5becc5314847eeff6c4
Nikita Izygin <calacitizen@gmail.com>
2014-12-27 10:52:23 +0300

    sliderbinding

--- new/javascript/modules/album-section/album-section.js
+++ new/javascript/modules/album-section/album-section.js
@@ -1,4 +1,4 @@
-define(['knockout', 'text!album-section/album-section.html', 'models/Model', 'photo/PhotoAlbum'], function AlbumSectionHandler(ko, template, Model, PhotoAlbum) {
+define(['knockout', 'text!album-section/album-section.html', 'models/Model', 'photo/PhotoAlbum', 'extensions/sliderBinding'], function AlbumSectionHandler(ko, template, Model, PhotoAlbum) {
     function AlbumSection(params) {
         this.randomAlbum = params.randomAlbum;
         this.userId = params.userId;

2342f1556962c76c95f95514a064a1f1cba51346
choojoy-work <nikita@happy-giraffe.ru>
2014-12-28 15:22:42 +0300

    Настройки

--- site/frontend/modules/signup/controllers/ApiController.php
+++ site/frontend/modules/signup/controllers/ApiController.php
@@ -23,7 +23,7 @@ class ApiController extends \site\frontend\components\api\ApiController
             $identity = new UserIdentity($form->user->email, $form->password);
             if ($identity->authenticate()) {
                 \Yii::app()->user->login($identity);
-                \Yii::app()->user->setFlash('redirected', true);
+                sleep(1);
             }
             $this->data = array(
                 'returnUrl' => $this->createUrl('/profile/default/index/', array('user_id' => $form->user->id)),

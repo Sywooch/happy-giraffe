e261c20e9f6ba6f8541466bf365cd5bb4a0353a3
Nikita Izygin <calacitizen@gmail.com>
2014-12-28 10:16:27 +0300

    avatqr

--- new/javascript/modules/avatar-uploader/avatar-uploader.js
+++ new/javascript/modules/avatar-uploader/avatar-uploader.js
@@ -1,6 +1,6 @@
 define(['knockout', 'text!avatar-uploader/avatar-uploader.html'], function AvatarUploaderHandler(ko, template) {
     function AvatarUploader(params) {
-
+        ko.applyBindings(computer, document.getElementById('photo-tab-computer'));
     }
     return {
         viewModel: AvatarUploader,

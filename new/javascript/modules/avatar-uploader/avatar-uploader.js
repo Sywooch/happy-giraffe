3a0cb0be0714af9371ebcc2afe67cf07e7eb0845
Nikita Izygin <calacitizen@gmail.com>
2014-12-28 12:46:17 +0300

    avatar

--- new/javascript/modules/avatar-uploader/avatar-uploader.js
+++ new/javascript/modules/avatar-uploader/avatar-uploader.js
@@ -1,8 +1,8 @@
-define(['knockout', 'text!avatar-uploader/avatar-uploader.html', 'ko_photoUpload', "user-config", 'bootstrap'], function AvatarUploaderHandler(ko, template, uploader, userConfig) {
+define(['knockout', 'text!avatar-uploader/avatar-uploader.html', 'ko_photoUpload', "user-config", 'bootstrap', 'jquery.Jcrop.min'], function AvatarUploaderHandler(ko, template, uploader, userConfig) {
     function AvatarUploader(params) {
         this.initData = params.initData;
         this.initData.form = params.initData;
-        var computer = new uploader.FromComputerSingleViewModel(this.initData);
+        var computer = new uploader.AvatarSingleViewModel(this.initData);
         ko.applyBindings(computer, document.getElementById('photo-tab-computer'));
     }
     return {

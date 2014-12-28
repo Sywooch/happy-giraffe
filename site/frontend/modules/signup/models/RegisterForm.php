ebb5ca309cd2963ed135995760f291ef582ee9c1
choojoy-work <nikita@happy-giraffe.ru>
2014-12-28 11:46:21 +0300

    Настройки

--- site/frontend/modules/signup/models/RegisterForm.php
+++ site/frontend/modules/signup/models/RegisterForm.php
@@ -29,7 +29,7 @@ class RegisterForm extends \CFormModel
             array('firstName', 'length', 'max' => 50),
             array('lastName', 'length', 'max' => 50),
             array('email', 'email'),
-            array('email', 'unique', 'className' => 'User', 'caseSensitive' => false, 'criteria' => array('condition' => 'deleted = 0')),
+            array('email', 'unique', 'className' => 'User', 'caseSensitive' => false, 'criteria' => array('condition' => 'deleted = 0 AND status = :active', 'params' => array(':active' => \User::STATUS_ACTIVE))),
             array('birthday', 'date', 'format' => 'yyyy-M-d'),
             array('gender', 'in', 'range' => array(self::GENDER_FEMALE, self::GENDER_MALE)),
             array('password', 'length', 'min' => 6, 'max' => 15),

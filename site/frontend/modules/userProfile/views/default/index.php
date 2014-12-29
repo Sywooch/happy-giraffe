e99cef8816caf67c7bbb388cbf74e7974061c2a0
choojoy-work <nikita@happy-giraffe.ru>
2014-12-29 15:32:35 +0300

    Настройки

--- site/frontend/modules/userProfile/views/default/index.php
+++ site/frontend/modules/userProfile/views/default/index.php
@@ -41,14 +41,15 @@ $this->breadcrumbs[] = $this->widget('Avatar', array(
                     <div class="profile-cap_hold profile-cap_hold__article">
                         <div class="profile-cap_tx">У вас пока нет активности на сайте, поделитесь чем-нибудь интересным с пользователями Веселого Жирафа.<br><a href="<?= $this->createUrl('/blog/default/form', array('type' => CommunityContent::TYPE_POST, 'useAMD' => true)) ?>" class="fancy">Добавить запись</a></div>
                     </div>
-                <?php endif; ?>
-                <div class="cap-empty cap-empty__user-profile">
-                    <div class="verticalalign-m-help"></div>
-                    <div class="cap-empty_hold">
-                        <div class="cap-empty_img"></div>
-                        <div class="cap-empty_t">Пользователь пока не был активен на сайте </div>
+                <?php else: ?>
+                    <div class="cap-empty cap-empty__user-profile">
+                        <div class="verticalalign-m-help"></div>
+                        <div class="cap-empty_hold">
+                            <div class="cap-empty_img"></div>
+                            <div class="cap-empty_t">Пользователь пока не был активен на сайте </div>
+                        </div>
                     </div>
-                </div>
+                <?php endif; ?>
             <?php endif; ?>
         </div>
         <!--/////-->

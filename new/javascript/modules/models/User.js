f84178f4a19304884949fba26ba8c5c62a696902
Nikita Izygin <calacitizen@gmail.com>
2015-01-14 18:02:04 +0300

    feature-settings

--- new/javascript/modules/models/User.js
+++ new/javascript/modules/models/User.js
@@ -1,9 +1,11 @@
 define(['knockout', 'models/Model', 'user-config'], function PresetManagerHandler(ko, Model, userConfig) {
     var User = {
         getUserUrl: '/api/users/get/',
+        getCurrentUserUrl: '/api/users/getCurrentUser/',
         changePasswordUrl: '/api/users/changePassword/',
         changeEmailUrl: '/api/users/changeEmail/',
         removeUserUrl: '/api/users/remove/',
+        updateUserUrl: '/api/users/update/',
         mailSubscriptionUrl: '/api/users/mailSubscription/',
         isGuest: userConfig.isGuest,
         isModer: userConfig.isModer,
@@ -22,6 +24,12 @@ define(['knockout', 'models/Model', 'user-config'], function PresetManagerHandle
                 return userInst;
             }
         },
+        get: function get(avatarSize) {
+            return Model.get(this.getUserUrl, { id: this.userId, avatarSize: avatarSize });
+        },
+        getCurrentUser: function getCurrentUser(avatarSize) {
+            return Model.get(this.getCurrentUserUrl, { id: this.userId, avatarSize: avatarSize });
+        },
         changePassword: function changePassword(newPassword) {
             return Model.get(this.changePasswordUrl, { id: this.id, password: newPassword });
         },
@@ -31,8 +39,23 @@ define(['knockout', 'models/Model', 'user-config'], function PresetManagerHandle
         remove: function remove() {
             return Model.get(this.removeUserUrl, { id: this.id });
         },
+        update: function update() {
+            return Model.get(this.updateUserUrl, { first_name: this.firstName, last_name: this.lastName, gender: this.gender, birthday: this.birthday });
+        },
         mailSubscribe: function mailSubscribe() {
             return Model.get(this.mailSubscriptionUrl, { id: this.id, value: this.subscriptionMail });
+        },
+        getBirthdayValue: function getBirthdayValue() {
+            return this.year() + '-' +  this.month() + '-' + this.day();
+        },
+        parseSocialServices: function parseSocialServices(socialServices) {
+            var socialObject = {};
+            if (socialServices.length > 0) {
+                for (var service in socialServices) {
+                    socialObject[socialServices[service].service] = socialServices[service];
+                }
+            }
+            return socialObject;
         },
         /**
          * init юзера
@@ -57,14 +80,39 @@ define(['knockout', 'models/Model', 'user-config'], function PresetManagerHandle
 
                 this.lastName = object.lastName;
 
+                this.birthday = object.birthday;
+
                 this.profileUrl = object.profileUrl;
 
                 this.publicChannel = object.publicChannel;
+
+                this.email = object.email;
 
                 this.subscriptionMail = object.subscriptionMail;
 
                 this.fullName = this.fullName();
 
+                return this;
+            }
+        },
+        settleSettings: function settleSettings(object) {
+            if (object !== undefined) {
+                this.id = object.id;
+                this.profileUrl = object.profileUrl;
+                this.publicChannel = object.publicChannel;
+                this.isOnline = object.isOnline;
+                this.firstName = Model.createStdProperty(object.firstName, 'firstName');
+                this.gender = Model.createStdProperty(object.gender, 'gender');
+                this.lastName = Model.createStdProperty(object.lastName, 'lastName');
+                this.birthday = Model.createStdProperty(object.birthday), 'birthday';
+                this.email = Model.createStdProperty(object.email, 'email');
+                this.subscriptionMail = Model.createStdProperty(object.subscriptionMail, 'subscriptionMail');
+                this.socialServices = Model.createStdProperty(this.parseSocialServices(object.socialServices), 'socialServices');
+                this.address = Model.createStdProperty(object.address, 'address');
+                this.birthday.day = ko.observable((object.birthday !== undefined) ? new Date(object.birthday).getDate() : null);
+                this.birthday.month = ko.observable((object.birthday !== undefined) ? new Date(object.birthday).getMonth() + 1 : null);
+                this.birthday.year = ko.observable((object.birthday !== undefined) ? new Date(object.birthday).getFullYear() : null);
+                this.birthday.value = ko.computed(this.getBirthdayValue, this.birthday);
                 return this;
             }
         }
--- new/javascript/modules/user-settings/user-settings.js
+++ new/javascript/modules/user-settings/user-settings.js
@@ -1,5 +1,39 @@
-define(['jquery', 'knockout', 'text!user-settings/user-settings.html'], function userSettingsHandler($, ko, template) {
+define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/User'], function userSettingsHandler($, ko, template, User) {
     function UserSettings(params) {
+        this.userId = User.userId;
+        this.loaded = ko.observable(false);
+        this.user = Object.create(User);
+        this.days = DateRange.days();
+        this.months = DateRange.months();
+        this.years = DateRange.years(1920, 2015);
+        this.getUserHandler = function getUserHandler(userData) {
+            if (userData.success === true) {
+                this.user.settleSettings(userData.data);
+                this.loaded(true);
+                console.log(this.user);
+            }
+        };
+        this.beginEditField = function beginEditField(data, event) {
+            data.editing(true);
+            initSelect2();
+        };
+        this.endEditField = function endEditField(data, event) {
+            var attribute = {};
+            attribute[data.name] = data.value();
+            //this.familyMember.updateMember(attribute).done(this.submitMemberHandler.bind(this));
+            data.editing(false);
+        };
+        User.getCurrentUser(40).done(this.getUserHandler.bind(this));
+
+        function initSelect2() {
+            // Измененный tag select
+            $("select.select-cus__search-off").select2({
+                width: '100%',
+                minimumResultsForSearch: -1,
+                dropdownCssClass: 'select2-drop__search-off',
+                escapeMarkup: function(m) { return m; }
+            });
+        };
 
     };
 

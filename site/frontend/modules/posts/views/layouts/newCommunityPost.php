baf0d188278ac9702113280a20cda1fd312d24fc
smaragdinus <kika.goth@gmail.com>
2015-01-15 15:12:47 +0800

    Вложенные рубрики в некоторых клубах

--- site/frontend/modules/posts/views/layouts/newCommunityPost.php
+++ site/frontend/modules/posts/views/layouts/newCommunityPost.php
@@ -47,11 +47,26 @@ $this->beginContent('//layouts/lite/community');
 
             <div class="menu-simple">
                 <ul class="menu-simple_ul">
-                    <?php foreach ($this->forum->rubrics as $rubric): ?>
-                        <li class="menu-simple_li<?php if ($this->rubric && $this->rubric->id == $rubric->id): ?> active<?php endif; ?>">
-                            <?= HHtml::link($rubric->title, $rubric->url, array('class' => 'menu-simple_a')) ?>
-                        </li>
-                    <?php endforeach; ?>
+                    <?php
+                    foreach ($this->forum->rubrics as $rubric) {
+                        // Если рубрика корневая
+                        if (!$rubric->parent) {
+                            $sub = '';
+                            if(!empty($rubric->childs)) {
+                                $sub = '<ul class="menu-simple_ul">';
+                                foreach ($rubric->childs as $child) {
+                                    $content = HHtml::link($child->title, $child->url, array('class' => 'menu-simple_a'));
+                                    $class = 'menu-simple_li' . (($this->rubric && $this->rubric->id == $child->id) ? ' active' : '');
+                                    $sub .= CHtml::tag('li', array('class' => $class), $content);
+                                }
+                                $sub .= '</ul>';
+                            }
+                            $content = HHtml::link($rubric->title, $rubric->url, array('class' => 'menu-simple_a')) . $sub;
+                            $class = 'menu-simple_li' . (($this->rubric && $this->rubric->id == $rubric->id) ? ' active' : '');
+                            echo CHtml::tag('li', array('class' => $class), $content);
+                        }
+                    }
+                    ?>
                 </ul>
             </div>
 

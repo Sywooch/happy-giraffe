8be337d7f96c92e256ccd5ec605347963076b976
Nikita Izygin <calacitizen@gmail.com>
2015-01-19 18:21:05 +0300

    almoust

--- new/javascript/modules/user-settings/user-settings.js
+++ new/javascript/modules/user-settings/user-settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/User'], function userSettingsHandler($, ko, template, User) {
+define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/User', 'models/Geography'], function userSettingsHandler($, ko, template, User, Geography) {
     function UserSettings(params) {
         this.userId = User.userId;
         this.loaded = ko.observable(false);
@@ -8,6 +8,7 @@ define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/U
         this.years = DateRange.years(1920, 2015);
         this.equalPassword = ko.observable('');
         this.mainPageUrl = '/';
+        this.countries = ko.observableArray();
         this.redirectUser = function redirectUser() {
             document.location.href = this.mainPageUrl;
         };
@@ -20,6 +21,16 @@ define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/U
         this.beginEditField = function beginEditField(data, event) {
             data.editing(true);
             initSelect2();
+        };
+        this.parseCountries = function parseCountries(countriesResponse) {
+            if (countriesResponse.success === true) {
+                this.countries(countriesResponse.data);
+                initGeographySelect2(this.user.address.value().country, this.user.address.value().city);
+            }
+        };
+        this.beginLocationEditField = function beginEditField(data, event) {
+            Geography.getCountries().done(this.parseCountries.bind(this));
+            data.editing(true);
         };
         this.endEditField = function endEditField(data, event) {
             var attribute = {};
@@ -52,7 +63,6 @@ define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/U
             this.user.updateSubscribtion();
         };
         User.getCurrentUser(40).done(this.getUserHandler.bind(this));
-
         function initSelect2() {
             // Измененный tag select
             $("select.select-cus__search-off").select2({
@@ -60,6 +70,50 @@ define(['jquery', 'knockout', 'text!user-settings/user-settings.html', 'models/U
                 minimumResultsForSearch: -1,
                 dropdownCssClass: 'select2-drop__search-off',
                 escapeMarkup: function(m) { return m; }
+            });
+        };
+        function initGeographySelect2(country, city) {
+            // Измененный tag select
+            $(".select-cus__js-country").select2({
+                width: '100%',
+                minimumResultsForSearch: -1,
+                dropdownCssClass: 'select2-drop__search-off select2-drop__separated-first-items',
+                // escapeMarkup: function(m) { return m; }
+            });
+            // Измененный tag select c инпутом поиска
+            $(".select-cus__search-on").select2({
+                width: '100%',
+                dropdownCssClass: 'select2-drop__search-on',
+                searchInputPlaceholder: "Начните вводить",
+                escapeMarkup: function(m) { return m; },
+                initSelection : function (element, callback) {
+                    var data = {id: city.id, text: city.name};
+                    callback(data);
+                },
+                ajax: {
+                    url: "/api/geo/searchCities/",
+                    dataType: 'json',
+                    type: "POST",
+                    delay: 250,
+                    data: function (params) {
+                        return JSON.stringify({
+                            term: params,
+                            countryId: country.id
+                        });
+                    },
+                    results: function (data, page) {
+                        return {
+                            results: $.map(data.data.cities, function (item) {
+                                return {
+                                    text: item.name,
+                                    slug: item.name,
+                                    id: item.id
+                                };
+                            })
+                        };
+                    },
+                    minimumInputLength: 1
+                }
             });
         };
 

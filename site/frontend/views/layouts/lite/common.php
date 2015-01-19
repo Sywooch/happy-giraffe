9a47af0b74165e65fa3e8a0e347e830e1e0c10df
choojoy-work <nikita@happy-giraffe.ru>
2015-01-19 12:45:15 +0300

    Исправление багов

--- site/frontend/views/layouts/lite/common.php
+++ site/frontend/views/layouts/lite/common.php
@@ -38,4 +38,34 @@
 <?php if (Yii::app()->user->isGuest): ?>
     <?php $this->widget('site.frontend.modules.signup.widgets.LayoutWidget'); ?>
 <?php endif; ?>
+<!--AdFox START-->
+<!--giraffe-->
+<!--Площадка: Весёлый Жираф / * / *-->
+<!--Тип баннера: Брендирование-->
+<!--Расположение: бэкграунд-->
+<script type="text/javascript">
+    <!--
+    if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+    if (typeof(document.referrer) != 'undefined') {
+        if (typeof(afReferrer) == 'undefined') {
+            afReferrer = escape(document.referrer);
+        }
+    } else {
+        afReferrer = '';
+    }
+    var addate = new Date();
+    var scrheight = '', scrwidth = '';
+    if (self.screen) {
+        scrwidth = screen.width;
+        scrheight = screen.height;
+    } else if (self.java) {
+        var jkit = java.awt.Toolkit.getDefaultToolkit();
+        var scrsize = jkit.getScreenSize();
+        scrwidth = scrsize.width;
+        scrheight = scrsize.height;
+    }
+    document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/211012/prepareCode?pp=dtx&amp;ps=bkqy&amp;p2=ewfb&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
+    // -->
+</script>
+<!--AdFox END-->
 </body></html>

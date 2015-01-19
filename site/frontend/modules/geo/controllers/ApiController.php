8d3a4b022c6631a28cc5ae9ec3902db0d708af01
choojoy-work <nikita@happy-giraffe.ru>
2015-01-19 12:57:12 +0300

    Исправление багов

--- site/frontend/modules/geo/controllers/ApiController.php
+++ site/frontend/modules/geo/controllers/ApiController.php
@@ -12,4 +12,42 @@ class ApiController extends \site\frontend\components\api\ApiController
         $this->data = $countries;
         $this->success = true;
     }
+
+    public function actionSearchCities($term, $pageLimit = 10, $page = 1, $countryId = null, $regionId = null)
+    {
+        $filter_parts = FilterParts::model()->findAll();
+        foreach ($filter_parts as $filter_part)
+            $term = str_replace($filter_part->part . ' ', '', $term);
+        $term = trim($term);
+
+        $criteria = new CDbCriteria();
+        $criteria->addSearchCondition('t.name', $term);
+        if ($countryId !== null)
+            $criteria->compare('t.country_id', $countryId);
+
+        if ($regionId !== null)
+            $criteria->compare('t.region_id', $regionId);
+
+        $countCriteria = clone $criteria;
+
+        $criteria->limit = $pageLimit;
+        $criteria->offset = $pageLimit * ($page - 1);
+        $criteria->with = array(
+            'district',
+            'region',
+        );
+        $criteria->order = "pos, t.type = 'г' DESC, CASE WHEN t.name like CONCAT(:name, ' %') THEN 0
+                WHEN t.name LIKE CONCAT(:name, '%') THEN 1
+                WHEN t.name LIKE CONCAT('% ', :name, '%') THEN 2
+                ELSE 3
+            END, t.name";
+        $criteria->params[':name'] = $term;
+
+        $cities = GeoCity::model()->findAll($criteria);
+
+        $more = ($pageLimit * $page) < GeoCity::model()->count($countCriteria);
+
+        $this->data = compact('cities', 'more');
+        $this->success = true;
+    }
 } 

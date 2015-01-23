bdd6ce882b31632f36f53ffde98486bf6d36053f
choojoy-work <nikita@happy-giraffe.ru>
2015-01-23 12:22:16 +0300

    Исправление багов

--- site/frontend/views/site/pages/advertiser.php
+++ site/frontend/views/site/pages/advertiser.php
@@ -31,7 +31,7 @@
         _paq.push(["setTrackerUrl", u+"piwik.php"]);
         _paq.push(["setSiteId", "3"]);
         <?php if (! Yii::app()->user->isGuest): ?>
-            _paq.push(['setUserId'], <?=Yii::app()->user->id?>);
+            _paq.push(['setUserId'], '<?=Yii::app()->user->id?>');
         <?php endif; ?>
         var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
         g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);

82e21be63f624b89aa62468b46ccc7f4798d6be6
choojoy-work <nikita@happy-giraffe.ru>
2015-01-28 16:17:36 +0300

    Фикс

--- site/common/models/AlbumPhoto.php
+++ site/common/models/AlbumPhoto.php
@@ -886,6 +886,7 @@ class AlbumPhoto extends HActiveRecord
             $a = explode('/', $url);
             $fsName = implode('/', array_slice($a, -3));
             $photo = \site\frontend\modules\photo\models\Photo::model()->findByAttributes(array('fs_name' => $fsName));
+            var_dump($photo);
             if ($photo->oldPhoto === null) {
                 $criteria = new CDbCriteria();
                 $criteria->addCondition('t.id > :id');
@@ -893,6 +894,7 @@ class AlbumPhoto extends HActiveRecord
                 $photo = \site\frontend\modules\photo\models\Photo::model()->findByAttributes(array(
                     'fs_name' => $fsName,
                 ), $criteria);
+                var_dump($photo);
                 return ($photo === null) ? null : $photo->oldPhoto;
             } else {
                 return $photo->oldPhoto;

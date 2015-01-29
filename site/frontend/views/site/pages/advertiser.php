d83480aefb299e3631a5aa849a58b2c46ed22f6b
choojoy-work <nikita@happy-giraffe.ru>
2015-01-29 11:51:33 +0300

    Фикс

--- site/frontend/views/site/pages/advertiser.php
+++ site/frontend/views/site/pages/advertiser.php
@@ -19,22 +19,3 @@
         </div>
     </div>
 </div>
-
-<!-- Piwik -->
-<script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(["trackPageView"]);
-    _paq.push(["enableLinkTracking"]);
-
-    (function() {
-        var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.happy-giraffe.ru/";
-        _paq.push(["setTrackerUrl", u+"piwik.php"]);
-        _paq.push(["setSiteId", "3"]);
-        <?php if (! Yii::app()->user->isGuest): ?>
-            _paq.push(['setUserId'], '<?=Yii::app()->user->id?>');
-        <?php endif; ?>
-        var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-        g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-    })();
-</script>
-<!-- End Piwik Code -->

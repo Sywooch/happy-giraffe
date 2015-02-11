00570441f5519ee006e1e3829a49ecacfd8e2324
choojoy-work <nikita@happy-giraffe.ru>
2015-02-11 11:14:14 +0300

    Аналитика

--- new/javascript/modules/photo-slider/photo-slider.js
+++ new/javascript/modules/photo-slider/photo-slider.js
@@ -67,6 +67,7 @@ define(['jquery', 'knockout', 'text!photo-slider/photo-slider.html', 'photo/Phot
             //---FCUK quick fix
         };
         this.addViews = function addViews() {
+            _paq.push(['trackPageView']);
             dataLayer.push({'event': 'virtualView'});
             yaCounter11221648.hit(this.current().element().url());
         };

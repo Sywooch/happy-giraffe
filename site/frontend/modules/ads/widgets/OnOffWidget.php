c5d7cbaffb0fbd6ca2a5612c9f5c5427eea3169d
choojoy-work <nikita@happy-giraffe.ru>
2015-02-12 21:15:00 +0300

    Анонсы

--- site/frontend/modules/ads/widgets/OnOffWidget.php
+++ site/frontend/modules/ads/widgets/OnOffWidget.php
@@ -12,16 +12,16 @@ class OnOffWidget extends \CWidget
     public $model;
     public $preset;
     public $line;
+    public $title;
 
     public function run()
     {
-        $ad = Ad::model()->preset($this->preset)->line($this->line)->entity($this->model)->find();
-        $this->render('OnOffWidget', array('on' => ($ad != null)));
+        $this->render('OnOffWidget');
     }
 
     public function getIsActive()
     {
-        $ad = Ad::model()->preset($this->preset)->line($this->line)->entity($this->model)->find();
+        $ad = Ad::model()->preset($this->preset)->line(\Yii::app()->getModule('ads')->lines[$this->line]['lineId'])->entity($this->model)->find();
         return $ad !== null;
     }
 

855d04cfbacb14c0b2f58b738bd32a77cade667e
choojoy-work <nikita@happy-giraffe.ru>
2015-02-12 16:56:31 +0300

    Анонсы

--- site/frontend/modules/ads/views/templates/post.php
+++ site/frontend/modules/ads/views/templates/post.php
@@ -11,12 +11,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Happy Giraffe</title>
-    <link rel="stylesheet" type="text/css" href="http://www.happy-giraffe.ru/lite/css/min/article-anonce.css">
+    <link rel="stylesheet" type="text/css" href="http://beta.happy-giraffe.ru/lite/css/min/article-anonce.css">
     <script type="text/javascript" src="https://raw.githubusercontent.com/davidjbradshaw/iframe-resizer/master/src/iframeResizer.contentWindow.js"></script>
 </head>
 <!--/ layout out -->
 <body>
-<div class="article-anonce article-anonce__red<?php if ($this->size == $this::SIZE_BIG): ?> article-anonce__xl<?php endif; ?>">
+<div class="article-anonce article-anonce__color-<?=$this->model->rubric->forum->club->section_id?><?php if ($this->size == $this::SIZE_BIG): ?> article-anonce__xl<?php endif; ?>">
     <div class="article-anonce_top">
         <a target="_top" href="<?=$this->model->user->profileUrl?>" class="article-anonce_header-a">
             <span href="#" class="ava"><img alt="<?=$this->model->user->fullName?>" src="<?=$this->model->user->avatarUrl?>" class="ava_img"></span>

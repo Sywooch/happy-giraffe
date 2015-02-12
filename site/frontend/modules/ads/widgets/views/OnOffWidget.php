103a16f48bab8bf33cf4570ce07984f173a253c8
choojoy-work <nikita@happy-giraffe.ru>
2015-02-12 21:15:00 +0300

    Анонсы

--- site/frontend/modules/ads/widgets/views/OnOffWidget.php
+++ site/frontend/modules/ads/widgets/views/OnOffWidget.php
@@ -1,1 +1,1 @@
-<ads-toggle params='params: <?=CJSON::encode($this->params)?>, isActive: <?=CJSON::encode($this->isActive)?>'></ads-toggle>
+<ads-toggle params='params: <?=CJSON::encode($this->params)?>, isActive: <?=CJSON::encode($this->isActive)?>'></ads-toggle> <?=$this->title?>

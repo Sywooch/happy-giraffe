2c6610c02f1a79bfc51c222dc066e9f40f0e64d3
choojoy-work <nikita@happy-giraffe.ru>
2015-02-12 15:42:57 +0300

    Анонсы

--- new/javascript/modules/kow.js
+++ new/javascript/modules/kow.js
@@ -50,4 +50,6 @@ define(["jquery", "knockout"], function ($, ko) {
     $('redactor-panel').each(componentIterator);
     ko.components.register('user-settings', { require: 'user-settings/user-settings' });
     $('user-settings').each(componentIterator);
+    ko.components.register('ads-toggle', { require: 'ads-toggle/ads-toggle' });
+    $('ads-toggle').each(componentIterator);
 });

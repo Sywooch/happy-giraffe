70fd0d30e02d9597d1286ec6d4058a95f2d55d19
choojoy-work <nikita@happy-giraffe.ru>
2015-02-13 15:07:02 +0300

    Анонсы

--- site/frontend/modules/ads/views/templates/photoPost.php
+++ site/frontend/modules/ads/views/templates/photoPost.php
@@ -22,7 +22,7 @@
             <span class="article-anonce_author"><?=$this->model->user->fullName?></span>
         </a>
     </div>
-    <a href="%%CLICK_URL_UNESC%%%%DEST_URL%%" class="article-anonce_hold">
+    <a target="_top" href="%%CLICK_URL_UNESC%%%%DEST_URL%%" class="article-anonce_hold">
         <div class="article-anonce_ico-hold">
             <div class="article-anonce_ico-a">
                 <div class="article-anonce_ico"><img src="<?=$this->iconSrc?>" alt="<?=$this->model->title?>" class="article-anonce_img"></div>

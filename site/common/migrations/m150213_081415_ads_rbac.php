fbcd955c29a860d6051818639c8e0aad8977211a
choojoy-work <nikita@happy-giraffe.ru>
2015-02-13 12:04:36 +0300

    Анонсы

--- site/common/migrations/m150213_081415_ads_rbac.php
+++ site/common/migrations/m150213_081415_ads_rbac.php
@@ -4,7 +4,7 @@ class m150213_081415_ads_rbac extends CDbMigration
 {
 	public function up()
 	{
-		$this->execute("INSERT INTO `newauth__items` (`name`, `type`, `description`, `bizrule`, `data`) VALUES ('manageAnnonces', '1', 'Управление анонсами', NULL, NULL);");
+		$this->execute("INSERT INTO `newauth__items` (`name`, `type`, `description`, `bizrule`, `data`) VALUES ('manageAnounces', '1', 'Управление анонсами', NULL, NULL);");
 		$this->execute("INSERT INTO `newauth__items` (`name`, `type`, `description`, `bizrule`, `data`) VALUES ('toggleAnounces', '0', 'Переключение состояния анонсов (вкл./выкл.)', NULL, NULL);");
 		$this->execute("INSERT INTO `newauth__items_childs` (`parent`, `child`) VALUES ('manageAnounces', 'toggleAnounces');");
 	}

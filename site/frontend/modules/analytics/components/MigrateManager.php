7ff62fcfd2993c53336fb56f086aa129265e5453
choojoy-work <nikita@happy-giraffe.ru>
2015-02-16 16:07:08 +0300

    Анонсы

--- site/frontend/modules/analytics/components/MigrateManager.php
+++ site/frontend/modules/analytics/components/MigrateManager.php
@@ -35,6 +35,7 @@ class MigrateManager
         $this->t = time();
         ini_set('max_execution_time', 0);
         ini_set('max_input_time', -1);
+        ini_set('memory_limit', -1);
         set_time_limit(0);
         ini_set('display_errors',1);
         ini_set('display_startup_errors',1);

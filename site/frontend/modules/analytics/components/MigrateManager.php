586cd06e3b28245c4ec204019ecc3d2727620bef
choojoy-work <nikita@happy-giraffe.ru>
2015-02-17 11:10:42 +0300

    Просмотры

--- site/frontend/modules/analytics/components/MigrateManager.php
+++ site/frontend/modules/analytics/components/MigrateManager.php
@@ -39,7 +39,7 @@ class MigrateManager
         set_time_limit(0);
         ini_set('display_errors',1);
         ini_set('display_startup_errors',1);
-        error_reporting(-1);
+        error_reporting(E_ALL);
         foreach ($this->_patterns as $pattern) {
             $this->processByRegex($pattern);
         }

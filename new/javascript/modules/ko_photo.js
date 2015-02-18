48e4d0522ec240cdda17ee44b4ac042d5594cbdc
Nikita Izygin <izygin.work@gmail.com>
2015-02-18 12:17:23 +0300

    Revert "Develop ads reconfigure"

--- new/javascript/modules/ko_photo.js
+++ new/javascript/modules/ko_photo.js
@@ -161,18 +161,10 @@ define('ko_photoUpload', ['jquery', 'knockout', 'knockout.mapping', 'models/Mode
     };
     PhotoUploadViewModel.prototype.removePhotoInternal = function(photo) {
         var self = this;
-        if (ko.isObservable(photo.photo)) {
-            if (photo.photo().status() == PhotoUpload.prototype.STATUS_LOADING) {
-                photo.photo().jqXHR.abort();
-            }
-            PhotoAddViewModel.prototype.removePhotoInternal.call(self, photo.photo());
-        } else {
-            if (photo.status() == PhotoUpload.prototype.STATUS_LOADING) {
-                photo.jqXHR.abort();
-            }
-            PhotoAddViewModel.prototype.removePhotoInternal.call(self, photo);
+        if (photo.status() == PhotoUpload.prototype.STATUS_LOADING) {
+            photo.jqXHR.abort();
         }
-
+        PhotoAddViewModel.prototype.removePhotoInternal.call(self, photo);
     };
 
 
@@ -363,20 +355,13 @@ define('ko_photoUpload', ['jquery', 'knockout', 'knockout.mapping', 'models/Mode
         self.rotateRight = function() {
             self.rotate(true);
         };
+
         self.rotate = function(clockwise) {
-            if (self.hasOwnProperty('photo')) {
-                $.post('/api/photo/photos/rotate/', JSON.stringify({ clockwise : clockwise, photoId : self.photo.id() }), function(response) {
-                    if (response.success) {
-                        ko.mapping.fromJS(new Photo(response.data), {}, self.photo);
-                    }
-                }, 'json');
-            } else {
-                $.post('/api/photo/photos/rotate/', JSON.stringify({ clockwise : clockwise, photoId : self.id() }), function(response) {
-                    if (response.success) {
-                        ko.mapping.fromJS(response.data, {}, self);
-                    }
-                }, 'json');
-            }
+            $.post('/api/photo/photos/rotate/', JSON.stringify({ clockwise : clockwise, photoId : self.id() }), function(response) {
+                if (response.success) {
+                    ko.mapping.fromJS(response.data, {}, self);
+                }
+            }, 'json');
         };
 
         self.cssClass = ko.computed(function() {

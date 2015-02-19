4d5c6f26037ad67350badf80f3f3d0b60f424a56
choojoy-work <nikita@happy-giraffe.ru>
2015-02-19 10:46:26 +0300

    Фикс

--- site/frontend/modules/analytics/commands/ViewsCommand.php
+++ site/frontend/modules/analytics/commands/ViewsCommand.php
@@ -50,7 +50,6 @@ class ViewsCommand extends \CConsoleCommand
                 'order' => 'RAND()',
             ));
             echo $i . "-" . $post->id . "\n";
-            sleep(1);
         }
     }
 

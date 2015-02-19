e3e0276c4866eaffb53a000311ab5dcd7c75bc13
choojoy-work <nikita@happy-giraffe.ru>
2015-02-19 23:14:27 +0300

    Фикс

--- site/frontend/modules/analytics/components/PiwikHttpApi.php
+++ site/frontend/modules/analytics/components/PiwikHttpApi.php
@@ -17,7 +17,7 @@ class PiwikHttpApi extends \CApplicationComponent
     {
         $params = array(
             'period' => 'range',
-            'date' => '2015-02-13,today',
+            'date' => '2015-02-13,yesterday',
             'segment' => 'pageUrl==' . $url,
         );
         $response = $this->makeRequest('VisitsSummary.getVisits', $params);

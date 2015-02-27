d573c9a3a9b24b04123736adaaf6d0dbe9c936a3
choojoy-work <nikita@happy-giraffe.ru>
2015-02-27 12:12:15 +0300

    Фикс

--- site/common/migrations/m150227_081453_contest.php
+++ site/common/migrations/m150227_081453_contest.php
@@ -13,19 +13,7 @@ CREATE TABLE `commentators__contests` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 
-		$this->execute("DROP TABLE IF EXISTS `commentators__contests_comments`;
-
-CREATE TABLE `commentators__contests_comments` (
-  `commentId` int(11) unsigned NOT NULL,
-  `participantId` int(11) unsigned NOT NULL,
-  `counts` tinyint(1) unsigned NOT NULL,
-  PRIMARY KEY (`commentId`,`participantId`),
-  KEY `participantId` (`participantId`),
-  CONSTRAINT `commentators__contests_comments_ibfk_2` FOREIGN KEY (`participantId`) REFERENCES `commentators__contests_participants` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `commentators__contests_comments_ibfk_1` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-
-		$this->execute("DROP TABLE IF EXISTS `commentators__contests_participants`;
+      $this->execute("DROP TABLE IF EXISTS `commentators__contests_participants`;
 
 CREATE TABLE `commentators__contests_participants` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -39,6 +27,18 @@ CREATE TABLE `commentators__contests_participants` (
   KEY `contestId` (`contestId`),
   CONSTRAINT `commentators__contests_participants_ibfk_2` FOREIGN KEY (`contestId`) REFERENCES `commentators__contests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `commentators__contests_participants_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+		$this->execute("DROP TABLE IF EXISTS `commentators__contests_comments`;
+
+CREATE TABLE `commentators__contests_comments` (
+  `commentId` int(11) unsigned NOT NULL,
+  `participantId` int(11) unsigned NOT NULL,
+  `counts` tinyint(1) unsigned NOT NULL,
+  PRIMARY KEY (`commentId`,`participantId`),
+  KEY `participantId` (`participantId`),
+  CONSTRAINT `commentators__contests_comments_ibfk_2` FOREIGN KEY (`participantId`) REFERENCES `commentators__contests_participants` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `commentators__contests_comments_ibfk_1` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 	}
 

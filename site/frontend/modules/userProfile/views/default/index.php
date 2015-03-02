27751074f77f8fb04ab5b8c1fa0d829100f9544c
smaragdinus <kika.goth@gmail.com>
2015-03-02 19:44:01 +0800

    Виджет вывода списка активностей.

--- site/frontend/modules/userProfile/views/default/index.php
+++ site/frontend/modules/userProfile/views/default/index.php
@@ -8,7 +8,7 @@ $this->breadcrumbs[] = $this->widget('Avatar', array(
     'user' => $user,
     'size' => \Avatar::SIZE_MICRO,
     'tag' => 'span',
-), true);
+        ), true);
 ?>
 
 <?php $this->widget('site\frontend\modules\userProfile\widgets\UserSectionWidget', array('user' => $user, 'showToOwner' => true)); ?>
@@ -18,50 +18,24 @@ $this->breadcrumbs[] = $this->widget('Avatar', array(
         <!--/////     -->
         <!-- Основная колонка-->
         <div class="b-main_col-article">
-            <?php if ($dataProvider->totalItemCount > 0): ?>
-            <div class="heading-sm">Моя активность</div>
-                <?php
-                $this->widget('LiteListView', array(
-                    'dataProvider' => $dataProvider,
-                    'itemView' => 'site.frontend.modules.posts.views.list._view',
-                    'tagName' => 'div',
-                    'itemsTagName' => false,
-                    'template' => '{items}<div class="yiipagination yiipagination__center">{pager}</div>',
-                    'pager' => array(
-                        'class' => 'LitePager',
-                        'maxButtonCount' => 10,
-                        'prevPageLabel' => '&nbsp;',
-                        'nextPageLabel' => '&nbsp;',
-                        'showPrevNext' => true,
-                    ),
-                ));
-                ?>
-            <?php else: ?>
-                <?php if ($user->id == Yii::app()->user->id): ?>
-                    <div class="heading-sm">Моя активность</div>
-                    <div class="profile-cap_hold profile-cap_hold__article">
-                        <div class="profile-cap_tx">У вас пока нет активности на сайте, поделитесь чем-нибудь интересным с пользователями Веселого Жирафа.<br><a href="<?= $this->createUrl('/blog/default/form', array('type' => CommunityContent::TYPE_POST, 'useAMD' => true)) ?>" class="fancy">Добавить запись</a></div>
-                    </div>
-                <?php else: ?>
-                    <div class="cap-empty cap-empty__user-profile">
-                        <div class="verticalalign-m-help"></div>
-                        <div class="cap-empty_hold">
-                            <div class="cap-empty_img"></div>
-                            <div class="cap-empty_t">Пользователь пока не был активен на сайте </div>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            <?php endif; ?>
+            <?php
+            $this->widget('site\frontend\modules\som\modules\activity\widgets\ActivityWidget', array(
+                'setNoindexIfEmpty' => true,
+                'setNoindexIfPage' => true,
+                'pageVar' => 'page',
+                'ownerId' => $user->id,
+            ));
+            ?>
         </div>
         <!--/////-->
         <!-- Сайд бар  -->
         <aside class="b-main_col-sidebar visible-md">
             <?php $this->widget('site\frontend\modules\userProfile\widgets\PhotoWidget', compact('user')); ?>
             <!-- виджет друзья-->
-                <friends-section params="userId: <?= $user->id ?>"></friends-section>
+            <friends-section params="userId: <?= $user->id ?>"></friends-section>
             <!-- /виджет друзья-->
             <!-- виджет клубы-->
-                <clubs-section params="userId: <?= $user->id ?>"></clubs-section>
+            <clubs-section params="userId: <?= $user->id ?>"></clubs-section>
             <!-- /виджет клубы-->
 
         </aside>

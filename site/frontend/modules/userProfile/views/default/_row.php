d2b6cabb32f1bbc634e3b77b4ea8f575ad7f1182
choojoy-work <nikita@happy-giraffe.ru>
2015-03-03 10:39:55 +0300

    Фикс

--- site/frontend/modules/userProfile/views/default/_row.php
+++ site/frontend/modules/userProfile/views/default/_row.php
@@ -1,7 +1,6 @@
 <?php $this->renderPartial('site.frontend.modules.posts.views.list._view', compact('data')); ?>
 
 <?php if ($index == 1): ?>
-    123
 <?php $this->widget('site\frontend\modules\comments\modules\contest\widgets\ProfileWidget', array(
         'userId' => $user->id,
     )); ?>

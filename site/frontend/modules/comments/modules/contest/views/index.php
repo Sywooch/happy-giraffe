de180187c017bd17cf9b2e293b970e2cef60a082
choojoy-work <nikita@happy-giraffe.ru>
2015-03-03 10:01:15 +0300

    Фикс

--- site/frontend/modules/comments/modules/contest/views/index.php
+++ site/frontend/modules/comments/modules/contest/views/index.php
@@ -1,15 +1,16 @@
 <?php
-    $cs = Yii::app()->clientScript;
-    $cs->registerAMD('contestCommentsIndex', array('kow'));
-    $cs->registerAMD('contestCommentsButton', array('joinOrAuth' => 'extensions/joinOrAuth', 'ContestComments' => 'models/ContestComments'), 'joinOrAuth(".contest-commentator_btn-orange", ContestComments);');
+$this->pageTitle = 'Лучший комментатор - О конкурсе';
+$cs = Yii::app()->clientScript;
+$cs->registerAMD('contestCommentsIndex', array('kow'));
+$cs->registerAMD('contestCommentsButton', array('joinOrAuth' => 'extensions/joinOrAuth', 'ContestComments' => 'models/ContestComments'), 'joinOrAuth(".contest-commentator_btn-orange", ContestComments);');
 ?>
 <!-- описание конкурса-->
 <div class="contest-commentator-desc">
     <div class="contest-commentator-desc_hold">
         <h3 class="contest-commentator-desc_t">Что нужно для участия?</h3>
-        <div class="contest-commentator-desc_tx">Все очень легко! Просто добавляйте комментарии к тому , что вам интересно, отвечайте на комментарии других.</div>
+        <div class="contest-commentator-desc_tx">Все очень просто! Добавляйте комментарии к тому, что вам нравится и отвечайте на комментарии других.</div>
         <h3 class="contest-commentator-desc_t">Как стать лидером?</h3>
-        <div class="contest-commentator-desc_tx">Для того чтобы стать лидером нужно написать много интересных и полезных комментариев.</div><a href="<?=$this->createUrl('/comments/contest/default/rules', array('contestId' => $this->contest->id))?>" class="contest-commentator-desc_a">Полные правила и рекомендации</a>
+        <div class="contest-commentator-desc_tx">Для того чтобы стать лидером нужно написать много интересных и полезных комментариев.</div><a href="#" class="contest-commentator-desc_a">Полные правила и рекомендации</a>
     </div>
     <?php if (Yii::app()->user->isGuest || ! $this->contest->isRegistered(Yii::app()->user->id)): ?>
         <div class="contest-commentator-desc_btn-hold"> <a href="#" class="btn btn-xxxl contest-commentator_btn-orange" data-bind="joinOrAuthBind: {}">Принять участие!</a></div>

cbb59d10830042c57918bce6f45d7469a2d11962
choojoy-work <nikita@happy-giraffe.ru>
2015-03-03 13:41:26 +0300

    Фикс

--- site/console/commands/SeoTempCommand.php
+++ site/console/commands/SeoTempCommand.php
@@ -111,7 +111,7 @@ class SeoTempCommand extends CConsoleCommand
                     $urlToCount[$url],
                     $post->isRemoved,
                     $post->isNoindex,
-                    $post->authorId,
+                    $post->author->user->profileUrl,
                     $post->uniqueIndex,
                 );
             }

5c726e46367d3a6d97fb4f4c03d4ae2285a4a8a9
choojoy-work <nikita@happy-giraffe.ru>
2015-03-05 13:49:25 +0300

    Фикс

--- site/frontend/modules/comments/modules/contest/widgets/views/MyGiraffeWidget.php
+++ site/frontend/modules/comments/modules/contest/widgets/views/MyGiraffeWidget.php
@@ -5,24 +5,26 @@
  */
 ?>
 
-<div class="w-contest-commentator"><a href="javascript:void(0)" onclick="var link = this; $.post('/ajax/setUserAttribute/', {key: '<?=$this->getAttributeKey()?>', value: '1'}, function() {$(link).parent().hide();});" class="w-contest-commentator_close">Скрыть</a>
-    <div class="w-contest-commentator_date"><?=Yii::app()->dateFormatter->format('d MMMM', $contest->startDate)?> - <?=Yii::app()->dateFormatter->format('d MMMM', $contest->endDate)?></div>
-    <h2 class="w-contest-commentator_t">Лучший комментатор</h2>
-    <div class="w-contest-commentator_t-sub">Лидеры конкурса</div>
-    <div class="w-contest-commentator_avas">
-        <ul class="ava-list">
-            <?php foreach ($leaders as $l): ?>
-                <li class="ava-list_li">
-                    <?php $this->widget('Avatar', array(
-                        'user' => $l->user,
-                    )); ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-    <?php if (Yii::app()->user->isGuest || ! $contest->isRegistered(Yii::app()->user->id)): ?>
-        <div class="w-contest-commentator_btn-hold">
-            <a href="<?=Yii::app()->createUrl('/comments/contest/default/index', array('contestId' => $contest->id))?>" class="btn btn-xxl btn-link">Принять участие!</a>
+<div class="b-article clearfix">
+    <div class="w-contest-commentator giraffe"><a href="javascript:void(0)" onclick="var link = this; $.post('/ajax/setUserAttribute/', {key: '<?=$this->getAttributeKey()?>', value: '1'}, function() {$(link).parent().hide();});" class="w-contest-commentator_close">Скрыть</a>
+        <div class="w-contest-commentator_date"><?=Yii::app()->dateFormatter->format('d MMMM', $contest->startDate)?> - <?=Yii::app()->dateFormatter->format('d MMMM', $contest->endDate)?></div>
+        <h2 class="w-contest-commentator_t">Лучший комментатор</h2>
+        <div class="w-contest-commentator_t-sub">Лидеры конкурса</div>
+        <div class="w-contest-commentator_avas">
+            <ul class="ava-list">
+                <?php foreach ($leaders as $l): ?>
+                    <li class="ava-list_li">
+                        <?php $this->widget('Avatar', array(
+                            'user' => $l->user,
+                        )); ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
-    <?php endif; ?>
+        <?php if (Yii::app()->user->isGuest || ! $contest->isRegistered(Yii::app()->user->id)): ?>
+            <div class="w-contest-commentator_btn-hold">
+                <a href="<?=Yii::app()->createUrl('/comments/contest/default/index', array('contestId' => $contest->id))?>" class="btn btn-xxl btn-link">Принять участие!</a>
+            </div>
+        <?php endif; ?>
+    </div>
 </div>

9a42ce7b0aac8e94d6d9c1495598c25852ec5370
choojoy-work <nikita@happy-giraffe.ru>
2015-03-06 12:30:59 +0300

    Фикс

--- site/common/components/MailQuestionsParser.php
+++ site/common/components/MailQuestionsParser.php
@@ -19,7 +19,7 @@ class MailQuestionsParser
             $i++;
             $url = '/family/?pg=' . $i;
             $this->processPage($url);
-        } while (count($this->emails) < 50000);
+        } while (count($this->emails) < 3000);
     }
 
     protected function processPage($url)

b7500103621bd917cc0dac8534c8c52d793913f8
choojoy-work <nikita@happy-giraffe.ru>
2015-03-06 12:25:55 +0300

    Фикс

--- site/frontend/modules/comments/modules/contest/views/_banner.php
+++ site/frontend/modules/comments/modules/contest/views/_banner.php
@@ -1,3 +1,3 @@
-<?php if ($this->module !== null && $this->module->id !== 'comments/contest'): ?>
+<?php if ($this->module === null || $this->module->id !== 'comments/contest'): ?>
 <div class="layout-header-contest" onclick="document.location.href = '/commentatorsContest/1/';"><a href="/commentatorsContest/1/" class="btn btn-xxl btn-link yellow">Принять участие</a></div>
 <?php endif; ?>

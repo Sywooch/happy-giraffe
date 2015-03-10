9d911e621085716b464a3d12b540c9297b9b57f8
choojoy-work <nikita@happy-giraffe.ru>
2015-03-10 16:26:26 +0300

    Фикс

--- site/frontend/modules/comments/modules/contest/widgets/views/MenuWidget.php
+++ site/frontend/modules/comments/modules/contest/widgets/views/MenuWidget.php
@@ -1,5 +1,5 @@
 <li class="header-menu_li">
-    <a href="<?=Yii::app()->createUrl('/comments/contest/default/index', array('contestId' => $this->contest->id))?>" class="header-menu_a">
+    <a href="<?=Yii::app()->createUrl($this->participant === null ? '/comments/contest/default/index' : '/comments/contest/default/posts', array('contestId' => $this->contest->id))?>" class="header-menu_a">
         <span class="header-menu_ico header-menu_ico__contest"></span>
         <span class="header-menu_tx">Конкурс</span>
         <?php if ($this->participant !== null && $this->participant->score > 0): ?>

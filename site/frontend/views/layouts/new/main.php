f24781cf7f15e8a905847422a36b542910a389c6
choojoy-work <nikita@happy-giraffe.ru>
2015-03-10 10:06:29 +0300

    Фикс

--- site/frontend/views/layouts/new/main.php
+++ site/frontend/views/layouts/new/main.php
@@ -1,5 +1,4 @@
 <?php $this->beginContent('//layouts/new/common'); ?>
-<?php $this->renderPartial('application.modules.comments.modules.contest.views._banner'); ?>
 <?php $this->renderPartial('//_header'); ?>
 <div class="layout-wrapper">
     <?=$content?>

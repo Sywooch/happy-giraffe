0dc5809439dacfaa9f23ec89b2d69506744a2168
choojoy-work <nikita@happy-giraffe.ru>
2015-03-12 14:58:32 +0300

    Фикс

--- site/frontend/views/layouts/lite/common_menu.php
+++ site/frontend/views/layouts/lite/common_menu.php
@@ -4,14 +4,12 @@
  */
 $this->beginContent('//layouts/lite/common');
 ?>
-    <div class="layout-header">
-        <?php $this->renderPartial('application.modules.comments.modules.contest.views._banner'); ?>
-        <?php if (Yii::app()->user->isGuest): ?>
-            <?php $this->renderPartial('//_header_guest'); ?>
-        <?php  else: ?>
-            <?php $this->renderDynamic(array($this, 'renderPartial'), '//_menu', null, true); ?>
-        <?php endif; ?>
-    </div>
+    <?php $this->renderPartial('application.modules.comments.modules.contest.views._banner'); ?>
+    <?php if (Yii::app()->user->isGuest): ?>
+        <?php $this->renderPartial('//_header_guest'); ?>
+    <?php  else: ?>
+        <?php $this->renderDynamic(array($this, 'renderPartial'), '//_menu', null, true); ?>
+    <?php endif; ?>
     <div class="layout-loose_hold clearfix">
         <!-- b-main -->
         <div class="b-main clearfix">

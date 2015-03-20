9f972fb96916733a61c6949ae8b29e69fb9591d4
choojoy-work <nikita@happy-giraffe.ru>
2015-03-20 17:32:40 +0300

    Update _banner.php

--- site/frontend/modules/comments/modules/contest/views/_banner.php
+++ site/frontend/modules/comments/modules/contest/views/_banner.php
@@ -1,5 +1,1 @@
-<div class="layout-header-gold">
-    <div class="layout-header-gold__top">Мы в финале конкурса</div>
-    <div class="layout-header-gold__title">Золотой сайт 2014</div>
-    <div class="layout-header-gold__mean">Важен каждый голос в народном голосовании!</div><a href="http://cup2014.goldensite.ru/" class="btn-link btn-xxl btn-green" target="_blank">Проголосуй за веселого жирафа!</a>
-</div>
+

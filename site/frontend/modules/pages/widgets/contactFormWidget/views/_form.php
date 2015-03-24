1775aa7c0ec5b4ec5e21623fc2f385bab5796150
choojoy-work <nikita@happy-giraffe.ru>
2015-03-24 14:13:44 +0300

    Консультация

--- site/frontend/modules/pages/widgets/contactFormWidget/views/_form.php
+++ site/frontend/modules/pages/widgets/contactFormWidget/views/_form.php
@@ -5,29 +5,42 @@
  */
 Yii::app()->clientScript->registerAMD('contactForm', array('ContactForm' => 'contactForm', 'ko' => 'knockout'), "ko.applyBindings(new ContactForm(), document.getElementById('contactForm'));");
 ?>
-<div class="info-form info-last" id="contactForm">
-    <div class="info-form-title">Напишите нам</div>
-    <div class="info-hero-line">Чем мы можем Вам помочь?</div>
-    <div class="info-form-block">
-        <div class="info-form-block__part">
-            <textarea name="" cols="30" rows="10" placeholder="Введите сообщение" class="itx-gray form-edit_itx" data-bind="textInput: message"></textarea>
-        </div>
-        <div class="info-form-block__part">
-            <input type="text" placeholder="Имя" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: name">
-            <input type="text" placeholder="Компания" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: companyName">
-            <input type="text" placeholder="Email" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: email">
-            <input type="text" placeholder="Телефон" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: phone">
-            <!-- ko if: attach() === false -->
-            <div class="info-form__file file-fake">Прикрепить файл
-                <input type="file" name="file">
+
+<div id="contactForm">
+    <!-- ko if: ! sent() -->
+    <div class="info-form info-last">
+        <div class="info-form-title">Напишите нам</div>
+        <div class="info-hero-line">Чем мы можем Вам помочь?</div>
+        <div class="info-form-block">
+            <div class="info-form-block__part">
+                <textarea name="" cols="30" rows="10" placeholder="Введите сообщение" class="itx-gray form-edit_itx" data-bind="textInput: message"></textarea>
             </div>
-            <!-- /ko -->
-            <!-- ko if: attach() !== false -->
-            <span data-bind="text: attach().fileName"></span>
-            <!-- /ko -->
+            <div class="info-form-block__part">
+                <input type="text" placeholder="Имя" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: name">
+                <input type="text" placeholder="Компания" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: companyName">
+                <input type="text" placeholder="Email" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: email">
+                <input type="text" placeholder="Телефон" class="info-form__input itx-gray form-edit_itx" data-bind="textInput: phone">
+                <!-- ko if: attach() === false -->
+                <div class="info-form__file file-fake">Прикрепить файл
+                    <input type="file" name="file">
+                </div>
+                <!-- /ko -->
+                <!-- ko if: attach() !== false -->
+                <span>Файл прикреплен</span>
+                <!-- /ko -->
+            </div>
+        </div>
+        <div class="info-form-ms text-center">
+            <input type="submit" value="Отправить" class="info-form__button" data-bind="click: send">
         </div>
     </div>
-    <div class="info-form-ms text-center">
-        <input type="submit" value="Отправить" class="info-form__button" data-bind="click: send">
+    <!-- /ko -->
+
+    <!-- ko if: sent() -->
+    <div class="info-form info-last">
+        <div class="info-form-great"><img src="/lite/images/info/mark.png" alt=""></div>
+        <div class="info-form-title">Спасибо предложение принято!</div>
+        <div class="info-hero-line">Наш менеджер свяжется с вами в ближайшее время.</div>
     </div>
+    <!-- /ko -->
 </div>

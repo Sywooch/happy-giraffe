274aecf9908f2c321001b80b97412182842e7d11
choojoy-work <nikita@happy-giraffe.ru>
2015-03-29 17:17:05 +0300

    Консультация

--- site/frontend/modules/consultation/widgets/views/OtherQuestionsWidget.php
+++ site/frontend/modules/consultation/widgets/views/OtherQuestionsWidget.php
@@ -6,7 +6,7 @@
 ?>
 
 <div class="b-consult-aside">
-    <div class="b-consult-aside__title">Другие вопросы <a href="<?=Yii::app()->createUrl('/consultation/default/index', array('slug' => $this->question->consultation->slug))?>" class="b-consult-aside__title__all">Все вопросы <?=$count?></a></div>
+    <div class="b-consult-aside__title">Другие вопросы <a href="<?=Yii::app()->controller->createUrl('index', array('slug' => $this->question->consultation->slug))?>" class="b-consult-aside__title__all">Все вопросы <?=$count?></a></div>
     <?php foreach ($questions as $question): ?>
     <div class="b-consult-aside-item">
         <a href="<?=$question->user->profileUrl?>" class="b-consult-aside-item__ava"><img src="<?=$question->user->avatarUrl?>" alt=""></a>

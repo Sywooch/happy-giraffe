3ae78898e8a264c0415c4b6a3a275b0a066c663b
choojoy-work <nikita@happy-giraffe.ru>
2015-03-30 13:30:42 +0300

    Консультация

--- site/frontend/modules/consultation/views/default/_question.php
+++ site/frontend/modules/consultation/views/default/_question.php
@@ -14,13 +14,13 @@
                 <div class="b-consult-qa-ms__message__text"><?=$data->text?></div>
             </div>
         </div>
-        <?php if (Yii::app()->user->checkAccess('manageOwnContent', array('entity' => $data))): ?>
+        <?php if ($data->answer === null && Yii::app()->user->checkAccess('manageOwnContent', array('entity' => $data))): ?>
             <a href="<?=$this->createUrl('create', array('slug' => $this->consultation->slug, 'questionId' => $data->id))?>">Редактировать</a>
         <?php endif; ?>
     </div>
     <?php if ($data->answer): ?>
     <div class="b-consult-qa-ms__answer comments_li__red">
-        <div class="b-consult-qa-ms__img"><img src="<?=$data->answer->user->avatarUrl?>" alt=""></div>
+        <div class="b-consult-qa-ms__img"><img src="<?=$data->answer->user->avatarUrl?>" alt="<?=$data->user->fullName ?>" class="ava_img"></div>
         <div class="b-consult-qa-mst"><a href="<?=$data->answer->user->profileUrl?>" class="b-consult-qa-ms__name"><?=$data->answer->user->fullName?></a>
             <?=HHtml::timeTag($data->answer, array('class' => 'tx-date'), null) ?>
             <div class="b-consult-qa-ms__message comments_cont">
@@ -33,6 +33,6 @@
     </div>
     <?php endif; ?>
     <?php if ($data->answer === null && Yii::app()->user->checkAccess('answerQuestions', array('consultation' => $this->consultation))): ?>
-        <a href="<?=$this->createUrl('answer', array('slug' => $this->consultation->slug, 'questionId' => $data->id))?>">Ответить</a>
+        <a href="<?=$this->createUrl('answer', array('slug' => $this->consultation->slug, 'questionId' => $data->id))?>"><?=($data->answer === null) ? 'Ответить' : 'Редактировать ответ' ?></a>
     <?php endif; ?>
 </div>

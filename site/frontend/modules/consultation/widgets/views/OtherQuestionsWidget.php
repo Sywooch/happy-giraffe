d1dec5c7d4a7206591480c135d3e526dc68f77cb
choojoy-work <nikita@happy-giraffe.ru>
2015-03-30 11:30:07 +0300

    Консультация

--- site/frontend/modules/consultation/widgets/views/OtherQuestionsWidget.php
+++ site/frontend/modules/consultation/widgets/views/OtherQuestionsWidget.php
@@ -14,7 +14,7 @@
         <?=HHtml::timeTag($question, array('class' => 'tx-date'), null) ?>
         <div class="b-consult-aside-item__message">
             <div class="b-consult-qa-ms__message__title"><?=$question->title?></div>
-            <?=$question->text?>
+            <?=\site\common\helpers\HStr::truncate(strip_tags($question->text), 300)?>
         </div>
     </div>
     <?php endforeach; ?>

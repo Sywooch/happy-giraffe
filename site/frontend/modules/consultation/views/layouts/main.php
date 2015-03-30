5958abf5d4f7fa72524624dffcd11808bd97db2d
choojoy-work <nikita@happy-giraffe.ru>
2015-03-30 12:17:11 +0300

    Консультация

--- site/frontend/modules/consultation/views/layouts/main.php
+++ site/frontend/modules/consultation/views/layouts/main.php
@@ -7,15 +7,17 @@ $this->beginContent('//layouts/lite/common_menu');
 
     <div class="b-main clearfix">
         <div class="b-consult">
-            <div class="b-consult-top">
-                <div class="b-consult-top__t">
-                    <h1 class="b-consult-top__title">Консультация</h1>
-                    <div class="b-consult-top__text">
-                        Обсуждаем проблему <br />
-                        недостаток грудного молока
+            <a href="<?=$this->consultation->getUrl()?>">
+                <div class="b-consult-top">
+                    <div class="b-consult-top__t">
+                        <h1 class="b-consult-top__title">Консультация</h1>
+                        <div class="b-consult-top__text">
+                            Обсуждаем проблему <br />
+                            недостаток грудного молока
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
             <div class="b-main_cont">
                 <div class="b-main_col-hold clearfix">
                     <?=$content?>

09931da32c5d60c71f348e10318b0e2d33e9cb4f
Nikita Izygin <calacitizen@gmail.com>
2015-04-02 14:33:47 +0300

    services

--- Gruntfile.js
+++ Gruntfile.js
@@ -347,7 +347,7 @@ module.exports = function(grunt){
           'lite/html/page/comments/**/*.html',
           'lite/html/page/photo-window/**/*.html',
           'lite/html/page/cook-recipe/**/*.html',
-
+          'lite/html/page/services/consult/consult-widget-open.html', // Нужен только пока идет консультация
           '!lite/html/page/sign/**/*.html',
           //'lite/html/page/**/*-user.html', // стариницы зареганого
           '!lite/html/page/comments/comments-page.html'

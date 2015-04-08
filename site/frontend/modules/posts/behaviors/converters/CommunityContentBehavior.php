27c76f4bf7ad2996bb2156f0288d6939fc6a9559
choojoy-work <nikita@happy-giraffe.ru>
2015-04-08 10:59:35 +0300

    Выгрузка постов от ВЖ

--- site/frontend/modules/posts/behaviors/converters/CommunityContentBehavior.php
+++ site/frontend/modules/posts/behaviors/converters/CommunityContentBehavior.php
@@ -210,6 +210,7 @@ class CommunityContentBehavior extends \CActiveRecordBehavior
         if (empty($newPost->metaObject->description))
             $newPost->metaObject->description = trim(preg_replace('~\s+~', ' ', strip_tags($oldPost->post->text)));
 
+        die('123');
         return $newPost->save();
     }
 

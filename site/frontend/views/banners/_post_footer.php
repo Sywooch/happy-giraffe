0f7821ef14fa5d53afdef9fb85e93ae2134e6e15
choojoy-work <nikita@happy-giraffe.ru>
2015-04-20 17:29:56 +0300

    Реализовано заполнение лент пользователей

--- site/frontend/views/banners/_post_footer.php
+++ site/frontend/views/banners/_post_footer.php
@@ -1,17 +1,15 @@
 <div style="margin: 15px 15px 15px 0;">
-    <?php if ($data instanceof CommunityContent || ($data instanceof \site\frontend\modules\posts\models\Content && $data->originService == 'oldCommunity')): ?>
     <?php $widget = $this->beginWidget('AdsWidget', array('dummyTag' => 'yandex-direct')); ?>
-    <!-- ﬂÌ‰ÂÍÒ.ƒËÂÍÚ -->
     <div id="yandex_ad_<?=$widget->id?>"></div>
     <script type="text/javascript">
         (function(w, d, n, s, t) {
             w[n] = w[n] || [];
             w[n].push(function() {
-                Ya.Direct.insertInto(87026, "yandex_ad_<?=$widget->id?>", {
-                    stat_id: 26,
+                Ya.Direct.insertInto(127976, "yandex_ad", {
+                    stat_id: 8,
                     ad_format: "direct",
                     font_size: 1.1,
-                    type: "vertical",
+                    type: "horizontal",
                     limit: 2,
                     title_font_size: 3,
                     links_underline: true,
@@ -32,7 +30,4 @@
         })(window, document, "yandex_context_callbacks");
     </script>
     <?php $this->endWidget(); ?>
-    <?php else: ?>
-        <?php Yii::app()->controller->renderPartial('//banners/_direct_others'); ?>
-    <?php endif; ?>
 </div>

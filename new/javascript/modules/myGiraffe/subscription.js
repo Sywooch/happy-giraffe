151e2704532dcaf905e5e78fe2fd97c11f685816
choojoy-work <nikita@happy-giraffe.ru>
2015-04-22 15:29:34 +0300

    Подписки

--- new/javascript/modules/myGiraffe/subscription.js
+++ new/javascript/modules/myGiraffe/subscription.js
@@ -1,6 +1,9 @@
-define(['jquery', 'knockout', 'text!myGiraffe/subscription.html'], function($, ko, template) {
+define(['jquery', 'knockout', 'text!myGiraffe/subscription.html', 'models/Model', 'user-config'], function($, ko, template, Model, userConfig) {
     function SubscriptionView() {
         var self = this;
+
+        self.readUrl = '/api/community/getUserSubscriptions/';
+        self.saveUrl = '/api/community/setUserSubscriptions/';
 
         self.clubIds = ko.observableArray([]);
 
@@ -17,7 +20,28 @@ define(['jquery', 'knockout', 'text!myGiraffe/subscription.html'], function($, k
             return ko.computed(function() {
                 return self.clubIds.indexOf(clubId) >= 0;
             });
-        }
+        };
+
+        self.save = function() {
+            Model.get(self.saveUrl, {
+                userId: userConfig.userId,
+                subscriptions: self.clubIds()
+            });
+        };
+
+        self.init = function() {
+            Model.get(self.readUrl, {
+                userId: userConfig.userId
+            }).done(function(response) {
+                for (var i in response.data) {
+                    self.clubIds.push(response.data[i].id);
+                }
+
+                console.log(self.clubIds());
+            });
+        };
+
+        self.init();
     }
 
     return {

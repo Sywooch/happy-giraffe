9580e3d5ac2d6d72913af49c3764514df3a54ab8
choojoy-work <nikita@happy-giraffe.ru>
2015-04-22 16:08:33 +0300

    Подписки

--- site/frontend/modules/posts/modules/myGiraffe/views/subscriptions/index.php
+++ site/frontend/modules/posts/modules/myGiraffe/views/subscriptions/index.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ * @var $this LiteController
+ */
 $cs = Yii::app()->clientScript;
 $cs->registerAMD('subscription', array('kow'));
 ?>
 
-<subscription></subscription>
+<div class="layout-loose_hold clearfix">
+    <div class="b-main">
+        <div class="b-main_cont b-main_cont__wide">
+            <div class="mygiraffe b-main_col-hold clearfix"><a href="javascript:void(0)" class="ico-close6" onclick="window.history.back()">×</a>
+                <subscription></subscription>
+            </div>
+        </div>
+    </div>
+</div>

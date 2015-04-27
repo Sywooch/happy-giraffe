b17e42d8d73c4b598ea43a5e0e7808e79b7175b4
smaragdinus <kika.goth@gmail.com>
2015-04-27 16:46:24 +0800

    Изображения в расширенном редакторе

--- site/frontend/modules/editorialDepartment/models/Content.php
+++ site/frontend/modules/editorialDepartment/models/Content.php
@@ -53,7 +53,7 @@ class Content extends \EMongoDocument
             'social' => '\site\frontend\modules\editorialDepartment\models\Social',
         );
     }
-    
+
     public function behaviors()
     {
         return array(
@@ -65,7 +65,13 @@ class Content extends \EMongoDocument
                 'createAttribute' => 'dtimeCreate',
                 'updateAttribute' => 'dtimeUpdate',
                 'setUpdateOnCreate' => true,
-            )
+            ),
+            'PhotoCollectionBehavior' => array(
+                'class' => 'site\frontend\modules\photo\components\PhotoCollectionBehavior',
+                'attributeCollections' => array('htmlTextPreview', 'htmlText'),
+                'entity' => 'entity',
+                'entityId' => 'entityId',
+            ),
         );
     }
 

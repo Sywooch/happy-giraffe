3f0be562ca648a745d588a73e38187d4fbb11a42
Nikita Izygin <calacitizen@gmail.com>
2015-04-29 11:48:38 +0300

    opeanair

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,6 +349,7 @@ module.exports = function(grunt){
           'lite/html/page/photo-window/**/*.html',
           'lite/html/page/cook-recipe/**/*.html',
           'lite/html/page/services/consult/consult-widget-open.html', // Нужен только пока идет консультация
+          'lite/html/page/member/mygiraffe/openair-base-list-user.html',
           '!lite/html/page/sign/**/*.html',
           //'lite/html/page/**/*-user.html', // стариницы зареганого
           '!lite/html/page/comments/comments-page.html'

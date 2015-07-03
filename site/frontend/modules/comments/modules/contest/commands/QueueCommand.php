a7cec69311301cc23e3365f6d6432595259ed8fe
choojoy-work <nikita@happy-giraffe.ru>
2015-07-03 13:13:06 +0300

    fix 404

--- site/frontend/modules/comments/modules/contest/commands/QueueCommand.php
+++ site/frontend/modules/comments/modules/contest/commands/QueueCommand.php
@@ -8,8 +8,8 @@ class QueueCommand extends \CConsoleCommand
     public function actionWorker()
     {
         \Yii::app()->gearman->worker()->addFunction('handleComment', function (\GearmanJob $job) {
-//            $workload = $job->workload();
-//            call_user_func_array(array('site\frontend\modules\comments\modules\contest\components\CommentsHandler', 'handle'), unserialize($workload));
+            $workload = $job->workload();
+            call_user_func_array(array('site\frontend\modules\comments\modules\contest\components\CommentsHandler', 'handle'), unserialize($workload));
         });
         while (\Yii::app()->gearman->worker()->work()) {
             echo "ok\n";

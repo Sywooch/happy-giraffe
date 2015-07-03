9da4a3d077c747d79068cc5f10ac38de8141e929
choojoy-work <nikita@happy-giraffe.ru>
2015-07-03 11:07:13 +0300

    fix 404

--- site/frontend/modules/ads/commands/Command.php
+++ site/frontend/modules/ads/commands/Command.php
@@ -19,7 +19,7 @@ class Command extends \CConsoleCommand
         $dp = new \CActiveDataProvider('\site\frontend\modules\ads\models\Ad', array(
             'criteria' => array(
                 'order' => 't.id DESC',
-                'condition' => 't.active = 1 AND NOW() < DATE_ADD(FROM_UNIXTIME(dtimeCreate), INTERVAL 2 DAY)',
+                'condition' => 't.active = 1 AND NOW() < DATE_ADD(FROM_UNIXTIME(dtimeCreate), INTERVAL 49 HOUR)',
             ),
         ));
         $iterator = new \CDataProviderIterator($dp, 100);

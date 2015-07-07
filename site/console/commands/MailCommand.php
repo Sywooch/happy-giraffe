0e761eb326aec5e6eaf7290b0caa3bbb1acfc504
choojoy-work <nikita@happy-giraffe.ru>
2015-07-07 11:37:37 +0300

    fix 404

--- site/console/commands/MailCommand.php
+++ site/console/commands/MailCommand.php
@@ -31,7 +31,7 @@ class MailCommand extends CConsoleCommand
             Yii::app()->end();
         $contents = $this->renderFile(Yii::getPathOfAlias('site.common.tpl.weeklyNews') . '.php', array('models' => $articles), true);
 
-        Yii::app()->email->sendCampaign($contents, HEmailSender::LIST_OUR_USERS);
+        Yii::app()->email->sendCampaign($contents, null, 'clicked');
     }
 
     public function actionTestWeekly($date = null)

8305d1d38232846d8a6d49ce52bdab47548397b5
choojoy-work <nikita@happy-giraffe.ru>
2015-07-09 15:50:25 +0300

    fix 404

--- site/frontend/modules/som/modules/activity/widgets/ActivityWidget.php
+++ site/frontend/modules/som/modules/activity/widgets/ActivityWidget.php
@@ -31,7 +31,7 @@ class ActivityWidget extends \CWidget
 
     public function getDataProvider()
     {
-        return new \CActiveDataProvider(Activity::model(), array(
+        return new \CActiveDataProvider(Activity::model()->byUser($this->ownerId), array(
             'pagination' => array(
                 'pageSize' => 10,
                 'pageVar' => is_null($this->pageVar) ? $this->id : $this->pageVar,

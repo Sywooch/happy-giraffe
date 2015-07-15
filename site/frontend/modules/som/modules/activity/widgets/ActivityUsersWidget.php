b54a3863865538731fbc43b1d2a5ba99a8e398b0
choojoy-work <nikita@happy-giraffe.ru>
2015-07-15 09:57:51 +0300

    fix 404

--- site/frontend/modules/som/modules/activity/widgets/ActivityUsersWidget.php
+++ site/frontend/modules/som/modules/activity/widgets/ActivityUsersWidget.php
@@ -18,15 +18,14 @@ class ActivityUsersWidget extends \CWidget
         $criteria = new \CDbCriteria();
         $criteria->group = 't.userId';
         $criteria->limit = self::USERS_COUNT;
+        $criteria->with = 'user';
+        $criteria->addCondition('avatarId IS NOT NULL');
         $activities = Activity::model()->findAll($criteria);
 
-        $usersIds = array_map(function($activity) {
-            return $activity->userId;
+        $users = array_map(function($activity) {
+            return $activity->user;
         }, $activities);
 
-        $criteria2 = new \CDbCriteria();
-        $criteria2->addInCondition('t.id', $usersIds);
-        $users = \User::model()->findAll($criteria2);
         $this->render('ActivityUsersWidget', compact('users'));
     }
 }

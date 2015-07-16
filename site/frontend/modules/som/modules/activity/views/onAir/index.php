adbb9962fa61caa824f413451f350567402066e6
choojoy-work <nikita@happy-giraffe.ru>
2015-07-16 11:14:39 +0300

    fix 404

--- site/frontend/modules/som/modules/activity/views/onAir/index.php
+++ site/frontend/modules/som/modules/activity/views/onAir/index.php
@@ -6,22 +6,24 @@ $this->pageTitle = 'Прямой эфир';
 ?>
 
 <div class="b-main_cont b-main_cont__wide">
-    <?php
-    $this->widget('zii.widgets.CMenu', array(
-        'htmlOptions' => array('class' => 'filter-group'),
-        'itemCssClass' => 'filter-group_i',
-        'items'=>array(
-            array('label' => 'Все', 'url' => array('/som/activity/onAir/index', 'filter' => 'all'), 'active' => $filter === null),
-            array('label' => 'Комментарии', 'url' => array('/som/activity/onAir/index', 'filter' => 'comments')),
-            array('label' => 'Записи', 'url' => array('/som/activity/onAir/index', 'filter' => 'posts')),
-        ),
-    ));
-    ?>
-
     <!--+userSubscribers('visible-md-block')-->
     <div class="b-main_col-hold clearfix">
         <!-- Основная колонка-->
         <div class="b-main_col-article">
+            <div style="text-align: center;">
+                <?php
+                $this->widget('zii.widgets.CMenu', array(
+                    'htmlOptions' => array('class' => 'filter-group', 'style' => 'margin-top: 20px;'),
+                    'itemCssClass' => 'filter-group_i',
+                    'items'=>array(
+                        array('label' => 'Все', 'url' => array('/som/activity/onAir/index', 'filter' => 'all'), 'active' => $filter === null),
+                        array('label' => 'Записи', 'url' => array('/som/activity/onAir/index', 'filter' => 'posts')),
+                        array('label' => 'Комментарии', 'url' => array('/som/activity/onAir/index', 'filter' => 'comments')),
+                    ),
+                ));
+                ?>
+            </div>
+
             <?php
             $this->widget('site\frontend\modules\som\modules\activity\widgets\ActivityWidget', array(
                 'pageVar' => 'page',

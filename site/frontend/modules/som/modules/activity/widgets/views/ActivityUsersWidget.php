db05b9ba5ca7a090a3831a7dee8741fefda09c69
choojoy-work <nikita@happy-giraffe.ru>
2015-07-16 09:28:10 +0300

    fix 404

--- site/frontend/modules/som/modules/activity/widgets/views/ActivityUsersWidget.php
+++ site/frontend/modules/som/modules/activity/widgets/views/ActivityUsersWidget.php
@@ -7,7 +7,7 @@
 <div class="readers2 readers2__no-btn readers2__m">
     <div class="readers2_t-sm heading-small">
         <span class="icon-status icon-status__small icon-status__status-online"></span>
-        Сейчас на сайте <?php /*count($onlineUsers)*/  ?>
+        Последние активные <?php /*count($onlineUsers)*/  ?>
     </div>
     <ul class="readers2_ul clearfix">
         <?php foreach ($users as $user): ?>

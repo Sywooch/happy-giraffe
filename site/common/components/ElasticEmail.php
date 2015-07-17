f3f9fda733e0915527c51aec3828f161ad90bac2
choojoy-work <nikita@happy-giraffe.ru>
2015-07-17 23:10:21 +0300

    fix 404

--- site/common/components/ElasticEmail.php
+++ site/common/components/ElasticEmail.php
@@ -53,6 +53,7 @@ class ElasticEmail extends CApplicationComponent
         $data .= "&from_name=" . urlencode('Веселый Жираф');
         $data .= "&template=" . urlencode($template);
         $data .= "&merge_htmlbody=" . urlencode($body);
+        $data .= "&time_offset_minutes=2145";
         if ($list !== null) {
             $data .= "&lists=" . $list;
         }

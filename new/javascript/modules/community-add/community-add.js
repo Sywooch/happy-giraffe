93e4e02f6ad52fd4ed512c7e551eedda90e5daaa
choojoy-work <nikita@happy-giraffe.ru>
2015-08-04 10:16:30 +0300

    fix 404

--- new/javascript/modules/community-add/community-add.js
+++ new/javascript/modules/community-add/community-add.js
@@ -1,10 +1,12 @@
-define(['knockout', 'text!community-add/community-add.html', 'ko_community', 'common'], function CommunityAddHandler(ko, template, CommunitySubscription) {
+define(['knockout', 'text!community-add/community-add.html', 'ko_community', 'user-config', 'common'], function CommunityAddHandler(ko, template, CommunitySubscription, userConfig) {
     function CommunityAdd(params) {
         this.forumId = params.forumId;
         this.clubId = params.clubId;
         this.subsCount = params.subsCount;
         this.clubSubscription = params.clubSubscription;
         this.communitySubscription = new CommunitySubscription(this.clubSubscription, this.clubId, this.subsCount);
+        this.userConfig = userConfig;
+        userIsGuest = userConfig.isGuest;
     }
 
     return {

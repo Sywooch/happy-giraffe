82776ee42dd3ff14ea62cd258e5746e851e0304e
choojoy-work <nikita@happy-giraffe.ru>
2015-08-05 12:20:23 +0300

    fix 404

--- site/frontend/modules/posts/modules/photoAds/controllers/ApiController.php
+++ site/frontend/modules/posts/modules/photoAds/controllers/ApiController.php
@@ -9,7 +9,7 @@ use site\frontend\modules\posts\modules\photoAds\components\PhotoAdsManager;
 
 class ApiController extends \site\frontend\components\api\ApiController
 {
-    const COOKIE_NAME = 'PhotoAds5';
+    const COOKIE_NAME = 'PhotoAds6';
     const CAP = 2;
 
     public function actionGetPosts($url, $limit = -1)
@@ -23,19 +23,17 @@ class ApiController extends \site\frontend\components\api\ApiController
     public function actionGetAdPosts($url, $limit = 1)
     {
         $this->data = array();
-        if (isset(\Yii::app()->request->cookies[self::COOKIE_NAME])) {
-            $cookieValue = isset(\Yii::app()->request->cookies[self::COOKIE_NAME]) ? unserialize(\Yii::app()->request->cookies[self::COOKIE_NAME]->value) : array();
+        $cookieValue = isset(\Yii::app()->request->cookies[self::COOKIE_NAME]) ? unserialize(\Yii::app()->request->cookies[self::COOKIE_NAME]->value) : array();
 
-            if (count($cookieValue) < 2) {
-                $manager = new PhotoAdsManager();
-                $posts = $manager->getPosts($url, false, $limit, $cookieValue);
-                foreach ($posts as $p) {
-                    $cookieValue[] = $p['post']->id;
-                }
-                \Yii::app()->request->cookies[self::COOKIE_NAME] = new \CHttpCookie(self::COOKIE_NAME, serialize($cookieValue));
-
-                $this->data = $posts;
+        if (count($cookieValue) < 2) {
+            $manager = new PhotoAdsManager();
+            $posts = $manager->getPosts($url, false, $limit, $cookieValue);
+            foreach ($posts as $p) {
+                $cookieValue[] = $p['post']->id;
             }
+            \Yii::app()->request->cookies[self::COOKIE_NAME] = new \CHttpCookie(self::COOKIE_NAME, serialize($cookieValue));
+
+            $this->data = $posts;
         }
         $this->success = true;
     }

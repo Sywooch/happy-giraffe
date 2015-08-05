8a3289c2eb317d0882518e53545f37848e18caed
choojoy-work <nikita@happy-giraffe.ru>
2015-08-05 11:24:29 +0300

    fix 404

--- site/frontend/modules/posts/views/community/view.php
+++ site/frontend/modules/posts/views/community/view.php
@@ -1,7 +1,7 @@
 <?php
 $cs = Yii::app()->clientScript;
 $cs->registerScriptFile('https://relap.io/api/v6/head.js?token=TzdPbOhsNbNOmXJ3', ClientScript::POS_HEAD);
-if (Yii::app()->vm->version == VersionManager::VERSION_DESKTOP) {
+if (Yii::app()->vm->version != VersionManager::VERSION_MOBILE) {
     $cs->registerAMD('photoAd', array('popup' => 'photo-ad/photo-popup'));
 }
 /** @todo перенести обработку $this->post->metaObject в контроллер */

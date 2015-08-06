555fff418d716108e6a80df060100a13f3b73b32
choojoy-work <nikita@happy-giraffe.ru>
2015-08-06 12:33:33 +0300

    fix 404

--- site/frontend/modules/comments/modules/contest/commands/QueueCommand.php
+++ site/frontend/modules/comments/modules/contest/commands/QueueCommand.php
@@ -7,6 +7,9 @@ class QueueCommand extends \CConsoleCommand
 {
     public function actionWorker()
     {
+        \Yii::app()->db->enableSlave = false;
+        \Yii::app()->db->createCommand('SET SESSION wait_timeout = 28800;')->execute();
+
         \Yii::app()->gearman->worker()->addFunction('handleComment', function (\GearmanJob $job) {
             $workload = $job->workload();
             call_user_func_array(array('site\frontend\modules\comments\modules\contest\components\CommentsHandler', 'handle'), unserialize($workload));

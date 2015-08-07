d420de776e74114029244031a2665f1d9610d407
choojoy-work <nikita@happy-giraffe.ru>
2015-08-07 17:45:11 +0300

    fix 404

--- new/javascript/modules/next-post/next-post.js
+++ new/javascript/modules/next-post/next-post.js
@@ -1,6 +1,7 @@
-define(['jquery', 'knockout', 'text!next-post/next-post.html', 'models/Model', 'waypoints'], function($, ko, template, Model) {
+define(['jquery', 'knockout', 'text!next-post/next-post.html', 'models/Model', 'extensions/adhistory', 'waypoints'], function($, ko, template, Model, AdHistory) {
     function NextPost(params) {
         var self = this;
+        self.post = params.post;
         self.getUrl = '/api/nextPost/get/';
         self.limit = 2;
         self.posts = ko.observableArray([]);
@@ -11,9 +12,9 @@ define(['jquery', 'knockout', 'text!next-post/next-post.html', 'models/Model', '
                 var waypoint = new Waypoint({
                     element: element,
                     handler: function () {
-                        if (self.loading === false ) {
+                        if (self.loading === false && self.posts().length < self.limit) {
                             self.loading = true;
-                            Model.get(self.getUrl, { postId: params.postId }).done(function(response) {
+                            Model.get(self.getUrl, { postId: self.post.id }).done(function(response) {
                                 if (response.success) {
                                     self.posts.push(response.data);
                                     this.context.refresh();
@@ -26,6 +27,24 @@ define(['jquery', 'knockout', 'text!next-post/next-post.html', 'models/Model', '
                 });
             }
         };
+
+        ko.bindingHandlers.waypoint = {
+            init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+                var value = valueAccessor();
+                var url = value.url;
+                var title = value.title;
+
+
+
+                var waypoint = new Waypoint({
+                    element: element,
+                    handler: function() {
+                        document.title = title;
+                        AdHistory.pushState(null, title, url);
+                    }
+                });
+            }
+        };
     }
 
     return {
@@ -33,3 +52,6 @@ define(['jquery', 'knockout', 'text!next-post/next-post.html', 'models/Model', '
         template: template
     };
 });
+
+// ИСКЛЮЧИТЬ ИЗ ВЫБОРКИ ПОСТОВ ТЕ, КОТОРЫЕ УЖЕ ОТОБРАЖАЮТСЯ НА СТРАНИЦЕ!!!
+// ПОФИКСИТЬ ЗАГРУЗКУ ФОТОПОСТОВ

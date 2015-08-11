eb451ac0bd99836e587d4d73782e0bd4db37db2f
choojoy-work <nikita@happy-giraffe.ru>
2015-08-11 12:47:34 +0300

    fix 404

--- new/javascript/modules/share-buttons/share-buttons.js
+++ new/javascript/modules/share-buttons/share-buttons.js
@@ -1,4 +1,4 @@
-define(['jquery', 'knockout', 'text!share-buttons/share-buttons.html', 'facebook', 'ok', 'vk'], function($, ko, template, FB, OK, VK) {
+define(['jquery', 'knockout', 'text!share-buttons/share-buttons.html', 'facebook', 'ok', 'vk', 'jquery.iframetracker'], function($, ko, template, FB, OK, VK) {
     FB.init({
         appId: "412497558776154"
     });
@@ -12,6 +12,16 @@ define(['jquery', 'knockout', 'text!share-buttons/share-buttons.html', 'facebook
                 var id = Date.now();
                 $(element).attr('id', id);
                 OK.CONNECT.insertShareWidget(id, url, '{width:150, height:21, st: \'straight\', sz:20, ck:2}');
+
+                jQuery(document).ready(function($){
+                    setTimeout(function(){
+                        $(element).iframeTracker({
+                            blurCallback: function(){
+                                dataLayer.push({"event": "okShare"});
+                            }
+                        });
+                    }, 5000);
+                });
             }
         };
 
@@ -26,6 +36,16 @@ define(['jquery', 'knockout', 'text!share-buttons/share-buttons.html', 'facebook
             init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
                 var url = valueAccessor();
                 FB.XFBML.parse($(element).parent()[0]);
+
+                jQuery(document).ready(function($){
+                    setTimeout(function(){
+                        $(element).iframeTracker({
+                            blurCallback: function(){
+                                dataLayer.push({"event": "fbShare"});
+                            }
+                        });
+                    }, 5000);
+                });
             }
         };
     }

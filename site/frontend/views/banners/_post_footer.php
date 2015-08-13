44738aa01db57470123eee7fab7a5384f03273a4
choojoy-work <nikita@happy-giraffe.ru>
2015-08-13 16:28:07 +0300

    fix 404

--- site/frontend/views/banners/_post_footer.php
+++ site/frontend/views/banners/_post_footer.php
@@ -33,7 +33,7 @@
         })(window, document, "yandex_context_callbacks");
     </script>
 
-    <?php if ($this->post->isAdult == 0): ?>
+    <?php if (false): ?>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <!-- Объявления -->
     <ins class="adsbygoogle"

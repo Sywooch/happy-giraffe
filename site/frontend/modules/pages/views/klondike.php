2195cb80c6a4f59fe65e7c0b74948de852abac55
choojoy-work <nikita@happy-giraffe.ru>
2015-08-17 14:32:20 +0300

    fix 404

--- site/frontend/modules/pages/views/klondike.php
+++ site/frontend/modules/pages/views/klondike.php
@@ -1,3 +1,3 @@
 <?php $this->layout = false; ?>
 
-<iframe src='http://plinga.com/game/iframe/23/2585/?referrer=http://www.happy-giraffe.ru/klondike&locale=ru-ru' width='100%' height='1003' frameborder='0' allowfullscreen='true'></iframe>
+<iframe src='http://plinga.com/game/iframe/23/2585/?referrer=http://www.happy-giraffe.ru/klondike&locale=ru-ru' width='100%' height='1003' frameborder='0' allowfullscreen='true' style="margin-top: -114px;"></iframe>

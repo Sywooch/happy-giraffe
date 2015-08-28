4b122f83007873b68769e0f0eeb735f3b8e03059
choojoy-work <nikita@happy-giraffe.ru>
2015-08-28 11:27:40 +0300

    fix 404

--- site/frontend/modules/posts/views/layouts/newBlogPost.php
+++ site/frontend/modules/posts/views/layouts/newBlogPost.php
@@ -8,37 +8,31 @@ $this->beginContent('//layouts/lite/main');
             echo $content;
             ?>
             <aside class="b-main_col-sidebar visible-md">
-                <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adfox')); ?>
-                <!--AdFox START-->
-                <!--giraffe-->
-                <!--Площадка: Весёлый Жираф / * / *-->
-                <!--Тип баннера: Тексто-графические-->
-                <!--Расположение: &lt;сайдбар&gt;-->
-                <script type="text/javascript">
-                    <!--
-                    if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
-                    if (typeof(document.referrer) != 'undefined') {
-                        if (typeof(afReferrer) == 'undefined') {
-                            afReferrer = escape(document.referrer);
-                        }
-                    } else {
-                        afReferrer = '';
-                    }
-                    var addate = new Date();
-                    var scrheight = '', scrwidth = '';
-                    if (self.screen) {
-                        scrwidth = screen.width;
-                        scrheight = screen.height;
-                    } else if (self.java) {
-                        var jkit = java.awt.Toolkit.getDefaultToolkit();
-                        var scrsize = jkit.getScreenSize();
-                        scrwidth = scrsize.width;
-                        scrheight = scrsize.height;
-                    }
-                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=exim&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
-                    // -->
-                </script>
-                <?php $this->endWidget(); ?>
+                <? if (! ($this instanceof site\frontend\modules\posts\controllers\PostController) || ($this->post->isNoindex == 0 && ! $this->post->templateObject->getAttr('hideAdsense', false))): ?>
+                    <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adsense')); ?>
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- ÕÂ·ÓÒÍÂ· new -->
+                    <ins class="adsbygoogle"
+                         style="display:inline-block;width:300px;height:600px"
+                         data-ad-client="ca-pub-3807022659655617"
+                         data-ad-slot="5201434880"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                    <?php $this->endWidget(); ?>
+                <?php endif; ?>
+
+                <div class="side-block onair-min">
+                    <div class="side-block_tx">Прямой эфир</div>
+
+                    <?php
+                    $this->widget('site\frontend\modules\som\modules\activity\widgets\ActivityWidget', array(
+                        'pageVar' => 'page',
+                        'view' => 'onair-min',
+                        'pageSize' => 5,
+                    ));
+                    ?>
+                </div>
 
                 <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adfox')); ?>
                 <div class="bnr-base">
@@ -75,30 +69,6 @@ $this->beginContent('//layouts/lite/main');
                     <!-- _________________________AdFox Asynchronous code END___________________________ -->
                 </div>
                 <?php $this->endWidget(); ?>
-
-                <div class="side-block onair-min">
-                    <div class="side-block_tx">Прямой эфир</div>
-
-                    <?php
-                    $this->widget('site\frontend\modules\som\modules\activity\widgets\ActivityWidget', array(
-                        'pageVar' => 'page',
-                        'view' => 'onair-min',
-                        'pageSize' => 5,
-                    ));
-                    ?>
-                </div>
-
-                <? if (! ($this instanceof site\frontend\modules\posts\controllers\PostController) || ($this->post->isNoindex == 0 && ! $this->post->templateObject->getAttr('hideAdsense', false))): ?>
-                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <!-- 300 -->
-                <ins class="adsbygoogle"
-                     style="display:inline-block;width:300px;height:250px"
-                     data-ad-client="ca-pub-3807022659655617"
-                     data-ad-slot="8256939681"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-                <?php endif; ?>
             </aside>
         </div>
     </div>

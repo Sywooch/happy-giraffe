12ee8a067879168278f59dc62b76ee0c7a305c6a
choojoy-work <nikita@happy-giraffe.ru>
2015-09-01 09:15:43 +0300

    fix 404

--- site/frontend/modules/posts/views/post/_view.php
+++ site/frontend/modules/posts/views/post/_view.php
@@ -15,7 +15,7 @@ if ($this->post->originService == 'oldBlog') {
     $entity = $this->post->originEntity;
 }
 
-Yii::app()->clientScript->registerAMD('vk_comments', array('vk2'), 'VK.init({apiId: 2855330, onlyWidgets: true}); VK.Widgets.Comments("vk_comments", {limit: 10, width: "600", attach: "*"});');
+Yii::app()->clientScript->registerAMD('vk_comments', array('vk2'), 'VK.init({apiId: 2855330, onlyWidgets: true}); VK.Widgets.Comments("vk_comments", {limit: 10, width: "600", attach: "*", pageUrl: "' . $this->post->url . '"});');
 ?>
 <!-- Основная колонка-->
 <div class="b-main_col-article">
@@ -113,18 +113,7 @@ Yii::app()->clientScript->registerAMD('vk_comments', array('vk2'), 'VK.init({api
     <!-- /b-article-->
     <?php //$this->renderPartial('//banners/_article_banner', compact('data')); ?>
 
-    <!-- Put this script tag to the <head> of your page -->
-    <script type="text/javascript" src="//vk.com/js/api/openapi.js?117"></script>
-
-    <script type="text/javascript">
-        VK.init({apiId: 2855330, onlyWidgets: true});
-    </script>
-
-    <!-- Put this div tag to the place, where the Comments block will be -->
     <div id="vk_comments"></div>
-    <script type="text/javascript">
-        VK.Widgets.Comments("vk_comments", {limit: 10, width: "600", attach: "*"});
-    </script>
 
     <!-- comments-->
     <section class="comments comments__buble">

8e06a56e1f684011cee46fba3fd1a4fbffbf22ab
choojoy-work <nikita@happy-giraffe.ru>
2015-09-17 15:37:27 +0300

    fix 404

--- site/frontend/modules/posts/views/layouts/newBlogPost.php
+++ site/frontend/modules/posts/views/layouts/newBlogPost.php
@@ -15,32 +15,31 @@ $this->beginContent('//layouts/lite/main');
                     <!--Площадка: Весёлый Жираф / * / *-->
                     <!--Тип баннера: Безразмерный 240x400-->
                     <!--Расположение: &lt;сайдбар&gt;-->
-                    <!-- ________________________AdFox Asynchronous code START__________________________ -->
                     <script type="text/javascript">
                         <!--
-                        if (typeof (pr) == 'undefined') {
-                            var pr = Math.floor(Math.random() * 1000000);
-                        }
-                        if (typeof (document.referrer) != 'undefined') {
-                            if (typeof (afReferrer) == 'undefined') {
+                        if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 4294967295) + 1; }
+                        if (typeof(document.referrer) != 'undefined') {
+                            if (typeof(afReferrer) == 'undefined') {
                                 afReferrer = escape(document.referrer);
                             }
                         } else {
                             afReferrer = '';
                         }
                         var addate = new Date();
-
-
-                        var dl = escape(document.location);
-                        var pr1 = Math.floor(Math.random() * 1000000);
-
-                        document.write('<div id="AdFox_banner_' + pr1 + '"><\/div>');
-                        document.write('<div style="visibility:hidden; position:absolute;"><iframe id="AdFox_iframe_' + pr1 + '" width=1 height=1 marginwidth=0 marginheight=0 scrolling=no frameborder=0><\/iframe><\/div>');
-
-                        AdFox_getCodeScript(1, pr1, 'http://ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr + '&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;dl=' + dl + '&amp;pr1=' + pr1);
+                        var scrheight = '', scrwidth = '';
+                        if (self.screen) {
+                            scrwidth = screen.width;
+                            scrheight = screen.height;
+                        } else if (self.java) {
+                            var jkit = java.awt.Toolkit.getDefaultToolkit();
+                            var scrsize = jkit.getScreenSize();
+                            scrwidth = scrsize.width;
+                            scrheight = scrsize.height;
+                        }
+                        document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=etcx&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
                         // -->
                     </script>
-                    <!-- _________________________AdFox Asynchronous code END___________________________ -->
+                    <!--AdFox END-->
                 </div>
                 <?php $this->endWidget(); ?>
 

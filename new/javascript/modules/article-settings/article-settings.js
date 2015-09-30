d8f343f6c52355990d94d1db36c8ce9f0b7d4ebb
choojoy-work <nikita@happy-giraffe.ru>
2015-09-30 15:53:42 +0300

    fix 404

--- new/javascript/modules/article-settings/article-settings.js
+++ new/javascript/modules/article-settings/article-settings.js
@@ -1,18 +1,18 @@
 define(['jquery', 'knockout', 'text!article-settings/article-settings.html', 'models/Model', 'ko_library'], function ($, ko, template, Model) {
     function ArticleSettings(params) {
-        this.removeBlogUrl = '/newblog/remove/';
-        this.restoreBlogUrl = '/newblog/restore/';
+        this.removeBlogUrl = '/api/posts/remove/';
+        this.restoreBlogUrl = '/api/posts/restore/';
         this.settingsClicker = 'a.article-settings_a__settings';
         this.settingsBlock = 'div.article-settings_hold';
         this.articleId = params.articleId;
         this.editUrl = params.editUrl;
         this.removed = ko.observable(false);
         this.removePost = function removePost() {
-            $.post(this.removeBlogUrl, { id: this.articleId });
+            $.post(this.removeBlogUrl, JSON.stringify({ id: this.articleId }));
             this.removed(true);
         };
         this.restorePost = function restorePost() {
-            $.post(this.restoreBlogUrl, { id: this.articleId });
+            $.post(this.restoreBlogUrl, JSON.stringify({ id: this.articleId }));
             this.removed(false);
         };
         this.settingsShowHandler = function settingsShowHandler(data, evt) {

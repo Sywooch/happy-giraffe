9b200ba998d7bfadd78053d511667533aa1a2be8
choojoy-work <nikita@happy-giraffe.ru>
2015-10-19 11:40:29 +0300

    buzz

--- site/frontend/modules/posts/views/layouts/newCommunityPost.php
+++ site/frontend/modules/posts/views/layouts/newCommunityPost.php
@@ -10,37 +10,39 @@ $this->beginContent('//layouts/lite/community');
             <aside class="b-main_col-sidebar visible-md">
                 <community-add params="forumId: <?= $this->forum->id ?>, clubSubscription: <?= CJSON::encode(UserClubSubscription::subscribed(Yii::app()->user->id, $this->club->id)) ?>, clubId: <?= $this->club->id ?>, subsCount: <?= (int)UserClubSubscription::model()->getSubscribersCount($this->club->id) ?>"></community-add>
 
-                <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adfox')); ?>
-                <!--AdFox START-->
-                <!--giraffe-->
-                <!--Площадка: Весёлый Жираф / * / *-->
-                <!--Тип баннера: Тексто-графические-->
-                <!--Расположение: &lt;сайдбар&gt;-->
-                <script type="text/javascript">
-                    <!--
-                    if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
-                    if (typeof(document.referrer) != 'undefined') {
-                        if (typeof(afReferrer) == 'undefined') {
-                            afReferrer = escape(document.referrer);
+                <?php if (false): ?>
+                    <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adfox')); ?>
+                    <!--AdFox START-->
+                    <!--giraffe-->
+                    <!--Площадка: Весёлый Жираф / * / *-->
+                    <!--Тип баннера: Тексто-графические-->
+                    <!--Расположение: &lt;сайдбар&gt;-->
+                    <script type="text/javascript">
+                        <!--
+                        if (typeof(pr) == 'undefined') { var pr = Math.floor(Math.random() * 1000000); }
+                        if (typeof(document.referrer) != 'undefined') {
+                            if (typeof(afReferrer) == 'undefined') {
+                                afReferrer = escape(document.referrer);
+                            }
+                        } else {
+                            afReferrer = '';
                         }
-                    } else {
-                        afReferrer = '';
-                    }
-                    var addate = new Date();
-                    var scrheight = '', scrwidth = '';
-                    if (self.screen) {
-                        scrwidth = screen.width;
-                        scrheight = screen.height;
-                    } else if (self.java) {
-                        var jkit = java.awt.Toolkit.getDefaultToolkit();
-                        var scrsize = jkit.getScreenSize();
-                        scrwidth = scrsize.width;
-                        scrheight = scrsize.height;
-                    }
-                    document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=exim&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
-                    // -->
-                </script>
-                <?php $this->endWidget(); ?>
+                        var addate = new Date();
+                        var scrheight = '', scrwidth = '';
+                        if (self.screen) {
+                            scrwidth = screen.width;
+                            scrheight = screen.height;
+                        } else if (self.java) {
+                            var jkit = java.awt.Toolkit.getDefaultToolkit();
+                            var scrsize = jkit.getScreenSize();
+                            scrwidth = scrsize.width;
+                            scrheight = scrsize.height;
+                        }
+                        document.write('<scr' + 'ipt type="text/javascript" src="//ads.adfox.ru/211012/prepareCode?pp=dey&amp;ps=bkqy&amp;p2=exim&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '&amp;pdw=' + scrwidth + '&amp;pdh=' + scrheight + '"><\/scr' + 'ipt>');
+                        // -->
+                    </script>
+                    <?php $this->endWidget(); ?>
+                <?php endif; ?>
 
                 <?php $this->beginWidget('AdsWidget', array('dummyTag' => 'adfox')); ?>
                 <div class="bnr-base">

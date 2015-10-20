39607f966836d975806c59b77767ecb496882716
choojoy-work <nikita@happy-giraffe.ru>
2015-10-20 17:21:09 +0300

    buzz

--- site/frontend/modules/som/modules/community/views/layouts/news.php
+++ site/frontend/modules/som/modules/community/views/layouts/news.php
@@ -46,13 +46,13 @@ $this->beginContent('//layouts/lite/main');
             <div class="side-block rubrics">
                 <div class="side-block_tx">Темы новостей</div>
                 <ul>
-                    <li class="rubrics_li"><a class="rubrics_a" href="<?=$this->createUrl('index')?>">Все</a>
+                    <li class="rubrics_li"><a class="rubrics_a" href="<?=$this->createUrl('/som/community/news/index')?>">Все</a>
                         <div class="rubrics_count"><span class="rubrics_count_tx"><?=\site\frontend\modules\community\helpers\StatsHelper::getByLabels(array(\site\frontend\modules\posts\models\Label::LABEL_NEWS))?></span></div>
                     </li>
                     <?php foreach (CommunityClub::model()->findAll() as $club): ?>
                         <?php $count = \site\frontend\modules\community\helpers\StatsHelper::getByLabels(array($club->toLabel(), \site\frontend\modules\posts\models\Label::LABEL_NEWS)); ?>
                         <?php if ($count > 0): ?>
-                        <li class="rubrics_li"><a class="rubrics_a" href="<?=$this->createUrl('index', array('slug' => $club->slug))?>"><?=$club->title?></a>
+                        <li class="rubrics_li"><a class="rubrics_a" href="<?=$this->createUrl('/som/community/news/index', array('slug' => $club->slug))?>"><?=$club->title?></a>
                             <div class="rubrics_count"><span class="rubrics_count_tx"><?=$count?></span></div>
                         </li>
                         <?php endif; ?>

fa32dbcbae8bc37f48b06296e18f62f11aa73529
choojoy-work <nikita@happy-giraffe.ru>
2015-12-09 11:45:21 +0300

    contractubex

--- site/frontend/modules/posts/modules/contractubex/views/layouts/contractubex.php
+++ site/frontend/modules/posts/modules/contractubex/views/layouts/contractubex.php
@@ -5,6 +5,15 @@
         echo $content;
         ?>
         <aside class="b-main_col-sidebar visible-md">
+            <a class="fancy-top" href="<?=$this->createUrl('/blog/default/form', array('type' => 1, 'club_id' => \site\frontend\modules\posts\modules\contractubex\components\ContractubexHelper::getForum()->id, 'useAMD' => true))?>">
+                <div class="sidebar-promo-banner">
+                    <div class="sidebar-promo-banner_h-first">Поделись</div>
+                    <div class="sidebar-promo-banner_h-second">советом!</div>
+                    <div class="sidebar-promo-banner_img"></div>
+                    <div class="sidebar-promo-banner_text">И получи значок признания!</div><span class="sidebar-promo-banner_button">Поделись советом!</span>
+                </div>
+            </a>
+
             <?php $this->widget('site\frontend\modules\posts\modules\contractubex\widgets\sidebarWidget\SidebarWidget', array('exclude' => array($this->post->id))); ?>
         </aside>
     </div>

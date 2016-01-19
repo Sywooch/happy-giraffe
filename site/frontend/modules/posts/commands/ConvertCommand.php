54efbeab91ef197661798cde0a647aa970ff5e94
choojoy-work <nikita@happy-giraffe.ru>
2016-01-19 11:37:43 +0300

    test

--- site/frontend/modules/posts/commands/ConvertCommand.php
+++ site/frontend/modules/posts/commands/ConvertCommand.php
@@ -104,6 +104,9 @@ class ConvertCommand extends \CConsoleCommand
     {
         try {
             $data = self::unserialize($job->workload());
+
+            var_dump($data);
+
             \Yii::app()->db->setActive(true);
             usleep(100000); // на всякий случай поспим 0.1 сек, что бы быть уверенным, что реплика прошла
             $model = \CActiveRecord::model($data['entity'])->resetScope()->findByPk($data['entityId']);

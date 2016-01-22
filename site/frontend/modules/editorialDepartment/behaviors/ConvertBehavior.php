ccd84a64b0c04af36d4d69486191122ea6129339
choojoy-work <nikita@happy-giraffe.ru>
2016-01-22 12:24:09 +0300

    widget remove

--- site/frontend/modules/editorialDepartment/behaviors/ConvertBehavior.php
+++ site/frontend/modules/editorialDepartment/behaviors/ConvertBehavior.php
@@ -57,7 +57,7 @@ class ConvertBehavior extends \EMongoDocumentBehavior
                     'entityId' => $this->owner->entityId,
                 ));
             } catch (\Exception $e) {
-                var_dump($entity);
+                var_dump($e);
                 die;
                 $post = new Content();
             }

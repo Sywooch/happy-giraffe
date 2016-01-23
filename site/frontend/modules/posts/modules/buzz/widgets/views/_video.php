a4c0596b45bbb96b7e25ed83dc592d89254c16d3
choojoy-work <nikita@happy-giraffe.ru>
2016-01-23 11:48:07 +0300

    widget remove

--- site/frontend/modules/posts/modules/buzz/widgets/views/_video.php
+++ site/frontend/modules/posts/modules/buzz/widgets/views/_video.php
@@ -8,6 +8,6 @@
     <div class="article-anonce_img-top">
         <div class="ico-ovrPlay_video ico-ovrPlay__s"></div>
     </div>
-    <img src="<?=$video->getThumbnail()?>" alt="<?=$post->title?>" class="article-anonce_img">
+    <img src="<?=$video->getThumbnail()?>" alt="Видео: как дети видят своих родителей, когда те пьяны" class="article-anonce_img">
     <div class="article-anonce_img-overlay"></div>
 </div>

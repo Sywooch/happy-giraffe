d76aae66d09bfbc409d04540e9edc945d07000df
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 11:55:13 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/views/default/index.php
+++ site/frontend/modules/posts/modules/forums/views/default/index.php
@@ -4,7 +4,7 @@
     <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); ?>
     <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); ?>
     <div class="clearfix"></div>
-    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); ?>
+    <?php //$this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); ?>
     <?php //$this->widget('site\frontend\modules\posts\modules\forums\widgets\onlineUsers\OnlineUsersWidget'); ?>
 </div>
 

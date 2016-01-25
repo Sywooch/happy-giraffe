de1d88d58b683a7440a873f1bc0d28a12243c834
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 16:09:18 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/widgets/lastPost/LastPostWidget.php
+++ site/frontend/modules/posts/modules/forums/widgets/lastPost/LastPostWidget.php
@@ -22,7 +22,7 @@ class LastPostWidget extends \CWidget
 //        if ($posts === false) {
             $labelId = Label::getIdsByLabels(array(Label::LABEL_FORUMS))[0];
             $dependency = new \CExpressionDependency();
-            $posts = Content::model()->byLabels(array(Label::LABEL_FORUMS))->orderDesc()->cache(0, $dependency)->findAll(array(
+            $posts = Content::model()->cache(0, $dependency, 5)->byLabels(array(Label::LABEL_FORUMS))->orderDesc()->findAll(array(
                 'limit' => self::LIMIT,
             ));
 //            \Yii::app()->cache->set(__CLASS__, $posts, 0, $dependency);

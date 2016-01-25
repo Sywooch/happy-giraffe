b104762cc29ba44c501a5c9ad453959feb744604
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 15:39:34 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/views/default/index.php
+++ site/frontend/modules/posts/modules/forums/views/default/index.php
@@ -2,11 +2,11 @@
     <div class="heading-link-xxl"> Мы здесь общаемся!</div>
     <!--Добавить в layout_base-->
 
-    <?php if($this->beginCache('Forums.UsersTopWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); $this->endCache();} ?>
-    <?php if($this->beginCache('Forums.LastPostWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); $this->endCache();} ?>
+    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); ?>
+    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); ?>
     <div class="clearfix"></div>
-    <?php if($this->beginCache('Forums.ClubsWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); $this->endCache();} ?>
-    <?php if($this->beginCache('Forums.OnlineUsersWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\onlineUsers\OnlineUsersWidget'); $this->endCache();} ?>
+    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); ?>
+    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\onlineUsers\OnlineUsersWidget'); ?>
 </div>
 
 
@@ -15,7 +15,13 @@
 
 
 <?php
-if (false) {
+if (false) { ?>
+<?php if($this->beginCache('Forums.UsersTopWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); $this->endCache();} ?>
+<?php if($this->beginCache('Forums.LastPostWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); $this->endCache();} ?>
+<div class="clearfix"></div>
+<?php if($this->beginCache('Forums.ClubsWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); $this->endCache();} ?>
+<?php if($this->beginCache('Forums.OnlineUsersWidget', array('duration'=>3600))) {$this->widget('site\frontend\modules\posts\modules\forums\widgets\onlineUsers\OnlineUsersWidget'); $this->endCache();} ?>
+<?
     Yii::beginProfile('lastPost');
     $this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget');
     Yii::endProfile('lastPost');

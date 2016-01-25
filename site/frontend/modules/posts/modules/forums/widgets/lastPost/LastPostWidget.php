cebc2233c8ca8d0203c35ac56f5449f1974d1c86
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 16:04:44 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/widgets/lastPost/LastPostWidget.php
+++ site/frontend/modules/posts/modules/forums/widgets/lastPost/LastPostWidget.php
@@ -18,11 +18,15 @@ class LastPostWidget extends \CWidget
     public function run()
     {
         \Yii::beginProfile('last1');
-        $labelId = Label::getIdsByLabels(array(Label::LABEL_FORUMS))[0];
-        $dependency = new \CDbCacheDependency('SELECT COUNT(*) FROM ' . Tag::model()->tableName() . ' WHERE labelId = ' . $labelId);
-        $posts = Content::model()->cache(0, $dependency)->byLabels(array(Label::LABEL_FORUMS))->orderDesc()->findAll(array(
-            'limit' => self::LIMIT,
-        ));
+        $posts = \Yii::app()->cache->get(__CLASS__);
+        if ($posts === false) {
+            $labelId = Label::getIdsByLabels(array(Label::LABEL_FORUMS))[0];
+            $dependency = new \CDbCacheDependency('SELECT COUNT(*) FROM ' . Tag::model()->tableName() . ' WHERE labelId = ' . $labelId);
+            $posts = Content::model()->cache(0, $dependency)->byLabels(array(Label::LABEL_FORUMS))->orderDesc()->findAll(array(
+                'limit' => self::LIMIT,
+            ));
+            \Yii::app()->cache->set(__CLASS__, $posts, 0, $dependency);
+        }
         \Yii::endProfile('last1');
 
         \Yii::beginProfile('last2');

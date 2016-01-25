97a37f6969753e0fed0c0fa775492e7d0baf2b13
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 16:54:26 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/widgets/clubs/ClubsWidget.php
+++ site/frontend/modules/posts/modules/forums/widgets/clubs/ClubsWidget.php
@@ -10,6 +10,7 @@ class ClubsWidget extends \CWidget
     public function run()
     {
         $sections = \CommunitySection::model()->with('clubs')->findAll();
+        $clubs = \Community::model()->findAll();
 
         $sql = <<<SQL
 SELECT * FROM (
@@ -17,21 +18,22 @@ SELECT pc.*, pt2.labelId
 FROM post__contents pc
 JOIN post__tags pt ON pt.contentId = pc.id
 JOIN post__tags pt2 ON pt2.contentId = pc.id
-WHERE pt.labelId IN (18319) AND pt2.labelId IN (102, 107)
+WHERE pt.labelId IN (:a) AND pt2.labelId IN (:b) AND isRemoved = 0
 GROUP BY pc.id
 HAVING COUNT(pt.labelId) = 1
 ORDER BY dtimePublication DESC) t
 GROUP BY t.labelId;
 SQL;
 
-        $criteria = clone Content::model()->byLabels(array(Label::LABEL_FORUMS))->orderDesc()->getDbCriteria();
-//        $criteria->select = 't.*, pt2.labelId';
-//        $criteria->join .= "JOIN post__tags pt2 ON pt2.contentId = t.id";
-        $command = \Yii::app()->db->getCommandBuilder()->createFindCommand(Content::model()->tableName(), $criteria);
+        $a = \Yii::app()->db->createCommand($sql);
 
-        //$command->text = "SELECT * FROM (" . $command->text . ") t GROUP BY t.labelId";
 
-        var_dump($command->queryAll()); die;
+        var_dump($a->execute(array(
+            ':a' => Label::LABEL_FORUMS,
+            ':b' => implode(',', Label::getIdsByLabels(array_map(function($club) {
+                return $club->toLabel();
+            }, $clubs))),
+        ))); die;
 
         $users = User::model()->findAllByPk(array_map(function($post) {
             return $post->authorId;

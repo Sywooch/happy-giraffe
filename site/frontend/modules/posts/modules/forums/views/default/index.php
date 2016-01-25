44486f25607d70d6a5fab669c49b9797e1d15673
choojoy-work <nikita@happy-giraffe.ru>
2016-01-25 11:49:49 +0300

    widget remove

--- site/frontend/modules/posts/modules/forums/views/default/index.php
+++ site/frontend/modules/posts/modules/forums/views/default/index.php
@@ -1,8 +1,8 @@
 <div class="b-main_cont">
     <div class="heading-link-xxl"> Мы здесь общаемся!</div>
     <!--Добавить в layout_base-->
-    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); ?>
-    <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); ?>
+    <?php //$this->widget('site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget'); ?>
+    <?php //$this->widget('site\frontend\modules\posts\modules\forums\widgets\lastPost\LastPostWidget'); ?>
     <div class="clearfix"></div>
     <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\clubs\ClubsWidget'); ?>
     <?php $this->widget('site\frontend\modules\posts\modules\forums\widgets\onlineUsers\OnlineUsersWidget'); ?>

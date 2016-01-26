e13aa1f5a97fd5e562a8be6f81438cc3aa3da4ba
choojoy-work <nikita@happy-giraffe.ru>
2016-01-26 09:26:54 +0300

    widget remove

--- site/frontend/controllers/SiteController.php
+++ site/frontend/controllers/SiteController.php
@@ -125,7 +125,7 @@ class SiteController extends LiteController
 	    if ($error = Yii::app()->errorHandler->error)
 	    {
 	    	if (Yii::app()->request->isAjaxRequest)
-                echo $error->message;
+                var_dump($error);
 	    	else
             {
                 $viewFile = Yii::app()->getSystemViewPath() . DIRECTORY_SEPARATOR . 'error' . $error['code'] . '.php';

b004bd2f8626fb5a303296980d6e6ecbe761ea05
choojoy-work <nikita@happy-giraffe.ru>
2016-01-27 15:30:34 +0300

    giraffe

--- site/frontend/modules/posts/modules/forums/widgets/usersTop/UsersTopWidget.php
+++ site/frontend/modules/posts/modules/forums/widgets/usersTop/UsersTopWidget.php
@@ -33,12 +33,26 @@ class UsersTopWidget extends \CWidget
         $value = false;
         if ($value === false) {
             $criteria = clone Content::model()->byLabels(array(Label::LABEL_FORUMS))->getDbCriteria();
+            $criteria->select = 'id, originEntityId, originEntityService';
             $criteria->compare('authorId', '<>' . \User::HAPPY_GIRAFFE);
             $criteria->compare('dtimeCreate', '>' . (time() - (3600 * 24 * self::DAYS)));
             $criteria->join = 'JOIN ' . Tag::model()->tableName() . ' tagModels ON tagModels.contentId = t.id';
             $command = \Yii::app()->db->getCommandBuilder()->createFindCommand(Content::model()->tableName(), $criteria);
-            $ids = $command->queryColumn();
+            $data = $command->queryAll();
+            $ids = array_map(function($row) {
+                return $row['id'];
+            }, $data);
+            $advPostIds = array();
+            $communityContentIds = array();
+            foreach ($data as $row) {
+                if ($row['originEntityService'] == 'AdvPost') {
+                    $advPostIds[] = $row['originEntityId'];
+                } else {
+                    $communityContentIds[] = $row['originEntityId'];
+                }
+            }
 
+            // посты
             $criteria2 = new \CDbCriteria();
             $criteria2->addInCondition('t.id', $ids);
             $criteria2->group = 'authorId';
@@ -46,16 +60,28 @@ class UsersTopWidget extends \CWidget
             $command2 = \Yii::app()->db->getCommandBuilder()->createFindCommand(Content::model()->tableName(), $criteria2);
             $posts = $command2->queryAll();
 
+            // комментарии к постам из расширенного редактора (AdvPost)
             $criteria2 = new \CDbCriteria();
-            $criteria2->addInCondition('t.id', $ids);
+            $criteria2->compare('t.entity', 'AdvPost');
+            $criteria2->addInCondition('t.entity_id', $advPostIds);
             $criteria2->group = 'author_id';
             $criteria2->select = 'author_id AS uId, COUNT(*) AS n';
             $command2 = \Yii::app()->db->getCommandBuilder()->createFindCommand(Comment::model()->tableName(), $criteria2);
             $comments = $command2->queryAll();
 
+            // комментарии к старым постам (CommunityContent)
+            $criteria2 = new \CDbCriteria();
+            $criteria2->compare('t.entity', 'CommunityContent');
+            $criteria2->addInCondition('t.entity_id', $communityContentIds);
+            $criteria2->group = 'author_id';
+            $criteria2->select = 'author_id AS uId, COUNT(*) AS n';
+            $command2 = \Yii::app()->db->getCommandBuilder()->createFindCommand(Comment::model()->tableName(), $criteria2);
+            $comments2 = $command2->queryAll();
+
             $scores = array();
             $this->process($posts, $scores, self::POSTS_MULTIPLIER);
             $this->process($comments, $scores, self::COMMENTS_MULTIPLIER);
+            $this->process($comments2, $scores, self::COMMENTS_MULTIPLIER);
             arsort($scores);
             $scores = array_slice($scores, 0, self::LIMIT, true);
             $value = $scores;

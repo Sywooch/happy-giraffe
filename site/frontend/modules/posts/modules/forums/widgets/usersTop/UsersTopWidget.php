5de37b7a868f7034cba59bc195615476d5230746
choojoy-work <nikita@happy-giraffe.ru>
2016-01-27 15:10:03 +0300

    giraffe

--- site/frontend/modules/posts/modules/forums/widgets/usersTop/UsersTopWidget.php
+++ site/frontend/modules/posts/modules/forums/widgets/usersTop/UsersTopWidget.php
@@ -33,16 +33,11 @@ class UsersTopWidget extends \CWidget
         $value = false;
         if ($value === false) {
             $criteria = clone Content::model()->byLabels(array(Label::LABEL_FORUMS))->getDbCriteria();
-            $criteria->addCondition('authorId != 1');
+            $criteria->compare('authorId', '<>' . \User::HAPPY_GIRAFFE);
             $criteria->compare('dtimeCreate', '>' . (time() - (3600 * 24 * self::DAYS)));
             $criteria->join = 'JOIN ' . Tag::model()->tableName() . ' tagModels ON tagModels.contentId = t.id';
             $command = \Yii::app()->db->getCommandBuilder()->createFindCommand(Content::model()->tableName(), $criteria);
-
-
-
             $ids = $command->queryColumn();
-
-            //var_dump($ids); die;
 
             $criteria2 = new \CDbCriteria();
             $criteria2->addInCondition('t.id', $ids);
@@ -57,7 +52,8 @@ class UsersTopWidget extends \CWidget
             $criteria2->select = 'author_id AS uId, COUNT(*) AS n';
             $command2 = \Yii::app()->db->getCommandBuilder()->createFindCommand(Comment::model()->tableName(), $criteria2);
             $comments = $command2->queryAll();
-            $comments = array();
+
+            var_dump($comments); die;
 
             $scores = array();
             $this->process($posts, $scores, self::POSTS_MULTIPLIER);

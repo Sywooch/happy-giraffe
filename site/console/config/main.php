6a833c9879b132f6fe28c38f8f36adb06571fa42
choojoy-work <nikita@happy-giraffe.ru>
2016-01-28 14:56:03 +0300

    giraffe

--- site/console/config/main.php
+++ site/console/config/main.php
@@ -243,7 +243,7 @@ return array(
         ),
     ),
     'params' => array(
-        'photos_url' => 'http://img.giraffe.code-geek.ru',
+        'photos_url' => 'http://img.happy-giraffe.ru',
         'use_proxy_auth' => true,
     ),
 );

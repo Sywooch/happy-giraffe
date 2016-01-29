c1eb0cea21ef8eb101c9279bb3fb6acfd8a80e02
choojoy-work <nikita@happy-giraffe.ru>
2016-01-29 10:33:14 +0300

    giraffe

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,4 +1,20 @@
 <div class="article-banner">
+    <div class="buzzplayer-stage" data-hash="XKYmYyY14N0uRxfFufNXA3md5rGHF7KhvnCf3IJaccg" data-width="500" data-expandable="true" data-target=".b-markdown:p">
+        <script type="text/javascript">
+            (function(w, d) {
+                var c = d.createElement("script");
+                c.src = "http://tube.buzzoola.com/new/build/buzzlibrary.js";
+                c.type = "text/javascript";
+                c.async = !0;
+                var f = function() {
+                    var p = d.getElementsByTagName("script")[0];
+                    p.parentNode.insertBefore(c, p);
+                };
+                "[object Opera]" == w.opera ? d.addEventListener("DOMContentLoaded", f, !1) : f();
+            })(window, document);
+        </script>
+    </div>
+<?php if(false): ?>
 <?php
     $this->beginWidget('AdsWidget', array(
         'dummyTag' => 'adfox',
@@ -6,4 +22,5 @@
     echo $this->renderPartial('site.frontend.widgets.views.ads.adfox.680x470');
     $this->endWidget();
 ?>
+<?php endif; ?>
 </div>

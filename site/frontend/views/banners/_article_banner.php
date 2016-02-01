dff4f245c52d1c876178ff89627d0b5bb855a3c4
choojoy-work <nikita@happy-giraffe.ru>
2016-02-01 11:54:39 +0300

    fix

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,19 +1,10 @@
 <div class="article-banner">
     <div class="buzzplayer-stage" data-hash="XKYmYyY14N0uRxfFufNXA3md5rGHF7KhvnCf3IJaccg" data-width="500" data-expandable="true" data-target=".b-markdown:p">
         <script type="text/javascript">
-            (function(w, d) {
-                var c = d.createElement("script");
-                c.src = "http://tube.buzzoola.com/new/build/buzzlibrary.js";
-                c.type = "text/javascript";
-                c.async = !0;
-                var f = function() {
-                    var p = d.getElementsByTagName("script")[0];
-                    p.parentNode.insertBefore(c, p);
-                };
-                "[object Opera]" == w.opera ? d.addEventListener("DOMContentLoaded", f, !1) : f();
-            })(window, document);
+            require(['http://tube.buzzoola.com/new/build/buzzlibrary.js']);
         </script>
     </div>
+</div>
 <?php if(false): ?>
 <?php
     $this->beginWidget('AdsWidget', array(
@@ -23,4 +14,3 @@
     $this->endWidget();
 ?>
 <?php endif; ?>
-</div>

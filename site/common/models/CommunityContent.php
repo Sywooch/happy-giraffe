634da457fb10d46d1205d36720bd2ab4fd64bcd1
InteI <jimmdigriz@yandex.ru>
2016-02-01 14:58:49 +0300

    wr2

--- site/common/models/CommunityContent.php
+++ site/common/models/CommunityContent.php
@@ -185,7 +185,7 @@ class CommunityContent extends HActiveRecord implements IPreview
                 'class' => 'site\frontend\modules\notifications\behaviors\ContentBehavior',
             ),
             'withRelated' => array(
-                'class' => 'site.common.extensions.wr.WithRelatedBehavior',
+                'class' => 'site.common.extensions.wr2.WithRelatedBehavior',
                 'callback' => function($model) {
                     $model->convertToNewPost->addTaskToConvert();
                 }

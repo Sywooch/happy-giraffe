40f49b15704432d8068252df9a9d5c4c92722438
choojoy-work <nikita@happy-giraffe.ru>
2016-02-01 11:56:49 +0300

    fix

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -2,6 +2,20 @@
     <div class="buzzplayer-stage" data-hash="XKYmYyY14N0uRxfFufNXA3md5rGHF7KhvnCf3IJaccg" data-width="500" data-expandable="true" data-target=".b-markdown:p">
         <script type="text/javascript">
             require(['http://tube.buzzoola.com/new/build/buzzlibrary.js']);
+
+            <?php if (false): ?>
+            (function(w, d) {
+                var c = d.createElement("script");
+                c.src = "http://tube.buzzoola.com/new/build/buzzlibrary.js";
+                c.type = "text/javascript";
+                c.async = !0;
+                var f = function() {
+                    var p = d.getElementsByTagName("script")[0];
+                    p.parentNode.insertBefore(c, p);
+                };
+                "[object Opera]" == w.opera ? d.addEventListener("DOMContentLoaded", f, !1) : f();
+            })(window, document);
+            <?php endif; ?>
         </script>
     </div>
 </div>

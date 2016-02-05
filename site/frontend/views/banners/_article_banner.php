3910c3f8ddc8cb38405c5fe04d351d69e3931fcc
choojoy-work <nikita@happy-giraffe.ru>
2016-02-05 20:53:01 +0300

    contractubex

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,11 +1,6 @@
 <div class="article-banner">
     <div class="buzzplayer-stage" data-hash="XKYmYyY14N0uRxfFufNXA3md5rGHF7KhvnCf3IJaccg" data-width="500" data-expandable="true" data-target=".b-markdown:p">
         <script type="text/javascript">
-            <?php if ($data->url == 'http://www.happy-giraffe.ru/community/39/forum/advpost/710643/'): ?>
-            require(['http://tube.buzzoola.com/new/build/buzzlibrary.js']);
-            <?php endif; ?>
-
-            <?php if ($data->url == 'http://www.happy-giraffe.ru/community/32/forum/advpost/711088/'): ?>
             (function(w, d) {
                 var c = d.createElement("script");
                 c.src = "http://tube.buzzoola.com/new/build/buzzlibrary.js";
@@ -17,7 +12,6 @@
                 };
                 "[object Opera]" == w.opera ? d.addEventListener("DOMContentLoaded", f, !1) : f();
             })(window, document);
-            <?php endif; ?>
         </script>
     </div>
 </div>

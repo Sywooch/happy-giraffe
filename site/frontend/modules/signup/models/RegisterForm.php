88aa3e09865c23b10051578028c793c799e6f171
choojoy-work <nikita@happy-giraffe.ru>
2016-02-17 12:21:15 +0300

    login

--- site/frontend/modules/signup/models/RegisterForm.php
+++ site/frontend/modules/signup/models/RegisterForm.php
@@ -11,7 +11,13 @@ use site\frontend\modules\users\components\AvatarManager;
 
 class RegisterForm extends \CFormModel
 {
+    const GENDER_FEMALE = 0;
+    const GENDER_MALE = 1;
+
     public $firstName;
+    public $lastName;
+    public $birthday;
+    public $gender;
     public $email;
     public $password;
     public $avatarSrc;
@@ -23,8 +29,11 @@ class RegisterForm extends \CFormModel
         return array(
             array('firstName, email, password', 'required'),
             array('firstName', 'length', 'max' => 50),
+            array('lastName', 'length', 'max' => 50),
             array('email', 'email'),
             array('email', 'unique', 'className' => 'User', 'caseSensitive' => false, 'criteria' => array('condition' => 'deleted = 0 AND status = :active', 'params' => array(':active' => \User::STATUS_ACTIVE))),
+            array('birthday', 'date', 'format' => 'yyyy-M-d'),
+            array('gender', 'in', 'range' => array(self::GENDER_FEMALE, self::GENDER_MALE)),
             array('password', 'length', 'min' => 6, 'max' => 15),
             array('avatarSrc', 'safe'),
         );
@@ -34,6 +43,12 @@ class RegisterForm extends \CFormModel
     {
         return array(
             'firstName' => 'Имя',
+            'lastName' => 'Фамилия',
+            'birthday' => 'Дата рождения',
+            'birthdayD' => 'День',
+            'birthdayM' => 'Месяц',
+            'birthdayY' => 'Год',
+            'gender' => 'Пол',
             'email' => 'E-mail',
             'password' => 'Пароль',
         );
@@ -45,6 +60,9 @@ class RegisterForm extends \CFormModel
         try {
             $this->user = new \User();
             $this->user->first_name = $this->firstName;
+            $this->user->last_name = $this->lastName;
+            $this->user->birthday = $this->birthday;
+            $this->user->gender = $this->gender;
             $this->user->email = $this->email;
             $this->user->password = \User::hashPassword($this->password);
             $this->user->status = \User::STATUS_ACTIVE;

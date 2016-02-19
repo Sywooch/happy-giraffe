e1c5eabe33eb1fc343d0721900e41130a651c2d2
InteI <jimmdigriz@yandex.ru>
2016-02-19 14:51:23 +0300

    fixes

--- site/frontend/modules/som/modules/idea/views/default/index.php
+++ site/frontend/modules/som/modules/idea/views/default/index.php
@@ -3,6 +3,17 @@ $this->pageTitle = 'Идеи';
 $cs = Yii::app()->clientScript;
 $cs->registerAMD('kow', array('kow'));
 ?>
-<div class="b-main_col-article">
-    <ideas-line params=""></ideas-line>
+<div class="b-main_cont ideas-cont">
+    <h1 class="heading-link-xxl"><?= $this->pageTitle ?></h1>
+
+    <div class="b-crumbs">
+        <div class="b-crumbs_tx">Я здесь:</div>
+        <ul class="b-crumbs_ul">
+            <li class="b-crumbs_li"><a href="#" class="b-crumbs_a">Ответы</a></li>
+            <li class="b-crumbs_li b-crumbs_li__last"><span class="b-crumbs_last">Обсуждаем проблемы ГВ</span></li>
+        </ul>
+    </div>
+    <div class="b-main_col-article">
+        <ideas-line params=""></ideas-line>
+    </div>
 </div>

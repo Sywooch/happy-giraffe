611d8e0dd03d2531bfbcce3858638962ee97e86d
choojoy-work <nikita@happy-giraffe.ru>
2016-03-22 15:19:00 +0300

    qa fixes

--- site/frontend/modules/som/modules/qa/views/_question.php
+++ site/frontend/modules/som/modules/qa/views/_question.php
@@ -20,6 +20,7 @@
         <div class="icons-meta_view"><span class="icons-meta_tx"><?=Yii::app()->getModule('analytics')->visitsManager->getVisits($data->url)?></span></div>
     </div>
     <div class="clearfix"></div><a class="questions_item_heading" href="<?=$data->url?>"><?=CHtml::encode($data->title)?></a>
+    <?php if ($data->consultationId !== null || $data->categoryId !== null): ?>
     <div class="questions_item_category">
         <div class="questions_item_category_ico sharp-test"></div>
         <?php if ($data->consultationId === null): ?>
@@ -28,6 +29,7 @@
             <a href="<?=$this->createUrl('/som/qa/consultation/index/', array('consultationId' => $data->consultation->id))?>" class="questions_item_category_link"><?=$data->consultation->title?></a>
         <?php endif; ?>
     </div>
+    <?php endif; ?>
     <?php if ($data->answersCount == 0): ?>
         <?php if (Yii::app()->user->checkAccess('createQaAnswer', array('question' => $data))): ?>
             <a class="questions_item_answers" href="<?=$data->url?>"><span class="questions_item_answers_ans">ответить</span></a>

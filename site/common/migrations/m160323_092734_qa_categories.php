7987e789611fcb9c979e17149d1f76e3ed90a60b
choojoy-work <nikita@happy-giraffe.ru>
2016-03-23 17:32:44 +0300

    qa fixes

--- site/common/migrations/m160323_092734_qa_categories.php
+++ site/common/migrations/m160323_092734_qa_categories.php
@@ -4,6 +4,7 @@ class m160323_092734_qa_categories extends CDbMigration
 {
 	public function up()
 	{
+		$this->execute("ALTER TABLE `qa__categories` ADD `sort` TINYINT(3)  UNSIGNED  NOT NULL  AFTER `title`;");
 	}
 
 	public function down()

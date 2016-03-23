f359d417989fdd724a4f175b9c0bd8de983efab9
choojoy-work <nikita@happy-giraffe.ru>
2016-03-23 17:31:57 +0300

    qa fixes

---
+++ site/common/migrations/m160323_092734_qa_categories.php
@@ -0,0 +1,25 @@
+<?php
+
+class m160323_092734_qa_categories extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m160323_092734_qa_categories does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}

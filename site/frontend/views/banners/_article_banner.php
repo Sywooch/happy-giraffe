566ead041408dc991168b4ddbda9cb3df2befbf1
choojoy-work <nikita@happy-giraffe.ru>
2016-04-25 17:02:18 +0400

    fix

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,11 +1,9 @@
 <div class="article-banner">
-
-</div>
-
-<?php
+    <?php
     $this->beginWidget('AdsWidget', array(
         'dummyTag' => 'adfox',
     ));
     echo $this->renderPartial('site.frontend.widgets.views.ads.adfox.680x470');
     $this->endWidget();
-?>
+    ?>
+</div>

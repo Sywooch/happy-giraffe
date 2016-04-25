637754bf96c87f33449a03d9168ad861179b5796
choojoy-work <nikita@happy-giraffe.ru>
2016-04-25 16:40:13 +0400

    remove buzzoola

--- site/frontend/views/banners/_article_banner.php
+++ site/frontend/views/banners/_article_banner.php
@@ -1,19 +1,5 @@
 <div class="article-banner">
-    <div class="buzzplayer-stage" data-hash="XKYmYyY14N0uRxfFufNXA3md5rGHF7KhvnCf3IJaccg" data-width="500" data-expandable="true" data-target=".b-markdown:p">
-        <script type="text/javascript">
-            (function(w, d) {
-                var c = d.createElement("script");
-                c.src = "http://tube.buzzoola.com/new/build/buzzlibrary.js";
-                c.type = "text/javascript";
-                c.async = !0;
-                var f = function() {
-                    var p = d.getElementsByTagName("script")[0];
-                    p.parentNode.insertBefore(c, p);
-                };
-                "[object Opera]" == w.opera ? d.addEventListener("DOMContentLoaded", f, !1) : f();
-            })(window, document);
-        </script>
-    </div>
+
 </div>
 
 <?php

cc9d55fe4dc64a2517daf1a96b5860241e0877e2
choojoy-work <nikita@happy-giraffe.ru>
2016-05-12 12:41:10 +0400

    fix

--- site/frontend/modules/editorialDepartment/components/UsersControl.php
+++ site/frontend/modules/editorialDepartment/components/UsersControl.php
@@ -12,7 +12,7 @@ class UsersControl extends \CComponent
 
     public static function getUsersList()
     {
-        return array(\Yii::app()->user->id, 455993, 175718/ п); // @todo Сделать роль для супермодератора, убрать хардкод
+        return array(\Yii::app()->user->id, 455993, 175718); // @todo Сделать роль для супермодератора, убрать хардкод
     }
 
 }

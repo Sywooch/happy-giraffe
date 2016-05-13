31cc9e12129138ecc7f166eeb98f525861105706
InteI <jimmdigriz@yandex.ru>
2016-05-13 17:42:57 +0300

    dialogs

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -51,6 +51,10 @@ return array(
         '<_v>/api/cache' => 'api/<_v>/api/cache',
         '<_v>/api/logs' => 'api/<_v>/api/logs',
         '<_v>/api/signals' => 'api/<_v>/api/signals',
+        '<_v>/api/messages' => 'api/<_v>/api/messages',
+        '<_v>/api/dialogs' => 'api/<_v>/api/dialogs',
+        '<_v>/api/blacklist' => 'api/<_v>/api/blacklist',
+        '<_v>/api/contacts' => 'api/<_v>/api/contacts',
         /**-------------------------------------------------------------------------- API END ----------------------------------------------*/
 
 

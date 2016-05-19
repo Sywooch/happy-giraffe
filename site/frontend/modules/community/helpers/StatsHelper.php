2fe51358e32b7e3ef8fee4be3ef350a3833fad96
danil <danil@danil-MS-7817>
2016-05-19 09:54:17 +0300

    Убрана отладка

--- site/frontend/modules/community/helpers/StatsHelper.php
+++ site/frontend/modules/community/helpers/StatsHelper.php
@@ -11,7 +11,6 @@ use site\frontend\modules\comments\models\Comment;
 use site\frontend\modules\posts\models\Content;
 use site\frontend\modules\posts\models\Label;
 use site\frontend\modules\posts\models\Tag;
-use \site\frontend\components\LineDebug;
 
 /*
  * у класса не было документации, но в целом он считает колличество комментариев
@@ -126,29 +125,24 @@ WHERE
         return $value;
     }
 
+    /**
+     * "прогрев кеша коментариев", к сожалению реализовано не как прогрев, 
+     * а как каждый раз пересчёт значения количества комментариев
+     */
     public static function warmCache()
     {
-        LineDebug::init();
         self::getByLabels(array(Label::LABEL_NEWS), true);
 
         $models = \CommunityClub::model()->findAll();
 
-        LineDebug::printDebugLine();
-
         echo "Клубы:\n";
         foreach ($models as $m)
         {
-            LineDebug::printDebugLine();
             echo "\t" . $m->title . "\r\n";
             self::getSubscribers($m->id, true);
-            LineDebug::printDebugLine();
             self::getPosts($m->id, true);
-            LineDebug::printDebugLine();
             self::getComments($m->id, true);
-            LineDebug::printDebugLine();
             self::getByLabels(array($m->toLabel(), Label::LABEL_NEWS), true);
-            LineDebug::printDebugLine();
-            #LineDebug::printSQLLog();  exit();
         }
 
         $rubrics = \CommunityRubric::model()->findAll('community_id IS NOT NULL AND parent_id IS NULL');
@@ -157,9 +151,7 @@ WHERE
         {
             echo "\t" . $rubric->title . "\r\n";
             self::getRubricCount($rubric->id, true);
-            LineDebug::printDebugLine();
         }
-        LineDebug::printDebugLine();
     }
 
     /**

402226d168b5101aa9d2e20a86316d064064334b
Sergey Gubarev <dev_seregik@mail.ru>
2016-05-19 11:54:57 +0300

    У виджета аватаров для больших картинок (профиль) убран лишний элемент

--- site/frontend/widgets/userAvatarWidget/views/simple.php
+++ site/frontend/widgets/userAvatarWidget/views/simple.php
@@ -7,10 +7,13 @@
 $avatarUrl = $this->user->getAvatarUrl($this->size);
 ?>
 <?=CHtml::openTag($tag, $options)?>
-    <?php if ($this->user->online):?>
+        
+    <?php if ($this->user->online && $this->size !== Avatar::SIZE_LARGE):?>
         <span class="ico-status ico-status__online"></span>
     <?php endif ?>
+    
     <?php if ($avatarUrl !== false): ?>
         <?=CHtml::image($avatarUrl, '', array('class' => 'ava_img', 'width' => $this->size, 'height' => $this->size)) ?>
     <?php endif; ?>
+    
 <?=CHtml::closeTag($tag); ?>

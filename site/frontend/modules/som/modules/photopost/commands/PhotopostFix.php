8e93c0feac1c516a47b46a1b1468fba8544cc7cb
danil <danil@danil-MS-7817>
2016-06-02 11:42:56 +0300

    Revert "исправлено создание превью поста"
    This reverts commit 0bfe58d005da86799180fa2d82a6967566f89973.

--- site/frontend/modules/som/modules/photopost/commands/PhotopostFix.php
+++ site/frontend/modules/som/modules/photopost/commands/PhotopostFix.php
@@ -48,7 +48,6 @@ class PhotopostFix extends \CConsoleCommand
             }
             print "post_content: {$post->id}, original_id: {$post->originEntityId}\r\n";
             print "updating";
-            $post->preview = $p->getPhotopostTag();
             $post->delActivity();
             $post->addActivity();
             print ", updated\r\n";

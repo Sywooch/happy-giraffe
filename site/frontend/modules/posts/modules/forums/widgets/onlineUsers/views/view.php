4b95c3820f9a0c6392f045a28853e047801c1bb1
choojoy-work <nikita@happy-giraffe.ru>
2016-06-21 18:37:17 +0400

    Виджеты

--- site/frontend/modules/posts/modules/forums/widgets/onlineUsers/views/view.php
+++ site/frontend/modules/posts/modules/forums/widgets/onlineUsers/views/view.php
@@ -10,7 +10,7 @@
     <ul class="now-online__list">
         <?php foreach ($users as $user): ?>
         <li class="now-online__item">
-            <a href="<?=$user->profileUrl?>" class="now-online__link"><img src="/images/icons/ava.jpg" alt="" class="now-online__img"></a>
+            <a href="<?=$user->getUrl()?>" class="now-online__link"><img src="<?=$user->getAvatarUrl(40)?>" alt="" class="now-online__img"></a>
         </li>
         <?php endforeach; ?>
     </ul>

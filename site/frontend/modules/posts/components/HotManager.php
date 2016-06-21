16a817ed3100424596193b4a6714a023989718f2
choojoy-work <nikita@happy-giraffe.ru>
2016-06-21 17:01:31 +0400

    Виджеты

--- site/frontend/modules/posts/components/HotManager.php
+++ site/frontend/modules/posts/components/HotManager.php
@@ -13,7 +13,7 @@ use site\frontend\modules\posts\models\Content;
 
 class HotManager
 {
-    const HOT_THRESHOLD = 1000;
+    const HOT_THRESHOLD = 500;
     const COMMENTS_COUNT_MULTIPLIER = 10;
     const VIEWS_COUNT_MULTIPLIER = 1;
     

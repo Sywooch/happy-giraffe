35eeda66df9be4e8b5758fc65182ca86004c4692
danil <danil@danil-MS-7817>
2016-06-27 11:36:33 +0300

    Добавление миграции на создание новых колонок и контроллера заполняющего новый индекс

--- site/common/migrations/m160627_080821_post_contents_performance.php
+++ site/common/migrations/m160627_080821_post_contents_performance.php
@@ -12,9 +12,8 @@ class m160627_080821_post_contents_performance extends CDbMigration
         print "create table index\r\n";
         $this->execute("CREATE INDEX post__contents_label_section_IDX ON happy_giraffe.post__contents (label_section,label_subsections)");
         print "start fil index columns\r\n";
-        $c = \site\frontend\modules\posts\commands\FillLables::create();
+        $c = new \site\frontend\modules\posts\commands\FillLables('migrate', new CConsoleCommandRunner);
         $c->actionIndex();
-        # Yii::app()->create
         return true;
     }
 

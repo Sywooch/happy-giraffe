6dcbbb7329704c61595a4cac0eabe6a8fe7e5b2b
Sergey Gubarev <dev_seregik@mail.ru>
2016-06-28 18:13:38 +0300

    tmp Блоги

--- site/frontend/modules/posts/modules/blogs/widgets/blogoefir/views/view.php
+++ site/frontend/modules/posts/modules/blogs/widgets/blogoefir/views/view.php
@@ -3,70 +3,7 @@
     	<div class="b-widget-header__title b-widget-header__title_live">Блогоэфир</div>
 	</div>
   	<div class="b-widget-content">
-        <ul class="b-widget-content__list">
-        
-        	<?php foreach ($rows as $row): ?>
-        		
-        	<li class="b-widget-content__item">
-            	<div class="b-widget-content__ava">
-            		<img src="<?php echo $row->user->avatarUrl; ?>" alt="">
-        		</div>
-        		
-        		<?php
-        		
-        		echo CHtml::link($row->user->fullName, $row->user->profileUrl, [
-        		    'class' => 'b-widget-content__username'
-        		]);
-        		
-        		?>
-            	
-            	<div class="b-widget-content__date">
-            		<?php echo HHtml::timeTag($row, ['class' => '']); ?>
-            	</div>
-            	
-                <!--             	
-                <div class="b-widget-content__date">30 минут назад</div> 
-                -->
-                
-            	<div class="b-widget-content__title">
-            		<?php 
-            		
-            		echo CHtml::link($row->title, $row->parsedUrl, [
-            		    'class' => 'b-widget-content__link'
-            		]);
-            		
-            		?>
-        		</div>
-      		</li>
-        	
-        	<?php endforeach; ?>
-        	
-        	<?php if (FALSE): ?>
-        	
-      		<li class="b-widget-content__item">
-            	<div class="b-widget-content__ava"><img src="/images/icons/ava.jpg" alt=""></div><a href="#" class="b-widget-content__username">Ольга Емельянова</a>
-            	<div class="b-widget-content__date">30 минут назад</div>
-            	<div class="b-widget-content__title"><a href="#" class="b-widget-content__link">Что вы ели при токсиккозе? Не могу больше пить чай с бубликами</a></div>
-      		</li>
-          	<li class="b-widget-content__item">
-            	<div class="b-widget-content__ava"><img src="/images/icons/ava.jpg" alt=""></div><a href="#" class="b-widget-content__username">Ольга Емельянова</a>
-            	<div class="b-widget-content__date">30 минут назад</div>
-            	<div class="b-widget-content__title"><a href="#" class="b-widget-content__link">Что вы ели при токсиккозе? Не могу больше пить чай с бубликами</a></div>
-          	</li>
-          	<li class="b-widget-content__item">
-            	<div class="b-widget-content__ava"><img src="/images/icons/ava.jpg" alt=""></div><a href="#" class="b-widget-content__username">Ольга Емельянова</a>
-            	<div class="b-widget-content__date">30 минут назад</div>
-            	<div class="b-widget-content__title"><a href="#" class="b-widget-content__link">Что вы ели при токсиккозе? Не могу больше пить чай с бубликами</a></div>
-          	</li>
-          	<li class="b-widget-content__item">
-            	<div class="b-widget-content__ava"><img src="/images/icons/ava.jpg" alt=""></div><a href="#" class="b-widget-content__username">Ольга Емельянова</a>
-            	<div class="b-widget-content__date">30 минут назад</div>
-            	<div class="b-widget-content__title"><a href="#" class="b-widget-content__link">Что вы ели при токсиккозе? Не могу больше пить чай с бубликами</a></div>
-          	</li>
-          	
-          	<?php endif; ?>
-          	
-        </ul>
+        <ul class="b-widget-content__list"><?php echo $items; ?></ul>
     	<div class="b-widget-controls">
       		<div class="b-widget-controls__left"></div>
       		<div class="b-widget-controls__right"></div>

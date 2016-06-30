e39e02db27f9c8db896fa5a88039eae636fa2fa4
choojoy-work <nikita@happy-giraffe.ru>
2016-06-30 12:21:31 +0400

    Интеграция верстки

--- site/frontend/views/layouts/lite/common_menu.php
+++ site/frontend/views/layouts/lite/common_menu.php
@@ -28,7 +28,7 @@ if (! Yii::app()->user->isGuest) {
                     <li class="header__li"><a href="<?=$this->createUrl('/som/qa/default/index')?>" class="header__link header__link_answers">Ответы</a></li>
                     <!--<li class="header__li"><a href="#" class="header__link header__link_blog">Блоги</a></li>-->
                     <li class="header__li"><a href="<?=$this->createUrl('/posts/buzz/list/index')?>" class="header__link header__link_like">Жизнь</a></li>
-                    <li class="header__li hidden-md"><a href="#" class="header__link header__link_search">Поиск</a></li>
+                    <li class="header__li hidden-md"><a href="#js-madal-search-box" class="header__link header__link_search popup-a">Поиск</a></li>
                 </ul>
                 <?php if (Yii::app()->user->isGuest): ?>
                     <div class="user-unloged">

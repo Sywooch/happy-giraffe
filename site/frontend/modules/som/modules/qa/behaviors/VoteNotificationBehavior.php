ffcc41ee16023ac25ed22590867f5b7bbce7e2bf
InteI <jimmdigriz@yandex.ru>
2016-07-18 17:51:38 +0300

    -----

--- site/frontend/modules/som/modules/qa/behaviors/VoteNotificationBehavior.php
+++ site/frontend/modules/som/modules/qa/behaviors/VoteNotificationBehavior.php
@@ -20,24 +20,70 @@ class VoteNotificationBehavior extends BaseBehavior
         /**
          * @var QaAnswer $answer
          */
-        $answer = $this->owner->answer;
-        if ($answer->isNewRecord) {
-            $this->addNotification($this->owner, $answer);
-        }
+        $this->addNotification($this->owner, $this->owner->answer);
+        return parent::afterSave($event);
     }
 
-    public function afterDelete($event)
+    public function beforeDelete($event)
     {
+        try {
+            /**
+             * @var Notification[] $signals
+             */
+            $signals = Notification::model()->byEntity($this->owner->answer)->findAll();
 
+            foreach ($signals as &$signal) {
+                $readEntityDeleted = $signal->readEntities && $this->removeEntity($signal->readEntities, $this->owner);
+                $unreadEntityDeleted = $signal->unreadEntities && $this->removeEntity($signal->unreadEntities, $this->owner);
+                if ($readEntityDeleted || $unreadEntityDeleted) {
+                    if ((count($signal->readEntities) + count($signal->unreadEntities)) == 0) {
+                        $signal->delete();
+                    } else {
+                        $signal->save();
+                    }
+                }
+            }
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+            die();
+        }
+
+        return parent::beforeDelete($event);
     }
 
     protected function removeEntity(&$entities, $model)
     {
-
+        foreach ($entities as $k => $entity) {
+            if ($entity->userId == $this->owner->userId && $entity->class == get_class($model)) {
+                unset($entities[$k]);
+                return true;
+            }
+        }
+        return false;
     }
 
     protected function addNotification(QaAnswerVote $vote, QaAnswer $answer)
     {
-        $notification = $this->findOrCreateNotification(get_class($answer), $answer->id, $answer->authorId, self::TYPE, array($vote->authorId, $vote->user->avatarUrl));
+        try {
+            $notification = $this->findOrCreateNotification(get_class($answer), $answer->id, $answer->authorId, self::TYPE, array($vote->userId, $vote->user->avatarUrl));
+//            $notification->entity->tooltip = "Вам сказали спасибо";
+//            $notification->entity->title = "Вам сказали спасибо";
+//            $notification->entity->url = $answer->question->url;
+//            $notification->entity->userId = $answer->authorId;
+
+            $entity = new Entity($vote);
+            $entity->userId = $vote->userId;
+
+            $entity->title = $answer->text;
+
+            $notification->unreadEntities[] = $entity;
+            if (!$notification->save()) {
+                echo print_r($notification->errors, true);
+                die();
+            }
+        } catch (\Exception $ex) {
+            echo $ex->getMessage();
+            die();
+        }
     }
 }

865d9e065527cda700330f66b87d47049dc90135
InteI <jimmdigriz@yandex.ru>
2016-07-19 10:32:45 +0300

    ---

--- site/frontend/modules/som/modules/qa/behaviors/VoteNotificationBehavior.php
+++ site/frontend/modules/som/modules/qa/behaviors/VoteNotificationBehavior.php
@@ -26,26 +26,21 @@ class VoteNotificationBehavior extends BaseBehavior
 
     public function beforeDelete($event)
     {
-        try {
-            /**
-             * @var Notification[] $signals
-             */
-            $signals = Notification::model()->byEntity($this->owner->answer)->findAll();
+        /**
+         * @var Notification[] $signals
+         */
+        $signals = Notification::model()->byEntity($this->owner->answer)->findAll();
 
-            foreach ($signals as &$signal) {
-                $readEntityDeleted = $signal->readEntities && $this->removeEntity($signal->readEntities, $this->owner);
-                $unreadEntityDeleted = $signal->unreadEntities && $this->removeEntity($signal->unreadEntities, $this->owner);
-                if ($readEntityDeleted || $unreadEntityDeleted) {
-                    if ((count($signal->readEntities) + count($signal->unreadEntities)) == 0) {
-                        $signal->delete();
-                    } else {
-                        $signal->save();
-                    }
+        foreach ($signals as &$signal) {
+            $readEntityDeleted = $signal->readEntities && $this->removeEntity($signal->readEntities, $this->owner);
+            $unreadEntityDeleted = $signal->unreadEntities && $this->removeEntity($signal->unreadEntities, $this->owner);
+            if ($readEntityDeleted || $unreadEntityDeleted) {
+                if ((count($signal->readEntities) + count($signal->unreadEntities)) == 0) {
+                    $signal->delete();
+                } else {
+                    $signal->save();
                 }
             }
-        } catch (\Exception $ex) {
-            echo $ex->getMessage();
-            die();
         }
 
         return parent::beforeDelete($event);
@@ -64,26 +59,13 @@ class VoteNotificationBehavior extends BaseBehavior
 
     protected function addNotification(QaAnswerVote $vote, QaAnswer $answer)
     {
-        try {
-            $notification = $this->findOrCreateNotification(get_class($answer), $answer->id, $answer->authorId, self::TYPE, array($vote->userId, $vote->user->avatarUrl));
-//            $notification->entity->tooltip = "Вам сказали спасибо";
-//            $notification->entity->title = "Вам сказали спасибо";
-//            $notification->entity->url = $answer->question->url;
-//            $notification->entity->userId = $answer->authorId;
+        $notification = $this->findOrCreateNotification(get_class($answer), $answer->id, $answer->authorId, self::TYPE, array($vote->userId, $vote->user->avatarUrl));
+        $entity = new Entity($vote);
+        $entity->userId = $vote->userId;
 
-            $entity = new Entity($vote);
-            $entity->userId = $vote->userId;
+        $entity->title = $answer->text;
 
-            $entity->title = $answer->text;
-
-            $notification->unreadEntities[] = $entity;
-            if (!$notification->save()) {
-                echo print_r($notification->errors, true);
-                die();
-            }
-        } catch (\Exception $ex) {
-            echo $ex->getMessage();
-            die();
-        }
+        $notification->unreadEntities[] = $entity;
+        $notification->save();
     }
 }

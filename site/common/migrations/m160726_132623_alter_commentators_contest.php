77748d7dd94b8d23ded06bd65170eb3dbfce7267
InteI <jimmdigriz@yandex.ru>
2016-07-29 10:32:30 +0300

    -

--- site/common/migrations/m160726_132623_alter_commentators_contest.php
+++ site/common/migrations/m160726_132623_alter_commentators_contest.php
@@ -4,12 +4,24 @@ class m160726_132623_alter_commentators_contest extends CDbMigration
 {
 	public function up()
 	{
-		$this->dropColumn('commentators__contests', 'startDate');
-		$this->dropColumn('commentators__contests', 'endDate');
+		try {
+			$this->dropColumn('commentators__contests', 'startDate');
+			$this->dropColumn('commentators__contests', 'endDate');
+		} catch (\Exception $ex) {
 
-		$this->addColumn('commentators__contests', 'month', 'string NOT NULL');
+		}
 
-		$this->createIndex('month_idx', 'commentators__contests', 'month');
+		try {
+			$this->addColumn('commentators__contests', 'month', 'string NOT NULL');
+		} catch (\Exception $ex) {
+
+		}
+
+		try {
+			$this->createIndex('month_idx', 'commentators__contests', 'month');
+		} catch (\Exception $ex) {
+			
+		}
 
 		$this->renameColumn('commentators__contests', 'title', 'name');
 	}

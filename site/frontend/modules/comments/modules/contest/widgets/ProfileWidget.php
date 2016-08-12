2a06c1b277e401dbe1d86fd0e3497b1a4dec167a
InteI <jimmdigriz@yandex.ru>
2016-08-12 10:41:34 +0300

    -

---
+++ site/frontend/modules/comments/modules/contest/widgets/ProfileWidget.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Никита
+ * @date 03/03/15
+ */
+namespace site\frontend\modules\comments\modules\contest\widgets;
+use site\frontend\modules\comments\modules\contest\models\CommentatorsContest;
+use site\frontend\modules\comments\modules\contest\models\CommentatorsContestParticipant;
+class ProfileWidget extends \CWidget
+{
+    public $userId;
+    public $contest;
+    public $participant;
+    public function init()
+    {
+        return '';
+        $this->contest = CommentatorsContest::model()->active()->find();
+        if ($this->contest) {
+            $this->participant = CommentatorsContestParticipant::model()->byContest($this->contest->id)->byUser($this->userId)->find();
+        }
+    }
+    public function run()
+    {
+        return '';
+        if ($this->participant !== null) {
+            $this->render('ProfileWidget');
+        }
+    }
+}

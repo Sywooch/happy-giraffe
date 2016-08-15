56a3fe391816cf39f5fc88f262aec788dbcc24f9
shtorm <emilvililyaev@gmail.com>
2016-08-15 14:03:57 +0300

    промежуточный

--- site/frontend/modules/landing/modules/pediatrician/controllers/DefaultController.php
+++ site/frontend/modules/landing/modules/pediatrician/controllers/DefaultController.php
@@ -2,22 +2,41 @@
 
 namespace site\frontend\modules\landing\modules\pediatrician\controllers;
 
+use site\frontend\modules\som\modules\qa\models\QaQuestion;
+use site\frontend\modules\som\modules\qa\models\QaCategory;
 /**
  * @author Sergey Gubarev
  */
 class DefaultController extends \LiteController
 {
-   
+
    /**
     * @var string
     */
-   public $litePackage = 'landing_pediatrician';
-   
+   public $litePackage = 'contest_commentator';
+
+   /**
+    * @var integer
+    */
+   private $_pediatorCategoryId = 124;
+
    //-----------------------------------------------------------------------------------------------------------
-   
+
    public function actionIndex()
    {
-       $this->render('index');
+       $question = clone QaQuestion::model();
+//        $objCategoty = QaCategory::model();
+//        $question->category($objCategoty::);
+       $question->category($this->_pediatorCategoryId);
+
+       $dp = new \CActiveDataProvider($question, array(
+           'pagination' => array(
+               'pageVar' => 'page',
+           ),
+       ));
+
+       $this->render('index', ['dp' => $dp]);
+
    }
-    
+
 }
--- site/frontend/modules/landing/modules/pediatrician/views/default/index.php
+++ site/frontend/modules/landing/modules/pediatrician/views/default/index.php
@@ -1,3 +1,30 @@
-<?php
-
-echo __FILE__;
+<div class="landing">
+    <?php $this->renderPartial('/_header'); ?>
+    <?php $this->renderPartial('/_editor'); ?>
+    <?php $this->renderPartial('/_doctors'); ?>
+    <div class="landing__body">
+        <div class="landing-question">
+            <div class="textalign-c margin-b30">
+            	<div class="font__title-xm font__bold">Последние вопросы</div>
+            </div>
+                <?php
+                $this->widget('LiteListView', array(
+                    'dataProvider' => $dp,
+                    'itemView' => '/_question',
+                    'itemsTagName' => 'ul',
+                    'itemsCssClass' => 'questions questions-modification',
+                    'template' => '{items}<div class="yiipagination yiipagination__center">{pager}</div>',
+                    'pager' => [
+                        'class'           => 'LitePagerDots',
+                        'prevPageLabel'   => '&nbsp;',
+                        'nextPageLabel'   => '&nbsp;',
+                        'showPrevNext'    => TRUE,
+                        'showButtonCount' => 5,
+                        'dotsLabel'       => '<li class="page-points">...</li>'
+                    ]
+                ));
+                ?>
+        </div>
+    </div>
+    <?php $this->renderPartial('/_footer'); ?>
+</div>

b0ebd95059b0e241811972ceb1a69ac88ebe0bf8
choojoy-work <nikita@happy-giraffe.ru>
2016-08-19 18:29:52 +0400

    Исправления

--- site/frontend/modules/landing/modules/pediatrician/views/_footer.php
+++ site/frontend/modules/landing/modules/pediatrician/views/_footer.php
@@ -1,6 +1,6 @@
 <div class="landing-footer landing-footer_bg">
     <div class="landing-footer__left">
-    	<div class="landing-footer__phone landing-footer__phone-position"></div>
+    	<a href="https://play.google.com/store/apps/details?id=ru.happy_giraffe.mypediatrician" class="landing-footer__phone landing-footer__phone-position display-b"></a>
     </div>
     <div class="landing-footer__right">
         <div class="landing-footer__info textalign-c">

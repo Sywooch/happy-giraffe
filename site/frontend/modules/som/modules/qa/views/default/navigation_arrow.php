66ddae94654166d6ea2444f41c8e8bc696cd0220
choojoy-work <nikita@happy-giraffe.ru>
2016-08-19 18:08:55 +0400

    Исправления

--- site/frontend/modules/som/modules/qa/views/default/navigation_arrow.php
+++ site/frontend/modules/som/modules/qa/views/default/navigation_arrow.php
@@ -7,7 +7,7 @@
 ?>
 <table class="article-nearby clearfix">
     <tr>
-        <td><?= $left ? '<a href="' . $left->url . '" class="article-nearby_a article-nearby_a__l" rel="prev"><span class="article-nearby_tx">' . $left->title . '</span></a>' : '&nbsp;' ?></td>
-        <td><?= $right ? '<a href="' . $right->url . '" class="article-nearby_a article-nearby_a__r" rel="next"><span class="article-nearby_tx">' . $right->title . '</span></a>' : '&nbsp;' ?></td>
+        <td><?= $left ? '<a href="' . $left->url . '" class="article-nearby_a article-nearby_a__l" rel="prev"><span class="article-nearby_tx">' . CHtml::encode($left->title) . '</span></a>' : '&nbsp;' ?></td>
+        <td><?= $right ? '<a href="' . $right->url . '" class="article-nearby_a article-nearby_a__r" rel="next"><span class="article-nearby_tx">' . CHtml::encode($right->title) . '</span></a>' : '&nbsp;' ?></td>
     </tr>
 </table>

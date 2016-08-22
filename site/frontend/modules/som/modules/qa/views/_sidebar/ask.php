4f0ddcd3e28d2f272819ede050f6ddb89b69259d
shtorm <emilvililyaev@gmail.com>
2016-08-22 15:31:50 +0300

    hot fix button+jsValidation

--- site/frontend/modules/som/modules/qa/views/_sidebar/ask.php
+++ site/frontend/modules/som/modules/qa/views/_sidebar/ask.php
@@ -1,3 +1,3 @@
 <div class="textalign-c">
-    <a class="btn btn-success btn-xl" href="<?=$this->createUrl('/som/qa/default/questionAddForm/')?>" data-bind="follow: {}">Задать вопрос</a>
+    <a class="btn btn-success btn-xl login-button" href="<?=$this->createUrl('/som/qa/default/questionAddForm/')?>" data-bind="follow: {}">Задать вопрос</a>
 </div>

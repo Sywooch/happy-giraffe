0f2ea045f056d8889b2df642512f55426e1bf7cf
choojoy-work <nikita@happy-giraffe.ru>
2016-08-30 16:04:17 +0400

    Специалист

--- site/frontend/modules/som/modules/qa/views/_answer.php
+++ site/frontend/modules/som/modules/qa/views/_answer.php
@@ -17,7 +17,7 @@
         <a href="<?=$data->user->profileUrl?>"><?=$data->user->fullName?></a>
         <?= HHtml::timeTag($data, array('class' => 'tx-date')); ?>
     </div>
-    <div class="answers-list_item_text-block">
+    <div class="answers-list_item_text-block" <?php if ($data->user->specialistInfo['title']): ?>style="background-color: #feebf6; border-radius: 7px;"<?php endif; ?>>
         <?php if (false && $data->isBest): ?>
             <div class="dialog-arrow dialog-arrow-bestred"></div>
         <?php endif; ?>

54ace2bb980a66b0a623a69c3b659fc593b9e694
Sergey Gubarev <dev_seregik@mail.ru>
2016-10-11 17:34:32 +0300

    pediator правки

--- site/frontend/modules/specialists/modules/pediatrician/views/default/profile.php
+++ site/frontend/modules/specialists/modules/pediatrician/views/default/profile.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * @var LiteController
  * @var \site\frontend\modules\specialists\models\ProfileForm $form
  */
 $this->pageTitle = 'Профиль специалиста';
+
 ?>
 
 <div class="user-setting__exit"><a href="<?=$this->createUrl('/site/logout')?>" class="user-setting__exit-link">Выход</a></div>

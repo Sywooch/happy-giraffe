972c6a1497bbf008bb2e4b3ce6818f54a930a89f
Sergey Gubarev <dev_seregik@mail.ru>
2016-10-12 12:17:29 +0300

    new pediator (menu)

--- site/frontend/modules/specialists/modules/pediatrician/views/layouts/main.php
+++ site/frontend/modules/specialists/modules/pediatrician/views/layouts/main.php
@@ -1,12 +1,11 @@
 <?php
+
 /**
  * @var LiteController $this
  * @var string $content
  */
 $this->beginContent('//layouts/lite/common');
 
-$questionsCount = \site\frontend\modules\specialists\modules\pediatrician\components\QaManager::getQuestionsCount(Yii::app()->user->id);
-$questionsCount = ($questionsCount > 99) ? '99+' : $questionsCount;
 ?>
 
 <div class="layout-header">
@@ -14,19 +13,27 @@ $questionsCount = ($questionsCount > 99) ? '99+' : $questionsCount;
         <div class="float-l">
             <div class="pediator-header__left">
                 <div class="pediator-header__ico"></div>
-                <div class="pediator-header__text">ЖИРАФ ПЕДИАТР</div>
             </div>
         </div>
         <nav class="pediator-nav__wrapper">
-            <ul class="pediator-nav__list">
+            <ul class="pediator-nav__lists">
                 <li class="pediator-nav__list<?php if ($this->action->id == 'questions'): ?> pediator-nav__list-active<?php endif; ?>">
                     <a href="<?=$this->createUrl('/specialists/pediatrician/default/questions')?>" class="pediator-nav__link">Вопросы</a>
-                    <?php if ($questionsCount > 0): ?>
-                        <span class="pediator-nav__mark"><span><?=$questionsCount?></span></span>
-                    <?php endif; ?>
+                </li>
+                <li class="pediator-nav__list">
+                	<a href="#" class="pediator-nav__link">Пульс</a>
                 </li>
                 <li class="pediator-nav__list<?php if ($this->action->id == 'answers'): ?> pediator-nav__list-active<?php endif; ?>">
                     <a href="<?=$this->createUrl('/specialists/pediatrician/default/answers')?>" class="pediator-nav__link">Мои ответы</a>
+                </li>
+                <li class="pediator-nav__list">
+                	<a href="#" class="pediator-nav__link">Статистика</a>
+                </li>
+                <li class="pediator-nav__list">
+                	<a href="#" class="pediator-nav__link">Рейтинг</a>
+                </li>
+                <li class="pediator-nav__list display-n">
+                	<a href="#" class="pediator-nav__link pediator-nav__link--answer">?</a>
                 </li>
             </ul>
         </nav>

edebf152f8502ae59517b0e8fe1d4c82ad8a5fa9
choojoy-work <nikita@happy-giraffe.ru>
2016-10-12 11:53:38 +0400

    Статистика

--- site/frontend/modules/specialists/modules/pediatrician/widgets/stats/views/stats.php
+++ site/frontend/modules/specialists/modules/pediatrician/widgets/stats/views/stats.php
@@ -16,102 +16,33 @@
             </tr>
             <tr class="statistic-table--bg-green statistic-table--style statistik__text--grey statistic-table--title-font-medium">
                 <td class="statistic-table--title statistik__text--grey-lighten statistic-table--title-font-xl">Ответы</td>
-                <?php $square = false; foreach ($month->days as $j => $day): if ($day['nAnswers'] > 0) $square = true; ?>
-                    <?php if ($day['nAnswers'] == 0): ?>
-                        <?php if (($month->year . '-' . $month->month . '-' . $j) > date('Y-m-d') || (! $square && ($i == count($months) - 1))): ?>
-                            <td><span class="statistic-table--color-ccc">•</span></td>
+                <?php foreach ($month->days as $j => $day): ?>
+                    <?php if ($day): ?>
+                        <?php if ($day['nAnswers'] > 0): ?>
+                            <td><?=$day['nAnswers']?></td>
                         <?php else: ?>
                             <td><span class="statistic-table__square statistic-table__square--green"></span></td>
                         <?php endif; ?>
                     <?php else: ?>
-                        <td><?=$day['nAnswers']?></td>
+                        <td><span class="statistic-table--color-ccc">•</span></td>
                     <?php endif; ?>
                 <?php endforeach; ?>
-                <!--<td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table__square statistic-table__square--green"></span></td>
-                <td><span class="statistic-table__square statistic-table__square--green"></span></td>
-                <td><span class="statistic-table__square statistic-table__square--green"></span></td>
-                <td>2</td>
-                <td>43</td>
-                <td>34</td>
-                <td>17</td>
-                <td>18</td>
-                <td>19</td>
-                <td>20</td>
-                <td>21</td>
-                <td>22</td>
-                <td>23</td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>-->
             </tr>
             <tr class="statistic-table--bg-red statistic-table--style statistik__text--grey statistic-table--title-font-medium">
                 <td class="statistic-table--title statistik__text--grey-lighten statistic-table--title-font-xl">Спасибо</td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table__square statistic-table__square--red"></span></td>
-                <td><span class="statistic-table__square statistic-table__square--red"></span></td>
-                <td><span class="statistic-table__square statistic-table__square--red"></span></td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">12</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">12</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">12</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">12</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">99</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">8</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">8</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">8</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">8</span></div>
-                </td>
-                <td>
-                    <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num">8</span></div>
-                </td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
-                <td><span class="statistic-table--color-ccc">•</span></td>
+                <?php foreach ($month->days as $j => $day): ?>
+                    <?php if ($day): ?>
+                        <?php if ($day['nLikes'] > 0): ?>
+                            <td>
+                                <div class="statistic-table__senks"><span class="statistic-table__roze"></span><span class="statistic-table__num"><?=$day['nLikes']?></span></div>
+                            </td>
+                        <?php else: ?>
+                            <td><span class="statistic-table__square statistic-table__square--red"></span></td>
+                        <?php endif; ?>
+                    <?php else: ?>
+                        <td><span class="statistic-table--color-ccc">•</span></td>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </tr>
             </tbody></table>
     </div>

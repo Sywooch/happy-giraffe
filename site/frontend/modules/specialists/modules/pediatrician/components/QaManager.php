8092e10a95cb8dc5cfc639445626564ae5222188
shtorm <emilvililyaev@gmail.com>
2016-11-01 17:35:00 +0300

    add key for answers

--- site/frontend/modules/specialists/modules/pediatrician/components/QaManager.php
+++ site/frontend/modules/specialists/modules/pediatrician/components/QaManager.php
@@ -85,39 +85,18 @@ class QaManager
         return $criteria;
     }
 
-    /**
-     * Emil Vililyaev: пока не используется
-     * @param integer $userID
-     * @return \CDbCriteria
-     */
-    public static function getPersonalQuestions($userID)
-    {
-        $criteria = new \CDbCriteria();
-        $criteria->select = 't.*';
-        $criteria->join = 'LEFT JOIN qa__answers AS t2 ON t2.root_id = t.id AND t2.isRemoved = 0';
-        $criteria->join .= ' LEFT JOIN qa__answers AS t3 ON t3.root_id = t2.id AND t3.isRemoved = 0';
-        $criteria->addCondition('t2.id IS NOT NULL');
-        $criteria->addCondition('t.authorId = :userId');
-        $criteria->addCondition('t3.id IS NULL AND t.isRemoved = 0');
-        $criteria->params[':userId'] = $userID;
-
-        $rows = \Yii::app()->db->getCommandBuilder()->createFindCommand('qa__answers', $criteria)->queryAll();
-
-        return $criteria;
-    }
-
     public static function getQuestionsCriteria($userId)
     {
         $criteria = new \CDbCriteria();
         $criteria->select = 't.*';
-        $criteria->join = 'LEFT OUTER JOIN ' . QaAnswer::model()->tableName() . ' answers ON answers.questionId = t.id AND answers.isRemoved = 0';
+        $criteria->join = 'LEFT OUTER JOIN ' . QaAnswer::model()->tableName() . ' answers FORCE INDEX FOR JOIN(`questionId_isRemoved`) ON answers.questionId = t.id AND answers.isRemoved = 0';
         $criteria->group = 't.id';
         $criteria->scopes = ['category' => [self::getCategoryId()]];
         $criteria->with = 'category';
         $criteria->addCondition('t.id NOT IN (SELECT questionId FROM ' . self::SKIPS_TABLE . ' WHERE userId = :userId)');
         $criteria->addCondition('(answers.id IS NULL) OR (t.id NOT IN(SELECT a1.questionId FROM qa__answers a1
-            LEFT JOIN qa__answers a2 ON a2.root_id = a1.id AND a2.isRemoved = 0
-            LEFT JOIN qa__answers a3 ON a3.root_id = a2.id AND a3.isRemoved = 0
+            LEFT JOIN qa__answers a2 FORCE INDEX FOR JOIN(`root_id_isRemoved`) ON a2.root_id = a1.id AND a2.isRemoved=0
+            LEFT JOIN qa__answers a3 FORCE INDEX FOR JOIN(`root_id_isRemoved`) ON a3.root_id = a2.id AND a3.isRemoved=0
             WHERE
             (a1.authorId IN (SELECT specialists__profiles.id FROM specialists__profiles)
                 AND (a3.authorId IN (SELECT specialists__profiles.id FROM specialists__profiles) OR (a3.authorId IS NULL AND a2.authorId IS NULL))

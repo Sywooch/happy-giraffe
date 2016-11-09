270edea5964839816cdb7cef2a657363130a39a8
choojoy-work <nikita@happy-giraffe.ru>
2016-11-09 12:40:38 +0400

    remove duel & old user widgets

--- site/common/migrations/m161109_083712_duel_remove.php
+++ site/common/migrations/m161109_083712_duel_remove.php
@@ -4,6 +4,11 @@ class m161109_083712_duel_remove extends CDbMigration
 {
 	public function up()
 	{
+		$this->execute("SET FOREIGN_KEY_CHECKS=0;");
+		$this->execute("DROP TABLE duel__question;");
+		$this->execute("DROP TABLE duel__answer;");
+		$this->execute("DROP TABLE duel__answer_votes;");
+		$this->execute("SET FOREIGN_KEY_CHECKS=1;");
 	}
 
 	public function down()

d69ae4d4048cb4ea3b4d3ea7264fc25becb6fe1b
choojoy-work <nikita@happy-giraffe.ru>
2016-11-10 13:17:32 +0400

    remove whatsNew part 4

--- site/frontend/modules/whatsNew/views/friends/types/service.php
+++
@@ -1,9 +0,0 @@
-<h3 class="masonry-news-list_title textalign-c">
-    <?=CHtml::link($data->service->title, $data->service->url)?>
-</h3>
-<div class="masonry-news-list_content">
-    <?=CHtml::link(CHtml::image($data->service->photo->getPreviewUrl(104, null, Image::WIDTH)), $data->service->url)?>
-</div>
-<div class="comments-all clearfix">
-    <?=CHtml::link('Попробовать сервис!', $data->service->url)?>
-</div>

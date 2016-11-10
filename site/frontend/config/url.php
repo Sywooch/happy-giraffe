948d2585ea571dc447bc3752eb74541e6ec9ca40
choojoy-work <nikita@happy-giraffe.ru>
2016-11-10 13:20:55 +0400

    remove familyOld module

--- site/frontend/config/url.php
+++ site/frontend/config/url.php
@@ -699,7 +699,6 @@ return array(
         'user/<userId:\d+>/family/photo<photoId:\d+>' => 'photo/singlePhoto/family',
         'user/<userId:\d+>/family' => 'family/default/index',
         'user/<userId:\d+>/family/edit' => 'family/default/fill',
-        'user/<userId:\d+>/familyOld' => 'familyOld/default/index',
 
         'contest/<cssClass:\w+>' => 'community/contest/index',
 

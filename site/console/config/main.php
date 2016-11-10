5c040b11f2782c9c77a823463c23ec2408d23e2a
InteI <jimmdigriz@yandex.ru>
2016-11-10 16:05:14 +0300

    -

--- site/console/config/main.php
+++ site/console/config/main.php
@@ -105,7 +105,10 @@ return array(
         ),
         'pushWorker' => [
             'class' => \site\frontend\modules\api\ApiModule::PUSH_WORKER,
-        ]
+        ],
+        'fixRatingByHistory' => [
+            'class' => 'site\frontend\modules\som\modules\qa\commands\FixRatingByHistoryCommand',
+        ],
     ),
     'import' => array(
         'site.common.components.*',

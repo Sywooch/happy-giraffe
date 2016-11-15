b9a52ff3d0b6683603353d42a99ea6defe6ead74
choojoy-work <nikita@happy-giraffe.ru>
2016-11-15 10:09:10 +0400

    remove backend

--- site/backend/modules/club/views/interest/update.php
+++
@@ -1,3 +0,0 @@
-<h1>Изменить интерес <?php echo $model->id; ?></h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- site/backend/modules/club/views/recipeBookDiseaseCategory/create.php
+++
@@ -1,3 +0,0 @@
-<h1>Добавление категории детских болезней</h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>

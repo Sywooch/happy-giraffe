3d3218d95475cae64288b0047c224592b4a47f9d
choojoy-work <nikita@happy-giraffe.ru>
2016-11-18 11:47:35 +0400

    cleaning /javascripts part 1

--- site/frontend/widgets/mapWidget/views/google_map.php
+++
@@ -1,7 +0,0 @@
-<div id="<?=$map_id ?>" style="width:<?= $this->width ?>px;height:<?= $this->height ?>px;"></div>
-<script type="text/javascript">
-    $(function() {
-        var map = new HGoogleMap();
-        map.create("<?= $map_id ?>", "<?= $this->location ?>");
-    });
-</script>

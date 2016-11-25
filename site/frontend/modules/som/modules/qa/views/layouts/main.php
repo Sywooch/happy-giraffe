b0507928dbe0db7e00a68c622de9a32623460674
Roman Kopka <roman.kopka@yandex.ru>
2016-11-25 12:28:22 +0300

    sidebar

--- site/frontend/modules/som/modules/qa/views/layouts/main.php
+++ site/frontend/modules/som/modules/qa/views/layouts/main.php
@@ -26,21 +26,20 @@ $this->renderSidebarClip();
               	<div class="b-main_col-article position-rel clearfix">
                 	<?=$content?>
             	</div>
-                <aside style="margin-top: 35px" class="b-main_col-sidebar visible-md">
-                    <div class="sidebar-widget sidebar-widget__padding">
-                    	<?php $this->renderPartial('/_sidebar/ask', array());?>
-                        <?php //$this->renderPartial('/_sidebar/personal', array());?>
-                        <?php $this->renderPartial('/_sidebar/menu', array());?>
-                        <?php $this->renderPartial('/_sidebar/top', array('member' => null, 'titlePrefix' => 'Знаток'));?>
-                    </div>
-                    <?php /**
-                        if ('rating' !== Yii::app()->controller->id)
-                            $this->renderPartial('/_sidebar/rating', array());
-                    **/ ?>
-                    <?php $this->renderPartial('/_sidebar/hot', array());?>
-                </aside>
         	</div>
+            <aside style="margin-top: 35px" class="b-main_col-sidebar visible-md">
+                <div class="sidebar-widget sidebar-widget__padding">
+                    <?php $this->renderPartial('/_sidebar/ask', array());?>
+                    <?php //$this->renderPartial('/_sidebar/personal', array());?>
+                    <?php $this->renderPartial('/_sidebar/menu', array());?>
+                    <?php $this->renderPartial('/_sidebar/top', array('member' => null, 'titlePrefix' => 'Знаток'));?>
+                </div>
+                <?php /**
+                if ('rating' !== Yii::app()->controller->id)
+                $this->renderPartial('/_sidebar/rating', array());
+                 **/ ?>
+                <?php $this->renderPartial('/_sidebar/hot', array());?>
+            </aside>
         </div>
     </div>
-</div>
 <?php $this->endContent(); ?>

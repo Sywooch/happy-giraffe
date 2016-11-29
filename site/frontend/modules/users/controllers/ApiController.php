5518cd7ac40971fa8724d4e8aa18582b81e76a01
Sergey Gubarev <dev_seregik@mail.ru>
2016-11-29 15:16:21 +0300

    user name unicode

--- site/frontend/modules/users/controllers/ApiController.php
+++ site/frontend/modules/users/controllers/ApiController.php
@@ -64,12 +64,12 @@ class ApiController extends \site\frontend\components\api\ApiController
         $user = $this->getModel('\site\frontend\modules\users\models\User', $id, 'editSettings');
         foreach($attributes AS $attr => &$at)
         {
-            $at = htmlentities($at, ENT_QUOTES, 'UTF-8');
-
             if ($attr == 'first_name' || $attr == 'last_name' || $attr == 'middle_name')
             {
                 $at = \Filters::unicodeToString($at);
             }
+
+            $at = htmlentities($at, ENT_QUOTES, 'UTF-8');
         }
 
         $user->attributes = $attributes;

5c153dad3a06e03d78e57614870406b86aa602b1
nanodesu88 <archon.saratov@gmail.com>
2016-12-08 11:16:24 +0300

    HAG-380

--- site/frontend/modules/som/modules/qa/controllers/ApiController.php
+++ site/frontend/modules/som/modules/qa/controllers/ApiController.php
@@ -22,102 +22,102 @@ class ApiController extends \site\frontend\components\api\ApiController
 {
     public static $answerModel = QaAnswer::class;
     public static $questionModel = QaQuestion::class;
-
+    
     protected function beforeAction($action)
     {
         \TimeLogger::model()->startTimer(date('j D в H:i:s') . ' [ACTION] ' . $action->id);
-
+        
         return parent::beforeAction($action);
     }
-
+    
     public function actions()
     {
-        return \CMap::mergeArray(parent::actions(), array(
+        return \CMap::mergeArray(parent::actions(), [
             // QaAnswer
-            'editAnswer' => array(
+            'editAnswer' => [
                 'class' => 'site\frontend\components\api\EditAction',
                 'modelName' => self::$answerModel,
                 'checkAccess' => 'updateQaAnswer',
-            ),
-            'removeAnswer' => array(
+            ],
+            'removeAnswer' => [
                 'class' => 'site\frontend\components\api\SoftDeleteAction',
                 'modelName' => self::$answerModel,
                 'checkAccess' => 'removeQaAnswer',
-            ),
-            'restoreAnswer' => array(
+            ],
+            'restoreAnswer' => [
                 'class' => 'site\frontend\components\api\SoftRestoreAction',
                 'modelName' => self::$answerModel,
                 'checkAccess' => 'restoreQaAnswer',
-            ),
-
+            ],
+            
             // QaQuestion
-            'removeQuestion' => array(
+            'removeQuestion' => [
                 'class' => 'site\frontend\components\api\SoftDeleteAction',
                 'modelName' => self::$questionModel,
                 'checkAccess' => 'removeQaQuestion',
-            ),
-            'restoreQuestion' => array(
+            ],
+            'restoreQuestion' => [
                 'class' => 'site\frontend\components\api\SoftRestoreAction',
                 'modelName' => self::$questionModel,
                 'checkAccess' => 'restoreQaQuestion',
-            ),
-        ));
+            ],
+        ]);
     }
-
-    public function actionCreateAnswer($questionId, $text, $answerId = NULL)
+    
+    public function actionCreateAnswer($questionId, $text, $answerId = null)
     {
         /** @var $user \WebUser */
         $user = \Yii::app()->user;
-
+        
         /** @var $question QaQuestion */
         $question = QaQuestion::model()->findByPk($questionId);
-
+        
         if (is_null($question) || !$question->checkCustomAccessByAnswered($user->getId())) {
             throw new \CHttpException(403, 'Access Denied');
         }
-
+        
         /** @var \site\frontend\modules\som\modules\qa\models\QaAnswer $answer */
         $answer = new self::$answerModel();
         $answer->attributes = [
             'questionId' => $questionId,
             'text' => $text,
         ];
-
+        
         if ($answer->validate()) {
             // Если ответил специалист то не нужно сразу отсылать оповещение и показывать ответ, т.к. на этой дело висит таймаут
             if ($question->category->isPediatrician() && $answer->author->isSpecialistOfGroup(SpecialistGroup::DOCTORS)) {
                 $answer->isPublished = false;
             }
         }
-
+        
         if (!is_null($answerId) && QaAnswer::model()->exists('id=' . $answerId)) {
             $answer->setAttribute('root_id', $answerId);
         }
-
+        
         $this->success = $answer->save();
         $this->data = $answer;
     }
-
+    
     public function actionGetAnswers($questionId)
     {
         $question = QaQuestion::model()->findByPk($questionId);
-
+        
         ContentBehavior::$active = true;
-
+        
         $answers = QaManager::getAnswers($question);
-
+        
         ContentBehavior::$active = false;
-
-        $votes = QaAnswerVote::model()->answers($answers)->user(\Yii::app()->user->id)->findAll(array('index' => 'answerId'));
-
-        $_answers = array();
-
+        
+        $votes = QaAnswerVote::model()->answers($answers)->user(\Yii::app()->user->id)->findAll(['index' => 'answerId']);
+        
+        $_answers = [];
+        
         foreach ($answers as $answer) {
             /** @var $answer QaAnswer */
             $_answer = $answer->toJSON();
-            $_answer['canEdit'] = \Yii::app()->user->checkAccess('updateQaAnswer', array('entity' => $answer));
-            $_answer['canRemove'] = \Yii::app()->user->checkAccess('removeQaAnswer', array('entity' => $answer));
-            $_answer['canVote'] = \Yii::app()->user->checkAccess('voteAnswer', array('entity' => $answer));
+            $_answer['canEdit'] = \Yii::app()->user->checkAccess('updateQaAnswer', ['entity' => $answer]);
+            $_answer['canRemove'] = \Yii::app()->user->checkAccess('removeQaAnswer', ['entity' => $answer]);
+            $_answer['canVote'] = \Yii::app()->user->checkAccess('voteAnswer', ['entity' => $answer]);
             $_answer['isVoted'] = isset($votes[$answer->id]);
             $_answer['isAdditional'] = $answer->isAdditional();
             $_answer['isAnswerToAdditional'] = $answer->isAnswerToAdditional();
@@ -125,51 +125,54 @@ class ApiController extends \site\frontend\components\api\ApiController
             $_answer['root_id'] = $answer->root_id;
             $_answers[] = $_answer;
         }
-
-        $this->data = array(
+        
+        $this->data = [
             'answers' => $_answers,
             'question' => $question->toJSON(),
-            'canAnswer' => \Yii::app()->user->checkAccess('createQaAnswer', array('question' => $this->getModel(self::$questionModel, $questionId))),
-        );
+            'canAnswer' => \Yii::app()->user->checkAccess('createQaAnswer', ['question' => $this->getModel(self::$questionModel, $questionId)]),
+        ];
         $this->success = true;
     }
-
+    
     public function actionGetChildAnswer($answerId)
     {
         $this->data = QaAnswer::model()->apiWith('user')->findAll('root_id=' . $answerId);
         $this->success = true;
     }
-
+    
     public function actionVote($answerId)
     {
         $answer = $this->getModel(self::$answerModel, $answerId);
-        if (!\Yii::app()->user->checkAccess('voteAnswer', array('entity' => $answer))) {
+        if (!\Yii::app()->user->checkAccess('voteAnswer', ['entity' => $answer])) {
             throw new \CHttpException(403);
         }
         $this->data = VotesManager::changeVote(\Yii::app()->user->id, $answerId);
         $this->success = $this->data !== false;
     }
-
+    
     /**
      * @param \CAction $action
      * @todo переделать в поведение
      */
     public function afterAction($action)
     {
-        $types = array(
+        $types = [
             'vote' => \CometModel::QA_VOTE,
             'createAnswer' => \CometModel::QA_NEW_ANSWER,
             'removeAnswer' => \CometModel::QA_REMOVE_ANSWER,
             'restoreAnswer' => \CometModel::QA_RESTORE_ANSWER,
             'editAnswer' => \CometModel::QA_EDIT_ANSWER,
-        );
-
+        ];
+        
         if ($this->success == true && in_array($action->id, array_keys($types))) // @fixme isset, array_key_exists?
         {
             $data = ($this->data instanceof \IHToJSON) ? $this->data->toJSON() : $this->data;
-            $this->send(AnswersWidget::getChannelIdByQuestion($this->data->questionId), $data, $types[$action->id]);
+            
+            if ($this->data instanceof QaAnswer && $this->data->isPublished) {
+                $this->send(AnswersWidget::getChannelIdByQuestion($this->data->questionId), $data, $types[$action->id]);
+            }
         }
-
+        
         parent::afterAction($action);
     }
 }

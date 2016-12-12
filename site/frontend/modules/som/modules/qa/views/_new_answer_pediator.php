99e3d52556c2cb2f6b8c89175c394b9ab0fc19f9
shtorm <emilvililyaev@gmail.com>
2016-12-12 15:52:59 +0300

    промежуточный

--- site/frontend/modules/som/modules/qa/views/_new_answer_pediator.php
+++ site/frontend/modules/som/modules/qa/views/_new_answer_pediator.php
@@ -1,43 +1,19 @@
 <?php
-
-use site\frontend\modules\som\modules\qa\models\qaTag\Enum;
-
 /**
-  * @var \site\frontend\modules\som\modules\qa\models\QaCTAnswer $data
-  */
+ * @var \site\frontend\modules\som\modules\qa\models\QaCTAnswer $data
+ */
  ?>
- <li class="b-answer__item">
-    <div class="b-pediator-answer">
-        <div class="b-pediator-answer__left">
-            <div class="b-pediator-answer__ava b-pediator-answer__ava--theme-pink">
-                <a href="<?=$data->author->getUrl()?>" class="ava ava--theme-pedaitor ava--medium ava--medium_male">
-                    <img src="<?=$data->author->getAvatarUrl(40)?>" class="ava__img" />
-                </a>
-            </div>
-        </div>
-        <div class="b-pediator-answer__right b-pediator-answer__right--pink">
-            <div class="b-answer__header b-answer-header">
-            	<a href="<?=$data->author->getUrl()?>" class="b-answer-header__link"><?=$data->user->getFullName()?></a>
-                <?=HHtml::timeTag($data, array('class' => 'b-answer-header__time'))?>
-                <div class="b-answer-header__spezialisation"><?=$data->author->specialistProfile->getSpecsString()?></div>
-                <?php $this->widget('site\frontend\modules\som\modules\qa\widgets\answers\AnswerHeaderWidget', [
-                    'userId' => $data->author->id,
-                ]);?>
-            </div>
-            <div class="b-answer__body b-answer-body">
-                <p class="b-pediator-answer__text"><?=strip_tags($data->text)?></p>
-                <a href="<?=$data->question->url?>" class="b-text--link-color b-title--bold b-title--h9"><?=strip_tags($data->question->title)?></a>
-            </div>
-        </div>
-        <div class="b-pedaitor-answer__footer b-answer-footer b-answer-footer--pink">
-            <div class="b-pedaitor-answer__footer__item">
-            	<a href="<?=$this->createUrl('/som/qa/default/pediatrician', ['tagId' => $data->question->tag->id])?>" class="b-answer-footer__age b-text--link-color"><?=(new Enum())->getTitleForWeb($data->question->tag->name)?></a>
-            </div>
-            <div class="b-pedaitor-answer__footer__item"><a href="javascript:void(0);" class="b-answer-footer__comment"><?=$data->getQuestion()->getAnswerManager()->getAnswersCount()?></a>
-                <button type="button" class="btn-answer btn-answer--theme-green btn-answer--active">
-                	<span class="btn-answer__num btn-answer__num--theme-green">Спасибо 98</span>
-                </button>
-            </div>
-        </div>
+<div class="b-pediator-answer__right b-pediator-answer__right--pink">
+    <div class="b-answer__header b-answer-header">
+    	<a href="<?=$data->author->getUrl()?>" class="b-answer-header__link"><?=$data->user->getFullName()?></a>
+        <?=HHtml::timeTag($data, array('class' => 'b-answer-header__time'))?>
+        <div class="b-answer-header__spezialisation"><?=$data->author->specialistProfile->getSpecsString()?></div>
+        <?php $this->widget('site\frontend\modules\som\modules\qa\widgets\answers\AnswerHeaderWidget', [
+            'userId' => $data->author->id,
+        ]);?>
     </div>
-</li>
+    <div class="b-answer__body b-answer-body">
+        <p class="b-pediator-answer__text"><?=strip_tags($data->text)?></p>
+        <a href="<?=$data->question->url?>" class="b-text--link-color b-title--bold b-title--h9"><?=strip_tags($data->question->title)?></a>
+    </div>
+</div>
---
+++ site/frontend/modules/som/modules/qa/widgets/usersTop/views/new_view_users.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @var \site\frontend\modules\posts\modules\forums\widgets\usersTop\UsersTopWidget $this
+ * @var array $rows
+ */
+
+function getColorClassViewUsers($position)
+{
+    switch ($position)
+    {
+        case 1:
+            return 'b-sidebar__place--yellow';
+        case 2:
+            return 'b-sidebar__place--blue';
+        case 3:
+            return 'b-sidebar__place--green';
+        default:
+            return 'b-sidebar__place--white';
+    }
+}
+?>
+
+<div class="b-text--left b-margin--bottom_40">
+    <div class="b-sidebar-widget">
+        <div class="b-sidebar-widget__header b-sidebar-widget-header b-sidebar-widget-header--green">
+            <div class="b-sidebar-widget-header__title b-sidebar-widget-header-title b-sidebar-widget-header-title--white"><?=$this->getTitle()?></div>
+        </div>
+        <ul class="b-sidebar-widget__body b-sidebar b-sidebar-widget__body--green">
+        	<?php foreach ($rows as $i => $row): ?>
+            <li class="b-sidebar__item">
+                <div class="b-sidebar__place"><span class="<?=getColorClassViewUsers($i+1)?>"><?=$i > 2 ? $i+1 : ''?></span>
+                </div>
+                <div class="b-sidebar__content b-sidebar-content b-sidebar-content--mod">
+                    <div class="b-sidebar-content__box">
+                        <a href="<?=$row['user']->profileUrl?>" class="ava ava--style ava--medium ava--default">
+                            <img src="<?=$row['user']->avatarUrl?>" class="ava__img" />
+                        </a>
+                    </div>
+                    <div class="b-sidebar-content__box">
+                    	<a href="<?=$row['user']->profileUrl?>" class="b-sidebar-content__link b-sidebar-content__link--white"><?=$row['user']->fullName?></a>
+                    	<span class="b-sidebar-content__answer b-sidebar-content__answer--white"> Ответы <?=$row['answers']?></span>
+                        <span class="b-sidebar-content__thank b-sidebar-content__thank--white"><?=$row['votes']?></span>
+                    </div>
+                </div>
+                <div class="b-sidebar__right b-text--right">
+                    <div class="b-sidebar__num b-sidebar__num--white"><?=intval($row['score'])?></div>
+                    <div class="b-sidebar__balls b-sidebar__balls--white">баллов</div>
+                </div>
+            </li>
+        	<?php endforeach; ?>
+        </ul>
+    </div>
+</div>
+

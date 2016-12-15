0ab4a0ae1beca4e64d8ba9c7c6fbb38204cf5ac3
Roman Kopka <roman.kopka@yandex.ru>
2016-12-15 18:10:37 +0300

    fix sidebar

--- site/frontend/modules/som/modules/qa/widgets/usersTop/views/new_view_specialists.php
+++ site/frontend/modules/som/modules/qa/widgets/usersTop/views/new_view_specialists.php
@@ -27,24 +27,26 @@ function getColorClassViewSpecialists($position)
         <ul class="b-sidebar-widget__body b-sidebar b-sidebar-widget__body--white">
             <?php foreach ($rows as $i => $row): ?>
             <li class="b-sidebar__item">
-                <div class="b-sidebar__place"><span class="<?=getColorClassViewSpecialists($i+1)?>"><?=$i > 2 ? $i+1 : ''?></span>
-                </div>
-                <div class="b-sidebar__content b-sidebar-content b-sidebar-content--mod">
-                    <div class="b-sidebar-content__box">
-                     	<a href="<?=$row['user']->profileUrl?>" class="ava ava--style ava--medium ava--default">
-                        	<img src="<?=$row['user']->avatarUrl?>" class="ava__img" />
-                        </a>
+                <a href="<?=$row['user']->profileUrl?>" class="b-sidebar-widget__table">
+                    <div class="b-sidebar__place"><span class="<?=getColorClassViewSpecialists($i+1)?>"><?=$i > 2 ? $i+1 : ''?></span>
                     </div>
-                    <div class="b-sidebar-content__box">
-                    	<a href="<?=$row['user']->profileUrl?>" class="b-sidebar-content__link"><?=$this->formattedName($row['user']->fullName)?></a>
-                    	<span class="b-sidebar-content__answer b-sidebar-content__answer--grey"> Ответы <?=$row['answers']?></span>
-                    	<span class="b-sidebar-content__thank b-sidebar-content__thank--grey"><?=$row['votes']?></span>
+                    <div class="b-sidebar__content b-sidebar-content b-sidebar-content--mod">
+                        <div class="b-sidebar-content__box">
+                            <span class="ava ava--style ava--medium ava--default">
+                                <img src="<?=$row['user']->avatarUrl?>" class="ava__img" />
+                            </span>
+                        </div>
+                        <div class="b-sidebar-content__box">
+                            <span class="b-sidebar-content__link"><?=$this->formattedName($row['user']->fullName)?></span>
+                            <span class="b-sidebar-content__answer b-sidebar-content__answer--grey"> Ответы <?=$row['answers']?></span>
+                            <span class="b-sidebar-content__thank b-sidebar-content__thank--grey"><?=$row['votes']?></span>
+                        </div>
                     </div>
-                </div>
-                <div class="b-sidebar__right b-text--right">
-                    <div class="b-sidebar__num b-sidebar__num--grey"><?=intval($row['score'])?></div>
-                    <div class="b-sidebar__balls b-sidebar__balls--grey">баллов</div>
-                </div>
+                    <div class="b-sidebar__right b-text--right">
+                        <div class="b-sidebar__num b-sidebar__num--grey"><?=intval($row['score'])?></div>
+                        <div class="b-sidebar__balls b-sidebar__balls--grey">баллов</div>
+                    </div>
+                </a>
             </li>
         	<?php endforeach; ?>
         </ul>

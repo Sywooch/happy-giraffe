9097bdd30c7a21ca7a6b09ff95c23e065a2933ab
choojoy-work <nikita@happy-giraffe.ru>
2017-01-17 13:11:12 +0400

    Возвращаем логин

---
+++ site/frontend/modules/signup/models/SocialLocation.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Никита
+ * @date 17/01/17
+ */
+
+namespace site\frontend\modules\signup\models;
+
+
+class SocialLocation extends \EMongoDocument
+{
+    public $service;
+    public $serviceId;
+    public $data;
+
+    public function getCollectionName()
+    {
+        return 'social_location';
+    }
+}

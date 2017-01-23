fee48960d4662a3376eeb8723748b9142aa38aa9
shtorm <emilvililyaev@gmail.com>
2017-01-23 15:27:10 +0300

    bug fix

--- site/frontend/modules/som/modules/qa/views/default/_view.php
+++ site/frontend/modules/som/modules/qa/views/default/_view.php
@@ -15,7 +15,14 @@ $breadcrumbs = [
     'Педиатр' => ['/som/qa/default/pediatrician'],
 ];
 
-$breadcrumbs[$question->tag->name] = $this->createUrl('/som/qa/default/pediatrician', ['tab' => 'new', 'tagId' => $question->tag->id]);
+$tag = $question->tag;
+
+if (!is_null($question->attachedChild)) {
+    $arrFooterData = $question->attChild->getAnswerFooterData();
+    $tag = $arrFooterData['tag'];
+}
+
+$breadcrumbs[$tag->name] = $this->createUrl('/som/qa/default/pediatrician', ['tab' => 'new', 'tagId' => $tag->id]);
 $breadcrumbs[] = CHtml::encode($question->title);
 
 ?>
@@ -65,14 +72,6 @@ $breadcrumbs[] = CHtml::encode($question->title);
             <div class="b-open-question__body">
                 <span class="b-title--h1 b-title--bold b-text-color--blue-link"><?= CHtml::encode($question->title) ?></span>
                 <div class="b-open-question__wrapper b-question-wrapper">
-                    <?php
-      			       $tag = $question->tag;
-
-          			   if (!is_null($question->attachedChild)) {
-                    	   $arrFooterData = $question->attChild->getAnswerFooterData();
-                    	   $tag = $arrFooterData['tag'];
-          			   }
-      			   ?>
                     <?php if (!is_null($tag)) { ?>
                     <div class="b-question-wrapper__item">
                         <a href="<?= $this->createUrl('/som/qa/default/index/', ['categoryId' => $question->category->id, 'tagId' => $tag->id]) ?>"

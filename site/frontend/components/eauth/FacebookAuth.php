10b1fdb52c69f0dbab82423ea700fc2259cb9486
choojoy-work <nikita@happy-giraffe.ru>
2017-01-26 12:36:00 +0400

    Возвращаем логин

--- site/frontend/components/eauth/FacebookAuth.php
+++ site/frontend/components/eauth/FacebookAuth.php
@@ -58,7 +58,7 @@ class FacebookAuth extends FacebookOAuthService
         }
         $this->attributes['country_id'] = $countryModel->id;
 
-        if (preg_match('#^[а-яА-Я ]+$#', $location->name)) {
+        if (preg_match('#^[а-яА-Я -]+$#u', $location->name)) {
             $pieces = explode(', ', $location->name);
             if (count($pieces) == 1) {
                 $cityName = $pieces;

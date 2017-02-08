3323e1243ffd792914d40f39b054b5cd178e0014
shtorm <emilvililyaev@gmail.com>
2017-02-08 18:10:35 +0300

    hot fix activity

--- site/frontend/modules/som/modules/activity/widgets/views/comment_json.php
+++ site/frontend/modules/som/modules/activity/widgets/views/comment_json.php
@@ -1,21 +1,13 @@
-<?php
-
-use site\frontend\modules\som\modules\activity\widgets\ActivityWidget;
-
-/*@var $data \site\frontend\modules\som\modules\qa\models\QaAnswer */
-
-?>
-
 <div class="b-article_in clearfix">
     <div class="comments comments__buble comments__anonce">
         <div class="comments_hold">
             <div class="comments_li comments_li__lilac">
                 <div class="comments_i clearfix">
                     <div class="comments_frame">
-                        <div onclick="location.href='<?=$data->question->url?>'" class="comments_link">
+                        <div onclick="location.href='<?=$data->dataArray['url']?>'" class="comments_link">
                             <div class="comments_cont">
                                 <div class="wysiwyg-content">
-                                    <?= $data->text ?>
+                                    <?= $data->dataArray['text'] ?>
                                 </div>
                             </div>
                         </div>
@@ -25,6 +17,10 @@ use site\frontend\modules\som\modules\activity\widgets\ActivityWidget;
         </div>
     </div>
     <div class="b-post-anonce-s">
+        <?php if ($data->dataArray['content']['cover']) {
+            ?><div class="b-post-anonce-s_img"><img src="<?= $data->dataArray['content']['cover'] ?>" alt="Заголовок статьи"></div><?php
+        }
+        ?>
         <div class="b-post-anonce-s_hold">
             <div class="b-post-anonce-s_header clearfix">
                 <a href="<?= $user->profileUrl ?>" class="ava ava__<?= $user->gender ? '' : 'fe' ?>male ava__small">
@@ -32,9 +28,9 @@ use site\frontend\modules\som\modules\activity\widgets\ActivityWidget;
                     <img alt="" src="<?= $user->avatarUrl ?>" class="ava_img">
                 </a>
                 <a href="<?= $user->profileUrl ?>" class="b-post-anonce-s_author"><?= $user->fullName ?></a>
-                <?= HHtml::timeTagByOptions($data->dtimeCreate, array('id' => 'activityCommentTime' . $data->id, 'class' => 'tx-date')) ?>
+                <?= HHtml::timeTagByOptions($data->dataArray['content']['dtimeCreate'], array('id' => 'activityCommentTime' . $data->id, 'class' => 'tx-date')) ?>
             </div>
-            <a href="<?=$data->question->url?>" class="b-post-anonce-s_t"><?=$data->question->title?></a>
+            <a href="<?= $data->dataArray['content']['url'] ?>" class="b-post-anonce-s_t"><?= $data->dataArray['content']['title'] ?></a>
         </div>
     </div>
 </div>

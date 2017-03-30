2239ef3e1bf58271403e442781eb10ab3896961b
toy-php <seymus007@gmail.com>
2017-03-30 11:56:06 +0300

    Убрал редирект, т.к. происходила двойная переадресация.

--- site/frontend/modules/specialists/modules/pediatrician/controllers/DefaultController.php
+++ site/frontend/modules/specialists/modules/pediatrician/controllers/DefaultController.php
@@ -135,13 +135,6 @@ class DefaultController extends \LiteController
             $this->render('accessDenied', ['displayType' => "accessDenied"]);
         }
 
-        if (!$question->checkAccessByViewQuestion($user->getId()))
-        {
-            $nextQuestion = QaManager::getNextQuestion($questionId, \Yii::app()->user->id);
-            $goTo = ($nextQuestion) ? $this->createUrl('/specialists/pediatrician/default/answer', ['questionId' => $nextQuestion->id]) : $this->createUrl('/specialists/pediatrician/default/questions');
-            $this->redirect($goTo);
-        }
-
         $this->render('answer', compact('question'));
     }
 

d65d7e33d6a9ce7ed697d3f37f1a175595699604
Roman Kopka <roman.kopka@yandex.ru>
2017-06-26 13:39:59 +0300

    подключил нужные стили

--- site/frontend/views/layouts/common.php
+++ site/frontend/views/layouts/common.php
@@ -21,9 +21,11 @@
         <?php
         $cs = Yii::app()->clientScript;
         $cs
-            ->registerCssFile('/redactor/redactor.css')
-            ->registerCssFile('/stylesheets/common.css')
-            ->registerCssFile('/stylesheets/global.css');
+//            ->registerCssFile('/redactor/redactor.css')
+//            ->registerCssFile('/stylesheets/common.css')
+//            ->registerCssFile('/stylesheets/global.css')
+            ->registerCssFile('/lite/css/dev/all.css')
+            ->registerCssFile('/app/builds/static/css/separate-css-sample.min.css');
             
         if(!$cs->useAMD)
             $cs

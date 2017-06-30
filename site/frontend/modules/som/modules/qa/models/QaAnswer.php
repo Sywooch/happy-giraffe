cd9150c7f485d82881d21d3ef5e70474c3e12161
toy-php <seymus007@gmail.com>
2017-06-30 13:11:09 +0300

    Добавлены тэги в список разрешенных для редактора

--- site/frontend/modules/som/modules/qa/models/QaAnswer.php
+++ site/frontend/modules/som/modules/qa/models/QaAnswer.php
@@ -205,7 +205,7 @@ class QaAnswer extends \HActiveRecord implements \IHToJSON
                 'attributes' => ['text'],
                 'options' => [
                     // 'AutoFormat.Linkify'    => true, @todo Sergey Gubarev: User warning: Cannot enable Linkify injector because a is not allowed
-                    'HTML.AllowedElements'  => ['h2', 'h3', 'h4', 'p', 'strike', 'b', 'em', 'i', 'img', 'br']
+                    'HTML.AllowedElements'  => ['h2', 'h3', 'h4', 'p', 'strike', 'b', 'em', 'i', 'img', 'br', 'strong', 'del']
                 ]
             ],
             'RatingBehavior' => [
